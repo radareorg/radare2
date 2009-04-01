b0b25a6eeac9bfe91109300e3375b77bf523161a
pancake <pancake@nopcode.org>
2009-04-01 01:30:36 +0000

    * Fix recursive include issue in build   - Oops O:) * Some fixups at ACR level for allowing --without-vala flag properly * Added empty r_sys_cmd() APIs for r_util

---
+++ libr/util/sys.c
@@ -0,0 +1,11 @@
+/* TODO: import stuff fron bininfo/p/bininfo_addr2line */
+
+char *r_sys_cmd_str(const char *cmd)
+{
+	return NULL;
+}
+
+char *r_sys_cmd_strf(const char *cmd, ...)
+{
+	return NULL;
+}
