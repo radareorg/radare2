d62886064a55aadd8385e417893f445734695a76
pancake <pancake@nopcode.org>
2009-07-16 09:24:17 +0000

    * Fix memory leak and simplify the code of the CSR disassembler

--- libr/asm/arch/csr/csr_disasm/dis.c
+++ libr/asm/arch/csr/csr_disasm/dis.c
@@ -14,7 +14,7 @@
 
 #include <r_types.h>
 
-static struct state *get_state(void)
+static inline struct state *get_state(void)
 {
 	return &_state;
 }
@@ -47,199 +47,51 @@ static void decode_unknown(struct state *s, struct directive *d)
 
 static int decode_fixed(struct state *s, struct directive *d)
 {
+	*d->d_asm='\0';
 	switch (i2u16(&d->d_inst)) {
 	case INST_NOP:
 		if (s->s_prefix)
 			return 0;
-		
 		s->s_nop++;
 		strcpy(d->d_asm, "nop");
-		return 1;
-
-	case INST_BRK:
-		strcpy(d->d_asm, "brk");
-		return 1;
-
-	case INST_SLEEP:
-		strcpy(d->d_asm, "sleep");
-		return 1;
-
-	case INST_SIF:
-		strcpy(d->d_asm, "sif");
-		return 1;
-
-	case INST_BC:
-		strcpy(d->d_asm, "bc");
-		return 1;
-
-	case INST_BRXL:
-		strcpy(d->d_asm, "brxl");
-		return 1;
-
-	case INST_U:
-		strcpy(d->d_asm, "");
-		s->s_u = 1;
-		return 1;
-
-	case INST_RTS:
-		strcpy(d->d_asm, "rts");
-		return 1;
+		break;
+	case INST_BRK: strcpy(d->d_asm, "brk"); break;
+	case INST_SLEEP: strcpy(d->d_asm, "sleep"); break;
+	case INST_SIF: strcpy(d->d_asm, "sif"); break;
+	case INST_BC: strcpy(d->d_asm, "bc"); break;
+	case INST_BRXL: strcpy(d->d_asm, "brxl"); break;
+	case INST_U: strcpy(d->d_asm, ""); s->s_u = 1; break;
+	case INST_RTS: strcpy(d->d_asm, "rts"); break;
 	}
-
-	return 0;
+	return d->d_asm[0]!=0;
 }
 
 static char *regname(int reg)
 {
 	switch (reg) {
-	case REG_AH:
-		return "AH";
-		break;
-
-	case REG_AL:
-		return "AL";
-		break;
-	
-	case REG_X:
-		return "X";
-		break;
-
-	case REG_Y:
-		return "Y";
-		break;
-
-	default:
-		return NULL;
+	case REG_AH: return "AH";
+	case REG_AL: return "AL";
+	case REG_X: return "X";
+	case REG_Y: return "Y";
 	}
+	return NULL;
 }
 
 static int get_num(int num, int shift)
 {
-	int tmp;
-	char x;
-
-	x = (char) ((num >> shift) & 0xff);
-	tmp = x;
-	tmp <<= shift;
-
-	return tmp;
+	char x = (char) ((num >> shift) & 0xff);
+	return (int)(x<<shift);
 }
 
 static int get_operand(struct state *s, struct directive *d)
 {
-	int total = 0;
-
-	total += get_num(d->d_inst.in_operand, 0);
-
+	int total = get_num(d->d_inst.in_operand, 0);
 	if (s->s_prefix)
 		total += get_num(s->s_prefix_val, 8);
-
 	if (s->s_prefix == 2)
 		total += get_num(s->s_prefix_val, 16);
-
 	return total;
 }
-
-#if 0
-static int label_off(struct directive *d)
-{
-#if 1
-//	int off = get_operand(d);
-	int off = d->d_operand;
-
-	int lame = off & 0x80;
-
-	/* XXX WTF? */
-	if (!d->d_prefix) {
-		off = (char) (off &0xff);
-	} else if (d->d_prefix == 1) {
-		off = (short) (off & 0xffff);
-
-		if (lame)
-			off -= 0x100;
-
-	} else {
-		off = (int) (off & 0xffffff);
-
-		if (off & 0x800000)
-			off |= 0xff000000;
-
-		if (off & 0x8000)
-			off -= 0x10000;
-
-		if (lame)
-			off -= 0x100;
-	}
-#endif
-//	int off = d->d_operand;
-
-	return d->d_off + off;
-}
-#endif
-
-#if 0
-static void label_add_ref(struct label *l, struct directive *d)
-{
-	struct directive **ptr = l->l_refs;
-
-	while (*ptr) {
-		assert((unsigned long) ptr - (unsigned long) l->l_refs <
-		       sizeof(l->l_refs));
-		
-		ptr++;
-	}
-	*ptr = d;
-	l->l_refc++;
-}
-#endif
-
-#if 0
-/* XXX slow */
-static struct label *find_label_add(struct state *s, struct directive *d)
-{
-	int off = label_off(d);
-	struct label *ptr = s->s_labels.l_next;
-	struct label *slot = &s->s_labels;
-
-	/* find */
-	while (ptr) {
-		if (ptr->l_off == off)
-			return ptr;
-
-		if (ptr->l_off > off)
-			break;
-		
-		slot = ptr;
-		ptr = ptr->l_next;
-	}
-
-	/* add */
-	ptr = malloc(sizeof(*ptr));
-	if (!ptr) {
-		perror("malloc()");
-		return NULL;
-	}
-	memset(ptr, 0, sizeof(*ptr));
-	sprintf(ptr->l_name, "0x%x", off); //L%d", s->s_labelno++);
-	ptr->l_off = off;
-
-	ptr->l_next = slot->l_next;
-	slot->l_next = ptr;
-
-	return ptr;
-}
-
-static void add_label(struct state *s, struct directive *d)
-{
-	struct label *l;
-
-	l = find_label_add(s, d);
-	assert(l);
-
-	label_add_ref(l, d);
-	strcat(d->d_asm, l->l_name);
-}
-#endif
 
 static int decode_known(struct state *s, struct directive *d)
 {
@@ -285,43 +137,35 @@ static int decode_known(struct state *s, struct directive *d)
 			op	= "st";
 			regn	= "FLAGS";
 			break;
-
 		case 2:
 			op	= "st";
 			regn	= "UX";
 			break;
-
 		case 3:
 			op	= "st";
 			regn	= "UY";
 			break;
-
 		case 5:
 			op	= "ld";
 			regn	= "FLAGS";
 			break;
-
 		case 6:
 			op	= "ld";
 			regn	= "UX";
 			break;
-
 		case 7:
 			op	= "ld";
 			regn	= "UY";
 			break;
-
 		case 0xa:
 			op	= "st";
 			regn	= "XH";
 			break;
-
 		case 0xd:
 			op	= "rti";
 			regn	= "";
 			rti	= 1;
 			break;
-
 		case 0xe:
 			op	= "ld";
 			regn	= "XH";
@@ -336,7 +180,6 @@ static int decode_known(struct state *s, struct directive *d)
 		idx = 1;
 		imm = 1;
 		break;
-
 	case 2:
 		if (in->in_mode == DATA_MODE_IMMEDIATE) {
 			op = "print";
@@ -349,7 +192,6 @@ static int decode_known(struct state *s, struct directive *d)
 			idx = 1;
 		}
 		break;
-
 	case 3:
 		op = "add";
 		reg = 1;
@@ -357,7 +199,6 @@ static int decode_known(struct state *s, struct directive *d)
 		idx = 1;
 		imm = 1;
 		break;
-
 	case 4:
 		op = "addc";
 		reg = 1;
@@ -365,7 +206,6 @@ static int decode_known(struct state *s, struct directive *d)
 		idx = 1;
 		imm = 1;
 		break;
-
 	case 5:
 		op = "sub";
 		reg = 1;
@@ -373,7 +213,6 @@ static int decode_known(struct state *s, struct directive *d)
 		idx = 1;
 		imm = 1;
 		break;
-
 	case 6:
 		op = "subc";
 		reg = 1;
@@ -381,7 +220,6 @@ static int decode_known(struct state *s, struct directive *d)
 		idx = 1;
 		imm = 1;
 		break;
-
 	case 7:
 		op = "nadd";
 		reg = 1;
@@ -389,7 +227,6 @@ static int decode_known(struct state *s, struct directive *d)
 		idx = 1;
 		imm = 1;
 		break;
-
 	case 8:
 		op = "cmp";
 		reg = 1;
@@ -397,37 +234,28 @@ static int decode_known(struct state *s, struct directive *d)
 		idx = 1;
 		imm = 1;
 		break;
-
 	case 0x9:
 		switch (in->in_reg) {
 		case 0:
-			if (s->s_u)
-				op = "umult";
-			else
-				op = "smult";
+			if (s->s_u) op = "umult";
+			else op = "smult";
 			imm = 1;
 			s->s_u = 0;
 			idx = 1;
 			ptr = 1;
 			break;
-
 		case 1:
-			if (s->s_u)
-				op = "udiv";
-			else
-				op = "sdiv";
+			if (s->s_u) op = "udiv";
+			else op = "sdiv";
 			s->s_u = 0;
 			imm = 1;
 			break;
-
 		case 2:
 			op = "tst";
 			imm = 1;
 			ptr = 1;
 			idx = 1;
-
 			break;
-
 		case 3:
 			branch = 1;
 			op = "bsr";
@@ -438,30 +266,24 @@ static int decode_known(struct state *s, struct directive *d)
 			break;
 		}
 		break;
-
 	case 0xa:
 		switch (in->in_reg) {
 		case 0:
 			op = "asl";
 			imm = 1;
 			break;
-		
 		case 1:
-			if (s->s_u)
-				op = "lsr";
-			else
-				op = "asr";
+			if (s->s_u) op = "lsr";
+			else op = "asr";
 			s->s_u = 0;
 			imm = 1;
 			idx = 1;
 			ptr = 1;
 			break;
-
 		case 2:
 			op = "rol";
 			imm = 1;
 			break;
-
 		case 3:
 			op = "ror";
 			imm = 1;
@@ -476,7 +298,6 @@ static int decode_known(struct state *s, struct directive *d)
 		idx = 1;
 		imm = 1;
 		break;
-
 	case 0xc:
 		op = "and";
 		reg = 1;
@@ -484,7 +305,6 @@ static int decode_known(struct state *s, struct directive *d)
 		idx = 1;
 		imm = 1;
 		break;
-
 	case 0xd:
 		op = "xor";
 		reg = 1;
@@ -492,12 +312,10 @@ static int decode_known(struct state *s, struct directive *d)
 		idx = 1;
 		imm = 1;
 		break;
-
 	case 0xe:
 		branch = 1;
 		if (in->in_mode == ADDR_MODE_RELATIVE)
 			rel = 1;
-
 		switch (in->in_reg) {
 		case 0:
 			op = "bra";
@@ -514,44 +332,28 @@ static int decode_known(struct state *s, struct directive *d)
 		case 1:
 			op = "blt"; /* yummy */
 			break;
-
 		case 2:
 			op = "bpl";
 			break;
-	
 		case 3:
 			op = "bmi";
 			break;
 		}
 		break;
-
 	case 0xf:
 		branch = 1;
 		if (in->in_mode == ADDR_MODE_RELATIVE)
 			rel = 1;
-
 		switch (in->in_reg) {
-		case 0:
-			op = "bne";
-			break;
-
-		case 1:
-			op = "beq";
-			break;
-
-		case 2:
-			op = "bcc";
-			break;
-
-		case 3:
-			op = "bcs";
-			break;
+		case 0: op = "bne"; break;
+		case 1: op = "beq"; break;
+		case 2: op = "bcc"; break;
+		case 3: op = "bcs"; break;
 		}
 		break;
 	}
 
-	if (!op)
-		return 0;
+	if (!op) return 0;
 
 	if (ptr && in->in_mode == DATA_MODE_IMMEDIATE)
 		ptr = 0;
@@ -571,10 +373,7 @@ static int decode_known(struct state *s, struct directive *d)
 	sprintf(d->d_asm, "%s ", op);
 	if (reg) {
 		char *r = regn;
-
-		if (!r)
-			r = regname(in->in_reg);
-
+		if (!r) r = regname(in->in_reg);
 		if (!rti) {
 			strcat(d->d_asm, r);
 			strcat(d->d_asm, ", ");
@@ -583,10 +382,8 @@ static int decode_known(struct state *s, struct directive *d)
 	if (ptr) {
 		strcat(d->d_asm, "@");
 		rel = 0;
-	} else if (imm)
-		strcat(d->d_asm, "#");
-	if (idx && ptr)
-		strcat(d->d_asm, "(");
+	} else if (imm) strcat(d->d_asm, "#");
+	if (idx && ptr) strcat(d->d_asm, "(");
 
 	d->d_prefix = s->s_prefix;
 //	d->d_operand = get_operand(s, d);
@@ -597,15 +394,12 @@ static int decode_known(struct state *s, struct directive *d)
 			d->d_operand *= -1;
 			sign = "-";
 		}	
-	}
-	else {
+	} else {
 		d->d_operand = s->s_prefix_val | in->in_operand;
 		if (d->d_operand & 0x80) {
 			if (d->d_prefix) {
-				if (!rel)
-					d->d_operand -= 0x100;
-			} else
-				d->d_operand |= 0xff00;
+				if (!rel) d->d_operand -= 0x100;
+			} else d->d_operand |= 0xff00;
 		}
 	}
 #endif
@@ -618,34 +412,14 @@ static int decode_known(struct state *s, struct directive *d)
 	if (d->d_operand & 0xff0000)
 		fmtsz += 2;
 
-// can be cleaned, no need to fmtsz
+	// can be cleaned, no need to fmtsz
 	snprintf(fmt, sizeof(fmt), "%s0x%%.%dX", sign, fmtsz);
 	snprintf(tmp, sizeof(tmp), fmt, d->d_operand);
-
-#if 0
-	if (rel) {
-		//add_label(s, d);
-		/* XXX prefix */
-		if (s->s_nopd && strcmp(s->s_nopd->d_asm, "")) {
-			/* lets be conservative for now */
-			if (d->d_operand == 0x7F
-			    || d->d_operand == 0x7E)
-				strcpy(s->s_nopd->d_asm, "");
-			else
-				printf("\nWarning: nop before branch at %x\n",
-				       d->d_off);
-		}
-	} else
-#endif
-
-		strcat(d->d_asm, tmp);
+	strcat(d->d_asm, tmp);
 
 	if (idx) {
 		char *r = in->in_mode == DATA_MODE_INDEXED_X ? "X" : "Y";
-
-		if (regn)
-			r = "Y";
-
+		if (regn) r = "Y";
 		snprintf(tmp, sizeof(tmp), ", %s", r);
 		strcat(d->d_asm, tmp);
 		if (ptr)
@@ -673,102 +447,17 @@ static int decode_known(struct state *s, struct directive *d)
 void csr_decode(struct state *s, struct directive *d)
 {
 	int prefix = s->s_prefix;
-
-	if (decode_fixed(s, d))
-		goto out;
-
-	if (decode_known(s, d))
-		goto out;
-
-	decode_unknown(s, d);
-
-out:
+	if (!decode_fixed(s, d))
+		if (!decode_known(s, d))
+			decode_unknown(s, d);
 	if (s->s_prefix == prefix)
 		s->s_prefix_val = s->s_prefix = 0;
 }
 
-#if 0
-static void add_tabs(struct state *s, struct directive *d)
-{
-	int outlen = 0;
-	int len;
-	char *ptr = d->d_asm;
-	int pos;
-	int wanted = 8*4;
-
-	len = strlen(ptr);
-	for (pos = 0; pos < len; pos++, ptr++) {
-		if (*ptr == '\t') {
-			outlen += 8 - (pos % 8);
-		} else
-			outlen++;
-	}
-
-	wanted -= outlen;
-	assert(wanted > 0);
-
-	pos = wanted / 8;
-	if (wanted % 8)
-		pos++;
-
-	while (pos--)
-		strcat(d->d_asm, " ");
-}
-#endif
-
-#if 0
-static void add_comment(struct state *s, struct directive *d)
-{
-	char tmp[128];
-
-	add_tabs(s, d);
-
-	snprintf(tmp, sizeof(tmp), "; @0x%x 0x%x", d->d_off,
-		 *((uint16_t*)&d->d_inst));
-
-	strcat(d->d_asm, tmp);
-}
-
-static int read_text(struct state *s, struct directive *d)
-{
-	char tmp[128];
-	char *x;
-	unsigned int inst;
-	uint16_t *p;
-
-	while ((x = fgets(tmp, sizeof(tmp), s->s_in))) {
-		if (tmp[0] == '@')
-			break;
-	}
-	if (!x)
-		return 0;
-
-	if (sscanf(tmp, "@0x%x 0x%x", &d->d_off, &inst)
-	    != 2)
-		return 0;
-
-	p = (uint16_t*) &d->d_inst;
-	*p = (uint16_t) inst;
-
-	return 1;
-}
-#endif
-
 static int read_bin(struct state *s, struct directive *d)
 {
-	//int rd;
-
 	memcpy(&d->d_inst, s->s_buf, sizeof(d->d_inst));
-#if 0
-	rd = fread(&d->d_inst, sizeof(d->d_inst), 1, s->s_in);
-	if (rd == -1)
-		err(1, "read()");
-	if (rd == 0)
-		return 0;
-#endif
-
 	d->d_off = s->s_off++;
-
 	return 1;
 }
 
@@ -783,14 +472,12 @@ struct directive *next_inst(struct state *s)
 		return NULL;
 	}
 	memset(d, 0, sizeof(*d));
-
 #if 0
 	if (s->s_format)
 		rd = read_text(s, d);
 	else
 #endif
-		rd = read_bin(s, d);
-
+	rd = read_bin(s, d);
 	if (!rd) {
 		free(d);
 		return NULL;
@@ -798,14 +485,6 @@ struct directive *next_inst(struct state *s)
 
 	return d;
 }
-
-#if 0
-static void print_label(struct state *s, struct label *l)
-{
-	output(s, "\n%s:\t\t\t\t\t; refs: %d\n",
-	       l->l_name, l->l_refc);
-}
-#endif
 
 #if 0
 static void own(struct state *s)
@@ -893,7 +572,6 @@ static void own(struct state *s)
 }
 #endif
 
-// XXX lot of memory leaks
 int arch_csr_disasm(char *str, unsigned char *buf, ut64 seek)
 {
 	struct state *s = get_state();
@@ -903,71 +581,16 @@ int arch_csr_disasm(char *str, unsigned char *buf, ut64 seek)
 	s->s_off = seek;
 	s->s_out = NULL;
 	d = next_inst(s);
-	if (d == NULL)
-		return 0;
-
-	csr_decode(s, d);
+	if (d != NULL) {
+		csr_decode(s, d);
+		strcpy(str, d->d_asm);
+		free(d);
+	} else *str = '\0';
 #if 0
 	if (s->s_ff_quirk) {
 		sprintf(d->d_asm, "DC\t0x%x", i2u16(&d->d_inst));
 		s->s_ff_quirk = 0;
 	}
 #endif
-	strcpy(str, d->d_asm);
 	return 0;
 }
-#if 0
-static int main(int argc, char *argv[])
-{
-	struct state *s = get_state();
-	struct directive *d;
-	char tmp[128];
-	int i = 0;
-	unsigned char buf[128];
-
-	memset(s, 0, sizeof(*s));
-memcpy(buf, "\xFF\x27\x00\x10", 4);
-s->s_buf = &buf;
-
-#if 0
-	if (argc < 2) {
-		printf("Usage: %s <file> [format]\n", argv[0]);
-		exit(1);
-	}
-
-	if (argc > 2)
-		s->s_format = atoi(argv[2]);
-
-	s->s_fname = argv[1];
-
-	if ((s->s_in = fopen(s->s_fname, "r")) == NULL)
-		err(1, "fopen()");
-
-	snprintf(tmp, sizeof(tmp), "%s.xap", s->s_fname);
-	s->s_out = fopen(tmp, "w");
-	if (!s->s_out)
-		err(1, "fopen()");
-#endif
-	s->s_off = 0;
-	s->s_out = -1;
-for(i=0;i<2;i++) {
-	d = next_inst(s);
-	decode(s, d);
-	if (s->s_ff_quirk) {
-		//strcpy(last->d_asm, "DC\tH'8000");
-		sprintf(d->d_asm, "DC\t0x%x", i2u16(&d->d_inst));
-		s->s_ff_quirk = 0;
-	}
-	printf("ASM : %s\n", d->d_asm);
-}
-
-#if 0
-	own(s);
-
-	fclose(s->s_in);
-	fclose(s->s_out);
-#endif
-
-	exit(0);
-}
-#endif
