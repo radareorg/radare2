8bf75e93d67da02ffb29d76590ca9c891d5e04f9
pancake <pancake@nopcode.org>
2009-08-22 02:03:58 +0000

    * Remove .orig files * Add MKPLUGINS=mk/hg-utils.mk in the default .acr template * Added hg-locdiff and hg-help, enhaced hg-miss

--- mk/hg-utils.mk
+++ mk/hg-utils.mk
@@ -1,4 +1,20 @@
+# radare :: pancake // nopcode.org
+# Makefile helpers for mercurial
+
 hg-miss:
-	@-hg st . | grep -e vala$$ | grep ^?
-	@-hg st . | grep -e \\.c$$ | grep -v vapi | grep ^?
-	@true
+	@-hg st . | grep -e vala$$ -e mk$$ | grep ^? | grep -v config-user || true
+	@-hg st . | grep -e \\.c$$ | grep -v vapi | grep ^? || true
+	@-hg st . | grep -e \\.h$$ | grep -v vapi | grep ^? | grep -v r_userconf || true
+
+FILES?=
+hg-locdiff:
+	@A=`hg diff ${FILES} | grep -v '+++' | grep ^+ |wc -l` ; \
+	B=`hg diff ${FILES} | grep -v -- '---' | grep ^- |wc -l` ; \
+	echo $$((A-B))
+
+hg-help:
+	@echo "hg-utils.mk mercurial utilities"
+	@echo "-----------.-------------------"
+	@echo "hg-miss    | list interesting missing files"
+	@echo "hg-locdiff | count the difference of LOCs for current commit or FILES"
+	@echo "           $$ hg-locdiff \"FILES=Makefile foo.c\""
