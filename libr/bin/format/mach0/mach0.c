94df3a38f7f34da9bccd597ba85305f3f197792f
Nibble <nibble.ds@gmail.com>
2010-01-10 14:13:41 +0100

    * Some work in mach0 plugin * Fix r_util build * Minor fixups in rasm

--- libr/bin/format/mach0/mach0.c
+++ libr/bin/format/mach0/mach0.c
@@ -1,15 +1,89 @@
-/* radare - LGPL - Copyright 2008 pancake@nopcode.org> */
+/* radare - LGPL - Copyright 2010 nibble at develsec.org */
+/* gcc -I../../../include -DMAIN mach0.c -o mach0 */
 
+#include <unistd.h>
 #include <r_types.h>
-#include <r_bin_mach0.h>
+#include "mach0.h"
+#include "mach0_specs.h"
 
-int r_bin_mach0_get_entrypoint(r_bin_mach0_obj *bin, r_bin_mach0_entrypoint *entrypoint)
+static int r_bin_mach0_init_mhdr(struct r_bin_mach0_obj_t* bin)
 {
-	/* TODO */
-	return 0;
+	lseek(bin->fd, 0, SEEK_SET);
+	if (read(bin->fd, &bin->mhdr, sizeof(struct mach_header))
+		!= sizeof(struct mach_header)) {
+		perror("read (mhdr)");
+		return R_FALSE;
+	}
+	if (bin->mhdr.magic != MH_MAGIC)
+		return R_FALSE;
+	return R_TRUE;
 }
 
-int r_bin_mach0_get_exports(r_bin_mach0_obj *bin, r_bin_mach0_export *export)
+static int r_bin_mach0_init_lcmd(struct r_bin_mach0_obj_t* bin)
 {
+	return R_TRUE;
+}
+
+static int r_bin_mach0_init_scmd(struct r_bin_mach0_obj_t* bin)
+{
+	return R_TRUE;
+}
+
+static int r_bin_mach0_init_scn(struct r_bin_mach0_obj_t* bin)
+{
+	return R_TRUE;
+}
+
+static int r_bin_mach0_init(struct r_bin_mach0_obj_t* bin)
+{
+	if (!r_bin_mach0_init_mhdr(bin)) {
+		ERR("Warning: File is no MACH0\n");
+		return R_FALSE;
+	}
+	if (!r_bin_mach0_init_lcmd(bin))
+		ERR("Warning: Cannot initalize load commands\n");
+	if (!r_bin_mach0_init_scmd(bin))
+		ERR("Warning: Cannot initalize segment commands\n");
+	if (!r_bin_mach0_init_scn(bin))
+		ERR("Warning: Cannot initalize sections\n");
+	return R_TRUE;
+}
+
+int r_bin_mach0_open(struct r_bin_mach0_obj_t* bin, const char* file)
+{
+	if ((bin->fd = open(file, O_RDONLY)) == -1)
+		return -1;
+	bin->file = file;
+	if (!r_bin_mach0_init(bin))
+		return -1;
+	return bin->fd;
+}
+
+int r_bin_mach0_close(struct r_bin_mach0_obj_t* bin)
+{
+	return close(bin->fd);
+}
+
+struct r_bin_mach0_section_t* r_bin_mach0_get_sections(struct r_bin_mach0_obj_t* bin)
+{
+	return NULL;
+}
+
+#ifdef MAIN
+int main(int argc, char *argv[])
+{
+	struct r_bin_mach0_obj_t bin;
+	struct r_bin_mach0_section_t *sections;
+	int i;
+
+	r_bin_mach0_open(&bin, argv[1]);
+	sections = r_bin_mach0_get_sections(&bin);
+	for (i = 0; sections && !sections[i].last; i++)
+		printf( "offset=%08llx address=%08llx size=%05i name=%s\n",
+				sections[i].offset, sections[i].addr, sections[i].size,
+				sections[i].name);
+	r_bin_mach0_close(&bin);
+
 	return 0;
 }
+#endif
--- libr/bin/format/mach0/mach0.h
+++ libr/bin/format/mach0/mach0.h
@@ -1,86 +1,65 @@
-/* radare - LGPL - Copyright 2008 nibble<.ds@gmail.com> */
+/* radare - LGPL - Copyright 2010 nibble at develsec.org */
 
-#ifndef _INCLUDE_R_BIN_PE_H_
-#define _INCLUDE_R_BIN_PE_H_
+#ifndef _INCLUDE_R_BIN_MACH0_H_
+#define _INCLUDE_R_BIN_MACH0_H_
 
-#include "r_tymach0s.h"
+#include <r_types.h>
+#include "mach0_specs.h"
 
-//#include "r_bin_mach0_smach0cs.h"
-
-#define R_BIN_PE_SCN_IS_SHAREABLE(x)       x & PE_IMAGE_SCN_MEM_SHARED
-#define R_BIN_PE_SCN_IS_EXECUTABLE(x)      x & PE_IMAGE_SCN_MEM_EXECUTE
-#define R_BIN_PE_SCN_IS_READABLE(x)        x & PE_IMAGE_SCN_MEM_READ
-#define R_BIN_PE_SCN_IS_WRITABLE(x)        x & PE_IMAGE_SCN_MEM_WRITE
+#define MACH0_STRING_LENGTH 256
 
 #if 0
-typedef struct {
-	mach0_image_dos_header             *dos_header;
-	mach0_image_nt_headers			    *nt_headers;
-	mach0_image_section_header         *section_header;
-	mach0_image_export_directory       *export_directory;
-	mach0_image_import_directory       *import_directory;
-	mach0_image_delay_import_directory *delay_import_directory;
-    const char* file;
-	int fd;
-} r_bin_mach0_obj;
-
-typedef struct {
-	PE_DWord rva;
-	PE_DWord offset;
-} r_bin_mach0_entrypoint;
-
-typedef struct {
-	PE_Byte  name[PE_IMAGE_SIZEOF_SHORT_NAME];
-	PE_DWord size;
-	PE_DWord vsize;
-	PE_DWord rva;
-	PE_DWord offset;
-	PE_DWord characteristics;
-} r_bin_mach0_section;
-
-typedef struct {
-	PE_Byte  name[PE_NAME_LENGTH];
-	PE_DWord rva;
-	PE_DWord offset;
-	PE_Word hint;
-	PE_Word ordinal;
-} r_bin_mach0_import;
-
-typedef struct {
-	PE_Byte  name[PE_NAME_LENGTH];
-	PE_Byte  forwarder[PE_NAME_LENGTH];
-	PE_DWord rva;
-	PE_DWord offset;
-	int      ordinal;
-} r_bin_mach0_export;
+#define R_BIN_MACH0_SCN_IS_SHAREABLE(x)   x
+#define R_BIN_MACH0_SCN_IS_EXECUTABLE(x)  x
+#define R_BIN_MACH0_SCN_IS_READABLE(x)    x
+#define R_BIN_MACH0_SCN_IS_WRITABLE(x)    x
 #endif
 
-int r_bin_mach0_close(r_bin_mach0_obj*);
+struct r_bin_mach0_section_t {
+	ut64 offset;
+	ut64 addr;
+	ut64 size;
+	ut64 align;
+	ut32 flags;
+	char name[MACH0_STRING_LENGTH];
+	int last;
+};
+
+struct r_bin_mach0_obj_t {
+	struct mach_header mhdr;
+	struct load_command* lcmd;
+	struct segment_command* scmd;
+	struct section* scn;
+
+    ut64        baddr;
+	int	        endian;
+    const char* file;
+	int			fd;
+};
+
+int r_bin_mach0_open(struct r_bin_mach0_obj_t* bin, const char* file);
+int r_bin_mach0_close(struct r_bin_mach0_obj_t* bin);
+struct r_bin_mach0_section_t* r_bin_mach0_get_sections(struct r_bin_mach0_obj_t* bin);
+
+#if 0
 int r_bin_mach0_get_arch(r_bin_mach0_obj*, char*);
 int r_bin_mach0_get_class(r_bin_mach0_obj*, char*);
 int r_bin_mach0_get_entrypoint(r_bin_mach0_obj*, r_bin_mach0_entrypoint*);
 int r_bin_mach0_get_exports(r_bin_mach0_obj*, r_bin_mach0_export*);
-int r_bin_mach0_get_exports_count(r_bin_mach0_obj*);
 int r_bin_mach0_get_file_alignment(r_bin_mach0_obj*);
-PE_DWord r_bin_mach0_get_image_base(r_bin_mach0_obj*);
 int r_bin_mach0_get_image_size(r_bin_mach0_obj*);
 int r_bin_mach0_get_imports(r_bin_mach0_obj*, r_bin_mach0_import*);
-int r_bin_mach0_get_imports_count(r_bin_mach0_obj*);
 int r_bin_mach0_get_libs(r_bin_mach0_obj*, int, r_bin_mach0_string*);
 int r_bin_mach0_get_machine(r_bin_mach0_obj*, char*);
 int r_bin_mach0_get_os(r_bin_mach0_obj*, char*);
 int r_bin_mach0_get_section_alignment(r_bin_mach0_obj*);
-int r_bin_mach0_get_sections(r_bin_mach0_obj*, r_bin_mach0_section*);
-int r_bin_mach0_get_sections_count(r_bin_mach0_obj*);
 int r_bin_mach0_get_strings(r_bin_mach0_obj*, int, int, r_bin_mach0_string*);
 int r_bin_mach0_get_subsystem(r_bin_mach0_obj*, char*);
-int r_bin_mach0_is_dll(r_bin_mach0_obj*);
 int r_bin_mach0_is_big_endian(r_bin_mach0_obj*);
-int r_bin_mach0_is_stripmach0d_relocs(r_bin_mach0_obj*);
-int r_bin_mach0_is_stripmach0d_line_nums(r_bin_mach0_obj*);
-int r_bin_mach0_is_stripmach0d_local_syms(r_bin_mach0_obj*);
-int r_bin_mach0_is_stripmach0d_debug(r_bin_mach0_obj*);
-int r_bin_mach0_omach0n(r_bin_mach0_obj*, const char*);
-
+int r_bin_mach0_is_stripped_relocs(r_bin_mach0_obj*);
+int r_bin_mach0_is_stripped_line_nums(r_bin_mach0_obj*);
+int r_bin_mach0_is_stripped_local_syms(r_bin_mach0_obj*);
+int r_bin_mach0_is_stripped_debug(r_bin_mach0_obj*);
 #endif
 
+#endif
