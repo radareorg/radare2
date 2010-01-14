ba848ec5dd18d6069c66d1dcd18690c858ca8a49
pancake <pancake@nopcode.org>
2010-01-14 23:31:39 +0100

    * Enhace the output of 'make sloc' * Added test program for r_hash from python

--- mk/sloc.mk
+++ mk/sloc.mk
@@ -1,8 +1,11 @@
 sloc:
 	@if [ -n "$${SLOCDIR}" ]; then cd $$SLOCDIR ; fi ; \
-	for a in `find * -iname *.c`; do \
-	printf "$$a \t" ; \
-	sloccount $$a |grep ansic= | cut -d ' ' -f 1 ; \
-	done
+	total=0 ; \
+	for a in `find * -iname '*.c'`; do \
+	lines=$$(sloccount $$a |grep ansic= | cut -d ' ' -f 1) ; \
+	printf "$$lines\t$$a\n" ; \
+	total=$$(($$total+$$lines)) ; \
+	done ; \
+	printf "$$total\tTOTAL\n"
 
 .PHONY: sloc
---
+++ swig/python/test-r_hash.py
@@ -0,0 +1,8 @@
+from r_hash import rHash, Size_MD4
+
+hash = rHash(False)
+ret = hash.do_md4 ("food", 4)
+str = "md4: "
+for i in range(0, Size_MD4):
+	str += "%02x"%ord(ret[i])
+print str
