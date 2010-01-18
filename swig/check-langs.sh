8e4364eb9a0ed1461d5e7b2f607b8a0f404337b6
pancake <pancake@nopcode.org>
2010-01-18 20:38:30 +0100

    * Check for supported langs before using valaswig   - Uses valaswig-cc --test

---
+++ swig/check-langs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+SUP_LANGS=""
+LANGS="python perl ruby"
+for a in ${LANGS}; do
+  printf "Checking $a support for valaswig... "
+  valaswig-cc --test $a
+  if [ $? = 0 ]; then
+    echo ok
+    SUP_LANGS="$a ${SUP_LANGS}"
+  else
+    echo fail
+  fi
+done
+
+:> supported.langs
+for a in ${SUP_LANGS}; do
+  echo $a >> supported.langs
+done
