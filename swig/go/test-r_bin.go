3b6a4a1475b9ee705f3bda9a125d6dd35b9e0baf
Nibble <nibble.ds@gmail.com>
2010-09-15 19:25:14 +0200

    * Add swig/go/fixgoswig.sh script   - Add iterators   - Fix bug with ut64 consts   - These issues must be fixed in swig

--- swig/go/test-r_bin.go
+++ swig/go/test-r_bin.go
@@ -14,19 +14,10 @@ func main() {
 	b := r_bin.NewRBin()
 	b.Load(os.Args[1], "")
 	baddr := b.GetBaddr()
-	sv := b.Get_sections()
 	fmt.Println("-> Sections")
 	fmt.Printf("baddr=%08x\n", baddr)
-	nsects := sv.Size()
-	for i := 0; i < nsects; i++ {
-		s := sv.Get(i)
-		fmt.Printf("offset=0x%08x va=0x%08x size=%05d %s\n",
-				s.GetOffset(), baddr+s.GetRva(), s.GetSize(), s.GetName())
-	}
-	/*
 	for _, s:= range b.Get_sections() {
 		fmt.Printf("offset=0x%08x va=0x%08x size=%05d %s\n",
 				s.GetOffset(), baddr+s.GetRva(), s.GetSize(), s.GetName())
 	}
-	*/
 }
