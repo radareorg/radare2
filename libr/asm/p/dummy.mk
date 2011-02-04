0906e809fe6c911b88dc553bc01e981e22094f9f
Nibble <nibble.ds@gmail.com>
2011-02-04 14:20:30 +0100

    * Remove all dummy plugins (speed up build)

--- libr/asm/p/dummy.mk
+++
@@ -1,8 +0,0 @@
-OBJ_DUMMY+=asm_dummy.o
-
-TARGET_DUMMY=asm_dummy.${EXT_SO}
-ALL_TARGETS+=${TARGET_DUMMY}
-STATIC_OBJ+=${OBJ_DUMMY}
-
-${TARGET_DUMMY}: ${OBJ_DUMMY}
-	${CC} ${LDFLAGS} ${CFLAGS} -o asm_dummy.${EXT_SO} asm_dummy.o
--- libr/parse/p/dummy.mk
+++
@@ -1,9 +0,0 @@
-OBJ_DUMMY+=parse_dummy.o
-
-TARGET_DUMMY=parse_dummy.${EXT_SO}
-ALL_TARGETS+=${TARGET_DUMMY}
-STATIC_OBJ+=${OBJ_DUMMY}
-
-${TARGET_DUMMY}: ${OBJ_DUMMY}
-	${CC} -shared ${CFLAGS} -o ${TARGET_DUMMY} ${OBJ_DUMMY}
-	@#strip -s asm_dummy.${EXT_SO}
