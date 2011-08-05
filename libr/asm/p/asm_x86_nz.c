094c4eb8bdec78c7f5570a8c819515d033056e22
pancake <pancake@nopcode.org>
2011-08-06 01:33:47 +0200

    * Added support for lot of opcodes in x86.nz asm plugin   - Added t/test.nz script to ensure assembler works

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -7,19 +7,17 @@
 #include <r_asm.h>
 
 static ut8 getreg(const char *str) {
-	if (!memcmp ("eax", str, 3)) return 0;
-	if (!memcmp ("ecx", str, 3)) return 1;
-	if (!memcmp ("edx", str, 3)) return 2;
-	if (!memcmp ("ebx", str, 3)) return 3;
-	if (!memcmp ("esp", str, 3)) return 4;
-	if (!memcmp ("ebp", str, 3)) return 5;
-	if (!memcmp ("esi", str, 3)) return 6;
-	if (!memcmp ("edi", str, 3)) return 7;
+	int i;
+	const char *regs[] = { "eax", "ecx", "edx", "ebx", "esp", "ebp", "esi", "edi", NULL };
+	if (str)
+		for (i=0; regs[i]; i++)
+			if (!memcmp (regs[i], str, strlen (regs[i])))
+				return i;
 	return 0xff;
 }
 
 static int isnum(const char *str) {
-	return (*str >= '0' && *str <= '9');
+	return str && (*str == '-' || (*str >= '0' && *str <= '9'));
 }
 
 static int assemble(RAsm *a, RAsmOp *ao, const char *str) {
@@ -33,22 +31,76 @@ static int assemble(RAsm *a, RAsmOp *ao, const char *str) {
 		data[l++] = 0xf3;
 		memmove (op, op+4, strlen (op+4)+1);
 	}
- 	arg = strchr (op, ' ');
-	if (arg) {
-		*arg = '\0';
-		arg++;
+	if (!strcmp (str, "call $$")) {
+		memcpy (data, "\xE8\xFF\xFF\xFF\xFF\xC1", 6);
+		return 6;
 	}
 	if (!strcmp (op, "hang") || !strcmp (str, "jmp $$")) {
 		data[l++] = 0xeb;
 		data[l++] = 0xfe;
 		return 2;
-	} else
+	}
+ 	arg = strchr (op, ' ');
 	if (arg) {
+		*arg = '\0';
+		arg++;
+	}
+	if (arg) {
+		char *arg2 = strchr (arg, ',');
+		if (arg2) {
+			*arg2 = 0;
+			for (arg2++; *arg2==' '; arg2++);
+		}
+		if (!strcmp (op, "add")) {
+			int pfx;
+			if (*arg=='[') {
+				arg++;
+				pfx = 0;
+			} else pfx = 0xc0;
+			if (arg2 == NULL) {
+				eprintf ("Invalid syntax\n");
+				return 0;
+			}
+			if (isnum (arg2)) {
+				data[l++] = 0x83;
+				data[l++] = pfx | getreg (arg);
+				data[l++] = atoi (arg2);
+			} else {
+				data[l++] = 0x01;
+				data[l++] = pfx | getreg (arg2)<<3 | getreg (arg);
+			}
+			return l;
+		} else
+		if (!strcmp (op, "sub")) {
+			int pfx;
+			if (*arg=='[') {
+				arg++;
+				pfx = 0;
+			} else pfx = 0xc0;
+			if (arg2 == NULL) {
+				eprintf ("Invalid syntax\n");
+				return 0;
+			}
+			if (isnum (arg2)) {
+				data[l++] = 0x83;
+				data[l++] = 0xe8 | getreg (arg);
+				data[l++] = atoi (arg2);
+			} else {
+				data[l++] = 0x29;
+				data[l++] = pfx | getreg (arg2)<<3 | getreg (arg);
+			}
+			return l;
+		} else
+		if (!strcmp (op, "test")) {
+			data[l++] = 0x85;
+			data[l++] = 0xc0 | getreg (arg)<<3 | getreg (arg2);
+			return l;
+		} else
 		if (!strcmp (op, "int")) {
 			int a = (int)r_num_get (NULL, arg);
-			data[l++]='\xcd';
-			data[l++]=(ut8)a;
-			return 2;
+			data[l++] = 0xcd;
+			data[l++] = (ut8)a;
+			return l;
 		} else
 		if (!strcmp (op, "call")) {
 			ut64 dst = r_num_math (NULL, arg);
@@ -73,24 +125,25 @@ static int assemble(RAsm *a, RAsmOp *ao, const char *str) {
 			data[l++] = ptr[2];
 			data[l++] = ptr[3];
 			return l;
+		} else if (!strcmp (op, "inc")) {
+			data[l++] = 0x40 | getreg (arg);
+			return l;
+		} else if (!strcmp (op, "dec")) {
+			data[l++] = 0x48 | getreg (arg);
+			return l;
 		} else if (!strcmp (op, "push")) {
 			ut64 dst = r_num_math (NULL, arg);
 			ut32 addr = dst;
 			ut8 *ptr = (ut8 *)&addr;
-
-			if (dst == 0 && arg[0]!='0') {
-				if (!strcmp(arg, "eax")) data[0]='\x50'; else
-				if (!strcmp(arg, "ebx")) data[0]='\x53'; else
-				if (!strcmp(arg, "ecx")) data[0]='\x51'; else
-				if (!strcmp(arg, "edx")) data[0]='\x52'; else
-				if (!strcmp(arg, "esi")) data[0]='\x56'; else
-				if (!strcmp(arg, "edi")) data[0]='\x57'; else
-				if (!strcmp(arg, "ebp")) data[0]='\x55'; else
-				if (!strcmp(arg, "esp")) data[0]='\x54';
-				else return 0; // invalid register name to push
-				return 1;
+			if (!isnum (arg)) {
+				ut8 ch = getreg (arg) | 0x50;
+				if (ch == 0xff) {
+					eprintf ("Invalid register name (%s)\n", arg);
+					return 0;
+				}
+				data[l++] = ch;
+				return l;
 			}
-
 			data[l++] = '\x68';
 			data[l++] = ptr[0];
 			data[l++] = ptr[1];
@@ -100,81 +153,60 @@ static int assemble(RAsm *a, RAsmOp *ao, const char *str) {
 		} else if (!strcmp (op, "pop")) {
 			ut64 dst = r_num_math (NULL, arg);
 			if (dst == 0) {
-				data[l++] = getreg (arg) | 0x50;
+				data[l++] = getreg (arg) | 0x58;
 				return l;
 			}
 			eprintf ("Invalid pop syntax\n");
 			return 0;
 		} else if (!strcmp (op, "xor")) {
-			char *arg2 = strchr (arg, ',');
-			if (arg2 == NULL) {
-				eprintf ("Invalid syntax\n");
-				return 0;
-			}
-			*arg2 = 0;
-			arg2++;
+			int pfx;
+			if (*arg=='[') {
+				arg++;
+				pfx = 0;
+			} else pfx = 0xc0;
 			data[l++] = 0x31;
 			if (isnum (arg2)) {
 				data[l++] = getreg (arg) | 0xf0;
 				data[l++] = atoi (arg2);
 				return l;
 			}
-			data[l++] = getreg (arg2);
-			// XXX: xor eax, eax is wrong f.ex
+			data[l] = getreg (arg);
+			data[l++] |= (getreg (arg2)<<3) | pfx;
 			return l;
 		} else if (!strcmp (op, "mov")) {
+			int pfx;
 			ut64 dst;
 			ut32 addr;
 			ut8 *ptr = (ut8 *)&addr;
-			char *arg2 = strchr (arg, ',');
-			if (arg2 == NULL) {
-				eprintf ("Invalid syntax\n");
-				return 0;
-			}
-			arg2[0]='\0';
-			dst = r_num_math (NULL, arg2+1);
+			dst = r_num_math (NULL, arg2);
 			addr = dst;
 
-			data[l] = getreg (arg) | 0xb0;
-			if (data[l++] == 0xff) {
-				data[l++]='\xb8';
-				eprintf ("oops. wrong arg?\n");
-			}
+			if (*arg=='[') {
+				arg++;
+				pfx = 0;
+			} else pfx = 0xc0;
 
-			if (dst==0 && arg2[1]!='0') {
-				int src = data[0];
-				data[l++]=0x89;
-				if (strstr (arg2+l, "eax")) {
-					switch(src) {
-					case 0xb8: data[l++]=0xc0; break;
-					case 0xbb: data[l++]=0xd8; break;
-					case 0xb9: data[l++]=0xc8; break;
-					case 0xba: data[l++]=0xd0; break;
-					case 0xbc: data[l++]=0xe0; break;
-					case 0xbd: data[l++]=0xe8; break;
-					case 0xbe: data[l++]=0xf0; break;
-					case 0xbf: data[l++]=0xf8; break;
-					default:
-						eprintf ("OOPS: unknown register\n");
-						return 0;
-					}
-					return l;
-				} else {
-					data[l] = getreg (arg2) | 0x58;
-					if (data[l] == 0xff) {
-						return 0;
-					}
-					return l+1;
-				}
+			if (*arg2=='[') {
+				arg2++;
+				data[l++] = 0x8b;
+				data[l++] = getreg (arg)<<3 | getreg (arg2);
+				return l;
+				pfx = 0;
+			} else pfx = 0xc0;
+
+			if (isnum (arg2)) {
+				data[l++]=0xb8;
+				data[l++] = ptr[0];
+				data[l++] = ptr[1];
+				data[l++] = ptr[2];
+				data[l++] = ptr[3];
+				return l;
 			} else {
-				data[1] = ptr[0];
-				data[2] = ptr[1];
-				data[3] = ptr[2];
-				data[4] = ptr[3];
+				data[l++] = 0x89;
+				data[l++] = getreg (arg2)<<3 | getreg (arg);
 			}
 			return l;
 		} else if (!strcmp (op, "jmp")) {
-			//ut64 dst = r_num_math (NULL, arg); // XXX: r_num_math  breaks ebp+33 to be 33 instead of 0
 			ut64 dst = r_num_get (NULL, arg) - offset;
 			ut32 addr = dst;
 			ut8 *ptr = (ut8 *)&addr;
@@ -210,16 +242,16 @@ static int assemble(RAsm *a, RAsmOp *ao, const char *str) {
 #endif
 			}
 
-			dst-=offset;
+			dst -= offset;
 	// 7C90EAF5   .- E9 42158783   JMP     0018003C
 	// RELATIVE LONG JUMP (nice coz is 4 bytes, not 5) 
 
 			if (dst>-0x80 && dst<0x7f) {
 				/* relative address */
-				addr-=2;
-				addr-=offset;
-				data[l++]='\xeb';
-				data[l++]=(char)dst;
+				addr -= 2;
+				addr -= offset;
+				data[l++] = '\xeb';
+				data[l++] = (char)dst;
 				return l;
 			} else {
 				/* absolute address */
@@ -265,30 +297,33 @@ static int assemble(RAsm *a, RAsmOp *ao, const char *str) {
 			}
 		}
 	} else {
+		if (!strcmp (op, "leave")) {
+			data[l++]='\xc9';
+		} else
+		if (!strcmp (op, "syscall")) {
+			data[l++]='\x0f';
+			data[l++]='\x05';
+		} else
 		if (!strcmp (op, "ret")) {
 			data[l++]='\xc3';
-			return l;
 		} else
 		if (!strcmp (op, "ret0")) {
 			memcpy (data+l, "\x31\xc0\xc3", 3);
-			return l+3;
+			l += 3;
 		} else
 		if (!strcmp(op, "int3")) {
 			data[l++]='\xcc';
-			return l;
 		} else
 		if (!strcmp (op, "pusha")) {
 			data[l++]='\x60';
-			return l;
 		} else
 		if (!strcmp (op, "popa")) {
 			data[l++] = 0x61;
-			return l;
 		} else
 		if (!strcmp (op, "nop")) {
 			data[l++]='\x90';
-			return l;
 		}
+		return l;
 	}
 	eprintf ("Unknown opcode\n");
 	return 0;
