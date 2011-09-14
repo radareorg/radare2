d72d66f14356c31dc78f150d7f3522d7bf0eeca2
pancake <pancake@nopcode.org>
2011-09-14 17:18:40 +0200

    * Honor OSABI in elf for arm bins too

--- libr/bin/format/elf/elf.c
+++ libr/bin/format/elf/elf.c
@@ -510,8 +510,12 @@ char* Elf_(r_bin_elf_get_osabi_name)(struct Elf_(r_bin_elf_obj_t) *bin) {
 	if (needle (bin, "netbsd")) return strdup ("netbsd");
 	if (needle (bin, "freebsd")) return strdup ("freebsd");
 	if (needle (bin, "GNU")) return strdup ("linux");
+	return strdup ("linux");
+#if 0
 	// XXX: this is wrong. openbsd bins are identified as linux ones.
 	switch (bin->ehdr.e_ident[EI_OSABI]) {
+	case ELFOSABI_ARM_AEABI:
+	case ELFOSABI_ARM:        return strdup ("arm");
 	case ELFOSABI_NONE:       return strdup ("linux"); // sysv
 	case ELFOSABI_HPUX:       return strdup ("hpux");
 	case ELFOSABI_NETBSD:     return strdup ("netbsd");
@@ -524,10 +528,9 @@ char* Elf_(r_bin_elf_get_osabi_name)(struct Elf_(r_bin_elf_obj_t) *bin) {
 	case ELFOSABI_MODESTO:    return strdup ("modesto");
 	case ELFOSABI_OPENBSD:    return strdup ("openbsd");
 	case ELFOSABI_STANDALONE: return strdup ("standalone");
-	case ELFOSABI_ARM_AEABI:
-	case ELFOSABI_ARM:        return strdup ("arm");
 	default:                  return r_str_dup_printf ("<unknown: %x>", bin->ehdr.e_ident[EI_OSABI]);
 	}
+#endif
 }
 
 int Elf_(r_bin_elf_is_big_endian)(struct Elf_(r_bin_elf_obj_t) *bin) {
