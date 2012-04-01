1d9668cb8ef04e3c194f35d8b7e02f9c556b36cf
pancake <pancake@nopcode.org>
2012-04-01 02:16:20 +0200

    * Bugfix for binr/blob

--- binr/blob/main.c
+++ binr/blob/main.c
@@ -7,6 +7,7 @@ int ragg2_main(int argc, char **argv);
 int rabin2_main(int argc, char **argv);
 int rarun2_main(int argc, char **argv);
 int rafind2_main(int argc, char **argv);
+int radiff2_main(int argc, char **argv);
 int rax2_main(int argc, char **argv);
 
 typedef struct {
@@ -17,6 +18,7 @@ typedef struct {
 Main foo[] = {
 	{ "r2", radare2_main },
 	{ "rax", rax2_main },
+	{ "radiff", radiff2_main },
 	{ "rafind", rafind2_main },
 	{ "rarun", rarun2_main },
 	{ "rasm", rasm2_main },
@@ -29,8 +31,10 @@ Main foo[] = {
 int main(int argc, char **argv) {
 	int i=0;
 	while (foo[i].name) {
-		if (strstr (argv[0], foo[i].name))
+		if (strstr (argv[0], foo[i].name)) {
+			printf ("Using (%s)\n", foo[i].name);
 			return foo[i].main (argc, argv);
+		}
 		i++;
 	}
 	for (i=0; foo[i].name; i++)
