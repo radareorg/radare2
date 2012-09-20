1f1d7729978b9b357cc4e9e27d06a15714c89875
Pau Oliva <pau@eslack.org>
2012-09-20 04:10:08 +0200

    fix build system for android ndk-r8b

--- sys/android-shell.sh
+++ sys/android-shell.sh
@@ -44,12 +44,16 @@ if [ ! -d "${NDK}" ]; then
 	exit 1
 fi
 
-NDKPATH_MIPS=`echo ${NDK}/toolchains/mipsel-*/prebuilt/${OS}-x86/bin/`
-NDKPATH_ARM=`echo ${NDK}/toolchains/arm-*/prebuilt/${OS}-x86/bin/`
-NDKPATH_X86=`echo ${NDK}/toolchains/x86-*/prebuilt/${OS}-x86/bin/`
+TOOLCHAIN_MIPS=`ls ${NDK}/toolchains/ |grep "^mips" |sort |tail -n 1`
+TOOLCHAIN_ARM=`ls ${NDK}/toolchains/ |grep "^arm" |sort |tail -n 1`
+TOOLCHAIN_X86=`ls ${NDK}/toolchains/ |grep "^x86" |sort |tail -n 1`
+
+NDKPATH_MIPS=${NDK}/toolchains/${TOOLCHAIN_MIPS}/prebuilt/${OS}-x86/bin/
+NDKPATH_ARM=${NDK}/toolchains/${TOOLCHAIN_ARM}/prebuilt/${OS}-x86/bin/
+NDKPATH_X86=${NDK}/toolchains/${TOOLCHAIN_X86}/prebuilt/${OS}-x86/bin/
 
 # r7b
-NDKPATH_ARM=`echo ${NDK}/toolchains/arm-*/prebuilt/$(uname|tr A-Z a-z)-x86/bin/`
+#NDKPATH_ARM=`echo ${NDK}/toolchains/arm-*/prebuilt/$(uname|tr A-Z a-z)-x86/bin/`
 #INCDIR=${NDK}/platforms/android-8/arch-arm/usr/include/
 #CFLAGS=-I${INCDIR}
 #echo $NDKPATH_ARM
