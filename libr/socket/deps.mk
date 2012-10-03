e8a601fe538583b558c45fd0d789eaf40cd5ad8b
pancake <pancake@nopcode.org>
2012-10-03 16:32:32 +0200

    More makefile simplifications (-56LOC)

---
+++ libr/socket/deps.mk
@@ -0,0 +1,22 @@
+ifeq (${HAVE_LIB_SSL},1)
+CFLAGS+=${SSL_CFLAGS}
+LDFLAGS+=${SSL_LDFLAGS}
+endif
+
+# OSX 10.7 (lion)
+ifeq (${OSTYPE},darwin)
+LDFLAGS+=-lcrypto
+endif
+# on solaris only
+ifeq (${OSTYPE},solaris)
+LDFLAGS+=-lsocket
+endif
+ifeq (${OSTYPE},qnx)
+LDFLAGS+=-lsocket
+endif
+# windows
+ifeq (${OSTYPE},windows)
+LDFLAGS=-lwsock32
+#LDFLAGS+=-lws2_32
+endif
+
