6eacc1284af3833924b47b95c4438e1884d50281
pancake <pancake@nopcode.org>
2013-06-14 03:05:52 +0200

    Fix build

--- binr/radare2/radare2.c
+++ binr/radare2/radare2.c
@@ -16,6 +16,43 @@ static int threaded = 0;
 #endif
 static struct r_core_t r;
 
+static int verify_version(int show) {
+	int i, ret;
+	typedef const char* (*vc)();
+	const char *base = GIT_TAP;
+	struct vcs_t {
+		const char *name;
+		vc callback;
+	} vcs[] = {
+		{ "r_anal", &r_anal_version },
+		{ "r_bin", &r_bin_version },
+		{ "r_cons", &r_cons_version },
+		{ "r_core", &r_core_version },
+		{ "r_util", &r_util_version },
+		{ "r_debug", &r_debug_version },
+		{ "r_io", &r_io_version },
+		{ "r_fs", &r_fs_version },
+		{ "r_crypto", &r_crypto_version },
+		{ "r_asm", &r_asm_version },
+		{ "r_parse", &r_parse_version },
+		{ "r_reg", &r_reg_version },
+		/* ... */
+		{NULL,NULL}
+	};
+
+	if (show)
+	printf ("%s  r2\n", base);
+	for (i=ret=0; vcs[i].name; i++) {
+		struct vcs_t *v = &vcs[i];
+		const char *name = v->callback ();
+		if (!ret && strcmp (base, name))
+			ret = 1;
+		if (show) printf ("%s  %s\n", name, v->name);
+	}
+	if (ret) eprintf ("Warning: r2 library versions missmatch!\n");
+	return ret;
+}
+
 static int main_help(int line) {
 	if (line<2)
 		printf ("Usage: r2 [-dDwntLqv] [-P patch] [-p prj] [-a arch] [-b bits] [-i file]\n"
