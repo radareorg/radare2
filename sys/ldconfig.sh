6efdfd0e7af884c916569393f9c8bb1f2aa959cb
pancake <pancake@nopcode.org>
2013-06-17 03:15:02 +0200

    Check write perms in sys/ldconfig.sh

--- sys/ldconfig.sh
+++ sys/ldconfig.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 LD=/etc/ld.so.conf.d
-type ldconfig > /dev/null 2>&1
-if [ $? = 0 ]; then
-	mkdir -p $LD
-	awk -F= '/^LIBDIR/{print $2}' config-user.mk > $LD/radare.conf
-	ldconfig
+if test -w $LD ; then
+	if type ldconfig > /dev/null 2>&1 ; then
+		mkdir -p $LD
+		awk -F= '/^LIBDIR/{print $2}' config-user.mk > $LD/radare.conf
+		ldconfig
+	fi
 fi
+exit 0
