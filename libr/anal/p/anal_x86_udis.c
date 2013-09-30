1f4c8aee8dbbc24822788fce3dd3f04b4810e6e6
pancake <pancake@nopcode.org>
2013-09-30 03:59:00 +0200

    Handle INT and INT3 instructions in x86 analysis

--- libr/anal/p/anal_x86_udis.c
+++ libr/anal/p/anal_x86_udis.c
@@ -239,6 +239,12 @@ int x86_udis86_op(RAnal *anal, RAnalOp *op, ut64 addr, const ut8 *data, int len)
 	case UD_Iror:
 		op->type = R_ANAL_OP_TYPE_ROR;
 		break;
+	case UD_Iint3:
+		op->type = R_ANAL_OP_TYPE_TRAP;
+		break;
+	case UD_Iint:
+		op->type = R_ANAL_OP_TYPE_SWI;
+		break;
 	case UD_Ilea:
 	case UD_Imov:
 		op->type = R_ANAL_OP_TYPE_MOV;
