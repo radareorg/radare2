0e470adc274cc4389e4a47548c36a82b0c2f7808
pancake <pancake@nopcode.org>
2013-11-03 03:05:00 +0100

    Fix #258 - Hide SIGTRAP signals

--- libr/debug/debug.c
+++ libr/debug/debug.c
@@ -247,8 +247,9 @@ R_API int r_debug_wait(RDebug *dbg) {
 			/* handle signal on continuations here */
 			int what = r_debug_signal_what (dbg, dbg->signum);
 			const char *name = r_debug_signal_resolve_i (dbg, dbg->signum);
-			r_cons_printf ("[+] signal %d aka %s received\n",
-				dbg->signum, name);
+			if (strcmp ("SIGTRAP", name))
+				r_cons_printf ("[+] signal %d aka %s received\n",
+					dbg->signum, name);
 			if (what & R_DBG_SIGNAL_SKIP) {
 				dbg->signum = 0;
 				// TODO: use ptrace-setsiginfo to ignore signal
