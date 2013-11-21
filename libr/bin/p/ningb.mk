4394ae51cee19e39d20ab91b7bd19d6747236809
condret <condret@runas-racer.com>
2013-11-21 23:29:03 +0100

    Create ningb.mk
    I hate Makefiles!!! (They look creepy for me)

---
+++ libr/bin/p/ningb.mk
@@ -0,0 +1,9 @@
+OBJ_NINGB=bin_ningb.o
+
+STATIC_OBJ+=${OBJ_NINGB}
+TARGET_NINGB=bin_ningb.${EXT_SO}
+
+ALL_TARGETS+=${TARGET_NINGB}
+
+${TARGET_NINGB}: ${OBJ_NINGB}
+	${CC} $(call libname,bin_ningb) ${CFLAGS} $(LDFLAGS) ${OBJ_NINGB}
