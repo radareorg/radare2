456b72f85ee45eaee90b586b05fa61b062499b3c
pancake <pancake@nopcode.org>
2013-11-22 18:20:15 +0100

    Fix #380 - shell backticks segfaults

--- libr/util/str.c
+++ libr/util/str.c
@@ -474,7 +474,7 @@ R_API int r_str_cmp(const char *a, const char *b, int len) {
 
 R_API int r_str_ccpy(char *dst, char *src, int ch) {
 	int i;
-	for (i=0;src[i] && src[i] != ch; i++)
+	for (i=0; src[i] && src[i] != ch; i++)
 		dst[i] = src[i];
 	dst[i] = '\0';
 	return i;
