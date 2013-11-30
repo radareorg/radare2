239203d8d97f18f3c163c7db62c596e018a97c71
pancake <pancake@nopcode.org>
2013-11-30 03:28:19 +0100

    Fix #382 - Refactor 16bit addressing issue to fix rel32 calculation

--- shlr/udis86/syn-att.c
+++ shlr/udis86/syn-att.c
@@ -95,8 +95,7 @@ gen_operand(struct ud* u, struct ud_operand* op)
     break;
 
   case UD_OP_JIMM:
-    //ud_syn_print_addr(u, ud_syn_rel_target(u, op));
-    ud_syn_print_addr(u, ud_syn_rel_target(u, op, u->dis_mode!=64? 1: 0));
+    ud_syn_print_addr(u, ud_syn_rel_target(u, op));
     break;
 
   case UD_OP_PTR:
