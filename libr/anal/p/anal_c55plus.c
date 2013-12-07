a93b1446852ac9d0a23007288d1e6c0784aa47bd
Anton Kochkov <anton.kochkov@gmail.com>
2013-12-08 02:58:31 +0400

    Improve C55x+ analysis

--- libr/anal/p/anal_c55plus.c
+++ libr/anal/p/anal_c55plus.c
@@ -26,48 +26,60 @@ static int c55plus_op(RAnal *anal, RAnalOp *op, ut64 addr, const ut8 *buf, int l
 	op->ptr = op->val = -1;
         op->size = ins_len;
 
-	if (ins_len >= 1 && *ins == 0x20) {
-		op->type = R_ANAL_OP_TYPE_NOP;
-
-	} else if (ins_len >= 4 && buf[0] == 0xD8) { //  BCC conditional absolute jump
+	if (ins_len >= 1) {
+		if (*ins == 0x20) {
+			op->type = R_ANAL_OP_TYPE_NOP;
+		} else if (*ins == 0x21) {
+			op->type = R_ANAL_OP_TYPE_RET;
+		}
+	} else if (ins_len >= 4 && buf[0] == 0xD8) {
+		//  BCC conditional absolute jump
 		op->type = R_ANAL_OP_TYPE_CJMP;
 		op->jump = (buf[1] << 16) | (buf[2] << 8) | buf[3];
 
-	} else if (ins_len >= 2 && buf[0] == 0x6A) { //  BCC conditional relative jump
+	} else if (ins_len >= 2 && buf[0] == 0x6A) {
+		//  BCC conditional relative jump
 		op->type = R_ANAL_OP_TYPE_CJMP;
 		op->jump = addr + ((st8)buf[1]) + ins_len;
 
-	} else if (ins_len >= 3 && buf[0] == 0x9A) { // BCC conditional relative jump
+	} else if (ins_len >= 3 && buf[0] == 0x9A) {
+		// BCC conditional relative jump
 		op->type = R_ANAL_OP_TYPE_CJMP;
 		op->jump = addr + (st16)((buf[1] << 8) | buf[2]) + ins_len;
 
-	} else if (ins_len >= 4 && buf[0] == 0x9C) { // B unconditional absolute jump
+	} else if (ins_len >= 4 && buf[0] == 0x9C) {
+		// B unconditional absolute jump
 		op->type = R_ANAL_OP_TYPE_JMP;
 		op->jump = (buf[1] << 16) | (buf[2] << 8) | buf[3];
 
-	} else if (ins_len >= 3 && buf[0] == 0x68) { // B unconditional relative jump
+	} else if (ins_len >= 3 && buf[0] == 0x68) {
+		// B unconditional relative jump
 		op->type = R_ANAL_OP_TYPE_JMP;
 		op->jump = addr + (st16)((buf[1] << 8) | buf[2]) + ins_len;
 
- 	} else if (ins_len == 2 && buf[0] == 0x02) { // CALL unconditional absolute call with acumulator register ACx
+ 	} else if (ins_len == 2 && buf[0] == 0x02) {
+		// CALL unconditional absolute call with acumulator register ACx
 
 		op->type = R_ANAL_OP_TYPE_UCALL;
 		op->fail = addr + ins_len;
 
- 	} else if (ins_len >= 3 && buf[0] == 0x69) { // CALL unconditional relative call
-
+ 	} else if (ins_len >= 3 && buf[0] == 0x69) {
+		// CALL unconditional relative call
 		op->type = R_ANAL_OP_TYPE_CALL;
 		op->jump = addr + (st16)((buf[1] << 8) | buf[2]) + ins_len;
 
- 	} else if (ins_len >= 3 && buf[0] == 0x9D) { // CALL unconditional absolute call
+ 	} else if (ins_len >= 3 && buf[0] == 0x9D) {
+		// CALL unconditional absolute call
 		op->type = R_ANAL_OP_TYPE_CALL;
 		op->jump = (buf[1] << 16) | (buf[2] << 8) | buf[3];
 
- 	} else if (ins_len >= 3 && buf[0] == 0x9B) { // CALLCC conditional relative call
+ 	} else if (ins_len >= 3 && buf[0] == 0x9B) {
+		// CALLCC conditional relative call
 		op->type = R_ANAL_OP_TYPE_CALL;
 		op->jump = addr + (st16)((buf[1] << 8) | buf[2]) + ins_len;
 
- 	} else if (ins_len >= 4 && buf[0] == 0xD9) { // CALLCC conditional absolute call
+ 	} else if (ins_len >= 4 && buf[0] == 0xD9) {
+		// CALLCC conditional absolute call
 		op->type = R_ANAL_OP_TYPE_CALL;
 		op->jump = (buf[1] << 16) | (buf[2] << 8) | buf[3];
 
