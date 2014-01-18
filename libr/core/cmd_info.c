942bbfa0cd523cef839473f992e637303524d687
pancake <pancake@nopcode.org>
2014-01-18 04:53:33 +0100

    'i' command now accepts multiple subcommands at once with proper JSON format

--- libr/core/cmd_info.c
+++ libr/core/cmd_info.c
@@ -75,7 +75,8 @@ static int cmd_info(void *data, const char *input) {
 	int newline = r_config_get_i (core->config, "scr.interactive");
 	ut64 offset = r_bin_get_offset (core->bin);
 	int va = core->io->va || core->io->debug;
-	int mode = 0;
+	int mode = R_CORE_BIN_SIMPLE;
+	int is_array = 0;
 	Sdb *db;
 	if (input[0]) {
 		switch (input[1]) {
@@ -85,105 +86,131 @@ static int cmd_info(void *data, const char *input) {
 		}
 	}
 
-	switch (*input) {
-	case 'b':
-		{
-		ut64 baddr = r_config_get_i (core->config, "bin.baddr");
-		// XXX: this will reload the bin using the buffer.
-		// An assumption is made that assumes there is an underlying
-		// plugin that will be used to load the bin (e.g. malloc://)
-		// TODO: Might be nice to reload a bin at a specified offset?
-		r_core_bin_reload (core, NULL, baddr);
-		r_core_block_read (core, 0);
-		}
-		break;
-	case 'k':
-		db = core->bin->cur.o->kv;
-		//:eprintf ("db = %p\n", db);
-		switch (input[1]) {
-		case 'v':
-			sdb_query (db, input+3);
+	if (strchr (input, '*'))
+		mode = R_CORE_BIN_RADARE;
+	if (strchr (input, 'j'))
+		mode = R_CORE_BIN_JSON;
+
+	if (mode == R_CORE_BIN_JSON) {
+		if (strlen (input+1)>1)
+			is_array = 1;
+	}
+	if (is_array)
+		r_cons_printf ("{");
+	while (*input) {
+		switch (*input) {
+		case 'b':
+			{
+			ut64 baddr = r_config_get_i (core->config, "bin.baddr");
+			// XXX: this will reload the bin using the buffer.
+			// An assumption is made that assumes there is an underlying
+			// plugin that will be used to load the bin (e.g. malloc://)
+			// TODO: Might be nice to reload a bin at a specified offset?
+			r_core_bin_reload (core, NULL, baddr);
+			r_core_block_read (core, 0);
+			}
 			break;
-		case '.':
-		case ' ':
-			sdb_query (db, input+2);
+		case 'k':
+			db = core->bin->cur.o->kv;
+			//:eprintf ("db = %p\n", db);
+			switch (input[1]) {
+			case 'v':
+				sdb_query (db, input+3);
+				break;
+			case '.':
+			case ' ':
+				sdb_query (db, input+2);
+				break;
+			case '\0':
+				sdb_list (db);
+				break;
+			case '?':
+			default:
+				eprintf ("Usage: ik [sdb-query]\n");
+			}
 			break;
-		case '\0':
-			sdb_list (db);
+		case 'o': r_core_bin_load (core, input[1]==' '?
+				input+2: core->file->filename,
+				r_config_get_i (core->config, "bin.baddr"));
+			break;
+	#define RBININFO(n,x) \
+	if (is_array) { \
+		if (is_array==1) is_array++; else r_cons_printf (","); \
+		r_cons_printf ("\"%s\":",n); \
+	}\
+	r_core_bin_info (core,x,mode,va,NULL,offset);
+		case 'S': RBININFO ("sections",R_CORE_BIN_ACC_SECTIONS); break;
+		case 'h': RBININFO ("fields", R_CORE_BIN_ACC_FIELDS); break;
+		case 'l': RBININFO ("libs", R_CORE_BIN_ACC_LIBS); break;
+		case 's': RBININFO ("symbols", R_CORE_BIN_ACC_SYMBOLS); break;
+		case 'R':
+		case 'r': RBININFO ("relocs", R_CORE_BIN_ACC_RELOCS); break;
+		case 'd': RBININFO ("dwarf", R_CORE_BIN_ACC_DWARF); break;
+		case 'i': RBININFO ("imports",R_CORE_BIN_ACC_IMPORTS); break;
+		case 'I': RBININFO ("info", R_CORE_BIN_ACC_INFO); break;
+		case 'e': RBININFO ("entries",R_CORE_BIN_ACC_ENTRIES); break;
+		case 'z': RBININFO ("strings",R_CORE_BIN_ACC_STRINGS); break;
+		case 'c':
+		case 'C': RBININFO ("classes",R_CORE_BIN_ACC_CLASSES); break;
+		case 'a':
+			{
+				char *p, cmd[3], chars[] = "IeisSz";
+				switch (mode) {
+				case R_CORE_BIN_RADARE: cmd_info (core, "i*IiesSz"); break;
+				case R_CORE_BIN_JSON: cmd_info (core, "ijIiesSz"); break;
+				default:
+				case R_CORE_BIN_SIMPLE: cmd_info (core, "iIiesSz"); break;
+				}
+			}
 			break;
 		case '?':
+			r_cons_printf (
+			"|Usage: i[aeciIsosSz][jq*]      ; get info from opened file\n"
+			"|Output mode:\n"
+			"| '*'   output in radare commands\n"
+			"| 'j'   output in json\n"
+			"| 'q'   simple quiet output\n"
+			"|Actions:\n"
+			"| io [file]   load info from file (or last opened) use bin.baddr\n"
+			"| ik [query]  key-value database from RBinObject\n"
+			"| ia          show all info (imports, exports, sections..)\n"
+			"| ic          list classes\n"
+			"| id          debug information (source lines)\n"
+			"| ie          entrypoint\n"
+			"| ih          headers\n"
+			"| ii          imports\n"
+			"| iI          binary info\n"
+			"| il          libraries\n"
+			"| is          symbols\n"
+			"| iS          sections\n"
+			"| ir/iR       relocs\n"
+			"| iz          strings\n"
+			"| ib          reload the current buffer for setting of the bin (use once only)\n"
+			);
+			break;
+		case '*':
+			mode = R_CORE_BIN_RADARE;
+			break;
+		case 'j':
+			mode = R_CORE_BIN_JSON;
+			break;
 		default:
-			eprintf ("Usage: ik [sdb-query]\n");
+			if (core->file) {
+				if (mode == R_CORE_BIN_JSON)
+					r_cons_printf ("{\"bin\":");
+				r_core_bin_info (core, R_CORE_BIN_ACC_INFO,
+					mode, va, NULL, offset);
+				if (mode == R_CORE_BIN_JSON)
+					r_cons_printf (",\"core\":");
+				r_core_file_info (core, mode);
+				if (mode == R_CORE_BIN_JSON)
+					r_cons_printf ("}\n");
+			} else eprintf ("No selected file\n");
 		}
-		break;
-	case 'o': r_core_bin_load (core, input[1]==' '?
-			input+2: core->file->filename,
-			r_config_get_i (core->config, "bin.baddr"));
-		break;
-#define RBININFO(x) r_core_bin_info(core,x,mode,va,NULL,offset); if (newline) r_cons_newline()
-	case 'S': RBININFO (R_CORE_BIN_ACC_SECTIONS); break;
-	case 'h': RBININFO (R_CORE_BIN_ACC_FIELDS); break;
-	case 'l': RBININFO (R_CORE_BIN_ACC_LIBS); break;
-	case 's': RBININFO (R_CORE_BIN_ACC_SYMBOLS); break;
-	case 'R':
-	case 'r': RBININFO (R_CORE_BIN_ACC_RELOCS); break;
-	case 'd': RBININFO (R_CORE_BIN_ACC_DWARF); break;
-	case 'i': RBININFO (R_CORE_BIN_ACC_IMPORTS); break;
-	case 'I': RBININFO (R_CORE_BIN_ACC_INFO); break;
-	case 'e': RBININFO (R_CORE_BIN_ACC_ENTRIES); break;
-	case 'z': RBININFO (R_CORE_BIN_ACC_STRINGS); break;
-	case 'c':
-	case 'C': RBININFO (R_CORE_BIN_ACC_CLASSES); break;
-	case 'a':
-		{
-			char *p, cmd[3], chars[] = "IeisSz";
-			for (p = chars; *p; p++) {
-				cmd[0] = *p;
-				cmd[1] = input[1];
-				cmd[2] = 0;
-				cmd_info (core, cmd);
-			}
-		}
-		break;
-	case '?':
-		r_cons_printf (
-		"|Usage: i[aeciIsosSz][jq*]      ; get info from opened file\n"
-		"|Output mode:\n"
-		"| '*'   output in radare commands\n"
-		"| 'j'   output in json\n"
-		"| 'q'   simple quiet output\n"
-		"|Actions:\n"
-		"| io [file]   load info from file (or last opened) use bin.baddr\n"
-		"| ik [query]  key-value database from RBinObject\n"
-		"| ia          show all info (imports, exports, sections..)\n"
-		"| ic          list classes\n"
-		"| id          debug information (source lines)\n"
-		"| ie          entrypoint\n"
-		"| ih          headers\n"
-		"| ii          imports\n"
-		"| iI          binary info\n"
-		"| il          libraries\n"
-		"| is          symbols\n"
-		"| iS          sections\n"
-		"| ir/iR       relocs\n"
-		"| iz          strings\n"
-		"| ib          reload the current buffer for setting of the bin (use once only)\n"
-		);
-		break;
-	case '*': mode = R_CORE_BIN_RADARE;
-	case 'j': if (*input=='j') mode = R_CORE_BIN_JSON;
-	default:
-		if (core->file) {
-			if (mode == R_CORE_BIN_JSON)
-				r_cons_printf ("{\"bin\":");
-			r_core_bin_info (core, R_CORE_BIN_ACC_INFO,
-				mode, va, NULL, offset);
-			if (mode == R_CORE_BIN_JSON)
-				r_cons_printf (",\"core\":");
-			r_core_file_info (core, mode);
-			if (mode == R_CORE_BIN_JSON)
-				r_cons_printf ("}\n");
-		} else eprintf ("No selected file\n");
+		input++;
 	}
+	if (is_array)
+		r_cons_printf ("}\n");
+	if (newline) r_cons_newline();
 	return 0;
 }
