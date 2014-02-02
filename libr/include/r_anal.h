1b645282f9acde3e997713a300da8b8791afcfe3
condret <condret@runas-racer.com>
2014-02-02 02:56:17 +0100

    Update r_anal.h

--- libr/include/r_anal.h
+++ libr/include/r_anal.h
@@ -1115,7 +1115,7 @@ extern RAnalPlugin r_anal_plugin_ebc;
 extern RAnalPlugin r_anal_plugin_gb;
 extern RAnalPlugin r_anal_plugin_nios2;
 extern RAnalPlugin r_anal_plugin_malbolge;
-
+extern RAnalPlugin r_anal_plugin_ws;
 #ifdef __cplusplus
 }
 #endif
