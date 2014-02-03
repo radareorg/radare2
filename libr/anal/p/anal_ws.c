ae2f69622b6e36c7a7415982b7f2ca0a7c89a3d4
condret <condret@runas-racer.com>
2014-02-03 02:58:29 +0100

    Update anal_ws.c

--- libr/anal/p/anal_ws.c
+++ libr/anal/p/anal_ws.c
@@ -5,9 +5,31 @@
 #include <r_asm.h>
 #include <r_anal.h>
 #include <r_lib.h>
+#include <r_io.h>
 #define WS_API static
 #include "../../asm/arch/whitespace/wsdis.c"
 
+
+static ut64 ws_find_label(int l, RIOBind iob)
+{
+	ut64 cur = 0, size = iob.size(iob.io);
+	ut8 buf[128];
+	RAsmOp *aop;
+	aop = R_NEW0(RAsmOp);
+	iob.read_at(iob.io, cur, buf, 128);
+	while(cur <= size && wsdis(aop, buf, 128)) {
+		if(	aop->buf_asm[0] == 'm' &&
+			aop->buf_asm[1] == 'a' &&
+			l == atoi(&aop->buf_asm[5])) {
+				r_asm_op_free(aop);
+				return cur;
+		}
+		cur = cur + aop->size;
+		iob.read_at(iob.io, cur, buf, 128);
+	}
+	r_asm_op_free(aop);
+	return 0;
+}
 
 static int ws_anal(RAnal *anal, RAnalOp *op, ut64 addr, const ut8 *data, int len)
 {
@@ -50,16 +72,23 @@ static int ws_anal(RAnal *anal, RAnalOp *op, ut64 addr, const ut8 *data, int len
 				op->type = R_ANAL_OP_TYPE_LOAD;
 				break;
 			case 'c':
-				if(aop->buf_asm[1] == 'a')
+				if(aop->buf_asm[1] == 'a') {
 					op->type = R_ANAL_OP_TYPE_CALL;
-				else
+					op->fail = addr + aop->size;
+					op->jump = ws_find_label(atoi(&aop->buf_asm[5]), anal->iob);
+				} else {
 					op->type = R_ANAL_OP_TYPE_UPUSH;
+				}
 				break;
 			case 'j':
-				if(aop->buf_asm[1] == 'm')
+				if(aop->buf_asm[1] == 'm') {
 					op->type = R_ANAL_OP_TYPE_JMP;
-				else
+					op->jump = ws_find_label(atoi(&aop->buf_asm[4]), anal->iob);
+				} else {
 					op->type = R_ANAL_OP_TYPE_CJMP;
+					op->jump = ws_find_label(atoi(&aop->buf_asm[3]), anal->iob);
+				}
+				op->fail = addr + aop->size;
 				break;
 			case 'g':
 				op->type = R_ANAL_OP_TYPE_IO;
@@ -68,10 +97,19 @@ static int ws_anal(RAnal *anal, RAnalOp *op, ut64 addr, const ut8 *data, int len
 				if(aop->buf_asm[1] == 'o') {
 					op->type = R_ANAL_OP_TYPE_POP;
 				} else {
-					if(aop->buf_asm[2] == 's')
+					if(aop->buf_asm[2] == 's') {
 						op->type = R_ANAL_OP_TYPE_PUSH;
-					else
+						if(127 > atoi(&aop->buf_asm[5])
+						&& atoi(&aop->buf_asm[5]) >= 33) {
+							char c[4];
+							c[3] = '\0';
+							c[0] = c[2] = '\'';
+							c[1] = (char) atoi(&aop->buf_asm[5]);
+							r_meta_set_string(anal, R_META_TYPE_COMMENT, addr, c);
+						}
+					} else {
 						op->type = R_ANAL_OP_TYPE_IO;
+					}
 				}
 				break;
 			case 's':
