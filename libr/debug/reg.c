radareorg/radare2

commit b4373c6b40b41d4816f178ee3a270f0094298b03

More work towards DRX support on Linux

libr/debug/reg.c
@@ -6,14 +6,22 @@
 
 R_API int r_debug_reg_sync(RDebug *dbg, int type, int write) {
 	ut8 buf[4096]; // XXX hacky!
-	int size, ret = R_FALSE;
+	int next, size, ret = R_FALSE;
 	if (!dbg || !dbg->reg || dbg->pid == -1)
 		return R_FALSE;
+	if (type == -1) {
+		type = R_REG_TYPE_GPR;
+		next = R_REG_TYPE_DRX;
+	} else next = 0;
+		type = R_REG_TYPE_DRX;
+		next = 0; //R_REG_TYPE_DRX;
+repeat:
 	if (write) {
 		if (dbg && dbg->h && dbg->h->reg_write) {
 			ut8 *buf = r_reg_get_bytes (dbg->reg, type, &size);
 			if (!dbg->h->reg_write (dbg, type, buf, sizeof (buf)))
 				eprintf ("r_debug_reg: error writing registers\n");
+			else ret = R_TRUE;
 		} //else eprintf ("r_debug_reg: cannot set registers\n");
 	} else {
 		/* read registers from debugger backend to dbg->regs */
@@ -25,6 +33,15 @@ R_API int r_debug_reg_sync(RDebug *dbg, int type, int write) {
 				ret = r_reg_set_bytes (dbg->reg, type, buf, size);
 			}
 		} //else eprintf ("r_debug_reg: cannot read registers\n");
+	}
+	if (next) {
+		type = next;
+		switch (next) {
+		case R_REG_TYPE_FPU: next = R_REG_TYPE_DRX; break;
+		case R_REG_TYPE_DRX: next = 0; break;
+		default: next = 0; break;
+		}
+		goto repeat;
 	}
 	return ret;
 }

libr/core/cmd_debug.c
@@ -508,10 +508,16 @@ free (rf);
 				off = r_num_math (core->num, ARG(1));
 				len = (int)r_num_math (core->num, ARG(2));
 				rwx = (char)r_str_rwx (ARG(3));
-				r_debug_reg_sync (core->dbg, R_REG_TYPE_DRX, R_FALSE);
-				r_debug_drx_set (core->dbg, n, off, len, rwx, 0);
-				r_debug_reg_sync (core->dbg, R_REG_TYPE_DRX, R_TRUE);
-			} else eprintf ("Usage: drx N [address] [length] [rwx]\n");
+				if (len== -1) {
+					r_debug_reg_sync (core->dbg, R_REG_TYPE_DRX, R_FALSE);
+					r_debug_drx_set (core->dbg, n, 0, 0, 0, 0);
+					r_debug_reg_sync (core->dbg, R_REG_TYPE_DRX, R_TRUE);
+				} else {
+					r_debug_reg_sync (core->dbg, R_REG_TYPE_DRX, R_FALSE);
+					r_debug_drx_set (core->dbg, n, off, len, rwx, 0);
+					r_debug_reg_sync (core->dbg, R_REG_TYPE_DRX, R_TRUE);
+				}
+			} else eprintf ("|Usage: drx N [address] [length] [rwx]\n");
 			free (s);
 			} break;
 		case '\0':
@@ -596,13 +602,13 @@ free (rf);
 		arg = strchr (str+1, '=');
 		if (arg) {
 			*arg = 0;
-			r = r_reg_get (core->dbg->reg, str+1, R_REG_TYPE_GPR);
+			r = r_reg_get (core->dbg->reg, str+1, -1); //R_REG_TYPE_GPR);
 			if (r) {
 				r_cons_printf ("0x%08"PFMT64x" ->", str,
 					r_reg_get_value (core->dbg->reg, r));
 				r_reg_set_value (core->dbg->reg, r,
 					r_num_math (core->num, arg+1));
-				r_debug_reg_sync (core->dbg, R_REG_TYPE_GPR, R_TRUE);
+				r_debug_reg_sync (core->dbg, -1, R_TRUE);
 				r_cons_printf ("0x%08"PFMT64x"\n",
 					r_reg_get_value (core->dbg->reg, r));
 			} else eprintf ("Unknown register '%s'\n", str+1);

libr/debug/p/debug_native.c
@@ -915,12 +915,24 @@ if (dbg->bits & R_SYS_BITS_32) {
 	"gpr	if	.1	.1161	0	interrupt\n"
 	"gpr	df	.1	.1162	0	direction\n"
 	"gpr	of	.1	.1163	0	overflow\n"
+#if 0
  	"drx	dr0	.64	0	0\n"
  	"drx	dr1	.64	8	0\n"
  	"drx	dr2	.64	16	0\n"
  	"drx	dr3	.64	24	0\n"
- 	"drx	dr4	.64	32	0\n"
- 	"drx	dr7	.64	40	0\n"
+	// dr4 32
+	// dr5 40
+ 	"drx	dr6	.64	48	0\n"
+ 	"drx	dr7	.64	56	0\n"
+#endif
+	"drx	dr0	.32	0	0\n"
+	"drx	dr1	.32	4	0\n"
+	"drx	dr2	.32	8	0\n"
+	"drx	dr3	.32	12	0\n"
+	//"drx	dr4	.32	16	0\n"
+	//"drx	dr5	.32	20	0\n"
+	"drx	dr6	.32	24	0\n"
+	"drx	dr7	.32	28	0\n"
 	);
 #else
 	// 32bit host debugging 32bit target
@@ -1053,8 +1065,10 @@ if (dbg->bits & R_SYS_BITS_32) {
  	"drx	dr1	.64	8	0\n"
  	"drx	dr2	.64	16	0\n"
  	"drx	dr3	.64	24	0\n"
- 	"drx	dr4	.64	32	0\n"
- 	"drx	dr7	.64	40	0\n"
+	// dr4 32
+	// dr5 40
+ 	"drx	dr6	.64	48	0\n"
+ 	"drx	dr7	.64	56	0\n"
 	);
 }
 #elif (defined(__arm64__) || __arm__) && __APPLE__
@@ -1852,24 +1866,17 @@ eprintf ("++ EFL = 0x%08x  %d\n", ctx.EFlags, r_offsetof (CONTEXT, EFlags));
 		// XXX: maybe the register map is not correct, must review
 	}
 #elif __linux__
-#ifdef __ANDROID__
-		return R_TRUE;
-#else
 	{
 		int i;
-		for (i=0; i<7; i++) { // DR0-DR7
-			ret = ptrace (PTRACE_PEEKUSER, pid, r_offsetof (
+		for (i=0; i<8; i++) { // DR0-DR7
+			if (i==4 || i == 5) continue;
+			long ret = ptrace (PTRACE_PEEKUSER, pid, r_offsetof (
 				struct user, u_debugreg[i]), 0);
-			if (ret != 0) {
-				perror("ptrace");
-				//		return R_FALSE;
-			} else {
-				memcpy (buf+(i*4), &ret, 4);
-			}
+eprintf ("PEEK %d = %x\n", i, ret);
+			memcpy (buf+(i*sizeof(ret)), &ret, sizeof(ret));
 		}
 		return sizeof (R_DEBUG_REG_T);
 	}
-#endif
 #endif
 #endif
 		return R_TRUE;
@@ -1939,12 +1946,16 @@ static int r_debug_native_reg_write(RDebug *dbg, int type, const ut8* buf, int s
 #ifndef __ANDROID__
 		{
 		int i;
-		ut32 *val = (ut32 *)buf;
-		for (i=0; i<7; i++) { // DR0-DR7
-			ptrace (PTRACE_POKEUSER, dbg->pid, r_offsetof (
-				struct user, u_debugreg[i]), *(val+i));
-			//if (ret != 0)
-			//	return R_FALSE;
+		long *val = (long*)buf;
+		for (i=0; i<8; i++) { // DR0-DR7
+			if (i==4 || i == 5) continue;
+			long ret = ptrace (PTRACE_POKEUSER, dbg->pid, r_offsetof (
+				struct user, u_debugreg[i]), val[i]); //*(val+i));
+			if (ret != 0) {
+				eprintf ("ptrace error for dr %d\n", i);
+				perror("ptrace");
+				//return R_FALSE;
+			}
 		}
 		}
 		return sizeof (R_DEBUG_REG_T);
@@ -2630,27 +2641,21 @@ static int r_debug_native_drx(RDebug *dbg, int n, ut64 addr, int sz, int rwx, in
 	regs[7] = r_reg_getv (R, "dr7");
 
 	if (sz == 0) {
-		drx_list (&regs);
+		drx_list ((drxt*)&regs);
 		return R_FALSE;
-	} else
-	if (sz<0) {
-		// remove
-		drx_set (regs, n, addr, -1, 0, 0);
-		r_reg_setv (R, "dr0", regs[0]);
-		r_reg_setv (R, "dr1", regs[1]);
-		r_reg_setv (R, "dr2", regs[2]);
-		r_reg_setv (R, "dr3", regs[3]);
-		r_reg_setv (R, "dr7", regs[7]);
-		return R_FALSE;
-	} else {
-		drx_set (regs, n, addr, sz, rwx, g);
-		r_reg_setv (R, "dr0", regs[0]);
-		r_reg_setv (R, "dr1", regs[1]);
-		r_reg_setv (R, "dr2", regs[2]);
-		r_reg_setv (R, "dr3", regs[3]);
-		r_reg_setv (R, "dr7", regs[7]);
-		return R_TRUE;
 	}
+	if (sz<0) { // remove
+		drx_set (&regs, n, addr, -1, 0, 0);
+	} else {
+		drx_set (&regs, n, addr, sz, rwx, g);
+	}
+	r_reg_setv (R, "dr0", regs[0]);
+	r_reg_setv (R, "dr1", regs[1]);
+	r_reg_setv (R, "dr2", regs[2]);
+	r_reg_setv (R, "dr3", regs[3]);
+	r_reg_setv (R, "dr6", regs[6]);
+	r_reg_setv (R, "dr7", regs[7]);
+	return R_TRUE;
 #else
 	eprintf ("drx: Unsupported platform\n");
 #endif

libr/debug/p/native/drx.c
@@ -1,7 +1,7 @@
 #include <r_types.h>
 
 /* -------------------- drx.h ------------------- */
-#define DRXN 7
+#define DRXN 8
 #define DR_STATUS 6
 #define DR_CONTROL 7
 
@@ -86,20 +86,12 @@ int drx_set(drxt *drx, int n, ut64 addr, int len, int rwx, int global) {
 	if (rwx<0 || rwx>3)
 		rwx = 0; // defaults to X
 	switch (len) {
-	case 1:
-		len = 0;
-		break;
-	case 2:
-		len = 1<<2;
-		break;
-	case 4:
-		len = 3<<2;
-		break;
-	case 8:
-		len = 2<<2; // AMD64 only
-		break;
+	case 1: len = 0; break;
+	case 2: len = 1<<2; break;
+	case 4: len = 3<<2; break;
+	case 8: len = 2<<2; break; // AMD64 only
 	default:
-		eprintf ("Invalid DRX length\n");
+		eprintf ("Invalid DRX length (%d) must be 1, 2, 4, 8 bytes\n", len);
 		return R_FALSE;
 	}
 	I386_DR_SET_RW_LEN (control, n, len|rwx);
@@ -113,6 +105,7 @@ int drx_set(drxt *drx, int n, ut64 addr, int len, int rwx, int global) {
   	control &= I386_DR_CONTROL_MASK;
 	drx[n] = addr;
 	drx[DR_CONTROL] = control;
+eprintf ("SET 7 %x\n", control);
 	return R_TRUE;
 }
 
@@ -144,7 +137,9 @@ int drx_next(drxt *drx) {
 void drx_list(drxt *drx) {
 	ut64 addr;
 	int i, rwx, len, g, en;
-	for(i=0; i<7; i++) {
+	for (i=0; i<8; i++) {
+		if (i==4 || i == 5) 
+			continue;
 		rwx = len = g = en = 0;
 		addr = drx_get (drx, i, &rwx, &len, &g, &en);
 		printf ("%c dr%d %c%c 0x%08"PFMT64x" %d\n",

commit 105345388b1c3f6ff154d9652e6c8ab3f6c0487c

Fix debugger get-reg regression introduced in b4373c6b40b41d4816f178ee3a270f0094298b03

libr/debug/reg.c
@@ -13,8 +13,6 @@ R_API int r_debug_reg_sync(RDebug *dbg, int type, int write) {
 		type = R_REG_TYPE_GPR;
 		next = R_REG_TYPE_DRX;
 	} else next = 0;
-		type = R_REG_TYPE_DRX;
-		next = 0; //R_REG_TYPE_DRX;
 repeat:
 	if (write) {
 		if (dbg && dbg->h && dbg->h->reg_write) {