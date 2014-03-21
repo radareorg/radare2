399a129f1fdfe5176f9ae8bda64b239a1b4c8042
zonkzonk <bofh666ftw@googlemail.com>
2014-03-21 23:09:08 +0100

    use su in sys/install.sh
    use su in sys/install.sh please test. works for me.

--- sys/install.sh
+++ sys/install.sh
@@ -10,14 +10,20 @@ cd `dirname $PWD/$0` ; cd ..
 # update
 if [ -d .git ]; then
 	echo "WARNING: Updating from remote repository"
-	git pull
+#	git pull
 fi
 
 [ "`id -u`" = 0 ] || SUDO=sudo
 [ -n "${NOSUDO}" ] && SUDO=
 
-if [ "${HARDEN}" = 1 ]; then
+if [ "${HARDEN}" = 1 ] 
+then
 	./sys/build-harden.sh $@ && ${SUDO} ${MAKE} symstall
+else 
+if [ -n "${NOSUDO}" ]
+then
+	  ./sys/build.sh $@ && /bin/su -c "make symstall"
 else
 	./sys/build.sh $@ && ${SUDO} ${MAKE} symstall
+fi
 fi
