b41a90c6e2aa2a558f1a185a243ede3a0f0d72e0
Jann Horn <jann@thejh.net>
2014-03-26 17:47:03 +0100

    fix base64 encoding

--- shlr/sdb/src/base64.c
+++ shlr/sdb/src/base64.c
@@ -14,7 +14,7 @@ static void b64_encode(const ut8 in[3], char out[4], int len) {
 	if (len<1) return;
 	out[0] = cb64[ in[0] >> 2 ];
 	out[1] = cb64[ ((in[0] & 0x03) << 4) | ((len>1)?((in[1] & 0xf0) >> 4):0) ];
-	out[2] = (len > 1 ? cb64[ ((in[1] & 0x0f) << 2) | ((in[2] & 0xc0) >> 6) ] : '=');
+	out[2] = (len > 1 ? cb64[ ((in[1] & 0x0f) << 2) | (len > 2 ? ((in[2] & 0xc0) >> 6) : 0) ] : '=');
 	out[3] = (len > 2 ? cb64[ in[2] & 0x3f ] : '=');
 }
 
