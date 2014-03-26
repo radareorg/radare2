5f1e3448b33a9d24c2f5b65a9cee1a557f95ca30
condret <condret@runas-racer.com>
2014-03-26 15:35:22 +0100

    clean up

--- libr/include/r_anal.h
+++ libr/include/r_anal.h
@@ -951,8 +951,8 @@ R_API RAnalBlock *r_anal_fcn_bbget(RAnalFunction *fcn, ut64 addr); // default 20
 R_API void r_anal_xrefs_list(RAnal *anal, int rad);
 R_API RList* r_anal_fcn_get_refs (RAnalFunction *anal);
 R_API RList* r_anal_fcn_get_xrefs (RAnalFunction *anal);
-R_API RList *r_anal_xrefs_set (RAnal *anal, const char *type, ut64 from, ut64 to);
-R_API RList *r_anal_xrefs_deln (RAnal *anal, const char *type, ut64 from, ut64 to);
+R_API void r_anal_xrefs_set (RAnal *anal, const char *type, ut64 from, ut64 to);
+R_API void r_anal_xrefs_deln (RAnal *anal, const char *type, ut64 from, ut64 to);
 R_API void r_anal_xrefs_save(RAnal *anal, const char *prjfile);
 R_API RList* r_anal_fcn_get_vars (RAnalFunction *anal);
 R_API RList* r_anal_fcn_get_bbs (RAnalFunction *anal);
