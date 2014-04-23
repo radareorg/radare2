8c9519025681d4bff79ea2111b357f12d4becf41
Fedor Sakharov <fedor.sakharov@gmail.com>
2014-04-23 14:30:25 +0400

    dbginfo: Don't print path to source file in linnum info

--- libr/bin/dbginfo.c
+++ libr/bin/dbginfo.c
@@ -19,11 +19,17 @@ R_API char *r_bin_addr2text(RBin *bin, ut64 addr) {
 	char file[1024];
 	int line;
 	char *out = NULL, *out2;
+	char *file_nopath;
 
 	if (r_bin_addr2line (bin, addr, file, sizeof (file), &line)) {
 		out = r_file_slurp_line (file, line, 0);
+		if (!out)
+			return 0;
+
 		out2 = malloc((strlen(file) + 64 + strlen(out))*sizeof(char));
-		snprintf(out2, strlen(file) + 63 + strlen(out), "%s:%d %s", file, line, out);
+		file_nopath = strrchr (file, '/');
+		snprintf(out2, strlen(file) + 63 + strlen(out), "%s:%d %s",
+				file_nopath ? file_nopath + 1 : file, line, out);
 
 		free (out);
 		return out2;
@@ -35,10 +41,14 @@ R_API char *r_bin_addr2fileline(RBin *bin, ut64 addr) {
 	char file[1024];
 	int line;
 	char *out = NULL;
+	char *file_nopath;
+
 	if (r_bin_addr2line (bin, addr, file, sizeof (file)-1, &line)) {
 		int sz = strlen (file)+10;
+		file_nopath = strrchr (file, '/');
 		out = malloc (sz);
-		snprintf (out, sizeof (sz), "%s:%d", file, line);
+		snprintf (out, sizeof (sz), "%s:%d",
+				file_nopath ? file_nopath + 1 : file , line);
 	}
 	return out;
 }
