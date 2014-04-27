607f6886bc1c5ec07146e21c4ca9df0bac1fbc99
Anton Kochkov <anton.kochkov@gmail.com>
2014-04-27 05:07:43 +0400

    Fix CID 1134864

--- libr/bin/p/bin_te.c
+++ libr/bin/p/bin_te.c
@@ -63,8 +63,10 @@ static RList* sections(RBinFile *arch) {
 	if (!(ret = r_list_new ()))
 		return NULL;
 	ret->free = free;
-	if (!(sections = r_bin_te_get_sections(arch->o->bin_obj)))
+	if (!(sections = r_bin_te_get_sections(arch->o->bin_obj))) {
+		free (ret);
 		return NULL;
+	}
 	for (i = 0; !sections[i].last; i++) {
 		if (!(ptr = R_NEW0 (RBinSection)))
 			break;
