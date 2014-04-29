c632ce8277fcbe99cde8f7e44f685cc362ec5b52
Anton Kochkov <anton.kochkov@gmail.com>
2014-04-30 02:19:55 +0400

    Fix CID 1191355

--- libr/io/p/io_gdb.c
+++ libr/io/p/io_gdb.c
@@ -7,19 +7,15 @@
 #define IRAPI static inline
 #include <libgdbr.h>
 
-
 typedef struct {
 	libgdbr_t desc;
 } RIOGdb;
 
-
 static libgdbr_t *desc = NULL;
-
 
 static int __plugin_open(RIO *io, const char *file, ut8 many) {
 	return (!strncmp (file, "gdb://", 6));
 }
-
 
 static int debug_gdb_read_at(ut8 *buf, int sz, ut64 addr) {
 	ut32 size_max = 500;
@@ -28,16 +24,15 @@ static int debug_gdb_read_at(ut8 *buf, int sz, ut64 addr) {
 	ut32 x;
 	if (sz < 1 || addr >= UT64_MAX) return -1;
 	for (x = 0; x < packets; x++) {
-		gdbr_read_memory(desc, addr + x * size_max, size_max); 
+		gdbr_read_memory(desc, addr + x * size_max, size_max);
 		memcpy((buf + x * size_max), desc->data + x * size_max, size_max);
 	}
 	if (last) {
 		gdbr_read_memory(desc, addr + x * size_max, last);
 		memcpy((buf + x * size_max), desc->data + x * size_max, last);
 	}
-	return sz;	
+	return sz;
 }
-
 
 static int debug_gdb_write_at(const ut8 *buf, int sz, ut64 addr) {
 	ut32 size_max = 500;
@@ -57,12 +52,11 @@ static int debug_gdb_write_at(const ut8 *buf, int sz, ut64 addr) {
 	return sz;
 }
 
-
 static RIODesc *__open(RIO *io, const char *file, int rw, int mode) {
 	char host[128], *port, *p;
 	if (!__plugin_open (io, file, 0))
 		return NULL;
-	RIOGdb *riog;	
+	RIOGdb *riog;
 	strncpy (host, file+6, sizeof (host)-1);
 	port = strchr (host , ':');
 	if (!port) {
@@ -86,9 +80,9 @@ static RIODesc *__open(RIO *io, const char *file, int rw, int mode) {
 		return r_io_desc_new (&r_io_plugin_gdb, riog->desc.fd, file, rw, mode, riog);
 	}
 	eprintf ("gdb.io.open: Cannot connect to host.\n");
+	free (riog);
 	return NULL;
 }
-
 
 static int __write(RIO *io, RIODesc *fd, const ut8 *buf, int count) {
 	ut64 addr = io->off;
@@ -96,11 +90,9 @@ static int __write(RIO *io, RIODesc *fd, const ut8 *buf, int count) {
 	return debug_gdb_write_at(buf, count, addr);
 }
 
-
 static ut64 __lseek(RIO *io, RIODesc *fd, ut64 offset, int whence) {
 	return offset;
 }
-
 
 static int __read(RIO *io, RIODesc *fd, ut8 *buf, int count) {
 	memset (buf, 0xff, count);
@@ -109,28 +101,26 @@ static int __read(RIO *io, RIODesc *fd, ut8 *buf, int count) {
 	return debug_gdb_read_at(buf, count, addr);
 }
 
-
 static int __close(RIODesc *fd) {
 	// TODO
 	return -1;
 }
 
-
 static int __system(RIO *io, RIODesc *fd, const char *cmd) {
 	return -1;
 }
 
-
 RIOPlugin r_io_plugin_gdb = {
-  //void *plugin;
+	//void *plugin;
 	.name = "gdb",
-	.desc = "Attach to gdbserver, 'qemu -s', gdb://localhost:1234", 
-  .open = __open,
-  .close = __close,
+	.desc = "Attach to gdbserver, 'qemu -s', gdb://localhost:1234",
+	.open = __open,
+	.close = __close,
 	.read = __read,
 	.write = __write,
-  .plugin_open = __plugin_open,
+	.plugin_open = __plugin_open,
 	.lseek = __lseek,
 	.system = __system,
 	.debug = (void *)1,
 };
+
