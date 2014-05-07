a8168018a9b65c85d171df41383e4eaf6db9d121
zonkzonk <bofh666ftw@googlemail.com>
2014-05-08 00:32:19 +0200

    fix wrong test in sync.sh

--- sys/sync.sh
+++ sys/sync.sh
@@ -4,4 +4,4 @@ if ! git remote | grep upstream > /dev/null
 then 
     git remote add upstream https://github.com/radare/radare2.git
 fi
-test -d radare2 && git fetch upstream && git rebase --onto master upstream/master
+git fetch upstream && git rebase --onto master upstream/master
