3b4f167473ec3a70ec0c8939ebb5ba3d8e7faa19
Anton Kochkov <anton.kochkov@gmail.com>
2014-05-17 02:52:27 +0400

    Fix CID 1214310

--- libr/bin/bin.c
+++ libr/bin/bin.c
@@ -1197,7 +1197,7 @@ R_API int r_bin_select(RBin *bin, const char *arch, int bits, const char *name) 
 	RBinObject *obj = NULL;
 	name = !name && cur ? cur->file : name;
 	binfile = r_bin_file_find_by_arch_bits (bin, arch, bits, name);
-	if (binfile) {
+	if (binfile && name) {
 		obj = r_bin_object_find_by_arch_bits (binfile, arch, bits, name);
 	}
 	return binfile && r_bin_file_set_cur_binfile_obj (bin, binfile, obj);
