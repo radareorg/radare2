557c0b2f03513d1cd805a688f76e13daaf11ae5e
Anton Kochkov <anton.kochkov@gmail.com>
2014-05-19 05:38:19 +0400

    Fix CID 1214585, 1214586

--- libr/util/uleb128.c
+++ libr/util/uleb128.c
@@ -17,14 +17,15 @@ R_API const ut8 *r_uleb128 (const ut8 *data, ut64 *v) {
 }
 
 R_API const ut8 *r_leb128 (const ut8 *data, st64 *v) {
-	ut8 c;
-	st64 s, sum = 0;
-	if (data)
-	for (s = 0; *data;) {
-		c = *(data++) & 0x0ff;
-		sum |= ((st64) (c & 0x7f) << s);
-		s += 7;
-		if (!(c & 0x80)) break;
+	ut8 c = 0;
+	st64 s = 0, sum = 0;
+	if (data) {
+		for (s = 0; *data;) {
+			c = *(data++) & 0x0ff;
+			sum |= ((st64) (c & 0x7f) << s);
+			s += 7;
+			if (!(c & 0x80)) break;
+		}
 	}
 	if ((s < (8 * sizeof (sum))) && (c & 0x40)) {
 		sum |= -((st64)1 << s);
