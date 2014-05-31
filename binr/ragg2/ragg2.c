d6c2866ab659c40fab46bc641dc80338b11be5b9
pancake <pancake@nopcode.org>
2014-05-31 14:22:39 +0200

    Handle multiple -B in ragg2

--- binr/ragg2/ragg2.c
+++ binr/ragg2/ragg2.c
@@ -87,7 +87,7 @@ static int openfile (const char *f, int x) {
 int main(int argc, char **argv) {
 	const char *file = NULL;
 	const char *padding = NULL;
-	const char *bytes = NULL;
+	char *bytes = NULL;
 	const char *contents = NULL;
 	const char *arch = R_SYS_ARCH;
 	const char *os = R_EGG_OS_NAME;
@@ -122,7 +122,7 @@ int main(int argc, char **argv) {
 			bits = atoi (optarg);
 			break;
 		case 'B':
-			bytes = optarg;
+			bytes = r_str_concat (bytes, optarg);
 			break;
 		case 'C':
 			contents = optarg;
@@ -291,6 +291,8 @@ int main(int argc, char **argv) {
 			}
 		} else eprintf ("Invalid hexpair string for -B\n");
 		free (b);
+		free (bytes);
+		bytes = NULL;
 	}
 	/* create output file if needed */
 	if (ofileauto) {
@@ -344,8 +346,7 @@ int main(int argc, char **argv) {
 		} else {
 			if (!format) {
 				eprintf ("No format specified wtf\n");
-				r_egg_free (egg);
-				return 1;
+				goto fail;
 			}
 			switch (*format) { //*format) {
 			case 'r':
