d55550550a31028fc363be61f9e0472bb5d8db55
jvoisin <julien.voisin@dustri.org>
2014-06-22 13:58:56 +0200

    Refactor a little bit rsa-find.c

--- libr/search/rsa-find.c
+++ libr/search/rsa-find.c
@@ -17,11 +17,11 @@ Parses a single field of the key, beginning at start.  Each field
 consists of a type, a length, and a value.  Puts the type of field
 into type, the number of bytes into len, and returns a pointer to
 the beginning of the value. */
-R_API ut8* r_parse_next(ut8* start, ut32 *type, ut32 *len) {
+static ut8* parse_next_rsa_field(const ut8* start, ut32 *type, ut32 *len) {
 	ut8 *val = NULL;
 	*type = start[0]; 
 	*len  = 0;
-	if ((start[1] & 0x80) == 0) {
+	if (!(start[1] & 0x80)) {
 		*len = start[1];
 		*val = start[2];
 	} else {
@@ -34,14 +34,15 @@ R_API ut8* r_parse_next(ut8* start, ut32 *type, ut32 *len) {
 	return val;
 }
 
-static int check_rsa_fields(ut8* start, int nbfields) {
+static int check_rsa_fields(const ut8* start, int nbfields) {
 	ut32 len=0, type;
 	int i;
-	start = r_parse_next (start,&type,&len); // skip sequence field
+	ut8 const* ptr = start;
+	ptr = parse_next_rsa_field (ptr, &type, &len); // skip sequence field
 	if (!len || len > 1024)
 		return R_FALSE;
 	for (i = 0; i < nbfields; i++)
-		if (!(start = r_parse_next (start,&type,&len)))
+		if (!(ptr = parse_next_rsa_field (ptr, &type, &len)))
 			return R_FALSE;
 	return R_TRUE;
 }
@@ -49,26 +50,27 @@ static int check_rsa_fields(ut8* start, int nbfields) {
 // Returns a pointer to the beginning of a BER-encoded key by working
 // backwards from the given memory map offset, looking for the
 // sequence identifier (this is not completely safe)
-static ut8 *r_find_key_start(const ut8 *map, int offset) {
+static int find_rsa_key_start(const ut8 *map, int offset) {
 	int k;
 	for (k = offset; k >= 0 && k > offset-20; k--)
 		if (map[k] == 0x30)
-			return &map[k];
-	return NULL;
+			return k;
+	return 0;
 }
 
 // Finds and prints private (or private and public) keys in the memory
 // map by searching for given target pattern
 R_API int r_search_rsa_update(void* s, ut64 from, const ut8 *buf, int len) {
-	unsigned int i;
+	unsigned int i, index;
 	const ut8 versionmarker[4] = {0x02, 0x01, 0x00, 0x02};
 	for (i = 0; i < len - sizeof (versionmarker); i++) {
 		if (memcmp (&buf[i], versionmarker, sizeof (versionmarker)))
 			continue;   
 
-		ut8 *key = r_find_key_start(buf, i);
-		if (!key)
+		index = find_rsa_key_start (buf, i);
+		if (!index)
 			continue;
+		const ut8* key = buf + index;
 
 		if (check_rsa_fields(key, NB_PRIVATES_FIELDS)) {
 			printf("FOUND PRIVATE KEY AT %x\n", (ut32)(key-buf));
