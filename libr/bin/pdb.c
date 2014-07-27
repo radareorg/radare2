2b3f60d1cd9c3508f882dd535c25ea0e1398c569
inisider <inisider@gmail.com>
2014-07-27 11:55:14 -0400

    add first implemntation of pdb7_parse()

--- libr/bin/pdb.c
+++ libr/bin/pdb.c
@@ -4,21 +4,180 @@
 
 #define PDB2_SIGNATURE "Microsoft C/C++ program database 2.00\r\n\032JG\0\0"
 #define PDB7_SIGNATURE "Microsoft C/C++ MSF 7.00\r\n\x1ADS\0\0\0"
-#define PDB7_SIGNATURE_LEN 40
+#define PDB7_SIGNATURE_LEN 32
 #define PDB2_SIGNATURE_LEN 51
 
+///////////////////////////////////////////////////////////////////////////////
+static int read_int_var(char *var_name, int *var, FILE *fp)
+{
+	int bytes_read = fread(var, 4, 1, fp);
+	if (bytes_read != 1) {
+		printf("error while reading from file [%s]", var_name);
+		return 0;
+	}
+
+	return 1;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+static int count_pages(int length, int page_size)
+{
+	int num_pages = 0;
+	num_pages = length / page_size;
+	if (length % page_size)
+		num_pages++;
+	return num_pages;
+}
+
+///////////////////////////////////////////////////////////////////////////////
 static int pdb7_parse(R_PDB *pdb)
 {
 	printf("pdb7_parse()\n");
+
+	char signature[PDB7_SIGNATURE_LEN + 1];
+	int page_size = 0;
+	int alloc_tbl_ptr = 0;
+	int num_file_pages = 0;
+	int root_size = 0;
+	int reserved = 0;
+
+	int num_root_pages = 0;
+	int num_root_index_pages = 0;
+	int *root_index_pages = 0;
+	void *root_page_data = 0;
+	int *root_page_list = 0;
+
+	int i = 0;
+	void *p_tmp;
+
+	int bytes_read = 0;
+
+	bytes_read = fread(signature, 1, PDB7_SIGNATURE_LEN, pdb->fp);
+	if (bytes_read != PDB7_SIGNATURE_LEN) {
+		printf("error while reading PDB7_SIGNATURE\n");
+		goto error;
+	}
+
+	if (read_int_var("page_size", &page_size, pdb->fp) == 0) {
+		goto error;
+	}
+
+	if (read_int_var("alloc_tbl_ptr", &alloc_tbl_ptr, pdb->fp) == 0) {
+		goto error;
+	}
+
+	if (read_int_var("num_file_pages", &num_file_pages, pdb->fp) == 0) {
+		goto error;
+	}
+
+	if (read_int_var("root_size", &root_size, pdb->fp) == 0) {
+		goto error;
+	}
+
+	if (read_int_var("reserved", &reserved, pdb->fp) == 0) {
+		goto error;
+	}
+
+	// FIXME: why they is not equal ????
+	if (memcmp(signature, PDB7_SIGNATURE, PDB7_SIGNATURE_LEN) != 0) {
+		printf("Invalid signature for PDB7 format\n");
+		//goto error;
+	}
+
+	// TODO:
+	// create stream of maps and names
+	// ...
+
+	num_root_pages = count_pages(root_size, page_size);
+	num_root_index_pages = count_pages((num_root_pages * 4), page_size);
+
+	root_index_pages = (int *)malloc(sizeof(int) * num_root_index_pages);
+	if (!root_index_pages) {
+		printf("error memory allocation\n");
+		goto error;
+	}
+
+	bytes_read = fread(root_index_pages, 4, num_root_index_pages, pdb->fp);
+	if (bytes_read != num_root_index_pages) {
+		printf("error while reading root_index_pages\n");
+		goto error;
+	}
+
+	root_page_data = (int *)malloc(page_size * num_root_index_pages);
+	if (!root_page_data) {
+		printf("error memory allocation of root_page_data\n");
+		goto error;
+	}
+
+	p_tmp = root_page_data;
+	for (i = 0; i < num_root_index_pages; i++) {
+		fseek(pdb->fp, root_index_pages[i] * page_size, SEEK_SET);
+		fread(p_tmp, page_size, 1, pdb->fp);
+		p_tmp = (char *)p_tmp + page_size;
+	}
+
+	root_page_list = (int *)malloc(sizeof(int) * num_root_pages);
+	if (!root_page_list) {
+		printf("error: memory allocation of root page\n");
+		goto error;
+	}
+
+	p_tmp = root_page_data;
+	for (i = 0; i < num_root_pages; i++) {
+		root_page_list[i] = *((int *)p_tmp);
+		p_tmp = (int *)p_tmp + 1;
+	}
+
+	// TODO: get root stream
+	// TODO: read root stream
+//self.root_stream = PDB7RootStream(self.fp, root_page_list,
+//    index=PDB_STREAM_ROOT, size=root_size, page_size=self.page_size)
+//self.read_root(self.root_stream)
+
+	if (root_page_list) {
+		free(root_page_list);
+		root_page_list = 0;
+	}
+
+	if (root_page_data) {
+		free(root_page_data);
+		root_page_data = 0;
+	}
+
+	if (root_index_pages) {
+		free(root_index_pages);
+		root_index_pages = 0;
+	}
+
+	return 1;
+
+error:
+	if (root_page_list) {
+		free(root_page_list);
+		root_page_list = 0;
+	}
+
+	if (root_page_data) {
+		free(root_page_data);
+		root_page_data = 0;
+	}
+
+	if (root_index_pages) {
+		free(root_index_pages);
+		root_index_pages = 0;
+	}
+
 	return 0;
 }
 
+///////////////////////////////////////////////////////////////////////////////
 static void finish_pdb_parse(R_PDB *pdb)
 {
 	fclose(pdb->fp);
 	printf("finish_pdb_parse()\n");
 }
 
+///////////////////////////////////////////////////////////////////////////////
 int init_pdb_parser(R_PDB *pdb)
 {
 	char *signature = 0;
