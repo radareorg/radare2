36e64b7ea662a0bc1158ff2327c194f2501c8bb9
pancake <pancake@nopcode.org>
2014-08-01 16:27:02 +0200

    Fix sys/duktape.sh

--- sys/duktape.sh
+++ sys/duktape.sh
@@ -5,27 +5,20 @@ cd `dirname $PWD/$0`
 ./clone-r2-bindings.sh
 cd ..
 
-. ./sys/CONFIG
-echo =============
-cat sys/CONFIG
-echo =============
-
 [ -z "${PREFIX}" ] && PREFIX=/usr
 ID=`id -u` 
 [ -n "${NOSUDO}" ] && SUDO=
 
 export DESTDIR
 
-cd radare2-bindings
-#./configure --prefix=${PREFIX} --enable=duktype || exit 1
-cd libr/lang/p
+cd radare2-bindings/libr/lang/p
 make clean
 make
 rm ~/.config/radare2/plugins/lang_duktape.*
-if [ "$ID" = 0 ]; then
-	make install DESTDIR=${DESTDIR}
-else
-	make install-home
+if [ "$1" != '--no-install' ]; then
+	if [ "$ID" = 0 ]; then
+		${SUDO} make install DESTDIR=${DESTDIR}
+	else
+		make install-home
+	fi
 fi
-[ "$1" != '--no-install' ] && \
-	${SUDO} make install DESTDIR=${DESTDIR}
