b2a4e24f8a88f345f7ffb59b7ddd8165a789fd74
pancake <pancake@nopcode.org>
2014-08-04 19:07:41 +0200

    Fix PE64 identifcation issue.

--- libr/bin/p/bin_pe64.c
+++ libr/bin/p/bin_pe64.c
@@ -22,7 +22,7 @@ static int check_bytes(const ut8 *buf, ut64 length) {
 	idx = buf[0x3c] | (buf[0x3d]<<8);
 	if (length >= idx+0x20)
 		if (!memcmp (buf, "MZ", 2) &&
-			!memcmp (buf+idx, "MZ", 2) &&
+			!memcmp (buf+idx, "PE", 2) &&
 			!memcmp (buf+idx+0x18, "\x0b\x02", 2))
 			ret = R_TRUE;
 	return ret;
