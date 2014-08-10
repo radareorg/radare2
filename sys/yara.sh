4b3e5de4ac7d5cd9193a1cc30e772401df018d5c
pancake <pancake@nopcode.org>
2014-08-10 17:29:58 +0200

    Fetch peid.yara with sys/yara.sh (1MB)

--- sys/yara.sh
+++ sys/yara.sh
@@ -3,6 +3,15 @@ PREFIX=/usr
 MAKE=make
 SUDO=sudo
 
+# https://raw.githubusercontent.com/sroberts/peid4yara/master/peid.yar
+if [ ! -f shlr/yara/peid.yar ]; then
+(
+	cd shlr/yara
+	wget -c http://radare.org/get/peid.yar.gz
+	gunzip peid.yar.gz
+)
+fi
+
 rm -rf yara
 git clone https://github.com/plusvic/yara.git || exit 1
 cd yara || exit 1
