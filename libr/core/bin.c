4ebe3dc0735c1ba02eba1cfb5d7597d84c7ef8f3
condret <condret@runas-racer.com>
2014-08-19 16:43:04 +0200

    the cpu-thing

--- libr/core/bin.c
+++ libr/core/bin.c
@@ -40,7 +40,9 @@ R_API int r_core_bin_set_env (RCore *r, RBinFile *binfile) {
 		r_config_set (r->config, "asm.arch", arch);
 		r_config_set_i (r->config, "asm.bits", bits);
 		r_config_set (r->config, "anal.arch", arch);
-		r_config_set (r->config, "anal.cpu", arch);
+		if (strlen(info->cpu))
+			r_config_set (r->config, "anal.cpu", info->cpu);
+		else	r_config_set (r->config, "anal.cpu", arch);
 		r_asm_use (r->assembler, arch);
 
 		r_core_bin_info (r, R_CORE_BIN_ACC_ALL, R_CORE_BIN_SET,
