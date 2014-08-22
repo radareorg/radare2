dbc16257775a39131ea09c6bb6b188cd6aefa518
pancake <pancake@nopcode.org>
2014-08-23 01:53:58 +0200

    Fix build

--- libr/bin/p/any.mk
+++ libr/bin/p/any.mk
@@ -6,4 +6,5 @@ TARGET_ANY=bin_any.${EXT_SO}
 ALL_TARGETS+=${TARGET_ANY}
 
 ${TARGET_ANY}: ${OBJ_ANY}
-	${CC} $(call libname,bin_any) ${CFLAGS} $(OBJ_ANY) $(LINK) $(LDFLAGS)
+	${CC} $(call libname,bin_any) ${CFLAGS} $(OBJ_ANY) $(LINK) $(LDFLAGS) \
+	-L../../magic -lr_magic
