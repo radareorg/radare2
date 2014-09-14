c74950d7b2cc47997711e6e5ed6c15265b0dd69a
Edward O'Callaghan <eocallaghan@alterapraxis.com>
2014-09-15 05:12:51 +1000

    mk/clang.mk: Provide Clang support
    Signed-off-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

---
+++ mk/clang.mk
@@ -0,0 +1,47 @@
+ifeq (${_INCLUDE_MK_CLANG_},)
+_INCLUDE_MK_CLANG_=1
+CC=clang
+LINK=
+RANLIB=ranlib
+ONELIB=0
+CC_AR=ar q ${LIBAR}
+CFLAGS+=-MD
+CFLAGS_INCLUDE=-I
+LDFLAGS_LINK=-l
+LDFLAGS_LINKPATH=-L
+CFLAGS_OPT0=-O0
+CFLAGS_OPT1=-O1
+CFLAGS_OPT2=-O2
+CFLAGS_OPT3=-O3
+CFLAGS_DEBUG=-g
+
+ifeq ($(OSTYPE),auto)
+OSTYPE=$(shell uname | tr 'A-Z' 'a-z')
+endif
+ifneq (,$(findstring cygwin,${OSTYPE}))
+PIC_CFLAGS=
+else
+ifneq (,$(findstring mingw32,${OSTYPE}))
+PIC_CFLAGS=
+else
+PIC_CFLAGS=-fPIC
+endif
+endif
+ifeq ($(OSTYPE),darwin)
+ARCH=$(shell uname -m)
+#CFLAGS+=-arch ${ARCH}
+#LDFLAGS+=-arch ${ARCH}
+LDFLAGS_LIB=-dynamiclib
+LDFLAGS_SONAME=-Wl,-install_name,${LIBDIR}/
+else
+LDFLAGS_LIB=${LDFLAGS} -shared
+#ifneq (${NAME},)
+#LDFLAGS_LIB+=-Wl,-soname,lib${NAME}.${EXT_SO}.${VERSION}
+#endif
+LDFLAGS_SONAME=-Wl,-soname=
+endif
+# XXX
+#LDFLAGS_SONAME=-D_
+
+CC_LIB=${CC} ${LDFLAGS_LIB} -o ${LIBSO}
+endif
