8be98f3d82661254ffded3430c81dc8d26c22e1c
pancake <pancake@nopcode.org>
2014-09-15 00:36:49 +0200

    Use () instead of [] for casting nested structs in pf

--- libr/util/p_format.c
+++ libr/util/p_format.c
@@ -386,7 +386,7 @@ R_API int r_print_format(RPrint *p, ut64 seek, const ut8* b, const int len,
 #define MUSTSET (setval && elem == idx)
 #define MUSTSEE (elem == -1 || elem == idx)
 #define SEEFLAG (elem == -2)
-			if (MUSTSEE) {
+			if (1 && MUSTSEE) {
 				if (!(MUSTSET)) {
 					if (oldprintf)
 						p->printf = oldprintf;
@@ -460,8 +460,9 @@ R_API int r_print_format(RPrint *p, ut64 seek, const ut8* b, const int len,
 				//tmp = (sizeof (void*)==8)? 'q': 'x';
 				break;
 			}
-			if (SEEFLAG) {
-				p->printf ("f %s=0x%08"PFMT64x"\n", r_str_word_get0 (args, idx) , seeki);
+			if (1 && SEEFLAG) {
+				p->printf ("f %s=0x%08"PFMT64x"\n",
+					r_str_word_get0 (args, idx) , seeki);
 			}
 
 			if (isptr == 3) {
@@ -508,19 +509,22 @@ R_API int r_print_format(RPrint *p, ut64 seek, const ut8* b, const int len,
 				i+= (size==-1) ? 1 : size;
 				break;
 			case 'c':
-				r_print_format_char(p, MUSTSET, setval, seeki, buf, i);
+				r_print_format_char (p, MUSTSET,
+					setval, seeki, buf, i);
 				i+= (size==-1) ? 1 : size;
 				break;
 			case 'X':
-				size = r_print_format_hexpairs(p, MUSTSET, setval, seeki, buf, size);
-				i+=size;
+				size = r_print_format_hexpairs(p, MUSTSET,
+					setval, seeki, buf, size);
+				i += size;
 				break;
 			case 'B':
-				if(r_print_format_10bytes(p, MUSTSET, setval, seeki, addr, buf) == 0)
-					i+= (size==-1) ? 4 : size;
+				if (r_print_format_10bytes (p, MUSTSET,
+					setval, seeki, addr, buf) == 0)
+					i += (size==-1) ? 4 : size;
 				break;
 			case 'f':
-				r_print_format_float(p, MUSTSET, setval, seeki, addr);
+				r_print_format_float (p, MUSTSET, setval, seeki, addr);
 				i+= (size==-1) ? 4 : size;
 				break;
 			case 'i':
@@ -556,9 +560,9 @@ R_API int r_print_format(RPrint *p, ut64 seek, const ut8* b, const int len,
 				break;
 			case 'z': // zero terminated string
 				if (MUSTSET) {
-					int buflen = strlen (buf);
+					int buflen = strlen ((const char *)buf);
 					if (buflen>seeki) {
-						buflen = strlen (buf+seeki);
+						buflen = strlen ((const char *)buf+seeki);
 					}
 					if (strlen (setval) > buflen) {
 						eprintf ("Warning: new string is longer than previous one \n");
@@ -610,8 +614,8 @@ R_API int r_print_format(RPrint *p, ut64 seek, const ut8* b, const int len,
 				{
 				int s;
 				char *structname = strdup (r_str_word_get0 (args, idx-1));
-				if (*structname == '[') {
-					name = strchr (structname, ']');
+				if (*structname == '(') {
+					name = strchr (structname, ')');
 				} else {
 					eprintf ("Struct name missing\n");
 					free (structname);
@@ -626,7 +630,8 @@ R_API int r_print_format(RPrint *p, ut64 seek, const ut8* b, const int len,
 				p->printf ("<struct>\n");
 				/* if (SEEFLAG) slide+=10000;*/
 				slide += (isptr) ? 100 : 1;
-				s = r_print_format_struct (p, seeki, buf+i, len, structname--, slide);
+				s = r_print_format_struct (p, seeki,
+					buf+i, len, structname--, slide);
 				free (structname);
 				i+= (isptr) ? 4 : s;
 				slide -= (isptr) ? 100 : 1;
