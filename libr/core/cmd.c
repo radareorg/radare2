41c116863f408d4b6b21cec08f7911c164254f2a
-TOSH- <tosh@t0x0sh.org>
2014-09-27 10:40:38 +0200

    Fix: typo in b? command

--- libr/core/cmd.c
+++ libr/core/cmd.c
@@ -516,7 +516,7 @@ static int cmd_bsize(void *data, const char *input) {
 			"b", "", "display current block size",
 			"b", " 33", "set block size to 33",
 			"b", "+3", "increase blocksize by 3",
-			"b", "-16", "decrement blocksize by 3",
+			"b", "-16", "decrease blocksize by 16",
 			"b", " eip+4", "numeric argument can be an expression",
 			"bf", " foo", "set block size to flag size",
 			"bm", " 1M", "set max block size",
@@ -1254,7 +1254,7 @@ R_API int r_core_cmd_foreach(RCore *core, const char *cmd, char *each) {
 
 	switch (each[0]) {
 	case '?':{
-		const char* help_msg[] = {	
+		const char* help_msg[] = {
 		"@@", "", " # foreach iterator command:",
 		"Repeat a command over a list of offsets", "", "",
 		"x", " @@ sym.*", "run 'x' over all flags matching 'sym.' in current flagspace",
