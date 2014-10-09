514b120a9477c13663be6608202fbdff91f4e9e8
pancake <pancake@nopcode.org>
2014-10-09 14:05:31 +0200

    Fix a read-out-of-bounds bug in the java class parser

--- shlr/java/class.c
+++ shlr/java/class.c
@@ -1869,15 +1869,15 @@ R_API RBinJavaAttrInfo* r_bin_java_get_attr_from_field(RBinJavaField *field, R_B
 
 R_API ut8* r_bin_java_get_attr_buf(RBinJavaObj *bin,  ut64 sz, const ut64 offset, const ut8 *buf, const ut64 len) {
 	ut8 *attr_buf = NULL;
+	int pending = len-offset;
 	const ut8 * a_buf =  offset + buf;
-	attr_buf = (ut8 *) malloc (sz+1);
-	memset (attr_buf, 0, sz+1);
+	attr_buf = (ut8 *) calloc (pending, 1);
 	if (attr_buf == NULL) {
 		eprintf ("Unable to allocate enough bytes (0x%04"PFMT64x
 			") to read in the attribute.\n", sz);
 		return attr_buf;
 	}
-	memcpy (attr_buf, a_buf, sz+1);
+	memcpy (attr_buf, a_buf, pending); //sz+1);
 	return attr_buf;
 }
 
