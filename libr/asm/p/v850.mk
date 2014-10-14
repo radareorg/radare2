14a6bbe1b21932acceb865c35d12865df52a9ba4
Fedor Sakharov <fedor.sakharov@gmail.com>
2014-10-14 15:53:59 +0400

    Move arch headers from ./lib/include

--- libr/asm/p/v850.mk
+++ libr/asm/p/v850.mk
@@ -1,5 +1,6 @@
 OBJ_V850=asm_v850.o
 OBJ_V850+=../arch/v850/v850_disas.o
+CFLAGS+=-I./arch/v850
 
 
 STATIC_OBJ+=${OBJ_V850}
@@ -8,4 +9,4 @@ TARGET_V850=asm_v850.${EXT_SO}
 ALL_TARGETS+=${TARGET_V850}
 
 ${TARGET_V850}: ${OBJ_V850}
-	${CC} ${LDFLAGS} ${CFLAGS} -I../arch/v850 -o ${TARGET_V850} ${OBJ_V850}
+	${CC} ${LDFLAGS} ${CFLAGS} -o ${TARGET_V850} ${OBJ_V850}
