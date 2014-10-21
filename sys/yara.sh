5201f772f84b916cdec20539e12fb7d5eabc4e8e
pancake <pancake@nopcode.org>
2014-10-21 02:06:22 +0200

    Add UPX linux signature for Yara. Force to use yara 2.1 in sys/yara.sh

--- sys/yara.sh
+++ sys/yara.sh
@@ -15,6 +15,7 @@ SUDO=sudo
 rm -rf yara
 git clone https://github.com/plusvic/yara.git || exit 1
 cd yara || exit 1
+git reset --hard 880c268ce0b98046a476784c412d9e91573c8a08
 sh bootstrap.sh
 ./configure --prefix=${PREFIX} || exit 1
 ${MAKE} || exit 1
