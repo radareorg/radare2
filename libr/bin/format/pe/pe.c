3e3b2220339d73e96d5fe67b4f66ac8d1bc1213f
Maijin <Maijin@users.noreply.github.com>
2014-10-31 15:13:18 +0100

    Add timestamp

--- libr/bin/format/pe/pe.c
+++ libr/bin/format/pe/pe.c
@@ -212,7 +212,7 @@ static int PE_(r_bin_pe_init_hdr)(struct PE_(r_bin_pe_obj_t)* bin) {
 					  " IMAGE_FILE_BYTES_REVERSED_LO=0x0080, IMAGE_FILE_32BIT_MACHINE=0x0100, IMAGE_FILE_DEBUG_STRIPPED=0x0200, "
 					  " IMAGE_FILE_REMOVABLE_RUN_FROM_SWAP=0x0400, IMAGE_FILE_NET_RUN_FROM_SWAP=0x0800, IMAGE_FILE_SYSTEM=0x1000, "
 					  " IMAGE_FILE_DLL=0x2000, IMAGE_FILE_UP_SYSTEM_ONLY=0x4000, IMAGE_FILE_BYTES_REVERSED_HI=0x8000 };", 0);
-	sdb_set (bin->kv, "pe_image_file_header.format", "[2]Ewxxxw[2]B"
+	sdb_set (bin->kv, "pe_image_file_header.format", "[2]Ewtxxw[2]B"
 					  " (pe_machine)machine numberOfSections timeDateStamp pointerToSymbolTable"
 					  " numberOfSymbols sizeOfOptionalHeader (pe_characteristics)characteristics", 0);
 	sdb_set (bin->kv, "pe_image_data_directory.format", "xx virtualAddress size",0);
