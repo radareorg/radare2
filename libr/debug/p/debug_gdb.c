d8f0af172a5719975742c506f2d0ca6df7237bda
trndr <trndr.k@gmail.com>
2014-11-03 20:02:21 +0000

    Try to implement MIPS gdb reg profile

--- libr/debug/p/debug_gdb.c
+++ libr/debug/p/debug_gdb.c
@@ -150,7 +150,14 @@ eprintf ("XWJSTEP TOFALSE\n");
 					eprintf("Not supported register profile\n");
 					return R_FALSE;
 				}
-
+				break;
+			case R_SYS_ARCH_MIPS:
+				if ( dbg->anal->bits == 32 ) {
+					gdbr_set_architecture(&g->desc, MIPS);
+				} else {
+					eprintf("Not supported register profile\n");
+					return R_FALSE;
+				}
 				break;
 			}
 		} else {
@@ -428,6 +435,85 @@ static const char *r_debug_gdb_reg_profile(RDebug *dbg) {
 			"gpr	mach	.32	80	0\n"
 			"gpr	macl	.32	84	0\n"
 		);
+	case R_SYS_ARCH_MIPS:
+		return strdup (
+			"=pc    pc\n"
+			"=sp    sp\n"
+			"=bp    gp\n"
+			"gpr	zero	.32	0	0\n"
+			"gpr	at	.32	4	0\n"
+			"gpr	v0	.32	8	0\n"
+			"gpr	v1	.32	12	0\n"
+			"gpr	a0	.32	16	0\n"
+			"gpr	a1	.32	20	0\n"
+			"gpr	a2	.32	24	0\n"
+			"gpr	a3	.32	28	0\n"
+			"gpr	t0	.32	32	0\n"
+			"gpr	t1	.32	36	0\n"
+			"gpr	t2	.32	40	0\n"
+			"gpr	t3	.32	44	0\n"
+			"gpr	t4	.32	48	0\n"
+			"gpr	t5	.32	52	0\n"
+			"gpr	t6	.32	56	0\n"
+			"gpr	t7	.32	60	0\n"
+			"gpr	s0	.32	64	0\n"
+			"gpr	s1	.32	68	0\n"
+			"gpr	s2	.32	72	0\n"
+			"gpr	s3	.32	76	0\n"
+			"gpr	s4	.32	80	0\n"
+			"gpr	s5	.32	84	0\n"
+			"gpr	s6	.32	88	0\n"
+			"gpr	s7	.32	92	0\n"
+			"gpr	t8	.32	96	0\n"
+			"gpr	t9	.32	100	0\n"
+			"gpr	k0	.32	104	0\n"
+			"gpr	k1	.32	108	0\n"
+			"gpr	gp	.32	112	0\n"
+			"gpr	sp	.32	116	0\n"
+			"gpr	s8	.32	120	0\n"
+			"gpr	ra	.32	124	0\n"
+			"gpr	sr	.32	128	0\n"
+			"gpr	lo	.32	132	0\n"
+			"gpr	hi	.32	134	0\n"
+			"gpr	bad	.32	140	0\n"
+			"gpr	cause	.32	144	0\n"
+			"gpr	pc	.32	148	0\n"
+			"gpr	f0	.32	152	0\n"
+			"gpr	f1	.32	156	0\n"
+			"gpr	f2	.32	160	0\n"
+			"gpr	f3	.32	164	0\n"
+			"gpr	f4	.32	168	0\n"
+			"gpr	f5	.32	172	0\n"
+			"gpr	f6	.32	176	0\n"
+			"gpr	f7	.32	180	0\n"
+			"gpr	f8	.32	184	0\n"
+			"gpr	f9	.32	188	0\n"
+			"gpr	f10	.32	192	0\n"
+			"gpr	f11	.32	196	0\n"
+			"gpr	f12	.32	200	0\n"
+			"gpr	f13	.32	204	0\n"
+			"gpr	f14	.32	208	0\n"
+			"gpr	f15	.32	212	0\n"
+			"gpr	f16	.32	216	0\n"
+			"gpr	f17	.32	220	0\n"
+			"gpr	f18	.32	224	0\n"
+			"gpr	f19	.32	228	0\n"
+			"gpr	f20	.32	232	0\n"
+			"gpr	f21	.32	236	0\n"
+			"gpr	f22	.32	240	0\n"
+			"gpr	f23	.32	244	0\n"
+			"gpr	f24	.32	248	0\n"
+			"gpr	f25	.32	252	0\n"
+			"gpr	f26	.32	256	0\n"
+			"gpr	f27	.32	260	0\n"
+			"gpr	f28	.32	264	0\n"
+			"gpr	f29	.32	268	0\n"
+			"gpr	f30	.32	272	0\n"
+			"gpr	f31	.32	276	0\n"
+			"gpr	fsr	.32	280	0\n"
+			"gpr	fir	.32	284	0\n"
+			"gpr	unknw	.32	288	0\n" //Not documented what this part of the register packet is
+		);
 	}
 	return NULL;
 }
@@ -455,7 +541,7 @@ static int r_debug_gdb_breakpoint (void *user, int type, ut64 addr, int hw, int 
 struct r_debug_plugin_t r_debug_plugin_gdb = {
 	.name = "gdb",
 	/* TODO: Add support for more architectures here */
-	.arch = R_SYS_ARCH_X86 | R_SYS_ARCH_ARM | R_SYS_ARCH_SH,
+	.arch = R_SYS_ARCH_X86 | R_SYS_ARCH_ARM | R_SYS_ARCH_SH | R_SYS_ARCH_MIPS,
 	.bits = R_SYS_BITS_32 | R_SYS_BITS_64,
 	.init = NULL,
 	.step = r_debug_gdb_step,
