2ca4ef7cc194e65ed2b349301fd91a5b09f8f9eb
pancake <pancake@nopcode.org>
2014-11-06 22:30:24 +0100

    Fix #1641 -  Check $CC ASAN support

--- sys/asan.sh
+++ sys/asan.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 export CFLAGS="-fsanitize=address -lasan"
 export LDFLAGS="-lasan"
-sys/install.sh
+echo 'int main(){return 0;}' > .a.c
+[ -z "${CC}" ] && CC=gcc
+${CC} ${CFLAGS} ${LDFLAGS} -o .a.out .a.c
+RET=$?
+rm -rf .a.out .a.c
+if [ $? != 0 ]; then
+	echo "Your compiler doesn't supports ASAN."
+	exit 1
+fi
+exec sys/install.sh $@
