3d8ec3e3bd494370a8f7fc6d5975aab0a4875537
Jeffrey Crowell <crowell@bu.edu>
2014-11-14 12:44:29 -0500

    indenting...

--- libr/core/cmd_search.c
+++ libr/core/cmd_search.c
@@ -539,7 +539,7 @@ static RList* construct_rop_gadget(RCore *core, ut64 addr, ut8 *buf, int idx, co
 					end = NULL;
 			}
 
-      		rs = r_search_free (rs);
+			rs = r_search_free (rs);
 
 			switch (aop.type) { // end of the gadget
 				case R_ANAL_OP_TYPE_TRAP:
