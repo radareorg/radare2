c1441b6ef20987df3e6df851324a0de4b4095bfc
Riccardo Schirone <sirmy15@gmail.com>
2014-11-22 18:34:29 -0800

    libr/bin/format/mach0: fix baddr

--- libr/bin/p/bin_mach0.c
+++ libr/bin/p/bin_mach0.c
@@ -63,14 +63,7 @@ static ut64 baddr(RBinFile *arch) {
 	if (bin->hdr.filetype != MH_EXECUTE)
 		return 0;
 
-	switch (bin->hdr.cputype) {
-		case CPU_TYPE_ARM:
-			return 0x1000;      /* 4k __PAGEZERO */
-		case CPU_TYPE_X86_64:
-			return 0x100000000; /* 4G __PAGEZERO */
-	}
-
-	return 0;
+	return MACH0_(r_bin_mach0_get_baddr)(bin);
 }
 
 static RList* entries(RBinFile *arch) {
