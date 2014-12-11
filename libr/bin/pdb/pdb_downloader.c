4328c440adab541278cc4445d9f3d65a994dcc4b
inisider <inisider@gmail.com>
2014-12-12 00:11:13 +0200

    some refactor

--- libr/bin/pdb/pdb_downloader.c
+++ libr/bin/pdb/pdb_downloader.c
@@ -1,55 +1,75 @@
 #include "pdb_downloader.h"
 
 #include <string.h>
-#include <r_types.h>
+//#include <r_types.h>
+#include <r_util.h>
 
 ///////////////////////////////////////////////////////////////////////////////
 static int download(struct SPDBDownloader *pdb_downloader)
 {
+	int res = 1;
 	SPDBDownloaderOpt *opt = pdb_downloader->opt;
-	int res = -1;
 	char *curl_cmd = 0;
 	int curl_cmd_len = strlen("curl -A ")
 			+ strlen(opt->user_agent)
 			+ strlen(opt->symbol_server)
 			+ 3 * strlen(opt->dbg_file)
 			+ strlen(opt->guid)
-			+ 3 + 1; // 3- / - symbol_server/dbg_file/guid/archive_name
+			+ strlen(opt->path)
+			+ 4 + 1; // 3- / - symbol_server/dbg_file/guid/archive_name
 	char *extractor_cmd = 0;
 	int extractor_cmd_len = 0;
 
+	char *abspath = 0;
+	int abspath_len = strlen(opt->dbg_file) + strlen(opt->path) + 1;
+
 	char *archive_name = strdup(opt->dbg_file);
 	archive_name[strlen(archive_name) - 1] = '_';
+
+	abspath = (char *) malloc(abspath_len);
+	snprintf(abspath, abspath_len, "%s%s", opt->path, opt->dbg_file);
 
 #ifdef WIN32
 	char *cabextractor = "expand";
 	#error ADD SUPPORT FOR WINDOWS
 #else
 	char *cabextractor = "cabextract";
-	char *format = "%s %s";
+	char *format = "%s -d %s %s";
 	extractor_cmd_len  = strlen(cabextractor) + 2;
 #endif
 
 	curl_cmd = (char *) malloc(curl_cmd_len + 1);
 	curl_cmd[curl_cmd_len] = '\0';
-	sprintf(curl_cmd, "curl -A %s %s/%s/%s/%s -o %s",
+	snprintf(curl_cmd, curl_cmd_len, "curl -A %s %s/%s/%s/%s -o %s/%s",
 			opt->user_agent,
 			opt->symbol_server,
 			opt->dbg_file,
 			opt->guid,
 			archive_name,
+			opt->path,
 			archive_name);
 
 	extractor_cmd = (char *) malloc(extractor_cmd_len);
 	extractor_cmd[extractor_cmd_len] = '\0';
-	sprintf(extractor_cmd, format, cabextractor, archive_name);
+	snprintf(extractor_cmd, extractor_cmd_len, format, cabextractor, opt->path, archive_name);
 
-	res &= system(curl_cmd);
-	res &= system(extractor_cmd);
+	if (r_sys_cmd(curl_cmd) == -1) {
+		printf("curl has not been finish with sucess\n");
+		res = 0;
+	}
+
+	r_file_rm(abspath);
+
+	if (r_sys_cmd(extractor_cmd) == -1) {
+		printf("cab extrach has not been finished with sucess\n");
+		res = 0;
+	}
 
 	R_FREE(archive_name);
 	R_FREE(curl_cmd);
 	R_FREE(extractor_cmd);
+	R_FREE(abspath);
+
 	return res;
 }
 
@@ -61,6 +81,7 @@ void init_pdb_downloader(SPDBDownloaderOpt *opt, SPDBDownloader *pdb_downloader)
 	pdb_downloader->opt->guid = strdup(opt->guid);
 	pdb_downloader->opt->symbol_server = strdup(opt->symbol_server);
 	pdb_downloader->opt->user_agent = strdup(opt->user_agent);
+	pdb_downloader->opt->path = strdup(opt->path);
 
 	pdb_downloader->download = download;
 }
@@ -72,6 +93,7 @@ void deinit_pdb_downloader(SPDBDownloader *pdb_downloader)
 	R_FREE(pdb_downloader->opt->guid);
 	R_FREE(pdb_downloader->opt->symbol_server);
 	R_FREE(pdb_downloader->opt->user_agent);
+	R_FREE(pdb_downloader->opt->path);
 	R_FREE(pdb_downloader->opt);
 	pdb_downloader->download = 0;
 }
--- libr/bin/pdb/pdb_downloader.h
+++ libr/bin/pdb/pdb_downloader.h
@@ -1,11 +1,16 @@
 #ifndef PDB_DOWNLOADER_H
 #define PDB_DOWNLOADER_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct SPDBDownloaderOpt {
 	char *user_agent;
 	char *symbol_server;
 	char *dbg_file;
 	char *guid;
+	char *path;
 } SPDBDownloaderOpt;
 
 typedef struct SPDBDownloader {
@@ -14,17 +19,21 @@ typedef struct SPDBDownloader {
 	int (*download)(struct SPDBDownloader *pdb_downloader);
 } SPDBDownloader;
 
-///
-/// \brief initialization of pdb downloader by SPDBDownloaderOpt
-/// \param opt PDB options
-/// \param pdb_downloader PDB downloader that will be init
-///
-void init_pdb_downloader(SPDBDownloaderOpt *opt, SPDBDownloader *pdb_downloader);
+/////
+///// \brief initialization of pdb downloader by SPDBDownloaderOpt
+///// \param opt PDB options
+///// \param pdb_downloader PDB downloader that will be init
+/////
+//void init_pdb_downloader(SPDBDownloaderOpt *opt, SPDBDownloader *pdb_downloader);
 
-///
-/// \brief deinitialization of PDB downloader
-/// \param pdb_downloader PDB downloader that will be deinit
-///
-void deinit_pdb_downloader(SPDBDownloader *pdb_downloader);
+/////
+///// \brief deinitialization of PDB downloader
+///// \param pdb_downloader PDB downloader that will be deinit
+/////
+//void deinit_pdb_downloader(SPDBDownloader *pdb_downloader);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
