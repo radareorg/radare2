64e9254b82953ccf25a0480087acdc33b49da0df
pancake <pancake@nopcode.org>
2014-12-18 01:54:04 +0100

    Fix #1840 - Fix formating for type Java CP values

--- shlr/java/class.c
+++ shlr/java/class.c
@@ -7298,10 +7298,11 @@ R_API RList * r_bin_java_find_cp_const_by_val_long (RBinJavaObj *bin_obj, const 
 	ut32 *v = NULL;
 	RListIter *iter;
 	RBinJavaCPTypeObj *cp_obj;
-	eprintf ("Looking for %d\n", R_BIN_JAVA_UINT (bytes, 0));
+	eprintf ("Looking for 0x%08x\n", R_BIN_JAVA_UINT (bytes, 0));
 	r_list_foreach (bin_obj->cp_list, iter, cp_obj) {
 		if (cp_obj->tag == R_BIN_JAVA_CP_LONG) {
-			if ( len == 8 && r_bin_java_raw_to_long (cp_obj->info.cp_long.bytes.raw, 0) == r_bin_java_raw_to_long  (bytes, 0) ) {
+			if (len == 8 && r_bin_java_raw_to_long (cp_obj->info.cp_long.bytes.raw, 0) == r_bin_java_raw_to_long  (bytes, 0) ) {
+				// TODO: we can safely store a ut32 inside the list without having to allocate it
 				v = malloc (sizeof (ut32));
 				*v = cp_obj->idx;
 				r_list_append (res, v);
@@ -8275,7 +8276,7 @@ R_API RList * r_bin_java_find_cp_const_by_val_int (RBinJavaObj *bin_obj, const u
 	ut32 *v = NULL;
 	RListIter *iter;
 	RBinJavaCPTypeObj *cp_obj;
-	eprintf ("Looking for %d\n", R_BIN_JAVA_UINT (bytes, 0));
+	eprintf ("Looking for 0x%08x\n", (ut32)R_BIN_JAVA_UINT (bytes, 0));
 	r_list_foreach (bin_obj->cp_list, iter, cp_obj) {
 		if (cp_obj->tag == R_BIN_JAVA_CP_INTEGER) {
 			if ( len == 4 && R_BIN_JAVA_UINT (bytes, 0) == R_BIN_JAVA_UINT (cp_obj->info.cp_integer.bytes.raw, 0) ) {
