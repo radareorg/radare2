11220f0c17bc14a788aa7e3b4892bc3ec12a5ff9
pancake <pancake@nopcode.org>
2014-12-22 02:27:05 +0100

    Add missing header defintions for r_io_wundo_apply|_all

--- libr/include/r_io.h
+++ libr/include/r_io.h
@@ -411,6 +411,8 @@ R_API void r_io_sundo_reset(RIO *io);
 R_API void r_io_sundo_list(RIO *io);
 /* write undo */
 R_API void r_io_wundo_new(RIO *io, ut64 off, const ut8 *data, int len);
+R_API void r_io_wundo_apply_all(RIO *io, int set);
+R_API int r_io_wundo_apply(RIO *io, struct r_io_undo_w_t *u, int set);
 R_API void r_io_wundo_clear(RIO *io);
 R_API int r_io_wundo_size(RIO *io);
 R_API void r_io_wundo_list(RIO *io);
