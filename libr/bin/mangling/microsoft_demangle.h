9ac6d540b542a97a1a51db03ae5c231a1fbcd4e0
inisider <inisider@gmail.com>
2015-01-17 22:18:13 +0200

    add some comments

--- libr/bin/mangling/microsoft_demangle.h
+++ libr/bin/mangling/microsoft_demangle.h
@@ -4,7 +4,8 @@
 #include "demangler_types.h"
 
 ///////////////////////////////////////////////////////////////////////////////
-/// \brief Do demangle for microsoft mangling scheme
+/// \brief Do demangle for microsoft mangling scheme. Demangled name need to be
+///			free by user
 /// \param demangler 'this' object of demangler
 /// \param demangled_name Demangled name of symbol of demangler object
 /// \return Returns OK if initialization has been finish with success, else one
