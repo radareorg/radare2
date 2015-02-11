radareorg/radare2

commit a5f53eae05487894e4e5747882dabb41c8afa74e

Initial implementation of asm.features on x86.cs and arm.cs

libr/asm/p/asm_arm_cs.c
@@ -4,11 +4,13 @@
 #include <r_lib.h>
 #include <capstone.h>
 
+static int check_features(RAsm *a, cs_insn *insn);
+static csh cd;
+
 static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 	cs_insn* insn = NULL;
 	cs_mode mode = 0;
 	int ret, n = 0;
-	csh cd;
 	mode = (a->bits==16)? CS_MODE_THUMB: CS_MODE_ARM;
 	if (a->big_endian)
 		mode |= CS_MODE_BIG_ENDIAN;
@@ -31,23 +33,36 @@ static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 	if (a->syntax == R_ASM_SYNTAX_REGNUM) {
 		cs_option (cd, CS_OPT_SYNTAX, CS_OPT_SYNTAX_NOREGNAME);
 	} else cs_option (cd, CS_OPT_SYNTAX, CS_OPT_SYNTAX_DEFAULT);
-	cs_option (cd, CS_OPT_DETAIL, CS_OPT_OFF);
+	if (a->features && *a->features) {
+		cs_option (cd, CS_OPT_DETAIL, CS_OPT_ON);
+	} else {
+		cs_option (cd, CS_OPT_DETAIL, CS_OPT_OFF);
+	}
 	n = cs_disasm (cd, buf, R_MIN (4, len),
 		a->pc, 1, &insn);
 	if (n<1) {
 		ret = -1;
 		goto beach;
 	}
+	op->size = 0;
 	if (insn->size<1) {
 		ret = -1;
 		goto beach;
 	}
-	op->size = insn->size;
-	snprintf (op->buf_asm, R_ASM_BUFSIZE, "%s%s%s",
-		insn->mnemonic,
-		insn->op_str[0]?" ":"",
-		insn->op_str);
-	r_str_rmch (op->buf_asm, '#');
+	if (a->features && *a->features) {
+		if (!check_features (a, insn)) {
+			op->size = insn->size;
+			strcpy (op->buf_asm, "illegal");
+		}
+	}
+	if (!op->size) {
+		op->size = insn->size;
+		snprintf (op->buf_asm, R_ASM_BUFSIZE, "%s%s%s",
+			insn->mnemonic,
+			insn->op_str[0]?" ":"",
+			insn->op_str);
+		r_str_rmch (op->buf_asm, '#');
+	}
 	cs_free (insn, n);
 	beach:
 	cs_close (&cd);
@@ -66,8 +81,49 @@ RAsmPlugin r_asm_plugin_arm_cs = {
 	.init = NULL,
 	.fini = NULL,
 	.disassemble = &disassemble,
-	.assemble = NULL
+	.assemble = NULL,
+	.features = 
+			// arm32 and arm64
+			"crypto,databarrier,divide,fparmv8,multpro,neon,t2extractpack,"
+			"thumb2dsp,trustzone,v4t,v5t,v5te,v6,v6t2,v7,v8,vfp2,vfp3,vfp4,"
+			"arm,mclass,notmclass,thumb,thumb1only,thumb2,prev8,fpvmlx,"
+			"mulops,crc,dpvfp,v6m"
 };
+
+extern const char *ARM_group_name(csh handle, unsigned int id);
+extern const char *ARM64_group_name(csh handle, unsigned int id);
+
+static int check_features(RAsm *a, cs_insn *insn) {
+	const char *name;
+	int i;
+	if (!insn || !insn->detail)
+		return 1;
+	for (i=0; i< insn->detail->groups_count; i++) {
+		int id = insn->detail->groups[i];
+		if (id == ARM_GRP_ARM)
+			continue;
+		if (id == ARM_GRP_THUMB)
+			continue;
+		if (id == ARM_GRP_THUMB1ONLY)
+			continue;
+		if (id == ARM_GRP_THUMB2)
+			continue;
+		if (id<128) continue;
+		if (a->bits == 64) {
+			// AARCH64
+			name = ARM64_group_name (cd, id);
+		} else {
+			// ARM
+			name = ARM_group_name (cd, id);
+		}
+		if (!name) return 1;
+		if (!strstr (a->features, name)) {
+			//eprintf ("CANNOT FIND %s\n", name);
+			return 0;
+		}
+	}
+	return 1;
+}
 
 #ifndef CORELIB
 struct r_lib_struct_t radare_plugin = {

libr/core/config.c
@@ -249,6 +249,30 @@ static int cb_asmbits(void *user, void *data) {
 	return ret;
 }
 
+static int cb_asmfeatures(void *user, void *data) {
+	RCore *core = (RCore *) user;
+	RConfigNode *node = (RConfigNode *) data;
+	if (*node->value=='?') {
+		int i;
+		if (core && core->assembler && core->assembler->cur) {
+			if (core->assembler->cur->features) {
+				char *feat = strdup (core->assembler->cur->features);
+				r_str_replace_char (feat, ',','\n');
+				r_cons_printf ("%s\n", feat);
+				free (feat);
+			}
+		}
+		return 0;
+	}
+	free (core->assembler->features);
+	if (node->value && node->value[0]) {
+		core->assembler->features = strdup (node->value);
+	} else {
+		core->assembler->features = NULL;
+	}
+	return 1;
+}
+
 static int cb_asmcpu(void *user, void *data) {
 	RCore *core = (RCore *) user;
 	RConfigNode *node = (RConfigNode *) data;
@@ -958,6 +982,7 @@ R_API int r_core_config_init(RCore *core) {
 	SETPREF("asm.varxs", "false", "Show accesses of local variables");
 	SETPREF("asm.varsub", "true", "Substitute variables in disassembly");
 	SETCB("asm.arch", R_SYS_ARCH, &cb_asmarch, "Set the arch to be usedd by asm");
+	SETCB("asm.features", "", &cb_asmfeatures, "Specify supported features by the target CPU (=? for help)");
 	SETCB("asm.cpu", R_SYS_ARCH, &cb_asmcpu, "Set the kind of asm.arch cpu");
 	SETCB("asm.parser", "x86.pseudo", &cb_asmparser, "Set the asm parser to use");
 	SETCB("asm.segoff", "false", &cb_segoff, "Show segmented address in prompt (x86-16)");

libr/asm/asm.c
@@ -1,4 +1,4 @@
-/* radare - LGPL - Copyright 2009-2014 - pancake, nibble */
+/* radare - LGPL - Copyright 2009-2015 - pancake, nibble */
 
 #include <stdio.h>
 #include <r_types.h>
@@ -129,6 +129,7 @@ R_API RAsm *r_asm_new() {
 	a->num = NULL;
 	a->user = NULL;
 	a->cur = NULL;
+	a->features = NULL;
 	a->binb.bin = NULL;
 	a->bits = 32;
 	a->cpu = NULL;
@@ -314,6 +315,9 @@ R_API int r_asm_disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 	sprintf (op->buf_asm,".byte 0x%02x %d", buf[0], len);
 	if (a->cur && a->cur->disassemble)
 		ret = a->cur->disassemble (a, op, buf, len);
+	// avoid undefined behaviour
+	if (ret<0)
+		ret = 0;
 	oplen = r_asm_op_get_size (op);
 	oplen = op->size;
 	if (oplen>len) oplen = len;

libr/asm/p/asm_x86_cs.c
@@ -24,6 +24,8 @@ static int the_end(void *p) {
 	return R_TRUE;
 }
 
+static int check_features(RAsm *a, cs_insn *insn);
+
 static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 	static int omode = 0;
 	int mode, ret;
@@ -41,6 +43,10 @@ static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 	if (cd == 0) {
 		ret = cs_open (CS_ARCH_X86, mode, &cd);
 		if (ret) return 0;
+	}
+	if (a->features && *a->features) {
+		cs_option (cd, CS_OPT_DETAIL, CS_OPT_ON);
+	} else {
 		cs_option (cd, CS_OPT_DETAIL, CS_OPT_OFF);
 	}
 	if (a->syntax == R_ASM_SYNTAX_ATT)
@@ -58,7 +64,14 @@ static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 #else
 	n = cs_disasm (cd, (const ut8*)buf, len, off, 1, &insn);
 #endif
-	if (n>0 && insn->size>0) {
+	op->size = 0;
+	if (a->features && *a->features) {
+		if (!check_features (a, insn)) {
+			op->size = insn->size;
+			strcpy (op->buf_asm, "illegal");
+		}
+	}
+	if (op->size==0 && n>0 && insn->size>0) {
 		char *ptrstr;
 		op->size = insn->size;
 		snprintf (op->buf_asm, R_ASM_BUFSIZE, "%s%s%s",
@@ -87,8 +100,35 @@ RAsmPlugin r_asm_plugin_x86_cs = {
 	.init = NULL,
 	.fini = the_end,
 	.disassemble = &disassemble,
-	.assemble = NULL
+	.assemble = NULL,
+	.features = "vm,3dnow,aes,adx,avx,avx2,avx512,bmi,bmi2,cmov,"
+		"f16c,fma,fma4,fsgsbase,hle,mmx,rtm,sha,sse1,sse2,"
+		"sse3,sse41,sse42,sse4a,ssse3,pclmul,xop"
 };
+
+
+extern const char *X86_group_name(csh handle, unsigned int id);
+
+static int check_features(RAsm *a, cs_insn *insn) {
+	const char *name;
+	int i;
+	if (!insn || !insn->detail)
+		return 1;
+	for (i=0; i< insn->detail->groups_count; i++) {
+		int id = insn->detail->groups[i];
+		if (id<128) continue;
+		if (id == X86_GRP_MODE32)
+			continue;
+		if (id == X86_GRP_MODE64)
+			continue;
+		name = X86_group_name (cd, id);
+		if (!name) return 1;
+		if (name && !strstr (a->features, name)) {
+			return 0;
+		}
+	}
+	return 1;
+}
 
 #ifndef CORELIB
 struct r_lib_struct_t radare_plugin = {

libr/core/disasm.c
@@ -2201,7 +2201,7 @@ toro:
 		if (ds->tries>0) {
 			//ds->addr += idx;
 			if (r_core_read_at (core, ds->addr, buf, len) ) {
-				idx = 0;
+			//	idx = 0;
 				goto toro;
 			}
 		}

libr/include/r_asm.h
@@ -1,4 +1,4 @@
-/* radare - LGPL - Copyright 2009-2014 - nibble, pancake */
+/* radare - LGPL - Copyright 2009-2015 - nibble, pancake */
 
 #ifndef R2_ASM_H
 #define R2_ASM_H
@@ -17,7 +17,7 @@ R_LIB_VERSION_HEADER(r_asm);
 
 #define R_ASM_OPCODES_PATH R2_LIBDIR "/radare2/" R2_VERSION "/opcodes"
 // XXX too big!
-#define R_ASM_BUFSIZE 512
+#define R_ASM_BUFSIZE 256
 
 /* backward compatibility */
 #define R_ASM_ARCH_NONE R_SYS_ARCH_NONE
@@ -98,6 +98,7 @@ typedef struct r_asm_t {
 	Sdb *pair;
 	RSyscall *syscall;
 	RNum *num;
+	char *features;
 } RAsm;
 
 typedef int (*RAsmModifyCallback)(RAsm *a, ut8 *buf, int field, ut64 val);
@@ -116,6 +117,7 @@ typedef struct r_asm_plugin_t {
 	int (*assemble)(RAsm *a, RAsmOp *op, const char *buf);
 	RAsmModifyCallback modify;
 	int (*set_subarch)(RAsm *a, const char *buf);
+	const char *features;
 } RAsmPlugin;
 
 #ifdef R_API

commit 6b215beec0358ed34ff29f0635dfec9f2943e1ea

fix build broken by a5f53eae05487894e4e5747882dabb41c8afa74e
remove unused var in config.c

libr/asm/p/asm_arm_cs.c
@@ -91,7 +91,7 @@ RAsmPlugin r_asm_plugin_arm_cs = {
 };
 
 extern const char *ARM_group_name(csh handle, unsigned int id);
-extern const char *ARM64_group_name(csh handle, unsigned int id);
+extern const char *AArch64_group_name(csh handle, unsigned int id);
 
 static int check_features(RAsm *a, cs_insn *insn) {
 	const char *name;
@@ -111,7 +111,7 @@ static int check_features(RAsm *a, cs_insn *insn) {
 		if (id<128) continue;
 		if (a->bits == 64) {
 			// AARCH64
-			name = ARM64_group_name (cd, id);
+			name = AArch64_group_name (cd, id);
 		} else {
 			// ARM
 			name = ARM_group_name (cd, id);

libr/core/config.c
@@ -253,7 +253,6 @@ static int cb_asmfeatures(void *user, void *data) {
 	RCore *core = (RCore *) user;
 	RConfigNode *node = (RConfigNode *) data;
 	if (*node->value=='?') {
-		int i;
 		if (core && core->assembler && core->assembler->cur) {
 			if (core->assembler->cur->features) {
 				char *feat = strdup (core->assembler->cur->features);