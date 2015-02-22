43f240520ed8f94b5dd40854b17aca8425612aad
Aaron Puchert <aaronpuchert@alice-dsl.net>
2015-02-22 22:05:33 +0100

    Rename x86.new assembler to x86.tab
    This stands for "table lookup assembler".

---
+++ libr/asm/p/x86_tab.mk
@@ -0,0 +1,9 @@
+OBJ_X86_TAB=asm_x86_tab.o
+
+STATIC_OBJ+=${OBJ_X86_TAB}
+TARGET_X86_TAB=asm_x86_tab.${EXT_SO}
+
+ALL_TARGETS+=${TARGET_X86_TAB}
+
+${TARGET_X86_TAB}: ${OBJ_X86_TAB}
+	${CC} $(call libname,asm_x86_tab) ${LDFLAGS} ${CFLAGS} -o ${TARGET_X86_TAB} ${OBJ_X86_TAB}
