3c998abaa9c3392b7c428847eddfa7f3cf603b4c
pancake <pancake@nopcode.org>
2015-03-03 02:40:50 +0100

    Fix osx-pkg prefix

--- sys/osx-pkg.sh
+++ sys/osx-pkg.sh
@@ -8,10 +8,11 @@
 
 SRC=/tmp/r2osx
 DST=`pwd`/sys/osx-pkg/radare2.unpkg
+VERSION=0.9.9git
 
 rm -rf ${SRC}
 make mrproper
-./configure --prefix=/usr/local
+./configure --prefix=/usr
 make -j4
 # TODO: run sys/install.sh
 make install PREFIX=/usr DESTDIR=${SRC}
@@ -23,4 +24,4 @@ mkbom ${SRC} ${DST}/Bom
 
 # Repackage
 
-pkgutil --flatten ${DST} ${DST}/../radare2.pkg
+pkgutil --flatten ${DST} ${DST}/../radare2-${VERSION}.pkg
