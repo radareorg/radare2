a25e54552ab4f5df8f7eae2426aec3a54c50b70d
zonkzonk <bofh666ftw@googlemail.com>
2015-03-03 21:04:48 +0100

    Update config.c
    rm http.public

--- libr/core/config.c
+++ libr/core/config.c
@@ -1154,7 +1154,6 @@ R_API int r_core_config_init(RCore *core) {
 	r_config_desc (cfg, "http.browser", "command to open http urls");
 #endif
 	SETI("http.maxsize", 0, "Define maximum file size to upload");
-	SETPREF("http.public", "false", "Set to true to listen on 0.0.0.0");
 	SETPREF("http.bind", "localhost", "Host to listen for http connections");
 #if __WINDOWS__
 	SETPREF("http.root", "www", "Http root directory");
