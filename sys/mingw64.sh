590f9023b01b272ff2fb80deebd61d6114fb8dfc
pancake <pancake@nopcode.org>
2015-03-20 16:53:55 +0100

    Bump capstone and check toolchain in mingw64.sh

--- sys/mingw64.sh
+++ sys/mingw64.sh
@@ -8,6 +8,14 @@ cd `dirname $PWD/$0` ; cd ..
 export PATH=${PWD}/sys/_work/mingw64/bin:${PATH}
 # TODO: add support for ccache
 
+type x86_64-w64-mingw32-gcc >/dev/null 2>&1
+if [ $? = 0 ]; then
+	C=x86_64-w64-mingw32-gcc
+else
+	echo "mingw64 package required."
+	exit 1
+fi
+
 make clean
 ./configure --without-gmp --with-compiler=x86_64-w64-mingw32-gcc --with-ostype=windows --host=x86_64-unknown-windows --without-ssl
 make -s -j ${MAKE_JOBS} CC="${C} -static-libgcc" && \
