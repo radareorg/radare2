40c5128b38e15b159de3ec41dfb500078634f9d7
Álvaro Felipe Melchor <alvaro.felipe91@gmail.com>
2015-03-24 19:39:15 +0100

    fix segfault due to a double free

--- libr/asm/arch/tms320/c55x_plus/decode_funcs.c
+++ libr/asm/arch/tms320/c55x_plus/decode_funcs.c
@@ -1098,9 +1098,6 @@ st8 *get_opers(ut8 oper_byte)
 			case 5u:
 				res =  strcat_dup(reg_name, " <= #0", 1);
 			}
-			// free (reg_name); Causes segfault
-			// TODO: still can leak
-free(reg_name);
 			return res;
 		}
 		reg_name = get_reg_name_1((oper_byte & 0xF) + 128);
