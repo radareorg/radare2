78da2985e8f2e8937bc3530773d2c90801e45722
AntonDevil <anton_nix@mail.ru>
2015-03-29 20:03:59 +0300

    fix #2257 arm asm
    There was type mismatch between declaration and definition

--- libr/asm/arch/arm/asm-arm.h
+++ libr/asm/arch/arm/asm-arm.h
@@ -1,6 +1,8 @@
 #ifndef _INCLUDE_ARMASS_H_
 #define _INCLUDE_ARMASS_H_
 
-int armass_assemble(const char *str, unsigned long off, int thumb);
+#include <r_types_base.h>
+
+int armass_assemble(const char *str, ut64 off, int thumb);
 
 #endif
