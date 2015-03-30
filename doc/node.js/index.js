ddbffe4f9dd1696526acef3cdd604fde4945a83b
pancake <pancake@nopcode.org>
2015-03-31 00:59:03 +0200

    Better r2-nodejs webserver, honor http.port, works from the shell too

--- doc/node.js/index.js
+++ doc/node.js/index.js
@@ -1,7 +1,17 @@
 
 /*
 
-r2 -c '#!pipe node index.js' /bin/ls
+Author : pancake <pancake@nopcode.org>
+
+Date: 2015-03-31
+
+From inside r2
+
+	r2 -c '#!pipe node index.js' /bin/ls
+
+Or from the shell:
+
+	node .
 
 */
 
@@ -12,24 +22,30 @@ var express = require('express');
 function runWebServer(r) {
 	r.cmd ("e http.root", function(wwwroot) {
 		wwwroot = wwwroot.trim ();
-		r.cmd ("e scr.color=false", function() {});
-		r.cmd ("e scr.interactive=false", function() {});
-		r.cmd ("e scr.html=true", function(){});
-		var app = express();
-		app.all('/cmd/*', function(req,res) {
-			var cmd = unescape (req.url.substring (5));
-			console.log ("cmd:", cmd);
-			r.cmd (cmd, function (data) {
-				res.send(data);
+		r.cmd ("e http.port", function(port) {
+			port = +port.trim ();
+			r.cmd ("e scr.color=false", function() {});
+			r.cmd ("e scr.interactive=false", function() {});
+			r.cmd ("e scr.html=true", function(){});
+			var app = express();
+			app.all('/cmd/*', function(req,res) {
+				var cmd = unescape (req.url.substring (5));
+				console.log ("cmd:", cmd);
+				r.cmd (cmd, function (data) {
+					res.send(data);
+				});
 			});
+			app.use(express.static(wwwroot));
+			r.cmd ("?e http://localhost:`e http.port`/p", function (data) {
+				console.log (data.replace(' ','').trim());
+			})
+			app.listen (port);
 		});
-		app.use(express.static(wwwroot));
-
-		r.cmd ("?e it works!", function (data) {
-			console.log ("Test:", data);
-		})
-		app.listen (8080);
 	});
 }
 
-var r = r2p.rlangpipe(runWebServer);
+if (process.env.R2PIPE_IN) {
+	var r = r2p.rlangpipe(runWebServer);
+} else {
+	var r = r2p.pipe ("/bin/ls", runWebServer);
+}
