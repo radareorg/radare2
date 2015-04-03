c3c16fd2e432d6f11b9128c7c50ca49b14b07d38
Anton Kochkov <anton.kochkov@gmail.com>
2015-04-04 01:59:27 +0300

    Remove inttypes.h usage from zip

--- shlr/zip/include/zipconf.h
+++ shlr/zip/include/zipconf.h
@@ -13,34 +13,35 @@
 #define LIBZIP_VERSION_MINOR 11
 #define LIBZIP_VERSION_MICRO 0
 
-#include <inttypes.h>
+#include <r_types.h>
+#include <stdint.h>
 
-typedef int8_t zip_int8_t;
+typedef st8 zip_int8_t;
 #define ZIP_INT8_MIN INT8_MIN
 #define ZIP_INT8_MAX INT8_MAX
 
-typedef uint8_t zip_uint8_t;
+typedef ut8 zip_uint8_t;
 #define ZIP_UINT8_MAX UINT8_MAX
 
-typedef int16_t zip_int16_t;
+typedef st16 zip_int16_t;
 #define ZIP_INT16_MIN INT16_MIN
 #define ZIP_INT16_MAX INT16_MAX
 
-typedef uint16_t zip_uint16_t;
+typedef ut16 zip_uint16_t;
 #define ZIP_UINT16_MAX UINT16_MAX
 
-typedef int32_t zip_int32_t;
+typedef st32 zip_int32_t;
 #define ZIP_INT32_MIN INT32_MIN
 #define ZIP_INT32_MAX INT32_MAX
 
-typedef uint32_t zip_uint32_t;
+typedef ut32 zip_uint32_t;
 #define ZIP_UINT32_MAX UINT32_MAX
 
-typedef int64_t zip_int64_t;
+typedef st64 zip_int64_t;
 #define ZIP_INT64_MIN INT64_MIN
 #define ZIP_INT64_MAX INT64_MAX
 
-typedef uint64_t zip_uint64_t;
+typedef ut64 zip_uint64_t;
 #define ZIP_UINT64_MAX UINT64_MAX
 
 
