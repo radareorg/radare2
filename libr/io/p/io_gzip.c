6b2879c20653a8b8307823f2712e84d3eebc3651
Jeffrey Crowell <crowell@bu.edu>
2015-04-02 21:59:11 -0400

    fix broken build
    the last commit must have been 2 juicy for everyone

--- libr/io/p/io_gzip.c
+++ libr/io/p/io_gzip.c
@@ -111,7 +111,7 @@ static RIODesc *__open(RIO *io, const char *pathname, int rw, int mode) {
 		RIOGzip *mal = R_NEW0 (RIOGzip);
 		int len;
 		ut8 *data = (ut8*)r_file_slurp (pathname+7, &len);
-		mal->buf = r_inflate (data, len, &mal->size);
+		mal->buf = r_inflate (data, len, NULL, &mal->size);
 		if (mal->buf) {
 			RETURN_IO_DESC_NEW (&r_io_plugin_malloc,
 				mal->fd, pathname, rw, mode, mal);
