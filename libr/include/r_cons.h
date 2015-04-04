e1d247682232a7b45bab39f25e7b9610f8ad068c
Anton Kochkov <anton.kochkov@gmail.com>
2015-04-05 01:39:05 +0300

    Fixed r_cons.h if defined both CYGWIN and MINGW32

--- libr/include/r_cons.h
+++ libr/include/r_cons.h
@@ -16,13 +16,13 @@ extern "C" {
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#if __UNIX__ || __CYGWIN__
+#if __UNIX__ || __CYGWIN__ && !defined(MINGW32)
 #include <termios.h>
 #include <sys/ioctl.h>
 #include <sys/wait.h>
 #include <sys/socket.h>
 #endif
-#if __WINDOWS__
+#if __WINDOWS__ && !defined(__CYGWIN__)
 #include <windows.h>
 #include <wincon.h>
 #endif
