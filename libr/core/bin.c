e03cff8a92bd3f6f8fa5d2994f237cba2a799e29
pancake <pancake@nopcode.org>
2015-04-06 12:55:35 +0200

    Fix PLT-related regression after optimization

--- libr/core/bin.c
+++ libr/core/bin.c
@@ -805,6 +805,7 @@ static int bin_relocs (RCore *r, int mode, ut64 baddr, int va) {
 }
 
 #define MYDB 1
+/* this is a hacky workaround that needs proper refactoring in Rbin to use Sdb */
 #if MYDB
 static Sdb *mydb = NULL;
 static RList *osymbols = NULL;
@@ -817,13 +818,15 @@ static RBinSymbol *get_symbol(RBin *bin, RList *symbols, const char *name) {
 		osymbols = symbols;
 	}
 	if (mydb) {
-		symbol = (void*)(size_t)sdb_num_get (mydb, name, NULL);
+		res = (RBinSymbol*)(void*)(size_t)
+			sdb_num_get (mydb, name, NULL);
 	} else {
 		mydb = sdb_new0 ();
 		r_list_foreach (symbols, iter, symbol) {
-			sdb_num_set (mydb, name, (ut64)(size_t)symbol, 0);
-			if (!res && !strcmp (symbol->name, name))
+			sdb_num_set (mydb, symbol->name, (ut64)(size_t)symbol, 0);
+			if (!res && !strcmp (symbol->name, name)) {
 				res = symbol;
+			}
 		}
 	}
 	return res;
@@ -833,7 +836,7 @@ static RBinSymbol *get_symbol(RBin *bin, RList *symbols, const char *name) {
 	RBinSymbol *symbol;
 	RListIter *iter;
 	r_list_foreach (symbols, iter, symbol) {
-		if (!res && !strcmp (symbol->name, name))
+		if (!strcmp (symbol->name, name))
 			return symbol;
 	}
 	return NULL;
@@ -850,6 +853,7 @@ static ut64 impaddr(RBin *bin, int va, ut64 baddr, const char *name) {
 	if (!(symbols = r_bin_get_symbols (bin))) {
 		return R_FALSE;
 	}
+	// TODO: avoid using snprintf here
 	snprintf (impname, sizeof (impname), "imp.%s", name);
 	s = get_symbol (bin, symbols, impname);
 	if (s) {
@@ -951,6 +955,7 @@ static int bin_imports (RCore *r, int mode, ut64 baddr, int va, const char *name
 		if (!mode) r_cons_printf ("\n%i imports\n", i);
 	}
 #if MYDB
+	osymbols = NULL;
 	sdb_free (mydb);
 	mydb = NULL;
 #endif
