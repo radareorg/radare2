d1351cf836df3e2e63043a6dc728e880316f00eb
ninjahacker <wardjm@gmail.com>
2015-04-06 21:48:21 -0400

    Added initial zimg support

---
+++ libr/bin/format/zimg/zimg.c
@@ -0,0 +1,30 @@
+/* radare - LGPL - Copyright 2009-2015 - ninjahacker */
+
+#include <r_types.h>
+#include <r_util.h>
+#include "zimg.h"
+
+
+struct r_bin_zimg_obj_t* r_bin_zimg_new_buf(RBuffer *buf) {
+	struct r_bin_zimg_obj_t *bin = R_NEW0 (struct r_bin_zimg_obj_t);
+	if (!bin) {
+		goto fail;
+	}
+	bin->size = buf->length;
+	bin->b = r_buf_new ();
+	if (!r_buf_set_bytes (bin->b, buf->buf, bin->size)){
+		goto fail;
+	}
+
+	bin->header = (*(struct zimg_header_t*)bin->b->buf);
+
+	return bin;
+
+fail:
+	if (bin) {
+		r_buf_free (bin->b);
+		free (bin);
+	}
+	return NULL;
+}
+
---
+++ libr/bin/format/zimg/zimg.h
@@ -0,0 +1,38 @@
+#include <r_types.h>
+#include <r_util.h>
+#include <r_lib.h>
+#include <r_bin.h>
+
+#define R_BIN_ZIMG_MAXSTR 256
+
+struct zimg_header_t {
+	ut8 magic[8];
+	ut32 filler[6];
+	ut8 arm_magic[4];
+	ut32 kernel_start;
+	ut32 kernel_end;
+};
+
+typedef struct r_bin_zimg_obj_t {
+	int size;
+	const char *file;
+	struct r_buf_t *b;
+	struct zimg_header_t header;
+	ut32 *strings;
+	RList *methods_list;
+	RList *imports_list;
+	ut64 code_from;
+	ut64 code_to;
+	Sdb *kv;
+} RBinZimgObj;
+
+struct r_bin_zimg_str_t {
+	char str[R_BIN_ZIMG_MAXSTR];
+	ut64 offset;
+	ut64 ordinal;
+	int size;
+	int last;
+};
+
+struct r_bin_zimg_obj_t *r_bin_zimg_new_buf(struct r_buf_t *buf);
+struct r_bin_zimg_str_t *r_bin_zimg_get_strings (struct r_bin_zimg_obj_t* bin);
