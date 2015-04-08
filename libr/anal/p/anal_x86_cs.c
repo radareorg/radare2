67eff928f6cf7c7eeb648db4262892eeb22eeb79
condret <condret@runas-racer.com>
2015-04-08 12:18:06 +0200

    add read-syscall-wrapper for esil-x86_32_linux

--- libr/anal/p/anal_x86_cs.c
+++ libr/anal/p/anal_x86_cs.c
@@ -1045,12 +1045,24 @@ static int x86_int_0x80 (RAnalEsil *esil, int interrupt)
 	r_anal_esil_reg_read (esil, "ecx", &ecx);
 	r_anal_esil_reg_read (esil, "edx", &edx);
 	syscall = (int) eax;
-	if (syscall == 4) {		// write
-		char *src = malloc ((size_t)edx);
-		r_anal_esil_mem_read (esil, ecx, (ut8 *)src, (int)edx);
-		write ((ut32)ebx, src, (size_t)edx);
-		free (src);
-		return R_TRUE;
+	switch (syscall) {
+		case 3:
+			{
+				char *dst = calloc (1, (size_t)edx);
+				read ((ut32)ebx, dst, (size_t)edx);
+				r_anal_esil_mem_write (esil, ecx, (ut8 *)dst, (int)edx);
+				free (dst);
+				return R_TRUE;
+			}
+		case 4:
+			{
+				char *src = malloc ((size_t)edx);
+				r_anal_esil_mem_read (esil, ecx, (ut8 *)src, (int)edx);
+				write ((ut32)ebx, src, (size_t)edx);
+				free (src);
+				return R_TRUE;
+			}
+		
 	}
 	eprintf ("syscall %d not implemented yet\n", syscall);
 	return R_FALSE;
