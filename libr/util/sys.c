75caca7ebe1bfe6b0100d259cf96bb5068ef2940
pancake <pancake@nopcode.org>
2015-04-11 20:54:31 +0200

    Fix w32 build

--- libr/util/sys.c
+++ libr/util/sys.c
@@ -67,7 +67,11 @@ static const struct {const char* name; ut64 bit;} arch_bit_array[] = {
 
 R_API int r_sys_fork() {
 #if HAVE_FORK
+#if __WINDOWS__
+	return -1;
+#else
 	return fork ();
+#endif
 #else
 	return -1;
 #endif
