a53c7249d963c33d3e57d6034f68a705b61b50a2
pancake <pancake@nopcode.org>
2015-04-13 03:27:24 +0200

    Update build support for iOS 8.3

--- sys/ios-sdk.sh
+++ sys/ios-sdk.sh
@@ -22,7 +22,7 @@ export PATH=`pwd`/sys:${PATH}
 export CC=`pwd`/sys/ios-sdk-gcc
 # set only for arm64, otherwise it is armv7
 # select ios sdk version
-export IOSVER=8.1
+export IOSVER=8.3
 export IOSINC=`pwd`/sys/ios-include
 export CFLAGS=-O2
 
