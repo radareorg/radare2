f35d16055160b1c561598b9501055c5769545386
Skia <lordbanana25@mailoo.org>
2015-04-23 02:12:45 +0200

    Fix #2411

--- libr/util/p_format.c
+++ libr/util/p_format.c
@@ -693,18 +693,20 @@ static void r_print_format_nulltermstring(const RPrint* p, const int len, int en
 	} else if (MUSTSEE) {
 		int j = i;
 		p->printf ("0x%08"PFMT64x" = ", seeki);
-		for (; j<len && ((size || size==-1) && buf[j]) ; j++) {
+		for (; j<len && ((size>0 || size==-1) && buf[j]) ; j++) {
 			if (IS_PRINTABLE (buf[j]))
 				p->printf ("%c", buf[j]);
 			else p->printf (".");
+			size--;
 		}
 	} else if (MUSTSEEJSON) {
 		int j = i;
 		p->printf ("%d,\"string\":\"", seeki);
-		for (; j<len && ((size || size==-1) && buf[j]) ; j++) {
+		for (; j<len && ((size>0 || size==-1) && buf[j]) ; j++) {
 			if (IS_PRINTABLE (buf[j]))
 				p->printf ("%c", buf[j]);
 			else p->printf (".");
+			size--;
 		}
 		p->printf ("\"}");
 	}
@@ -729,10 +731,11 @@ static void r_print_format_nulltermwidestring(const RPrint* p, const int len, in
 	} else if (MUSTSEE) {
 		int j = i;
 		p->printf ("0x%08"PFMT64x" = ", seeki);
-		for (; j<len && ((size || size==-1) && buf[j]) ; j+=2) {
+		for (; j<len && ((size>0 || size==-1) && buf[j]) ; j+=2) {
 			if (IS_PRINTABLE (buf[j]))
 				p->printf ("%c", buf[j]);
 			else p->printf (".");
+			size--;
 		}
 	}
 }
