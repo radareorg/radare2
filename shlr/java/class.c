bf1e8b88cc3fd82bbf348aa466f783b56f251a1a
pancake <pancake@nopcode.org>
2015-05-19 10:56:43 +0200

    Fix huge analysis times for Java

--- shlr/java/class.c
+++ shlr/java/class.c
@@ -974,66 +974,68 @@ R_API int extract_type_value (const char *arg_str, char **output) {
 	char *str = NULL;
 	if (output == NULL) {
 		return 0;
-	}else if (output && *output && *output != NULL) {
+	} else if (output && *output && *output != NULL) {
 		free(*output);
 		*output = NULL;
 	}
 	while (arg_str && *arg_str && !found_one) {
 		// handle the end of an object
 		switch (*arg_str) {
-			case 'V':
-				len = 1;
-				str = get_type_value_str ("void", array_cnt);
-				break;
-			case 'J':
-				len = 1;
-				str = get_type_value_str ("long", array_cnt);
-				array_cnt = 0;
-				break;
-			case 'I':
-				len = 1;
-				str = get_type_value_str ("int", array_cnt);
-				array_cnt = 0;
-				break;
-			case 'D':
-				len = 1;
-				str = get_type_value_str ("double", array_cnt);
-				array_cnt = 0;
-				break;
-			case 'F':
-				len = 1;
-				str = get_type_value_str ("float", array_cnt);
-				array_cnt = 0;
-				break;
-			case 'B':
-				len = 1;
-				str = get_type_value_str ("byte", array_cnt);
-				array_cnt = 0;
-				break;
-			case 'C':
-				len = 1;
-				str = get_type_value_str ("char", array_cnt);
-				array_cnt = 0;
-				break;
-			case 'Z':
-				len = 1;
-				str = get_type_value_str ("boolean", array_cnt);
-				array_cnt = 0;
-				break;
-			case 'S':
-				len = 1;
-				str = get_type_value_str ("short", array_cnt);
-				array_cnt = 0;
-				break;
-			case '[': len = 1; array_cnt ++; break;
-			case 'L':
-				len = r_bin_java_extract_reference_name (arg_str, &str, array_cnt);
-				array_cnt = 0;
-				break;
-			case '(': len = 1; str = strdup ("("); break;
-			case ')': len = 1; str = strdup (")"); break;
-			default : break;
+		case 'V':
+			len = 1;
+			str = get_type_value_str ("void", array_cnt);
+			break;
+		case 'J':
+			len = 1;
+			str = get_type_value_str ("long", array_cnt);
+			array_cnt = 0;
+			break;
+		case 'I':
+			len = 1;
+			str = get_type_value_str ("int", array_cnt);
+			array_cnt = 0;
+			break;
+		case 'D':
+			len = 1;
+			str = get_type_value_str ("double", array_cnt);
+			array_cnt = 0;
+			break;
+		case 'F':
+			len = 1;
+			str = get_type_value_str ("float", array_cnt);
+			array_cnt = 0;
+			break;
+		case 'B':
+			len = 1;
+			str = get_type_value_str ("byte", array_cnt);
+			array_cnt = 0;
+			break;
+		case 'C':
+			len = 1;
+			str = get_type_value_str ("char", array_cnt);
+			array_cnt = 0;
+			break;
+		case 'Z':
+			len = 1;
+			str = get_type_value_str ("boolean", array_cnt);
+			array_cnt = 0;
+			break;
+		case 'S':
+			len = 1;
+			str = get_type_value_str ("short", array_cnt);
+			array_cnt = 0;
+			break;
+		case '[': len = 1; array_cnt ++; break;
+		case 'L':
+			len = r_bin_java_extract_reference_name (arg_str, &str, array_cnt);
+			array_cnt = 0;
+			break;
+		case '(': len = 1; str = strdup ("("); break;
+		case ')': len = 1; str = strdup (")"); break;
+		default : break;
 		}
+		if (len<1)
+			break;
 		consumed += len;
 		arg_str += len;
 		if (str) {
