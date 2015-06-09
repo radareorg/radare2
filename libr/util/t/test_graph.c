493e27688d4d99523b1e7a5b65144f0e42cf0567
Riccardo Schirone <sirmy15@gmail.com>
2015-06-09 16:45:39 +0200

    libr/util: reimplement graph api
    libr/util/t/graph: add test

---
+++ libr/util/t/test_graph.c
@@ -0,0 +1,86 @@
+#include <r_util.h>
+
+void check_list(RList *act, RList *exp, char *descr) {
+	RListIter *ita = r_list_iterator(act);
+	RListIter *ite = r_list_iterator(exp);
+
+	while (r_list_iter_next(ita) && r_list_iter_next(ite)) {
+		int a = (int)r_list_iter_get(ita);
+		int e = (int)r_list_iter_get(ite);
+
+		if (a != e) {
+			printf("[-][%s] test failed (actual: %d; expected: %d)\n", descr, a, e);
+		}
+	}
+
+	if (!ita && !ite) {
+		printf("[+][%s] test passed (lists have same elements)\n", descr);
+	} else {
+		printf("[-][%s] test failed (one list shorter or different)\n", descr);
+	}
+}
+
+void check (int n, int exp, char *descr) {
+	if (n == exp) {
+		printf("[+][%s] test passed (actual: %d; expected: %d)\n", descr, n, exp);
+	} else {
+		printf("[-][%s] test failed (actual: %d; expected: %d)\n", descr, n, exp);
+	}
+}
+
+void check_ptr (void *act, void *exp, char *descr) {
+	check((int)act, (int)exp, descr);
+}
+
+int main(int argc, char **argv) {
+	RGraph *g = r_graph_new();
+
+	check(g->n_nodes, 0, "n_nodes.start");
+	r_graph_add_node(g, (void *)1);
+	check(g->n_nodes, 1, "n_nodes.insert");
+	r_graph_reset(g);
+	check(g->n_nodes, 0, "n_nodes.reset");
+
+	RGraphNode *gn = r_graph_add_node(g, (void *)1);
+	check_ptr(r_graph_get_node(g, gn->idx), gn, "get_node.1");
+	RGraphNode *gn2 = r_graph_add_node(g, (void *)2);
+	check_ptr(r_graph_get_node(g, gn2->idx), gn2, "get_node.2");
+	r_graph_add_edge(g, gn, gn2);
+	check(r_graph_adjacent(g, gn, gn2), R_TRUE, "is_adjacent.1");
+	RList *exp_gn_neigh = r_list_new();
+	r_list_append(exp_gn_neigh, gn2);
+	check_list(r_graph_get_neighbours(g, gn), exp_gn_neigh, "get_neighbours.1");
+
+	RGraphNode *gn3 = r_graph_add_node(g, (void *)3);
+	r_graph_add_edge(g, gn, gn3);
+	r_list_append(exp_gn_neigh, gn3);
+	check_list(r_graph_get_neighbours(g, gn), exp_gn_neigh, "get_neighbours.2");
+	r_list_free(exp_gn_neigh);
+
+	RGraphNode *gn4 = r_graph_add_node(g, (void *)4);
+	RGraphNode *gn5 = r_graph_add_node(g, (void *)5);
+	RGraphNode *gn6 = r_graph_add_node(g, (void *)6);
+	RGraphNode *gn7 = r_graph_add_node(g, (void *)7);
+	RGraphNode *gn8 = r_graph_add_node(g, (void *)8);
+	RGraphNode *gn9 = r_graph_add_node(g, (void *)9);
+	RGraphNode *gn10 = r_graph_add_node(g, (void *)10);
+	RList *exp_nodes = r_list_new();
+	r_list_append(exp_nodes, gn);
+	r_list_append(exp_nodes, gn2);
+	r_list_append(exp_nodes, gn3);
+	r_list_append(exp_nodes, gn4);
+	r_list_append(exp_nodes, gn5);
+	r_list_append(exp_nodes, gn6);
+	r_list_append(exp_nodes, gn7);
+	r_list_append(exp_nodes, gn8);
+	r_list_append(exp_nodes, gn9);
+	r_list_append(exp_nodes, gn10);
+	RList *nodes = r_graph_get_nodes(g);
+	check(g->n_nodes, 10, "n_nodes.again");
+	check_list(nodes, exp_nodes, "get_all_nodes");
+	r_list_free(exp_nodes);
+	r_list_free(nodes);
+
+	r_graph_free (g);
+	return 0;
+}
