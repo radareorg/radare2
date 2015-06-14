5946193d297eb46322c16cd17b3f2e6cf53f4bf5
pancake <pancake@nopcode.org>
2015-06-14 02:36:25 +0200

    Fix rahash2 -b issue

--- binr/rahash2/rahash2.c
+++ binr/rahash2/rahash2.c
@@ -161,11 +161,8 @@ static int do_hash(const char *file, const char *algo, RIO *io, int bsize, int r
 						hashbit, s.buf, s.len, rad, 0, ule);
 				}
 				for (j=from; j<to; j+=bsize) {
-					int len = ((j+bsize)<fsize)?  bsize:
-						(bsize>j)?(fsize-j):0;
-					if (j+len>to)
-						len = to-j;
-					r_io_pread (io, j, buf, bsize);
+					int len = ((j+bsize)>to)? (to-j): bsize;
+					r_io_pread (io, j, buf, len);
 					do_hash_internal (ctx, hashbit, buf,
 						len, rad, 0, ule);
 				}
