38d79c727d37b7e06eed794870289a8f5d53761c
jvoisin <julien.voisin@dustri.org>
2015-07-08 22:52:06 +0200

    Some coverifix

--- libr/util/calc.c
+++ libr/util/calc.c
@@ -271,7 +271,7 @@ static RNumCalcToken get_token(RNum *num, RNumCalc *nc) {
 			nc->string_value[i++] = ch;
 			if (ch == '[') {
 				while (cin_get (num, nc, &ch) && ch!=']') {
-					if (i > R_NUMCALC_STRSZ) {
+					if (i > R_NUMCALC_STRSZ - 1) {
 						error (num, nc, "string too long");
 						return 0;
 					}
