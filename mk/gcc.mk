2ba692a3732f54b0dbd81f93b55ea2f0d74118c7
Anton Kochkov <anton.kochkov@gmail.com>
2015-07-11 02:19:20 +0300

    Improve MSYS2 support: MinGW-W64 detection

--- mk/gcc.mk
+++ mk/gcc.mk
@@ -24,10 +24,14 @@ else
 ifneq (,$(findstring mingw32,${OSTYPE}))
 PIC_CFLAGS=
 else
+ifneq (,$(findstring mingw64,${OSTYPE}))
+PIC_CFLAGS=
+else
 ifneq (,$(findstring msys,${OSTYPE}))
 PIC_CFLAGS=
 else
 PIC_CFLAGS=-fPIC
+endif
 endif
 endif
 endif
