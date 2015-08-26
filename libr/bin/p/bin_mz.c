d3070e54ff47c3688cfa4b6e26ee70a1cd871225
Riccardo Schirone <sirmy15@gmail.com>
2015-08-26 02:08:59 +0200

    util/str: consider (m)apped flag when printing/parsing srwx info

--- libr/bin/p/bin_mz.c
+++ libr/bin/p/bin_mz.c
@@ -121,7 +121,7 @@ static RList * sections(RBinFile *arch) {
 		ptr->vsize = segments[i].size;
 		ptr->paddr = segments[i].paddr;
 		ptr->vaddr = segments[i].paddr;
-		ptr->srwx = r_str_rwx ("rwx") | R_BIN_SCN_MAP;
+		ptr->srwx = r_str_rwx ("rwxm");
 		r_list_append (ret, ptr);
 	}
 	free ((void *)segments);
