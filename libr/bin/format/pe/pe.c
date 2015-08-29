b270e92021cc98b7c8de094afd98045e674b92c3
jvoisin <julien.voisin@dustri.org>
2015-08-29 23:30:16 +0200

    Refactor a bit bin_pe_parse_imports

--- libr/bin/format/pe/pe.c
+++ libr/bin/format/pe/pe.c
@@ -163,33 +163,35 @@ static int bin_pe_parse_imports(struct PE_(r_bin_pe_obj_t)* bin, struct r_bin_pe
 	char *symname;
 	char *filename;
 	char *symdllname = NULL;
+
 	if (!dll_name || *dll_name == '0')
 		return 0;
+
 	if ((off = bin_pe_vaddr_to_paddr(bin, OriginalFirstThunk)) == 0 &&
 		(off = bin_pe_vaddr_to_paddr(bin, FirstThunk)) == 0)
 		return 0;
 
 	do {
-		if (import_ordinal >= UT16_MAX) {
+		if (import_ordinal >= UT16_MAX)
 			break;
-		}
-		if (r_buf_read_at (bin->b, off + i * sizeof (PE_DWord),
+		else if (r_buf_read_at (bin->b, off + i * sizeof (PE_DWord),
 				(ut8*)&import_table, sizeof (PE_DWord)) == -1) {
 			eprintf("Error: read (import table)\n");
-			return 0;
+			goto error;
 		}
-		if (import_table) {
+		else if (import_table) {
 			if (import_table & ILT_MASK1) {
 				import_ordinal = import_table & ILT_MASK2;
 				import_hint = 0;
 				snprintf (import_name, PE_NAME_LENGTH, "qq%s_Ordinal_%i",
 					dll_name, import_ordinal);
-				//
 				symdllname = strdup (dll_name);
-				// rip ".dll"
-				int len = strlen (symdllname);
+
+				// remove the trailling ".dll"
+				size_t len = strlen (symdllname);
 				len = len < 4 ? 0 : len - 4;
 				symdllname[len] = 0;
+
 				if (!sdb_module || strcmp (symdllname, sdb_module)) {
 					sdb_free (db);
 					db = NULL;
@@ -220,41 +222,46 @@ static int bin_pe_parse_imports(struct PE_(r_bin_pe_obj_t)* bin, struct r_bin_pe
 				}
 			} else {
 				import_ordinal++;
-				ut64 off = bin_pe_vaddr_to_paddr(bin, import_table);
+				const ut64 off = bin_pe_vaddr_to_paddr(bin, import_table);
 				if (r_buf_read_at (bin->b, off, (ut8*)&import_hint, sizeof (PE_Word)) == -1) {
 					eprintf ("Error: read import hint at 0x%08"PFMT64x"\n", off);
-					return 0;
+					goto error;
 				}
-				name[0] = 0;
+				name[0] = '\0';
 				if (r_buf_read_at (bin->b, bin_pe_vaddr_to_paddr(bin, import_table) + sizeof(PE_Word),
 							(ut8*)name, PE_NAME_LENGTH) == -1) {
 					eprintf ("Error: read (import name)\n");
-					return 0;
-				}
-				if (!*name)
+					goto error;
+				} else if (!*name) {
 					break;
+				}
 				snprintf (import_name, PE_NAME_LENGTH, "%s_%s", dll_name, name);
 			}
 			if (!(*importp = realloc (*importp, (*nimp + 1) * sizeof(struct r_bin_pe_import_t)))) {
-				free (symdllname);
-				free (sdb_module);
 				r_sys_perror ("realloc (import)");
-				return R_FALSE;
+				goto error;
 			}
 			memcpy((*importp)[*nimp].name, import_name, PE_NAME_LENGTH);
 			(*importp)[*nimp].name[PE_NAME_LENGTH] = '\0';
-			(*importp)[*nimp].vaddr = FirstThunk + i * sizeof (PE_DWord);
-			(*importp)[*nimp].vaddr += bin->nt_headers->optional_header.ImageBase;
+			(*importp)[*nimp].vaddr = FirstThunk + i * sizeof (PE_DWord) \
+				+ bin->nt_headers->optional_header.ImageBase;
 			(*importp)[*nimp].paddr = bin_pe_vaddr_to_paddr(bin, FirstThunk) + i * sizeof(PE_DWord);
 			(*importp)[*nimp].hint = import_hint;
 			(*importp)[*nimp].ordinal = import_ordinal;
 			(*importp)[*nimp].last = 0;
-			(*nimp)++; i++;
+			(*nimp)++;
+			i++;
 		}
 	} while (import_table);
+
 	free (symdllname);
 	free (sdb_module);
 	return i;
+
+error:
+	free (symdllname);
+	free (sdb_module);
+	return R_FALSE;
 }
 
 static int bin_pe_init_hdr(struct PE_(r_bin_pe_obj_t)* bin) {
