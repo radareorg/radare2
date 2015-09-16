670cf2362bcd60214ab3380c7c32f058ae692745
condret <condret@runas-racer.com>
2015-09-16 20:16:06 +0000

    some anal-snes

--- libr/anal/p/anal_snes.c
+++ libr/anal/p/anal_snes.c
@@ -10,11 +10,30 @@
 static int snes_anop(RAnal *anal, RAnalOp *op, ut64 addr, const ut8 *data, int len) {
 	memset (op, '\0', sizeof (RAnalOp));
 	op->size = snes_op[data[0]].len;
+	if (op->size > len)
+		return op->size = 0;
 	op->addr = addr;
 	op->type = R_ANAL_OP_TYPE_UNK;
 	switch (data[0]) {
 		case 0xea:
 			op->type = R_ANAL_OP_TYPE_NOP;
+			break;
+		case 0xfb:
+			op->type = R_ANAL_OP_TYPE_XCHG;
+			break;
+		case 0x1b:				//TCS
+		case 0x3b:				//TSC
+		case 0x5b:				//TCD
+		case 0x7b:				//TDC
+		case 0x8a:				//TXA
+		case 0x98:				//TYA
+		case 0x9a:				//TXS
+		case 0x9b:				//TXY
+		case 0xa8:				//TAY
+		case 0xaa:				//TAX
+		case 0xba:				//TSX
+		case 0xbb:				//TYX
+			op->type = R_ANAL_OP_TYPE_MOV;
 			break;
 	}
 	return op->size;
