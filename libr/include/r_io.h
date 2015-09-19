5629e904a09692c578762a850285a83870d911a3
condret <condret@runas-racer.com>
2015-09-19 00:36:31 +0000

    start integrating plugins into siol

--- libr/include/r_io.h
+++ libr/include/r_io.h
@@ -13,6 +13,29 @@
 #define R_IO_SEEK_CUR	1
 #define R_IO_SEEK_END	2
 
+#define R_IO_UNDOS 64
+typedef struct r_io_undo_t {
+	int s_enable;
+	int w_enable;
+	/* write stuff */
+	RList *w_list;
+	int w_init;
+	/* seek stuff */
+	int idx;
+	int undos; /* available undos */
+	int redos; /* available redos */
+	ut64 seek[R_IO_UNDOS];
+	/*int fd[R_IO_UNDOS]; // XXX: Must be RIODesc* */
+} RIOUndo;
+
+typedef struct r_io_undo_w_t {
+	int set;
+	ut64 off;
+	ut8 *o;   /* old data */
+	ut8 *n;   /* new data */
+	int len;  /* length */
+} RIOUndoWrite;
+
 typedef struct r_io_t {
 	struct r_io_desc_t *desc;
 	ut64 off;
@@ -24,16 +47,43 @@ typedef struct r_io_t {
 	SdbList *maps;
 	//SdbList *cache;
 	Sdb *files;
+	RIOUndo undo;
 } RIO;
 
-//RIOCbs instances should be predefined and not generated to prevent memleaks
-typedef struct r_io_callbacks_t {
-	struct r_io_desc_t *(*open)(RIO *io, const char *uri, int flags, int mode);
-	int (*read)(RIO *io, struct r_io_desc_t *desc, ut8 *buf, int len);
-	ut64 (*lseek)(RIO *io, struct r_io_desc_t *desc, ut64 offset, int whence);
-	int (*write)(RIO *io, struct r_io_desc_t *desc, const ut8 *buf, int len);
-	int (*close)(struct r_io_desc_t *desc);
-} RIOCbs;
+typedef struct r_io_desc_t {
+	int fd;
+	int flags;
+	char *uri;
+	char *name;
+	void *data;
+	struct r_io_plugin_t *plugin;
+	RIO *io;
+} RIODesc;
+
+typedef struct r_io_plugin_t {
+	void *plugin;
+	char *name;
+	char *desc;
+	char *license;
+	void *widget;
+	int (*listener)(RIODesc *io);
+	int (*init)();
+	RIOUndo undo;
+	int isdbg;
+	int (*is_file_opened)(RIO *io, RIODesc *fd, const char *);
+	int (*system)(RIO *io, RIODesc *fd, const char *);
+	RIODesc* (*open)(RIO *io, const char *, int rw, int mode);
+	RList* /*RIODesc* */ (*open_many)(RIO *io, const char *, int rw, int mode);
+	int (*read)(RIO *io, RIODesc *fd, ut8 *buf, int count);
+	ut64 (*lseek)(RIO *io, RIODesc *fd, ut64 offset, int whence);
+	int (*write)(RIO *io, RIODesc *fd, const ut8 *buf, int count);
+	int (*close)(RIODesc *desc);
+	int (*resize)(RIO *io, RIODesc *fd, ut64 size);
+	int (*extend)(RIO *io, RIODesc *fd, ut64 size);
+	int (*accept)(RIO *io, RIODesc *desc, int fd);
+	int (*create)(RIO *io, const char *file, int mode, int type);
+	int (*plugin_open)(RIO *io, const char *, ut8 many);
+} RIOPlugin;
 
 typedef struct r_io_map_t {
 	int fd;
@@ -45,18 +95,9 @@ typedef struct r_io_map_t {
 	char *name;
 } RIOMap;
 
-typedef struct r_io_desc_t {
-	int fd;
-	int flags;
-	char *uri;
-	void *data;
-	RIOCbs *cbs;
-	RIO *io;
-} RIODesc;
-
 //desc.c
 R_API int r_io_desc_init (RIO *io);
-R_API RIODesc *r_io_desc_new (RIOCbs *cbs, int fd, char *uri, int flags, void *data);
+R_API RIODesc *r_io_desc_new (RIOPlugin *plugin, int fd, char *uri, int flags, void *data);
 R_API void r_io_desc_free (RIODesc *desc);
 R_API int r_io_desc_add (RIO *io, RIODesc *desc);
 R_API int r_io_desc_del (RIO *io, int fd);
@@ -85,9 +126,9 @@ R_API void r_io_map_del_name (RIOMap *map);
 //io.c
 R_API RIO *r_io_new ();
 R_API RIO *r_io_init (RIO *io);
-R_API RIODesc *r_io_open_nomap (RIO *io, RIOCbs *cbs, char *uri, int flags, int mode);
-R_API RIODesc *r_io_open (RIO *io, RIOCbs *cbs, char *uri, int flags, int mode);
-R_API RIODesc *r_io_open_at (RIO *io, RIOCbs *cbs, char *uri, int flags, int mode, ut64 at);
+R_API RIODesc *r_io_open_nomap (RIO *io, char *uri, int flags, int mode);
+R_API RIODesc *r_io_open (RIO *io, char *uri, int flags, int mode);
+R_API RIODesc *r_io_open_at (RIO *io, char *uri, int flags, int mode, ut64 at);
 R_API int r_io_close (RIO *io, int fd);
 R_API int r_io_pread_at (RIO *io, ut64 paddr, ut8 *buf, int len);
 R_API int r_io_pwrite_at (RIO *io, ut64 paddr, ut8 *buf, int len);
@@ -97,5 +138,15 @@ R_API int r_io_read_at (RIO *io, ut64 paddr, ut8 *buf, int len);
 R_API int r_io_write_at (RIO *io, ut64 paddr, ut8 *buf, int len);
 R_API int r_io_fini (RIO *io);
 R_API void r_io_free (RIO *io);
+
+R_API int r_io_plugin_init(RIO *io);
+R_API int r_io_plugin_open(RIO *io, int fd, RIOPlugin *plugin);
+R_API int r_io_plugin_close(RIO *io, int fd, RIOPlugin *plugin);
+R_API int r_io_plugin_generate(RIO *io);
+R_API int r_io_plugin_add(RIO *io, RIOPlugin *plugin);
+R_API int r_io_plugin_list(RIO *io);
+R_API RIOPlugin *r_io_plugin_resolve(RIO *io, const char *filename, ut8 many);
+R_API RIOPlugin *r_io_plugin_resolve_fd(RIO *io, int fd);
+R_API RIOPlugin *r_io_plugin_get_default(RIO *io, const char *filename, ut8 many);
 
 #endif
