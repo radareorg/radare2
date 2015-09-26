942172ca4c0a726f65771d4b39a83ac9dea122d1
Riccardo Schirone <sirmy15@gmail.com>
2015-09-26 14:55:52 +0200

    core/bin: fix json in bin_main

--- libr/core/bin.c
+++ libr/core/bin.c
@@ -458,29 +458,26 @@ static int bin_pdb(RCore *core, int mode) {
 
 static int bin_main(RCore *r, int mode, int va) {
 	RBinAddr *binmain = r_bin_get_sym (r->bin, R_BIN_SYM_MAIN);
-	ut64 main_addr = 0LL;
+	ut64 addr;
 	if (!binmain) return false;
 
-	if (va) {
-		main_addr = r_bin_a2b (r->bin, binmain->vaddr);
-	} else {
-		main_addr = binmain->paddr;
-	}
+	addr = va ? r_bin_a2b (r->bin, binmain->vaddr) : binmain->paddr;
 
-	if ((mode & R_CORE_BIN_SIMPLE) || mode & R_CORE_BIN_JSON) {
-		r_cons_printf ("%"PFMT64d, main_addr);
-	} else if ((mode & R_CORE_BIN_SET)) {
+	if (IS_MODE_SET (mode)) {
 		r_flag_space_set (r->flags, "symbols");
-		r_flag_set (r->flags, "main", main_addr, r->blocksize, 0);
+		r_flag_set (r->flags, "main", addr, r->blocksize, 0);
+	} else if (IS_MODE_SIMPLE (mode)) {
+		r_cons_printf ("%"PFMT64d, addr);
+	} else if (IS_MODE_RAD (mode)) {
+		r_cons_printf ("fs symbols\n");
+		r_cons_printf ("f main @ 0x%08"PFMT64x"\n", addr);
+	} else if (IS_MODE_JSON (mode)) {
+		r_cons_printf ("{\"vaddr\":%" PFMT64d
+			",\"paddr\":%" PFMT64d "}", addr, binmain->paddr);
 	} else {
-		if (mode) {
-			r_cons_printf ("fs symbols\n");
-			r_cons_printf ("f main @ 0x%08"PFMT64x"\n", main_addr);
-		} else {
-			r_cons_printf ("[Main]\n");
-			r_cons_printf ("vaddr=0x%08"PFMT64x" paddr=0x%08"PFMT64x"\n",
-					main_addr, binmain->paddr);
-		}
+		r_cons_printf ("[Main]\n");
+		r_cons_printf ("vaddr=0x%08"PFMT64x" paddr=0x%08"PFMT64x"\n",
+			addr, binmain->paddr);
 	}
 	return true;
 }
