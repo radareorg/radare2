e4464575ce95155457b8c72aa2d7834547324878
pancake <pancake@nopcode.org>
2015-10-09 11:42:49 +0200

    iOS build enhancements

--- sys/ios-shell.sh
+++ sys/ios-shell.sh
@@ -2,7 +2,9 @@
 [ -z "$SHELL" ] && SHELL=/bin/sh
 
 if [ -d sys ]; then
+	export IOSINC=`pwd`/sys/ios-include
 	export PATH="$PWD/sys/:$PATH"
+	export PS1="[ios-sdk \w]> "
 	$SHELL
 else
 	echo "Run from r2 root directory"
