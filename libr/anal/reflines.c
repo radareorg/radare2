radareorg/radare2

commit 95960b5592586a4a9a023ad9573978f1b8eb657b

Fix oobread in mips analysis

libr/anal/reflines.c
@@ -48,7 +48,7 @@ R_API RList *r_anal_reflines_get(RAnal *anal, ut64 addr, const ut8 *buf, ut64 le
 		addr += sz;
 		// This can segflauta if opcode length and buffer check fails
 		r_anal_op_fini (&op);
-		sz = r_anal_op (anal, &op, addr, ptr, (int)(end - ptr));
+		sz = r_anal_op (anal, &op, addr, ptr, (int)(end - ptr - (sz*2)));
 		if (sz > 0) {
 			/* store data */
 			switch (op.type) {

libr/anal/p/anal_mips_cs.c
@@ -323,6 +323,8 @@ static int analop(RAnal *a, RAnalOp *op, ut64 addr, const ut8 *buf, int len) {
 // XXX no arch->cpu ?!?! CS_MODE_MICRO, N64
 	op->delay = 0;
 	op->type = R_ANAL_OP_TYPE_ILL;
+	if (len<4)
+		return -1;
 	op->size = 4;
 	if (handle == 0) {
 		ret = cs_open (CS_ARCH_MIPS, mode, &handle);

commit ce1f9afd6864695afbb37564a17b75a4079f8820

Fix reflines regression introduced in 95960b5592586a4a9a023ad9573978f1b8eb657b

libr/anal/reflines.c
@@ -48,7 +48,7 @@ R_API RList *r_anal_reflines_get(RAnal *anal, ut64 addr, const ut8 *buf, ut64 le
 		addr += sz;
 		// This can segflauta if opcode length and buffer check fails
 		r_anal_op_fini (&op);
-		sz = r_anal_op (anal, &op, addr, ptr, (int)(end - ptr - (sz*2)));
+		sz = r_anal_op (anal, &op, addr, ptr, (int)(end - ptr));
 		if (sz > 0) {
 			/* store data */
 			switch (op.type) {