a67f7d6d8a586374fe4007844e1bb5a62004fb2f
Anton Kochkov <anton.kochkov@gmail.com>
2015-10-21 17:44:59 +0300

    Fix the memleak

--- libr/core/cmd.c
+++ libr/core/cmd.c
@@ -2146,8 +2146,10 @@ R_API int r_core_cmd_command(RCore *core, const char *command) {
 	char *buf, *rcmd, *ptr;
 	char *cmd = r_core_sysenv_begin (core, command);
 	rcmd = ptr = buf = r_sys_cmd_str (cmd, 0, &len);
-	if (buf == NULL)
+	if (buf == NULL) {
+		free (cmd);
 		return -1;
+	}
 	ret = r_core_cmd (core, rcmd, 0);
 	r_core_sysenv_end (core, command);
 	free (buf);
