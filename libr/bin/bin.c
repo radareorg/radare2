7a1b6516c1fbbbe01d28af0f7d72ee21eb238bf2
pancake <pancake@nopcode.org>
2015-10-24 13:20:55 +0200

    Fix java class crash reported by @revskills (db3d80b14..)

--- libr/bin/bin.c
+++ libr/bin/bin.c
@@ -897,8 +897,6 @@ R_API RBinPlugin * r_bin_get_binplugin_by_bytes (RBin *bin, const ut8* bytes, ut
 	r_list_foreach (bin->plugins, it, plugin) {
 		if (plugin->check_bytes && plugin->check_bytes (bytes, sz) )
 			return plugin;
-		// must be set to null
-		plugin = NULL;
 	}
 	return NULL;
 }
