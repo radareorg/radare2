9ff2cb4e5bc2c0f10f714148c884b4fcca18c6fe
pancake <pancake@nopcode.org>
2015-10-25 02:31:42 +0200

    Fix #java #parser regression introduced in 7a1b6516c1fbbbe01d28af0f7d72ee21eb238bf2

--- shlr/java/class.c
+++ shlr/java/class.c
@@ -1961,22 +1961,23 @@ R_API RBinJavaAttrMetas* r_bin_java_get_attr_type_by_name(const char *name) {
 	return res;
 }
 
-R_API RBinJavaAttrInfo* r_bin_java_read_next_attr(RBinJavaObj *bin, const ut64 offset, const ut8* buf, const ut64 len) {
+R_API RBinJavaAttrInfo* r_bin_java_read_next_attr(RBinJavaObj *bin, const ut64 offset, const ut8* buf, const ut64 buf_len) {
 	RBinJavaAttrInfo* attr = NULL;
 	ut32 sz = 0;
 	ut8* buffer = NULL;
 	const ut8* a_buf = offset + buf;
 	ut8 attr_idx_len = 6;
-	if (offset +6 > len) {
+	if (offset +6 > buf_len) {
 		eprintf ("[X] r_bin_java: Error unable to parse remainder of classfile in Attribute offset "
-			"(0x%"PFMT64x") > len  of remaining bytes (0x%"PFMT64x").\n", offset, len);
+			"(0x%"PFMT64x") > len  of remaining bytes (0x%"PFMT64x").\n", offset, buf_len);
 		return attr;
 	}
 	// ut16 attr_idx, ut32 length of attr.
 	sz = R_BIN_JAVA_UINT (a_buf, 2) + attr_idx_len; //r_bin_java_read_int (bin, buf_offset+2) + attr_idx_len;
-	if (sz + offset > len) {
+	if (sz + offset > buf_len) {
 		eprintf ("[X] r_bin_java: Error unable to parse remainder of classfile in Attribute len "
-			"(0x%x) + offset (0x%"PFMT64x") exceeds length of buffer (0x%"PFMT64x").\n", sz, offset, len);
+			"(0x%x) + offset (0x%"PFMT64x") exceeds length of buffer (0x%"PFMT64x").\n",
+			sz, offset, buf_len);
 		return attr;
 	}
 	// when reading the attr bytes, need to also
@@ -1984,15 +1985,19 @@ R_API RBinJavaAttrInfo* r_bin_java_read_next_attr(RBinJavaObj *bin, const ut64 o
 	// are not included in the attribute length
 	//,
 	//	sz, buf_offset, buf_offset+sz);
-	buffer = r_bin_java_get_attr_buf (bin, sz, offset, buf, len);
-	attr = r_bin_java_read_next_attr_from_buffer (buffer, sz, offset);
-	free (buffer);
-	
-	if (!attr){
-		return NULL;
+	buffer = r_bin_java_get_attr_buf (bin, sz, offset, buf, buf_len);
+	//printf ("%d %d %d\n", sz, buf_len, offset);
+	if (offset < buf_len) {
+		attr = r_bin_java_read_next_attr_from_buffer (buffer, buf_len - offset, offset);
+		free (buffer);
+
+		if (!attr) {
+			return NULL;
+		}
+		attr->size = sz;
+	} else {
+		eprintf ("IS OOB\n");
 	}
-	
-	attr->size = sz;
 	return attr;
 }
 
@@ -3612,11 +3617,10 @@ R_API RBinJavaAttrInfo* r_bin_java_line_number_table_attr_new (ut8 *buffer, ut64
 	attr->info.line_number_table_attr.line_number_table_length = R_BIN_JAVA_USHORT (buffer, offset);
 	offset += 2;
 	attr->info.line_number_table_attr.line_number_table = r_list_newf (free);
+	if (attr->info.line_number_table_attr.line_number_table_length > sz) {
+		return NULL;
+	}
 	for (i = 0; i < attr->info.line_number_table_attr.line_number_table_length; i++) {
-		if (offset+8 >= sz) {
-			eprintf ("r_bin_java_line_number_table_attr_new: avoid oob\n");
-			break;
-		}
 		cur_location = buf_offset + offset;
 		lnattr = R_NEW0 (RBinJavaLineNumberAttribute);
 		if (!lnattr) {
