b05c48f4c030b91366ffbb6655ebb1a11a9b9c4c
pancake <pancake@nopcode.org>
2015-10-27 04:52:24 +0100

    Fix pfd crash and support enums and more formats

--- libr/util/p_format.c
+++ libr/util/p_format.c
@@ -606,14 +606,13 @@ static int r_print_format_10bytes(const RPrint* p, int mode, const char* setval,
 	if (MUSTSET) {
 		p->cb_printf ("?e pf B not yet implemented\n");
 	} else if (mode & R_PRINT_DOT) {
-		for (; j<10; j++)
+		for (j = 0; j<10; j++)
 			p->cb_printf ("%02x ", buf[j]);
 	} else if (MUSTSEE) {
 		if (!p->iob.read_at) {
 			printf ("(cannot read memory)\n");
 			return -1;
-		} else
-			p->iob.read_at (p->iob.io, (ut64)addr, buffer, 248);
+		} else p->iob.read_at (p->iob.io, (ut64)addr, buffer, 248);
 		if (!SEEVALUE) p->cb_printf ("0x%08"PFMT64x" = ", seeki);
 		j=0;
 		for (; j<10; j++)
@@ -651,7 +650,7 @@ static int r_print_format_hexpairs(const RPrint* p, int endian, int mode,
 	if (MUSTSET) {
 		p->cb_printf ("?e pf X not yet implemented\n");
 	} else if (mode & R_PRINT_DOT) {
-		for (; j<size; j++)
+		for (j = 0; j<size; j++)
 			p->cb_printf ("%02x", buf[i+j]);
 	} else if (MUSTSEE) {
 		size = (size < 1) ? 1 : size;
@@ -735,7 +734,7 @@ static void r_print_format_word(const RPrint* p, int endian, int mode,
 	} else if (mode & R_PRINT_DOT) {
 		if (size==-1)
 			p->cb_printf ("0x%04x", addr);
-		while (size--) {
+		while ((size-=2)>0) { //size--) {
 			if (endian)
 				addr = (*(buf+i))<<8 | (*(buf+i+1));
 			else addr = (*(buf+i+1))<<8 | (*(buf+i));
@@ -774,7 +773,7 @@ static void r_print_format_word(const RPrint* p, int endian, int mode,
 			p->cb_printf ("%d", addr);
 		else {
 			p->cb_printf ("[ ");
-			while (size--) {
+			while (size-=2 >0) {
 				if (endian)
 					addr = (*(buf+i))<<8 | (*(buf+i+1));
 				else addr = (*(buf+i+1))<<8 | (*(buf+i));
@@ -821,13 +820,16 @@ static void r_print_format_nulltermstring(const RPrint* p, const int len, int en
 		free(ons);
 	} else if (mode & R_PRINT_DOT) {
 		int j = i;
-		p->cb_printf ("\"", seeki);
+		p->cb_printf ("\\\"", seeki);
 		for (; j<len && ((size==-1 || size-- >0) && buf[j]) ; j++) {
-			if (IS_PRINTABLE (buf[j]))
-				p->cb_printf ("%c", buf[j]);
-			else p->cb_printf (".");
+			char ch = buf[j];
+			if (ch == '"') {
+				p->cb_printf ("\\\"");
+			} else if (IS_PRINTABLE (ch)) {
+				p->cb_printf ("%c", ch);
+			} else p->cb_printf (".");
 		}
-		p->cb_printf ("\"");
+		p->cb_printf ("\\\"");
 	} else if (MUSTSEE) {
 		int j = i;
 		if (!SEEVALUE) p->cb_printf ("0x%08"PFMT64x" = ", seeki);
@@ -911,13 +913,15 @@ static void r_print_format_enum (const RPrint* p, ut64 seeki, char* fmtname,
 	if (p->get_enumname)
 		enumvalue = p->get_enumname (p->user, fmtname, addr);
 	if (enumvalue && *enumvalue) {
-		if (MUSTSEEJSON) p->cb_printf ("\"%s\"}", fmtname);
+		if (mode & R_PRINT_DOT) {
+			p->cb_printf ("%s.%s", fmtname, enumvalue);
+		} else if (MUSTSEEJSON) p->cb_printf ("\"%s\"}", fmtname);
 		else if (MUSTSEE) p->cb_printf (" %s (enum) = 0x%"PFMT64x" ; %s\n",
 				fieldname, addr, enumvalue);
 	} else {
 		if (MUSTSEEJSON) p->cb_printf ("\"`te %s 0x%x`\"}", fmtname, addr);
-		else if (MUSTSEE) p->cb_printf (" %s (enum) = `te %s 0x%x`\n",
-				fieldname, fmtname, addr);
+		else if (MUSTSEE) p->cb_printf (" %s (enum) = %s\n",//`te %s 0x%x`\n",
+				fieldname, enumvalue); //fmtname, addr);
 	}
 	free (enumvalue);
 }
@@ -1284,7 +1288,7 @@ R_API int r_print_format(RPrint *p, ut64 seek, const ut8* b, const int len,
 				{
 				nexti = i + (p->bits/8);
 				i = 0;
-				if(tmp == '?' )seeki = addr;
+				if (tmp == '?' ) seeki = addr;
 				memset (buf, '\0', len);
 				if (MUSTSEE)
 					p->cb_printf ("(*0x%"PFMT64x") ", addr);
@@ -1361,8 +1365,13 @@ R_API int r_print_format(RPrint *p, ut64 seek, const ut8* b, const int len,
 
 			/* dot */
 			if (mode & R_PRINT_DOT) {
-				//eprintf ("IS DOT %s\n", formatname);
-				p->cb_printf ("|{%c|%s|<%s>", tmp, fieldname, fieldname);
+				if (fieldname) {
+					p->cb_printf ("|{0x%"PFMT64x"|%c|%s|<%s>",
+						seeki, tmp, fieldname, fieldname);
+				} else {
+					p->cb_printf ("|{0x%"PFMT64x"|%c|",
+						seeki, tmp);
+				}
 			}
 
 			/* json */
@@ -1556,6 +1565,9 @@ R_API int r_print_format(RPrint *p, ut64 seek, const ut8* b, const int len,
 			} else {
 				eprintf ("Likely a heap buffer overflow in %s at %d\n", __FILE__, __LINE__);
 				goto beach;
+			}
+			if (mode & R_PRINT_DOT) {
+				p->cb_printf ("}");
 			}
 			if (viewflags && p->offname) {
 				const char *s = p->offname (p->user, seeki);
