afbcddb5089e4c2b4021df542a8d5b9ac1f469e1
pancake <pancake@nopcode.org>
2015-10-27 15:45:27 +0100

    Fix #3516 - masm syntax was not working

--- libr/asm/p/asm_x86_cs.c
+++ libr/asm/p/asm_x86_cs.c
@@ -50,7 +50,7 @@ static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 		cs_option (cd, CS_OPT_DETAIL, CS_OPT_OFF);
 	}
 	if (a->syntax == R_ASM_SYNTAX_MASM) {
-#if CS_OPT_SYNTAX_MASM
+#if CS_API_MAJOR >= 4
 		cs_option (cd, CS_OPT_SYNTAX, CS_OPT_SYNTAX_MASM);
 #endif
 	} else if (a->syntax == R_ASM_SYNTAX_ATT) {
