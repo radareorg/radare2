92f4e722283d44b4a76fcada2ce1a6c5beef5bf3
pancake <pancake@nopcode.org>
2015-11-05 16:51:36 +0100

    Fix infinite analysis for archs without anal plugs

--- libr/anal/op.c
+++ libr/anal/op.c
@@ -61,10 +61,17 @@ R_API int r_anal_op(RAnal *anal, RAnalOp *op, ut64 addr, const ut8 *data, int le
 		}
 	}
 	if (len>0 && anal && memset (op, 0, sizeof (RAnalOp)) &&
-		anal->cur && anal->cur->op) {
+		anal->cur && anal->cur->op && strcmp (anal->cur->name, "null")) {
 		ret = anal->cur->op (anal, op, addr, data, len);
 		op->addr = addr;
 		if (ret<1) op->type = R_ANAL_OP_TYPE_ILL;
+	} else {
+		if (!memcmp (data, "\xff\xff\xff\xff", R_MIN(4, len))) {
+			op->type = R_ANAL_OP_TYPE_ILL;
+			ret = 2; // HACK
+		} else {
+			op->type = R_ANAL_OP_TYPE_MOV;
+		}
 	}
 	return ret;
 }
