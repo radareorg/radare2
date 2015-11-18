radareorg/radare2

commit 081ba834312fb9ad45e0794c9a6ea0dca2d24604

Fix crash 6f29967c0 by revskill

libr/bin/format/mach0/mach0.c
@@ -925,19 +925,21 @@ struct section_t* MACH0_(get_sections)(struct MACH0_(obj_t)* bin) {
 	if (!bin || !bin->sects)
 		return NULL;
 	to = R_MIN (bin->nsects, 128); // limit number of sections here to avoid fuzzed bins
-	if (to<1)
+	if (to < 1)
 		return NULL;
 	if (!(sections = malloc ((bin->nsects + 1) * sizeof (struct section_t))))
 		return NULL;
-	for (i = 0; i<to; i++) {
+	for (i = 0; i < to; i++) {
 		sections[i].offset = (ut64)bin->sects[i].offset;
 		sections[i].addr = (ut64)bin->sects[i].addr;
 		sections[i].size = (ut64)bin->sects[i].size;
 		sections[i].align = bin->sects[i].align;
 		sections[i].flags = bin->sects[i].flags;
-		strncpy (segname, bin->sects[i].segname, sizeof (segname)-1);
-		strncpy (sectname, bin->sects[i].sectname, sizeof (sectname)-1);
+		r_str_ncpy (segname, bin->sects[i].segname, sizeof (segname)-1);
+		r_str_ncpy (sectname, bin->sects[i].sectname, sizeof (sectname)-1);
 		// hack to support multiple sections with same name
+		// ensure '\0' terminated
+		bin->sects[i].sectname[15] = '\0';
 		snprintf (segname, sizeof (segname), "%d", i); // wtf
 		snprintf (sectname, sizeof (sectname), "%s", bin->sects[i].sectname);
 		for (j=0; j<bin->nsegs; j++) {

commit b35b830446fdb89f12c1afadd3f353685554c4eb

Fix mach0 regression introduced in 081ba8343

libr/bin/format/mach0/mach0.c
@@ -919,7 +919,7 @@ static int prot2perm (int x) {
 
 struct section_t* MACH0_(get_sections)(struct MACH0_(obj_t)* bin) {
 	struct section_t *sections;
-	char segname[17], sectname[17];
+	char segname[32], sectname[32];
 	int i, j, to;
 
 	if (!bin || !bin->sects)
@@ -939,7 +939,6 @@ struct section_t* MACH0_(get_sections)(struct MACH0_(obj_t)* bin) {
 		r_str_ncpy (sectname, bin->sects[i].sectname, sizeof (sectname)-1);
 		// hack to support multiple sections with same name
 		// ensure '\0' terminated
-		bin->sects[i].sectname[15] = '\0';
 		snprintf (segname, sizeof (segname), "%d", i); // wtf
 		snprintf (sectname, sizeof (sectname), "%s", bin->sects[i].sectname);
 		for (j=0; j<bin->nsegs; j++) {