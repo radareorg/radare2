e68ed8dd91665fcccca66f3b3879f041e782f57c
Maijin <maijin@malware.lu>
2015-11-27 00:19:10 +0100

    Add support for SPC700 file format

---
+++ libr/bin/p/spc700.mk
@@ -0,0 +1,9 @@
+OBJ_SPC700=bin_spc700.o
+STATIC_OBJ+=${OBJ_SPC700}
+TARGET_SPC700=bin_spc700.${EXT_SO}
+
+ALL_TARGETS+=${TARGET_SPC700}
+
+${TARGET_SPC700}: ${OBJ_SPC700}
+	${CC} $(call libname,bin_spc700) -shared ${CFLAGS} \
+		-o ${TARGET_SPC700} ${OBJ_SPC700} ${LDFLAGS}
