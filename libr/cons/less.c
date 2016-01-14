3535c284e135e225de9eccd18ef0a5d6c1aaf47b
pancake <pancake@nopcode.org>
2016-01-14 23:27:09 +0100

    Handle '?' and 'u' keys in r_cons_less

--- libr/cons/less.c
+++ libr/cons/less.c
@@ -141,6 +141,16 @@ static int all_matches(const char *s, RRegex *rx, RList **mla,
 }
 
 R_API int r_cons_less_str(const char *str, const char *exitkeys) {
+	static int in_help = false;
+	static const char *r_cons_less_help = \
+		" u/space  - page up/down\n"
+		" jk       - line down/up\n"
+		" gG       - begin/end buffer\n"
+		" /        - search in buffer\n"
+		" n/p      - next/prev search result\n"
+		" q        - quit\n"
+		" ?        - show this help\n"
+		"\n";
 	int lines_count;
 	RRegex *rx = NULL;
 	int w, h, ch, to, ui = 1, from = 0, i;
@@ -177,6 +187,17 @@ R_API int r_cons_less_str(const char *str, const char *exitkeys) {
 		}
 		ch = r_cons_arrow_to_hjkl (ch);
 		switch (ch) {
+		case '?':
+			if (!in_help) {
+				in_help = true;
+				r_cons_less_str (r_cons_less_help, NULL);
+				in_help = false;
+			}
+			break;
+		case 'u':
+			from -= h;
+			if (from < 0) from = 0;
+			break;
 		case ' ': from += h; break;
 		case 'g': from = 0; break;
 		case 'G': from = lines_count-1-h; break;
@@ -190,21 +211,21 @@ R_API int r_cons_less_str(const char *str, const char *exitkeys) {
 		case 'K': from = (from>=h)? from-h: 0;
 			break;
 		case '/': 	/* search */
-			r_cons_reset_colors();
-			r_line_set_prompt("/");
-			sreg = r_line_readline();
+			r_cons_reset_colors ();
+			r_line_set_prompt ("/");
+			sreg = r_line_readline ();
 			from = R_MIN(lines_count - 1, from);
 			/* repeat last search if empty string is provided */
-			if(sreg[0]){ /* prepare for a new search */
-				if(rx) r_regex_free(rx);
+			if (sreg[0]) { /* prepare for a new search */
+				if (rx) r_regex_free(rx);
 				rx = r_regex_new(sreg, "");
 			} else { /* we got an empty string */
 				from = next_match(from, mla, lines_count);
 				break;
 			}
-			if(!rx) break;
+			if (!rx) break;
 			/* find all occurences */
-			if(all_matches(p, rx, mla, lines, lines_count))
+			if (all_matches (p, rx, mla, lines, lines_count))
 				from = next_match(from, mla, lines_count);
 			break;
 		case 'n': 	/* next match */
@@ -217,7 +238,7 @@ R_API int r_cons_less_str(const char *str, const char *exitkeys) {
 		}
 	}
 	for (i = 0; i < lines_count; i++) {
-		r_list_free(mla[i]);
+		r_list_free (mla[i]);
 	}
 	free (mla);
 	if (rx) r_regex_free (rx);
