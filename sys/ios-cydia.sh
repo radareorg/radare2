436313175f5d78690e748a3c68621627a66ba25a
pancake <pancake@nopcode.org>
2016-01-14 23:26:50 +0100

    Enhance the ios-cydia.sh a bit

--- sys/ios-cydia.sh
+++ sys/ios-cydia.sh
@@ -26,25 +26,30 @@ export IOSVER=8.3
 export IOSINC=$(pwd)/sys/ios-include
 export CFLAGS=-O2
 
+makeDeb() {
+	( cd binr/radare2 ; make ios_sdk_sign )
+	rm -rf /tmp/r2ios
+	make install DESTDIR=/tmp/r2ios
+	rm -rf /tmp/r2ios/usr/share/radare2/*/www/enyo/node_modules
+	( cd /tmp/r2ios && tar czvf ../r2ios-${CPU}.tar.gz ./* )
+	rm -rf sys/cydia/radare2/root
+	mkdir -p sys/cydia/radare2/root
+	sudo tar xpzvf /tmp/r2ios-${CPU}.tar.gz -C sys/cydia/radare2/root
+	( cd sys/cydia/radare2 ; sudo make clean ; sudo make )
+}
+
 if true ; then
-make clean
-./configure --prefix=${PREFIX} --with-ostype=darwin \
-	--with-compiler=ios-sdk --target=arm-unknown-darwin
+	make clean
+	./configure --prefix=${PREFIX} --with-ostype=darwin \
+		--with-compiler=ios-sdk --target=arm-unknown-darwin
+	if [ $? = 0 ]; then
+		time make -j4
+		if [ $? = 0 ]; then
+			makeDeb
+		fi
+	fi
 else
-	true
+	makeDeb
 fi
 
-if [ $? = 0 ]; then
-	time make -j4
-	if [ $? = 0 ]; then
-		( cd binr/radare2 ; make ios_sdk_sign )
-		rm -rf /tmp/r2ios
-		make install DESTDIR=/tmp/r2ios
-		rm -rf /tmp/r2ios/usr/share/radare2/*/www/enyo/node_modules
-		( cd /tmp/r2ios && tar czvf ../r2ios-${CPU}.tar.gz ./* )
-		rm -rf sys/cydia/radare2/root
-		mkdir -p sys/cydia/radare2/root
-		sudo tar xpzvf /tmp/r2ios-${CPU}.tar.gz -C sys/cydia/radare2/root
-		( cd sys/cydia/radare2 ; sudo make clean ; sudo make )
-	fi
-fi
+
