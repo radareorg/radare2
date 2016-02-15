72d289ff4d7fbb99af7bcdd1d388a060d4b9b915
pancake <pancake@nopcode.org>
2016-02-16 00:13:41 +0100

    Better addr|int detection in r_config_node_type

--- libr/config/config.c
+++ libr/config/config.c
@@ -156,8 +156,12 @@ R_API const char *r_config_node_type(RConfigNode *node) {
 		int f = node->flags;
 		if (f & CN_BOOL) return "bool";
 		if (f & CN_STR) return "str";
-		if (f & CN_OFFT) return "addr";
-		if (f & CN_INT) return "int";
+		if (f & CN_OFFT || f & CN_INT) {
+			if (!strncmp (node->value, "0x", 2)) {
+				return "addr";
+			}
+			return "int";
+		}
 	}
 	return "";
 }
