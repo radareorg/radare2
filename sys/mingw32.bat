e5b2b94e3a2a3f312aa7b7f251334b1cd57274f1
Anton Kochkov <xvilka@gmail.com>
2016-02-29 12:04:13 +0300

    Simplify AppVeyor build script

--- sys/mingw32.bat
+++ sys/mingw32.bat
@@ -1,4 +1,5 @@
 SET PATH=C:\MinGW\msys\1.0\bin;C:\Program Files (x86)\Git\bin;%PATH%
+SET ENV_R2_VER=0.10.1-git
 echo %PATH%
 sh.exe -c "export PATH=/c/mingw/bin:/c/mingw/msys/1.0/bin:/c/Program\ Files\ \(x86\)/Git/bin:${PATH} ; gcc -v"
 sh.exe -c "uname | tr 'A-Z' 'a-z'"
@@ -8,7 +9,7 @@ sh.exe -c "export PATH=/c/mingw/bin:/c/mingw/msys/1.0/bin:/c/Program\ Files\ \(x
 if "%APPVEYOR%" == "True" (
      appveyor DownloadFile https://raw.githubusercontent.com/radare/radare2-win-installer/master/radare2.iss
      appveyor DownloadFile https://raw.githubusercontent.com/radare/radare2-win-installer/master/radare2.ico
-     dir %APPVEYOR_BUILD_FOLDER%\radare2-w32-0.10.1-git
-     7z.exe a -tzip %APPVEYOR_BUILD_FOLDER%\radare2-w32-0.10.1-git.zip %APPVEYOR_BUILD_FOLDER%\radare2-w32-0.10.1-git
-     iscc -DRadare2Location=%APPVEYOR_BUILD_FOLDER%\radare2-w32-0.10.1-git\* -DLicenseLocation=%APPVEYOR_BUILD_FOLDER%\COPYING.LESSER -DIcoLocation=%APPVEYOR_BUILD_FOLDER%\radare2.ico radare2.iss
+     dir %APPVEYOR_BUILD_FOLDER%\radare2-w32-%ENV_R2_VER%
+     7z.exe a -tzip %APPVEYOR_BUILD_FOLDER%\radare2-w32-%ENV_R2_VER%.zip %APPVEYOR_BUILD_FOLDER%\radare2-w32-%EVN_R2_VER%
+     iscc -DRadare2Location=%APPVEYOR_BUILD_FOLDER%\radare2-w32-%ENV_R2_VER%\* -DLicenseLocation=%APPVEYOR_BUILD_FOLDER%\COPYING.LESSER -DIcoLocation=%APPVEYOR_BUILD_FOLDER%\radare2.ico radare2.iss
 )
