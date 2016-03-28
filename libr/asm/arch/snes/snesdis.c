58328def229de37c2b2c9d2ae11854d0453ca009
Evan Shaw <edsrzf@gmail.com>
2016-03-29 07:18:12 +1300

    Show absolute offsets for relative branches in SNES disassembly
    This makes the disassembly easier to read.

--- libr/asm/arch/snes/snesdis.c
+++ libr/asm/arch/snes/snesdis.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #include "snes_op_table.h"
 
-static int snesDisass(int bits, RAsmOp *op, const ut8 *buf, int len){
+static int snesDisass(int bits, ut64 pc, RAsmOp *op, const ut8 *buf, int len){
 	snes_op_t *s_op = &snes_op[buf[0]];
 	int op_len = snes_op_get_size(bits, s_op);
 	if (len < op_len)
@@ -17,13 +17,19 @@ static int snesDisass(int bits, RAsmOp *op, const ut8 *buf, int len){
 		strncpy (op->buf_asm, s_op->name, sizeof (op->buf_asm));
 		break;
 	case SNES_OP_16BIT:
-		snprintf (op->buf_asm, sizeof (op->buf_asm), s_op->name, buf[1]);
+		if (*buf % 0x20 == 0x10 || *buf == 0x80) { // relative branch
+			snprintf (op->buf_asm, sizeof (op->buf_asm), s_op->name, pc + 2 + (st8)buf[1]);
+		} else {
+			snprintf (op->buf_asm, sizeof (op->buf_asm), s_op->name, buf[1]);
+		}
 		break;
 	case SNES_OP_24BIT:
 		if (*buf == 0x44 || *buf == 0x54) { // mvp and mvn
-			sprintf (op->buf_asm, s_op->name, buf[1], buf[2]);
+			snprintf (op->buf_asm, sizeof (op->buf_asm), s_op->name, buf[1], buf[2]);
+		} else if (*buf == 0x82) { // brl
+			snprintf (op->buf_asm, sizeof (op->buf_asm), s_op->name, pc + 3 + (st16)ut8p_bw(buf+1));
 		} else {
-			sprintf (op->buf_asm, s_op->name, ut8p_bw(buf+1));
+			snprintf (op->buf_asm, sizeof (op->buf_asm), s_op->name, ut8p_bw(buf+1));
 		}
 		break;
 	case SNES_OP_32BIT:
