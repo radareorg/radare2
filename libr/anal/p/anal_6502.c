radareorg/radare2

commit 36e42b33fe37e922f132e286508bd88ef9fb137d

Support 16-bit immediate operands in SNES
Size for immediate operands depends on the value of the M flag
(for register A) or the X flag (for registers X and Y). A register is
8-bit when its respective flag is set and 16-bit when clear.

These flags can be set or unset independently, so this still isn't quite right
for when one flag is set and the other isn't, but it's an improvement. It's
possible to force a particular instruction to decode correctly by using the
`afh` command.

libr/asm/arch/snes/snes_op_table.h
@@ -6,7 +6,8 @@ enum {
 	SNES_OP_8BIT = 1,
 	SNES_OP_16BIT,
 	SNES_OP_24BIT,
-	SNES_OP_32BIT
+	SNES_OP_32BIT,
+	SNES_OP_IMM // 8- or 16-bit immediate depending on X or M flag
 };
 
 typedef struct {
@@ -25,7 +26,7 @@ static snes_op_t snes_op[]={
 {"asl 0x%02x",		SNES_OP_16BIT},
 {"ora [0x%02x]",	SNES_OP_16BIT},
 {"php",			SNES_OP_8BIT},
-{"ora #0x%02x",		SNES_OP_16BIT},
+{"ora",			SNES_OP_IMM},
 {"asl a",		SNES_OP_8BIT},
 {"phd",			SNES_OP_8BIT},
 {"tsb 0x%04x",		SNES_OP_24BIT},
@@ -57,7 +58,7 @@ static snes_op_t snes_op[]={
 {"rol 0x%02x",		SNES_OP_16BIT},
 {"and [0x%02x]",	SNES_OP_16BIT},
 {"plp",			SNES_OP_8BIT},
-{"and #0x%02x",		SNES_OP_16BIT},
+{"and",			SNES_OP_IMM},
 {"rol a",		SNES_OP_8BIT},
 {"pld",			SNES_OP_8BIT},
 {"bit 0x%04x",		SNES_OP_24BIT},
@@ -89,7 +90,7 @@ static snes_op_t snes_op[]={
 {"lsr 0x%02x",		SNES_OP_16BIT},
 {"eor [0x%02x]",	SNES_OP_16BIT},
 {"pha",			SNES_OP_8BIT},
-{"eor #0x%02x",		SNES_OP_16BIT},
+{"eor",			SNES_OP_IMM},
 {"lsr a",		SNES_OP_8BIT},
 {"phk",			SNES_OP_8BIT},
 {"jmp 0x%04x",		SNES_OP_24BIT},
@@ -121,7 +122,7 @@ static snes_op_t snes_op[]={
 {"ror 0x%02x",		SNES_OP_16BIT},
 {"adc [0x%02x]",	SNES_OP_16BIT},
 {"pla",			SNES_OP_8BIT},
-{"adc #0x%02x",		SNES_OP_16BIT},
+{"adc",			SNES_OP_IMM},
 {"ror a",		SNES_OP_8BIT},
 {"rtl",			SNES_OP_8BIT},
 {"jmp (0x%04x)",	SNES_OP_24BIT},
@@ -153,7 +154,7 @@ static snes_op_t snes_op[]={
 {"stx 0x%02x",		SNES_OP_16BIT},
 {"sta [0x%02x]",	SNES_OP_16BIT},
 {"dey",			SNES_OP_8BIT},
-{"bit #0x%02x",		SNES_OP_16BIT},
+{"bit",			SNES_OP_IMM},
 {"txa",			SNES_OP_8BIT},
 {"phb",			SNES_OP_8BIT},
 {"sty 0x%04x",		SNES_OP_24BIT},
@@ -176,16 +177,16 @@ static snes_op_t snes_op[]={
 {"sta 0x%04x,x",	SNES_OP_24BIT},
 {"stz 0x%04x,x",	SNES_OP_24BIT},
 {"sta 0x%06x,x",	SNES_OP_32BIT},
-{"ldy #0x%02x",		SNES_OP_16BIT},
+{"ldy",			SNES_OP_IMM},
 {"lda (0x%02x,x)",	SNES_OP_16BIT},
-{"ldx #0x%02x",		SNES_OP_16BIT},
+{"ldx",			SNES_OP_IMM},
 {"lda 0x%02x,s",	SNES_OP_16BIT},
 {"ldy 0x%02x",		SNES_OP_16BIT},
 {"lda 0x%02x",		SNES_OP_16BIT},
 {"ldx 0x%02x",		SNES_OP_16BIT},
 {"lda [0x%02x]",	SNES_OP_16BIT},
 {"tay",			SNES_OP_8BIT},
-{"lda #0x%02x",		SNES_OP_16BIT},
+{"lda",			SNES_OP_IMM},
 {"tax",			SNES_OP_8BIT},
 {"plb",			SNES_OP_8BIT},
 {"ldy 0x%04x",		SNES_OP_24BIT},
@@ -208,7 +209,7 @@ static snes_op_t snes_op[]={
 {"lda 0x%04x,x",	SNES_OP_24BIT},
 {"ldx 0x%04x,y",	SNES_OP_24BIT},
 {"lda 0x%06x,x",	SNES_OP_32BIT},
-{"cpy #0x%02x",		SNES_OP_16BIT},
+{"cpy",			SNES_OP_IMM},
 {"cmp (0x%02x,x)",	SNES_OP_16BIT},
 {"rep #0x%02x",		SNES_OP_16BIT},
 {"cmp 0x%02x,s",	SNES_OP_16BIT},
@@ -217,7 +218,7 @@ static snes_op_t snes_op[]={
 {"dec 0x%02x",		SNES_OP_16BIT},
 {"cmp [0x%02x]",	SNES_OP_16BIT},
 {"iny",			SNES_OP_8BIT},
-{"cmp #0x%02x",		SNES_OP_16BIT},
+{"cmp",			SNES_OP_IMM},
 {"dex",			SNES_OP_8BIT},
 {"wai",			SNES_OP_8BIT},
 {"cpy 0x%04x",		SNES_OP_24BIT},
@@ -240,7 +241,7 @@ static snes_op_t snes_op[]={
 {"cmp 0x%04x,x",	SNES_OP_24BIT},
 {"dec 0x%04x,x",	SNES_OP_24BIT},
 {"cmp 0x%06x,x",	SNES_OP_32BIT},
-{"cpx #0x%02x",		SNES_OP_16BIT},
+{"cpx",			SNES_OP_IMM},
 {"sbc (0x%02x,x)",	SNES_OP_16BIT},
 {"sep #0x%02x",		SNES_OP_16BIT},
 {"sbc 0x%02x,s",	SNES_OP_16BIT},
@@ -249,7 +250,7 @@ static snes_op_t snes_op[]={
 {"inc 0x%02x",		SNES_OP_16BIT},
 {"sbc [0x%02x]",	SNES_OP_16BIT},
 {"inx",			SNES_OP_8BIT},
-{"sbc #0x%02x",		SNES_OP_16BIT},
+{"sbc",			SNES_OP_IMM},
 {"nop",			SNES_OP_8BIT},
 {"swa",			SNES_OP_8BIT},
 {"cpx 0x%04x",		SNES_OP_24BIT},

libr/asm/arch/snes/snesdis.c
@@ -7,29 +7,38 @@
 #include <string.h>
 #include "snes_op_table.h"
 
-static int snesDisass(RAsmOp *op, const ut8 *buf, int len){
-	if (len<snes_op[buf[0]].len)
+static int snesDisass(int bits, RAsmOp *op, const ut8 *buf, int len){
+	snes_op_t *s_op = &snes_op[buf[0]];
+	int op_len = s_op->len;
+	if (op_len == SNES_OP_IMM)
+		op_len = bits == 8 ? SNES_OP_16BIT : SNES_OP_24BIT;
+	if (len < op_len)
 		return 0;
-	switch (snes_op[buf[0]].len) {
-		case SNES_OP_8BIT:
-			sprintf(op->buf_asm,"%s",snes_op[buf[0]].name);
-			break;
-		case SNES_OP_16BIT:
-			sprintf(op->buf_asm,snes_op[buf[0]].name,buf[1]);
-			break;
-		case SNES_OP_24BIT:
-			if(*buf==0x44 || *buf==0x54){
-				sprintf (op->buf_asm, snes_op[buf[0]].name,
-					buf[1], buf[2]);
-			} else {
-				sprintf (op->buf_asm, snes_op[buf[0]].name,
-					buf[1]+0x100*buf[2]);
-			}
-			break;
-		case SNES_OP_32BIT:
-			sprintf (op->buf_asm, snes_op[buf[0]].name,
-				buf[1]+0x100*buf[2]+0x10000*buf[3]);
-			break;
+	switch (s_op->len) {
+	case SNES_OP_8BIT:
+		strcpy (op->buf_asm, s_op->name);
+		break;
+	case SNES_OP_16BIT:
+		sprintf (op->buf_asm, s_op->name, buf[1]);
+		break;
+	case SNES_OP_24BIT:
+		if (*buf == 0x44 || *buf == 0x54) { // mvp and mvn
+			sprintf (op->buf_asm, s_op->name, buf[1], buf[2]);
+		} else {
+			sprintf (op->buf_asm, s_op->name, ut8p_bw(buf+1));
+		}
+		break;
+	case SNES_OP_32BIT:
+		sprintf (op->buf_asm, s_op->name, buf[1]|buf[2]<<8|buf[3]<<16);
+		break;
+	case SNES_OP_IMM:
+		if (bits == 8) {
+			sprintf (op->buf_asm, "%s #0x%02x", s_op->name, buf[1]);
+		} else {
+			sprintf (op->buf_asm, "%s #0x%04x", s_op->name,
+				ut8p_bw(buf+1));
+		}
+		break;
 	}
-	return snes_op[buf[0]].len;
+	return op_len;
 }

libr/asm/arch/6502/6502dis.c
@@ -174,5 +174,5 @@ static int _6502Disass (RAsmOp *op, const ut8 *buf, ut64 len) {
 		}
 	}
 beach:
-	return snesDisass (op, buf, len);
+	return snesDisass (8, op, buf, len);
 }

libr/asm/p/asm_snes.c
@@ -7,7 +7,7 @@
 #include "../arch/snes/snesdis.c"
 
 static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
-	int dlen = snesDisass(op,buf,len);
+	int dlen = snesDisass (a->bits, op, buf, len);
 	if (dlen<0) dlen=0;
 	op->size = dlen;
 	return dlen;

commit 8fb5984fe29b5dddfa128ec65f236dbdfdeb041e

fixing 6502 analysis
36e42b33fe37e922f132e286508bd88ef9fb137d broke analysis by introducing a
new enum value. The analysis code didn't switch on the enum but rather
used it directly as an integer, so since the new SNES_OP_IMM has 5 as
its integer value the analysis code would think that the op was 5 bytes
in length.

libr/anal/p/anal_6502.c
@@ -292,7 +292,7 @@ static int _6502_op(RAnal *anal, RAnalOp *op, ut64 addr, const ut8 *data, int le
 	const int buffsize = sizeof(addrbuf)-1;
 
 	memset (op, '\0', sizeof (RAnalOp));
-	op->size = snes_op[data[0]].len;	//snes-arch is similiar to nes/6502
+	op->size = snes_op_get_size(8, &snes_op[data[0]]);	//snes-arch is similiar to nes/6502
 	op->addr = addr;
 	op->type = R_ANAL_OP_TYPE_UNK;
 	r_strbuf_init (&op->esil);

libr/anal/p/anal_snes.c
@@ -9,7 +9,7 @@
 
 static int snes_anop(RAnal *anal, RAnalOp *op, ut64 addr, const ut8 *data, int len) {
 	memset (op, '\0', sizeof (RAnalOp));
-	op->size = snes_op[data[0]].len;
+	op->size = snes_op_get_size(anal->bits, &snes_op[data[0]]);
 	if (op->size > len)
 		return op->size = 0;
 	op->addr = addr;

libr/asm/arch/snes/snes_op_table.h
@@ -10,11 +10,19 @@ enum {
 	SNES_OP_IMM // 8- or 16-bit immediate depending on X or M flag
 };
 
+
 typedef struct {
 	const char *name;
 	ut8 len;
 } snes_op_t;
 
+static int snes_op_get_size(int bits, snes_op_t* op) {
+	if (op->len == SNES_OP_IMM) {
+		return bits == 8 ? SNES_OP_16BIT : SNES_OP_24BIT;
+	} else {
+		return op->len;
+	}
+}
 
 static snes_op_t snes_op[]={
 {"brk 0x%02x",		SNES_OP_16BIT},

libr/asm/arch/snes/snesdis.c
@@ -9,9 +9,7 @@
 
 static int snesDisass(int bits, RAsmOp *op, const ut8 *buf, int len){
 	snes_op_t *s_op = &snes_op[buf[0]];
-	int op_len = s_op->len;
-	if (op_len == SNES_OP_IMM)
-		op_len = bits == 8 ? SNES_OP_16BIT : SNES_OP_24BIT;
+	int op_len = snes_op_get_size(bits, s_op);
 	if (len < op_len)
 		return 0;
 	switch (s_op->len) {