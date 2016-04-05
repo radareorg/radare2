c834239830e4021d70d5c29a8691bacd3569d908
pancake <pancake@nopcode.org>
2016-04-05 13:30:10 +0200

    Find strings in more sections for PE binaries

--- libr/bin/bin.c
+++ libr/bin/bin.c
@@ -277,8 +277,13 @@ static int is_data_section(RBinFile *a, RBinSection *s) {
 			return true;
 #define X 1
 #define ROW (4 | 2)
+		/* probably too much false positives in here, or thats fine? */
 		if (strstr (o->info->bclass, "PE") && s->srwx & ROW && !(s->srwx & X) && s->size > 0) {
-			if (!strcmp (s->name, ".rdata")) // Maybe other sections are interesting too?
+			if (!strcmp (s->name, ".rsrc"))
+				return true;
+			if (!strcmp (s->name, ".data"))
+				return true;
+			if (!strcmp (s->name, ".rdata"))
 				return true;
 		}
 	}
