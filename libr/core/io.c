3253e1c6254559f02cd46be137144da359d51750
Christiane Ruetten <cr@mozilla.com>
2016-04-05 21:59:38 +0200

    Extending `wo*` commands to use clipboard
    When the [val] argument to wo* subcommands is omitted, clipboard
    content is used instead. This allows, for example, to easily XOR
    two memory blocks.

--- libr/core/io.c
+++ libr/core/io.c
@@ -97,23 +97,41 @@ R_API int r_core_dump(RCore *core, const char *file, ut64 addr, ut64 size, int a
 
 R_API int r_core_write_op(RCore *core, const char *arg, char op) {
 	int i, j, len, ret = false;
-	char *str;
+	char *str = NULL;
 	ut8 *buf;
 
 	// XXX we can work with config.block instead of dupping it
 	buf = (ut8 *)malloc (core->blocksize);
-	str = (char *)malloc (strlen (arg)+1);
-	if (buf == NULL || str == NULL)
+	if (!buf)
 		goto beach;
 	memcpy (buf, core->block, core->blocksize);
+
 	if (op!='e') {
-		len = r_hex_str2bin (arg, (ut8 *)str);
-		if (len < 0) {
-			eprintf ("Invalid hexpair string\n");
-			goto beach;
+		// fill key buffer either from arg or from clipboard
+		if (arg) {  // parse arg for key
+			str = (char *)malloc (strlen (arg));
+			if (!str)
+				goto beach;
+			len = r_hex_str2bin (arg, (ut8 *)str);
+			// r_hex_str2bin() is guaranteed to output maximum half
+			// the input size, thus len <= strlen (arg) / 2.
+			if (len <= 0) {
+				eprintf ("Invalid hexpair string\n");
+				goto beach;
+			}
+		} else {  // use clipboard as key
+			len = core->yank_buf->length;
+			if (len <= 0) {
+				eprintf ("Clipboard is empty and no value argument(s) given\n");
+				goto beach;
+			}
+			str = r_mem_dup (core->yank_buf->buf, len);
+			if (!str)
+				goto beach;
 		}
 	} else len = 0;
 
+	// execute the operand
 	if (op=='e') {
 		int wordsize = 1;
 		char *os, *p, *s = strdup (arg);
