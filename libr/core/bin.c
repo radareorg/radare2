2e06e5159fc3a28c5b9e70cc8daca93f5d582c30
Rakholiya Jenish <rakholiyajenish.07@gmail.com>
2016-04-08 03:44:32 +0200

    Add imq/imj (fixes from @radare)

--- libr/core/bin.c
+++ libr/core/bin.c
@@ -1853,19 +1853,33 @@ static int bin_libs(RCore *r, int mode) {
 	return true;
 }
 
-static void bin_mem_print(RList *mems, int perms, int depth) {
+static void bin_mem_print(RList *mems, int perms, int depth, int mode) {
 	RBinMem *mem;
 	RListIter *iter;
 
 	if (!mems) return;
 
 	r_list_foreach (mems, iter, mem) {
-		if (mem) {
+		if (IS_MODE_JSON (mode)) {
+			r_cons_printf ("{\"name\":\"%s\",\"size\":%d,\"address\":%d,"
+					"\"flags\":\"%s\"}", mem->name, mem->size,
+					mem->addr, r_str_rwx_i (mem->perms & perms));
+		} else if (IS_MODE_SIMPLE (mode)) {
+			r_cons_printf ("0x%08"PFMT64x"\n", mem->addr);
+		} else {
 			r_cons_printf ("0x%08"PFMT64x" +0x%04x %s %*s%-*s\n",
-				mem->addr, mem->size, r_str_rwx_i (mem->perms & perms),
-				depth, "", 20-depth, mem->name);
-			if (mem->mirrors) {
-				bin_mem_print (mem->mirrors, mem->perms & perms, depth + 1);
+					mem->addr, mem->size, r_str_rwx_i (mem->perms & perms),
+					depth, "", 20-depth, mem->name);
+		}
+		if (mem->mirrors) {
+			if (IS_MODE_JSON (mode)) {
+				r_cons_printf (",");
+			}
+			bin_mem_print (mem->mirrors, mem->perms & perms, depth + 1, mode);
+		}
+		if (IS_MODE_JSON(mode)) {
+			if (iter->n) {
+				r_cons_printf (",");
 			}
 		}
 	}
@@ -1874,18 +1888,21 @@ static void bin_mem_print(RList *mems, int perms, int depth) {
 static int bin_mem(RCore *r, int mode) {
 	RList *mem = NULL;
 	if (!r)	return false;
-	if (!(IS_MODE_RAD (mode) || IS_MODE_SET (mode))) {
-		r_cons_printf ("[Memory]\n\n");
+	if (!IS_MODE_JSON(mode)) {
+		if (!(IS_MODE_RAD (mode) || IS_MODE_SET (mode))) {
+			r_cons_printf ("[Memory]\n\n");
+		}
 	}
 	if (!(mem = r_bin_get_mem (r->bin))) {
 		return false;
 	}
 	if (IS_MODE_JSON (mode)) {
-		r_cons_printf ("TODO\n");
-		return false;
-	}
-	if (!(IS_MODE_RAD (mode) || IS_MODE_SET (mode))) {
-		bin_mem_print (mem, 7, 0);
+		r_cons_printf ("[");
+		bin_mem_print (mem, 7, 0, R_CORE_BIN_JSON);
+		r_cons_printf ("]\n");
+		return true;
+	} else if (!(IS_MODE_RAD (mode) || IS_MODE_SET (mode))) {
+		bin_mem_print (mem, 7, 0, mode);
 	}
 	return true;
 }
