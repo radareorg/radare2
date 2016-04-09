d09eaaed33b1da54f0cbc29c425e85dd6d0f8c15
Anton Kochkov <anton.kochkov@gmail.com>
2016-04-09 16:00:36 +0300

    Fix CID 1352383

--- libr/core/cmd_debug.c
+++ libr/core/cmd_debug.c
@@ -1174,6 +1174,7 @@ static void cmd_debug_reg(RCore *core, const char *str) {
 		const ut8 *buf = r_reg_get_bytes (core->dbg->reg, R_REG_TYPE_GPR, &len);
 		//r_print_hexdump (core->print, 0LL, buf, len, 16, 16);
 		r_print_hexdump (core->print, 0LL, buf, len, 32, 4);
+		free (buf);
 		}
 		break;
 	case 'c': // "drc"
