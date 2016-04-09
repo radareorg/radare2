63990bef32a493f0d79c674959e2dba79cf15d60
Álvaro Felipe Melchor <alvaro.felipe91@gmail.com>
2016-04-09 20:05:18 +0200

    Fix #4589

--- libr/bin/p/bin_dex.c
+++ libr/bin/p/bin_dex.c
@@ -165,15 +165,19 @@ static RBinInfo *info(RBinFile *arch) {
 }
 
 static RList* strings (RBinFile *arch) {
-	struct r_bin_dex_obj_t *bin = (struct r_bin_dex_obj_t *) arch->o->bin_obj;
+	struct r_bin_dex_obj_t *bin = NULL;
 	RBinString *ptr = NULL;
 	RList *ret = NULL;
 	int i, len;
 	ut8 buf[6];
+	ut64 off;
 
+	if (!arch || !arch->o)
+		return NULL;
+	bin = (struct r_bin_dex_obj_t *) arch->o->bin_obj;
 	if (!bin || !bin->strings)
 		return NULL;
-	if (bin->header.strings_size>bin->size) {
+	if (bin->header.strings_size > bin->size) {
 		bin->strings = NULL;
 		return NULL;
 	}
@@ -183,12 +187,20 @@ static RList* strings (RBinFile *arch) {
 	for (i = 0; i < bin->header.strings_size; i++) {
 		if (!(ptr = R_NEW0 (RBinString)))
 			break;
+		if (bin->strings[i] > bin->size || bin->strings[i] + 6 > bin->size)
+			goto out_error;
 		r_buf_read_at (bin->b, bin->strings[i], (ut8*)&buf, 6);
 		len = dex_read_uleb128 (buf);
-		if (len>1 && len < R_BIN_SIZEOF_STRINGS) {
+		if (len > 1 && len < R_BIN_SIZEOF_STRINGS) {
 			ptr->string = malloc (len + 1);
-			r_buf_read_at (bin->b, bin->strings[i]+dex_uleb128_len (buf),
-					(ut8*)ptr->string, len);
+			if (!ptr->string) 
+				goto out_error;
+			off = bin->strings[i] + dex_uleb128_len (buf);
+			if (off > bin->size || off + len > bin->size) {
+				free (ptr->string);
+				goto out_error;
+			}	
+			r_buf_read_at (bin->b, off, (ut8*)ptr->string, len);
 			ptr->string[len] = 0;
 			ptr->vaddr = ptr->paddr = bin->strings[i];
 			ptr->size = len;
@@ -201,6 +213,10 @@ static RList* strings (RBinFile *arch) {
 		}
 	}
 	return ret;
+out_error:
+	r_list_free (ret);
+	free (ptr);
+	return NULL;
 }
 
 #if 0
