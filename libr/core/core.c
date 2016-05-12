22d8c75169a122d34b281db182931def63e72e2e
rene <rlaemmert@gmail.com>
2016-05-12 23:36:12 +0200

    changed reallocation size

--- libr/core/core.c
+++ libr/core/core.c
@@ -135,7 +135,7 @@ R_API char* r_core_add_asmqjmp(RCore *core, ut64 addr) {
 		}
 
 		if (core->asmqjmps_count >= core->asmqjmps_size - 2) {
-			core->asmqjmps = realloc (core->asmqjmps, core->asmqjmps_size * sizeof (ut64));
+			core->asmqjmps = realloc (core->asmqjmps, core->asmqjmps_size * 2 * sizeof (ut64));
 			if (!core->asmqjmps) return NULL;
 			core->asmqjmps_size *= 2;
 		}
