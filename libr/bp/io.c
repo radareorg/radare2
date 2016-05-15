f1e910f0fedd64626fad9bf28e35241259e2db60
Joshua J. Drake <github.jdrake@qoop.org>
2016-05-15 15:02:05 -0500

    Fix #4560: Handle recoils well with swstep=true
    When resuming after (during) recoil from a breakpoint, the swstep
    implementation would fail to advance. In short, the debugger would immediately
    interrupt again because r_bp_restore was re-setting the original breakpoint
    just before continue. The following changes fix this issue:
    
    1. Modify r_bp_get_in to stop including the byte after a breakpoint. This was
    causing r_bp_recoil to fail because it thought there was already a breakpoint
    on the next instruction.
    
    2. Pass the real breakpoint address (pc - dbg->bpsize) to r_bp_recoil so
    that r_bp_get_in can work properly. Stop adding the b->size there to keep it
    going too.
    
    3. Add a state flag to core->dbg to track that we are in the midst of a recoil.
    
    4. When continuing from recoil (in r_debug_continue_kill), restore all
    breakpoints except the one we just hit (with the new r_bp_restore_except) to
    avoid hitting it again. Once we move past this instruction, that breakpoint
    will be set again.

--- libr/bp/io.c
+++ libr/bp/io.c
@@ -9,10 +9,21 @@
  * reflect all r_bp stuff in the process using dbg->bp_write or ->breakpoint
  */
 R_API int r_bp_restore(RBreakpoint *bp, int set) {
+	return r_bp_restore_except (bp, set, 0);
+}
+
+/**
+ * reflect all r_bp stuff in the process using dbg->bp_write or ->breakpoint
+ *
+ * except the specified breakpoint...
+ */
+R_API int r_bp_restore_except(RBreakpoint *bp, int set, ut64 addr) {
 	RListIter *iter;
 	RBreakpointItem *b;
 
 	r_list_foreach (bp->bps, iter, b) {
+		if (addr && b->addr == addr)
+			continue;
 		if (bp->breakpoint && bp->breakpoint (b, set, bp->user))
 			continue;
 		/* write obytes from every breakpoint in r_bp if not handled by plugin */
@@ -39,7 +50,7 @@ R_API int r_bp_recoil(RBreakpoint *bp, ut64 addr) {
 		//eprintf("HIT AT ADDR 0x%"PFMT64x"\n", addr);
 		//eprintf("  recoil = %d\n", b->recoil);
 		//eprintf("  size = %d\n", b->size);
-		if (!b->hw && ((b->addr + b->size) == addr))
+		if (!b->hw && b->addr == addr)
 			return b->recoil;
 	}
 	return 0;
