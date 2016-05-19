7401e36e0c128b6ac871aaacbc93195ba0fcc32b
pancake <pancake@nopcode.org>
2016-05-19 04:12:19 +0200

    Update vala tarball

--- sys/vala.sh
+++ sys/vala.sh
@@ -19,11 +19,11 @@ fi
 #valac --help > /dev/null 2>&1
 #if [ ! $? = 0 ]; then
 	# must install from tarball
-	VV=0.13.4
+	VV=0.32.0
 	SV=$(echo ${VV}|cut -d . -f 1,2)
 	if [ ! -d vala-${VV} ]; then
-		wget "http://download.gnome.org/sources/vala/${SV}/vala-${VV}.tar.bz2"
-		tar xjvf vala-${VV}.tar.bz2
+		wget "http://download.gnome.org/sources/vala/${SV}/vala-${VV}.tar.xz"
+		tar xJvf vala-${VV}.tar.xz
 	fi
 	cd vala-${VV} || exit 1
 	./configure --prefix="${PREFIX}" || exit 1
