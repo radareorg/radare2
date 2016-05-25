04671e8db9d3e2a1b1269bd501a6be717ab88133
Sergei Trofimovich <slyfox@gentoo.org>
2016-05-25 20:56:10 +0100

    libr/socket/run.c: fix use of uninitialized value
    Noticed by gcc:
     * QA Notice: Package triggers severe warnings which indicate that it
     *            may exhibit random runtime failures.
     * run.c:65:5: warning: ‘p’ is used uninitialized in this function [-Wuninitialized]
    
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- libr/socket/run.c
+++ libr/socket/run.c
@@ -62,7 +62,7 @@ R_API void r_run_reset(RRunProfile *p) {
 
 R_API int r_run_parse(RRunProfile *pf, const char *profile) {
 	char *p, *o, *str = strdup (profile);
-	if (!p) return 0;
+	if (!str) return 0;
 	for (o = p = str; (o = strchr (p, '\n')); p = o) {
 		*o++ = 0;
 		r_run_parseline (pf, p);
