1f9d61010e70764499d55c9a53ef7ec7bde5588e
skuater <skuater@hotmail.com>
2016-05-29 05:49:32 +0200

    fixed load and implemented kernel write operation

--- libr/io/p/io_r2k.c
+++ libr/io/p/io_r2k.c
@@ -39,7 +39,7 @@ typedef struct _RTL_PROCESS_MODULES
 #define		CLOSE_DRIVER	    	CTL_CODE(FILE_DEVICE_UNKNOWN, 0x803, METHOD_BUFFERED, FILE_READ_ACCESS | FILE_WRITE_ACCESS)
 #define		IOCTL_READ_PHYS_MEM	CTL_CODE(FILE_DEVICE_UNKNOWN, 0x807, METHOD_BUFFERED, FILE_READ_ACCESS | FILE_WRITE_ACCESS)
 #define		IOCTL_READ_KERNEL_MEM	CTL_CODE(FILE_DEVICE_UNKNOWN, 0x804, METHOD_BUFFERED, FILE_READ_ACCESS | FILE_WRITE_ACCESS)
-#define		IOCTL_WRITE_KERNEL_MEM	CTL_CODE(FILE_DEVICE_UNKNOWN, 0x80a, METHOD_BUFFERED, FILE_READ_ACCESS | FILE_WRITE_ACCESS)
+#define		IOCTL_WRITE_KERNEL_MEM	CTL_CODE(FILE_DEVICE_UNKNOWN, 0x805, METHOD_BUFFERED, FILE_READ_ACCESS | FILE_WRITE_ACCESS)
 #define		IOCTL_GET_PHYSADDR	CTL_CODE(FILE_DEVICE_UNKNOWN, 0x809, METHOD_BUFFERED, FILE_READ_ACCESS | FILE_WRITE_ACCESS)
 #define		IOCTL_WRITE_PHYS_MEM	CTL_CODE(FILE_DEVICE_UNKNOWN, 0x808, METHOD_BUFFERED, FILE_READ_ACCESS | FILE_WRITE_ACCESS)
 #define		IOCTL_GET_SYSTEM_MODULES	CTL_CODE(FILE_DEVICE_UNKNOWN, 0x80a, METHOD_BUFFERED, FILE_READ_ACCESS | FILE_WRITE_ACCESS)
@@ -115,13 +115,14 @@ BOOL StartStopService(LPCSTR  lpServiceName, BOOL bStop) {
 }
 BOOL InitDriver(VOID)
 {
-	BOOL	Ret = FALSE;
 	gHandleDriver = CreateFileA(strDeviceName, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_DIRECTORY, 0);
 	if (gHandleDriver != INVALID_HANDLE_VALUE)
-		Ret = TRUE;
-	return(Ret);
+	{
+		return TRUE;
+	}
+	return(FALSE);
 }
-char *GetFileName(const char *path)
+char *GetFileName(unsigned char *path)
 {
 	char *pfile;
 	pfile = path + strlen(path);
@@ -136,22 +137,26 @@ char *GetFileName(const char *path)
 	return pfile;
 }
 static int GetSystemModules(RIO *io) {
-	DWORD ret = -1, bRead = 0;
+	DWORD bRead = 0;
 	int i;
 	LPVOID	lpBufMods = NULL;
 	int bufmodsize = 1024 * 1024;
-	if (!(lpBufMods = malloc(bufmodsize))) {
-		eprintf("[r2k] GetSystemModules: Error cant allocate %i bytes of memory.\n", bufmodsize);
-		return -1;
-	}
-	if (DeviceIoControl(gHandleDriver, IOCTL_GET_SYSTEM_MODULES, lpBufMods, bufmodsize, lpBufMods, bufmodsize, &bRead, NULL)) {
-		PRTL_PROCESS_MODULES pm = (PRTL_PROCESS_MODULES)lpBufMods;
-		PRTL_PROCESS_MODULE_INFORMATION pMod = pm->Modules;
-		for (i = 0; i < pm->NumberOfModules; i++)
-		{
-			//eprintf("%p %x = %-50s \n", pMod[i].ImageBase, pMod[i].ImageBase, GetFileName(pMod[i].FullPathName));
-			io->cb_printf("f nt.%s 0x%x @ 0x%p\n", GetFileName(pMod[i].FullPathName), pMod[i].ImageSize, pMod[i].ImageBase);
+	if(gHandleDriver) {
+		if (!(lpBufMods = malloc(bufmodsize))) {
+			eprintf("[r2k] GetSystemModules: Error cant allocate %i bytes of memory.\n", bufmodsize);
+			return -1;
 		}
+		if (DeviceIoControl(gHandleDriver, IOCTL_GET_SYSTEM_MODULES, lpBufMods, bufmodsize, lpBufMods, bufmodsize, &bRead, NULL)) {
+			PRTL_PROCESS_MODULES pm = (PRTL_PROCESS_MODULES)lpBufMods;
+			PRTL_PROCESS_MODULE_INFORMATION pMod = pm->Modules;
+			for (i = 0; i < pm->NumberOfModules; i++)
+			{
+				//eprintf("%p %x = %-50s \n", pMod[i].ImageBase, pMod[i].ImageBase, GetFileName(pMod[i].FullPathName));
+				io->cb_printf("f nt.%s 0x%x @ 0x%p\n", GetFileName(pMod[i].FullPathName), pMod[i].ImageSize, pMod[i].ImageBase);
+			}
+		}
+	} else {
+		eprintf("Driver not initialized.\n");
 	}
 	return 1;
 }
@@ -161,82 +166,92 @@ static int ReadKernelMemory(ut64 address, ut8 *buf, int len) {
 	int bufsize;
 	PPA p;
 	memset(buf, '\xff', len);
-
-	bufsize = sizeof(PA) + len;
-	if (!(lpBuffer = malloc(bufsize))) {
-		eprintf("[r2k] ReadKernelMemory: Error cant allocate %i bytes of memory.\n", bufsize);
-		return -1;
+	if(gHandleDriver) {
+		bufsize = sizeof(PA) + len;
+		if (!(lpBuffer = malloc(bufsize))) {
+			eprintf("[r2k] ReadKernelMemory: Error cant allocate %i bytes of memory.\n", bufsize);
+			return -1;
+		}
+		p = (PPA)lpBuffer;
+		p->address.QuadPart = address;
+		p->len = len;
+		if (DeviceIoControl(gHandleDriver, IOCTL_READ_KERNEL_MEM, lpBuffer, bufsize, lpBuffer, bufsize, &bRead, NULL)) {
+			memcpy(buf, lpBuffer, len);
+			ret = len;
+		}
+		else {
+			ret = -1;
+			//eprintf("[r2k] ReadKernelMemory: Error IOCTL_READ_KERNEL_MEM.\n");
+		}
+		free(lpBuffer);
+	} else {
+		eprintf("Driver not initialized.\n");
 	}
-	p = (PPA)lpBuffer;
-	p->address.QuadPart = address;
-	p->len = len;
-	if (DeviceIoControl(gHandleDriver, IOCTL_READ_KERNEL_MEM, lpBuffer, bufsize, lpBuffer, bufsize, &bRead, NULL)) {
-		memcpy(buf, lpBuffer, len);
-		ret = len;
-	}
-	else {
-		ret = -1;
-		eprintf("[r2k] ReadKernelMemory: Error IOCTL_READ_KERNEL_MEM.\n");
-	}
-
-	free(lpBuffer);
 	return ret;
 }
 
-static int WriteKernelMemory(ut64 address, ut8 *buf, int len) {
+static int WriteKernelMemory(ut64 address, const ut8 *buf, int len) {
 	DWORD ret = -1, bRead = 0;
 	LPVOID	lpBuffer = NULL;
 	int bufsize;
 	PPA p;
-	bufsize = sizeof(PA) + len;
-	if (!(lpBuffer = malloc(bufsize))) {
-		eprintf("[r2k] WriteKernelMemory: Error cant allocate %i bytes of memory.\n", bufsize);
-		return -1;
+	if(gHandleDriver) {
+		bufsize = sizeof(PA) + len;
+		if (!(lpBuffer = malloc(bufsize))) {
+			eprintf("[r2k] WriteKernelMemory: Error cant allocate %i bytes of memory.\n", bufsize);
+			return -1;
+		}
+		p = (PPA)lpBuffer;
+		p->address.QuadPart = address;
+		p->len = len;
+		memcpy(&p->buffer, buf, len);
+		if (DeviceIoControl(gHandleDriver, IOCTL_WRITE_KERNEL_MEM, lpBuffer, bufsize, lpBuffer, bufsize, &bRead, NULL)) {
+			ret = len;
+		}
+		else {
+			eprintf("[r2k] WriteKernelMemory: Error IOCTL_WRITE_KERNEL_MEM.\n");
+			ret = -1;
+		}
+		free(lpBuffer);
+	} else {
+		eprintf("Driver not initialized.\n");
 	}
-	p = (PPA)lpBuffer;
-	p->address.QuadPart = address;
-	p->len = len;
-	memcpy(&p->buffer, buf, len);
-	if (DeviceIoControl(gHandleDriver, IOCTL_WRITE_KERNEL_MEM, lpBuffer, bufsize, lpBuffer, bufsize, &bRead, NULL)) {
-		ret = len;
-	}
-	else {
-		ret = -1;
-		eprintf("[r2k] WriteKernelMemory: Error IOCTL_WRITE_KERNEL_MEM.\n");
-	}
-	free(lpBuffer);
 	return ret;
 }
 
 static int Init(const char * driverPath) {
 	BOOL ret = FALSE;
-	if (!InitDriver()) {
-
-		StartStopService("r2k64",TRUE);
-		RemoveService("r2k64");
+	if (InitDriver() == FALSE) {
 		if (strlen(driverPath))
 		{
+			StartStopService("r2k",TRUE);
+			RemoveService("r2k");
 			eprintf("Installing driver: %s\n", driverPath);
-			if (InstallService(driverPath, "r2k64", "r2k64")) {
-				StartStopService("r2k64",FALSE);
+			if (InstallService(driverPath, "r2k", "r2k")) {
+				StartStopService("r2k",FALSE);
 				ret = InitDriver();
 			}
+		} else {
+			eprintf("Error initalizating driver, try r2k://pathtodriver\nEx: radare2.exe r2k://c:\\r2k.sys");
+		
 		}
 	}
-	
-	if (ret) {
-		eprintf("Driver present [OK]\n");
-	} 
 	else {
-		eprintf("Driver preset [FAIL]\n");
-	}
+		eprintf("Driver present [OK]\n");
+		ret = TRUE;
+	} 
 	return ret;
 }
 #endif
 
 int r2k__write(RIO *io, RIODesc *fd, const ut8 *buf, int count) {
-	eprintf("writing to: 0x%"PFMT64x" len: %x\n",io->off, count);
+#if __WINDOWS__
+	//eprintf("writing to: 0x%"PFMT64x" len: %x\n",io->off, count);
+	return WriteKernelMemory(io->off, buf, count);
+#else
+	eprintf("TODO: r2k not implemented for this plataform.\n");
 	return -1;
+#endif
 }
 
 static int r2k__read(RIO *io, RIODesc *fd, ut8 *buf, int count) {
@@ -252,6 +267,7 @@ static int r2k__read(RIO *io, RIODesc *fd, ut8 *buf, int count) {
 static int r2k__close(RIODesc *fd) {
 	if(gHandleDriver) {
 		CloseHandle(gHandleDriver);
+		StartStopService("r2k",TRUE);
 	}
 	return 0;
 }
@@ -280,7 +296,7 @@ static int r2k__system(RIO *io, RIODesc *fd, const char *cmd) {
 static RIODesc *r2k__open(RIO *io, const char *pathname, int rw, int mode) {
 	if (!strncmp (pathname, "r2k://", 6)) {
 		RIOW32 *w32 = R_NEW0 (RIOW32);
-		if(!Init(&pathname[6])) {
+		if(Init(&pathname[6]) == FALSE) {
 			eprintf("r2k__open: Error cant init driver: %s\n", &pathname[6]);
 		}
 		return r_io_desc_new(&r_io_plugin_r2k, -1, pathname, rw, mode, w32);
