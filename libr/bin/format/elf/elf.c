e8b8831b834e7089ebe02c4d7ce282d9c7a65c16
pancake <pancake@nopcode.org>
2016-06-06 23:18:21 +0200

    Fix elf-ppc endian bit

--- libr/bin/format/elf/elf.c
+++ libr/bin/format/elf/elf.c
@@ -1632,9 +1632,6 @@ ut8 *Elf_(r_bin_elf_grab_regstate)(struct Elf_(r_bin_elf_obj_t) *bin, int *len) 
 }
 
 int Elf_(r_bin_elf_is_big_endian)(struct Elf_(r_bin_elf_obj_t) *bin) {
-	if (bin->ehdr.e_machine == EM_PPC) {
-		return false;
-	}
 	return (bin->ehdr.e_ident[EI_DATA] == ELFDATA2MSB);
 }
 
