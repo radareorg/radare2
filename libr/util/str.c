a1bdabf787398b1bd0b3a653d4a7d1bfade8508f
Jeffrey Crowell <crowell@bu.edu>
2016-06-08 01:48:43 -0400

    fix offbyone in r_str_bits64
    manifested in odd numbers having leading 1

--- libr/util/str.c
+++ libr/util/str.c
@@ -122,7 +122,7 @@ R_API int r_str_bits(char *strout, const ut8 *buf, int len, const char *bitz) {
 R_API int r_str_bits64(char* strout, ut64 in) {
 	int i, bit, count = 0;
 	count = 0;
-	for (i = sizeof (in) * 8; i >= 0; --i) {
+	for (i = (sizeof (in) * 8) - 1; i >= 0; --i) {
 		bit = in >> i;
 		if (bit & 1) {
 			strout[count] = '1';
