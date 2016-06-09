21d554846d19a613f57b8af3a6c5f1764e538a76
Álvaro Felipe Melchor <alvaro.felipe91@gmail.com>
2016-06-09 18:27:55 +0200

    fix mem leak java code

--- shlr/java/class.c
+++ shlr/java/class.c
@@ -3793,7 +3793,10 @@ R_API RBinJavaAttrInfo* r_bin_java_source_code_file_attr_new (ut8 *buffer, ut64 
 	ut64 offset = 0;
 	RBinJavaAttrInfo* attr = r_bin_java_default_attr_new (buffer, sz, buf_offset);
 	offset += 6;
-	if (!attr || !sz) return NULL;
+	if (!attr || !sz) {
+	    	free (attr);
+	    	return NULL;
+	}
 	attr->type = R_BIN_JAVA_ATTR_TYPE_SOURCE_FILE_ATTR;
 	//if (buffer + offset > buffer + sz) return NULL;
 	attr->info.source_file_attr.sourcefile_idx = R_BIN_JAVA_USHORT (buffer, offset);
