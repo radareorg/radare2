1117f5e19c1df1dda588adacfaad2a1c0d1eda71
Maijin <maijin21@gmail.com>
2016-06-19 12:30:30 +0200

    Fix #3874

--- libr/core/core.c
+++ libr/core/core.c
@@ -1321,6 +1321,7 @@ R_API int r_core_init(RCore *core) {
 	r_anal_noreturn_add (core->anal, "sym.abort", UT64_MAX);
 	r_anal_noreturn_add (core->anal, "abort", UT64_MAX);
 	r_anal_noreturn_add (core->anal, "sym.exit", UT64_MAX);
+	r_anal_noreturn_add (core->anal, "sym._exit", UT64_MAX);
 	r_anal_noreturn_add (core->anal, "sym.imp.__libc_init", UT64_MAX); /* mips */
 	/* windows */
 	r_anal_noreturn_add (core->anal, "sym.imp.kernel32.dll_ExitProcess", UT64_MAX);
