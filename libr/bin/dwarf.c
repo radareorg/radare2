97b6bdbc40fb06401ff11e1d86db037560e20cb6
Álvaro Felipe Melchor <alvaro.felipe91@gmail.com>
2016-06-26 23:44:14 +0200

    Fix null dereference introduced before

--- libr/bin/dwarf.c
+++ libr/bin/dwarf.c
@@ -1591,15 +1591,14 @@ R_API RBinDwarfDebugAbbrev *r_bin_dwarf_parse_abbrev(RBin *a, int mode) {
 	RBinSection *section = getsection (a, "debug_abbrev");
 	RBinDwarfDebugAbbrev *da = NULL;
 	RBinFile *binfile = a ? a->cur: NULL;
-	if (section->size > a->cur->size) {
+	if (!section || !binfile) return NULL;
+	if (section->size > binfile->size) {
 		return NULL;
 	}
-	if (binfile && section) {
-		len = section->size;
-		buf = calloc (1,len);
-		r_buf_read_at (binfile->buf, section->paddr, buf, len);
-		da = r_bin_dwarf_parse_abbrev_raw (buf, len, mode);
-		free (buf);
-	}
+	len = section->size;
+	buf = calloc (1,len);
+	r_buf_read_at (binfile->buf, section->paddr, buf, len);
+	da = r_bin_dwarf_parse_abbrev_raw (buf, len, mode);
+	free (buf);
 	return da;
 }
