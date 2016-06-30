35f81786327d995c90e3c3459d05bfbe69643d1e
Anton Kochkov <anton.kochkov@gmail.com>
2016-06-30 21:17:31 +0300

    Fix Android build

--- libr/debug/p/debug_native.c
+++ libr/debug/p/debug_native.c
@@ -67,6 +67,9 @@ static int r_debug_native_reg_write (RDebug *dbg, int type, const ut8* buf, int 
 #elif __linux__
 #include "native/linux/linux_debug.h"
 #include "native/procfs.h"
+# ifdef __ANDROID__
+#  define WAIT_ANY -1
+# endif
 #if __x86_64__
 #include "native/linux/linux_coredump.h"
 #endif
