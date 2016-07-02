168cb35f1a3224e6487b54a16b8e0d25706e77c1
Bobby Smiles <bobby.smiles32@gmail.com>
2016-07-02 16:31:06 +0200

    Add basic support for N64 RSP processor.
    This includes:
    * a table driven instruction decoder (rsp_idec)
    * a disassembler
    * a very primitive anal plugin

--- libr/include/r_asm.h
+++ libr/include/r_asm.h
@@ -234,6 +234,7 @@ extern RAsmPlugin r_asm_plugin_lanai_gnu;
 extern RAsmPlugin r_asm_plugin_xtensa;
 extern RAsmPlugin r_asm_plugin_tricore;
 extern RAsmPlugin r_asm_plugin_pic18c;
+extern RAsmPlugin r_asm_plugin_rsp;
 #endif
 
 #ifdef __cplusplus
