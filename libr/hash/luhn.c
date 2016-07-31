e28d14ba86bd48f451411fabb7188811db38567a
Moritz Eckert <mo@mightym0.de>
2016-07-31 15:11:18 +0200

    ensure non c99 compatibility

--- libr/hash/luhn.c
+++ libr/hash/luhn.c
@@ -4,12 +4,14 @@
 #include <r_util.h>
 
 R_API ut64 r_hash_luhn(const ut8 *buf, ut64 len) {
+	int i;
+	int curDigit;
 	ut64 sum = 0;
 	int parity = (len-1) % 2;
 	char curChar[2] = "\0"; 
-	for (int i = len; i > 0 ; i--) {
+	for (i = len; i > 0 ; i--) {
 		curChar[0] = buf[i-1];
-		int curDigit = atoi(curChar);
+		curDigit = atoi(curChar);
 
 		if (parity == i % 2) {
 			curDigit = curDigit * 2;
