21137515732a22745e55f038f8d1b91b65a6cdd3
pancake <pancake@nopcode.org>
2016-08-05 14:41:44 +0200

    Fix some false positives in aad using the API instead of r2cmd

--- libr/core/cmd_anal.c
+++ libr/core/cmd_anal.c
@@ -4172,7 +4172,9 @@ static void cmd_anal_aad(RCore *core, const char *input) {
 	RList *list = r_list_newf (NULL);
 	r_anal_xrefs_from (core->anal, list, "xref", R_ANAL_REF_TYPE_DATA, UT64_MAX);
 	r_list_foreach (list, iter, ref) {
-		r_core_cmdf (core, "af @ 0x%"PFMT64x, ref->addr);
+		if (r_io_is_valid_offset (core->io, ref->addr, false)) {
+			r_core_anal_fcn (core, ref->at, ref->addr, R_ANAL_REF_TYPE_NULL, 1);
+		}
 	}
 	r_list_free (list);
 }
