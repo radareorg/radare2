e1ed47a05726d0798b54821b7e5a0de9dcec0682
Alexandru Razvan Caciulescu <alexandru.razvan.c@gmail.com>
2016-08-11 16:06:31 +0200

    Adding Arithmetic Pattern classification for /R (work in progress)

--- libr/core/cmd_search_rop.c
+++ libr/core/cmd_search_rop.c
@@ -52,9 +52,138 @@ static RList* get_constants (const char *str) {
 	return list;
 }
 
+static bool isFlag (RRegItem *reg) {
+	const char *type = r_reg_get_type (reg->type);
+
+	if (!strcmp (type, "flg"))
+		return true;
+	return false;
+}
+
+// binary op
+static bool simulate_op (const char *op, ut64 src1, ut64 src2, ut64 old_src1, ut64 old_src2, ut64 *result) {
+	if (!strcmp (op, "^")) {
+		*result =  src1 ^ src2;
+		return true;
+	}
+	if (!strcmp (op, "+")) {
+		*result = src1 + src2;
+		return true;
+	}
+	if (!strcmp (op, "-")) {
+		*result = src1 - src2;
+		return true;
+	}
+	if (!strcmp (op, "*")) {
+		*result = src1 * src2;
+		return true;
+	}
+	if (!strcmp (op, "|")) {
+		*result = src1 | src2;
+		return true;
+	}
+	if (!strcmp (op, "/")) {
+		*result = src1 / src2;
+		return true;
+	}
+	if (!strcmp (op, "%")) {
+		*result = src1 % src2;
+		return true;
+	}
+	if (!strcmp (op, "<<")) {
+		*result = src1 << src2;
+		return true;
+	}
+	if (!strcmp (op, ">>")) {
+		*result = src1 >> src2;
+		return true;
+	}
+	if (!strcmp (op, "&")) {
+		*result = src1 & src2;
+		return true;
+	}
+	if (!strcmp (op, "+=")) {
+		*result = old_src1 + src2;
+		return true;
+	}
+	if (!strcmp (op, "-=")) {
+		*result = old_src1 - src2;
+		return true;
+	}
+	if (!strcmp (op, "*=")) {
+		*result = old_src1 * src2;
+		return true;
+	}
+	if (!strcmp (op, "/=")) {
+		*result = old_src1 / src2;
+		return true;
+	}
+	if (!strcmp (op, "%=")) {
+		*result = old_src1 % src2;
+		return true;
+	}
+	if (!strcmp (op, "<<")) {
+		*result = src1 << src2;
+		return true;
+	}
+	if (!strcmp (op, ">>")) {
+		*result = src1 >> src2;
+		return true;
+	}
+	if (!strcmp (op, "&=")) {
+		*result = src1 & src2;
+		return true;
+	}
+	if (!strcmp (op, "^=")) {
+		*result = src1 ^ src2;
+		return true;
+	}
+	if (!strcmp (op, "|=")) {
+		*result = src1 | src2;
+		return true;
+	}
+	return false;
+}
+
+// fill REGs with rwx addr from MEM
+static void fillRegisterValues (RCore *core) {
+	RIOSection *sec;
+	RListIter *iter_io, *iter_reg;
+	RList *regs;
+	RRegItem *reg_item;
+	ut64 addr;
+
+	regs = r_reg_get_list (core->dbg->reg, 0);
+	if (!regs) {
+		return;
+	}
+
+	iter_reg = regs->head;
+	reg_item = iter_reg->data;
+	if (!(iter_reg && reg_item)) {
+		return;
+	}
+	r_reg_arena_pop (core->dbg->reg);
+	r_list_foreach (core->io->sections, iter_io, sec) {
+		if (sec->rwx & 1) {
+			addr = sec->vaddr;
+			// r_cons_printf ("RWX addr = 0x%"PFMT64x"\n", addr);
+			// r_reg_arena_pop (core->dbg->reg);
+			r_reg_set_value (core->dbg->reg, reg_item, addr);
+			// r_reg_arena_push (core->dbg->reg);
+			iter_reg = iter_reg->n;
+			reg_item = iter_reg->data;
+			if (!(iter_reg && reg_item)) {
+				break;
+			}
+		}
+	}
+	r_reg_arena_push (core->dbg->reg);
+}
+
 static char* rop_classify_constant (RCore *core, RList *ropList) {
 	char *esil_str, *constant;
-	char *mov = NULL;
+	char *ct = NULL;
 	RListIter *iter_src, *iter_r, *iter_dst, *iter_const;
 	RRegItem *item_dst;
 	RList *head, *constants;
@@ -69,10 +198,6 @@ static char* rop_classify_constant (RCore *core, RList *ropList) {
 	}
 
 	r_list_foreach (ropList, iter_r, esil_str) {
-		if (strchr (esil_str, '[')) { // avoid MEM read/write for now
-			return NULL;
-		}
-
 		constants = get_constants (esil_str);
 		// if there are no constants in the instruction continue
 		if (!constants || !constants->head) {
@@ -80,6 +205,7 @@ static char* rop_classify_constant (RCore *core, RList *ropList) {
 		}
 
 		// init regs with random values
+		// fillRegisterValues (core);
 		head = r_reg_get_list (core->dbg->reg, 0);
 		if (!head) {
 			return NULL;
@@ -127,9 +253,8 @@ static char* rop_classify_constant (RCore *core, RList *ropList) {
 
 			if (value_dst != diff_dst) {
 				r_list_foreach (constants, iter_const, constant) {
-					// if (value_dst == strtol (constant, NULL, 0)) {
 					if (value_dst == r_num_get (NULL, constant)) {
-						mov = r_str_concatf (mov, "%s <-- 0x%"PFMT64x";", item_dst->name, value_dst);
+						ct = r_str_concatf (ct, "%s <-- 0x%"PFMT64x";", item_dst->name, value_dst);
 					}
 				}
 			}
@@ -137,7 +262,7 @@ static char* rop_classify_constant (RCore *core, RList *ropList) {
 		free (out);
 	}
 
-	return mov;
+	return ct;
 }
 
 static char* rop_classify_mov (RCore *core, RList *ropList) {
@@ -157,11 +282,8 @@ static char* rop_classify_mov (RCore *core, RList *ropList) {
 	}
 
 	r_list_foreach (ropList, iter_r, esil_str) {
-		if (strchr (esil_str, '[')) { // avoid MEM read/write for now
-			return NULL;
-		}
-
 		// init regs with random values
+		// fillRegisterValues (core);
 		head = r_reg_get_list (core->dbg->reg, 0);
 		if (!head) {
 			return NULL;
@@ -200,12 +322,17 @@ static char* rop_classify_mov (RCore *core, RList *ropList) {
 				continue;
 			}
 
+			// you never mov into flags
+			if (isFlag (item_dst)) {
+				continue;
+			}
+
 			value_dst = r_reg_get_value (core->dbg->reg, item_dst);
 			r_reg_arena_swap (core->dbg->reg, false);
 			diff_dst = r_reg_get_value (core->dbg->reg, item_dst);
 			r_reg_arena_swap (core->dbg->reg, false);
 			//restore initial value
-			r_reg_set_value (core->dbg->reg, item_dst, diff_dst);
+			// r_reg_set_value (core->dbg->reg, item_dst, diff_dst);
 
 			r_list_foreach (head, iter_src, item_src) {
 				ut64 diff_src, value_src;
@@ -214,6 +341,12 @@ static char* rop_classify_mov (RCore *core, RList *ropList) {
 					continue;
 				}
 				if (item_src == item_dst) {
+					continue;
+				}
+
+				// you never mov from flags
+				// if (isFlag (item_src->name, core->dbg->reg)) {
+				if (isFlag (item_src)) {
 					continue;
 				}
 
@@ -233,6 +366,140 @@ static char* rop_classify_mov (RCore *core, RList *ropList) {
 	}
 
 	return mov;
+}
+
+static char* rop_classify_arithmetic (RCore *core, RList *ropList) {
+	char *esil_str, *op;
+	char *arithmetic = NULL, *arithmetic_f = NULL;
+	RListIter *iter_src1, *iter_src2, *iter_r, *iter_dst, *iter_ops;
+	RRegItem *item_src1, *item_src2, *item_dst;
+	RList *head;
+	RList *ops_list = NULL, *flg_read = NULL, *flg_write = NULL, *reg_read = NULL,
+		*reg_write = NULL, *mem_read = NULL, *mem_write = NULL;
+	const bool romem = r_config_get_i (core->config, "esil.romem");
+	const bool stats = r_config_get_i (core->config, "esil.stats");
+	ut64 *op_result = malloc (sizeof (*op_result));
+	ut64 *op_result_r = malloc (sizeof (*op_result_r));
+
+	if (!romem || !stats) {
+		// eprintf ("Error: esil.romem and esil.stats must be set TRUE");
+		return NULL;
+	}
+
+	r_list_foreach (ropList, iter_r, esil_str) {
+		// init regs with random values
+		// fillRegisterValues (core);
+		head = r_reg_get_list (core->dbg->reg, 0);
+		if (!head) {
+			return NULL;
+		}
+		r_list_foreach (head, iter_dst, item_dst) {
+			int r = r_num_rand (30000);
+			r_reg_arena_pop (core->dbg->reg);
+			r_reg_set_value (core->dbg->reg, item_dst, r);
+			r_reg_arena_push (core->dbg->reg);
+		}
+
+		// r_cons_printf ("Emulating arithmetic pattern:%s\n", esil_str);
+		cmd_anal_esil (core, esil_str);
+		char *out = sdb_querys (core->anal->esil->stats, NULL, 0, "*");
+		// r_cons_println (out);
+		if (out) {
+			ops_list = parse_list (strstr (out, "ops.list"));
+			flg_read = parse_list (strstr (out, "flg.read"));
+			flg_write = parse_list (strstr (out, "flg.write"));
+			reg_read = parse_list (strstr (out, "reg.read"));
+			reg_write = parse_list (strstr (out, "reg.write"));
+			mem_read = parse_list (strstr (out, "mem.read"));
+			mem_write = parse_list (strstr (out, "mem.write"));
+		}
+
+		if (!ops_list) {
+			return NULL;
+		}
+
+		r_list_foreach (ops_list, iter_ops, op) {
+			r_list_foreach (head, iter_src1, item_src1) {
+				ut64 value_src1, diff_src1;
+
+				value_src1 = r_reg_get_value (core->dbg->reg, item_src1);
+				r_reg_arena_swap (core->dbg->reg, false);
+				diff_src1 = r_reg_get_value (core->dbg->reg, item_src1);
+				r_reg_arena_swap (core->dbg->reg, false);
+
+				if (!r_list_find (reg_read, item_src1->name, (RListComparator)strcmp)) {
+					continue;
+				}
+
+				r_list_foreach (head, iter_src2, item_src2) {
+					ut64 value_src2, diff_src2;
+
+					value_src2 = r_reg_get_value (core->dbg->reg, item_src2);
+					r_reg_arena_swap (core->dbg->reg, false);
+					diff_src2 = r_reg_get_value (core->dbg->reg, item_src2);
+					r_reg_arena_swap (core->dbg->reg, false);
+
+					if (!r_list_find (reg_read, item_src2->name, (RListComparator)strcmp)) {
+						continue;
+					}
+					// TODO check condition
+					if (iter_src1 == iter_src2) {
+						continue;
+					}
+
+					r_list_foreach (head, iter_dst, item_dst) {
+						ut64 value_dst, diff_dst;
+						bool redundant = false, simulate, simulate_r;
+
+						value_dst = r_reg_get_value (core->dbg->reg, item_dst);
+						r_reg_arena_swap (core->dbg->reg, false);
+						diff_dst = r_reg_get_value (core->dbg->reg, item_dst);
+						r_reg_arena_swap (core->dbg->reg, false);
+
+						if (!r_list_find (reg_write, item_dst->name, (RListComparator)strcmp)) {
+							continue;
+						}
+
+						// dont check flags for arithmetic
+						// if (isFlag (item_dst->name, core->dbg->reg)) {
+						if (isFlag (item_dst)) {
+							continue;
+						}
+
+						// r_cons_printf ("Checking %s = %s %s %s\n", item_dst->name, item_src1->name, op, item_src2->name);
+						// r_cons_printf ("Current values: %s = %llu; %s = %llu; %s = %llu, old_%s = %llu old_%s = %llu\n", item_dst->name, value_dst, item_src1->name, value_src1, item_src2->name, value_src2, item_src1->name, diff_src1, item_src2->name, diff_src2);
+						simulate = simulate_op (op, value_src1, value_src2, diff_src1, diff_src2, op_result);
+						simulate_r = simulate_op (op, value_src2, value_src1, diff_src2, diff_src1, op_result_r);
+						if (value_src1 != 0 && value_src2 != 0 && simulate && value_dst == *op_result) {
+							// r_cons_println ("Debug: FOUND ONE !");
+							char *tmp = r_str_newf ("%s <-- %s %s %s;", item_dst->name, item_src1->name, op, item_src2->name);
+							if (arithmetic && !strstr (arithmetic, tmp)) {
+								arithmetic = r_str_concat (arithmetic, tmp);
+							} else if (!arithmetic) {
+								arithmetic = r_str_concat (arithmetic, tmp);
+							}
+							free (tmp);
+							redundant = true;
+						} else if (!redundant && value_src1 != 0 && value_src2 != 0 && simulate_r && value_dst == *op_result_r) {
+							// r_cons_println ("Debug: FOUND ONE reversed!");
+							char *tmp = r_str_newf ("%s <-- %s %s %s;", item_dst->name, item_src2->name, op, item_src1->name);
+							if (arithmetic && !strstr (arithmetic, tmp)) {
+								arithmetic = r_str_concat (arithmetic, tmp);
+							} else if (!arithmetic) {
+								arithmetic = r_str_concat (arithmetic, tmp);
+							}
+							free (tmp);
+						}
+					}
+				}
+			}
+		}
+		free (out);
+	}
+	free (op_result);
+	free (op_result_r);
+
+	return arithmetic;
 }
 
 static int rop_classify_nops (RCore *core, RList *ropList) {
@@ -257,9 +524,11 @@ static int rop_classify_nops (RCore *core, RList *ropList) {
 	// RReg *hack = core->dbg->reg;
 	// core->dbg->reg = core->anal->reg;
 	r_list_foreach (ropList, iter_r, esil_str) {
-		if (strchr (esil_str, '[')) { // avoid MEM read/write for now
-			return -1;
-		}
+		// if (strchr (esil_str, '[')) { // avoid MEM read/write for now
+		//  return -1;
+		// }
+
+		fillRegisterValues (core);
 
 		// r_cons_printf ("Emulating:%s\n", esil_str);
 		cmd_anal_esil (core, esil_str);
@@ -274,11 +543,10 @@ static int rop_classify_nops (RCore *core, RList *ropList) {
 			mem_read = parse_list (strstr (out, "mem.read"));
 			mem_write = parse_list (strstr (out, "mem.write"));
 		}
-		// will work once we do MEM check
-		// else {
+		else {
 			// directly say NOP
-			// return changes;
-		// }
+			continue;
+		}
 
 		head = r_reg_get_list (core->dbg->reg, 0);
 		if (!head) {
@@ -293,7 +561,7 @@ static int rop_classify_nops (RCore *core, RList *ropList) {
 			r_reg_arena_swap (core->dbg->reg, false);
 			delta = value - diff;
 			//restore initial value
-			r_reg_set_value (core->dbg->reg, item, diff);
+			// r_reg_set_value (core->dbg->reg, item, diff);
 
 			if (delta != 0) {
 				// r_cons_printf ("REG changed: %s ( %d --> %d) \n", item->name, diff, value);
@@ -307,24 +575,31 @@ static int rop_classify_nops (RCore *core, RList *ropList) {
 	return changes;
 }
 
-R_API void rop_classify (RCore *core, Sdb *db, RList *ropList, const char *key, unsigned int size) {
+static void rop_classify (RCore *core, Sdb *db, RList *ropList, const char *key, unsigned int size) {
 	int nop = rop_classify_nops (core, ropList);
-	char *mov;
+	char *mov  = rop_classify_mov (core, ropList);
+	char *ct  = rop_classify_constant (core, ropList);
+	char *arithm  = rop_classify_arithmetic (core, ropList);
+	char *str = r_str_newf ("0x%"PFMT64x, size);
+
 	if (nop == 1) {
-		char *str = r_str_newf ("0x%"PFMT64x"  -->  NOP", size);
-		sdb_set (db, key, str, 0);
-	} else if (nop == -1) {
-		char *str = r_str_newf ("0x%"PFMT64x"  ------>  MEM access", size);
-		sdb_set (db, key, str, 0);
-	} else if (mov = rop_classify_mov (core, ropList)) {
-		char *str = r_str_newf ("0x%"PFMT64x"  ---->  MOV { %s }", size, mov);
-		free (mov);
-		sdb_set (db, key, str, 0);
-	} else if (mov = rop_classify_constant (core, ropList)) {
-		char *str = r_str_newf ("0x%"PFMT64x"  ---->  LOAD CONST { %s }", size, mov);
-		free (mov);
+		str = r_str_concat (str, "  -->  NOP");
 		sdb_set (db, key, str, 0);
 	} else {
-		sdb_num_set (db, key, size, 0);
+		if (mov) {
+			str = r_str_concatf (str, " MOV { %s }", mov);
+			free (mov);
+		}
+		if (ct) {
+			str = r_str_concatf (str, " LOAD CONST { %s }", ct);
+			free (ct);
+		}
+		if (arithm) {
+			str = r_str_concatf (str, " ARITHMETIC { %s }", arithm);
+			free (arithm);
+		}
 	}
+
+	sdb_set (db, key, str, 0);
+	free (str);
 }
