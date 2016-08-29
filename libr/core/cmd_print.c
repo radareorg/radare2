530ee2d06fdd1a8fce2ff182b096ffc45bcc3cee
Riccardo Schirone <ret2libc@users.noreply.github.com>
2016-08-29 12:29:30 +0200

    bin: fix elf program header name INERP -> INTERP
    core/cmd_print: update pf help message

--- libr/core/cmd_print.c
+++ libr/core/cmd_print.c
@@ -361,7 +361,7 @@ static void print_format_help_help_help(RCore *core) {
 	"pf", " 0iwq foo bar troll", "Same as above, but considered as a union (all fields at offset 0)",
 	"pf.", "plop ? (troll)mystruct", "Use structure troll previously defined",
 	"pf", " 10xiz pointer length string", "Print a size 10 array of the xiz struct with its field names",
-	"pf", " {integer}bifc", "Print integer times the following format (bifc)",
+	"pf", " {integer}? (bifc)", "Print integer times the following format (bifc)",
 	"pf", " [4]w[7]i", "Print an array of 4 words and then an array of 7 integers",
 	"pf", " ic...?i foo bar \"(pf xw yo foo)troll\" yo", "Print nested anonymous structres",
 	"pf", "n2", "print signed short (2 bytes) value. Use N insted of n for printing unsigned values",
