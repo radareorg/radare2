d8baf62e688f94500b7fd317ddc1325d465bc709
Maijin <maijin21@gmail.com>
2016-09-02 16:23:29 +0200

    Update anal.c

--- libr/core/anal.c
+++ libr/core/anal.c
@@ -244,9 +244,9 @@ static bool blacklisted_word(char* name) {
 	const char * list[] = {"__stack_chk_guard", "__stderrp", "__stdinp", "__stdoutp", 
 	"_DefaultRuneLocale"};
 	int i;
-	for (i = 0; i < sizeof(list)/sizeof(list[0]); i++) {
-        	if (strstr(name, list[i]) != NULL) return true;
-    	}
+	for (i = 0; i < sizeof (list) / sizeof (list[0]); i++) {
+        	if (strstr (name, list[i])) { return true; }
+	}
 	return false;
 }
 
