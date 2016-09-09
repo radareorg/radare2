1cdeb062b19457cb243e854861e614c196b8dd08
h4ng3r <h4ng3r@users.noreply.github.com>
2016-09-09 18:00:37 +0200

    fix pancake comments

--- libr/bin/p/bin_dex.c
+++ libr/bin/p/bin_dex.c
@@ -264,23 +264,23 @@ static int check_bytes(const ut8 *buf, ut64 length) {
 		return false;
 	// Non-extended opcode dex file
 	if (!memcmp (buf, "dex\n035\0", 8)) {
-			return true;
+		return true;
 	}
 	// Extended (jumnbo) opcode dex file, ICS+ only (sdk level 14+)
 	if (!memcmp (buf, "dex\n036\0", 8)) {
-			return true;
+		return true;
 	}
 	// M3 (Nov-Dec 07)
 	if (!memcmp (buf, "dex\n009\0", 8)) {
-			return true;
+		return true;
 	}
 	// M5 (Feb-Mar 08)
-		if (!memcmp (buf, "dex\n009\0", 8)) {
-			return true;
+	if (!memcmp (buf, "dex\n009\0", 8)) {
+		return true;
 	}
 	// Default fall through, should still be a dex file
 	if (!memcmp (buf, "dex\n", 4)) {
-				return true;
+		return true;
 	}
 	return false;
 }
