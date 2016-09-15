4f6b64a9846f62da04cb1d0add40b48cbe62a0a0
pancake <pancake@nopcode.org>
2016-09-15 16:21:44 +0200

    Some code cleanup/review for rasm2 + arc

--- libr/asm/p/asm_arc.c
+++ libr/asm/p/asm_arc.c
@@ -1,4 +1,4 @@
-/* radare - LGPL - Copyright 2012-2013 - pancake */
+/* radare - LGPL - Copyright 2012-2016 - pancake */
 
 #include <stdio.h>
 #include <stdarg.h>
@@ -10,16 +10,26 @@
 #include "dis-asm.h"
 #include <mybfd.h>
 
-static unsigned long Offset = 0;
+/* extern */
+int decodeInstr (bfd_vma address, disassemble_info * info);
+int ARCTangent_decodeInstr (bfd_vma address, disassemble_info * info);
+int ARCompact_decodeInstr (bfd_vma address, disassemble_info * info);
+
+/* ugly globals */
+static ut32 Offset = 0;
 static char *buf_global = NULL;
 static int buf_len = 0;
-static unsigned char bytes[32];
+static ut8 bytes[32] = {0};
 
 static int arc_buffer_read_memory (bfd_vma memaddr, bfd_byte *myaddr, unsigned int length, struct disassemble_info *info) {
 	int delta = (memaddr - Offset);
-	if (delta<0) return -1; // disable backward reads
-	if ((delta+length)>sizeof (bytes)) return -1;
-	memcpy (myaddr, bytes+delta, R_MIN (buf_len, length));
+	if (delta < 0) {
+		return -1; // disable backward reads
+	}
+	if ((delta + length) > sizeof (bytes)) {
+		return -1;
+	}
+	memcpy (myaddr, bytes + delta, R_MIN (buf_len - delta, length));
 	return 0;
 }
 
@@ -32,18 +42,19 @@ static void memory_error_func(int status, bfd_vma memaddr, struct disassemble_in
 }
 
 static void print_address(bfd_vma address, struct disassemble_info *info) {
-	char tmp[32];
-	if (buf_global == NULL)
-		return;
-	sprintf (tmp, "0x%08"PFMT64x"", (ut64)address);
-	strcat (buf_global, tmp);
+	char tmp[64];
+	if (buf_global) {
+		sprintf (tmp, "0x%08"PFMT64x"", (ut64)address);
+		strcat (buf_global, tmp);
+	}
 }
 
 static int buf_fprintf(void *stream, const char *format, ...) {
 	va_list ap;
 	char tmp[1024];
-	if (buf_global == NULL)
+	if (!buf_global) {
 		return 0;
+	}
 	va_start (ap, format);
 	vsnprintf (tmp, sizeof (tmp), format, ap);
 	strcat (buf_global, tmp);
@@ -51,17 +62,16 @@ static int buf_fprintf(void *stream, const char *format, ...) {
 	return 0;
 }
 
-int decodeInstr (bfd_vma address, disassemble_info * info);
-int ARCTangent_decodeInstr (bfd_vma address, disassemble_info * info);
-int ARCompact_decodeInstr (bfd_vma address, disassemble_info * info);
-
 static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 	static struct disassemble_info disasm_obj;
-	if (len<2) return -1;
+	if (len < 2) {
+		return -1;
+	}
 	buf_global = op->buf_asm;
 	Offset = a->pc;
-	if (len>sizeof (bytes))
+	if (len > sizeof (bytes)) {
 		len = sizeof (bytes);
+	}
 	memcpy (bytes, buf, len); // TODO handle compact
 	buf_len = len;
 	/* prepare disassembler */
@@ -76,30 +86,30 @@ static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 	disasm_obj.fprintf_func = &buf_fprintf;
 	disasm_obj.stream = stdout;
 	disasm_obj.mach = 0;
-
-	op->buf_asm[0]='\0';
-	if (a->bits==16)
+	op->buf_asm[0] = '\0';
+	if (a->bits == 16) {
 		op->size = ARCompact_decodeInstr ((bfd_vma)Offset, &disasm_obj);
-	else op->size = ARCTangent_decodeInstr ((bfd_vma)Offset, &disasm_obj);
-
-	if (op->size == -1)
+	} else {
+		op->size = ARCTangent_decodeInstr ((bfd_vma)Offset, &disasm_obj);
+	}
+	if (op->size == -1) {
 		strncpy (op->buf_asm, " (data)", R_ASM_BUFSIZE);
+	}
 	return op->size;
 }
 
 RAsmPlugin r_asm_plugin_arc = {
 	.name = "arc",
 	.arch = "arc",
-	.bits = 16|32,
+	.bits = 16 | 32,
 	.endian = R_SYS_ENDIAN_LITTLE | R_SYS_ENDIAN_BIG,
 	.desc = "Argonaut RISC Core",
 	.disassemble = &disassemble,
-	.assemble = NULL,
 	.license = "GPL3"
 };
 
 #ifndef CORELIB
-struct r_lib_struct_t radare_plugin = {
+RLibStruct radare_plugin = {
 	.type = R_LIB_TYPE_ASM,
 	.data = &r_asm_plugin_arc,
 	.version = R2_VERSION
--- libr/asm/p/asm_arm_cs.c
+++ libr/asm/p/asm_arm_cs.c
@@ -173,7 +173,7 @@ static int check_features(RAsm *a, cs_insn *insn) {
 }
 
 #ifndef CORELIB
-struct r_lib_struct_t radare_plugin = {
+RLibStruct radare_plugin = {
 	.type = R_LIB_TYPE_ASM,
 	.data = &r_asm_plugin_arm_cs,
 	.version = R2_VERSION
