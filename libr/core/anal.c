a80c1f2a4a5c631f4dac6aa618e9555d60df0ff7
Álvaro Felipe Melchor <alvaro.felipe91@gmail.com>
2016-09-22 19:04:02 +0200

    Fix warning

--- libr/core/anal.c
+++ libr/core/anal.c
@@ -3195,7 +3195,7 @@ R_API void r_core_anal_esil(RCore *core, const char *str, const char *target) {
 					if ((target && dst == ntarget) || !target) {
 						if (myvalid (dst) && r_io_is_valid_offset (mycore->io, dst, 0)) {
 							RAnalRefType ref =
-								op.type & R_ANAL_OP_TYPE_MASK == R_ANAL_OP_TYPE_UCALL
+								(op.type & R_ANAL_OP_TYPE_MASK) == R_ANAL_OP_TYPE_UCALL
 								? R_ANAL_REF_TYPE_CALL
 								: R_ANAL_REF_TYPE_CODE;
 							r_anal_ref_add (core->anal, dst, cur, ref);
