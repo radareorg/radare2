1923ee74eff4439dc6acc1d41206c33b956f841e
radare <pancake@nopcode.org>
2016-09-26 23:13:49 +0200

    Fix #5698 - Support memrefcalls in xrefs and hotkeys common on w32 bins

--- libr/core/core.c
+++ libr/core/core.c
@@ -129,25 +129,26 @@ R_API ut64 r_core_get_asmqjmps(RCore *core, const char *str) {
  * The returned buffer needs to be freed
  */
 R_API char* r_core_add_asmqjmp(RCore *core, ut64 addr) {
-	int i, found = 0;
-	if (!core->asmqjmps) return NULL;
+	bool found = false;
+	if (!core->asmqjmps) {
+		return NULL;
+	}
 	if (core->is_asmqjmps_letter) {
 		if (core->asmqjmps_count >= R_CORE_ASMQJMPS_MAX_LETTERS) {
 			return NULL;
 		}
-
 		if (core->asmqjmps_count >= core->asmqjmps_size - 2) {
 			core->asmqjmps = realloc (core->asmqjmps, core->asmqjmps_size * 2 * sizeof (ut64));
 			if (!core->asmqjmps) return NULL;
 			core->asmqjmps_size *= 2;
 		}
 	}
-
 	if (core->asmqjmps_count < core->asmqjmps_size - 1) {
+		int i;
 		char t[R_CORE_ASMQJMPS_LEN_LETTERS + 1];
 		for (i = 0; i < core->asmqjmps_count + 1; i++) {
 			if (core->asmqjmps[i] == addr) {
-				found = 1;
+				found = true;
 				break;
 			}
 		}
@@ -157,9 +158,8 @@ R_API char* r_core_add_asmqjmp(RCore *core, ut64 addr) {
 		}
 		r_core_set_asmqjmps (core, t, sizeof (t), i);
 		return strdup (t);
-	} else {
-		return NULL;
 	}
+	return NULL;
 }
 
 /* returns in str a string that represents the shortcut to access the asmqjmp
