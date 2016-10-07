439661b9ab2ec4d27a3aa7c6df3c31d87a144da6
Gerardo García Peña <killabytenow@gmail.com>
2016-10-08 01:19:14 +0200

    core dumped fixed

--- libr/anal/p/anal_avr.c
+++ libr/anal/p/anal_avr.c
@@ -565,7 +565,7 @@ INST_HANDLER (fmulsu) {	// FMULSU Rd, Rr
 	ESIL_A ("1,");
 	ESIL_A ("r%d,DUP,0x80,&,?{,0xffff00,|,},", d);	// sign extension Rd
 	ESIL_A ("r%d", r);				// unsigned Rr
-	ESIL_A ("*,<<,", r, d);				// 0: (Rd*Rr)<<1
+	ESIL_A ("*,<<,");				// 0: (Rd*Rr)<<1
 
 	ESIL_A ("0xffff,&,");				// prevent overflow
 	ESIL_A ("DUP,0xff,&,r0,=,");			// r0 = LO(0)
@@ -831,7 +831,7 @@ INST_HANDLER (mulsu) {	// MULSU Rd, Rr
 	int d = (buf[0] >> 4 & 0x07) + 16;
 	int r = (buf[0] & 0x07) + 16;
 
-	ESIL_A ("r%s,", r);				// unsigned Rr
+	ESIL_A ("r%d,", r);				// unsigned Rr
 	ESIL_A ("r%d,DUP,0x80,&,?{,0xffff00,|,},", d);	// sign extension Rd
 	ESIL_A ("*,");					// 0: (Rd*Rr)
 	ESIL_A ("DUP,0xff,&,r0,=,");			// r0 = LO(0)
