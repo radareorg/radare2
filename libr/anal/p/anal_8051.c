d5eb782a6cfd2af01e956fae4f24a81dcbf94336
Gerardo García Peña <killabytenow@gmail.com>
2016-10-10 14:55:28 +0200

    Added support for modifying the incoming value in operation `RAnalEsilCallbacks::hook_reg_write()`.

--- libr/anal/p/anal_8051.c
+++ libr/anal/p/anal_8051.c
@@ -401,14 +401,14 @@ static int i8051_hook_reg_read(RAnalEsil *esil, const char *name, ut64 *res, int
 	return ret;
 }
 
-static int i8051_hook_reg_write(RAnalEsil *esil, const char *name, ut64 val) {
+static int i8051_hook_reg_write(RAnalEsil *esil, const char *name, ut64 *val) {
 	int ret = 0;
 	struct r_i8015_reg *ri;
 	RAnalEsilCallbacks cbs = esil->cb;
 
 	if ((ri = i8051_reg_find (name))) {
 		ut8 offset = i8051_reg_get_offset(esil, ri);
-		ret = r_anal_esil_mem_write (esil, IRAM + offset, (ut8*)&val, ri->num_bytes);
+		ret = r_anal_esil_mem_write (esil, IRAM + offset, (ut8*) val, ri->num_bytes);
 	}
 
 	esil->cb = ocbs;
