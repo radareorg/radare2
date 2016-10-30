c3cb5e34f34130a8c0d2c41c31333ff911813efa
Maijin <maijin21@gmail.com>
2016-10-30 12:07:56 +0100

    Add Oj

--- libr/asm/p/cs_mnemonics.c
+++ libr/asm/p/cs_mnemonics.c
@@ -1,20 +1,35 @@
-
-static char *mnemonics(RAsm *a, int id) {
+static char *mnemonics(RAsm *a, int id, bool json) {
 	int i;
 	a->cur->disassemble (a, NULL, NULL, -1);
 	if (id != -1) {
 		const char *name = cs_insn_name (cd, id);
+		if (json) {
+			return name? r_str_newf ("[\"%s\"]\n", name): NULL;
+		}
 		return name? r_str_newf ("%s\n", name): NULL;
 	}
 	RStrBuf *buf = r_strbuf_new ("");
+	if (json){
+			r_strbuf_append(buf, "[");
+	}
 	for (i = 1; ; i++) {
 		const char *op = cs_insn_name (cd, i);
 		if (!op) {
 			break;
 		}
+		if (json) {
+			r_strbuf_append(buf, "\"");
+		}
 		r_strbuf_append (buf, op);
-		r_strbuf_append (buf, "\n");
+		if (json) {
+			if (cs_insn_name (cd, i+1)) {
+				r_strbuf_append(buf, "\",");
+			} else {
+				r_strbuf_append(buf, "\"]\n");
+			}
+		} else {
+			r_strbuf_append (buf, "\n");
+		}
 	}
 	return r_strbuf_drain (buf);
 }
-
