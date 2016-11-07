33f7ef8edefe602a298fe66fc7480f518ba62e3c
pancake <pancake@nopcode.org>
2016-11-07 05:21:49 +0100

    Fix #6144 - asm.capitalize do not uppercase locals

--- libr/parse/p/parse_x86_pseudo.c
+++ libr/parse/p/parse_x86_pseudo.c
@@ -251,6 +251,9 @@ static bool varsub (RParse *p, RAnalFunction *f, ut64 addr, int oplen, char *dat
 	spargs = p->varlist (p->anal, f, 's');
 	/*iterate over stack pointer arguments/variables*/
 	bool ucase = *tstr >= 'A' && *tstr <= 'Z';
+	if (ucase && tstr[1]) {
+		ucase = tstr[1] >= 'A' && tstr[1] <= 'Z';
+	}
 	r_list_foreach (spargs, spiter, sparg) {
 		if (sparg->delta < 10) {
 			snprintf (oldstr, sizeof (oldstr)-1, "[%s + %d]",
