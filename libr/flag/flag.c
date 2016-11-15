f28a62f29692a9ff6df2aeda3f1d928a569d1264
pancake <pancake@nopcode.org>
2016-11-15 13:31:53 +0100

    recursive sdb_fmt considered harmful

--- libr/flag/flag.c
+++ libr/flag/flag.c
@@ -391,11 +391,15 @@ R_API RFlagItem *r_flag_set_next(RFlag *f, const char *name, ut64 off, ut32 size
 	if (!r_flag_get (f, name)) {
 		return r_flag_set (f, name, off, size);
 	}
-	int i;
+	int i, newNameSize = strlen (name);
+	char *newName = malloc (newNameSize + 16);
+	strcpy (newName, name);
 	for (i = 0; ; i++) {
-		const char *newName = sdb_fmt (0, "%s.%d", name, i);
+		snprintf (newName + newNameSize, 15, ".%d", i);
 		if (!r_flag_get (f, newName)) {
-			return r_flag_set (f, newName, off, size);
+			RFlagItem *fi = r_flag_set (f, newName, off, size);
+			free (newName);
+			return fi;
 		}
 	}
 	return NULL;
