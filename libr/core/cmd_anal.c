013583a573b13c1bc7150d3391d1c282d0688f4a
Maijin <maijin21@gmail.com>
2016-11-17 16:38:49 +0100

    Add help for asc[a]

--- libr/core/cmd_anal.c
+++ libr/core/cmd_anal.c
@@ -3317,6 +3317,7 @@ static void cmd_anal_syscall(RCore *core, const char *input) {
 		"Usage: as[ljk?]", "", "syscall name <-> number utility",
 		"as", "", "show current syscall and arguments",
 		"as", " 4", "show syscall 4 based on asm.os and current regs/mem",
+		"asc[a]", " 4", "dump syscall info in .asm or .h",
 		"asf", " [k[=[v]]]", "list/set/unset pf function signatures (see fcnsign)",
 		"asj", "", "list of syscalls in JSON",
 		"asl", "", "list of syscalls by asm.os and asm.arch",
