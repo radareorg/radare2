f8d45c4b70d4aebbfba6985d1b239f85947c84be
oddcoder <ahmedsoliman@oddcoder.com>
2016-11-21 18:52:50 +0200

    Bye Bye tcc

--- libr/parse/code.c
+++ libr/parse/code.c
@@ -2,8 +2,6 @@
 
 #include "r_util.h"
 #include "r_types.h"
-#include "libr_tcc.h"
-
 /* parse C code and return it in key-value form */
 
 static void appendstring(const char *msg, char **s) {
@@ -22,24 +20,9 @@ static void appendstring(const char *msg, char **s) {
 }
 
 R_API char *r_parse_c_file(const char *path) {
-	char *str = NULL;
-	TCCState *T = tcc_new ();
-	tcc_set_callback (T, &appendstring, &str);
-	if (tcc_add_file (T, path) == -1) {
-		free (str);
-		str = NULL;
-	}
-	tcc_delete (T);
-	return str;
 }
 
 R_API char *r_parse_c_string(const char *code) {
-	char *str = NULL;
-	TCCState *T = tcc_new ();
-	tcc_set_callback (T, &appendstring, &str);
-	tcc_compile_string (T, code);
-	tcc_delete (T);
-	return str;
 }
 
 R_API int r_parse_is_c_file (const char *file) {
--- sys/tcc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-export CC="tcc"
-exec sys/install.sh $*
