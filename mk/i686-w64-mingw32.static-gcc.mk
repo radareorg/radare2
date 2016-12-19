fce076964831a0223352a6a99319458f83e0c98c
Roman Valls Guimerà <brainstorm@users.noreply.github.com>
2016-12-19 04:55:21 +0100

    Add mxe+dockcross support for docker-windows builds: https://github.com/mxe/mxe (#6342)

---
+++ mk/i686-w64-mingw32.static-gcc.mk
@@ -0,0 +1,21 @@
+ifeq (${_INCLUDE_MK_GCC_},)
+_INCLUDE_MK_GCC_=1
+CC=i686-w64-mingw32.static-gcc
+RANLIB=i686-w64-mingw32.static-ranlib
+ONELIB=0
+OSTYPE=windows
+LINK=
+CC_AR=i686-w64-mingw32.static-ar -r ${LIBAR}
+PICFLAGS=
+CFLAGS+=${PICFLAGS} -MD -D__WINDOWS__=1
+CC_LIB=${CC} -shared -o
+CFLAGS_INCLUDE=-I
+LDFLAGS+=-static-libgcc
+LDFLAGS_LINK=-l
+LDFLAGS_LINKPATH=-L
+CFLAGS_OPT0=-O0
+CFLAGS_OPT1=-O1
+CFLAGS_OPT2=-O2
+CFLAGS_OPT3=-O3
+CFLAGS_DEBUG=-g
+endif
