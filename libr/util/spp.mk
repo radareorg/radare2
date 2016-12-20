b936c17d6132fc2912c8504512bb732a043024c3
pancake <pancake@nopcode.org>
2016-12-20 01:18:26 +0100

    Fix spp build

--- libr/util/spp.mk
+++ libr/util/spp.mk
@@ -3,5 +3,11 @@ SPP_OBJS=spp.o
 SPPOBJS=$(addprefix ${SPPPATH},${SPP_OBJS})
 OBJS+=$(SPPOBJS)
 
-$(SPPPATH)/config.h:
-	cp -f ${SPPPATH}/config.def.h ${SPPPATH}/config.h
+$(SPPPATH)/config.h: $(SPPPATH)/config.def.h
+	$(MAKE) spp_config
+
+spp_config:
+	cp -f $(SPPPATH)/config.def.h $(SPPPATH)/config.h
+	$(MAKE) spp_build
+
+spp_build: $(SPPOBJS)
