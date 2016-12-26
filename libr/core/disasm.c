0590f3a41cc993ad6fab90aeb08dc0debb213afd
Álvaro Felipe Melchor <alvaro.felipe91@gmail.com>
2016-12-26 18:32:16 +0100

    Fix regression and oob read

--- libr/core/disasm.c
+++ libr/core/disasm.c
@@ -3091,15 +3091,12 @@ static void ds_print_esil_anal(RDisasmState *ds) {
 					if (ds->show_color) {
 						ds_comment_esil (ds, true, false, ds->pal_comment);
 					}
-					ds_align_comment (ds);
-					{
-						ds_comment_esil (ds, ds->show_color? false : true, false, 
-						  	"; %s%s%s(", r_str_get (fcn_type), (fcn_type && *fcn_type &&
+					ds_comment_esil (ds, ds->show_color? false : true, false, 
+					  		"; %s%s%s(", r_str_get (fcn_type), (fcn_type && *fcn_type &&
 							fcn_type[strlen (fcn_type) - 1] == '*') ? "" : " ",
-							r_str_get (key));
-					}
+					  		r_str_get (key));
 					if (!nargs) {
-						ds_comment_esil (ds, false, true, "%c void);");
+						ds_comment_esil (ds, false, true, "void);");
 						break;
 					}
 					cc = r_anal_type_func_cc (core->anal, key);
