2e2101ccf5618233ba63772de30ca96ebb8ea977
Gerardo García Peña <killabytenow@gmail.com>
2016-12-31 14:07:16 +0100

    bugfix

--- libr/anal/p/anal_avr.c
+++ libr/anal/p/anal_avr.c
@@ -385,7 +385,7 @@ INST_HANDLER (add) {	// ADD Rd, Rr
 	int d = ((buf[0] >> 4) & 0xf) | ((buf[1] & 1) << 4);
 	int r = (buf[0] & 0xf) | ((buf[1] & 2) << 3);
 	ESIL_A ("r%d,r%d,+,", r, d);			// Rd + Rr
-	__generic_math_update_flags_rr(op, r, d);	// FLAGS
+	__generic_add_update_flags_rr(op, r, d);	// FLAGS
 	ESIL_A ("r%d,=,", d);				// Rd = result
 }
 
