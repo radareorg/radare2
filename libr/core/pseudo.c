e9310d31c2fa6c4a185de3e2fdf30ec36a5b7588
Maijin <maijin21@gmail.com>
2017-01-20 14:48:14 +0100

    Fix regression in pdc

--- libr/core/pseudo.c
+++ libr/core/pseudo.c
@@ -22,7 +22,7 @@ R_API int r_core_pseudo_code(RCore *core, const char *input) {
 	}
 	r_config_set_i (core->config, "asm.pseudo", 1);
 	r_config_set_i (core->config, "asm.decode", 0);
-	r_config_set_i (core->config, "asm.filter", 0);
+	r_config_set_i (core->config, "asm.filter", 1);
 	r_config_set_i (core->config, "asm.lines", 0);
 	r_config_set_i (core->config, "asm.bytes", 0);
 	r_config_set_i (core->config, "asm.offset", 0);
