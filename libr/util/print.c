radareorg/radare2

commit c176c48ffccd4d470add516c137a550dff241901

Implement: Show comments in hexdump #6460 (#6527)
* Add hex.comments variable to config
* Add get_comments callback to the core api
* Add comment printing functionality to px
* Add pxc function
* Change visual mode hexdump to pxc
* Boolify use_comments

libr/util/print.c
@@ -789,6 +789,11 @@ R_API void r_print_hexdump(RPrint *p, ut64 addr, const ut8 *buf, int len, int ba
 			for (i = 0; i < inc; i++) {
 				printfmt ("%c", hex[(i+k)%16]);
 			}
+			/* print comment header*/
+			if (p->use_comments) {
+				printfmt (col == 1 ? "|" : " ");
+				printfmt (" commment ");
+			}
 			printfmt (col == 2 ? "|\n" : "\n");
 		}
 	}
@@ -903,9 +908,11 @@ R_API void r_print_hexdump(RPrint *p, ut64 addr, const ut8 *buf, int len, int ba
 		printfmt ((col == 2)? "|" : " ");
 		for (j = i; j < i + inc; j++) {
 			if (j >= len) {
-				break;
+				//break;
+				printfmt (" ");
+			} else {
+				r_print_byte (p, "%c", j, buf[j]);
 			}
-			r_print_byte (p, "%c", j, buf[j]);
 		}
 		if (col == 2) printfmt("|");
 		if (p && p->flags & R_PRINT_FLAGS_REFS) {
@@ -922,6 +929,20 @@ R_API void r_print_hexdump(RPrint *p, ut64 addr, const ut8 *buf, int len, int ba
 				rstr = p->hasrefs (p->user, off, true);
 				if (rstr && *rstr) {
 					printfmt ("%s", rstr);
+				}
+			}
+		}
+		if (p->use_comments) {
+			for (j = i; j < i + inc; j++) {
+				const char *comment = p->get_comments (p->user, addr + j);
+				if (comment) {
+					if (p && p->colorfor) {
+						a = p->colorfor (p->user, addr + j, true);
+						if (a && *a) { b = Color_RESET; } else { a = b = ""; }
+					} else {
+						a = b = "";
+					}
+					printfmt("%s  ; %s", a, comment);
 				}
 			}
 		}

libr/core/cconfig.c
@@ -952,6 +952,17 @@ static int cb_hexpairs(void *user, void *data) {
 	return true;
 }
 
+static int cb_hexcomments(void *user, void *data) {
+	RCore *core = (RCore *) user;
+	RConfigNode *node = (RConfigNode *) data;
+	if (node->i_value) {
+		core->print->flags |= R_PRINT_FLAGS_COMMENT;
+	} else {
+		core->print->flags &= ~R_PRINT_FLAGS_COMMENT;
+	}
+	return true;
+}
+
 R_API int r_core_esil_cmd(RAnalEsil *esil, const char *cmd, int a1, int a2) {
 	if (cmd && *cmd) {
 		RCore *core = esil->anal->user;
@@ -2025,6 +2036,7 @@ R_API int r_core_config_init(RCore *core) {
 	SETI("hex.depth", 5, "Maximal level of recurrence while telescoping memory");
 	SETPREF("hex.onechar", "false", "Number of columns in hexdump");
 	SETICB("hex.stride", 0, &cb_hexstride, "Line stride in hexdump (default is 0)");
+	SETCB("hex.comments", "true", &cb_hexcomments, "Show comments in 'px' hexdump");
 
 	/* http */
 	SETPREF("http.log", "true", "Show HTTP requests processed");

libr/core/cmd_print.c
@@ -3685,6 +3685,8 @@ static int cmd_print(void *data, const char *input) {
 			core->print->flags &= ~R_PRINT_FLAGS_OFFSET;
 			core->print->flags &= ~R_PRINT_FLAGS_HEADER;
 		}
+		/* Don't show comments in default case */
+		core->print->use_comments = false;
 		}
 		r_cons_break_push (NULL, NULL);
 		switch (input[1]) {
@@ -3699,6 +3701,7 @@ static int cmd_print(void *data, const char *input) {
 				"pxa", "", "show annotated hexdump",
 				"pxA", "", "show op analysis color map",
 				"pxb", "", "dump bits in hexdump form",
+				"pxc", "", "show hexdump with comments",
 				"pxd", "[124]", "signed integer dump (1 byte, 2 and 4)",
 				"pxe", "", "emoji hexdump! :)",
 				"pxi", "", "HexII compact binary representation",
@@ -3773,6 +3776,23 @@ static int cmd_print(void *data, const char *input) {
 					c = -1;
 				}
 			}
+			}
+			break;
+		case 'c':
+			core->print->use_comments = core->print->flags & R_PRINT_FLAGS_COMMENT;
+			if (l) {
+				ut64 from = r_config_get_i (core->config, "diff.from");
+				ut64 to = r_config_get_i (core->config, "diff.to");
+				if (from == to && !from) {
+					if (!r_core_block_size (core, len)) {
+						 len = core->blocksize;
+					}
+					r_print_hexdump (core->print, core->offset,
+					  core->block, len, 16, 1);
+				} else {
+					 r_core_print_cmp (core, from, to);
+				}
+				core->num->value = len;
 			}
 			break;
 		case 'i': // "pxi"

libr/core/core.c
@@ -1436,6 +1436,13 @@ static char *r_core_anal_hasrefs_to_depth(RCore *core, ut64 value, int depth) {
 	return r_strbuf_drain (s);
 }
 
+R_API char *r_core_anal_get_comments(RCore *core, ut64 addr) {
+	if (core) {
+		return r_meta_get_string (core->anal, R_META_TYPE_COMMENT, addr);
+	}
+	return NULL;
+}
+
 R_API const char *r_core_anal_optype_colorfor(RCore *core, ut64 addr, bool verbose) {
 	ut64 type;
 	if (!(core->print->flags & R_PRINT_FLAGS_COLOR)) {
@@ -1493,6 +1500,8 @@ R_API int r_core_init(RCore *core) {
 	core->print->disasm = __disasm;
 	core->print->colorfor = (RPrintColorFor)r_core_anal_optype_colorfor;
 	core->print->hasrefs = (RPrintColorFor)r_core_anal_hasrefs;
+	core->print->get_comments = (RPrintCommentCallback) r_core_anal_get_comments;
+	core->print->use_comments = false;
 	core->rtr_n = 0;
 	core->blocksize_max = R_CORE_BLOCKSIZE_MAX;
 	core->tasks = r_list_new ();

libr/core/visual.c
@@ -11,7 +11,7 @@ static void visual_refresh(RCore *core);
 #define PIDX (R_ABS(core->printidx%NPF))
 
 static const char *printfmtSingle[] = {
-	"x", "pd $r",
+	"xc", "pd $r",
 	"pxw 64@r:SP;dr=;pd $r",
 	"pxw", "pc", "pxA", "pxa"
 };

libr/include/r_core.h
@@ -557,6 +557,7 @@ typedef struct {
 } RCoreAnalStats;
 
 R_API char *r_core_anal_hasrefs(RCore *core, ut64 value, bool verbose);
+R_API char *r_core_anal_get_comments(RCore *core, ut64 addr);
 R_API RCoreAnalStats* r_core_anal_get_stats (RCore *a, ut64 from, ut64 to, ut64 step);
 R_API void r_core_anal_stats_free (RCoreAnalStats *s);
 R_API void r_core_anal_list_vtables(void *core, bool printJson);

libr/include/r_print.h
@@ -21,9 +21,11 @@ extern "C" {
 #define R_PRINT_FLAGS_REFS    0x00000080
 #define R_PRINT_FLAGS_DIFFOUT 0x00000100 /* only show different rows in `cc` hexdiffing */
 #define R_PRINT_FLAGS_ADDRDEC 0x00000200
+#define R_PRINT_FLAGS_COMMENT 0x00000400
 
 typedef int (*RPrintZoomCallback)(void *user, int mode, ut64 addr, ut8 *bufz, ut64 size);
 typedef const char *(*RPrintNameCallback)(void *user, ut64 addr);
+typedef const char *(*RPrintCommentCallback)(void *user, ut64 addr);
 typedef const char *(*RPrintColorFor)(void *user, ut64 addr, bool verbose);
 
 typedef struct r_print_zoom_t {
@@ -59,6 +61,7 @@ typedef struct r_print_t {
 	int ocur;
 	int cols;
 	int flags;
+	bool use_comments;
 	int addrmod;
 	int col;
 	int stride;
@@ -68,6 +71,7 @@ typedef struct r_print_t {
 	RPrintNameCallback offname;
 	RPrintColorFor colorfor;
 	RPrintColorFor hasrefs;
+	RPrintCommentCallback get_comments;
 	RStrHT *formats;
 	RCons *cons;
 	RConsBind consbind;

commit 41505235cf7efa59c59ec82a91e2dbf6dbe01bbf

Fix "commment" typo
This fixes a typo introduced in
c176c48ffccd4d470add516c137a550dff241901.

libr/util/print.c
@@ -792,7 +792,7 @@ R_API void r_print_hexdump(RPrint *p, ut64 addr, const ut8 *buf, int len, int ba
 			/* print comment header*/
 			if (p->use_comments) {
 				printfmt (col == 1 ? "|" : " ");
-				printfmt (" commment ");
+				printfmt (" comment ");
 			}
 			printfmt (col == 2 ? "|\n" : "\n");
 		}