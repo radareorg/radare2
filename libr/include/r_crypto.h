276f9acae702d3fdaf86bbc3b3bd66b79caecd98
Giovanni <wargio@libero.it>
2017-01-28 12:52:39 +0100

    Add DES support to rahash2

--- libr/include/r_crypto.h
+++ libr/include/r_crypto.h
@@ -63,6 +63,7 @@ R_API const char *r_crypto_name(ut64 bit);
 
 /* plugin pointers */
 extern RCryptoPlugin r_crypto_plugin_aes;
+extern RCryptoPlugin r_crypto_plugin_des;
 extern RCryptoPlugin r_crypto_plugin_rc4;
 extern RCryptoPlugin r_crypto_plugin_xor;
 extern RCryptoPlugin r_crypto_plugin_blowfish;
@@ -88,6 +89,7 @@ extern RCryptoPlugin r_crypto_plugin_cps2;
 #define R_CRYPTO_ROT 1<<7
 #define R_CRYPTO_BLOWFISH 1<<8
 #define R_CRYPTO_CPS2 1<<9
+#define R_CRYPTO_DES_ECB 1<<10
 #define R_CRYPTO_ALL 0xFFFF
 
 #ifdef __cplusplus
