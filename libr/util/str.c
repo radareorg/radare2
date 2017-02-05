fbb46ee68291a13c4d9985240ca50aa4e6cffb5e
SchumBlubBlub <SchumBlubBlub@users.noreply.github.com>
2017-02-06 00:00:40 +0300

    Fix buggy r_str_isnumber

--- libr/util/str.c
+++ libr/util/str.c
@@ -2506,13 +2506,13 @@ R_API int *r_str_split_lines(char *str, int *count) {
 }
 
 R_API bool r_str_isnumber (const char *str) {
-	if (!*str) {
+	if (!str || !*str) {
 		return false;
 	}
-	bool isnum = IS_DIGIT (*str) || *str == '-';
-	str++;
-	while (*str++) {
-		if (!IS_DIGIT (*str) || *str == '-') {
+
+	bool isnum = !IS_DIGIT (*str) || *str == '-';
+	while (isnum && *++str) {
+		if (!IS_DIGIT (*str)) {
 			isnum = false;
 		}
 	}
