6e69663f9b4bc0aaece2924a317917e57c511170
h4ng3r <h4ng3r@users.noreply.github.com>
2017-02-09 00:15:09 +0100

    fix put and get disas and some tests #6739 #155

--- libr/bin/p/bin_dex.c
+++ libr/bin/p/bin_dex.c
@@ -839,18 +839,24 @@ static char *dex_class_name(RBinDexObj *bin, RBinDexClass *c) {
 }
 
 static char *dex_field_name(RBinDexObj *bin, int fid) {
-	int tid;
+	int cid, tid, type_id;
 	if (!bin || !bin->fields) {
 		return NULL;
 	}
 	if (fid < 0 || fid >= bin->header.fields_size) {
 		return NULL;
 	}
-	tid = bin->fields[fid].class_id;
-	if (tid < 0 || tid >= bin->header.types_size) {
+	cid = bin->fields[fid].class_id;
+	if (cid < 0 || cid >= bin->header.types_size) {
 		return NULL;
 	}
-	return getstr (bin, bin->types[tid].descriptor_id);
+	type_id = bin->fields[fid].type_id;
+	if (type_id < 0 || type_id >= bin->header.types_size) {
+		return NULL;
+	}
+	tid = bin->fields[fid].name_id;
+	return r_str_newf ("%s->%s %s", getstr (bin, bin->types[cid].descriptor_id),
+		getstr (bin, tid), getstr (bin, bin->types[type_id].descriptor_id));
 }
 
 static char *dex_method_fullname(RBinDexObj *bin, int method_idx) {
