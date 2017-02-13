0bf91b4ca4cb841bdaf01b376880b08a1cbf3307
Der0ad <wargio@libero.it>
2017-02-13 17:43:05 +0100

    Fixed memory leak

--- libr/util/r_pkcs7.c
+++ libr/util/r_pkcs7.c
@@ -194,7 +194,8 @@ void r_pkcs7_free_signerinfo (RPKCS7SignerInfo* si) {
 		r_pkcs7_free_issuerandserialnumber (&si->issuerAndSerialNumber);
 		r_x509_free_algorithmidentifier (&si->digestAlgorithm);
 		r_pkcs7_free_attributes (&si->authenticatedAttributes);
-
+		r_x509_free_algorithmidentifier (&si->digestEncryptionAlgorithm);
+		r_asn1_free_object(si->encryptedDigest);
 		r_pkcs7_free_attributes (&si->unauthenticatedAttributes);
 		free (si);
 	}
