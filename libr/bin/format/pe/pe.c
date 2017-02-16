cb78f408ac758f63bba16123012ab80c57e4de6a
William D. Jones <thor0505@comcast.net>
2017-02-16 08:29:30 -0500

    Fix Windows search path for sdbs in pe.c

--- libr/bin/format/pe/pe.c
+++ libr/bin/format/pe/pe.c
@@ -221,7 +221,12 @@ static int bin_pe_parse_imports(struct PE_(r_bin_pe_obj_t)* bin,
 						db = sdb_new (NULL, filename, 0);
 					} else {
 #if __WINDOWS__
-						filename = sdb_fmt (1, "share/radare2/"R2_VERSION "/format/dll/%s.sdb", symdllname);
+						char invoke_dir[MAX_PATH];
+						if (r_sys_get_src_dir_w32(invoke_dir)) {
+							filename = sdb_fmt (1, "%s/share/radare2/"R2_VERSION "/format/dll/%s.sdb", invoke_dir, symdllname);
+						} else {
+							filename = sdb_fmt (1, "share/radare2/"R2_VERSION "/format/dll/%s.sdb", symdllname);
+						}
 #else
 						filename = sdb_fmt (1, R2_PREFIX "/share/radare2/" R2_VERSION "/format/dll/%s.sdb", symdllname);
 #endif
