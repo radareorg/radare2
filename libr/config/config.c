a5fe3bc19040df689a559ba6d4417e75865b499e
yetmorecode <yetmorecode@posteo.de>
2017-02-23 01:32:36 +0100

    Added more JSON escaping

--- libr/config/config.c
+++ libr/config/config.c
@@ -49,23 +49,23 @@ R_API void r_config_node_free(void *n) {
 
 static void config_print_value_json(RConfig *cfg, RConfigNode *node) {
 	const char *val = node->value;
+	if (!val) {
+		val = "0";
+	}
+	const char *sval = r_str_escape (val);
 	if (node->flags & CN_BOOL || node->flags & CN_INT || node->flags & CN_OFFT) {
-		if (!val) {
-			val = "0";
-		}
 		if (!strncmp (val, "0x", 2)) {
 			ut64 n = r_num_get (NULL, val);
 			cfg->cb_printf ("%"PFMT64d, n);
 		} else if (r_str_isnumber (val) || !strcmp (val, "true") || !strcmp (val, "false")) {
 			cfg->cb_printf ("%s", val);
 		} else {
-			char *sval = r_str_escape (val);
 			cfg->cb_printf ("\"%s\"", sval);
-			free (sval);
 		}
 	} else {
-		cfg->cb_printf ("\"%s\"", val);
+		cfg->cb_printf ("\"%s\"", sval);
 	}
+	free (sval);
 }
 
 static void config_print_node(RConfig *cfg, RConfigNode *node, const char *pfx, const char *sfx, bool verbose, bool json) {
