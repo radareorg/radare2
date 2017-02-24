04b2d36bd7dd1b8ea34f144016d6744b5e6ec304
rene <rlaemmert@gmail.com>
2017-02-24 07:05:31 +0100

    fixes r2 -H <var>

--- binr/radare2/radare2.c
+++ binr/radare2/radare2.c
@@ -474,7 +474,7 @@ int main(int argc, char **argv, char **envp) {
 		return 0;
 	}
 
-	while ((c = getopt (argc, argv, "=02AMCwfF:hH::m:e:nk:Ndqs:p:b:B:a:Lui:I:l:P:R:c:D:vVSzu"
+	while ((c = getopt (argc, argv, "=02AMCwfF:H:h::m:e:nk:Ndqs:p:b:B:a:Lui:I:l:P:R:c:D:vVSzu"
 #if USE_THREADS
 "t"
 #endif
