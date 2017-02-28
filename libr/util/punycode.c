0eda1af89db63d9b79cda5ab0b39574ab7560dc0
Rakholiya Jenish <rakholiyajenish.07@gmail.com>
2017-02-28 19:13:11 +0530

    fix punycode issue 4837

--- libr/util/punycode.c
+++ libr/util/punycode.c
@@ -10,9 +10,111 @@
 #define INITIAL_N 128
 #define INITIAL_BIAS 72
 
+int utf32len (unsigned int *input) {
+	int i = 0;
+	while (*(input + i)) {
+		i++;
+	}
+	return i;
+}
+
+unsigned char *utf32toutf8 (unsigned int *input) {
+	if (!input) {
+		eprintf ("ERROR input is null\n");
+		return NULL;
+	}
+
+	int i = 0;
+	int j = 0;
+	int len = utf32len (input);
+	unsigned char *result = malloc ((len * 4) + 1);
+	if (!result) {
+		eprintf ("ERROR: out of memory\n");
+		return NULL;
+	}
+
+	for (i = 0; i < len; i++) {
+		if (input[i] < 0x80) {
+			result[j] = input[i];
+			j++;
+		} else if (input[i] < 0x800) {
+			result[j + 1] = 0x80 | (input[i] & 0x3f);
+			result[j] = 0xc0 | ((input[i] >> 6) & 0x1f);
+			j += 2;
+		} else if (input[i] < 0x10000) {
+			result[j + 2] = 0x80 | (input[i] & 0x3f);
+			result[j + 1] = 0x80 | ((input[i] >> 6) & 0x3f);
+			result[j] = 0xe0 | ((input[i] >> 12) & 0xf);
+			j += 3;
+		} else if (input[i] < 0x200000) {
+			result[j + 3] = 0x80 | (input[i] & 0x3f);
+			result[j + 2] = 0x80 | ((input[i] >> 6) & 0x3f);
+			result[j + 1] = 0x80 | ((input[i] >> 12) & 0x3f);
+			result[j] = 0xf0 | ((input[i] >> 18) & 0x7);
+			j += 4;
+		} else {
+			eprintf ("ERROR in toutf8. Seems like input is invalid\n");
+			free (result);
+			return NULL;
+		}
+	}
+
+	result[j] = 0;
+	return result;
+}
+
+ut32 *utf8toutf32 (const ut8 *input) {
+	if (!input) {
+		eprintf ("ERROR input is null\n");
+		return NULL;
+	}
+
+	int i = 0;
+	int j = 0;
+	int val = 0;
+	int len = strlen ((const char *) input);
+	ut32 *result = calloc (strlen ((const char *) input) + 1, 4);
+
+	if (!result) {
+		eprintf ("ERROR: out of memory\n");
+		return NULL;
+	}
+
+	while (i < len) {
+		if (input[i] >> 7 == 0) {
+			val = input[i];
+			i += 1;
+		} else if (input[i] >> 5 == 0x6) {
+			val = (((input[i] & 0x1f) << 6) & 0xfc0) |
+			(input[i + 1] & 0x3f);
+			i += 2;
+		} else if (input[i] >> 4 == 0xe) {
+			val = (((input[i] & 0xf) << 12) & 0xf000) |
+			(((input[i + 1] & 0x3f) << 6) & 0xffc0) |
+			(input[i + 2] & 0x3f);
+			i += 3;
+		} else if (input[i] >> 3 == 0x1e) {
+			val = (((input[i] & 0xf) << 18) & 0x1c0000) |
+			(((input[i + 1] & 0x3f) << 12) & 0x1ff000) |
+			(((input[i + 2] & 0x3f) << 6) & 0x1fffc0) |
+			(input[i + 3] & 0x3f);
+			i += 4;
+		} else {
+			eprintf ("ERROR in toutf32. Seems like input is invalid.\n");
+			free (result);
+			return NULL;
+		}
+		result[j] = val;
+		j++;
+	}
+
+	return result;
+}
+
+
 ut32 adapt_bias(ut32 delta, unsigned n_points, int is_first) {
 	ut32 k = 0;
-	delta /= is_first ? DAMP : 2;
+	delta /= is_first? DAMP: 2;
 	delta += delta / n_points;
 
 	while (delta > ((BASE - TMIN) * TMAX) / 2) {
@@ -61,8 +163,8 @@ static ut32 encode_var_int(const ut32 bias, const ut32 delta, char *dst) {
 	return i;
 }
 
-static char decode_digit(char v) {
-	if (IS_DIGIT(v)) {
+static ut32 decode_digit(ut32 v) {
+	if (IS_DIGIT (v)) {
 		return v - 22;
 	}
 	if (v >= 'a' && v <= 'z') {
@@ -71,15 +173,17 @@ static char decode_digit(char v) {
 	if (v >= 'A' && v <= 'Z') {
 		return v - 'A';
 	}
-	return -1;
+	return UT32_MAX;
 }
 
-R_API char *r_punycode_encode(const char *src, int srclen, int *dstlen) {
-	ut8 m, n;
+R_API char *r_punycode_encode(const ut8 *src, int srclen, int *dstlen) {
+	ut32 m, n;
 	ut32 b, h;
 	ut32 si, di;
 	ut32 delta, bias;
-	char *dst;
+	ut32 *actualsrc = NULL;
+	ut32 len = 0;
+	char *dst = NULL;
 
 	*dstlen = 0;
 
@@ -87,14 +191,22 @@ R_API char *r_punycode_encode(const char *src, int srclen, int *dstlen) {
 		return NULL;
 	}
 
-	dst = calloc (2 * srclen + 10, 1);
-	if (!dst) {
+	actualsrc = utf8toutf32 (src);
+	if (!actualsrc) {
 		return NULL;
 	}
 
-	for (si = 0, di = 0; si < srclen; si++) {
-		if ((ut8)src[si] < 128) {
-			dst[di++] = src[si];
+	len = utf32len (actualsrc);
+
+	dst = calloc (2 * len + 10, 1);
+	if (!dst) {
+		free (actualsrc);
+		return NULL;
+	}
+
+	for (si = 0, di = 0; si < len; si++) {
+		if (actualsrc[si] < 128) {
+			dst[di++] = actualsrc[si];
 		}
 	}
 
@@ -107,14 +219,15 @@ R_API char *r_punycode_encode(const char *src, int srclen, int *dstlen) {
 	n = INITIAL_N;
 	bias = INITIAL_BIAS;
 
-	for (delta = 0; h < srclen; n++, delta++) {
-		for (m = 0xff, si = 0; si < srclen; si++) {
-			if ((ut8)src[si] >= n && (ut8)src[si] <= m) {
-				m = src[si];
+	for (delta = 0; h < len; n++, delta++) {
+		for (m = UT32_MAX, si = 0; si < len; si++) {
+			if (actualsrc[si] >= n && actualsrc[si] < m) {
+				m = actualsrc[si];
 			}
 		}
 
-		if ((unsigned int)(m - n) > (UT32_MAX - delta) / (h + 1)) {
+		if ((m - n) > (UT32_MAX - delta) / (h + 1)) {
+			free (actualsrc);
 			free (dst);
 			return NULL;
 		}
@@ -122,38 +235,40 @@ R_API char *r_punycode_encode(const char *src, int srclen, int *dstlen) {
 		delta += (m - n) * (h + 1);
 		n = m;
 
-		for (si = 0; si < srclen; si++) {
-			if ((ut8)src[si] < n) {
+		for (si = 0; si < len; si++) {
+			if (actualsrc[si] < n) {
 				if (++delta == 0) {
+					free (actualsrc);
 					free (dst);
 					return NULL;
 				}
-			} else if ((ut8)src[si] == n) {
+			} else if (actualsrc[si] == n) {
 				di += encode_var_int (bias, delta, &dst[di]);
-				bias = adapt_bias (delta, h+1, h == b);
+				bias = adapt_bias (delta, h + 1, h == b);
 				delta = 0;
 				h++;
 			}
 		}
 	}
 	*dstlen = di;
+	free (actualsrc);
 	return dst;
 }
 
 R_API char *r_punycode_decode(const char *src, int srclen, int *dstlen) {
-	ut8 n;
 	const char *p;
 	ut32 si, di;
-	ut32 b, t, i, k, w;
+	ut32 b, n, t, i, k, w;
 	ut32 digit, org_i, bias;
-	char *dst;
+	ut32 *dst = NULL;
+	ut8 *finaldst = NULL;
 
 	*dstlen = 0;
 	if (!src || srclen < 1) {
 		return NULL;
 	}
 
-	dst = calloc (2*srclen + 10, 1);
+	dst = calloc (2 * srclen + 10, 4);
 	if (!dst) {
 		return NULL;
 	}
@@ -165,7 +280,9 @@ R_API char *r_punycode_decode(const char *src, int srclen, int *dstlen) {
 		}
 	}
 
-	for (p = src + srclen - 1; p > src && *p != '-'; p--);
+	for (p = src + srclen - 1; p > src && *p != '-'; p--) {
+		;
+	}
 	b = p - src;
 
 	di = b;
@@ -180,10 +297,10 @@ R_API char *r_punycode_decode(const char *src, int srclen, int *dstlen) {
 	for (si = b + (b > 0); si < srclen; di++) {
 		org_i = i;
 
-		for (w = 1, k = BASE; ; k += BASE) {
+		for (w = 1, k = BASE;; k += BASE) {
 			digit = decode_digit (src[si++]);
 
-			if ((char)digit == -1) {
+			if (digit == UT32_MAX) {
 				free (dst);
 				return NULL;
 			}
@@ -212,10 +329,10 @@ R_API char *r_punycode_decode(const char *src, int srclen, int *dstlen) {
 				return NULL;
 			}
 
-			w *= BASE- t;
+			w *= BASE - t;
 		}
 
-		bias = adapt_bias(i - org_i, di+1, org_i == 0);
+		bias = adapt_bias (i - org_i, di + 1, org_i == 0);
 
 		if (i / (di + 1) > UT32_MAX - n) {
 			free (dst);
@@ -225,9 +342,17 @@ R_API char *r_punycode_decode(const char *src, int srclen, int *dstlen) {
 		n += i / (di + 1);
 		i %= (di + 1);
 
-		memmove (dst+i+1, dst+i, (di-i) * sizeof(char));
+		memmove (dst + i + 1, dst + i, (di - i) * sizeof(ut32));
 		dst[i++] = n;
 	}
-	*dstlen = di;
-	return dst;
+
+	finaldst = utf32toutf8 (dst);
+	free (dst);
+	if (finaldst) {
+		*dstlen = strlen ((const char *) finaldst);
+	} else {
+		eprintf ("ERROR: finaldst is null\n");
+		return NULL;
+	}
+	return (char *) finaldst;
 }
