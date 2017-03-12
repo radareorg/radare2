6870bec29f1f0d2ccd12f3edc983f48b0bcd3d74
Álvaro Felipe Melchor <alvaro.felipe91@gmail.com>
2017-03-12 12:31:22 +0100

    Fix #6853 - get rid of asserts

--- libr/debug/map.c
+++ libr/debug/map.c
@@ -195,12 +195,14 @@ R_API RDebugMap *r_debug_map_new(char *name, ut64 addr, ut64 addr_end, int perm,
 	RDebugMap *map;
 	/* range could be 0k on OpenBSD, it's a honeypot */
 	if (!name || addr > addr_end) {
-		eprintf ("r_debug_map_new: error assert(\
+		eprintf ("r_debug_map_new: error (\
 			%"PFMT64x">%"PFMT64x")\n", addr, addr_end);
 		return NULL;
 	}
 	map = R_NEW0 (RDebugMap);
-	if (!map) return NULL;
+	if (!map) {
+		return NULL;
+	}
 	map->name = strdup (name);
 	map->addr = addr;
 	map->addr_end = addr_end;
