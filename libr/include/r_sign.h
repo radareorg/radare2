d1b053439662d2db8877c36a37f13d5c28f1cf0e
Roi Martin <jroi.martin@gmail.com>
2017-03-13 00:41:53 +0000

    Move search code into r_sign. Refactoring

--- libr/include/r_sign.h
+++ libr/include/r_sign.h
@@ -3,8 +3,7 @@
 
 #include <r_types.h>
 #include <r_anal.h>
-#include <r_util.h>
-#include <r_list.h>
+#include <r_search.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -21,7 +20,6 @@ enum {
 	R_SIGN_METR  = 'm',  // Function metrics
 };
 
-/* signature struct */
 typedef struct r_sign_item_t {
 	char *name;
 	int space;
@@ -31,14 +29,33 @@ typedef struct r_sign_item_t {
 	ut8 *mask;
 } RSignItem;
 
-typedef int (*RSignForeachCallback)(void *user, RSignItem *it);
+typedef int (*RSignForeachCallback)(RSignItem *it, void *user);
+typedef int (*RSignSearchCallback)(RSearchKeyword *kw, RSignItem *it, ut64 addr, void *user);
+
+typedef struct r_sign_search_t {
+	RSearch *search;
+	RList *items;
+	RSignSearchCallback cb;
+	void *user;
+} RSignSearch;
 
 #ifdef R_API
 R_API bool r_sign_add(RAnal *a, int type, const char *name, ut64 size, const ut8 *bytes, const ut8 *mask);
 R_API bool r_sign_add_anal(RAnal *a, const char *name, ut64 size, const ut8 *bytes);
 R_API bool r_sign_delete(RAnal *a, const char *name);
 R_API void r_sign_list(RAnal *a, int format);
-R_API void r_sign_foreach(RAnal *a, RSignForeachCallback cb, void *user);
+
+R_API int r_sign_space_count_for(RAnal *a, int idx);
+R_API void r_sign_space_unset_for(RAnal *a, int idx);
+
+R_API bool r_sign_foreach(RAnal *a, RSignForeachCallback cb, void *user);
+
+R_API RSignSearch *r_sign_search_new();
+R_API void r_sign_search_free(RSignSearch *ss);
+R_API void r_sign_search_init(RAnal *a, RSignSearch *ss, RSignSearchCallback cb, void *user);
+R_API int r_sign_search_update(RAnal *a, RSignSearch *ss, ut64 *at, const ut8 *buf, int len);
+
+R_API RSignItem *r_sign_item_dup(RSignItem *it);
 R_API void r_sign_item_free(void *_item);
 #endif
 
