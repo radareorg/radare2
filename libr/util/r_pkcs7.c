798984a0861f62bcc2a99f1569a698e54306267a
pancake <pancake@nopcode.org>
2017-03-14 17:20:30 +0100

    Fix #7022 - oobread in pkcs triggered with a PE

--- libr/util/r_pkcs7.c
+++ libr/util/r_pkcs7.c
@@ -117,7 +117,7 @@ bool r_pkcs7_parse_contentinfo (RPKCS7ContentInfo* ci, RASN1Object *object) {
 	}
 
 	ci->contentType = r_asn1_stringify_oid (object->list.objects[0]->sector, object->list.objects[0]->length);
-	if (object->list.length == 2 || !object->list.objects[1]) {
+	if (object->list.length > 1) {
 		R_PTR_MOVE (ci->content, object->list.objects[1]);
 	}
 
