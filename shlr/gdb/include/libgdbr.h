574828c9c75042cc4cc2a1a3bdc3c1c5f58d4dd8
Srimanta Barua <srimanta.barua1@gmail.com>
2017-03-16 23:13:15 +0530

    Fix indent, brackets. Use bool instead of bitflags

--- shlr/gdb/include/libgdbr.h
+++ shlr/gdb/include/libgdbr.h
@@ -26,8 +26,8 @@
  */
 typedef struct libgdbr_message_t {
 	ssize_t len; /*! Len of the message */
-	char* msg;	/*! Pointer to the buffer that contains the message */
-	uint8_t chk;	/*! Cheksum of the current message read from the packet */
+	char *msg;      /*! Pointer to the buffer that contains the message */
+	uint8_t chk;    /*! Cheksum of the current message read from the packet */
 } libgdbr_message_t;
 
 /*!
@@ -35,58 +35,58 @@ typedef struct libgdbr_message_t {
  */
 
 typedef struct libgdbr_stub_features_t {
-	ssize_t pkt_sz; /* Max packet size */
-	unsigned qXfer_btrace_read : 1;
-	unsigned qXfer_btrace_conf_read : 1;
-	unsigned qXfer_spu_read : 1;
-	unsigned qXfer_spu_write : 1;
-	unsigned qXfer_libraries_read : 1;
-	unsigned qXfer_libraries_svr4_read : 1;
-	unsigned qXfer_siginfo_read : 1;
-	unsigned qXfer_siginfo_write : 1;
-	unsigned qXfer_auxv_read : 1;
-	unsigned qXfer_exec_file_read : 1;
-	unsigned qXfer_features_read : 1;
-	unsigned qXfer_memory_map_read : 1;
-	unsigned qXfer_sdata_read : 1;
-	unsigned qXfer_threads_read : 1;
-	unsigned qXfer_traceframe_info_read : 1;
-	unsigned qXfer_uib_read : 1;
-	unsigned qXfer_fdpic_read : 1;
-	unsigned qXfer_osdata_read : 1;
-	unsigned Qbtrace_off : 1;
-	unsigned Qbtrace_bts : 1;
-	unsigned Qbtrace_pt : 1;
-	unsigned Qbtrace_conf_bts_size : 1;
-	unsigned Qbtrace_conf_pt_size: 1;
-	unsigned QNonStop : 1;
-	unsigned QCatchSyscalls : 1;
-	unsigned QPassSignals : 1;
-	unsigned QStartNoAckMode : 1;
-	unsigned QAgent : 1;
-	unsigned QAllow : 1;
-	unsigned QDisableRandomization : 1;
-	unsigned QTBuffer_size : 1;
-	unsigned QThreadEvents : 1;
-	unsigned StaticTracepoint : 1;
-	unsigned InstallInTrace : 1;
-	unsigned ConditionalBreakpoints : 1;
-	unsigned ConditionalTracepoints : 1;
-	unsigned ReverseContinue : 1;
-	unsigned ReverseStep : 1;
-	unsigned swbreak : 1;
-	unsigned hwbreak : 1;
-	unsigned fork_events : 1;
-	unsigned vfork__events : 1;
-	unsigned exec_events : 1;
-	unsigned vContSupported : 1;
-	unsigned no_resumed : 1;
-	unsigned augmented_libraries_svr4_read : 1;
-	unsigned multiprocess : 1;
-	unsigned TracepointSource : 1;
-	unsigned EnableDisableTracepoints : 1;
-	unsigned tracenz : 1;
-	unsigned BreakpointCommands : 1;
+	ut32 pkt_sz; /* Max packet size */
+	bool qXfer_btrace_read;
+	bool qXfer_btrace_conf_read;
+	bool qXfer_spu_read;
+	bool qXfer_spu_write;
+	bool qXfer_libraries_read;
+	bool qXfer_libraries_svr4_read;
+	bool qXfer_siginfo_read;
+	bool qXfer_siginfo_write;
+	bool qXfer_auxv_read;
+	bool qXfer_exec_file_read;
+	bool qXfer_features_read;
+	bool qXfer_memory_map_read;
+	bool qXfer_sdata_read;
+	bool qXfer_threads_read;
+	bool qXfer_traceframe_info_read;
+	bool qXfer_uib_read;
+	bool qXfer_fdpic_read;
+	bool qXfer_osdata_read;
+	bool Qbtrace_off;
+	bool Qbtrace_bts;
+	bool Qbtrace_pt;
+	bool Qbtrace_conf_bts_size;
+	bool Qbtrace_conf_pt_size;
+	bool QNonStop;
+	bool QCatchSyscalls;
+	bool QPassSignals;
+	bool QStartNoAckMode;
+	bool QAgent;
+	bool QAllow;
+	bool QDisableRandomization;
+	bool QTBuffer_size;
+	bool QThreadEvents;
+	bool StaticTracepoint;
+	bool InstallInTrace;
+	bool ConditionalBreakpoints;
+	bool ConditionalTracepoints;
+	bool ReverseContinue;
+	bool ReverseStep;
+	bool swbreak;
+	bool hwbreak;
+	bool fork_events;
+	bool vfork__events;
+	bool exec_events;
+	bool vContSupported;
+	bool no_resumed;
+	bool augmented_libraries_svr4_read;
+	bool multiprocess;
+	bool TracepointSource;
+	bool EnableDisableTracepoints;
+	bool tracenz;
+	bool BreakpointCommands;
 } libgdbr_stub_features_t;
 
 /*!
@@ -114,27 +114,27 @@ typedef struct libgdbr_fstat_t {
  * the instance of the lib
  */
 typedef struct libgdbr_t {
-	char* send_buff; // defines a buffer for reading and sending stuff
+	char *send_buff; // defines a buffer for reading and sending stuff
 	ssize_t send_len;
 	ssize_t send_max; // defines the maximal len for the given buffer
-	char* read_buff;
+	char *read_buff;
 	ssize_t read_max; // defines the maximal len for the given buffer
 
 	// is already handled (i.e. already send or ...)
-	RSocket* sock;
+	RSocket *sock;
 	int connected;
 	int acks;
-	char* data;
+	char *data;
 	ssize_t data_len;
 	ssize_t data_max;
 	uint8_t architecture;
-	registers_t* registers;
+	registers_t *registers;
 	int last_code;
 	ssize_t pid; // little endian
 	ssize_t tid; // little endian
 	bool attached; // Remote server attached to process or created
 	libgdbr_stub_features_t stub_features;
-	char* exec_file_name;
+	char *exec_file_name;
 	int exec_fd;
 	uint64_t exec_file_sz;
 } libgdbr_t;
@@ -143,20 +143,20 @@ typedef struct libgdbr_t {
  * \brief Function initializes the libgdbr lib
  * \returns a failure code (currently -1) or 0 if call successfully
  */
-int gdbr_init(libgdbr_t* g);
+int gdbr_init(libgdbr_t *g);
 
 /*!
  * \brief Function initializes the architecture of the gdbsession
  * \param architecture defines the architecure used (registersize, and such)
  * \returns a failure code
  */
-int gdbr_set_architecture(libgdbr_t* g, uint8_t architecture);
+int gdbr_set_architecture(libgdbr_t *g, uint8_t architecture);
 
 /*!
  * \brief frees all buffers and cleans the libgdbr instance stuff
  * \returns a failure code (currently -1) or 0 if call successfully
  */
-int gdbr_cleanup(libgdbr_t* g);
+int gdbr_cleanup(libgdbr_t *g);
 
 /*!
  * \brief Function connects to a gdbserver instance
@@ -164,18 +164,18 @@ int gdbr_cleanup(libgdbr_t* g);
  * \param number that represents the port
  * \returns a failure code (currently -1) or 0 if call successfully
  */
-int gdbr_connect(libgdbr_t* g, const char* server, int port);
+int gdbr_connect(libgdbr_t *g, const char *server, int port);
 
 /*!
  * \brief disconnects the lib
  * \returns a failure code (currently -1) or 0 if call successfully
  */
-int gdbr_disconnect(libgdbr_t* g);
+int gdbr_disconnect(libgdbr_t *g);
 
 // Commands
-int gdbr_continue(libgdbr_t* g, int thread_id);
-int gdbr_step(libgdbr_t* g, int thread_id);
-int gdbr_read_registers(libgdbr_t* g);
+int gdbr_continue(libgdbr_t *g, int thread_id);
+int gdbr_step(libgdbr_t *g, int thread_id);
+int gdbr_read_registers(libgdbr_t *g);
 
 /*!
  * \brief Function writes general purpose registers
@@ -185,14 +185,14 @@ int gdbr_read_registers(libgdbr_t* g);
  * i.e. eax=0x123,ebx=0x234
  * \returns a failurre code (currently -1) or 0 if call successfully
  */
-int gdbr_write_bin_registers(libgdbr_t* g);
-int gdbr_write_reg(libgdbr_t* g, const char* name, char* value, int len);
-int gdbr_write_register(libgdbr_t* g, int index, char* value, int len);
-int gdbr_write_registers(libgdbr_t* g, char* registers);
-int gdbr_read_memory(libgdbr_t* g, ut64 address, ut64 len);
-int gdbr_write_memory(libgdbr_t* g, ut64 address, const uint8_t* data, ut64 len);
-int gdbr_send_command(libgdbr_t* g, char* command);
-int test_command(libgdbr_t* g, const char* command);
+int gdbr_write_bin_registers(libgdbr_t *g);
+int gdbr_write_reg(libgdbr_t *g, const char *name, char *value, int len);
+int gdbr_write_register(libgdbr_t *g, int index, char *value, int len);
+int gdbr_write_registers(libgdbr_t *g, char *registers);
+int gdbr_read_memory(libgdbr_t *g, ut64 address, ut64 len);
+int gdbr_write_memory(libgdbr_t *g, ut64 address, const uint8_t *data, ut64 len);
+int gdbr_send_command(libgdbr_t *g, char *command);
+int test_command(libgdbr_t *g, const char *command);
 
 /*!
  * \brief Function sets normal breakpoint (0xcc, int3)
@@ -201,9 +201,9 @@ int test_command(libgdbr_t* g, const char* command);
  * \param conditions TODO: examine how this condition string should look like
  * \returns a failure code (currently -1) or 0 if call successfully
  */
-int gdbr_set_bp(libgdbr_t* g, ut64 address, const char* conditions);
-int gdbr_set_hwbp(libgdbr_t* g, ut64 address, const char* conditions);
-int gdbr_remove_bp(libgdbr_t* g, ut64 address);
-int gdbr_remove_hwbp(libgdbr_t* g, ut64 address);
+int gdbr_set_bp(libgdbr_t *g, ut64 address, const char *conditions);
+int gdbr_set_hwbp(libgdbr_t *g, ut64 address, const char *conditions);
+int gdbr_remove_bp(libgdbr_t *g, ut64 address);
+int gdbr_remove_hwbp(libgdbr_t *g, ut64 address);
 
 #endif
--- shlr/gdb/src/messages.c
+++ shlr/gdb/src/messages.c
@@ -6,7 +6,7 @@
 #include "utils.h"
 
 
-int handle_g(libgdbr_t* g) {
+int handle_g(libgdbr_t *g) {
 	if (unpack_hex (g->data, g->data_len, g->data) < 0) {
 		return -1;
 	}
@@ -14,38 +14,37 @@ int handle_g(libgdbr_t* g) {
 	return send_ack (g);
 }
 
-int handle_G(libgdbr_t* g) {
+int handle_G(libgdbr_t *g) {
 	return send_ack (g);
 }
 
-int handle_M(libgdbr_t* g) {
+int handle_M(libgdbr_t *g) {
 	return send_ack (g);
 }
 
-int handle_P(libgdbr_t* g) {
+int handle_P(libgdbr_t *g) {
 	if (g->data_len == 0) {
 		g->last_code = MSG_NOT_SUPPORTED;
-	}
-	else {
+	} else {
 		g->last_code = MSG_OK;
 	}
 	return send_ack (g);
 }
 
-int handle_m(libgdbr_t* g) {
+int handle_m(libgdbr_t *g) {
 	int len = strlen (g->data);
 	g->data_len = strlen (g->data) / 2;
 	unpack_hex (g->data, len, g->data);
 	return send_ack (g);
 }
 
-int handle_cmd(libgdbr_t* g) {
+int handle_cmd(libgdbr_t *g) {
 	unpack_hex (g->data, strlen (g->data), g->data);
 	g->data_len = strlen (g->data) / 2;
 	return send_ack (g);
 }
 
-int handle_qStatus(libgdbr_t* g) {
+int handle_qStatus(libgdbr_t *g) {
 	char *tok = NULL;
 	tok = strtok (g->data, ";");
 	// TODO: We do not yet handle the case where a trace is already running
@@ -58,13 +57,13 @@ int handle_qStatus(libgdbr_t* g) {
 		if (!strncmp (tok, "tnotrun:0", 9)) {
 			return send_ack (g);
 		}
-	    tok = strtok (NULL, ";");
+		tok = strtok (NULL, ";");
 	}
 	send_ack (g);
 	return -1;
 }
 
-int handle_qC(libgdbr_t* g) {
+int handle_qC(libgdbr_t *g) {
 	char *t1, *t2;
 	// We get process and thread ID
 	if (strncmp (g->data, "QC", 2)) {
@@ -86,7 +85,7 @@ int handle_qC(libgdbr_t* g) {
 	return send_ack (g);
 }
 
-int handle_execFileRead (libgdbr_t* g) {
+int handle_execFileRead(libgdbr_t *g) {
 	if (g->data[0] == 'E') {
 		send_ack (g);
 		return -1;
@@ -100,7 +99,7 @@ int handle_execFileRead (libgdbr_t* g) {
 	return send_ack (g);
 }
 
-int handle_fOpen (libgdbr_t* g) {
+int handle_fOpen(libgdbr_t *g) {
 	if (!*g->data || g->data[0] != 'F') {
 		send_ack (g);
 		return -1;
@@ -109,7 +108,7 @@ int handle_fOpen (libgdbr_t* g) {
 	return send_ack (g);
 }
 
-int handle_fstat(libgdbr_t* g) {
+int handle_fstat(libgdbr_t *g) {
 	if (!*g->data || g->data[0] != 'F' || g->data[1] == '-') {
 		send_ack (g);
 		return -1;
@@ -125,126 +124,122 @@ int handle_fstat(libgdbr_t* g) {
 		return -1;
 	}
 /*
-	libgdbr_fstat_t *fstat = (libgdbr_fstat_t*) (ptr + 1);
-	g->exec_file_sz = 0;
-	unsigned char *c = &fstat->size;
-	for (int i = 0; i < 8; i++) {
-		g->exec_file_sz <<= 4;
-		g->exec_file_sz |= *c;
-	}
-*/
+        libgdbr_fstat_t *fstat = (libgdbr_fstat_t*) (ptr + 1);
+        g->exec_file_sz = 0;
+        unsigned char *c = &fstat->size;
+        for (int i = 0; i < 8; i++) {
+                g->exec_file_sz <<= 4;
+                g->exec_file_sz |= *c;
+        }
+ */
 	return send_ack (g);
 }
 
-int handle_qSupported(libgdbr_t* g) {
+int handle_qSupported(libgdbr_t *g) {
 	// TODO handle the message correct and set all infos like packetsize, thread stuff and features
 	char *tok = NULL;
 	tok = strtok (g->data, ";");
 	while (tok != NULL) {
-		if (!strncmp (tok, "PacketSize=", 11)) {
-			char temp_buf[20] = { 0 };
-			temp_buf[0] = '0';
-			temp_buf[1] = 'x';
-			snprintf (temp_buf + 2, 16, "%s", tok + 11);
-			g->stub_features.pkt_sz = strtoul (temp_buf, NULL, 16);
-		} else if (!strncmp (tok, "qXfer:", 6)) {
+		if (r_str_startswith (tok, "PacketSize=")) {
+			g->stub_features.pkt_sz = strtoul (tok + strlen ("PacketSize="), NULL, 16);
+		} else if (r_str_startswith (tok, "qXfer:")) {
 			if (!tok[6]) {
-				tok = strtok(NULL, ";");
+				tok = strtok (NULL, ";");
 				continue;
 			}
 			char *p = tok + 6;
-			if (!strncmp (p, "btrace:read", 11)) {
-				g->stub_features.qXfer_btrace_read = (p[11] == '+') ? 1 : 0;
-			} else if (!strncmp (p, "btrace-conf:read", 16)) {
-				g->stub_features.qXfer_btrace_conf_read = (p[16] == '+') ? 1 : 0;
-			} else if (!strncmp (p, "spu:read", 8)) {
-				g->stub_features.qXfer_spu_read = (p[8] == '+') ? 1 : 0;
-			} else if (!strncmp (p, "spu:write", 9)) {
-				g->stub_features.qXfer_spu_write = (p[9] == '+') ? 1 : 0;
-			} else if (!strncmp (p, "libraries:read", 14)) {
-				g->stub_features.qXfer_libraries_read = (p[14] == '+') ? 1 : 0;
-			} else if (!strncmp (p, "libraries-svr4:read", 19)) {
-				g->stub_features.qXfer_libraries_svr4_read = (p[19] == '+') ? 1 : 0;
-			} else if (!strncmp (p, "memory-map:read", 15)) {
-				g->stub_features.qXfer_memory_map_read = (p[15] == '+') ? 1 : 0;
-			} else if (!strncmp (p, "auxv:read", 9)) {
-				g->stub_features.qXfer_auxv_read = (p[9] == '+') ? 1 : 0;
-			} else if (!strncmp (p, "exec-file:read", 14)) {
-				g->stub_features.qXfer_exec_file_read = (p[14] == '+') ? 1 : 0;
-			} else if (!strncmp (p, "features:read", 13)) {
-				g->stub_features.qXfer_features_read = (p[13] == '+') ? 1 : 0;
-			} else if (!strncmp (p, "sdata:read", 10)) {
-				g->stub_features.qXfer_sdata_read = (p[10] == '+') ? 1 : 0;
-			} else if (!strncmp (p, "siginfo:read", 12)) {
-				g->stub_features.qXfer_siginfo_read = (p[12] == '+') ? 1 : 0;
-			} else if (!strncmp (p, "siginfo:write", 13)) {
-				g->stub_features.qXfer_siginfo_write = (p[13] == '+') ? 1 : 0;
-			} else if (!strncmp (p, "threads:read", 12)) {
-				g->stub_features.qXfer_threads_read = (p[12] == '+') ? 1 : 0;
-			} else if (!strncmp (p, "traceframe-info:read", 20)) {
-				g->stub_features.qXfer_traceframe_info_read = (p[20] == '+') ? 1 : 0;
-			} else if (!strncmp (p, "uib:read", 8)) {
-				g->stub_features.qXfer_uib_read = (p[8] == '+') ? 1 : 0;
-			} else if (!strncmp (p, "fdpic:read", 10)) {
-				g->stub_features.qXfer_fdpic_read = (p[10] == '+') ? 1 : 0;
-			} else if (!strncmp (p, "osdata:read", 11)) {
-				g->stub_features.qXfer_osdata_read = (p[11] == '+') ? 1 : 0;
+			if (r_str_startswith (p, "btrace:read")) {
+				g->stub_features.qXfer_btrace_read = (p[strlen ("btrace:read")] == '+');
+			} else if (r_str_startswith (p, "btrace-conf:read")) {
+				g->stub_features.qXfer_btrace_conf_read = (p[strlen ("btrace-conf:read")] == '+');
+			} else if (r_str_startswith (p, "spu:read")) {
+				g->stub_features.qXfer_spu_read = (p[strlen ("spu:read")] == '+');
+			} else if (r_str_startswith (p, "spu:write")) {
+				g->stub_features.qXfer_spu_write = (p[strlen ("spu:write")] == '+');
+			} else if (r_str_startswith (p, "libraries:read")) {
+				g->stub_features.qXfer_libraries_read = (p[strlen ("libraries:read")] == '+');
+			} else if (r_str_startswith (p, "libraries-svr4:read")) {
+				g->stub_features.qXfer_libraries_svr4_read = (p[strlen ("libraries-svr4:read")] == '+');
+			} else if (r_str_startswith (p, "memory-map:read")) {
+				g->stub_features.qXfer_memory_map_read = (p[strlen ("memory-map:read")] == '+');
+			} else if (r_str_startswith (p, "auxv:read")) {
+				g->stub_features.qXfer_auxv_read = (p[strlen ("auxv:read")] == '+');
+			} else if (r_str_startswith (p, "exec-file:read")) {
+				g->stub_features.qXfer_exec_file_read = (p[strlen ("exec-file:read")] == '+');
+			} else if (r_str_startswith (p, "features:read")) {
+				g->stub_features.qXfer_features_read = (p[strlen ("features:read")] == '+');
+			} else if (r_str_startswith (p, "sdata:read")) {
+				g->stub_features.qXfer_sdata_read = (p[strlen ("sdata:read")] == '+');
+			} else if (r_str_startswith (p, "siginfo:read")) {
+				g->stub_features.qXfer_siginfo_read = (p[strlen ("siginfo:read")] == '+');
+			} else if (r_str_startswith (p, "siginfo:write")) {
+				g->stub_features.qXfer_siginfo_write = (p[strlen ("siginfo:write")] == '+');
+			} else if (r_str_startswith (p, "threads:read")) {
+				g->stub_features.qXfer_threads_read = (p[strlen ("threads:read")] == '+');
+			} else if (r_str_startswith (p, "traceframe-info:read")) {
+				g->stub_features.qXfer_traceframe_info_read = (p[strlen ("traceframe-info:read")] == '+');
+			} else if (r_str_startswith (p, "uib:read")) {
+				g->stub_features.qXfer_uib_read = (p[strlen ("uib:read")] == '+');
+			} else if (r_str_startswith (p, "fdpic:read")) {
+				g->stub_features.qXfer_fdpic_read = (p[strlen ("fdpic:read")] == '+');
+			} else if (r_str_startswith (p, "osdata:read")) {
+				g->stub_features.qXfer_osdata_read = (p[strlen ("osdata:read")] == '+');
 			}
 		} else if (tok[0] == 'Q') {
-			if (!strncmp (tok, "Qbtrace", 7)) {
-				if (!tok[7]) {
-					tok = strtok(NULL, ";");
+			if (r_str_startswith (tok, "Qbtrace")) {
+				if (!tok[strlen ("Qbtrace")]) {
+					tok = strtok (NULL, ";");
 					continue;
 				}
 				char *p = tok + 7;
-				if (!strncmp (p, ":off", 4)) {
-					g->stub_features.Qbtrace_off = (p[4] == '+') ? 1 : 0;
-				} else if (!strncmp (p, ":bts", 4)) {
-					g->stub_features.Qbtrace_bts = (p[4] == '+') ? 1 : 0;
-				} else if (!strncmp (p, ":pt", 3)) {
-					g->stub_features.Qbtrace_pt = (p[3] == '+') ? 1 : 0;
-				} else if (!strncmp (p, "-conf:bts:size", 14)) {
-					g->stub_features.Qbtrace_conf_bts_size = (p[14] == '+') ? 1 : 0;
-				} else if (!strncmp (p, ":-conf:pt:size", 14)) {
-					g->stub_features.Qbtrace_conf_pt_size = (p[14] == '+') ? 1 : 0;
+				if (r_str_startswith (p, ":off")) {
+					g->stub_features.Qbtrace_off = (p[4] == '+');
+				} else if (r_str_startswith (p, ":bts")) {
+					g->stub_features.Qbtrace_bts = (p[4] == '+');
+				} else if (r_str_startswith (p, ":pt")) {
+					g->stub_features.Qbtrace_pt = (p[3] == '+');
+				} else if (r_str_startswith (p, "-conf:bts:size")) {
+					g->stub_features.Qbtrace_conf_bts_size = (p[strlen ("-conf:bts:size")] == '+');
+				} else if (r_str_startswith (p, ":-conf:pt:size")) {
+					g->stub_features.Qbtrace_conf_pt_size = (p[strlen ("-conf:pt:size")] == '+');
 				}
-			} else if (!strncmp (tok, "QNonStop", 8)) {
-				g->stub_features.QNonStop = (tok[8] == '+') ? 1 : 0;
-			} else if (!strncmp (tok, "QCatchSyscalls", 14)) {
-				g->stub_features.QCatchSyscalls = (tok[14] == '+') ? 1 : 0;
-			} else if (!strncmp (tok, "QPassSignals", 12)) {
-				g->stub_features.QPassSignals = (tok[12] == '+') ? 1 : 0;
-			} else if (!strncmp (tok, "QStartNoAckMode", 15)) {
-				g->stub_features.QStartNoAckMode = (tok[15] == '+') ? 1 : 0;
-			} else if (!strncmp (tok, "QAgent", 6)) {
-				g->stub_features.QAgent = (tok[6] == '+') ? 1 : 0;
-			} else if (!strncmp (tok, "QAllow", 6)) {
-				g->stub_features.QAllow = (tok[6] == '+') ? 1 : 0;
-			} else if (!strncmp (tok, "QDisableRandomization", 21)) {
-				g->stub_features.QDisableRandomization = (tok[21] == '+') ? 1 : 0;
-			} else if (!strncmp (tok, "QTBuffer:size", 13)) {
-				g->stub_features.QTBuffer_size = (tok[13] == '+') ? 1 : 0;
-			} else if (!strncmp (tok, "QThreadEvents", 13)) {
-				g->stub_features.QThreadEvents = (tok[13] == '+') ? 1 : 0;
+			} else if (r_str_startswith (tok, "QNonStop")) {
+				g->stub_features.QNonStop = (tok[strlen ("QNonStop")] == '+');
+			} else if (r_str_startswith (tok, "QCatchSyscalls")) {
+				g->stub_features.QCatchSyscalls = (tok[strlen ("QCatchSyscalls")] == '+');
+			} else if (r_str_startswith (tok, "QPassSignals")) {
+				g->stub_features.QPassSignals = (tok[strlen ("QPassSignals")] == '+');
+			} else if (r_str_startswith (tok, "QStartNoAckMode")) {
+				g->stub_features.QStartNoAckMode = (tok[strlen ("QStartNoAckMode")] == '+');
+			} else if (r_str_startswith (tok, "QAgent")) {
+				g->stub_features.QAgent = (tok[strlen ("QAgent")] == '+');
+			} else if (r_str_startswith (tok, "QAllow")) {
+				g->stub_features.QAllow = (tok[strlen ("QAllow")] == '+');
+			} else if (r_str_startswith (tok, "QDisableRandomization")) {
+				g->stub_features.QDisableRandomization = (tok[strlen ("QDisableRandomization")] == '+');
+			} else if (r_str_startswith (tok, "QTBuffer:size")) {
+				g->stub_features.QTBuffer_size = (tok[strlen ("QTBuffer:size")] == '+');
+			} else if (r_str_startswith (tok, "QThreadEvents")) {
+				g->stub_features.QThreadEvents = (tok[strlen ("QThreadEvents")] == '+');
 			}
-		} else if (!strncmp (tok, "multiprocess", 12)) {
-		    g->stub_features.multiprocess = (tok[12] == '+') ? 1 : 0;
+		} else if (r_str_startswith (tok, "multiprocess")) {
+			g->stub_features.multiprocess = (tok[strlen ("multiprocess")] == '+');
 		}
 		// TODO
-		tok = strtok(NULL, ";");
+		tok = strtok (NULL, ";");
 	}
 	return send_ack (g);
 }
 
-int handle_cont(libgdbr_t* g) {
+int handle_cont(libgdbr_t *g) {
 	// Possible answers here 'S,T,W,X,O,F'
 	return send_ack (g);
 }
 
-int handle_setbp(libgdbr_t* g) {
+int handle_setbp(libgdbr_t *g) {
 	return send_ack (g);
 }
 
-int handle_removebp(libgdbr_t* g) {
+int handle_removebp(libgdbr_t *g) {
 	return send_ack (g);
 }
