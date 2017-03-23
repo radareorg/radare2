884b1b913eccf6419a5e666344ec7f2f76078952
Roi Martin <jroi.martin@gmail.com>
2017-03-23 18:00:34 +0000

    Change default zign.min to 16 bytes

--- libr/core/cconfig.c
+++ libr/core/cconfig.c
@@ -1922,7 +1922,7 @@ R_API int r_core_config_init(RCore *core) {
 	SETCB("cfg.sandbox", "false", &cb_cfgsanbox, "Sandbox mode disables systems and open on upper directories");
 	SETPREF("cfg.wseek", "false", "Seek after write");
 	SETCB("cfg.bigendian", "false", &cb_bigendian, "Use little (false) or big (true) endianness");
-	SETI("zign.min", 8, "Minimum zignature length");
+	SETI("zign.min", 16, "Minimum zignature length");
 	SETI("zign.max", 500, "Maximum zignature length");
 	SETPREF("zign.prefix", "sign", "Default prefix for zignatures matches");
 
