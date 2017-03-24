3aa18670ec3277e23f3a168bbc79e2149e6e80e7
Sven Steinbauer <sven@unlogic.co.uk>
2017-03-24 11:22:04 +0000

    Support assembling the BND prefix on x86.nz branch instructions

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -116,6 +116,7 @@ typedef struct Opcode_t {
 	ut8 opcode[3];
 	int operands_count;
 	Operand operands[2];
+	bool has_bnd;
 } Opcode;
 
 static ut8 getsib(const ut8 sib) {
@@ -1610,7 +1611,7 @@ static int opout(RAsm *a, ut8 *data, const Opcode op) {
 static int opret(RAsm *a, ut8 *data, const Opcode op) {
 	int l = 0;
 	int immediate = 0;
-	 if (op.operands[0].type == OT_UNKNOWN) {
+	if (op.operands[0].type == OT_UNKNOWN) {
 		data[l++] = 0xc3;
 	} else if (op.operands[0].type & (OT_CONSTANT | OT_WORD)) {
 		data[l++] = 0xc2;
@@ -2396,6 +2397,11 @@ static int parseOperand(RAsm *a, const char *str, Operand *op) {
 }
 
 static int parseOpcode(RAsm *a, const char *op, Opcode *out) {
+	out->has_bnd = false;
+	if (!strncmp (op, "bnd ", 4)) {
+		out->has_bnd = true;
+		op += 4;
+	}
 	char *args = strchr (op, ' ');
 	out->mnemonic = args ? r_str_ndup (op, args - op) : strdup (op);
 	out->operands[0].type = out->operands[1].type = 0;
@@ -2466,6 +2472,7 @@ static int assemble(RAsm *a, RAsmOp *ao, const char *str) {
 	ut8 *data = ao->buf;
 	char op[128];
 	LookupTable *lt_ptr;
+	int retval;
 
 	if (a->bits == 16) {
 		return assemble16 (a, ao, str);
@@ -2487,7 +2494,17 @@ static int assemble(RAsm *a, RAsmOp *ao, const char *str) {
 				return lt_ptr->size;
 			} else {
 				if (lt_ptr->opdo) {
-					return lt_ptr->opdo (a, data, instr);
+					if (instr.has_bnd) {
+						data[0] = 0xf2;
+						data ++;
+					}
+					retval = lt_ptr->opdo (a, data, instr);
+					// if op supports bnd then the first byte will
+					// be 0xf2.
+					if (instr.has_bnd) {
+						retval++;
+					}
+					return retval;
 				}
 				break;
 			}
