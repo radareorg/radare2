2642a03f619664f02ed3efe94f726baf2609b450
Der0ad <wargio@libero.it>
2017-03-27 11:16:24 +0200

    fixed null deref on cms

--- libr/util/r_pkcs7.c
+++ libr/util/r_pkcs7.c
@@ -284,7 +284,9 @@ RCMS *r_pkcs7_parse_cms (const ut8 *buffer, ut32 length) {
 		return NULL;
 	}
 	object = r_asn1_create_object (buffer, length);
-	if (!object || object->list.length != 2 || !object->list.objects[0] || object->list.objects[1]->list.length != 1) {
+	if (!object || object->list.length != 2 || !object->list.objects ||
+		!object->list.objects[0] || !object->list.objects[1] ||
+		object->list.objects[1]->list.length != 1) {
 		r_asn1_free_object (object);
 		free (container);
 		return NULL;
