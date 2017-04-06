9a1190c112ff2b07580129cedf105d26370ba44b
Ren Kimura <rkx1209dev@gmail.com>
2017-04-06 22:59:19 +0900

    Add bp_plugin_del and dbh- command (#7209)

--- libr/core/cmd_debug.c
+++ libr/core/cmd_debug.c
@@ -2098,6 +2098,7 @@ static void r_core_cmd_bp(RCore *core, const char *input) {
 		"dbits", " <index>", "Swap Nth breakpoint trace",
 		//
 		"dbh", " x86", "Set/list breakpoint plugin handlers",
+		"dbh-", " <name>", "Remove breakpoint plugin handler",
 		"drx", " number addr len rwx", "Modify hardware breakpoint",
 		"drx-", "number", "Clear hardware breakpoint",
 		NULL};
@@ -2468,6 +2469,11 @@ static void r_core_cmd_bp(RCore *core, const char *input) {
 		case ' ':
 			if (!r_bp_use (core->dbg->bp, input + 3, core->anal->bits))
 				eprintf ("Invalid name: '%s'.\n", input + 3);
+			break;
+		case '-':
+			if (!r_bp_plugin_del (core->dbg->bp, input + 4)) {
+				eprintf ("Invalid name: '%s'.\n", input + 4);
+			}
 			break;
 		case '?':
 		default:
