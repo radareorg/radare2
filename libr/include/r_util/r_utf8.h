dc4c6e11fb6896ffd80e72fed3f2bea44c82f34a
marcel <marcel.bathke@googlemail.com>
2017-04-12 23:09:52 +0200

    added utf16toutf8/utf8toutf16

--- libr/include/r_util/r_utf8.h
+++ libr/include/r_util/r_utf8.h
@@ -9,4 +9,6 @@ R_API int r_utf8_encode_str(const RRune *str, ut8 *dst, const int dst_length);
 R_API int r_utf8_size(const ut8 *ptr);
 R_API int r_utf8_strlen(const ut8 *str);
 R_API int r_isprint(const RRune c);
+R_API char *r_utf16_to_utf8(const wchar_t *wc);
+R_API wchar_t *r_utf8_to_utf16(const char *cstring);
 #endif //  R_UTF8_H
