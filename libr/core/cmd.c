69af25afddeb65cd61cf02d5e2a6ada45fd2343f
Pepe Vila <pvtolkien@gmail.com>
2017-04-16 01:44:23 +0200

    display the help for the % command

--- libr/core/cmd.c
+++ libr/core/cmd.c
@@ -1105,7 +1105,24 @@ static int cmd_pointer(void *data, const char *input) {
 }
 
 static int cmd_env(void *data, const char *input) {
-	return r_core_cmdf ((RCore*)data, "env %s", input);
+	RCore *core = (RCore*)data;
+	int ret = true;
+	switch (*input) {
+	case '?':
+		{
+		const char* help_msg[] = {
+			"Usage:", "%[name[=value]]", "Set each NAME to VALUE in the environment",
+			"%", "", "list all environment variables",
+			"%", "SHELL", "prints SHELL value",
+			"%", "TMPDIR=/tmp", "sets TMPDIR value to \"/tmp\"",
+			NULL};
+		r_core_cmd_help (core, help_msg);
+		}
+		break;
+	default:
+		ret = r_core_cmdf (core, "env %s", input);
+	}
+	return ret;
 }
 
 static int cmd_system(void *data, const char *input) {
