29d4faf23421873c0fde50d8b3753bfadb8faa45
h4ng3r <h4ng3r@users.noreply.github.com>
2017-05-06 21:39:20 +0200

    Fix #7376

--- libr/core/cconfig.c
+++ libr/core/cconfig.c
@@ -2010,6 +2010,7 @@ R_API int r_core_config_init(RCore *core) {
 	SETPREF ("asm.marks", "true", "Show marks before the disassembly");
 	SETPREF ("asm.cmtrefs", "false", "Show flag and comments from refs in disasm");
 	SETPREF ("asm.cmtpatch", "false", "Show patch comments in disasm");
+	SETPREF ("asm.payloads", "false", "Show payload bytes in disasm");
 	SETCB ("bin.strpurge", "false", &cb_strpurge, "Try to purge false positive strings");
 	SETPREF ("bin.libs", "false", "Try to load libraries after loading main binary");
 	n = NODECB ("bin.strfilter", "", &cb_strfilter);
