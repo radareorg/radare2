60de019ad1324892f955470462e9681c0ff070b0
Juan Pablo Canepa <jpcanepa@synopsys.com>
2017-05-11 10:22:42 -0700

    Removed unneeded message from build system

--- libr/bin/p/vsf.mk
+++ libr/bin/p/vsf.mk
@@ -6,6 +6,5 @@ TARGET_VSF=bin_vsf.${EXT_SO}
 ALL_TARGETS+=${TARGET_VSF}
 
 ${TARGET_VSF}: ${OBJ_VSF}
-	echo "FUCKOFF"
 	${CC} $(call libname,bin_vsf) -shared ${CFLAGS} \
 		-o ${TARGET_VSF} ${OBJ_VSF} $(LINK) $(LDFLAGS)
