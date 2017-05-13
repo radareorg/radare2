44b17fe5153e885982fa1c493410288b59fcc035
deroad <wargio@libero.it>
2017-05-13 13:05:43 +0200

    asm.emustr

--- libr/core/pseudo.c
+++ libr/core/pseudo.c
@@ -154,7 +154,7 @@ R_API int r_core_pseudo_code(RCore *core, const char *input) {
 	r_config_save_num (hc, "asm.pseudo", "asm.decode", "asm.lines", "asm.bytes", NULL);
 	r_config_save_num (hc, "asm.offset", "asm.flags", "asm.fcnlines", "asm.comments", NULL);
 	r_config_save_num (hc, "asm.functions", "asm.section", "asm.cmtcol", "asm.filter", NULL);
-	r_config_save_num (hc, "scr.color", "io.cache", "asm.emu", "asm.emuwrite", NULL);
+	r_config_save_num (hc, "scr.color", "asm.emustr", "asm.emu", "asm.emuwrite", NULL);
 	if (!fcn) {
 		eprintf ("Cannot find function in 0x%08"PFMT64x"\n", core->offset);
 		r_config_hold_free (hc);
@@ -168,8 +168,8 @@ R_API int r_core_pseudo_code(RCore *core, const char *input) {
 	r_config_set_i (core->config, "asm.bytes", 0);
 	r_config_set_i (core->config, "asm.offset", 0);
 	r_config_set_i (core->config, "asm.flags", 0);
-	r_config_set_i (core->config, "io.cache", 1);
 	r_config_set_i (core->config, "asm.emu", 1);
+	r_config_set_i (core->config, "asm.emustr", 1);
 	r_config_set_i (core->config, "asm.emuwrite", 1);
 	r_config_set_i (core->config, "asm.fcnlines", 0);
 	r_config_set_i (core->config, "asm.comments", 1);
