ee95b8c1f52a399f41098752194d961338c04a5a
rene <rlaemmert@gmail.com>
2017-05-18 15:07:42 +0200

    avoid setting the value if the node-value is the exact same pointer

--- libr/config/config.c
+++ libr/config/config.c
@@ -385,6 +385,9 @@ R_API RConfigNode* r_config_set(RConfig *cfg, const char *name, const char *valu
 				node->value = strdup ("");
 				node->i_value = 0;
 			} else {
+				if (node->value == value) {
+					goto beach;
+				}
 				free (node->value);
 				node->value = strdup (value);
 				if (IS_DIGIT (*value)) {
