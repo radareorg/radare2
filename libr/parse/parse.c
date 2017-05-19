a4b311ebacc36ba0819b7b28f974c95ea25d4b68
Tobias Gross <tobias.gross@me.com>
2017-05-19 13:32:03 +0200

    Fix lea instruction not displayed right (#7529)

--- libr/parse/parse.c
+++ libr/parse/parse.c
@@ -275,7 +275,7 @@ static int filter(RParse *p, RFlag *f, char *data, char *str, int len, bool big_
 						const char *p = strchr (str, '[');
 						const char *a = strchr (str, '+');
 						const char *m = strchr (str, '*');
-						if (p && a && m) {
+						if (p && (a || m)) {
 							banned = true;
 						}
 					}
