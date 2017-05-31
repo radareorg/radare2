f8c493b59e7da50a2c66031c05d43498338e3dad
skuater <skuater@raspbian1.local>
2017-05-31 13:28:40 +0200

    fixed .bat

--- runmeson.bat
+++ runmeson.bat
@@ -5,24 +5,18 @@ git clone -b next --depth 10 http://github.com/aquynh/capstone.git shlr\capstone
 :START
 IF "%1"=="-p" GOTO BUILDPROJECT
 IF "%1"=="-r" GOTO REBUILD
-rem COPY libr\config.mk.meson libr\config.mk
-rem COPY libr\config.h.meson libr\config.h
 IF EXIST build GOTO BUILD
-"c:\Program Files (x86)\python 3.5\python.exe" "c:\Program Files (x86)\python 3.5\scripts\meson.py" --prefix=%CD% build
+python meson.py --prefix=%CD% build
 :BUILD
 ECHO [ R2 MESON NINJA BUILD ]
-rem copy build\config.h libr\include\config.h
-rem copy build\r_version.h libr\include\r_version.h
-rem copy build\r_userconf.h libr\include\r_userconf.h
-rem copy shlr\spp\config.def.h shlr\spp\config.h
+copy shlr\spp\config.def.h shlr\spp\config.h
 ninja -C build
 GOTO EXIT
 :BUILDPROJECT
 ECHO [ R2 MESON BUILDING VS2015 SLN]
 IF EXIST build rd /s /q build
-"c:\Program Files (x86)\python 3.5\python.exe" "c:\Program Files (x86)\python 3.5\scripts\meson.py" --prefix=%CD% build --backend=vs2015
+python meson.py --prefix=%CD% build --backend=vs2015
 GOTO EXIT
 :REBUILD
-"c:\Program Files (x86)\python 3.5\python.exe" "c:\Program Files (x86)\python 3.5\scripts\meson.py" --internal regenerate %CD% "%CD%\build" --backend ninja
-
+python.exe meson.py --internal regenerate %CD% "%CD%\build" --backend ninja
 :EXIT
