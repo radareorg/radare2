478952dca1f4a2ec83c92c9e506304af0481f896
Roi Martin <jroi.martin@gmail.com>
2017-06-03 12:16:12 +0000

    Rewrite autocompleteProcessPath()

--- libr/util/str.c
+++ libr/util/str.c
@@ -588,7 +588,7 @@ R_API char *r_str_ichr(char *str, char chr) {
 R_API const char *r_str_lchr(const char *str, char chr) {
 	if (str) {
 		int len = strlen (str);
-		for (;len >= 0; len--) {
+		for (; len >= 0; len--) {
 			if (str[len] == chr) {
 				return str + len;
 			}
