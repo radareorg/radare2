d5ede2bf44123f9bdf7cc1a2e0ac11451b05306a
pancake <pancake@nopcode.org>
2017-06-08 04:21:25 +0200

    Party reverts f85bc674b2a2256a364fe796351bc1971e106005.

--- libr/core/cbin.c
+++ libr/core/cbin.c
@@ -96,8 +96,7 @@ R_API int r_core_bin_set_env(RCore *r, RBinFile *binfile) {
 	RBinInfo *info = binobj ? binobj->info: NULL;
 	if (info) {
 		int va = info->has_va;
-		char * arch = strdup (info->arch? info->arch: R_SYS_ARCH);
-		char * cpu = info->cpu? strdup(info->cpu): NULL;
+		const char * arch = info->arch;
 		ut16 bits = info->bits;
 		ut64 baseaddr = r_bin_get_baddr (r->bin);
 		/* Hack to make baddr work on some corner */
@@ -107,16 +106,14 @@ R_API int r_core_bin_set_env(RCore *r, RBinFile *binfile) {
 		r_config_set (r->config, "asm.arch", arch);
 		r_config_set_i (r->config, "asm.bits", bits);
 		r_config_set (r->config, "anal.arch", arch);
-		if (cpu && *cpu) {
-			r_config_set (r->config, "anal.cpu", cpu);
+		if (info->cpu && *info->cpu) {
+			r_config_set (r->config, "anal.cpu", info->cpu);
 		} else {
 			r_config_set (r->config, "anal.cpu", arch);
 		}
 		r_asm_use (r->assembler, arch);
 		r_core_bin_info (r, R_CORE_BIN_ACC_ALL, R_CORE_BIN_SET, va, NULL, NULL);
 		r_core_bin_set_cur (r, binfile);
-		free (cpu);
-		free (arch);
 		return true;
 	}
 	return false;
