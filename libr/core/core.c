52c5c5b9777332a8805afc2ef0587746c3f862e4
Xiao Di Guan <xdg@puxlit.net>
2017-06-15 22:04:48 +1000

    Flush breakpoint command output on every hit (#3377)

--- libr/core/core.c
+++ libr/core/core.c
@@ -88,11 +88,21 @@ static int on_fcn_rename(void *_anal, void* _user, RAnalFunction *fcn, const cha
 
 static void r_core_debug_breakpoint_hit(RCore *core, RBreakpointItem *bpi) {
 	const char *cmdbp = r_config_get (core->config, "cmd.bp");
-	if (cmdbp && *cmdbp) {
+	const bool cmdbp_exists = (cmdbp && *cmdbp);
+	const bool bpcmd_exists = (bpi->data && bpi->data[0]);
+	const bool may_output = (cmdbp_exists || bpcmd_exists);
+	if (may_output) {
+		r_cons_push ();
+	}
+	if (cmdbp_exists) {
 		r_core_cmd0 (core, cmdbp);
 	}
-	if (bpi->data && bpi->data[0]) {
+	if (bpcmd_exists) {
 		r_core_cmd0 (core, bpi->data);
+	}
+	if (may_output) {
+		r_cons_flush ();
+		r_cons_pop ();
 	}
 }
 
