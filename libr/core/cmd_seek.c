d79f84fdd9da0caeb00b64007eb2a473998f56dd
Lowly Worm <cutlassc91@gmail.com>
2017-06-18 23:35:25 -0700

    add sj (json seek history (fixes #7663)

--- libr/core/cmd_seek.c
+++ libr/core/cmd_seek.c
@@ -347,9 +347,63 @@ static int cmd_seek(void *data, const char *input) {
 		}
 		r_core_seek_base (core, input);
 		break;
+	case 'j':  // sj
+		{
+			RList /*<ut64 *>*/ *addrs = r_list_newf (free);
+			RList /*<char *>*/ *names = r_list_newf (free);
+			RList *list = r_io_sundo_list (core->io, '!');
+			ut64 lsz = 0;
+			ut64 i;
+			RListIter *iter;
+			RIOUndos *undo;
+			if (list) {
+				r_list_foreach (list, iter, undo) {
+					char *name = NULL;
+
+					core->flags->space_strict = true;
+					RFlagItem *f = r_flag_get_at (core->flags, undo->off, true);
+					core->flags->space_strict = false;
+					if (f) {
+						if (f->offset != undo->off) {
+							name = r_str_newf ("%s + %d\n", f->name,
+									(int)(undo->off- f->offset));
+						} else {
+							name = strdup (f->name);
+						}
+					}
+					if (!name) {
+						name = strdup ("");
+					}
+					ut64 *val = malloc (sizeof (ut64));
+					if (!val) {
+						break;
+					}
+					*val = undo->off;
+					r_list_append (addrs, val);
+					r_list_append (names, strdup (name));
+					lsz++;
+					free (name);
+				}
+				r_list_free (list);
+			}
+			r_cons_printf ("[");
+			for (i = 0; i < lsz; ++i) {
+				ut64 *addr = r_list_get_n (addrs, i);
+				const char *name = r_list_get_n (names, i);
+				// XXX(should the "name" field be optional? That might make
+				// a bit more sense.
+				r_cons_printf ("{\"offset\":%"PFMT64d",\"symbol\":\"%s\"}", *addr, name);
+				if (i != lsz - 1) {
+					r_cons_printf (",");
+				}
+			}
+			r_cons_printf ("]\n");
+			r_list_free (addrs);
+			r_list_free (names);
+		}
+		break;
 	case '*':
 	case '=':
-	case 'j':
 	case '!':
 		{
 			RList *list = r_io_sundo_list (core->io, input[0]);
