4872bc1aef8539954a6607bc7dc8fb1491db52ee
ZHANG ZHUO <zhangzhuo@sjtu.edu.cn>
2017-06-21 16:25:52 +0800

    simplify rcc_mathop and fix a bug when operation starts with bit opetation

--- libr/egg/egg_lang.c
+++ libr/egg/egg_lang.c
@@ -321,23 +321,22 @@ static void rcc_internal_mathop(REgg *egg, char *ptr, char *ep, char op) {
 static void rcc_mathop(REgg *egg, char **pos, int level) {
 	REggEmit *e = egg->remit;
 	int op_ret = level;
-	char op, *next_pos, *p;
+	char op, *next_pos;
 
 	while (**pos && is_space (**pos)) (*pos)++;
 	next_pos = *pos + 1;
 
 	do {
 		op = (is_op (**pos) && !(is_var (*pos)))? **pos: '=';
-		p = (is_op (**pos) && !(is_var (*pos)))? *pos + 1: *pos;
+		*pos = (is_op (**pos) && !(is_var (*pos)))? *pos + 1: *pos;
 		op_ret = get_op (&next_pos);
 		if (op_ret > level) {
-			(*pos)++;
 			rcc_mathop (egg, pos, op_ret);
 			rcc_internal_mathop (egg, strdup (e->regs (egg, op_ret - 1))
 				, strdup (e->regs (egg, level - 1)), op);
 			next_pos = *pos + 1;
 		} else {
-			rcc_internal_mathop (egg, p, strdup (e->regs (egg, level - 1)), op);
+			rcc_internal_mathop (egg, *pos, strdup (e->regs (egg, level - 1)), op);
 			*pos = next_pos;
 			next_pos++;
 		}
