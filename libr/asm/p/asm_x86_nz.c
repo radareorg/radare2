de520838b64d96941a04736b696696ff0a7998f3
Sven Steinbauer <sven@unlogic.co.uk>
2017-06-28 12:28:24 +0100

    Fix and refactor imul for x86.nz
    The imul op wasn't correctly supported in x86.nz so decided to update
    and refactor it. It now supports all operand numbers and combinations as
    per the docs.

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -68,6 +68,8 @@ static ut64 getnum(RAsm *a, const char *s);
 #define SPECIAL_SPEC 0x00010000
 #define SPECIAL_MASK 0x00000007
 
+#define MAX_OPERANDS 3
+
 const ut8 SEG_REG_PREFIXES[] = {0x26, 0x2e, 0x36, 0x3e, 0x64, 0x65};
 
 typedef enum tokentype_t {
@@ -115,7 +117,7 @@ typedef struct Opcode_t {
 	bool is_short;
 	ut8 opcode[3];
 	int operands_count;
-	Operand operands[2];
+	Operand operands[MAX_OPERANDS];
 	bool has_bnd;
 } Opcode;
 
@@ -736,7 +738,138 @@ static int opimul(RAsm *a, ut8 *data, const Opcode op) {
 	int offset = 0;
 	int mod = 0;
 	int base = 0;
-	ut64 immediate = 0;
+	st64 immediate = 0;
+
+	if (a->bits == 64) {
+		data[l++] = 0x48;
+	}
+	switch (op.operands_count) {
+	case 1:
+		if (op.operands[0].type & OT_MEMORY) {
+			if (op.operands[0].type & OT_BYTE) {
+				data[l++] = 0xf6;
+			} else {
+				data[l++] = 0xf7;
+			}
+			data[l++] = 0x28 | op.operands[0].regs[0];
+		} else {
+			data[l++] = 0xf7;
+			data[l++] = 0xe8 | op.operands[0].reg;
+		}
+		break;
+	case 2:
+		if (op.operands[0].type & OT_GPREG) {
+			if (op.operands[1].type & OT_CONSTANT) {
+				if (op.operands[1].immediate == -1) {
+					eprintf ("Error: Immediate exceeds max\n");
+					return -1;
+				}
+				immediate = op.operands[1].immediate * op.operands[1].sign;
+				if (op.operands[0].type & OT_GPREG) {
+					if (immediate >= 128) {
+						data[l++] = 0x69;
+					} else {
+						data[l++] = 0x6b;
+					}
+					data[l++] = 0xc0 | op.operands[0].reg << 3 | op.operands[0].reg;
+					data[l++] = immediate;
+					if (immediate >= 128) {
+						data[l++] = immediate >> 8;
+						data[l++] = immediate >> 16;
+						data[l++] = immediate >> 24;
+					}
+					if (a->bits == 64 && immediate > UT32_MAX) {
+						data[l++] = immediate >> 32;
+						data[l++] = immediate >> 40;
+						data[l++] = immediate >> 48;
+						data[l++] = immediate >> 56;
+					}
+				}
+			} else if (op.operands[1].type & OT_MEMORY) {
+				data[l++] = 0x0f;
+				data[l++] = 0xaf;
+				if (op.operands[1].regs[0] != X86R_UNDEFINED) {
+					offset = op.operands[1].offset * op.operands[1].offset_sign;
+					if (offset != 0) {
+						if (offset >= 128 || offset <= -128) {
+							data[l] = 0x80;
+						} else {
+							data[l] = 0x40;
+						}
+						data[l++] |= op.operands[0].reg << 3 | op.operands[1].regs[0];
+						data[l++] = offset;
+						if (offset >= 128 || offset <= -128) {
+							data[l++] = offset >> 8;
+							data[l++] = offset >> 16;
+							data[l++] = offset >> 24;
+						}
+					} else {
+						if (op.operands[1].regs[1] != X86R_UNDEFINED) {
+							data[l++] = 0x04 | op.operands[0].reg << 3;
+							data[l++] = op.operands[1].regs[1] << 3 | op.operands[1].regs[0];
+						} else {
+							data[l++] = op.operands[0].reg << 3 | op.operands[1].regs[0];
+						}
+					}
+				} else {
+					immediate = op.operands[1].immediate * op.operands[1].sign;
+					data[l++] = op.operands[0].reg << 3 | 0x5;
+					data[l++] = immediate;
+					data[l++] = immediate >> 8;
+					data[l++] = immediate >> 16;
+					data[l++] = immediate >> 24;
+				}
+			} else if (op.operands[1].type & OT_GPREG) {
+				data[l++] = 0x0f;
+				data[l++] = 0xaf;
+				data[l++] = 0xc0 | op.operands[0].reg << 3 | op.operands[1].reg;
+			}
+		}
+		break;
+	case 3:
+		if (op.operands[0].type & OT_GPREG &&
+		    (op.operands[1].type & OT_GPREG || op.operands[1].type & OT_MEMORY) &&
+		    op.operands[2].type & OT_CONSTANT) {
+				data[l++] = 0x6b;
+				if (op.operands[1].type & OT_MEMORY) {
+					if (op.operands[1].regs[1] != X86R_UNDEFINED) {
+						data[l++] = 0x04 | op.operands[0].reg << 3;
+						data[l++] = op.operands[1].regs[0] |  op.operands[1].regs[1] << 3;
+					} else {
+						offset = op.operands[1].offset * op.operands[1].offset_sign;
+						if (offset != 0) {
+							if (offset >= 128 || offset <= -128) {
+								data[l] = 0x80;
+							} else {
+								data[l] = 0x40;
+							}
+							data[l++] |= op.operands[0].reg << 3;
+							data[l++] = offset;
+							if (offset >= 128 || offset <= -128) {
+								data[l++] = offset >> 8;
+								data[l++] = offset >> 16;
+								data[l++] = offset >> 24;
+							}
+						} else {
+							data[l++] = 0x00 | op.operands[0].reg << 3 | op.operands[1].regs[0];
+						}
+					}
+				} else {
+					data[l++] = 0xc0 | op.operands[0].reg << 3 | op.operands[1].reg;
+				}
+				immediate = op.operands[2].immediate * op.operands[2].sign;
+				data[l++] = immediate;
+				if (immediate >= 128 || immediate <= -128) {
+					data[l++] = immediate >> 8;
+					data[l++] = immediate >> 16;
+					data[l++] = immediate >> 24;
+				}
+		}
+		break;
+	default:
+		return -1;
+	}
+	return l;
 
 	if (op.operands[1].type & OT_CONSTANT) {
 		if (op.operands[1].immediate == -1) {
@@ -744,15 +877,22 @@ static int opimul(RAsm *a, ut8 *data, const Opcode op) {
 			return -1;
 		}
 		immediate = op.operands[1].immediate * op.operands[1].sign;
-		if (a->bits == 64 && immediate > UT32_MAX) {
+		if (a->bits == 64) {
 			data[l++] = 0x48;
 		}
 		if (op.operands[0].type & OT_GPREG) {
-			data[l++] = 0xb8 | op.operands[0].reg;
+			if (immediate >= 128) {
+				data[l++] = 0x69;
+			} else {
+				data[l++] = 0x6b;
+			}
+			data[l++] = 0xc0 | op.operands[0].reg << 3 | op.operands[0].reg;
 			data[l++] = immediate;
-			data[l++] = immediate >> 8;
-			data[l++] = immediate >> 16;
-			data[l++] = immediate >> 24;
+			if (immediate >= 128) {
+				data[l++] = immediate >> 8;
+				data[l++] = immediate >> 16;
+				data[l++] = immediate >> 24;
+			}
 			if (a->bits == 64 && immediate > UT32_MAX) {
 				data[l++] = immediate >> 32;
 				data[l++] = immediate >> 40;
@@ -2426,13 +2566,15 @@ static int parseOpcode(RAsm *a, const char *op, Opcode *out) {
 		args += 5;
 	}
 	parseOperand (a, args, &(out->operands[0]));
-	args = strchr (args, ',');
-	if (args) {
+	out->operands_count = 1;
+	while (out->operands_count <= MAX_OPERANDS) {
+		args = strchr (args, ',');
+		if (!args) {
+			break;
+		}
 		args++;
-		parseOperand (a, args, &(out->operands[1]));
-		out->operands_count = 2;
-	} else {
-		out->operands_count = 1;
+		parseOperand (a, args, &(out->operands[out->operands_count]));
+		out->operands_count++;
 	}
 	return 0;
 }
