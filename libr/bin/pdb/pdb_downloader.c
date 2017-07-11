908f35d502fe88968ab4295c88656b76eee8094e
xarkes <antide.petit@gmail.com>
2017-07-11 15:00:26 +0200

    Added idpd to download pdb file and automated idp

--- libr/bin/pdb/pdb_downloader.c
+++ libr/bin/pdb/pdb_downloader.c
@@ -2,8 +2,10 @@
 
 #include <string.h>
 #include <r_util.h>
+#include <r_core.h>
 
 static int checkPrograms () {
+	// TODO > /dev/null is not portable
 	if (r_sys_cmd ("cabextract -v > /dev/null") != 0) {
 		eprintf ("Missing cabextract\n");
 		return 0;
@@ -107,11 +109,55 @@ void init_pdb_downloader(SPDBDownloaderOpt *opt, SPDBDownloader *pd) {
 }
 
 void deinit_pdb_downloader(SPDBDownloader *pd) {
-	R_FREE(pd->opt->dbg_file);
-	R_FREE(pd->opt->guid);
-	R_FREE(pd->opt->symbol_server);
-	R_FREE(pd->opt->user_agent);
-	R_FREE(pd->opt->path);
-	R_FREE(pd->opt);
+	R_FREE (pd->opt->dbg_file);
+	R_FREE (pd->opt->guid);
+	R_FREE (pd->opt->symbol_server);
+	R_FREE (pd->opt->user_agent);
+	R_FREE (pd->opt->path);
+	R_FREE (pd->opt);
 	pd->download = 0;
+}
+
+int r_bin_pdb_download(RCore* core, int isradjson, int* actions_done, SPDBOptions* options) {
+	int ret;
+	char *path;
+	SPDBDownloaderOpt opt;
+	SPDBDownloader pdb_downloader;
+	RBinInfo *info = r_bin_get_info (core->bin);
+
+	if (!info || !info->debug_file_name) {
+		eprintf ("Can't find debug filename.\n");
+		return 1;
+	}
+
+	if (!options || !options->symbol_server || !options->user_agent) {
+		eprintf ("Can't retrieve pdb configurations.\n");
+		return 1;
+	}
+
+	path = info->file ? r_file_dirname (info->file) : strdup (".");
+
+	opt.dbg_file = info->debug_file_name;
+	opt.guid = info->guid;
+	opt.symbol_server = options->symbol_server;
+	opt.user_agent = options->user_agent;
+	opt.path = path;
+	opt.extract = options->extract;
+
+	init_pdb_downloader (&opt, &pdb_downloader);
+	ret = pdb_downloader.download (&pdb_downloader);
+	if (isradjson && actions_done) {
+		printf ("%s\"pdb\":{\"file\":\"%s\",\"download\":%s}",
+			*actions_done ? "," : "", opt.dbg_file, ret ? "true" : "false");
+	} else {
+		printf ("PDB \"%s\" download %s\n",
+			opt.dbg_file, ret ? "success" : "failed");
+	}
+	if (actions_done) {
+		*actions_done++;
+	}
+	deinit_pdb_downloader (&pdb_downloader);
+
+	free (path);
+	return 0;
 }
