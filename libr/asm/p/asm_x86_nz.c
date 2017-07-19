3251748eadda7d38f7acc833f401788a372ef6d5
Sven Steinbauer <sven@unlogic.co.uk>
2017-07-19 11:08:23 +0100

    Revert changed test for AL

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -132,7 +132,7 @@ static int is_al_reg(const Operand *op) {
 	if (op->type & OT_MEMORY) {
 		return 0;
 	}
-	if (op->reg == X86R_AX && op->type & OT_BYTE) {
+	if (op->reg == X86R_AL && op->type & OT_BYTE) {
 		return 1;
 	}
 	return 0;
