ab221f832691729a2f9bf0490b8a873379754b86
Giovanni <wargio@libero.it>
2017-07-20 08:20:19 +0200

    Implement pseudo for sh4

--- libr/include/r_parse.h
+++ libr/include/r_parse.h
@@ -70,6 +70,7 @@ extern struct r_parse_plugin_t r_parse_plugin_mips_pseudo;
 extern struct r_parse_plugin_t r_parse_plugin_dalvik_pseudo;
 extern struct r_parse_plugin_t r_parse_plugin_mreplace;
 extern struct r_parse_plugin_t r_parse_plugin_ppc_pseudo;
+extern struct r_parse_plugin_t r_parse_plugin_sh_pseudo;
 extern struct r_parse_plugin_t r_parse_plugin_6502_pseudo;
 extern struct r_parse_plugin_t r_parse_plugin_m68k_pseudo;
 #endif
