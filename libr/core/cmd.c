57a83f018662a817be50da523d216a18d181cceb
Fangrui Song <i@maskray.me>
2017-07-25 09:18:18 -0700

    S commands: move help_msg_*[] arrays to top

--- libr/core/cmd.c
+++ libr/core/cmd.c
@@ -3152,6 +3152,7 @@ R_API void r_core_cmd_init(RCore *core) {
 	r_cmd_add (core->rcmd, "t",        "type information (cparse)", &cmd_type);
 	r_cmd_add (core->rcmd, "zign",     "zignatures", &cmd_zign);
 	r_cmd_add (core->rcmd, "Section",  "setup section io information", &cmd_section);
+	cmd_section_init ();
 	r_cmd_add (core->rcmd, "bsize",    "change block size", &cmd_bsize);
 	r_cmd_add (core->rcmd, "kuery",    "perform sdb query", &cmd_kuery);
 	r_cmd_add (core->rcmd, "eval",     "evaluate configuration variable", &cmd_eval);
