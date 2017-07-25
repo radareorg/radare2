52f23733d2387ea04387cc9dec1c24200ee40c4b
Fangrui Song <i@maskray.me>
2017-07-25 09:46:08 -0700

    d commands: move help_msg_*[] arrays to top

--- libr/core/cmd.c
+++ libr/core/cmd.c
@@ -3144,6 +3144,7 @@ R_API void r_core_cmd_init(RCore *core) {
 	r_cmd_add (core->rcmd, "flag",     "get/set flags", &cmd_flag);
 	r_cmd_add (core->rcmd, "g",        "egg manipulation", &cmd_egg);
 	r_cmd_add (core->rcmd, "debug",    "debugger operations", &cmd_debug);
+	cmd_debug_init ();
 	r_cmd_add (core->rcmd, "ls",       "list files and directories", &cmd_ls);
 	r_cmd_add (core->rcmd, "info",     "get file info", &cmd_info);
 	r_cmd_add (core->rcmd, "cmp",      "compare memory", &cmd_cmp);
