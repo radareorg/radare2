7a74331c1e586cf037894c50c54db055c0999fda
pancake <pancake@nopcode.org>
2017-07-28 02:24:06 +0200

    Aim to make more portable the is_dead check in debugger

--- libr/debug/debug.c
+++ libr/debug/debug.c
@@ -1441,9 +1441,19 @@ R_API int r_debug_child_clone(RDebug *dbg) {
 
 R_API bool r_debug_is_dead (RDebug *dbg) {
 	bool is_dead = (dbg->pid == -1 && strncmp (dbg->h->name, "gdb", 3)) || (dbg->reason.type == R_DEBUG_REASON_DEAD);
+#if 0
+	if (!strncmp (dbg->h->name, "clcy", 4)) {
+		return false;
+	}
+#endif
+	if (dbg->pid > 0) {
+		is_dead = !dbg->h->kill (dbg, dbg->pid, false, 0);
+	}
+#if 0
 	if (!is_dead && dbg->h && dbg->h->kill) {
 		is_dead = !dbg->h->kill (dbg, dbg->pid, false, 0);
 	}
+#endif
 	if (is_dead) {
 		dbg->reason.type = R_DEBUG_REASON_DEAD;
 	}
