49e6a76e21d9b6755752daa13dbdacc95584810c
pancake <pancake@nopcode.org>
2017-07-31 10:28:50 +0200

    Fix regressions

--- libr/util/str.c
+++ libr/util/str.c
@@ -163,7 +163,7 @@ R_API int r_str_bits(char *strout, const ut8 *buf, int len, const char *bitz) {
 	} else {
 		for (i = j = 0; i < len; i++) {
 			idx = (i / 8);
-			int bit = i % 8;
+			int bit = 7 - (i % 8);
 			strout[j++] = (buf[idx] & (1 << bit))? '1' : '0';
 		}
 	}
