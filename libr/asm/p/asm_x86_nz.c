3aff806fcb6897fde0e4ca41161c9b088d00656d
Ben Gardiner <ben.l.gardiner@gmail.com>
2017-08-01 18:54:34 -0400

    Fix #8095 rasm2 x64 bswap issue #8095 (#8096)

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -572,8 +572,17 @@ static int opbswap(RAsm *a, ut8 *data, const Opcode *op) {
 		if (op->operands[0].reg == X86R_UNDEFINED) {
 			return -1;
 		}
-		data[l++] = 0x0f;
-		data[l++] = 0xc8 + op->operands[0].reg;
+
+		if (op->operands[0].type & OT_QWORD) {
+			data[l++] = 0x48;
+			data[l++] = 0x0f;
+			data[l++] = 0xc8 + op->operands[0].reg;
+		} else if (op->operands[0].type & OT_DWORD) {
+			data[l++] = 0x0f;
+			data[l++] = 0xc8 + op->operands[0].reg;
+		} else {
+			return -1;
+		}
 	}
 	return l;
 }
