261675ef3ba864372af111a3f81ac0a9983926c3
Rene Laemmert <rlaemmert@gmail.com>
2017-08-04 19:00:02 +0200

    Make old gcc happy again (#8128)

--- shlr/heap/include/r_jemalloc/internal/util.h
+++ shlr/heap/include/r_jemalloc/internal/util.h
@@ -98,8 +98,8 @@
 #ifdef JEMALLOC_H_EXTERNS
 
 int	buferror(int err, char *buf, size_t buflen);
-uintmax_t	malloc_strtoumax(const char *restrict nptr,
-    char **restrict endptr, int base);
+uintmax_t	malloc_strtoumax(const char *nptr,
+    char **endptr, int base);
 void	malloc_write(const char *s);
 
 /*
