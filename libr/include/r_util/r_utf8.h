26ee1a0f560057a7d2e1568fdb01e746e0e00a88
Khairul Kasmiran <kazarmy@gmail.com>
2017-08-10 19:45:59 +0800

    RRune: int -> ut32

--- libr/include/r_util/r_utf8.h
+++ libr/include/r_util/r_utf8.h
@@ -1,8 +1,7 @@
 #ifndef R_UTF8_H
 #define R_UTF8_H
 
-//typedef wchar_t RRune;
-typedef int RRune;
+typedef ut32 RRune;
 R_API int r_utf8_encode(ut8 *ptr, const RRune ch);
 R_API int r_utf8_decode(const ut8 *ptr, int ptrlen, RRune *ch);
 R_API int r_utf8_encode_str(const RRune *str, ut8 *dst, const int dst_length);
