5e70e0f8b6f882dc1461d7441f3136ebdfb71447
Lowly Worm <cutlassc91@gmail.com>
2017-08-15 22:28:30 -0700

    update incorrect FSF addresses
    closes #8123

--- libr/asm/arch/hexagon/gnu/hexagon-dis.c
+++ libr/asm/arch/hexagon/gnu/hexagon-dis.c
@@ -14,7 +14,8 @@
 
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335 USA
+*/
 
 #include "ansidecl.h"
 #include "libiberty.h"
