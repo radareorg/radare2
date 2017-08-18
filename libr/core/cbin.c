f9c59824bc38202d7ec805809f31fc5537af8714
Khairul Kasmiran <kazarmy@gmail.com>
2017-08-19 00:40:45 +0800

    iz/izz: show Unicode blocks

--- libr/core/cbin.c
+++ libr/core/cbin.c
@@ -399,12 +399,36 @@ static void _print_strings(RCore *r, RList *list, int mode, int va) {
 			free (str);
 			free (f_name);
 		} else {
+			int *block_list;
 			r_cons_printf ("vaddr=0x%08"PFMT64x" paddr=0x%08"
 				PFMT64x" ordinal=%03u sz=%u len=%u "
-				"section=%s type=%s string=%s\n",
+				"section=%s type=%s string=%s",
 				vaddr, paddr, string->ordinal, string->size,
 				string->length, section_name, type_string,
 				string->string);
+			switch (string->type) {
+			case R_STRING_TYPE_UTF8:
+			case R_STRING_TYPE_WIDE:
+			case R_STRING_TYPE_WIDE32:
+				block_list = r_utf_block_list (string->string);
+				if (block_list) {
+					if (block_list[0] == 0 && block_list[1] == -1) {
+						/* Don't show block list if
+						   just Basic Latin (0x00 - 0x7F) */
+						break;
+					}
+					int *block_ptr = block_list;
+					r_cons_printf (" \x1b[36mblocks=\x1b[0m");
+					for (; *block_ptr != -1; block_ptr++) {
+						if (block_ptr != block_list) {
+							r_cons_printf (",");
+						}
+						r_cons_printf ("%s", r_utf_blocks[*block_ptr].name);
+					}
+					free (block_list);
+				}
+			}
+			r_cons_printf ("\n");
 		}
 	}
 	if (IS_MODE_JSON (mode)) {
--- libr/include/r_util/r_utf8.h
+++ libr/include/r_util/r_utf8.h
@@ -1,6 +1,9 @@
 #ifndef R_UTF8_H
 #define R_UTF8_H
 
+typedef struct { ut32 from, to; const char *name; } RUtfBlock;
+extern const RUtfBlock r_utf_blocks[];
+
 typedef ut32 RRune;
 R_API int r_utf8_encode(ut8 *ptr, const RRune ch);
 R_API int r_utf8_decode(const ut8 *ptr, int ptrlen, RRune *ch);
@@ -10,4 +13,6 @@ R_API int r_utf8_strlen(const ut8 *str);
 R_API int r_isprint(const RRune c);
 R_API char *r_utf16_to_utf8(const wchar_t *wc);
 R_API wchar_t *r_utf8_to_utf16(const char *cstring);
+R_API int r_utf_block_idx (RRune ch);
+R_API int *r_utf_block_list (const ut8 *str);
 #endif //  R_UTF8_H
