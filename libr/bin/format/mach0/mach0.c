18cfc6d56271fba8b66e286d965c91835eb9e73f
Der0ad <wargio@libero.it>
2017-08-24 12:50:24 +0200

    1372391/1372398 Resource leak

--- libr/bin/format/mach0/mach0.c
+++ libr/bin/format/mach0/mach0.c
@@ -2396,13 +2396,13 @@ RList* MACH0_(mach_fields)(RBinFile *arch) {
 #define ROW(nam,siz,val,fmt) \
 	r_list_append (ret, r_bin_field_new (addr, addr, siz, nam, sdb_fmt (0, "0x%08x", val), fmt)); \
 	addr += 4;
-
 	ROW("hdr.magic", 4, mh->magic, "x");
 	ROW("hdr.cputype", 4, mh->cputype, NULL);
 	ROW("hdr.cpusubtype", 4, mh->cpusubtype, NULL);
 	ROW("hdr.filetype", 4, mh->filetype, NULL);
 	ROW("hdr.ncmds", 4, mh->ncmds, NULL);
 	ROW("hdr.sizeofcmds", 4, mh->sizeofcmds, NULL);
+	free (mh);
 	return ret;
 }
 
