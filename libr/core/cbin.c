68c4dbae7e5e77b5b7b8231b5b6e1ac995218204
alvarofe <alvaro.felipe91@gmail.com>
2017-08-25 00:23:30 +0200

    Bring back bin_id to sections

--- libr/core/cbin.c
+++ libr/core/cbin.c
@@ -2092,7 +2092,7 @@ static int bin_sections(RCore *r, int mode, ut64 laddr, int va, ut64 at, const c
 				r_io_section_add (r->io, section->paddr, addr,
 						  section->size, section->vsize,
 						  section->srwx, section->name,
-						  0, fd);
+						  r->bin->cur->id, fd);
 			}
 		} else if (IS_MODE_SIMPLE (mode)) {
 			char *hashstr = NULL;
