e5842479ce9dac5da86433d340798994ce8185aa
condret <condr3t@protonmail.com>
2017-09-04 02:39:27 +0000

    add interval api

--- libr/include/r_util.h
+++ libr/include/r_util.h
@@ -66,6 +66,7 @@ int gettimeofday (struct timeval* p, void* tz);
 #include "r_util/r_json.h"
 #include "r_util/r_x509.h"
 #include "r_util/r_pkcs7.h"
+#include "r_util/r_interval.h"
 
 #ifdef __cplusplus
 extern "C" {
