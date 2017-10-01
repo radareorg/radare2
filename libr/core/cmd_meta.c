2a85d07280c10978001f28122b4242c449227e49
Khairul Kasmiran <kazarmy@gmail.com>
2017-10-01 14:50:58 +0800

    Define Cs cmd descriptor for recursive help

--- libr/core/cmd_meta.c
+++ libr/core/cmd_meta.c
@@ -111,6 +111,7 @@ static const char *help_msg_Cvs[] = {
 
 static void cmd_meta_init(RCore *core) {
 	DEFINE_CMD_DESCRIPTOR (core, C);
+	DEFINE_CMD_DESCRIPTOR (core, Cs);
 	DEFINE_CMD_DESCRIPTOR (core, CC);
 	DEFINE_CMD_DESCRIPTOR (core, CS);
 	DEFINE_CMD_DESCRIPTOR (core, Cvb);
