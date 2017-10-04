8da1d5c43af43a25a55ebb0f9107d310b782abe0
Jose Diaz <josediazplay@gmail.com>
2017-10-04 23:48:03 +0200

    Leak "so" command

--- libr/core/cmd_seek.c
+++ libr/core/cmd_seek.c
@@ -626,7 +626,6 @@ static int cmd_seek(void *data, const char *input) {
 	}
 	case 'o': // "so"
 	{
-		RAnalOp op;
 		int val = 0, ret, i, n = r_num_math (core->num, input + 1);
 		if (n == 0) {
 			n = 1;
@@ -644,12 +643,15 @@ static int cmd_seek(void *data, const char *input) {
 			val += ret;
 		} else {
 			for (val = i = 0; i < n; i++) {
+				RAnalOp op;
+
 				ret = r_anal_op (core->anal, &op,
 					core->offset, core->block, core->blocksize);
 				if (ret < 1) {
 					ret = 1;
 				}
 				r_core_seek_delta (core, ret);
+				r_anal_op_fini (&op);
 				val += ret;
 			}
 		}
