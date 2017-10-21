5084bae46a16c0fdbd060a1658d933d89e23fef5
icebp <8842017icebp@users.noreplay.github.com>
2017-10-21 23:57:03 +0300

    sticking with the original error message.

--- libr/core/cmd_hash.c
+++ libr/core/cmd_hash.c
@@ -235,10 +235,7 @@ static int cmd_hash_bang (RCore *core, const char *input) {
 			} else eprintf ("Error: scr.interactive required to run the rlang prompt\n");
 		}
 	} else {
-		if (!p || *p==' ')
-			eprintf ("Invalid hashbang. See '#!' for help.\n");
-		else
-			eprintf ("No bindings installed for rlang '%s'\n", p);
+		eprintf ("Invalid hashbang. See '#!' for help.\n");
 	}
 	return true;
 }
