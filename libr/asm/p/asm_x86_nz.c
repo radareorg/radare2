63c79bb5d8f448a29c590b22b0d19712003777e1
Sven Steinbauer <sven@unlogic.co.uk>
2017-11-14 14:53:22 +0000

    Remove undeed reg variable

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -622,7 +622,6 @@ static int opcall(RAsm *a, ut8 *data, const Opcode *op) {
 	int immediate = 0;
 	int offset = 0;
 	int mod = 0;
-	int reg = op->operands[0].reg;
 
 	if (op->operands[0].type & OT_GPREG) {
 		if (op->operands[0].reg == X86R_UNDEFINED) {
@@ -633,7 +632,7 @@ static int opcall(RAsm *a, ut8 *data, const Opcode *op) {
 		}
 		data[l++] = 0xff;
 		mod = 3;
-		data[l++] = mod << 6 | 2 << 3 | reg;
+		data[l++] = mod << 6 | 2 << 3 | op->operands[0].reg;
 	} else if (op->operands[0].type & OT_MEMORY) {
 		if (op->operands[0].regs[0] == X86R_UNDEFINED) {
 			return -1;
