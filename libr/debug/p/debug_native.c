b591e5128b63e2ec1147374430989110ea0f745c
Michael Zhilin <zhilin@netcracker.com>
2017-11-21 23:45:31 +0300

    [FreeBSD] ino64 change on -head

--- libr/debug/p/debug_native.c
+++ libr/debug/p/debug_native.c
@@ -1648,6 +1648,7 @@ static RList *r_debug_desc_native_list (int pid) {
 		case KF_TYPE_VNODE: type = 'v'; break;
 		case KF_TYPE_SOCKET:
 			type = 's';
+#if __FreeBSD_version < 1200031
 			if (kve->kf_sock_domain == AF_LOCAL) {
 				struct sockaddr_un *sun =
 					(struct sockaddr_un *)&kve->kf_sa_local;
@@ -1661,6 +1662,21 @@ static RList *r_debug_desc_native_list (int pid) {
 				addr_to_string (&kve->kf_sa_peer, path + strlen (path),
 						sizeof (path));
 			}
+#else
+			if (kve->kf_sock_domain == AF_LOCAL) {
+				struct sockaddr_un *sun =
+					(struct sockaddr_un *)&kve->kf_un.kf_sock.kf_sa_local;;
+				if (sun->sun_path[0] != 0)
+					addr_to_string (&kve->kf_un.kf_sock.kf_sa_local, path, sizeof(path));
+				else
+					addr_to_string (&kve->kf_un.kf_sock.kf_sa_peer, path, sizeof(path));
+			} else {
+				addr_to_string (&kve->kf_un.kf_sock.kf_sa_local, path, sizeof(path));
+				strcat (path, " ");
+				addr_to_string (&kve->kf_un.kf_sock.kf_sa_peer, path + strlen (path),
+						sizeof (path));
+			}
+#endif
 			str = path;
 			break;
 		case KF_TYPE_PIPE: type = 'p'; break;
