77ebba9699783f32df12ed61df1b84681c074188
pancake <pancake@nopcode.org>
2017-11-22 01:01:00 +0100

    Fix unaligned issue in sub

--- libr/core/disasm.c
+++ libr/core/disasm.c
@@ -2816,15 +2816,16 @@ static void ds_print_ptr(RDisasmState *ds, int len, int idx) {
 	ds->chref = 0;
 	if ((char)v > 0 && v >= '!') {
 		ds->chref = (char)v;
-		ALIGN;
 		if (ds->immstr) {
 			char *str = r_str_from_ut64 (r_read_ble64 (&v, core->print->big_endian));
 			if (str && *str) {
+				ALIGN;
 				ds_comment (ds, true, "; '%s'%s", str, nl);
 			}
 			free (str);
 		} else {
 			if ((char)v > 0 && v >= '!' && v <= '~') {
+				ALIGN;
 				ds_comment (ds, true, "; '%c'%s", (char)v, nl);
 			}
 		}
