7b43c8acdbbba0f706503ae2ff280846c8930997
Florian Märkl <info@florianmaerkl.de>
2017-12-14 20:29:02 +0100

    Reset color after asm.describe Comment

--- libr/core/disasm.c
+++ libr/core/disasm.c
@@ -3854,6 +3854,7 @@ static void ds_print_comments_right(RDisasmState *ds) {
 			}
 			r_cons_strcat ("; ");
 			r_cons_strcat (desc);
+			ds_print_color_reset (ds);
 		}
 		if (ds->show_comment_right && ds->comment) {
 			char *comment = r_str_chop (ds->comment);
