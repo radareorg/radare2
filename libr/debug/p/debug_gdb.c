e0f32383164b8a07c326bf4630998babf9e4bdab
Anton Kochkov <xvilka@gmail.com>
2017-12-29 14:58:35 +0800

    Enable MSVC gdb build (#9087)
    * Enable MSVC gdb build
    * Link with GDB
    * Link with r_socket for gdb

--- libr/debug/p/debug_gdb.c
+++ libr/debug/p/debug_gdb.c
@@ -124,7 +124,14 @@ static RList *r_debug_gdb_map_get(RDebug* dbg) { //TODO
 	ut64 buflen = 16384;
 	// If /proc/%d/maps is not valid for gdbserver, we return NULL, as of now
 	snprintf (path, sizeof (path) - 1, "/proc/%d/maps", desc->pid);
-	if (gdbr_open_file (desc, path, O_RDONLY, S_IRUSR | S_IWUSR | S_IXUSR) < 0) {
+
+#ifdef _MSC_VER
+#define GDB_FILE_OPEN_MODE (_S_IREAD | _S_IWRITE)
+#else
+#define GDB_FILE_OPEN_MODE (S_IRUSR | S_IWUSR | S_IXUSR)
+#endif
+
+	if (gdbr_open_file (desc, path, O_RDONLY, GDB_FILE_OPEN_MODE) < 0) {
 		return NULL;
 	}
 	if (!(buf = malloc (buflen))) {
