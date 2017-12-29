eca97d881bc9e160c7e53ab4cebc85481cd7ed50
Anton Kochkov <xvilka@gmail.com>
2017-12-29 20:38:06 +0800

    WASM analysis - initial stub

--- libr/asm/p/wasm.mk
+++ libr/asm/p/wasm.mk
@@ -1,10 +1,14 @@
+WASM_ROOT=$(LIBR)/asm/arch/wasm
 OBJ_WASM=asm_wasm.o
+OBJ_WASM+=$(WASM_ROOT)/wasm.o
+CFLAGS+=-I$(WASM_ROOT)
 
 TARGET_WASM=asm_wasm.${EXT_SO}
 STATIC_OBJ+=${OBJ_WASM}
 
 ifeq ($(WITHPIC),1)
 ALL_TARGETS+=${TARGET_WASM}
+
 ${TARGET_WASM}: ${OBJ_WASM}
 	${CC} $(call libname,asm_wasm) ${LDFLAGS} ${CFLAGS} -o ${TARGET_WASM} ${OBJ_WASM}
 endif
