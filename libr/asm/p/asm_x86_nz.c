b5d5a3ca19901ae04d49796dae0f3ddc2feb8a98
emvivre <emvivre@urdn.com.ua>
2017-12-30 12:37:03 +0100

    Add fnop operator for x86 assembler (No Operation)

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -2266,6 +2266,7 @@ LookupTable oplookup[] = {
 	{"div", 0, &opdiv, 0},
 	{"emms", 0, NULL, 0x0f77, 2},
 	{"femms", 0, NULL, 0x0f0e, 2},
+	{"fnop", 0, NULL, 0xd9d0, 2},
 	{"fsin", 0, NULL, 0xd9fe, 2},
 	{"fsincos", 0, NULL, 0xd9fb, 2},
 	{"fwait", 0, NULL, 0x9b, 1},
