f9efed9e9b8b2ba1375eaaabf57561e99a0b9f8d
Andrea Sindoni <andrea.sindoni@gmail.com>
2018-01-02 22:31:35 +0100

    Fixed NULL pointer dereference in the file egg.c (#9115)

--- libr/egg/egg.c
+++ libr/egg/egg.c
@@ -456,6 +456,7 @@ R_API int r_egg_encode(REgg *egg, const char *name) {
 	r_list_foreach (egg->plugins, iter, p) {
 		if (p->type == R_EGG_PLUGIN_ENCODER && !strcmp (name, p->name)) {
 			b = p->build (egg);
+			if (!b) return false;
 			r_buf_free (egg->bin);
 			egg->bin = b;
 			return true;
