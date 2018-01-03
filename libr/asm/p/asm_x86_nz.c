7b8692bcdfa760c9d19018bc00b3bdd083d2e68b
emvivre <emvivre@urdn.com.ua>
2018-01-03 21:59:34 +0100

    Add more checking on the operands type of some instuctions in the x86 assembler.

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -2196,33 +2196,35 @@ static int opfcmov(RAsm *a, ut8 *data, const Opcode *op) {
 	char* fcmov = op->mnemonic + strlen("fcmov");
 	switch (op->operands_count) {
 	case 2:
-		if ( op->operands[0].reg != 0 ) {
-			return -1;
-		}
-		if ( !strcmp( fcmov, "b" ) ) {
-			data[l++] = 0xda;
-			data[l++] = 0xc0 | op->operands[1].reg;
-		} else if ( !strcmp( fcmov, "e" ) ) {
-			data[l++] = 0xda;
-			data[l++] = 0xc8 | op->operands[1].reg;
-		} else if ( !strcmp( fcmov, "be" ) ) {
-			data[l++] = 0xda;
-			data[l++] = 0xd0 | op->operands[1].reg;
-		} else if ( !strcmp( fcmov, "u" ) ) {
-			data[l++] = 0xda;
-			data[l++] = 0xd8 | op->operands[1].reg;
-		} else if ( !strcmp( fcmov, "nb" ) ) {
-			data[l++] = 0xdb;
-			data[l++] = 0xc0 | op->operands[1].reg;
-		} else if ( !strcmp( fcmov, "ne" ) ) {
-			data[l++] = 0xdb;
-			data[l++] = 0xc8 | op->operands[1].reg;
-		} else if ( !strcmp( fcmov, "nbe" ) ) {
-			data[l++] = 0xdb;
-			data[l++] = 0xd0 | op->operands[1].reg;
-		} else if ( !strcmp( fcmov, "nu" ) ) {
-			data[l++] = 0xdb;
-			data[l++] = 0xd8 | op->operands[1].reg;
+		if ( op->operands[0].type & OT_FPUREG & ~OT_REGALL && op->operands[0].reg == 0 &&
+		     op->operands[1].type & OT_FPUREG & ~OT_REGALL ) {
+			if ( !strcmp( fcmov, "b" ) ) {
+				data[l++] = 0xda;
+				data[l++] = 0xc0 | op->operands[1].reg;
+			} else if ( !strcmp( fcmov, "e" ) ) {
+				data[l++] = 0xda;
+				data[l++] = 0xc8 | op->operands[1].reg;
+			} else if ( !strcmp( fcmov, "be" ) ) {
+				data[l++] = 0xda;
+				data[l++] = 0xd0 | op->operands[1].reg;
+			} else if ( !strcmp( fcmov, "u" ) ) {
+				data[l++] = 0xda;
+				data[l++] = 0xd8 | op->operands[1].reg;
+			} else if ( !strcmp( fcmov, "nb" ) ) {
+				data[l++] = 0xdb;
+				data[l++] = 0xc0 | op->operands[1].reg;
+			} else if ( !strcmp( fcmov, "ne" ) ) {
+				data[l++] = 0xdb;
+				data[l++] = 0xc8 | op->operands[1].reg;
+			} else if ( !strcmp( fcmov, "nbe" ) ) {
+				data[l++] = 0xdb;
+				data[l++] = 0xd0 | op->operands[1].reg;
+			} else if ( !strcmp( fcmov, "nu" ) ) {
+				data[l++] = 0xdb;
+				data[l++] = 0xd8 | op->operands[1].reg;
+			} else {
+				return -1;
+			}
 		} else {
 			return -1;
 		}
@@ -2237,8 +2239,12 @@ static int opffree(RAsm *a, ut8 *data, const Opcode *op) {
 	int l = 0;
 	switch (op->operands_count) {
 	case 1:
-		data[l++] = 0xdd;
-		data[l++] = 0xc0 | op->operands[0].reg;
+		if (op->operands[0].type & OT_FPUREG & ~OT_REGALL)  {
+			data[l++] = 0xdd;
+			data[l++] = 0xc0 | op->operands[0].reg;
+		} else {
+			return -1;
+		}
 		break;
 	default:
 		return -1;
@@ -2288,8 +2294,12 @@ static int opfucom(RAsm *a, ut8 *data, const Opcode *op) {
 	int l = 0;
 	switch (op->operands_count) {
 	case 1:
-		data[l++] = 0xdd;
-		data[l++] = 0xe0 | op->operands[0].reg;
+		if ( op->operands[0].type & OT_FPUREG & ~OT_REGALL ) {
+			data[l++] = 0xdd;
+			data[l++] = 0xe0 | op->operands[0].reg;
+		} else {
+			return -1;
+		}
 		break;
 	case 0:
 		data[l++] = 0xdd;
@@ -2305,8 +2315,12 @@ static int opfucomp(RAsm *a, ut8 *data, const Opcode *op) {
 	int l = 0;
 	switch (op->operands_count) {
 	case 1:
-		data[l++] = 0xdd;
-		data[l++] = 0xe8 | op->operands[0].reg;
+		if ( op->operands[0].type & OT_FPUREG & ~OT_REGALL ) {
+			data[l++] = 0xdd;
+			data[l++] = 0xe8 | op->operands[0].reg;
+		} else {
+			return -1;
+		}
 		break;
 	case 0:
 		data[l++] = 0xdd;
