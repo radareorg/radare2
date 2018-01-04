39e03eec8749f4a72df4c62b985f725417695a9c
emvivre <emvivre@urdn.com.ua>
2018-01-04 22:48:35 +0100

    Add FADD/FADDP/FIADD operator for x86 assembler (Add)

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -2342,6 +2342,90 @@ static int opfucomp(RAsm *a, ut8 *data, const Opcode *op) {
 	return l;
 }
 
+static int opfaddp(RAsm *a, ut8 *data, const Opcode *op) {
+	int l = 0;
+	switch (op->operands_count) {
+	case 2:
+		if ( op->operands[0].type & OT_FPUREG & ~OT_REGALL &&
+		     op->operands[1].type & OT_FPUREG & ~OT_REGALL && op->operands[1].reg == 0 ) {
+			data[l++] = 0xde;
+			data[l++] = 0xc0 | op->operands[0].reg;
+		} else {
+			return -1;
+		}
+		break;
+	case 0:
+		data[l++] = 0xde;
+		data[l++] = 0xc1;
+		break;
+	default:
+		return -1;
+	}
+	return l;
+}
+
+static int opfiadd(RAsm *a, ut8 *data, const Opcode *op) {
+	int l = 0;
+	switch (op->operands_count) {
+	case 1:
+		if ( op->operands[0].type & OT_MEMORY ) {
+			if ( op->operands[0].type & OT_WORD ) {
+				data[l++] = 0xde;
+				data[l++] = 0x00 | op->operands[0].regs[0];
+			} else if ( op->operands[0].type & OT_DWORD ) {
+				data[l++] = 0xda;
+				data[l++] = 0x00 | op->operands[0].regs[0];
+			} else {
+				return -1;
+			}
+		} else {
+			return -1;
+		}
+		break;
+	default:
+		return -1;
+	}
+	return l;
+}
+
+static int opfadd(RAsm *a, ut8 *data, const Opcode *op) {
+	int l = 0;
+	switch (op->operands_count) {
+	case 1:
+		if ( op->operands[0].type & OT_MEMORY ) {
+			if ( op->operands[0].type & OT_QWORD ) {
+				data[l++] = 0xdc;
+				data[l++] = 0x00 | op->operands[0].regs[0];
+			} else if ( op->operands[0].type & OT_DWORD ) {
+				data[l++] = 0xd8;
+				data[l++] = 0x00 | op->operands[0].regs[0];
+			} else {
+				return -1;
+			}
+		} else {
+			return -1;
+		}
+		break;
+	case 2:
+		if ( op->operands[0].type & OT_FPUREG & ~OT_REGALL && op->operands[0].reg == 0 &&
+		     op->operands[1].type & OT_FPUREG & ~OT_REGALL ) {
+			data[l++] = 0xd8;
+			data[l++] = 0xc0 | op->operands[1].reg;
+		} else if ( op->operands[0].type & OT_FPUREG & ~OT_REGALL &&
+			    op->operands[1].type & OT_FPUREG & ~OT_REGALL && op->operands[1].reg == 0 ) {
+			data[l++] = 0xdc;
+			data[l++] = 0xc0 | op->operands[0].reg;
+		} else {
+			return -1;
+		}
+		break;
+	default:
+		return -1;
+	}
+	return l;
+}
+
+
 typedef struct lookup_t {
 	char mnemonic[12];
 	int only_x32;
@@ -2419,6 +2503,8 @@ LookupTable oplookup[] = {
 	{"emms", 0, NULL, 0x0f77, 2},
 	{"f2xm1", 0, NULL, 0xd9f0, 2},
 	{"fabs", 0, NULL, 0xd9e1, 2},
+	{"fadd", 0, &opfadd, 0},
+	{"faddp", 0, &opfaddp, 0},
 	{"fchs", 0, NULL, 0xd9e0, 2},
 	{"fclex", 0, NULL, 0x9bdbe2, 3},
 	{"fcmovb", 0, &opfcmov, 0},
@@ -2433,6 +2519,7 @@ LookupTable oplookup[] = {
 	{"fdecstp", 0, NULL, 0xd9f6, 2},
 	{"femms", 0, NULL, 0x0f0e, 2},
 	{"ffree", 0, &opffree, 0},
+	{"fiadd", 0, &opfiadd, 0},
 	{"fincstp", 0, NULL, 0xd9f7, 2},
 	{"finit", 0, NULL, 0x9bdbe3, 3},
 	{"fld1", 0, NULL, 0xd9e8, 2},
