3393b6a114707ddca5533e002e2cc9c13626ed99
Sven Steinbauer <sven@unlogic.co.uk>
2018-01-05 09:44:10 +0000

    Fix clflush to work with r/m mod

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -1102,10 +1102,29 @@ static int opin(RAsm *a, ut8 *data, const Opcode *op) {
 
 static int opclflush(RAsm *a, ut8 *data, const Opcode *op) {
 	int l = 0;
+	int offset = 0;
+	int mod_byte = 0;
+
 	if (op->operands[0].type & OT_MEMORY) {
 		data[l++] = 0x0f;
 		data[l++] = 0xae;
-		data[l++] = (7 << 3) | op->operands[0].regs[0];
+		offset = op->operands[0].offset * op->operands[0].offset_sign;
+		if (offset) {
+			if (offset < ST8_MIN || offset > ST8_MAX) {
+				mod_byte = 2;
+			} else {
+				mod_byte = 1;
+			}
+		}
+		data[l++] = (mod_byte << 6) | (7 << 3) | op->operands[0].regs[0];
+		if (mod_byte) {
+			data[l++] = offset;
+			if (mod_byte == 2) {
+				data[l++] = offset >> 8;
+				data[l++] = offset >> 16;
+				data[l++] = offset >> 24;
+			}
+		}
 	}
 	return l;
 }
