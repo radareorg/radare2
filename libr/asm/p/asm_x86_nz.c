28918381e91c9214608071dbd8b08b3a24de5deb
emvivre <emvivre@urdn.com.ua>
2018-01-07 11:41:34 +0100

    Add FSUBR/FSUBRP/FISUBR operator for x86 assembler (Reverse Subtract)

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -3005,7 +3005,7 @@ static int opfsub(RAsm *a, ut8 *data, const Opcode *op) {
 		} else if ( op->operands[0].type & OT_FPUREG & ~OT_REGALL &&
 			    op->operands[1].type & OT_FPUREG & ~OT_REGALL && op->operands[1].reg == 0 ) {
 			data[l++] = 0xdc;
-			data[l++] = 0xe0 | op->operands[0].reg;
+			data[l++] = 0xe8 | op->operands[0].reg;
 		} else {
 			return -1;
 		}
@@ -3049,6 +3049,89 @@ static int opfisub(RAsm *a, ut8 *data, const Opcode *op) {
 			} else if ( op->operands[0].type & OT_WORD ) {
 				data[l++] = 0xde;
 				data[l++] = 0x20 | op->operands[0].regs[0];
+			} else {
+				return -1;
+			}
+		} else {
+			return -1;
+		}
+		break;
+	default:
+		return -1;
+	}
+	return l;
+}
+
+static int opfsubr(RAsm *a, ut8 *data, const Opcode *op) {
+	int l = 0;
+	switch (op->operands_count) {
+	case 1:
+		if ( op->operands[0].type & OT_MEMORY ) {
+			if ( op->operands[0].type & OT_DWORD ) {
+				data[l++] = 0xd8;
+				data[l++] = 0x28 | op->operands[0].regs[0];
+			} else if ( op->operands[0].type & OT_QWORD ) {
+				data[l++] = 0xdc;
+				data[l++] = 0x28 | op->operands[0].regs[0];
+			} else {
+				return -1;
+			}
+		} else {
+			return -1;
+		}
+		break;
+	case 2:
+		if ( op->operands[0].type & OT_FPUREG & ~OT_REGALL && op->operands[0].reg == 0 &&
+		     op->operands[1].type & OT_FPUREG & ~OT_REGALL ) {
+			data[l++] = 0xd8;
+			data[l++] = 0xe8 | op->operands[1].reg;
+		} else if ( op->operands[0].type & OT_FPUREG & ~OT_REGALL &&
+			    op->operands[1].type & OT_FPUREG & ~OT_REGALL && op->operands[1].reg == 0 ) {
+			data[l++] = 0xdc;
+			data[l++] = 0xe0 | op->operands[0].reg;
+		} else {
+			return -1;
+		}
+		break;
+	default:
+		return -1;
+	}
+	return l;
+}
+
+static int opfsubrp(RAsm *a, ut8 *data, const Opcode *op) {
+	int l = 0;
+	switch (op->operands_count) {
+	case 0:
+		data[l++] = 0xde;
+		data[l++] = 0xe1;
+		break;
+	case 2:
+		if ( op->operands[0].type & OT_FPUREG & ~OT_REGALL &&
+		     op->operands[1].type & OT_FPUREG & ~OT_REGALL && op->operands[1].reg == 0 ) {
+			data[l++] = 0xde;
+			data[l++] = 0xe0 | op->operands[0].reg;
+		} else {
+			return -1;
+		}
+		break;
+	default:
+		return -1;
+	}
+	return l;
+}
+
+static int opfisubr(RAsm *a, ut8 *data, const Opcode *op) {
+	int l = 0;
+	switch (op->operands_count) {
+	case 1:
+		if ( op->operands[0].type & OT_MEMORY ) {
+			if ( op->operands[0].type & OT_DWORD ) {
+				data[l++] = 0xda;
+				data[l++] = 0x28 | op->operands[0].regs[0];
+			} else if ( op->operands[0].type & OT_WORD ) {
+				data[l++] = 0xde;
+				data[l++] = 0x28 | op->operands[0].regs[0];
 			} else {
 				return -1;
 			}
@@ -3170,10 +3253,11 @@ LookupTable oplookup[] = {
 	{"fimul", 0, &opfimul, 0},
 	{"fincstp", 0, NULL, 0xd9f7, 2},
 	{"finit", 0, NULL, 0x9bdbe3, 3},
-	{"fisub", 0, &opfisub, 0},
         {"fist", 0, &opfist, 0},
 	{"fistp", 0, &opfistp, 0},
         {"fisttp", 0, &opfisttp, 0},
+	{"fisub", 0, &opfisub, 0},
+	{"fisubr", 0, &opfisubr, 0},
 	{"fld1", 0, NULL, 0xd9e8, 2},
 	{"fldcw", 0, &opfldcw, 0},
 	{"fldenv", 0, &opfldenv, 0},
@@ -3202,6 +3286,8 @@ LookupTable oplookup[] = {
         {"fstenv", 0, &opfstenv, 0},
 	{"fsub", 0, &opfsub, 0},
 	{"fsubp", 0, &opfsubp, 0},
+	{"fsubr", 0, &opfsubr, 0},
+	{"fsubrp", 0, &opfsubrp, 0},
 	{"ftst", 0, NULL, 0xd9e4, 2},
 	{"fucom", 0, &opfucom, 0},
 	{"fucomp", 0, &opfucomp, 0},
