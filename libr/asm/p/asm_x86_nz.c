65a71bcaed51db2753ff27050f4ea6f52a442596
emvivre <emvivre@urdn.com.ua>
2018-01-07 11:24:05 +0100

    Add FMUL/FMULP/FIMUL operator for x86 assembler (Multiply)

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -2896,6 +2896,89 @@ static int opfidivr(RAsm *a, ut8 *data, const Opcode *op) {
 	return l;
 }
 
+static int opfmul(RAsm *a, ut8 *data, const Opcode *op) {
+	int l = 0;
+	switch (op->operands_count) {
+	case 1:
+		if ( op->operands[0].type & OT_MEMORY ) {
+			if ( op->operands[0].type & OT_DWORD ) {
+				data[l++] = 0xd8;
+				data[l++] = 0x08 | op->operands[0].regs[0];
+			} else if ( op->operands[0].type & OT_QWORD ) {
+				data[l++] = 0xdc;
+				data[l++] = 0x08 | op->operands[0].regs[0];
+			} else {
+				return -1;
+			}
+		} else {
+			return -1;
+		}
+		break;
+	case 2:
+		if ( op->operands[0].type & OT_FPUREG & ~OT_REGALL && op->operands[0].reg == 0 &&
+		     op->operands[1].type & OT_FPUREG & ~OT_REGALL ) {
+			data[l++] = 0xd8;
+			data[l++] = 0xc8 | op->operands[1].reg;
+		} else if ( op->operands[0].type & OT_FPUREG & ~OT_REGALL &&
+			    op->operands[1].type & OT_FPUREG & ~OT_REGALL && op->operands[1].reg == 0 ) {
+			data[l++] = 0xdc;
+			data[l++] = 0xc8 | op->operands[0].reg;
+		} else {
+			return -1;
+		}
+		break;
+	default:
+		return -1;
+	}
+	return l;
+}
+
+static int opfmulp(RAsm *a, ut8 *data, const Opcode *op) {
+	int l = 0;
+	switch (op->operands_count) {
+	case 0:
+		data[l++] = 0xde;
+		data[l++] = 0xc9;
+		break;
+	case 2:
+		if ( op->operands[0].type & OT_FPUREG & ~OT_REGALL &&
+		     op->operands[1].type & OT_FPUREG & ~OT_REGALL && op->operands[1].reg == 0 ) {
+			data[l++] = 0xde;
+			data[l++] = 0xc8 | op->operands[0].reg;
+		} else {
+			return -1;
+		}
+		break;
+	default:
+		return -1;
+	}
+	return l;
+}
+
+static int opfimul(RAsm *a, ut8 *data, const Opcode *op) {
+	int l = 0;
+	switch (op->operands_count) {
+	case 1:
+		if ( op->operands[0].type & OT_MEMORY ) {
+			if ( op->operands[0].type & OT_DWORD ) {
+				data[l++] = 0xda;
+				data[l++] = 0x08 | op->operands[0].regs[0];
+			} else if ( op->operands[0].type & OT_WORD ) {
+				data[l++] = 0xde;
+				data[l++] = 0x08 | op->operands[0].regs[0];
+			} else {
+				return -1;
+			}
+		} else {
+			return -1;
+		}
+		break;
+	default:
+		return -1;
+	}
+	return l;
+}
+
 typedef struct lookup_t {
 	char mnemonic[12];
 	int only_x32;
@@ -3001,6 +3084,7 @@ LookupTable oplookup[] = {
 	{"fidiv", 0, &opfidiv, 0},
 	{"fidivr", 0, &opfidivr, 0},
 	{"fild", 0, &opfild, 0},
+	{"fimul", 0, &opfimul, 0},
 	{"fincstp", 0, NULL, 0xd9f7, 2},
 	{"finit", 0, NULL, 0x9bdbe3, 3},
         {"fist", 0, &opfist, 0},
@@ -3015,6 +3099,8 @@ LookupTable oplookup[] = {
 	{"fldln2", 0, NULL, 0xd9ed, 2},
 	{"fldpi", 0, NULL, 0xd9eb, 2},
 	{"fldz", 0, NULL, 0xd9ee, 2},
+	{"fmul", 0, &opfmul, 0},
+	{"fmulp", 0, &opfmulp, 0},
 	{"fnclex", 0, NULL, 0xdbe2, 2},
 	{"fninit", 0, NULL, 0xdbe3, 2},
 	{"fnop", 0, NULL, 0xd9d0, 2},
