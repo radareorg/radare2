7dc638c5d1d502ba1344e0d267113289a81c9e08
Lowly Worm <cutlassc91@gmail.com>
2018-01-13 00:14:07 -0800

    show both label and title for aggv
    fixes #9185

--- libr/core/cmd_anal.c
+++ libr/core/cmd_anal.c
@@ -5392,11 +5392,12 @@ static void agraph_print_node_dot(RANode *n, void *user) {
 	char *label = strdup (n->body);
 	//label = r_str_replace (label, "\n", "\\l", 1);
 	if (!label || !*label) {
-		free (label);
-		label = strdup (n->title);
+		r_cons_printf ("\"%s\" [URL=\"%s\", color=\"lightgray\", label=\"%s\"]\n",
+				n->title, n->title, n->title);
+	} else {
+		r_cons_printf ("\"%s\" [URL=\"%s\", color=\"lightgray\", label=\"%s\\n%s\"]\n",
+				n->title, n->title, n->title, label);
 	}
-	r_cons_printf ("\"%s\" [URL=\"%s\", color=\"lightgray\", label=\"%s\"]\n",
-		n->title, n->title, label);
 	free (label);
 }
 
