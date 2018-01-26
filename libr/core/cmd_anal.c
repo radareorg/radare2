0e15dd3300b3e257b69d062542d50c5464ad1770
sivaramaaa <sivaramaaa>
2018-01-27 03:31:15 +0530

    Fix #9244 , Honor anal.in for aab

--- libr/core/cmd_anal.c
+++ libr/core/cmd_anal.c
@@ -4545,12 +4545,11 @@ static void cmd_anal_aftertraps(RCore *core, const char *input) {
 }
 
 static void cmd_anal_blocks(RCore *core, const char *input) {
-	SdbListIter *iter;
-	RIOSection *s;
-	ut64 min = UT64_MAX;
-	ut64 max = 0;
 
+	ut64 from , to;
+	char *arg = strchr (input, ' ');
 	r_cons_break_push (NULL, NULL);
+#if 0
 	ls_foreach (core->io->sections, iter, s) {
 		/* is executable */
 		if (!(s->flags & R_IO_EXEC)) {
@@ -4570,6 +4569,30 @@ static void cmd_anal_blocks(RCore *core, const char *input) {
 		if (r_cons_is_breaked ()) {
 			goto ctrl_c;
 		}
+	}
+#endif
+	if (!arg) {
+		const char *analin = r_config_get (core->config, "anal.in");
+		RList *list = r_core_get_boundaries_prot (core, R_IO_EXEC, analin);
+		RListIter *iter;
+		RIOMap* map;
+		r_list_foreach (list, iter, map) {
+			from = map->itv.addr;
+			to = r_itv_end (map->itv);
+			if (r_cons_is_breaked ()) {
+				goto ctrl_c;
+			}
+			if (!from && !to) {
+				eprintf ("Cannot determine search boundaries\n");
+			} else if (to - from > UT32_MAX) {
+				eprintf ("Skipping huge range\n");
+			} else {
+				r_core_cmdf (core, "abb 0x%08"PFMT64x" @ 0x%08"PFMT64x, (to - from), from);
+			}
+		}
+	} else {
+		int sz = r_num_math (core->num, arg + 1);
+		r_core_cmdf (core, "abb 0x%08"PFMT64x" @ 0x%08"PFMT64x, sz, core->offset);
 	}
 ctrl_c:
 	r_cons_break_pop ();
