970b6dd111bcafb762e29483b3c291fbecfa9e73
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2018-02-03 15:35:16 +0100

    fix(anal_avr): correct ESIL semantics of sbc
    Rd-Rr-C must be computed as Rd-(Rr+cf), not Rd-(Rr-C) (which adds the
    carry, instead of subtracting it).

--- libr/anal/p/anal_avr.c
+++ libr/anal/p/anal_avr.c
@@ -1134,7 +1134,7 @@ INST_HANDLER (sbc) {	// SBC Rd, Rr
 	int r = (buf[0] & 0x0f) | ((buf[1] & 0x2) << 3);
 	int d = ((buf[0] >> 4) & 0xf) | ((buf[1] & 0x1) << 4);
 
-	ESIL_A ("cf,r%d,-,r%d,-,", r, d);		// 0: (Rd-Rr-C)
+	ESIL_A ("cf,r%d,+,r%d,-,", r, d);		// 0: (Rd-Rr-C)
 	__generic_sub_update_flags_rr (op, d, r, 1);	// FLAGS (carry)
 	ESIL_A ("r%d,=,", d);				// Rd = Result
 }
