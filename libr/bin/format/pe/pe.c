b6f17c63a60aa07f1b2474b2d828646fe000528b
Rafael Rivera <rafael@withinwindows.com>
2018-02-14 22:51:39 -0800

    Ensure pdb path is stripped down to filename (#9395)

--- libr/bin/format/pe/pe.c
+++ libr/bin/format/pe/pe.c
@@ -2717,6 +2717,7 @@ static void get_nb10(ut8* dbg_data, SCV_NB10_HEADER* res) {
 static int get_debug_info(struct PE_(r_bin_pe_obj_t)* bin, PE_(image_debug_directory_entry)* dbg_dir_entry, ut8* dbg_data, int dbg_data_len, SDebugInfo* res) {
 	#define SIZEOF_FILE_NAME 255
 	int i = 0;
+	const char* basename;
 	if (!dbg_data) {
 		return 0;
 	}
@@ -2743,8 +2744,9 @@ static int get_debug_info(struct PE_(r_bin_pe_obj_t)* bin, PE_(image_debug_direc
 				rsds_hdr.guid.data4[6],
 				rsds_hdr.guid.data4[7],
 				rsds_hdr.age);
+			basename = r_file_basename (rsds_hdr.file_name);
 			strncpy (res->file_name, (const char*)
-				rsds_hdr.file_name, sizeof (res->file_name));
+				basename, sizeof (res->file_name));
 			res->file_name[sizeof (res->file_name) - 1] = 0;
 			rsds_hdr.free ((struct SCV_RSDS_HEADER*) &rsds_hdr);
 		} else if (strncmp ((const char*) dbg_data, "NB10", 4) == 0) {
