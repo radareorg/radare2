3be163aa6943794f7d6f88000ca5e8b81bc38596
Fangrui Song <i@maskray.me>
2018-02-17 14:29:11 -0800

    cmd_eval.c: better help

--- libr/core/cmd_eval.c
+++ libr/core/cmd_eval.c
@@ -27,11 +27,35 @@ static const char *help_msg_e[] = {
 	NULL
 };
 
+static const char *help_msg_ec[] = {
+	"Usage ec[s?] [key][[=| ]fg] [bg]", "", "",
+	"ec", "", "list all color keys",
+	"ec*", "", "same as above, but using r2 commands",
+	"ecd", "", "set default palette",
+	"ecr", "", "set random palette (see also scr.randpal)",
+	"ecs", "", "show a colorful palette",
+	"ecj", "", "show palette in JSON",
+	"ecc", " [prefix]", "show palette in CSS",
+	"eco", " dark|white", "load white color scheme template",
+	"ecp", "", "load previous color theme",
+	"ecn", "", "load next color theme",
+	"ecH", "[?]", "highlight word or instruction",
+	"ec", " prompt red", "change color of prompt",
+	"ec", " prompt red blue", "change color and background of prompt",
+	"", " ", "",
+	"colors:", "", "rgb:000, red, green, blue, ...",
+	"e scr.rgbcolor", "=1|0", "for 256 color cube (boolean)",
+	"e scr.truecolor", "=1|0", "for 256*256*256 colors (boolean)",
+	"$DATADIR/radare2/cons", "", "~/.config/radare2/cons ./",
+	NULL
+};
+
 static char *curtheme = NULL;
 static bool getNext = false;
 
 static void cmd_eval_init(RCore *core) {
 	DEFINE_CMD_DESCRIPTOR (core, e);
+	DEFINE_CMD_DESCRIPTOR (core, ec);
 }
 
 static bool load_theme(RCore *core, const char *path) {
@@ -199,7 +223,18 @@ static int cmd_eval(void *data, const char *input) {
 	char *p;
 	RCore *core = (RCore *)data;
 	switch (input[0]) {
-	case 't': // env
+	case '\0': // "e"
+		r_config_list (core->config, NULL, 0);
+		break;
+	case '?': // "e?"
+	default:
+		switch (input[1]) {
+		case '\0': r_core_cmd_help (core, help_msg_e); break;
+		case '?': r_config_list (core->config, input+2, 2); break;
+		default: r_config_list (core->config, input+1, 2); break;
+		}
+		break;
+	case 't': // "et"
 		if (input[1] == 'a') {
 			r_cons_printf ("%s\n", (r_num_rand (10) % 2)? "wen": "son");
 		} else if (input[1]==' ' && input[2]) {
@@ -214,7 +249,7 @@ static int cmd_eval(void *data, const char *input) {
 			eprintf ("Usage: et [varname]  ; show type of eval var\n");
 		}
 		break;
-	case 'n': // env
+	case 'n': // "en"
 		if (!strchr (input, '=')) {
 			char *var, *p;
 			var = strchr (input, ' ');
@@ -253,38 +288,13 @@ static int cmd_eval(void *data, const char *input) {
 	case 'q': // quiet list of eval keys
 		r_config_list (core->config, NULL, 'q');
 		break;
-	case '\0': // "e"
-		r_config_list (core->config, NULL, 0);
-		break;
 	case 'c': // "ec"
 		switch (input[1]) {
 		case 'd':
 			r_cons_pal_init (NULL);
 			break;
-		case '?': {
-			const char *helpmsg[] = {
-			"Usage ec[s?] [key][[=| ]fg] [bg]","","",
-			"ec","","list all color keys",
-			"ec*","","same as above, but using r2 commands",
-			"ecd","","set default palette",
-			"ecr","","set random palette (see also scr.randpal)",
-			"ecs","","show a colorful palette",
-			"ecj","","show palette in JSON",
-			"ecc"," [prefix]","show palette in CSS",
-			"eco"," dark|white","load white color scheme template",
-			"ecp","","load previous color theme",
-			"ecn","","load next color theme",
-			"ecH","[?]","highlight word or instruction",
-			"ec"," prompt red","change color of prompt",
-			"ec"," prompt red blue","change color and background of prompt",
-			""," ","",
-			"colors:","","rgb:000, red, green, blue, ...",
-			"e scr.rgbcolor","=1|0","for 256 color cube (boolean)",
-			"e scr.truecolor","=1|0","for 256*256*256 colors (boolean)",
-			"$DATADIR/radare2/cons","","~/.config/radare2/cons ./",
-			NULL};
-			r_core_cmd_help (core, helpmsg);
-			}
+		case '?':
+			r_core_cmd_help (core, help_msg_ec);
 			break;
 		case 'o': // "eco"
 			if (input[2] == 'j') {
@@ -372,7 +382,7 @@ static int cmd_eval(void *data, const char *input) {
 				r_meta_set_string (core->anal, R_META_TYPE_HIGHLIGHT, core->offset, "");
 				return false;
 			case '\0':
-			case 'i': // "ecHi
+			case 'i': // "ecHi"
 				if (argc) {
 					char *dup = r_str_newf ("bgonly %s", argv[0]);
 					color_code = r_cons_pal_parse (dup);
@@ -432,7 +442,7 @@ static int cmd_eval(void *data, const char *input) {
 		}
 		}
 		break;
-	case 'e':
+	case 'e': // "ee"
 		if (input[1] == ' ') {
 			char *p;
 			const char *val, *input2 = strchr (input+2, ' ');
@@ -447,28 +457,22 @@ static int cmd_eval(void *data, const char *input) {
 			eprintf ("Usage: ee varname\n");
 		}
 		break;
-	case '!':
+	case '!': // "e!"
 		input = r_str_trim_ro (input+1);
 		if (!r_config_toggle (core->config, input))
 			eprintf ("r_config: '%s' is not a boolean variable.\n", input);
 		break;
-	case 's':
+	case 's': // "es"
 		r_config_list (core->config, (input[1])? input + 1: NULL, 's');
 		break;
-	case '-':
+	case '-': // "e-"
 		r_core_config_init (core);
 		//eprintf ("BUG: 'e-' command locks the eval hashtable. patches are welcome :)\n");
 		break;
-	case '*': r_config_list (core->config, NULL, 1); break;
-	case '?':
-		switch (input[1]) {
-		case '?': r_config_list (core->config, input+2, 2); break;
-		default: r_config_list (core->config, input+1, 2); break;
-		case 0:
-			r_core_cmd_help (core, help_msg_e);
-		}
+	case '*': // "e*"
+		r_config_list (core->config, NULL, 1);
 		break;
-	case 'r':
+	case 'r': // "er"
 		if (input[1]) {
 			const char *key = input+((input[1]==' ')?2:1);
 			if (!r_config_readonly (core->config, key)) {
@@ -479,7 +483,6 @@ static int cmd_eval(void *data, const char *input) {
 		}
 		break;
 	case ' ': r_config_eval (core->config, input+1); break;
-	default: r_config_eval (core->config, input); break;
 	}
 	return 0;
 }
