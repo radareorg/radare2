236e18e6cc1e0576530b3be79f37a49ba4355b43
Michael Brickman <mbricman8@gmail.com>
2018-02-26 22:49:41 +0200

    replace all the loops over list in the pfj to use r_str_escape_utf8

--- libr/util/p_format.c
+++ libr/util/p_format.c
@@ -995,19 +995,12 @@ static void r_print_format_nulltermstring(const RPrint* p, const int len, int en
 			}
 		}
 	} else if (MUSTSEEJSON) {
-		int j = i;
+		char * utf_encoded_buf = NULL;
 		p->cb_printf ("%d,\"string\":\"", seeki);
-		for (; j < len && ((size == -1 || size-- > 0) && buf[j]) ; j++) {
-			if (IS_PRINTABLE (buf[j])) {
-				/* encoding here so the format of . for non printable chars will stay */
-				char *encoded_char = r_str_utf16_encode ((const char *)(buf + j), 1);
-				if (encoded_char) {
-					p->cb_printf ("%s", encoded_char);
-					free (encoded_char);
-				}
-			} else {
-				p->cb_printf (".");
-			}
+		utf_encoded_buf = r_str_escape_utf8 ((const char *) buf, true, true);
+		if (utf_encoded_buf){
+			p->cb_printf ("%s", utf_encoded_buf);
+			free (utf_encoded_buf);
 		}
 		p->cb_printf ("\"}");
 	}
