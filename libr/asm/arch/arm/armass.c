8e581c1b2521044523a6d135065b2b01c8399bff
vdf <vdf_git@protonmail.com>
2018-03-07 08:53:12 +0100

    fixed travis errors

--- libr/asm/arch/arm/armass.c
+++ libr/asm/arch/arm/armass.c
@@ -200,7 +200,8 @@ st32 firstsigdigit (ut32 num) {
 	st32 b = -1;
 	ut32 forwardmask = 0x80000000;
 	ut32 backwardmask = 0x1;
-	for (st32 i = 0; i < 32; i++ ) {
+	ut32 i;
+	for (i = 0; i < 32; i++ ) {
 		if ( (forwardmask & num) && (f == -1)) {
 			f = i;
 		}
@@ -226,11 +227,11 @@ static ut32 getthimmed12(const char *str) {
 	ut32 result = 0;
 	if (num <= 0xff) {
 		return num << 16;
-	} else 	if ( (num & 0xff00ff00 == 0) && ((num & 0x00ff0000) == ((num & 0x000000ff) << 16)) ) {
+	} else 	if ( ((num & 0xff00ff00) == 0) && ((num & 0x00ff0000) == ((num & 0x000000ff) << 16)) ) {
 		result |= (num & 0x000000ff) << 8;
 		result |= 0x00000010;
 		return result;
-	} else if ( (num & 0x00ff00ff == 0) && ((num & 0xff000000) == ((num & 0x0000ff00) << 16)) ) {
+	} else if ( ((num & 0x00ff00ff) == 0) && ((num & 0xff000000) == ((num & 0x0000ff00) << 16)) ) {
 		result |= num & 0x0000ff00;
 		result |= 0x00000020;
 		return result;
@@ -241,7 +242,7 @@ static ut32 getthimmed12(const char *str) {
 	} else {
 		FSD = firstsigdigit(num);
 		if (FSD != -1) {
-			result |= ((num >> 24-FSD) & 0x0000007f) << 8;
+		        result |= ((num >> (24-FSD)) & 0x0000007f) << 8;
 			result |= ((8+FSD) & 0x7) << 4;
 			result |= ((8+FSD) & 0x8) << 18;
 			return result;
