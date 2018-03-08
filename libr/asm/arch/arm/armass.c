9b575b826ae85c978667e89b259a9ca680cf9256
vdf <vdf_git@protonmail.com>
2018-03-08 10:12:39 +0100

    bugfixes

--- libr/asm/arch/arm/armass.c
+++ libr/asm/arch/arm/armass.c
@@ -335,7 +335,7 @@ static ut32 thumb_getshift(const char *str) {
 	const char *shifts[] = {
 		"LSL", "LSR", "ASR", "ROR", 0, "RRX"
 	};
-	char *type;
+	char *type = strdup (str);
 	char *arg;
 	char *space;
 	ut32 res = 0;
@@ -344,44 +344,50 @@ static ut32 thumb_getshift(const char *str) {
 	ut32 argn;
 	ut32 i;
 	
-	type = strdup(str);
-
-	r_str_case(type,true);
+	r_str_case (type,true);
 	
-	if (strcmp(type, shifts[5])) {
+	if (!strcmp (type, shifts[5])) {
 		// handle RRX alias case
-		res |= 3 << 20;
-		return res;
-	} else {
-		space = strchr(type, ' ');
-		if (!space) {
-			return 0;
-		}
-		*space = 0;
-		arg = strdup(++space);
-
-		for (i = 0; shifts[i]; i++) {
-			if (!strcmp (type, shifts[i])) {
-				shift = true;
-				break;
-			}
-		}
-		if (!shift) {
-			err = true;
-			return 0;
-		}
-		res |= i << 20;
-		
-		argn = getnum(arg);
-		if (err || argn > 32) {
-			err = true;
-			return 0;
-		}
-		res |= ((argn & 0x1c) << 26);
-		res |= ((argn & 0x3) << 22);
-
+		res |= 3 << 20;	
+		free (type);
 		return res;
 	}
+	
+	space = strchr (type, ' ');
+	if (!space) {
+		free (type);
+		return 0;
+	}
+	*space = 0;
+	arg = strdup (++space);
+	
+	for (i = 0; shifts[i]; i++) {
+		if (!strcmp (type, shifts[i])) {
+			shift = true;
+			break;
+		}
+	}
+	if (!shift) {
+		err = true;
+		free (type);
+		free (arg);
+		return 0;
+	}
+	res |= i << 20;
+		
+	argn = getnum (arg);
+	if (err || argn > 32) {
+		err = true;
+		free (type);
+		free (arg);
+		return 0;
+	}
+	res |= ( (argn & 0x1c) << 26);
+	res |= ( (argn & 0x3) << 22);
+
+	free (type);
+	free (arg);
+	return res;
 }
 
 static ut32 getshift(const char *str) {
