5fa1b0d218c346ce762f0250509eb0378ed39489
vdf-git <36852239+vdf-git@users.noreply.github.com>
2018-03-09 00:32:19 +0100

    Arm assembler thumb/thumb shift (#9621)

--- libr/asm/arch/arm/armass.c
+++ libr/asm/arch/arm/armass.c
@@ -330,6 +330,66 @@ static int getlist(char *op) {
 	return list;
 }
 
+static ut32 thumb_getshift(const char *str) {
+	// only immediate shifts are ever used by thumb-2. Bit positions are different from ARM.
+	const char *shifts[] = {
+		"LSL", "LSR", "ASR", "ROR", 0, "RRX"
+	};
+	char *type = strdup (str);
+	char *arg;
+	char *space;
+	ut32 res = 0;
+	ut32 shift = false;
+	err = false;
+	ut32 argn;
+	ut32 i;
+	
+	r_str_case (type,true);
+	
+	if (!strcmp (type, shifts[5])) {
+		// handle RRX alias case
+		res |= 3 << 20;	
+		free (type);
+		return res;
+	}
+	
+	space = strchr (type, ' ');
+	if (!space) {
+		free (type);
+		return 0;
+	}
+	*space = 0;
+	arg = strdup (++space);
+	
+	for (i = 0; shifts[i]; i++) {
+		if (!strcmp (type, shifts[i])) {
+			shift = true;
+			break;
+		}
+	}
+	if (!shift) {
+		err = true;
+		free (type);
+		free (arg);
+		return 0;
+	}
+	res |= i << 20;
+		
+	argn = getnum (arg);
+	if (err || argn > 32) {
+		err = true;
+		free (type);
+		free (arg);
+		return 0;
+	}
+	res |= ( (argn & 0x1c) << 26);
+	res |= ( (argn & 0x3) << 22);
+
+	free (type);
+	free (arg);
+	return res;
+}
+
 static ut32 getshift(const char *str) {
 	char type[128];
 	char arg[128];
