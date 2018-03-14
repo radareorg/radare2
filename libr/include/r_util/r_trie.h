7a2f31abe6d66e1ea9e6978e81278e61b901aac1
Farid AYOUJIL <ayoujil.farid@gmail.com>
2018-03-14 13:05:37 +0000

    Adding trie to util

--- libr/include/r_util/r_trie.h
+++ libr/include/r_util/r_trie.h
@@ -4,15 +4,15 @@
 
 #define ALPHABET_SIZE 63 // A-Z, a-z, 0-9 and _
 
-typedef struct r_trie_node{
+typedef struct r_trie_node {
 	struct r_trie_node * child[ALPHABET_SIZE];
 	int is_leaf;
 	void * data;
 } TrieNode;
 
-typedef struct r_trie{
+typedef struct r_trie {
 	TrieNode *root;
-}Trie;
+} Trie;
 
 R_API Trie *r_trie_new(void);
 R_API bool r_trie_insert(Trie * t, char * name, void * f);
@@ -20,6 +20,4 @@ R_API bool r_trie_update(Trie * t, char * name, void * f);
 R_API bool r_trie_delete(Trie * t, char * name);
 R_API void *r_trie_find(Trie * t, char * name);
 R_API void r_trie_free(Trie * t);
-
-
 #endif
