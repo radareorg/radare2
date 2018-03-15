a5c9fc78555891193f231c7b51db2f2c9954d411
Gromimousse <36596087+Gromimousse@users.noreply.github.com>
2018-03-15 11:48:21 +0100

    Ragg2-cc -> Ragg2 merge (#9658)
    - change of 'access' function for 'r_file_exists' for windows portability
    - several changes in the way ragg2.c is done, see PR #9658 pancake comments
    - change function for the remove of 'file'.text
    - open changed for r_file_dump
    - some elt of cEnv_t are now const
    - skip all the pointers verification in set_cEnv but do them in check_cEnv instead
    - add 2 r_str_sanitize for file and CC in parseCFile
    - rewrite the removal of .o, .tmp, .bin, .s files, with cleaner code
    - changed the long command with sed and grep to 2 C functions.
      - The function parseCompiled that basically does what the command was doing
      - And r_str_stripLines that is equivalent to "grep -v" (maybe we should put this one in str.c ?)
    - simplify a bit getCompiler function with a const char* array
    - add ternary operator for armOrMips condition
    - use r_file_path for finding path to compilers
    - new file created in libr/egg which contains all the C file parser
    - modifications of 2 files to match the change :
      - libr/egg/r_egg.h
      - libr/egg/Makefile
    - the function r_str_stripLine is simplier (the mallocs wasn't needed)
    - the function r_str_stripLine is moved to libr/util/str.c
    - libr/include/r_util/r_str.h is changed accordingly

--- libr/util/str.c
+++ libr/util/str.c
@@ -2936,3 +2936,42 @@ R_API int r_snprintf(char *string, int len, const char *fmt, ...) {
 	va_end (ap);
 	return ret;
 }
+
+// Strips all the lines in str that contain key
+R_API void r_str_stripLine(char *str, const char *key)
+{
+	size_t i, j, klen, slen, off;
+	const char *ptr; 
+
+	if (!str || !key) {
+		return;
+	}
+	klen = strlen (key);
+	slen = strlen (str);
+
+	for (i = 0; i < slen; ) {
+		ptr = (char*) r_mem_mem ((ut8*) str + i, slen - i, (ut8*) "\n", 1);
+		if (!ptr) {
+			ptr = (char*) r_mem_mem ((ut8*) str + i, slen - i, (ut8*) key, klen);
+			if (ptr) {
+				str[i] = '\0';
+				break;
+			}
+			break;
+		}
+			
+		off = (size_t) (ptr - (str + i)) + 1;
+
+		ptr = (char*) r_mem_mem ((ut8*) str + i, off, (ut8*) key, klen);
+		if (ptr) {
+			for (j = i; j < slen - off + 1; j++) {
+				str[j] = str[j + off];
+			}
+			slen -= off;
+		} else {
+			i += off;
+		}
+	}
+	return;
+}
+
