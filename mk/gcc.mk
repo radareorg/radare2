527b7f64c4a3c7bca3dd2bd897636564c8fa7efa
johannes <johannes.brechtmann@gmail.com>
2018-03-19 00:40:02 +0100

    Makefile: use ${LD} or ${CROSS}ld instead of ld

--- mk/gcc.mk
+++ mk/gcc.mk
@@ -14,17 +14,18 @@ CFLAGS_OPT1=-O1
 CFLAGS_OPT2=-O2
 CFLAGS_OPT3=-O3
 CFLAGS_DEBUG=-g
+LD?=ld
 
 ifeq ($(OSTYPE),darwin)
 ARCH=$(shell uname -m)
 #CFLAGS+=-arch ${ARCH}
 #LDFLAGS+=-arch ${ARCH}
-PARTIALLD=ld -r -all_load
+PARTIALLD=${LD} -r -all_load
 CFLAGS+=-fno-common
 LDFLAGS_LIB=-dynamiclib
 LDFLAGS_SONAME=-Wl,-install_name,${LIBDIR}/
 else
-PARTIALLD=ld -r --whole-archive
+PARTIALLD=${LD} -r --whole-archive
 LDFLAGS_LIB=${LDFLAGS} -shared
 #ifneq (${NAME},)
 #LDFLAGS_LIB+=-Wl,-soname,lib${NAME}.${EXT_SO}.${VERSION}
