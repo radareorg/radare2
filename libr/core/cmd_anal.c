3b8e645770c633b4d6df7660910849da886072e1
pancake <pancake@nopcode.org>
2018-04-04 22:19:35 +0200

    Fix #9707 - Remove aos from help message

--- libr/core/cmd_anal.c
+++ libr/core/cmd_anal.c
@@ -481,7 +481,6 @@ static const char *help_msg_ao[] = {
 	"aoj", " N", "display opcode analysis information in JSON for N opcodes",
 	"aoe", " N", "display esil form for N opcodes",
 	"aor", " N", "display reil form for N opcodes",
-	"aos", " [esil]", "show sdb representation of esil expression (TODO)",
 	"ao", " 5", "display opcode analysis of 5 opcodes",
 	"ao*", "", "display opcode in r commands",
 	NULL
