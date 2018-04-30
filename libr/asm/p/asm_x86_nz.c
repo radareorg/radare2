8e76e119318611cb06a45b06455ec5a584128f4d
Silur <abrahamendre9@gmail.com>
2018-04-30 13:16:11 +0200

    minor syntax fixes

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -555,20 +555,20 @@ static int opor(RAsm *a, ut8 * data, const Opcode *op) {
 
 static int opxadd(RAsm *a, ut8 *data, const Opcode *op) {
 	int i = 0;
-	if(op->operands_count < 1) {
+	if (op->operands_count < 2 ) {
 		return -1;
 	}
-	if(a->bits == 64) {
+	if (a->bits == 64) {
 		data[i++] = 0x48;
 	};
 	data[i++] = 0x0f;
-	if(op->operands[0].type & OT_BYTE &&
+	if (op->operands[0].type & OT_BYTE &&
 	   op->operands[1].type & OT_BYTE) {
 		data[i++] = 0xc0;
 	} else {
 		data[i++] = 0xc1;
 	}
-	if(op->operands[0].type & OT_REGALL &&
+	if (op->operands[0].type & OT_REGALL &&
        op->operands[1].type & OT_REGALL) { // TODO memory modes
 		data[i] |= 0xc0;
 		data[i] |= (op->operands[1].reg << 3);
