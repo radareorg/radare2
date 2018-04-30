a2fbe0ed2dfa95365e04b61a1c33dc2cc7905516
Silur <abrahamendre9@gmail.com>
2018-04-30 17:55:11 +0200

    xadd for x86.nz (regs only) (#9978)
    * xadd for x86.nz (regs only)
    
    * minor syntax fixes

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -553,6 +553,30 @@ static int opor(RAsm *a, ut8 * data, const Opcode *op) {
 	return process_1byte_op (a, data, op, 0x08);
 }
 
+static int opxadd(RAsm *a, ut8 *data, const Opcode *op) {
+	int i = 0;
+	if (op->operands_count < 2 ) {
+		return -1;
+	}
+	if (a->bits == 64) {
+		data[i++] = 0x48;
+	};
+	data[i++] = 0x0f;
+	if (op->operands[0].type & OT_BYTE &&
+	   op->operands[1].type & OT_BYTE) {
+		data[i++] = 0xc0;
+	} else {
+		data[i++] = 0xc1;
+	}
+	if (op->operands[0].type & OT_REGALL &&
+       op->operands[1].type & OT_REGALL) { // TODO memory modes
+		data[i] |= 0xc0;
+		data[i] |= (op->operands[1].reg << 3);
+		data[i++] |= op->operands[0].reg;
+	}
+	return i;
+}
+
 static int opxor(RAsm *a, ut8 * data, const Opcode *op) {
 	if (op->operands_count < 2) {
 		return -1;
@@ -3959,6 +3983,7 @@ LookupTable oplookup[] = {
 	{"wait", 0, NULL, 0x9b, 1},
 	{"wbinvd", 0, NULL, 0x0f09, 2},
 	{"wrmsr", 0, NULL, 0x0f30, 2},
+	{"xadd", 0, &opxadd, 0},
 	{"xchg", 0, &opxchg, 0},
 	{"xgetbv", 0, NULL, 0x0f01d0, 3},
 	{"xlatb", 0, NULL, 0xd7, 1},
