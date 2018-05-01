efe67e42470228d99111ee0e17c9af96bd19ad42
Khairul Kasmiran <kazarmy@gmail.com>
2018-05-01 15:10:53 +0800

    Revert "Force I.ansicon to true"
    This reverts commit 1b85a0f6bff8d69b3b01114de855a7cc4d923cdb.

--- libr/cons/cons.c
+++ libr/cons/cons.c
@@ -301,7 +301,7 @@ R_API RCons *r_cons_new() {
 	I.num = NULL;
 	I.null = 0;
 #if __WINDOWS__ && !__CYGWIN__
-	I.ansicon = true;
+	I.ansicon = r_sys_getenv ("ANSICON");
 #endif
 #if EMSCRIPTEN
 	/* do nothing here :? */
