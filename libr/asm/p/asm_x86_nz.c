8a781abc5149b1ffb938f50f634cb2463fbdf7a5
Silur <abrahamendre9@gmail.com>
2018-05-06 20:39:26 +0200

    add extra check for extended reg+mem operand case (#10028)

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -1724,6 +1724,8 @@ static int opmov(RAsm *a, ut8 *data, const Opcode *op) {
 					}
 					data[l++] = 0x48;
 				}
+			} else if (op->operands[1].type & OT_DWORD) {
+				data[l++] = 0x44;
 			} else if (!(op->operands[1].type & OT_QWORD)) {
 				data[l++] = 0x67;
 			}
