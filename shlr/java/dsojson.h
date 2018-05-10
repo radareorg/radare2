e738b1f4594e6d8bfbf215140aebca76e5b9fc0c
Paul I <pelijah@users.noreply.github.com>
2018-05-11 01:11:16 +0300

    Meson: some cleanup

--- shlr/java/dsojson.h
+++ shlr/java/dsojson.h
@@ -57,59 +57,59 @@ typedef struct  basic_json_t {
 	} val;
 } DsoJsonObj;
 
-R_API ut8  dso_json_char_needs_hexing ( ut8 b);
+ut8  dso_json_char_needs_hexing ( ut8 b);
 
-R_API void dso_json_obj_del (DsoJsonObj *dso_obj);
-R_API char * dso_json_obj_to_str (DsoJsonObj * dso_obj);
-R_API DsoJsonObj * dso_json_null_new (void);
-R_API void dso_json_null_free (void *x);
-R_API DsoJsonObj * dso_json_str_new (void);
-R_API void dso_json_str_free (void *y);
-R_API DsoJsonObj * dso_json_str_new_from_str (const char *str);
-R_API DsoJsonObj * dso_json_str_new_from_str_len (const char *str, unsigned int len);
-R_API DsoJsonObj * dso_json_str_new_from_num (long num);
+void dso_json_obj_del (DsoJsonObj *dso_obj);
+char * dso_json_obj_to_str (DsoJsonObj * dso_obj);
+DsoJsonObj * dso_json_null_new (void);
+void dso_json_null_free (void *x);
+DsoJsonObj * dso_json_str_new (void);
+void dso_json_str_free (void *y);
+DsoJsonObj * dso_json_str_new_from_str (const char *str);
+DsoJsonObj * dso_json_str_new_from_str_len (const char *str, unsigned int len);
+DsoJsonObj * dso_json_str_new_from_num (long num);
 
-R_API DsoJsonObj * dso_json_dict_entry_new (void);
-R_API void dso_json_dict_entry_free (void *y);
-R_API int dso_json_dict_entry_set_key_str (DsoJsonObj * entry_obj, char *key);
-R_API int dso_json_dict_entry_set_key_str_len (DsoJsonObj * entry_obj, char *key, unsigned int len);
-R_API int dso_json_dict_entry_set_key_num (DsoJsonObj * entry_obj, st64 num);
-R_API int dso_json_dict_entry_value_append_str (DsoJsonObj *entry_obj, char *str);
-R_API int dso_json_dict_entry_value_append_str_len (DsoJsonObj *entry_obj, char *str, unsigned int len);
-R_API int dso_json_dict_entry_value_append_num (DsoJsonObj *entry_obj, st64 num);
-R_API int dso_json_dict_entry_value_append_obj (DsoJsonObj *entry_obj, DsoJsonObj *obj);
-R_API int dso_json_dict_entry_value_set_str (DsoJsonObj *entry_obj, char *str);
-R_API int dso_json_dict_entry_value_set_str_len (DsoJsonObj *entry_obj, char *str, unsigned int len);
-R_API int dso_json_dict_entry_value_set_num (DsoJsonObj *entry_obj, st64 num);
-R_API int dso_json_dict_entry_value_set_empty_dict (DsoJsonObj *entry_obj);
-R_API int dso_json_dict_entry_value_set_empty_list (DsoJsonObj *entry_obj);
-R_API int dso_json_dict_entry_value_set_obj (DsoJsonObj *entry_obj, DsoJsonObj *obj);
+DsoJsonObj * dso_json_dict_entry_new (void);
+void dso_json_dict_entry_free (void *y);
+int dso_json_dict_entry_set_key_str (DsoJsonObj * entry_obj, char *key);
+int dso_json_dict_entry_set_key_str_len (DsoJsonObj * entry_obj, char *key, unsigned int len);
+int dso_json_dict_entry_set_key_num (DsoJsonObj * entry_obj, st64 num);
+int dso_json_dict_entry_value_append_str (DsoJsonObj *entry_obj, char *str);
+int dso_json_dict_entry_value_append_str_len (DsoJsonObj *entry_obj, char *str, unsigned int len);
+int dso_json_dict_entry_value_append_num (DsoJsonObj *entry_obj, st64 num);
+int dso_json_dict_entry_value_append_obj (DsoJsonObj *entry_obj, DsoJsonObj *obj);
+int dso_json_dict_entry_value_set_str (DsoJsonObj *entry_obj, char *str);
+int dso_json_dict_entry_value_set_str_len (DsoJsonObj *entry_obj, char *str, unsigned int len);
+int dso_json_dict_entry_value_set_num (DsoJsonObj *entry_obj, st64 num);
+int dso_json_dict_entry_value_set_empty_dict (DsoJsonObj *entry_obj);
+int dso_json_dict_entry_value_set_empty_list (DsoJsonObj *entry_obj);
+int dso_json_dict_entry_value_set_obj (DsoJsonObj *entry_obj, DsoJsonObj *obj);
 
-R_API DsoJsonObj * dso_json_list_new (void);
-R_API void dso_json_list_free (DsoJsonObj *x);
-R_API int dso_json_list_append (DsoJsonObj *list_obj, DsoJsonObj *y);
-R_API int dso_json_list_append_str (DsoJsonObj *list_obj, char *y);
-R_API int dso_json_list_append_num (DsoJsonObj *list_obj, ut64 y);
+DsoJsonObj * dso_json_list_new (void);
+void dso_json_list_free (DsoJsonObj *x);
+int dso_json_list_append (DsoJsonObj *list_obj, DsoJsonObj *y);
+int dso_json_list_append_str (DsoJsonObj *list_obj, char *y);
+int dso_json_list_append_num (DsoJsonObj *list_obj, ut64 y);
 
-R_API DsoJsonObj * dso_json_dict_new (void);
-R_API int dso_json_dict_insert_str_key_obj (DsoJsonObj *dict, char *key, DsoJsonObj *val_obj);
-R_API int dso_json_dict_insert_str_key_num (DsoJsonObj *dict, char *key, int val);
-R_API int dso_json_dict_insert_str_key_str (DsoJsonObj *dict, char *key, char *val);
-R_API int dso_json_dict_insert_num_key_obj (DsoJsonObj *dict, int key, DsoJsonObj *val_obj);
-R_API int dso_json_dict_insert_num_key_num (DsoJsonObj *dict, int key, int val);
-R_API int dso_json_dict_insert_num_key_str (DsoJsonObj *dict, int key, char *val);
-R_API int dso_json_dict_insert_key_obj (DsoJsonObj *dict, DsoJsonObj *key, DsoJsonObj *value);
-R_API int dso_json_dict_remove_key_obj (DsoJsonObj *dict, DsoJsonObj *key);
-R_API int dso_json_dict_remove_key_str (DsoJsonObj *dict, char *key);
-R_API int dso_json_dict_contains_key_str (DsoJsonObj *dict, char *key);
-R_API int dso_json_dict_contains_key_obj (DsoJsonObj *dict, DsoJsonObj *key);
-R_API void dso_json_dict_free (void * y);
+DsoJsonObj * dso_json_dict_new (void);
+int dso_json_dict_insert_str_key_obj (DsoJsonObj *dict, char *key, DsoJsonObj *val_obj);
+int dso_json_dict_insert_str_key_num (DsoJsonObj *dict, char *key, int val);
+int dso_json_dict_insert_str_key_str (DsoJsonObj *dict, char *key, char *val);
+int dso_json_dict_insert_num_key_obj (DsoJsonObj *dict, int key, DsoJsonObj *val_obj);
+int dso_json_dict_insert_num_key_num (DsoJsonObj *dict, int key, int val);
+int dso_json_dict_insert_num_key_str (DsoJsonObj *dict, int key, char *val);
+int dso_json_dict_insert_key_obj (DsoJsonObj *dict, DsoJsonObj *key, DsoJsonObj *value);
+int dso_json_dict_remove_key_obj (DsoJsonObj *dict, DsoJsonObj *key);
+int dso_json_dict_remove_key_str (DsoJsonObj *dict, char *key);
+int dso_json_dict_contains_key_str (DsoJsonObj *dict, char *key);
+int dso_json_dict_contains_key_obj (DsoJsonObj *dict, DsoJsonObj *key);
+void dso_json_dict_free (void * y);
 
-R_API DsoJsonObj * dso_json_num_new (void);
-R_API void dso_json_num_free (void *y);
-R_API DsoJsonObj * dso_json_num_new_from_num (ut64 num);
+DsoJsonObj * dso_json_num_new (void);
+void dso_json_num_free (void *y);
+DsoJsonObj * dso_json_num_new_from_num (ut64 num);
 
-R_API char * dso_json_convert_string (const char * bytes, ut32 len );
+char * dso_json_convert_string (const char * bytes, ut32 len );
 
 
 #endif
