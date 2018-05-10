3961a3458465b093cf2a20da5d3d00f51d3183da
Riccardo Schirone <ret2libc@users.noreply.github.com>
2018-05-10 20:35:33 +0200

    libr: remove some old "mrwx" permissions. m does not exist anymore (#10067)

--- libr/core/cmd_section.c
+++ libr/core/cmd_section.c
@@ -17,7 +17,7 @@ static const char *help_msg_S[] = {
 	"S=","","list sections (ascii-art bars) (io.va to display paddr or vaddr)",
 	"Sa","[-] [A] [B] [[off]]","Specify arch and bits for given section",
 	"Sd[a]"," [file]","dump current (all) section to a file (see dmd)",
-	"Sf"," [baddr]","Alias for S 0 0 $s $s foo mrwx",
+	"Sf"," [baddr]","Alias for S 0 0 $s $s foo rwx",
 	"Sj","","list sections in JSON (alias for iSj)",
 	"Sl"," [file]","load contents of file into current section (see dml)",
 	"Sr"," [name]","rename section on current seek",
@@ -342,9 +342,9 @@ static int cmd_section(void *data, const char *input) {
 	case 'f': // "Sf"
 		if (input[1] == ' ') {
 			ut64 n = r_num_math (core->num, input + 1);
-			r_core_cmdf (core, "S 0x%"PFMT64x" 0x%"PFMT64x" $s $s foo mrwx", n, n);
+			r_core_cmdf (core, "S 0x%"PFMT64x" 0x%"PFMT64x" $s $s foo rwx", n, n);
 		} else {
-			r_core_cmd0 (core, "S 0 0 $s $s foo mrwx");
+			r_core_cmd0 (core, "S 0 0 $s $s foo rwx");
 		}
 		break;
 	case 'j': // "Sj"
