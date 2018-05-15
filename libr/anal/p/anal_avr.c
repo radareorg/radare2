d04c78773f6959bcb427453f8e5b9824d5ba9eff
pancake <pancake@nopcode.org>
2018-05-15 14:44:47 +0200

    Fix #10091 - crash in AVR analysis

--- libr/anal/p/anal_avr.c
+++ libr/anal/p/anal_avr.c
@@ -925,6 +925,9 @@ INST_HANDLER (lds) {	// LDS Rd, k
 }
 
 INST_HANDLER (sts) {	// STS k, Rr
+	if (len < 4) {
+		return;
+	}
 	int r = ((buf[0] >> 4) & 0xf) | ((buf[1] & 0x1) << 4);
 	int k = (buf[3] << 8) | buf[2];
 	op->ptr = k;
