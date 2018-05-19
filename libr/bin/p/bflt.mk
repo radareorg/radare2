220a94d100a57712779e2f16abf98c7c67040308
pancake <pancake@nopcode.org>
2018-05-20 00:45:51 +0200

    Fix static build

--- libr/bin/p/bflt.mk
+++ libr/bin/p/bflt.mk
@@ -6,19 +6,6 @@ TARGET_BFLT=bin_bflt.${EXT_SO}
 
 ALL_TARGETS+=${TARGET_BFLT}
 
-ifeq ($(WITHPIC),0)
-LINK+=../../io/libr_io.${EXT_AR}
-LINK+=../../util/libr_util.${EXT_AR}
-LINK+=../../magic/libr_magic.${EXT_AR}
-LINK+=../../socket/libr_socket.${EXT_AR}
-LINK+=../../../shlr/gdb/lib/libgdbr.${EXT_AR}
-LINK+=../../../shlr/windbg/libr_windbg.${EXT_AR}
-LINK+=../../../shlr/qnx/lib/libqnxr.${EXT_AR}
-LINK+=../../../shlr/bochs/lib/libbochs.${EXT_AR}
-LINK+=../../../shlr/java/libr_java.${EXT_AR}
-LINK+=../../../shlr/ar/libr_ar.${EXT_AR}
-endif
-
 ${TARGET_BFLT}: ${OBJ_BFLT}
 	${CC} $(call libname,bin_bflt) ${CFLAGS} \
 		$(OBJ_BFLT) $(LINK) $(LDFLAGS)
