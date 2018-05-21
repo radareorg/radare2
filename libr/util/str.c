25bc31e6b5e88bd6475e94ebb31c393dbb2896f7
pancake <pancake@nopcode.org>
2018-05-21 19:02:03 +0200

    Fix windows build

--- libr/util/str.c
+++ libr/util/str.c
@@ -35,7 +35,7 @@ static const char *rwxstr[] = {
 
 R_API int r_str_casecmp(const char *s1, const char *s2) {
 #ifdef _MSC_VER
-	return stricmp (s1, s2, n);
+	return stricmp (s1, s2);
 #else
 	return strcasecmp (s1, s2);
 #endif
