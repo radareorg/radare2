3d6efce76d77f63668f6f0e346a38a5e8fc60c4e
pancake <pancake@nopcode.org>
2018-05-28 11:09:31 +0200

    Fix #10207 - Use r_cons_print instead of printf

--- binr/rabin2/rabin2.c
+++ binr/rabin2/rabin2.c
@@ -150,7 +150,7 @@ static bool extract_binobj(const RBinFile *bf, RBinXtrData *data, int idx) {
 	int bits = 0;
 	char *libname = NULL;
 	const char *filename = bf ? bf->file : NULL;
-	char *path = NULL, *outpath = NULL, *outfile = NULL, *ptr = NULL;
+	char *path = NULL, *outpath = NULL, *ptr = NULL;
 	ut32 outfile_sz = 0, outpath_sz = 0;
 	bool res = false;
 
@@ -200,26 +200,15 @@ static bool extract_binobj(const RBinFile *bf, RBinXtrData *data, int idx) {
 		return false;
 	}
 
-	outfile_sz = outpath_sz + strlen (ptr) + strlen (arch) + 23;
-	if (outfile_sz) {
-		outfile = malloc (outfile_sz);
-	}
-
-	if (outfile) {
-		if (libname) {
-			snprintf (outfile, outfile_sz, "%s/%s.%s.%s_%i.%d",
-					  outpath, ptr, arch, libname, bits, idx);
-		} else {
-			snprintf (outfile, outfile_sz, "%s/%s.%s_%i.%d",
-					  outpath, ptr, arch, bits, idx);
-		}
-	}
+	char *outfile = libname
+		? r_str_newf ("%s/%s.%s.%s_%i.%d", outpath, ptr, arch, libname, bits, idx)
+		: r_str_newf ("%s/%s.%s_%i.%d", outpath, ptr, arch, bits, idx);
 
 	if (!outfile || !r_file_dump (outfile, bytes, bin_size, 0)) {
 		eprintf ("Error extracting %s\n", outfile);
 		res = false;
 	} else {
-		printf ("%s created (%"PFMT64d")\n", outfile, bin_size);
+		eprintf ("%s created (%"PFMT64d")\n", outfile, bin_size);
 		res = true;
 	}
 
@@ -1116,7 +1105,7 @@ int main(int argc, char **argv) {
 		rabin_do_operation (op);
 	}
 	if (isradjson) {
-		printf ("}");
+		r_cons_print ("}");
 	}
 	r_cons_flush ();
 	r_core_fini (&core);
