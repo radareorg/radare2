4b1a31bb647229f3a9b9597d895640cd679c2fc4
rene <rlaemmert@gmail.com>
2018-05-29 10:11:06 +0200

    started changing bin_load to bin_open

--- libr/bin/open.c
+++ libr/bin/open.c
@@ -18,14 +18,16 @@ R_API void r_bin_options_free(RBinOptions *bo) {
 }
 
 R_API int r_bin_open(RBin *bin, const char *filename, RBinOptions *bo) {
-	ut64 baddr = 0LL;
-	int iofd = -1, rawstr = 0;
+	ut64 baddr = 0LL, laddr = 0LL;
+	int iofd = -1, rawstr = 0, xtr_idx = 0;
 	if (bo) {
 		baddr = bo->baseaddr;
+		laddr = bo->loadaddr;
+		xtr_idx = bo->xtr_idx;
 		iofd = bo->iofd;
 		rawstr = bo->rawstr;
 	}
-	if (r_bin_load (bin, filename, baddr, 0, 0, iofd, rawstr)) {
+	if (r_bin_load (bin, filename, baddr, laddr, xtr_idx, iofd, rawstr)) {
 		int id = bin->cur->id;
 		r_id_storage_set (bin->ids, bin->cur, id);
 		return id;
