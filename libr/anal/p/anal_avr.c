d3796376f542906506be5b77a335db6e082d7e4a
condret <condr3t@protonmail.com>
2018-06-04 12:59:12 +0000

    fix avr esil "=="

--- libr/anal/p/anal_avr.c
+++ libr/anal/p/anal_avr.c
@@ -681,7 +681,7 @@ INST_HANDLER (dec) {	// DEC Rd
 	int d = ((buf[0] >> 4) & 0xf) | ((buf[1] & 0x1) << 4);
 	ESIL_A ("-1,r%d,+,", d);				// --Rd
 								// FLAGS:
-	ESIL_A ("0,RPICK,0x7f,==,vf,=,");			// V
+	ESIL_A ("0,RPICK,0x7f,==,$z,vf,=,");			// V
 	ESIL_A ("0,RPICK,0x80,&,!,!,nf,=,");			// N
 	ESIL_A ("0,RPICK,!,zf,=,");				// Z
 	ESIL_A ("vf,nf,^,sf,=,");				// S
@@ -848,7 +848,7 @@ INST_HANDLER (inc) {	// INC Rd
 	int d = ((buf[0] >> 4) & 0xf) | ((buf[1] & 0x1) << 4);
 	ESIL_A ("1,r%d,+,", d);					// ++Rd
 								// FLAGS:
-	ESIL_A ("0,RPICK,0x80,==,vf,=,");			// V
+	ESIL_A ("0,RPICK,0x80,==,$z,vf,=,");			// V
 	ESIL_A ("0,RPICK,0x80,&,!,!,nf,=,");			// N
 	ESIL_A ("0,RPICK,!,zf,=,");				// Z
 	ESIL_A ("vf,nf,^,sf,=,");				// S
