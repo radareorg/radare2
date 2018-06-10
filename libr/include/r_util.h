e3b05c7caac3084d1e8a1565fa547031068d7077
David Carlier <devnexen@gmail.com>
2018-06-10 20:58:19 +0100

    config before file creation

--- libr/include/r_util.h
+++ libr/include/r_util.h
@@ -70,7 +70,6 @@ int gettimeofday (struct timeval* p, void* tz);
 #include "r_util/r_json.h"
 #include "r_util/r_x509.h"
 #include "r_util/r_pkcs7.h"
-#include "r_util/r_rand.h"
 
 #ifdef __cplusplus
 extern "C" {
