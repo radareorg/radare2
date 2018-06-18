77d52da13b53a131ef201453e91987114d584aaf
Khairul Kasmiran <kazarmy@gmail.com>
2018-06-18 12:54:14 +0800

    dc: show error msg if debugger is dead (Linux)

--- libr/core/cmd_debug.c
+++ libr/core/cmd_debug.c
@@ -3879,7 +3879,7 @@ static bool cmd_dcu (RCore *core, const char *input) {
 }
 
 static int cmd_debug_continue (RCore *core, const char *input) {
-	int pid, old_pid, signum;
+	int pid, old_pid, signum, ret = true;
 	char *ptr;
 	// TODO: we must use this for step 'ds' too maybe...
 	switch (input[1]) {
@@ -3887,10 +3887,13 @@ static int cmd_debug_continue (RCore *core, const char *input) {
 		r_reg_arena_swap (core->dbg->reg, true);
 #if __linux__
 		core->dbg->continue_all_threads = true;
-		r_debug_continue (core->dbg);
+		ret = r_debug_continue (core->dbg);
 #else
 		r_debug_continue (core->dbg);
 #endif
+		if (!ret && r_debug_is_dead (core->dbg)) {
+			eprintf ("Cannot continue, run ood?\n");
+		}
 		break;
 	case 'a': // "dca"
 		eprintf ("TODO: dca\n");
