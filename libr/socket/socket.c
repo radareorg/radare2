2202d140675d70cb6b767641b3528e2c9e0ea572
Florian Märkl <info@florianmaerkl.de>
2018-06-20 11:20:55 +0200

    Timeout socket accept using select (#10415)

--- libr/socket/socket.c
+++ libr/socket/socket.c
@@ -590,6 +590,30 @@ R_API RSocket *r_socket_accept(RSocket *s) {
 	return sock;
 }
 
+R_API RSocket *r_socket_accept_timeout(RSocket *s, unsigned int timeout) {
+	fd_set read_fds;
+	fd_set except_fds;
+
+	FD_ZERO (&read_fds);
+	FD_SET (s->fd, &read_fds);
+
+	FD_ZERO (&except_fds);
+	FD_SET (s->fd, &except_fds);
+
+	struct timeval t;
+	t.tv_sec = timeout;
+	t.tv_usec = 0;
+
+	int r = select (s->fd + 1, &read_fds, NULL, &except_fds, &t);
+	if(r < 0) {
+		perror ("select");
+	} else if (r > 0 && FD_ISSET (s->fd, &read_fds)) {
+		return r_socket_accept (s);
+	}
+
+	return NULL;
+}
+
 R_API int r_socket_block_time (RSocket *s, int block, int sec) {
 #if __UNIX__ || defined(__CYGWIN__)
 	int ret, flags;
