583bbd22b6dc9b37513dcc4964a60a03b98f267c
wargio <wargio@libero.it>
2018-06-21 18:07:50 +0200

    hide readonly

--- libr/core/cmd.c
+++ libr/core/cmd.c
@@ -1414,13 +1414,16 @@ static inline void print_dict(RCoreAutocomplete* a, int sub) {
 	}
 	for (i = 0; i < a->n_subcmds; ++i) {
 		RCoreAutocomplete* b = a->subcmds[i];
+		if (b->locked) {
+			continue;
+		}
 		for (j = 0; j < R_CORE_AUTOCMPLT_END; ++j) {
 			if (b->type == autocomplete_flags[j].type) {
 				name = autocomplete_flags[j].name;
 				break;
 			}
 		}
-		eprintf ("[%3d] %s: '%s'%s\n", sub, name, b->cmd, b->locked ? " (readonly)" : "");
+		eprintf ("[%3d] %s: '%s'\n", sub, name, b->cmd);
 		print_dict (a->subcmds[i], sub + 1);
 	}
 }
