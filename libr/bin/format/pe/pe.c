8c3d9c6266edd0eb7ca01bb4a85e061c39a1f558
ITAYC0HEN <itaycohen23@gmail.com>
2018-06-23 11:57:21 +0100

    fix indention

--- libr/bin/format/pe/pe.c
+++ libr/bin/format/pe/pe.c
@@ -2190,52 +2190,72 @@ static char* _resource_lang_str(int id) {
 static char* _resource_type_str(int type) {
 	char * typeName;
 	switch (type) {
-		case 1: typeName = "CURSOR";
-			break;
-		case 2: typeName = "BITMAP";
-			break;
-		case 3: typeName = "ICON";
-			break;
-		case 4: typeName = "MENU";
-			break;
-		case 5: typeName = "DIALOG";
-			break;
-		case 6: typeName = "STRING";
-			break;
-		case 7: typeName = "FONTDIR";
-			break;
-		case 8: typeName = "FONT";
-			break;
-		case 9: typeName = "ACCELERATOR";
-			break;
-		case 10: typeName = "RCDATA";
-			break;
-		case 11: typeName = "MESSAGETABLE";
-			break;
-		case 12: typeName = "GROUP_CURSOR";
-			break;
-		case 14: typeName = "GROUP_ICON";
-			break;
-		case 16: typeName = "VERSION";
-			break;
-		case 17: typeName = "DLGINCLUDE";
-			break;
-		case 19: typeName = "PLUGPLAY";
-			break;
-		case 20: typeName = "VXD";
-			break;
-		case 21: typeName = "ANICURSOR";
-			break;
-		case 22: typeName = "ANIICON";
-			break;
-		case 23: typeName = "HTML";
-			break;
-		case 24: typeName = "MANIFEST";
-			break;
-		default: return r_str_newf ("UNKNOWN (%d)", type);
-
-		return strdup (typeName);
+	case 1:
+		typeName = "CURSOR";
+		break;
+	case 2:
+		typeName = "BITMAP";
+		break;
+	case 3:
+		typeName = "ICON";
+		break;
+	case 4:
+		typeName = "MENU";
+		break;
+	case 5:
+		typeName = "DIALOG";
+		break;
+	case 6:
+		typeName = "STRING";
+		break;
+	case 7:
+		typeName = "FONTDIR";
+		break;
+	case 8:
+		typeName = "FONT";
+		break;
+	case 9:
+		typeName = "ACCELERATOR";
+		break;
+	case 10:
+		typeName = "RCDATA";
+		break;
+	case 11:
+		typeName = "MESSAGETABLE";
+		break;
+	case 12:
+		typeName = "GROUP_CURSOR";
+		break;
+	case 14:
+		typeName = "GROUP_ICON";
+		break;
+	case 16:
+		typeName = "VERSION";
+		break;
+	case 17:
+		typeName = "DLGINCLUDE";
+		break;
+	case 19:
+		typeName = "PLUGPLAY";
+		break;
+	case 20:
+		typeName = "VXD";
+		break;
+	case 21:
+		typeName = "ANICURSOR";
+		break;
+	case 22:
+		typeName = "ANIICON";
+		break;
+	case 23:
+		typeName = "HTML";
+		break;
+	case 24:
+		typeName = "MANIFEST";
+		break;
+	default: return r_str_newf ("UNKNOWN (%d)",type);
 	}
+	return strdup (typeName);
 }
 
 static void _parse_resource_directory(struct PE_(r_bin_pe_obj_t) *bin, Pe_image_resource_directory *dir, ut64 offDir, int type, int id, SdbHash *dirs) {
