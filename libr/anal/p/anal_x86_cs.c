35d4f326136a943fe3a242c73d7b341a37fe8ad8
Khairul Kasmiran <kazarmy@gmail.com>
2018-06-25 21:37:48 +0800

    x86_cs MOV family: set val to 2nd operand if imm and if 1st operand is reg

--- libr/anal/p/anal_x86_cs.c
+++ libr/anal/p/anal_x86_cs.c
@@ -1981,6 +1981,10 @@ static void anop(RAnal *a, RAnalOp *op, ut64 addr, const ut8 *buf, int len, csh 
 			}
 			break;
 		case X86_OP_REG:
+			if (INSOP(1).type == X86_OP_IMM) {
+				op->val = INSOP(1).imm;
+			}
+			break;
 		default:
 			break;
 		}
