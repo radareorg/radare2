ed73ea036cff9db9dfe6dddb011e9d8d58ff0feb
Riccardo Schirone <ret2libc@users.noreply.github.com>
2018-07-06 23:35:55 +0200

    travis.yml: try to run ASAN on travis (#10645)
    * fixing travis with docker. Env vars weren't passed to docker, so it was compiling always in the same way and not with clang/gcc
    * replacing damo22/r2-travis with radareorg/r2-travis
    * introduce an ASAN build on travis

---
+++ travis-extract-var.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+print_var()
+{
+    VAR_NAME=$1
+    VAR_VALUE=$2
+    if [ "${VAR_VALUE}" != "" ] ; then
+	echo "${VAR_NAME}=${VAR_VALUE}"
+    fi
+}
+
+env -0 | while IFS='=' read -r -d '' n v; do
+    if [[ "${n}" =~ ^TRAVIS_* || "${n}" =~ ^R2* || "${n}" =~ ^ASAN* ]]; then
+	print_var "${n}" "${v}"
+    fi
+done
+
+# print extra variables
+print_var CC "${CC}"
+print_var CXX "${CXX}"
+print_var CFLAGS "${CFLAGS}"
+print_var LDFLAGS "${LDFLAGS}"
+print_var CXXFLAGS "${CXXFLAGS}"
+
