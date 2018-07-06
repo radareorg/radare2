9ce48349804dfbf16a24df1694d9c5b5ea3381ed
Riccardo Schirone <sirmy15@gmail.com>
2018-07-06 16:20:16 +0200

    travis: extract variable from host and pass them to docker

---
+++ travis-extract-var.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+print_var()
+{
+    VAR_NAME=$1
+    VAR_VALUE=$2
+    if [ "${VAR_VALUE}" != "" ] ; then
+	echo "${VAR_NAME}='${VAR_VALUE}'"
+    fi
+}
+
+# print all variables that start with TRAVIS_ and R2R_
+env -0 | while IFS='=' read -r -d '' n v; do
+    if [ "${n}" == "TRAVIS_*" ] || [ "%{n}" == "R2R_*" ]; then
+	print_var "${n}" "${v}"
+    fi
+done
+
+# print extra variables
+print_var CC "${CC}"
+print_var CXX "${CXX}"
+print_var CFLAGS "${CFLAGS}"
+print_var LDFLAGS "${LDFLAGS}"
+print_var CXXFLAGS "${CXXFLAGS}"
+
