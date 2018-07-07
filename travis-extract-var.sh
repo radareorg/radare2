f7df892e9c3977a6334371bdffcb55e99e170b1a
Riccardo Schirone <sirmy15@gmail.com>
2018-07-07 23:55:11 +0200

    travis: pass SAN options

--- travis-extract-var.sh
+++ travis-extract-var.sh
@@ -10,7 +10,7 @@ print_var()
 }
 
 env -0 | while IFS='=' read -r -d '' n v; do
-    if [[ "${n}" =~ ^TRAVIS_* || "${n}" =~ ^R2* || "${n}" =~ *SAN* ]]; then
+    if [[ "${n}" =~ ^TRAVIS_* || "${n}" =~ ^R2* || "${n}" =~ SAN_* ]]; then
 	print_var "${n}" "${v}"
     fi
 done
@@ -21,4 +21,3 @@ print_var CXX "${CXX}"
 print_var CFLAGS "${CFLAGS}"
 print_var LDFLAGS "${LDFLAGS}"
 print_var CXXFLAGS "${CXXFLAGS}"
-
