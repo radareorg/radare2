020a58803224b7e688ffc4a19254265319fc81a7
pancake <pancake@nopcode.org>
2018-07-16 14:47:44 +0200

    Fix #10733 - Fix the asan-fuzz-crash in the MZ parser

--- libr/bin/format/mz/mz.c
+++ libr/bin/format/mz/mz.c
@@ -36,7 +36,7 @@ static int cmp_segs(const void *a, const void *b) {
 
 // This function reads from the file buffer,
 // thus using endian-agnostic functions
-void trv_segs (const void *seg, const void *segs) {
+static void trv_segs (const void *seg, const void *segs) {
 	const ut8 * const mseg = (const ut8 * const)seg;
 	ut16 ** const msegs = (ut16 **)segs;
 	if (mseg && msegs && *msegs) {
@@ -72,7 +72,6 @@ struct r_bin_mz_segment_t * r_bin_mz_get_segments(const struct r_bin_mz_obj_t *b
 #if 1
 	struct btree_node *tree;
 	struct r_bin_mz_segment_t *ret;
-	ut16 *curr_seg;
 	int i, num_segs;
 	const ut16 first_segment = 0;
 	const ut16 stack_segment = bin->dos_header->ss;
@@ -81,14 +80,16 @@ struct r_bin_mz_segment_t * r_bin_mz_get_segments(const struct r_bin_mz_obj_t *b
 	const ut64 last_parag = ((bin->dos_file_size + 0xF) >> 4) - \
 		bin->dos_header->header_paragraphs;
 
+
 	btree_init (&tree);
 	for (i = 0; i < num_relocs; i++) {
 		ut64 paddr = r_bin_mz_seg_to_paddr (bin, relocs[i].segment) + relocs[i].offset;
 		if ((paddr + 2) < bin->dos_file_size) {
-			ut8 bb[32] = {0};
-			r_buf_read_at (bin->b, paddr, bb, sizeof (bb));
+			int left = 0;
+			ut8 *bb = r_buf_get_at (bin->b, paddr, &left);
 			/* Add segment only if it's located inside dos executable data */
-			if (r_read_le16 (bb) <= last_parag) {
+			if (left > 4 && r_read_le16 (bb) <= last_parag) {
+				int le = r_read_le16 (bb);
 				btree_add (&tree, bb, cmp_segs);
 			} else {
 				num_relocs = i;
@@ -98,12 +99,9 @@ struct r_bin_mz_segment_t * r_bin_mz_get_segments(const struct r_bin_mz_obj_t *b
 	}
 
 	/* Add segment address of first segment to make sure that it will be
-	added. If relocations empty or there isn't first segment in relocations.)
-	*/
+	added. If relocations empty or there isn't first segment in relocations.) */
 	btree_add (&tree, (void *)&first_segment, cmp_segs);
-	/* Add segment address of stack segment if it's resides inside dos
-	executable.
-	*/
+	/* Add segment address of stack segment if it's resides inside dos executable. */
 	if (r_bin_mz_seg_to_paddr (bin, stack_segment) < bin->dos_file_size) {
 		btree_add (&tree, (void *)&stack_segment, cmp_segs);
 	}
@@ -112,13 +110,13 @@ struct r_bin_mz_segment_t * r_bin_mz_get_segments(const struct r_bin_mz_obj_t *b
 		btree_cleartree (tree, NULL);
 		return NULL;
 	}
-	ut16 *segments = calloc (1 + num_relocs, sizeof (*segments));
+	ut16 *segments = calloc (2 + num_relocs, sizeof (*segments));
 	if (!segments) {
 		eprintf ("Error: calloc (segments)\n");
 		btree_cleartree (tree, NULL);
 		return NULL;
 	}
-	curr_seg = segments;
+	ut16 *curr_seg = segments;
 	btree_traverse (tree, 0, &curr_seg, trv_segs);
 	num_segs = curr_seg - segments;
 	ret = calloc (num_segs + 1, sizeof (struct r_bin_mz_segment_t));
