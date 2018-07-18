455c8c91834f310bdd7feb50cff5f3dec6d5a1ad
Iru Cai <mytbk920423@gmail.com>
2018-07-18 21:00:27 +0800

    asm_x86_nz: fix "out dx, *"

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -2036,16 +2036,16 @@ static int opout(RAsm *a, ut8 *data, const Opcode *op) {
 	st32 immediate = 0;
 	if (op->operands[0].reg == X86R_DX) {
 		if (op->operands[1].reg == X86R_AL && op->operands[1].type & OT_BYTE) {
-			data[l++] = 0xec;
+			data[l++] = 0xee;
 			return l;
 		}
 		if (op->operands[1].reg == X86R_AX && op->operands[1].type & OT_WORD) {
 			data[l++] = 0x66;
-			data[l++] = 0xed;
+			data[l++] = 0xef;
 			return l;
 		}
 		if (op->operands[1].reg == X86R_EAX && op->operands[1].type & OT_DWORD) {
-			data[l++] = 0xed;
+			data[l++] = 0xef;
 			return l;
 		}
 	} else if (op->operands[0].type & OT_CONSTANT) {
