8a471bbca533ff34da301472d1ba09d0b466abd7
cyanpencil <lucadb96@gmail.com>
2018-07-18 11:48:29 +0200

    Fixed leak in r_anal_var_all_list()

--- libr/anal/var.c
+++ libr/anal/var.c
@@ -793,9 +793,15 @@ R_API RList *r_anal_var_all_list(RAnal *anal, RAnalFunction *fcn) {
 	if (!list) {
 		return NULL;
 	}
-	r_list_join (list, r_anal_var_list (anal, fcn, R_ANAL_VAR_KIND_REG));
-	r_list_join (list, r_anal_var_list (anal, fcn, R_ANAL_VAR_KIND_BPV));
-	r_list_join (list, r_anal_var_list (anal, fcn, R_ANAL_VAR_KIND_SPV));
+	RList *reg_vars = r_anal_var_list (anal, fcn, R_ANAL_VAR_KIND_REG);
+	RList *bpv_vars = r_anal_var_list (anal, fcn, R_ANAL_VAR_KIND_BPV);
+	RList *spv_vars = r_anal_var_list (anal, fcn, R_ANAL_VAR_KIND_SPV);
+	r_list_join (list, reg_vars);
+	r_list_join (list, bpv_vars);
+	r_list_join (list, spv_vars);
+	r_list_free (reg_vars);
+	r_list_free (bpv_vars);
+	r_list_free (spv_vars);
 	return list;
 }
 
