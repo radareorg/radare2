943ebfebd5c37113ed404323bf9c099d21a7f2d2
Khairul Kasmiran <kazarmy@gmail.com>
2018-07-24 22:53:02 +0800

    Fixed typo in rabin2 usage help

--- binr/rabin2/rabin2.c
+++ binr/rabin2/rabin2.c
@@ -27,7 +27,7 @@ static RLib *l;
 static int rabin_show_help(int v) {
 	printf ("Usage: rabin2 [-AcdeEghHiIjlLMqrRsSUvVxzZ] [-@ at] [-a arch] [-b bits] [-B addr]\n"
 		"              [-C F:C:D] [-f str] [-m addr] [-n str] [-N m:M] [-P[-P] pdb]\n"
-		"              [-o str] [-O str] [-k query] [-D lang symname] | file\n");
+		"              [-o str] [-O str] [-k query] [-D lang symname] file\n");
 	if (v) {
 		printf (
 		" -@ [addr]       show section, symbol or import at addr\n"
