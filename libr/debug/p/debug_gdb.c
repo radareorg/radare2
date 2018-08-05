38d915ada56546e9d3dc5ee1c0605b618bf7aac0
AsFaBw <thomasdetert@web.de>
2018-08-05 12:42:36 +0100

    fix-10811

--- libr/debug/p/debug_gdb.c
+++ libr/debug/p/debug_gdb.c
@@ -976,17 +976,43 @@ static int r_debug_gdb_breakpoint (RBreakpoint *bp, RBreakpointItem *b, bool set
 	if (!b) {
 		return false;
 	}
-
 	bpsize = b->size;
-	// TODO handle rwx and conditions
-	if (set)
-		ret = b->hw?
-			gdbr_set_hwbp (desc, b->addr, "", bpsize):
-			gdbr_set_bp (desc, b->addr, "", bpsize);
-	else
-		ret = b->hw?
-			gdbr_remove_hwbp (desc, b->addr, bpsize):
-			gdbr_remove_bp (desc, b->addr, bpsize);
+        // TODO handle conditions
+	switch (b->rwx){
+	case R_BP_PROT_EXEC : {
+		if (set)
+			ret = b->hw?
+					gdbr_set_hwbp (desc, b->addr, "", bpsize):
+					gdbr_set_bp (desc, b->addr, "", bpsize);
+		else
+			ret = b->hw?
+					gdbr_remove_hwbp (desc, b->addr, bpsize):
+					gdbr_remove_bp (desc, b->addr, bpsize);
+		break;
+	}
+	// TODO handle size (area of watch in upper layer and then bpsize. For the moment watches are set on exact on byte
+	case R_BP_PROT_WRITE : {
+		if (set)
+			gdbr_set_hww (desc, b->addr, "", 1);
+		else
+			gdbr_remove_hwbp (desc, b->addr, 1);
+		break;
+	}
+	case R_BP_PROT_READ : {
+		if (set)
+			gdbr_set_hwr (desc, b->addr, "", 1);
+		else
+			gdbr_remove_hwr (desc, b->addr, 1);
+		break;
+	}
+	case R_BP_PROT_ACCESS : {
+		if (set)
+			gdbr_set_hwa (desc, b->addr, "", 1);
+		else
+			gdbr_remove_hwa (desc, b->addr, 1);            
+		break;
+	}
+	}
 	return !ret;
 }
 
