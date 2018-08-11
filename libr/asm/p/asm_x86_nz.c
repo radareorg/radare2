f4d63d96333af1ee891748b2ffcd92f4fcc98679
pancake <pancake@nopcode.org>
2018-08-11 21:21:53 +0200

    Fix #11021 - Fail when trying to use invalid register with XOR on x86

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -356,9 +356,7 @@ static int process_1byte_op(RAsm *a, ut8 *data, const Opcode *op, int op1) {
 		}
 	}
 
-	if (a->bits == 64 &&
-		((op->operands[0].type & OT_QWORD) |
-		 (op->operands[1].type & OT_QWORD))) {
+	if (a->bits == 64 && ((op->operands[0].type & OT_QWORD) | (op->operands[1].type & OT_QWORD))) {
 		if (op->operands[0].extended) {
 				rex = 1;
 		}
@@ -584,6 +582,12 @@ static int opxor(RAsm *a, ut8 * data, const Opcode *op) {
 	if (op->operands_count < 2) {
 		return -1;
 	}
+	if (op->operands[0].type == 0x80 && op->operands[0].reg == X86R_UNDEFINED) {
+		return -1;
+	}
+	if (op->operands[1].type == 0x80 && op->operands[0].reg == X86R_UNDEFINED) {
+		return -1;
+	}
 	if (op->operands[1].type & OT_CONSTANT) {
 		if (op->operands[0].type & OT_GPREG &&
 		    op->operands[0].type & OT_WORD) {
@@ -596,8 +600,7 @@ static int opxor(RAsm *a, ut8 * data, const Opcode *op) {
 	return process_1byte_op (a, data, op, 0x30);
 }
 
-static int opnot(RAsm *a, ut8 * data, const Opcode *op)
-{
+static int opnot(RAsm *a, ut8 * data, const Opcode *op) {
 	int l = 0;
 
 	if(op->operands[0].reg == X86R_UNDEFINED)  {
