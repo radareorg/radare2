c6f0342a411289d65b7d009ac52d4e73619059a4
David CARLIER <devnexen@gmail.com>
2018-08-17 11:25:42 +0100

    Java plugin: add human readable version. (#11118)
    When possible adding this to the lang value
    of the javac compiler used with.

--- shlr/java/class.c
+++ shlr/java/class.c
@@ -3001,6 +3001,23 @@ R_API RList *r_bin_java_get_symbols(RBinJavaObj *bin) {
 		}
 	}
 	bin->lang = "java";
+	if (bin->cf.major[1] >= 46) {
+		switch (bin->cf.major[1]) {
+			static char lang[10];
+			int langid;
+			case 46:
+			case 47:
+			case 48:
+				langid = 2 + (bin->cf.major[1] - 46);
+				snprintf (lang, sizeof(lang) - 1, "java 1.%d", langid);
+				bin->lang = lang;
+				break;
+			default:
+				langid = 5 + (bin->cf.major[1] - 49);
+				snprintf (lang, sizeof(lang) - 1, "java %d", langid);
+				bin->lang = lang;
+		}
+	}
 	imports = r_bin_java_get_imports (bin);
 	r_list_foreach (imports, iter, imp) {
 		sym = R_NEW0 (RBinSymbol);
