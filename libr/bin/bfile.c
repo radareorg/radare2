abae652ce696165c4e6d2453f7f196e534fc497e
pancake <pancake@nopcode.org>
2018-08-19 04:16:04 +0200

    Fix another memleak in RBin

--- libr/bin/bfile.c
+++ libr/bin/bfile.c
@@ -788,9 +788,8 @@ R_API void r_bin_file_free(void /*RBinFile*/ *bf_) {
 	// format data first
 	if (plugin && plugin->destroy) {
 		plugin->destroy (a);
-	} else {
-		r_buf_free (a->buf);
 	}
+	r_buf_free (a->buf);
 	a->buf = NULL;
 	if (a->curxtr && a->curxtr->destroy && a->xtr_obj) {
 		a->curxtr->free_xtr ((void *)(a->xtr_obj));
