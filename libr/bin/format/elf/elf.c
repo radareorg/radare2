3fc726311a1b1841306ebae4a0168ea1b9dd5329
Khairul Kasmiran <kazarmy@gmail.com>
2018-08-26 14:40:34 +0800

    Fix iM paddr when it's from the main symbol

--- libr/bin/format/elf/elf.c
+++ libr/bin/format/elf/elf.c
@@ -1675,8 +1675,7 @@ static ut64 getmainsymbol(ELFOBJ *bin) {
 	}
 	for (i = 0; !symbol[i].last; i++) {
 		if (!strcmp (symbol[i].name, "main")) {
-			ut64 paddr = symbol[i].offset;
-			return Elf_(r_bin_elf_p2v) (bin, paddr);
+			return symbol[i].offset;
 		}
 	}
 	return UT64_MAX;
