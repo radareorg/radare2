4be03703ec3ef03449cf939902c3022aa6cf4af7
pancake <pancake@nopcode.org>
2018-08-30 22:59:26 +0200

    Kill Sj and Sd commands

--- libr/core/cmd_section.c
+++ libr/core/cmd_section.c
@@ -16,9 +16,7 @@ static const char *help_msg_S[] = {
 	"S-.","","remove section at core->offset (can be changed with @)",
 	"S=","","list sections (ascii-art bars) (io.va to display paddr or vaddr)",
 	"Sa","[-] [A] [B] [[off]]","Specify arch and bits for given section",
-	"Sd[a]"," [file]","dump current (all) section to a file (see dmd)",
 	"Sf"," [baddr]","Alias for S 0 0 $s $s foo rwx",
-	"Sj","","list sections in JSON (alias for iSj)",
 	"Sl"," [file]","load contents of file into current section (see dml)",
 	"Sr"," [name]","rename section on current seek",
 	"SR", "[?]", "Remap sections with different mode of operation", 
@@ -347,9 +345,6 @@ static int cmd_section(void *data, const char *input) {
 			r_core_cmd0 (core, "S 0 0 $s $s foo rwx");
 		}
 		break;
-	case 'j': // "Sj"
-		r_core_cmd0 (core, "iSj");
-		break;
 	case 'a': // "Sa"
 		switch (input[1]) {
 		case '\0':
@@ -422,30 +417,6 @@ static int cmd_section(void *data, const char *input) {
 			}
 		} else {
 			r_core_cmd_help (core, help_msg_Sr);
-		}
-		break;
-	case 'd': // "Sd"
-		{
-		char *file = NULL;
-		int len = 128;
-		switch (input[1]) {
-		case 0:
-			(void) dumpSectionToDisk (core, NULL);
-			break;
-		case ' ': // "Sd "
-			if (input[2]) {
-				file = (char *)calloc (len, sizeof (char));
-				if (file) {
-					snprintf (file, len, "%s", input + 2);
-				}
-			}
-			(void) dumpSectionToDisk (core, file);
-			free (file);
-			break;
-		case 'a': // "Sda"
-			(void)dumpSectionsToDisk (core);
-			break;
-		}
 		}
 		break;
 	case 'l': // "Sl"
