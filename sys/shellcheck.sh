ad02255f581753c5cd84915d81d7f12c819759ba
pancake <pancake@nopcode.org>
2018-08-31 17:58:37 +0200

    Fix shellcheck.sh

--- sys/shellcheck.sh
+++ sys/shellcheck.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 checkshellscript() {
 	filelist="$1"
 	checkfun="$2"
@@ -13,7 +12,7 @@ checkshellscript() {
 }
 
 echo "Find all shellscripts, caching $SCRIPTS"
-SCRIPTS=$(find . \! -path '/.git' -print0  | xargs -0 file | grep "POSIX shell script" | cut -d: -f1)
+SCRIPTS=$(git grep '/bin/sh' | cut -d: -f1)
 
 checkshellscript "./sys/scripts.list" "shellcheck --format=gcc"
 checkshellscript "./sys/scripts.list" "checkbashisms"
