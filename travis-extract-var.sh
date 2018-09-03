d47d6f83693dcfef9bdb56d8b7eb892913b3993d
Riccardo Schirone <sirmy15@gmail.com>
2018-09-03 16:17:46 +0200

    enable codecov.io

--- travis-extract-var.sh
+++ travis-extract-var.sh
@@ -5,13 +5,13 @@ print_var()
     VAR_NAME=$1
     VAR_VALUE=$2
     if [ "${VAR_VALUE}" != "" ] ; then
-	printf "export %s=%q\n" "${VAR_NAME}" "${VAR_VALUE}"
+        printf "export %s=%q\n" "${VAR_NAME}" "${VAR_VALUE}"
     fi
 }
 
 env -0 | while IFS='=' read -r -d '' n v; do
     if [[ "${n}" =~ ^TRAVIS_* || "${n}" =~ ^R2* || "${n}" =~ SAN_* ]]; then
-	print_var "${n}" "${v}"
+        print_var "${n}" "${v}"
     fi
 done
 
@@ -22,3 +22,5 @@ print_var CFLAGS "${CFLAGS}"
 print_var LDFLAGS "${LDFLAGS}"
 print_var CXXFLAGS "${CXXFLAGS}"
 print_var INSTALL_SYSTEM "${INSTALL_SYSTEM}"
+print_var MESON_OPTIONS "${MESON_OPTIONS}"
+print_var COVERAGE "${COVERAGE}"
