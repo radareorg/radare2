57924911e212c4457c9912b3ffe51c676eb2d0ac
Riccardo Schirone <sirmy15@gmail.com>
2018-09-05 21:03:56 +0200

    anal_x86_cs: just use one expression for the PUSH x86 operation

--- libr/anal/p/anal_x86_cs.c
+++ libr/anal/p/anal_x86_cs.c
@@ -930,13 +930,8 @@ static void anop_esil(RAnal *a, RAnalOp *op, ut64 addr, const ut8 *buf, int len,
 	case X86_INS_PUSH:
 		{
 			dst = getarg (&gop, 0, 0, NULL, DST_AR);
-			if (!strcmp (dst, sp)) {
-				esilprintf (op, "%s,%s,&,%d,%s,-=,1,PICK,%s,=[%d],POP",
-					sp, sp, rs, sp, sp, rs);
-			} else {
-				esilprintf (op, "%s,%d,%s,-=,%s,=[%d]",
-					dst ? dst : "eax", rs, sp, sp, rs);
-			}
+			esilprintf (op, "%s,%d,%s,-,=[%d],%d,%s,-=",
+				dst ? dst : "eax", rs, sp, rs, rs, sp);
 		}
 		break;
 	case X86_INS_PUSHF:
