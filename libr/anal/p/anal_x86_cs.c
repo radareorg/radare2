283ab4b1be7b5e3a4568c44af0d4483b86697b46
Riccardo Schirone <sirmy15@gmail.com>
2018-09-05 18:59:38 +0200

    anal_x86: keep same syntax and POP value out of esil stack in push sp
    NOTE: The other syntax was causing issues in afta, not sure why

--- libr/anal/p/anal_x86_cs.c
+++ libr/anal/p/anal_x86_cs.c
@@ -931,11 +931,11 @@ static void anop_esil(RAnal *a, RAnalOp *op, ut64 addr, const ut8 *buf, int len,
 		{
 			dst = getarg (&gop, 0, 0, NULL, DST_AR);
 			if (!strcmp (dst, sp)) {
-				esilprintf (op, "%s,%s,&,%d,%s,-=,1,PICK,%s,=[%d]",
-					sp, sp, rs, sp, sp, rs);				
+				esilprintf (op, "%s,%s,&,%d,%s,-=,1,PICK,%s,=[%d],POP",
+					sp, sp, rs, sp, sp, rs);
 			} else {
-				esilprintf (op, "%d,%s,-=,%s,%s,=[%d]",
-					rs, sp, dst?dst:"eax", sp, rs);				
+				esilprintf (op, "%s,%d,%s,-=,%s,=[%d]",
+					dst ? dst : "eax", rs, sp, sp, rs);
 			}
 		}
 		break;
