6ee97b9516de3620bb9126e80db820cf6c0c9215
wargio <wargio@libero.it>
2018-09-08 16:44:10 +0200

    fixed name of dyn lib

--- libr/anal/p/mcore.mk
+++ libr/anal/p/mcore.mk
@@ -7,5 +7,5 @@ TARGET_MCORE=anal_mcore.${EXT_SO}
 ALL_TARGETS+=${TARGET_MCORE}
 
 ${TARGET_MCORE}: ${OBJ_MCORE}
-	${CC} ${CFLAGS} $(call libname,anal_sysz) $(CS_LDFLAGS) \
+	${CC} ${CFLAGS} $(call libname,anal_mcore) $(CS_LDFLAGS) \
 		-o anal_mcore.${EXT_SO} ${OBJ_MCORE}
