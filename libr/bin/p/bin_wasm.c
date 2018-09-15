radareorg/radare2

commit 7e083b57fb08fc7775e5f367e8257ec76f34e9bd

Fix last coverities (2 memleaks)

libr/bin/p/bin_wasm.c
@@ -72,6 +72,7 @@ static RList *entries(RBinFile *bf) {
 		}
 		if (!addr) {
 			r_list_free (ret);
+			r_list_free (codes);
 			return NULL;
 		}
 	}

libr/core/rtr.c
@@ -1,4 +1,4 @@
-/* radare - Copyright 2009-2017 - pancake, nibble */
+/* radare - Copyright 2009-2018 - pancake, nibble */
 
 #include "r_core.h"
 #include "r_socket.h"
@@ -508,6 +508,7 @@ static int r_core_rtr_http_run(RCore *core, int launch, int browse, const char *
 
 	newblk = malloc (core->blocksize);
 	if (!newblk) {
+		r_socket_free (s);
 		return 1;
 	}
 	memcpy (newblk, core->block, core->blocksize);

commit e238026086c467fc5e9980414a50ff1b0c815421

Fix #11399  Use-after-free and a memory leak in handling of WASM binaries  (#11533)
* Fix issue #11399: use-after-free in symbols()

Commit 7e083b57f introduced the issue #11399. The list referenced by
`codes` in entries(), is the same list that bf->g_codes is pointing at.
By freeing it, we introduce a use-after-free condition in a later call
to symbols(), where we try to iterate over the list that bf->g_codes
was supposed to be referencing.

* Fix memleak during loading of WASM binaries

A memory leak is reported by ASAN when handling WASM binaries. It is
caused by multiple allocations of RBinInfo structure. First, RBinInfo
is allocated within a call to size() from r_bin_object_set_items(). Then
there is another, explicit allocation of an RBinInfo structure through
a call to the info() callback of the WASM RBinPlugin. This causes loss
of reference to the initial structure, and subsequently a leak.

There are no apparent uses of RBinInfo structure inbetween these two
points, and the size() result is in no way dependent on this structure,
therefore I resolved the memory leak issue by removing the allocation
from within the size() function.

libr/bin/p/bin_wasm.c
@@ -72,7 +72,6 @@ static RList *entries(RBinFile *bf) {
 		}
 		if (!addr) {
 			r_list_free (ret);
-			r_list_free (codes);
 			return NULL;
 		}
 	}
@@ -270,10 +269,7 @@ static RBinInfo *info(RBinFile *bf) {
 }
 
 static ut64 size(RBinFile *bf) {
-	if (!bf->o->info) {
-		bf->o->info = info (bf);
-	}
-	if (!bf->o->info) {
+	if (!bf || !bf->buf) {
 		return 0;
 	}
 	return bf->buf->length;