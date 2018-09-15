radareorg/radare2

commit 7e083b57fb08fc7775e5f367e8257ec76f34e9bd

Fix last coverities (2 memleaks)

libr/bin/p/bin_wasm.c
@@ -72,6 +72,7 @@ static RList *entries(RBinFile *bf) {
 		}
 		if (!addr) {
 			r_list_free (ret);
+			r_list_free (codes);
 			return NULL;
 		}
 	}

libr/core/rtr.c
@@ -1,4 +1,4 @@
-/* radare - Copyright 2009-2017 - pancake, nibble */
+/* radare - Copyright 2009-2018 - pancake, nibble */
 
 #include "r_core.h"
 #include "r_socket.h"
@@ -508,6 +508,7 @@ static int r_core_rtr_http_run(RCore *core, int launch, int browse, const char *
 
 	newblk = malloc (core->blocksize);
 	if (!newblk) {
+		r_socket_free (s);
 		return 1;
 	}
 	memcpy (newblk, core->block, core->blocksize);

commit c826a7b14dabd8dd91bfe51817031ebfc4938d94

Fix issue #11399: use-after-free in symbols()
Commit 7e083b57f introduced the issue #11399. The list referenced by
`codes` in entries(), is the same list that bf->g_codes is pointing at.
By freeing it, we introduce a use-after-free condition in a later call
to symbols(), where we try to iterate over the list that bf->g_codes
was supposed to be referencing.

libr/bin/p/bin_wasm.c
@@ -72,7 +72,6 @@ static RList *entries(RBinFile *bf) {
 		}
 		if (!addr) {
 			r_list_free (ret);
-			r_list_free (codes);
 			return NULL;
 		}
 	}