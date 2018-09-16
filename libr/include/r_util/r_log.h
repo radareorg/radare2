ab4dbd509f15ada62dbb361da177ebd37d0e0ea7
Riccardo Schirone <ret2libc@users.noreply.github.com>
2018-09-16 23:24:57 +0200

    r_assert: create functions to handle preconditions (#11286)

--- libr/include/r_util/r_log.h
+++ libr/include/r_util/r_log.h
@@ -5,10 +5,23 @@
 extern "C" {
 #endif
 
+#include <stdarg.h>
+
+typedef enum r_log_level {
+	R_LOG_DEBUG = 0,
+	R_LOG_INFO,
+	R_LOG_WARNING,
+	R_LOG_ERROR,
+	R_LOG_CRITICAL,
+	R_LOG_MAX_VALUE
+} RLogLevel;
+
+R_API void r_log_file(const char *str);
 R_API void r_log_msg(const char *str);
 R_API void r_log_error(const char *str);
-R_API void r_log_file(const char *str);
 R_API void r_log_progress(const char *str, int percent);
+R_API void r_vlogf(RLogLevel level, const char *fmt, va_list ap);
+R_API void r_logf(RLogLevel level, const char *fmt, ...);
 
 #ifdef __cplusplus
 }
