3841d41505b5d263091fdd94c7c7d02d1697f657
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2018-09-18 17:43:08 +0800

    r_bin_elf_p2v/v2p_new: return UT64_MAX on error + is: show ---------- for paddr if symbol's value is not vaddr (#11513)

--- libr/bin/format/elf/elf.h
+++ libr/bin/format/elf/elf.h
@@ -39,6 +39,7 @@ typedef struct r_bin_elf_symbol_t {
 	int last;
 	bool in_shdr;
 	bool is_sht_null;
+	bool is_value;  /* when true, sym has no paddr, only vaddr */
 } RBinElfSymbol;
 
 typedef struct r_bin_elf_reloc_t {
@@ -123,6 +124,8 @@ ut64 Elf_(r_bin_elf_get_section_offset)(struct Elf_(r_bin_elf_obj_t) *bin, const
 ut64 Elf_(r_bin_elf_get_baddr)(struct Elf_(r_bin_elf_obj_t) *bin);
 ut64 Elf_(r_bin_elf_p2v)(struct Elf_(r_bin_elf_obj_t) *bin, ut64 paddr);
 ut64 Elf_(r_bin_elf_v2p)(struct Elf_(r_bin_elf_obj_t) *bin, ut64 vaddr);
+ut64 Elf_(r_bin_elf_p2v_new)(struct Elf_(r_bin_elf_obj_t) *bin, ut64 paddr);
+ut64 Elf_(r_bin_elf_v2p_new)(struct Elf_(r_bin_elf_obj_t) *bin, ut64 vaddr);
 ut64 Elf_(r_bin_elf_get_boffset)(struct Elf_(r_bin_elf_obj_t) *bin);
 ut64 Elf_(r_bin_elf_get_entry_offset)(struct Elf_(r_bin_elf_obj_t) *bin);
 ut64 Elf_(r_bin_elf_get_main_offset)(struct Elf_(r_bin_elf_obj_t) *bin);
