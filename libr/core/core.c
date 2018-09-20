9160c00f2c0eb9f64958b48f6ce63eacbffc9928
Dimitris Karagkasidis <t.pagef.lt@gmail.com>
2018-09-20 11:18:27 +0000

    Fix minor memleak in cb_pager()
    The heap space allocated by core->cons->pager within cb_pager() is not
    properly released, resulting in ASAN complaining about the memory leak.

--- libr/core/core.c
+++ libr/core/core.c
@@ -2396,7 +2396,7 @@ R_API RCore *r_core_fini(RCore *c) {
 	R_FREE (c->cmdlog);
 	r_th_lock_free (c->lock);
 	R_FREE (c->lastsearch);
-	c->cons->pager = NULL;
+	free (c->cons->pager);
 	free (c->panels_tmpcfg);
 	free (c->cmdqueue);
 	free (c->lastcmd);
