6c146ebf90ca56186bf13c2420098a827541ef51
Paul I <pelijah@users.noreply.github.com>
2018-09-23 13:13:34 +0300

    Fix GNU-based RAsm plugins to use RStrBuf API properly (#11625)

--- libr/asm/p/asm_ppc_gnu.c
+++ libr/asm/p/asm_ppc_gnu.c
@@ -13,7 +13,7 @@
 
 
 static unsigned long Offset = 0;
-static char *buf_global = NULL;
+static RStrBuf *buf_global = NULL;
 static unsigned char bytes[4];
 
 static int ppc_buffer_read_memory (bfd_vma memaddr, bfd_byte *myaddr, ut32 length, struct disassemble_info *info) {
@@ -29,44 +29,15 @@ static void memory_error_func(int status, bfd_vma memaddr, struct disassemble_in
 	//--
 }
 
-static void print_address(bfd_vma address, struct disassemble_info *info) {
-	char tmp[32];
-	if (buf_global) {
-		sprintf (tmp, "0x%08"PFMT64x"", (ut64)address);
-		strcat (buf_global, tmp);
-	}
-}
-
-static int buf_fprintf(void *stream, const char *format, ...) {
-	int flen, glen;
-	va_list ap;
-	char *tmp;
-	if (!buf_global) {
-		return 0;
-	}
-	va_start (ap, format);
-	flen = strlen (format);
-	glen = strlen (buf_global);
-	tmp = malloc (flen + glen + 2);
-	if (!tmp) {
-		return 0;
-	}
-	memcpy (tmp, buf_global, glen);
-	memcpy (tmp+glen, format, flen);
-	tmp[flen+glen] = 0;
-// XXX: overflow here?
-	vsprintf (buf_global, tmp, ap);
-	va_end (ap);
-	free (tmp);
-	return 0;
-}
+DECLARE_GENERIC_PRINT_ADDRESS_FUNC()
+DECLARE_GENERIC_FPRINTF_FUNC()
 
 static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 	struct disassemble_info disasm_obj;
 	if (len<4) {
 		return -1;
 	}
-	buf_global = r_strbuf_get (&op->buf_asm);
+	buf_global = &op->buf_asm;
 	Offset = a->pc;
 	memcpy (bytes, buf, 4); // TODO handle thumb
 
@@ -77,9 +48,9 @@ static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 	disasm_obj.read_memory_func = &ppc_buffer_read_memory;
 	disasm_obj.symbol_at_address_func = &symbol_at_address;
 	disasm_obj.memory_error_func = &memory_error_func;
-	disasm_obj.print_address_func = &print_address;
+	disasm_obj.print_address_func = &generic_print_address_func;
 	disasm_obj.endian = !a->big_endian;
-	disasm_obj.fprintf_func = &buf_fprintf;
+	disasm_obj.fprintf_func = &generic_fprintf_func;
 	disasm_obj.stream = stdout;
 	if (a->big_endian) {
 		op->size = print_insn_big_powerpc ((bfd_vma)Offset, &disasm_obj);
