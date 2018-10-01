d0d8b00e464694bc3aeb11cd9f742a075c7fda98
David Carlier <devnexen@gmail.com>
2018-10-01 21:24:08 +0100

    few tweaks

--- shlr/java/class.c
+++ shlr/java/class.c
@@ -3598,10 +3598,7 @@ R_API ut64 r_bin_java_signature_attr_calc_size(RBinJavaAttrInfo *attr) {
 R_API RBinJavaAttrInfo *r_bin_java_enclosing_methods_attr_new(ut8 *buffer, ut64 sz, ut64 buf_offset) {
 	ut64 offset = 6;
 	RBinJavaAttrInfo *attr = r_bin_java_default_attr_new (buffer, sz, buf_offset);
-	if (!attr) {
-		return NULL;
-	}
-	if (sz < 10) {
+	if (!attr || sz < 10) {
 		free (attr);
 		return NULL;
 	}
