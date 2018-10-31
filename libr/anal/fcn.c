d2fe95113098cf331dc5a1d181da8b2e349b07d7
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2018-10-31 18:47:17 +0800

    anal.jmpmid: Continue analysis after jump to middle of instruction ##anal

--- libr/anal/fcn.c
+++ libr/anal/fcn.c
@@ -328,6 +328,7 @@ R_API void r_anal_fcn_free(void *_fcn) {
 	free (fcn);
 }
 
+// TODO: overlapping basic blocks can cause problems here
 static RAnalBlock *bbget(RAnalFunction *fcn, ut64 addr) {
 	RListIter *iter;
 	RAnalBlock *bb;
@@ -883,6 +884,7 @@ static int fcn_recurse(RAnal *anal, RAnalFunction *fcn, ut64 addr, ut8 *buf, ut6
 		0
 	};
 	char tmp_buf[MAX_FLG_NAME_SIZE + 5] = "skip";
+	bool x86 = anal->cur->arch && !strcmp (anal->cur->arch, "x86");
 
 	if (r_cons_is_breaked ()) {
 		return R_ANAL_RET_END;
@@ -910,13 +912,15 @@ static int fcn_recurse(RAnal *anal, RAnalFunction *fcn, ut64 addr, ut8 *buf, ut6
 	if (r_anal_get_fcn_at (anal, addr, 0)) {
 		return R_ANAL_RET_ERROR; // MUST BE NOT FOUND
 	}
-	bb = bbget (fcn, addr);
+	bb = bbget (fcn, addr); // TODO: bbget_all()
 	if (bb) {
-		r_anal_fcn_split_bb (anal, fcn, bb, addr);
-		if (anal->opt.recont) {
-			return R_ANAL_RET_END;
+		if (!anal->opt.jmpmid || !x86 || r_anal_bb_op_starts_at (bb, addr)) {
+			r_anal_fcn_split_bb (anal, fcn, bb, addr);
+			if (anal->opt.recont) {
+				return R_ANAL_RET_END;
+			}
+			return R_ANAL_RET_ERROR; // MUST BE NOT DUP
 		}
-		return R_ANAL_RET_ERROR; // MUST BE NOT DUP
 	}
 
 	bb = appendBasicBlock (anal, fcn, addr);
@@ -971,8 +975,9 @@ repeat:
 			r_anal_hint_set_bits (anal, op.jump, op.hint.new_bits);
 		}
 		if (idx > 0 && !overlapped) {
-			bbg = bbget (fcn, addr + idx);
-			if (bbg && bbg != bb) {
+			bbg = bbget (fcn, addr + idx); // TODO: bbget_all()
+			if (bbg && bbg != bb
+			    && (!anal->opt.jmpmid || !x86 || r_anal_bb_op_starts_at (bbg, addr + idx))) {
 				bb->jump = addr + idx;
 				overlapped = 1;
 				VERBOSE_ANAL eprintf("Overlapped at 0x%08"PFMT64x "\n", addr + idx);
