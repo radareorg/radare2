a188ba70806906da9a13150292a75aa7db19235a
David Carlier <devnexen@gmail.com>
2018-11-01 19:51:05 +0000

    Following up self:// pluging support on BSD.
    Final nail on the coffin, here now the NetBSD much closer to
    FreeBSD workflow.

--- libr/io/p/io_self.c
+++ libr/io/p/io_self.c
@@ -23,7 +23,7 @@ void macosx_debug_regions (RIO *io, task_t task, mach_vm_address_t address, int 
 #include <sys/sysctl.h>
 #include <sys/user.h>
 #include <libutil.h>
-#elif __OpenBSD__
+#elif __OpenBSD__ || __NetBSD__
 #include <sys/sysctl.h>
 #endif
 #include <errno.h>
@@ -121,7 +121,6 @@ static int update_self_regions(RIO *io, int pid) {
 
 	return true;
 #elif __BSD__
-	// TODO NetBSD should be doable
 	return bsd_proc_vmmaps(io, pid);
 #else
 #ifdef _MSC_VER
@@ -644,9 +643,90 @@ exit:
 
 	return true;
 #elif __NetBSD__
-#warning "Not implemented yet"
-#endif
+	size_t size;
+	bool ret = false;
+	int mib[5] = {
+		CTL_VM, VM_PROC, VM_PROC_MAP, pid, sizeof (struct kinfo_vmentry)
+	};
+	int s = sysctl (mib, 5, NULL, &size, NULL, 0);
+	if (s == -1) {
+		eprintf ("sysctl failed: %s\n", strerror (errno));
+		return false;
+	}
 
+	ut8 *p = malloc (size);
+	if (p) {
+		size = size * 4 / 3;
+		s = sysctl (mib, 5, p, &size, NULL, 0);
+		if (s == -1) {
+			eprintf ("sysctl failed: %s\n", strerror (errno));
+			goto exit;
+		}
+		ut8 *p_start = p;
+		ut8 *p_end = p + size;
+		int n = 0;
+
+		while (p_start < p_end) {
+			struct kinfo_vmentry *entry = (struct kinfo_vmentry *)p_start;
+			size_t sz = sizeof(*entry);
+			if (sz == 0) {
+				break;
+			}
+			p_start += sz;
+			n ++;
+		}
+
+		struct kinfo_vmentry *entries = calloc(n, sizeof(*entries));
+		if (!entries) {
+			eprintf ("entries allocation failed\n");
+			goto exit;
+		}
+
+		p_start = p;
+
+		while (p_start < p_end) {
+			struct kinfo_vmentry *entry = (struct kinfo_vmentry *)p_start;
+			size_t sz = sizeof(*entry);
+			int perm = 0;
+			if (sz == 0) {
+				break;
+			}
+
+			if (entry->kve_protection & KVME_PROT_READ) {
+				perm |= R_PERM_R;
+			}
+			if (entry->kve_protection & KVME_PROT_WRITE) {
+				perm |= R_PERM_W;
+			}
+			if (entry->kve_protection & KVME_PROT_EXEC) {
+				perm |= R_PERM_X;
+			}
+
+			if (entry->kve_path[0] != '\0') {
+				io->cb_printf (" %p - %p (%s)\n",
+					(void *)entry->kve_start,
+					(void *)entry->kve_end,
+					entry->kve_path);
+			}
+
+			self_sections[self_sections_count].from = entry->kve_start;
+			self_sections[self_sections_count].to = entry->kve_end;
+			self_sections[self_sections_count].name = strdup (entry->kve_path);
+			self_sections[self_sections_count].perm = perm;
+			self_sections_count++;
+			p_start += sz;
+		}
+
+		free (entries);
+		ret = true;
+	} else {
+		eprintf ("buffer allocation failed\n");
+	}
+
+exit:
+	free (p);
+	return ret;
+#endif
 }
 #endif
 
