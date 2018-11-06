6821d9c1fc95a9008a4cf1993428e5f7972243d0
Riccardo Schirone <sirmy15@gmail.com>
2018-11-06 21:04:42 +0100

    bin: make r_bin_object_delete_items static

--- libr/bin/obj.c
+++ libr/bin/obj.c
@@ -8,6 +8,44 @@
 	if (binfile->rbin->verbose) \
 	eprintf
 
+static void object_delete_items(RBinObject *o) {
+	ut32 i = 0;
+	r_return_if_fail (o);
+	sdb_free (o->addr2klassmethod);
+	r_list_free (o->entries);
+	r_list_free (o->fields);
+	r_list_free (o->imports);
+	r_list_free (o->libs);
+	r_list_free (o->relocs);
+	r_list_free (o->sections);
+	r_list_free (o->strings);
+	r_list_free (o->symbols);
+	r_list_free (o->classes);
+	r_list_free (o->lines);
+	sdb_free (o->kv);
+	if (o->mem) {
+		o->mem->free = r_bin_mem_free;
+	}
+	r_list_free (o->mem);
+	o->mem = NULL;
+	o->entries = NULL;
+	o->fields = NULL;
+	o->imports = NULL;
+	o->libs = NULL;
+	o->relocs = NULL;
+	o->sections = NULL;
+	o->strings = NULL;
+	o->symbols = NULL;
+	o->classes = NULL;
+	o->lines = NULL;
+	o->info = NULL;
+	o->kv = NULL;
+	for (i = 0; i < R_BIN_SYM_LAST; i++) {
+		free (o->binsym[i]);
+		o->binsym[i] = NULL;
+	}
+}
+
 R_IPI void r_bin_object_free(void /*RBinObject*/ *o_) {
 	RBinObject *o = o_;
 	if (!o) {
@@ -15,7 +53,7 @@ R_IPI void r_bin_object_free(void /*RBinObject*/ *o_) {
 	}
 	free (o->regstate);
 	r_bin_info_free (o->info);
-	r_bin_object_delete_items (o);
+	object_delete_items (o);
 	R_FREE (o);
 }
 
@@ -315,44 +353,6 @@ static void r_bin_mem_free(void *data) {
 		mem->mirrors = NULL;
 	}
 	free (mem);
-}
-
-R_IPI void r_bin_object_delete_items(RBinObject *o) {
-	ut32 i = 0;
-	r_return_if_fail (o);
-	sdb_free (o->addr2klassmethod);
-	r_list_free (o->entries);
-	r_list_free (o->fields);
-	r_list_free (o->imports);
-	r_list_free (o->libs);
-	r_list_free (o->relocs);
-	r_list_free (o->sections);
-	r_list_free (o->strings);
-	r_list_free (o->symbols);
-	r_list_free (o->classes);
-	r_list_free (o->lines);
-	sdb_free (o->kv);
-	if (o->mem) {
-		o->mem->free = r_bin_mem_free;
-	}
-	r_list_free (o->mem);
-	o->mem = NULL;
-	o->entries = NULL;
-	o->fields = NULL;
-	o->imports = NULL;
-	o->libs = NULL;
-	o->relocs = NULL;
-	o->sections = NULL;
-	o->strings = NULL;
-	o->symbols = NULL;
-	o->classes = NULL;
-	o->lines = NULL;
-	o->info = NULL;
-	o->kv = NULL;
-	for (i = 0; i < R_BIN_SYM_LAST; i++) {
-		free (o->binsym[i]);
-		o->binsym[i] = NULL;
-	}
 }
 
 R_IPI RBinObject *r_bin_object_find_by_arch_bits(RBinFile *binfile, const char *arch, int bits, const char *name) {
