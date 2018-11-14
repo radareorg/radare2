3aff4e017f224d69814059295ee05d7e5ddec3ac
pancake <pancake@nopcode.org>
2018-11-14 17:28:00 +0100

    Improve the wasm disassembler

--- libr/anal/p/anal_wasm.c
+++ libr/anal/p/anal_wasm.c
@@ -1,4 +1,4 @@
-/* radare2 - LGPL - Copyright 2017 - xvilka */
+/* radare2 - LGPL - Copyright 2017-2018 - xvilka */
 #include <string.h>
 #include <r_types.h>
 #include <r_lib.h>
@@ -15,8 +15,9 @@ static struct wasm_stack_t {
 	ut64 end;
 	int size;
 } wasm_stack [WASM_STACK_SIZE];
-int wasm_stack_ptr = 0;
-WasmOpCodes op_old = 0;
+
+static int wasm_stack_ptr = 0;
+static WasmOpCodes op_old = 0;
 
 static ut64 get_cf_offset(RAnal *anal, const ut8 *data) {
        char flgname[64] = {0};
@@ -95,140 +96,188 @@ static int wasm_op(RAnal *anal, RAnalOp *op, ut64 addr, const ut8 *data, int len
 
 	if (!strncmp (wop.txt, "invalid", 7)) {
 		op->type = R_ANAL_OP_TYPE_ILL;
+		wasm_stack_ptr = 0;
 		return -1;
 	}
 	if (wasm_stack_ptr >= WASM_STACK_SIZE) {
+		wasm_stack_ptr = 0;
 		op->type = R_ANAL_OP_TYPE_NULL;
 		return -1;
-	} else {
-		switch (wop.op) {
-		/* Calls here are using index instead of address */
-		case WASM_OP_LOOP:
-			//op->type = R_ANAL_OP_TYPE_CJMP;
-			addr2 = find_if_else (addr + op->size, data + op->size, len - op->size, true);
-			if (addr2 != UT64_MAX) {
-				wasm_stack[wasm_stack_ptr].loop = addr;
-				wasm_stack[wasm_stack_ptr].end = addr2;
-				wasm_stack[wasm_stack_ptr].size = wop.len;
-				wasm_stack_ptr++;
-				addr2 = UT64_MAX;
-			}
-			//op->fail = addr + op->size;
-			break;
-		case WASM_OP_BLOCK:
-			addr2 = find_if_else (addr + op->size, data + op->size, len - op->size, true);
-			if (addr2 != UT64_MAX) {
-				wasm_stack[wasm_stack_ptr].loop = UT64_MAX;
-				wasm_stack[wasm_stack_ptr].end = addr2;
-				wasm_stack[wasm_stack_ptr].size = wop.len;
-				wasm_stack_ptr++;
-				addr2 = UT64_MAX;
-			}
-			break;
-		case WASM_OP_IF:
-			op->type = R_ANAL_OP_TYPE_CJMP;
-			op->jump = find_if_else (addr + op->size, data + op->size, len - op->size, false);
-			op->fail = addr + op->size;
-			break;
-		case WASM_OP_ELSE:
-			op->type = R_ANAL_OP_TYPE_JMP;
-			op->jump = find_if_else (addr + op->size, data + op->size, len - op->size, false);
-			break;
-		case WASM_OP_END:
-			if (addr != UT64_MAX) {
-				for (i = 0; i < wasm_stack_ptr; ++i) {
-					if (wasm_stack[i].end == addr && wasm_stack[i].loop != UT64_MAX) {
-						op->type = R_ANAL_OP_TYPE_CJMP;
-						op->jump = wasm_stack[i].loop;
-						op->fail = addr + op->size;
-						break;
-					}
-				}
-			}
-			if (op_old == WASM_OP_CALL || op_old == WASM_OP_CALLINDIRECT || op_old == WASM_OP_RETURN) {
-				op->eob = true;
-				for (i = wasm_stack_ptr - 1; i > 0; --i) {
-					if (addr > wasm_stack[i].loop && addr < wasm_stack[i].end) {
-						op->eob = false;
-						break;
-					}
-				}
-			}
-			break;
-		case WASM_OP_GETLOCAL:
-			op->type = R_ANAL_OP_TYPE_LOAD;
-			break;
-		case WASM_OP_SETLOCAL:
-		case WASM_OP_TEELOCAL:
-			op->type = R_ANAL_OP_TYPE_STORE;
-			break;
-		case WASM_OP_I32EQZ:
-		case WASM_OP_I32EQ:
-		case WASM_OP_I32NE:
-		case WASM_OP_I32LTS:
-		case WASM_OP_I32LTU:
-		case WASM_OP_I32GTS:
-		case WASM_OP_I32GTU:
-		case WASM_OP_I32LES:
-		case WASM_OP_I32LEU:
-		case WASM_OP_I32GES:
-		case WASM_OP_I32GEU:
-		case WASM_OP_I64EQZ:
-		case WASM_OP_I64EQ:
-		case WASM_OP_I64NE:
-		case WASM_OP_I64LTS:
-		case WASM_OP_I64LTU:
-		case WASM_OP_I64GTS:
-		case WASM_OP_I64GTU:
-		case WASM_OP_I64LES:
-		case WASM_OP_I64LEU:
-		case WASM_OP_I64GES:
-		case WASM_OP_I64GEU:
-		case WASM_OP_F32EQ:
-		case WASM_OP_F32NE:
-		case WASM_OP_F32LT:
-		case WASM_OP_F32GT:
-		case WASM_OP_F32LE:
-		case WASM_OP_F32GE:
-		case WASM_OP_F64EQ:
-		case WASM_OP_F64NE:
-		case WASM_OP_F64LT:
-		case WASM_OP_F64GT:
-		case WASM_OP_F64LE:
-		case WASM_OP_F64GE:
-			op->type = R_ANAL_OP_TYPE_CMP;
-			break;
-		case WASM_OP_NOP:
-			op->type = R_ANAL_OP_TYPE_NOP;
-			break;
-		case WASM_OP_CALL:
-		case WASM_OP_CALLINDIRECT:
-			op->type = R_ANAL_OP_TYPE_CALL;
-			op->jump = get_cf_offset (anal, data);
-			op->fail = addr + op->size;
-			if (op->jump != UT64_MAX) {
-				op->ptr = op->jump;
-			}
-			break;
-		case WASM_OP_BR:
-			op->type = R_ANAL_OP_TYPE_JMP;
-			set_br_jump(op, data);
-			break;
-		case WASM_OP_BRIF:
-			op->fail = addr + op->size;
-			op->type = R_ANAL_OP_TYPE_CJMP;
-			set_br_jump(op, data);
-			break;
-		case WASM_OP_RETURN:
-			op->type = R_ANAL_OP_TYPE_CRET;
-			if (find_if_else (addr + op->size, data + op->size, len - op->size, false) == (addr + 1)) {
-				op->type = R_ANAL_OP_TYPE_RET;
-			}
-		default:
-			break;
-		}
-		op_old = wop.op;
 	}
+	switch (wop.op) {
+	/* Calls here are using index instead of address */
+	case WASM_OP_LOOP:
+		//op->type = R_ANAL_OP_TYPE_CJMP;
+		addr2 = find_if_else (addr + op->size, data + op->size, len - op->size, true);
+		op->type = R_ANAL_OP_TYPE_CJMP;
+		if (addr2 != UT64_MAX) {
+			wasm_stack[wasm_stack_ptr].loop = addr;
+			wasm_stack[wasm_stack_ptr].end = addr2;
+			wasm_stack[wasm_stack_ptr].size = wop.len;
+			wasm_stack_ptr++;
+			addr2 = UT64_MAX;
+		}
+		//op->fail = addr + op->size;
+		break;
+	case WASM_OP_BLOCK:
+		op->type = R_ANAL_OP_TYPE_NOP;
+		addr2 = find_if_else (addr + op->size, data + op->size, len - op->size, true);
+		if (addr2 != UT64_MAX) {
+			wasm_stack[wasm_stack_ptr].loop = UT64_MAX;
+			wasm_stack[wasm_stack_ptr].end = addr2;
+			wasm_stack[wasm_stack_ptr].size = wop.len;
+			wasm_stack_ptr++;
+			addr2 = UT64_MAX;
+		}
+		break;
+	case WASM_OP_IF:
+		op->type = R_ANAL_OP_TYPE_CJMP;
+		op->jump = find_if_else (addr + op->size, data + op->size, len - op->size, false);
+		op->fail = addr + op->size;
+		break;
+	case WASM_OP_ELSE:
+		op->type = R_ANAL_OP_TYPE_JMP;
+		op->jump = find_if_else (addr + op->size, data + op->size, len - op->size, false);
+		break;
+	case WASM_OP_I32REMS:
+	case WASM_OP_I32REMU:
+		op->type = R_ANAL_OP_TYPE_MOD;
+		break;
+	case WASM_OP_END:
+		op->type = R_ANAL_OP_TYPE_RET;
+		if (addr != UT64_MAX) {
+			for (i = 0; i < wasm_stack_ptr; ++i) {
+				if (wasm_stack[i].end == addr && wasm_stack[i].loop != UT64_MAX) {
+					op->type = R_ANAL_OP_TYPE_CJMP;
+					op->jump = wasm_stack[i].loop;
+					op->fail = addr + op->size;
+					break;
+				}
+			}
+		}
+		if (op_old == WASM_OP_CALL || op_old == WASM_OP_CALLINDIRECT || op_old == WASM_OP_RETURN) {
+			op->eob = true;
+			for (i = wasm_stack_ptr - 1; i > 0; --i) {
+				if (addr > wasm_stack[i].loop && addr < wasm_stack[i].end) {
+					op->eob = false;
+					break;
+				}
+			}
+		}
+		break;
+	case WASM_OP_GETLOCAL:
+	case WASM_OP_I32LOAD:
+	case WASM_OP_I64LOAD:
+	case WASM_OP_F32LOAD:
+	case WASM_OP_F64LOAD:
+	case WASM_OP_I32LOAD8S:
+	case WASM_OP_I32LOAD8U:
+	case WASM_OP_I32LOAD16S:
+	case WASM_OP_I32LOAD16U:
+	case WASM_OP_I64LOAD8S:
+	case WASM_OP_I64LOAD8U:
+	case WASM_OP_I64LOAD16S:
+	case WASM_OP_I64LOAD16U:
+	case WASM_OP_I64LOAD32S:
+	case WASM_OP_I64LOAD32U:
+		op->type = R_ANAL_OP_TYPE_LOAD;
+		break;
+	case WASM_OP_SETLOCAL:
+	case WASM_OP_TEELOCAL:
+		op->type = R_ANAL_OP_TYPE_STORE;
+		break;
+	case WASM_OP_I32EQZ:
+	case WASM_OP_I32EQ:
+	case WASM_OP_I32NE:
+	case WASM_OP_I32LTS:
+	case WASM_OP_I32LTU:
+	case WASM_OP_I32GTS:
+	case WASM_OP_I32GTU:
+	case WASM_OP_I32LES:
+	case WASM_OP_I32LEU:
+	case WASM_OP_I32GES:
+	case WASM_OP_I32GEU:
+	case WASM_OP_I64EQZ:
+	case WASM_OP_I64EQ:
+	case WASM_OP_I64NE:
+	case WASM_OP_I64LTS:
+	case WASM_OP_I64LTU:
+	case WASM_OP_I64GTS:
+	case WASM_OP_I64GTU:
+	case WASM_OP_I64LES:
+	case WASM_OP_I64LEU:
+	case WASM_OP_I64GES:
+	case WASM_OP_I64GEU:
+	case WASM_OP_F32EQ:
+	case WASM_OP_F32NE:
+	case WASM_OP_F32LT:
+	case WASM_OP_F32GT:
+	case WASM_OP_F32LE:
+	case WASM_OP_F32GE:
+	case WASM_OP_F64EQ:
+	case WASM_OP_F64NE:
+	case WASM_OP_F64LT:
+	case WASM_OP_F64GT:
+	case WASM_OP_F64LE:
+	case WASM_OP_F64GE:
+		op->type = R_ANAL_OP_TYPE_CMP;
+		break;
+	case WASM_OP_I64OR:
+	case WASM_OP_I32OR:
+		op->type = R_ANAL_OP_TYPE_OR;
+		break;
+	case WASM_OP_I64XOR:
+	case WASM_OP_I32XOR:
+		op->type = R_ANAL_OP_TYPE_XOR;
+		break;
+	case WASM_OP_I32CONST:
+	case WASM_OP_I64CONST:
+	case WASM_OP_F32CONST:
+	case WASM_OP_F64CONST:
+		op->type = R_ANAL_OP_TYPE_MOV;
+		break;
+	case WASM_OP_I64ADD:
+	case WASM_OP_I32ADD:
+	case WASM_OP_F32ADD:
+	case WASM_OP_F64ADD:
+		op->type = R_ANAL_OP_TYPE_ADD;
+		break;
+	case WASM_OP_I64SUB:
+	case WASM_OP_I32SUB:
+	case WASM_OP_F32SUB:
+	case WASM_OP_F64SUB:
+		op->type = R_ANAL_OP_TYPE_SUB;
+		break;
+	case WASM_OP_NOP:
+		op->type = R_ANAL_OP_TYPE_NOP;
+		break;
+	case WASM_OP_CALL:
+	case WASM_OP_CALLINDIRECT:
+		op->type = R_ANAL_OP_TYPE_CALL;
+		op->jump = get_cf_offset (anal, data);
+		op->fail = addr + op->size;
+		if (op->jump != UT64_MAX) {
+			op->ptr = op->jump;
+		}
+		break;
+	case WASM_OP_BR:
+		op->type = R_ANAL_OP_TYPE_JMP;
+		set_br_jump(op, data);
+		break;
+	case WASM_OP_BRIF:
+		op->fail = addr + op->size;
+		op->type = R_ANAL_OP_TYPE_CJMP;
+		set_br_jump(op, data);
+		break;
+	case WASM_OP_RETURN:
+		op->type = R_ANAL_OP_TYPE_CRET;
+		if (find_if_else (addr + op->size, data + op->size, len - op->size, false) == (addr + 1)) {
+			op->type = R_ANAL_OP_TYPE_RET;
+		}
+	default:
+		break;
+	}
+	op_old = wop.op;
 	return op->size;
 }
 
