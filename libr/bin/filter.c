588eaec0d8ab87511fd8ec24515103ee43df37fd
dav1901 <44604348+dav1901@users.noreply.github.com>
2018-11-14 10:17:44 +0200

    fix illegal access issue

--- libr/bin/filter.c
+++ libr/bin/filter.c
@@ -9,11 +9,12 @@ static char *hashify(char *s, ut64 vaddr) {
 	char *os = s;
 	while (*s) {
 		if (!IS_PRINTABLE (*s)) {
-			free (os);
 			if (vaddr && vaddr != UT64_MAX) {
+				free (os);
 				return r_str_newf ("_%" PFMT64d, vaddr);
 			} else {
 				ut32 hash = sdb_hash (s);
+				free (os);
 				return r_str_newf ("%x", hash);
 			}
 		}
