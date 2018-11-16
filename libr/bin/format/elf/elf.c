d3d7c5a22a1b4ef245bfbdd399554d6f4050a8d0
Riccardo Schirone <sirmy15@gmail.com>
2018-11-16 13:52:25 +0100

    bin/elf: small style fixes in get_boffset

--- libr/bin/format/elf/elf.c
+++ libr/bin/format/elf/elf.c
@@ -1635,24 +1635,28 @@ ut64 Elf_(r_bin_elf_get_baddr)(ELFOBJ *bin) {
 		//we act as a loader for ELF
 		return 0x08000000;
 	}
-	return base == UT64_MAX ? 0 : base;
+	return base == UT64_MAX? 0: base;
 }
 
 ut64 Elf_(r_bin_elf_get_boffset)(ELFOBJ *bin) {
 	int i;
 	ut64 tmp, base = UT64_MAX;
-	if (bin && bin->phdr) {
-		for (i = 0; i < bin->ehdr.e_phnum; i++) {
-			if (bin->phdr[i].p_type == PT_LOAD) {
-				tmp =  (ut64)bin->phdr[i].p_offset & ELF_PAGE_MASK;
-				tmp = tmp - (tmp % (1 << ELF_PAGE_SIZE));
-				if (tmp < base) {
-					base = tmp;
-				}
+	r_return_val_if_fail (bin, 0);
+
+	if (!bin->phdr) {
+		return 0;
+	}
+
+	for (i = 0; i < bin->ehdr.e_phnum; i++) {
+		if (bin->phdr[i].p_type == PT_LOAD) {
+			tmp = (ut64)bin->phdr[i].p_offset & ELF_PAGE_MASK;
+			tmp = tmp - (tmp % (1 << ELF_PAGE_SIZE));
+			if (tmp < base) {
+				base = tmp;
 			}
 		}
 	}
-	return base == UT64_MAX ? 0 : base;
+	return base == UT64_MAX? 0: base;
 }
 
 ut64 Elf_(r_bin_elf_get_init_offset)(ELFOBJ *bin) {
