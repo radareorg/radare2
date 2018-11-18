b3009a6590a4e9e8e2c251f8faba5eeb3722f7d6
Ayman Khamouma <ak42@mg.blackbunny.io>
2018-11-18 01:17:14 +0100

    Cosmetix fix

--- libr/core/canal.c
+++ libr/core/canal.c
@@ -2846,21 +2846,21 @@ static RList *anal_graph_to(RCore *core, ut64 addr, int depth, HtUP *avoid) {
 
 R_API RList* r_core_anal_graph_to(RCore *core, ut64 addr, int n) {
 	int depth = r_config_get_i (core->config, "anal.graph_depth");
-    RList *path, *paths = r_list_new ();
-    HtUP *avoid = ht_up_new0 ();
+	RList *path, *paths = r_list_new ();
+	HtUP *avoid = ht_up_new0 ();
 	n = 5;
-    while (n) {
-        path = anal_graph_to (core, addr, depth, avoid);
-        if (path && (r_list_length (path) >= 2)) {
+	while (n) {
+		path = anal_graph_to (core, addr, depth, avoid);
+		if (path && (r_list_length (path) >= 2)) {
 			RAnalBlock *last = r_list_get_n (path, r_list_length (path) - 2);
-            r_list_append (paths, path);
-            ht_up_update (avoid, last->addr, last);
-            n--;
+			r_list_append (paths, path);
+			ht_up_update (avoid, last->addr, last);
+			n--;
 			continue;
-        }
+		}
 		// no more path found
 		break;
-    }
+	}
 	return paths;
 }
 
