17038e13e179f06d9fe1e99c63a3588ec695989b
pancake <pancake@nopcode.org>
2018-11-21 18:29:37 +0100

    Support REX in NOT instruction for the x86 assembler

--- libr/asm/p/asm_x86_nz.c
+++ libr/asm/p/asm_x86_nz.c
@@ -611,10 +611,29 @@ static int opxor(RAsm *a, ut8 * data, const Opcode *op) {
 static int opnot(RAsm *a, ut8 * data, const Opcode *op) {
 	int l = 0;
 
-	if(op->operands[0].reg == X86R_UNDEFINED)  {
+	if (op->operands[0].reg == X86R_UNDEFINED)  {
 		return -1;
 	}
 
+	int size = op->operands[0].type & ALL_SIZE;
+	if (op->operands[0].explicit_size) {
+		size = op->operands[0].dest_size;
+	}
+	//rex prefix
+	int rex = 1 << 6;
+	bool use_rex = false;
+	if (size & OT_QWORD) {			//W field
+		use_rex = true;
+		rex |= 1 << 3;
+	}
+	if (op->operands[0].extended) {		//B field
+		use_rex = true;
+		rex |= 1;
+	}
+
+	if (use_rex) {
+		data[l++] = rex;
+	}
 	data[l++] = 0xf7;
 	data[l++] = 0xd0 | op->operands[0].reg;
 
