214baa4856a411457c5e54b71de2a6da7acff3ce
pancake <pancake@nopcode.org>
2018-11-28 11:53:27 +0100

    Fix null deref in RSocket.free when compiled with SSL

--- libr/socket/socket.c
+++ libr/socket/socket.c
@@ -468,7 +468,7 @@ R_API int r_socket_close (RSocket *s) {
 R_API int r_socket_free (RSocket *s) {
 	int res = r_socket_close (s);
 #if HAVE_LIB_SSL
-	if (s->is_ssl) {
+	if (s && s->is_ssl) {
 		if (s->sfd) {
 			SSL_free (s->sfd);
 		}
