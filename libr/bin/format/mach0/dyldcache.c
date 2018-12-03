b143e1b1b5622ef2f41a90f4c0f7ed4c477caf40
pancake <pancake@nopcode.org>
2018-12-03 03:31:57 +0100

    Release 3.1.1

--- libr/bin/format/mach0/dyldcache.c
+++ libr/bin/format/mach0/dyldcache.c
@@ -32,8 +32,8 @@ static char *r_buf_read_string (RBuffer *buf, ut64 addr, int len) {
 	ut8 *data = malloc (len);
 	if (data) {
 		r_buf_read_at (buf, addr, data, len);
-		data[len-1] = 0;
-		return data;
+		data[len - 1] = 0;
+		return (char *)data;
 	}
 	return NULL;
 }
