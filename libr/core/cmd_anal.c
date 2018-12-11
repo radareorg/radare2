radareorg/radare2

commit b703dfbf17c0cff54181627fb40da92de3a52b79

anal/xrefs: single, global xrefs API
* several bugfixes
* Fix xrefs/refs confusion with asm.cmt.refs
* Also check if xref source is valid offset
* Fix ax* to show type of ref
* anal/xrefs: pass the right hashtable to mylistrefs
* r_anal_refs_get should use anal->refs, not anal->xrefs
* anal/fcn: add xref also when there's a CALL to a noreturn function
* projects: use radare2 output of ax command to save/restore xrefs
* core/cmd_anal: when analyzing calls, we should use REF_TYPE_CALL
* core/canal: use xrefs API, because the list is a just a copy
* core/canal: rename "loc." entries to "fcn." when a CALL is found
* sort xrefs to make results consistent
* core/canal: avoid recomputing function every time during `aan`
* anal: move fcn_refs/xrefs functions to the xrefs.c file
* core/canal.c: avoid iterating fcn xrefs list
* anal/xrefs: compute fcn xrefs by analyzing fcn addresses
* remove other unused functions
* anal/xrefs: remove fcn_xrefs_add and fcn_xrefs_deln
* anal/xrefs: remove old fcn->refs/xrefs
* anal/xrefs: directly store RAnalRef objects in the hash table
* libr: prevent memory leaks when using refs/xrefs
* anal/xrefs: merge anal/ref and anal/xref and clean API

Big xrefs/refs refactoring that provides a more uniform and simple API.
It avoids changes to refs/xrefs except through the API and it keeps all
xrefs/refs info in one single place, to improve consistency.

Thanks to:
Riccardo Schirone <sirmy15@gmail.com>
pancake <pancake@nopcode.org>
rene <rlaemmert@gmail.com>

libr/core/cmd_anal.c
@@ -560,7 +560,8 @@ static const char *help_msg_ax[] = {
 	"Usage:", "ax[?d-l*]", " # see also 'afx?'",
 	"ax", "", "list refs",
 	"ax", " addr [at]", "add code ref pointing to addr (from curseek)",
-	"ax-", " [at]", "clean all refs (or refs from addr)",
+	"ax-", " [at]", "clean all refs/refs from addr",
+	"ax-*", "", "clean all refs/refs",
 	"axc", " addr [at]", "add code jmp ref // unused?",
 	"axC", " addr [at]", "add code call ref",
 	"axg", " [addr]", "show xrefs graph to reach current function",
@@ -571,7 +572,6 @@ static const char *help_msg_ax[] = {
 	"axF", " [flg-glob]", "find data/code references of flags",
 	"axt", " [addr]", "find data/code references to this address",
 	"axf", " [addr]", "find data/code references from this address",
-	"axk", " [query]", "perform sdb query",
 	"ax*", "", "output radare commands",
 	NULL
 };
@@ -2571,7 +2571,7 @@ static int cmd_anal_fcn(RCore *core, const char *input) {
 				if (fcn) {
 					RAnalRef *ref;
 					RListIter *iter;
-					RList *refs = r_anal_fcn_get_refs (core->anal, fcn);
+					RList *refs = r_anal_fcn_get_refs_sorted (core->anal, fcn);
 					r_list_foreach (refs, iter, ref) {
 						if (input[2] == 'j') {
 							r_cons_printf ("{\"type\":\"%c\",\"from\":%"PFMT64d",\"to\":%"PFMT64d"}%s",
@@ -2602,10 +2602,11 @@ static int cmd_anal_fcn(RCore *core, const char *input) {
 			ut64 a, b;
 			char *mi = strdup (input);
 			if (mi && mi[3] == ' ' && (p = strchr (mi + 4, ' '))) {
+				RAnalRefType reftype = r_anal_xrefs_type (input[2]);
 				*p = 0;
 				a = r_num_math (core->num, mi + 3);
 				b = r_num_math (core->num, p + 1);
-				r_anal_xrefs_set (core->anal, input[2], a, b);
+				r_anal_xrefs_set (core->anal, a, b, reftype);
 			} else {
 				r_core_cmd_help (core, help_msg_afx);
 			}
@@ -2621,7 +2622,7 @@ static int cmd_anal_fcn(RCore *core, const char *input) {
 				*p = 0;
 				a = r_num_math (core->num, mi);
 				b = r_num_math (core->num, p + 1);
-				r_anal_xrefs_deln (core->anal, -1, a, b);
+				r_anal_xrefs_deln (core->anal, a, b, -1);
 			} else {
 				eprintf ("Usage: afx- [src] [dst]\n");
 			}
@@ -2687,7 +2688,7 @@ static int cmd_anal_fcn(RCore *core, const char *input) {
 			if (fcn) {
 				RAnalRef *ref;
 				RListIter *iter;
-				RList *refs = r_anal_fcn_get_refs (core->anal, fcn);
+				RList *refs = r_anal_fcn_get_refs_sorted (core->anal, fcn);
 				r_list_foreach (refs, iter, ref) {
 					if (ref->addr == UT64_MAX) {
 						//eprintf ("Warning: ignore 0x%08"PFMT64x" call 0x%08"PFMT64x"\n", ref->at, ref->addr);
@@ -2707,7 +2708,7 @@ static int cmd_anal_fcn(RCore *core, const char *input) {
 					if (f) {
 						RListIter *iter;
 						RAnalRef *ref;
-						RList *refs1 = r_anal_fcn_get_refs (core->anal, f);
+						RList *refs1 = r_anal_fcn_get_refs_sorted (core->anal, f);
 						r_list_foreach (refs1, iter, ref) {
 							if (!r_io_is_valid_offset (core->io, ref->addr, !core->anal->opt.noncode)) {
 								continue;
@@ -4748,14 +4749,14 @@ static void _anal_calls(RCore *core, ut64 addr, ut64 addr_end) {
 				if (!anal_is_bad_call (core, from, to, addr, buf, bufi)) {
 					fcn = r_anal_get_fcn_in (core->anal, op.jump, R_ANAL_FCN_TYPE_ROOT);
 					if (!fcn) {
-						r_core_anal_fcn (core, op.jump, addr, R_ANAL_REF_TYPE_NULL, depth);
+						r_core_anal_fcn (core, op.jump, addr, R_ANAL_REF_TYPE_CALL, depth);
 					}
 				}
 #else
 				// add xref here
-				r_anal_xrefs_set (core->anal, R_ANAL_REF_TYPE_CALL, addr, op.jump);
+				r_anal_xrefs_set (core->anal, addr, op.jump, R_ANAL_REF_TYPE_CALL);
 				if (r_io_is_valid_offset (core->io, op.jump, 1)) {
-					r_core_anal_fcn (core, op.jump, addr, R_ANAL_REF_TYPE_NULL, depth);
+					r_core_anal_fcn (core, op.jump, addr, R_ANAL_REF_TYPE_CALL, depth);
 				}
 #endif
 			}
@@ -5086,7 +5087,7 @@ static bool cmd_anal_refs(RCore *core, const char *input) {
 		RAnalRef *ref;
 		char *cp_inp = strdup (input + 1);
 		char *ptr = r_str_trim_head (cp_inp);
-		if (!strcmp (ptr, "*")) {
+		if (!strcmp (ptr, "*")) { // "ax-*"
 			r_anal_xrefs_init (core->anal);
 		} else {
 			int n = r_str_word_set0 (ptr);
@@ -5106,14 +5107,14 @@ static bool cmd_anal_refs(RCore *core, const char *input) {
 			if (list) {
 				r_list_foreach (list, iter, ref) {
 					if (from != UT64_MAX && from == ref->addr) {
-						r_anal_ref_del (core->anal, ref->addr, ref->at);
+						r_anal_xref_del (core->anal, ref->addr, ref->at);
 					}
 					if (from == UT64_MAX) {
-						r_anal_ref_del (core->anal, ref->addr, ref->at);
+						r_anal_xref_del (core->anal, ref->addr, ref->at);
 					}
 				}
-				r_list_free (list);
 			}
+			r_list_free (list);
 		}
 		free (cp_inp);
 	} break;
@@ -5126,15 +5127,6 @@ static bool cmd_anal_refs(RCore *core, const char *input) {
 				anal_axg (core, input[1] ? input + 2 : NULL, 0, db, R_CORE_ANAL_JSON);
 			}
 			sdb_free (db);
-		}
-		break;
-	case 'k': // "axk"
-		if (input[1] == '?') {
-			eprintf ("Usage: axk [query]\n");
-		} else if (input[1] == ' ') {
-			sdb_query (core->anal->sdb_xrefs, input + 2);
-		} else {
-			r_core_anal_ref_list (core, 'k');
 		}
 		break;
 	case '\0': // "ax"
@@ -5183,7 +5175,7 @@ static bool cmd_anal_refs(RCore *core, const char *input) {
 					r_parse_filter (core->parser, core->flags,
 							asmop.buf_asm, str, sizeof (str), core->print->big_endian);
 
-					r_cons_printf ("{\"from\":%" PFMT64u ",\"type\":\"%s\",\"opcode\":\"%s\"", ref->addr, r_anal_ref_to_string (ref->type), str);
+					r_cons_printf ("{\"from\":%" PFMT64u ",\"type\":\"%s\",\"opcode\":\"%s\"", ref->addr, r_anal_xrefs_type_tostring (ref->type), str);
 					if (fcn) {
 						r_cons_printf (",\"fcn_addr\":%"PFMT64d",\"fcn_name\":\"%s\"", fcn->addr, fcn->name);
 					}
@@ -5256,17 +5248,17 @@ static bool cmd_anal_refs(RCore *core, const char *input) {
 						? r_str_newf ("%s; %s", fcn ?  fcn->name : "(nofunc)", strtok (comment, "\n"))
 						: r_str_newf ("%s", fcn ? fcn->name : "(nofunc)");
 					r_cons_printf ("%s 0x%" PFMT64x " [%s] %s\n",
-						buf_fcn, ref->addr, r_anal_ref_to_string (ref->type), buf_asm);
+						buf_fcn, ref->addr, r_anal_xrefs_type_tostring (ref->type), buf_asm);
 					free (buf_asm);
 					free (buf_fcn);
 				}
 			}
-			r_list_free (list);
 		} else {
 			if (input[1] == 'j') { // "axtj"
 				r_cons_print ("[]\n");
 			}
 		}
+		r_list_free (list);
 	} break;
 	case 'f': { // "axf"
 		ut8 buf[12];
@@ -5286,7 +5278,7 @@ static bool cmd_anal_refs(RCore *core, const char *input) {
 			list = list_ = r_anal_xrefs_get_from (core->anal, addr);
 			if (!list) {
 				RAnalFunction * fcn = r_anal_get_fcn_in (core->anal, addr, 0);
-				list = r_anal_fcn_get_refs (core->anal, fcn);
+				list = r_anal_fcn_get_refs_sorted (core->anal, fcn);
 			}
 		} else {
 			list = r_anal_refs_get (core->anal, addr);
@@ -5304,7 +5296,7 @@ static bool cmd_anal_refs(RCore *core, const char *input) {
 					r_asm_set_pc (core->assembler, ref->at);
 					r_asm_disassemble (core->assembler, &asmop, buf, 12);
 					r_cons_printf ("{\"from\":%" PFMT64d ",\"to\":%" PFMT64d ",\"type\":\"%s\",\"opcode\":\"%s\"}%s",
-						ref->at, ref->addr, r_anal_ref_to_string (ref->type), asmop.buf_asm, iter->n? ",": "");
+						ref->at, ref->addr, r_anal_xrefs_type_tostring (ref->type), asmop.buf_asm, iter->n? ",": "");
 				}
 				r_cons_print ("]\n");
 			} else if (input[1] == '*') { // axf*
@@ -5342,13 +5334,12 @@ static bool cmd_anal_refs(RCore *core, const char *input) {
 					free (buf_asm);
 				}
 			}
-			r_list_free (list_);
-			r_list_free (list);
 		} else {
 			if (input[1] == 'j') { // axfj
 				r_cons_print ("[]\n");
 			}
 		}
+		r_list_free (list);
 	} break;
 	case 'F':
 		find_refs (core, input + 1);
@@ -5362,6 +5353,7 @@ static bool cmd_anal_refs(RCore *core, const char *input) {
 		int n = r_str_word_set0 (ptr);
 		ut64 at = core->offset;
 		ut64 addr = UT64_MAX;
+		RAnalRefType reftype = r_anal_xrefs_type (input[0]);
 		switch (n) {
 		case 2: // get at
 			at = r_num_math (core->num, r_str_word_get0 (ptr, 1));
@@ -5373,7 +5365,7 @@ static bool cmd_anal_refs(RCore *core, const char *input) {
 			free (ptr);
 			return false;
 		}
-		r_anal_xrefs_set (core->anal, input[0], at, addr);
+		r_anal_xrefs_set (core->anal, at, addr, reftype);
 		free (ptr);
 		}
 	   	break;
@@ -6042,9 +6034,9 @@ static int compute_calls(RCore *core) {
 		xrefs = r_anal_fcn_get_xrefs (core->anal, fcn);
 		if (xrefs) {
 			cov += r_list_length (xrefs);
-			r_list_free (xrefs);
 			xrefs = NULL;
 		}
+		r_list_free (xrefs);
 	}
 	return cov;
 }
@@ -6143,7 +6135,7 @@ void _CbInRangeAav(RCore *core, ut64 from, ut64 to, int vsize, bool asterisk, in
 		r_cons_printf ("f+ aav.0x%08"PFMT64x "= 0x%08"PFMT64x, to, to);
 	} else {
 #if 1
-		r_anal_ref_add (core->anal, to, from, ' ');
+		r_anal_xrefs_set (core->anal, from, to, R_ANAL_REF_TYPE_NULL);
 		r_meta_add (core->anal, 'd', from, from + vsize, NULL);
 		if (!r_flag_get_at (core->flags, to, false)) {
 			char *name = r_str_newf ("aav.0x%08"PFMT64x, to);

libr/anal/Makefile
@@ -27,7 +27,7 @@ plugins: ${LIBSO} ${LIBAR}
 include ${STATIC_ANAL_PLUGINS}
 
 STATIC_OBJS=$(addprefix $(LTOP)/anal/p/,$(STATIC_OBJ))
-OBJLIBS=meta.o reflines.o ref.o op.o fcn.o bb.o var.o
+OBJLIBS=meta.o reflines.o op.o fcn.o bb.o var.o
 OBJLIBS+=cond.o value.o cc.o diff.o types.o
 OBJLIBS+=hint.o anal.o data.o xrefs.o esil.o sign.o
 OBJLIBS+=anal_ex.o switch.o state.o cycles.o

libr/anal/anal.c
@@ -78,14 +78,9 @@ R_API RAnal *r_anal_new() {
 	anal->sdb_fcns = sdb_ns (anal->sdb, "fcns", 1);
 	anal->sdb_meta = sdb_ns (anal->sdb, "meta", 1);
 	anal->sdb_hints = sdb_ns (anal->sdb, "hints", 1);
-	anal->sdb_xrefs = sdb_ns (anal->sdb, "xrefs", 1);
 	anal->sdb_types = sdb_ns (anal->sdb, "types", 1);
 	anal->sdb_cc = sdb_ns (anal->sdb, "cc", 1);
 	anal->sdb_zigns = sdb_ns (anal->sdb, "zigns", 1);
-#if USE_DICT
-	anal->dict_refs = dict_new (100, dict_free);
-	anal->dict_xrefs = dict_new (100, dict_free);
-#endif
 	anal->zign_path = strdup ("");
 	anal->cb_printf = (PrintfCallback) printf;
 	(void)r_anal_pin_init (anal);
@@ -141,6 +136,8 @@ R_API RAnal *r_anal_free(RAnal *a) {
 	r_reg_free (a->reg);
 	r_anal_op_free (a->queued);
 	r_list_free (a->bits_ranges);
+	ht_free (a->dict_refs);
+	ht_free (a->dict_xrefs);
 	a->sdb = NULL;
 	sdb_ns_free (a->sdb);
 	if (a->esil) {
@@ -364,13 +361,6 @@ R_API RList* r_anal_get_fcns (RAnal *anal) {
 	return anal->fcns;
 }
 
-R_API bool r_anal_project_save(RAnal *anal, const char *prjfile) {
-	if (prjfile && *prjfile) {
-		return r_anal_xrefs_save (anal, prjfile);
-	}
-	return false;
-}
-
 R_API RAnalOp *r_anal_op_hexstr(RAnal *anal, ut64 addr, const char *str) {
 	int len;
 	ut8 *buf;
@@ -412,7 +402,6 @@ R_API int r_anal_purge (RAnal *anal) {
 	sdb_reset (anal->sdb_fcns);
 	sdb_reset (anal->sdb_meta);
 	sdb_reset (anal->sdb_hints);
-	sdb_reset (anal->sdb_xrefs);
 	sdb_reset (anal->sdb_types);
 	sdb_reset (anal->sdb_zigns);
 	r_list_free (anal->fcns);

libr/anal/bb.c
@@ -144,7 +144,7 @@ R_API int r_anal_bb(RAnal *anal, RAnalBlock *bb, ut64 addr, ut8 *buf, ut64 len, 
 					ut8 b[8];
 					ut64 ptr = idx+addr+src->delta;
 					anal->iob.read_at (anal->iob.io, ptr, b, memref);
-					r_anal_ref_add (anal, ptr, addr+idx-op->size, 'd');
+					r_anal_xrefs_set (anal, addr+idx-op->size, ptr, R_ANAL_REF_TYPE_DATA);
 				}
 			}
 		}

libr/anal/fcn.c
@@ -293,10 +293,6 @@ R_API RAnalFunction *r_anal_fcn_new() {
 	fcn->cc = NULL;
 	/* Function attributes: weak/noreturn/format/etc */
 	fcn->addr = UT64_MAX;
-#if FCN_OLD
-	fcn->refs = r_anal_ref_list_new ();
-	fcn->xrefs = r_anal_ref_list_new ();
-#endif
 	fcn->fcn_locs = NULL;
 	fcn->bbs = r_anal_bb_list_new ();
 	fcn->fingerprint = NULL;
@@ -323,10 +319,6 @@ R_API void r_anal_fcn_free(void *_fcn) {
 	free (fcn->name);
 	free (fcn->attr);
 	r_tinyrange_fini (&fcn->bbr);
-#if FCN_OLD
-	r_list_free (fcn->refs);
-	r_list_free (fcn->xrefs);
-#endif
 	// all functions are freed in anal->fcns
 	fcn->fcn_locs = NULL;
 	if (fcn->bbs) {
@@ -349,64 +341,6 @@ static bool refExists(RList *refs, RAnalRef *ref) {
 			return true;
 		}
 	}
-	return false;
-}
-
-R_API int r_anal_fcn_xref_add(RAnal *a, RAnalFunction *fcn, ut64 at, ut64 addr, int type) {
-	RAnalRef *ref;
-	if (!fcn || !a) {
-		return false;
-	}
-	if (!a->iob.is_valid_offset (a->iob.io, addr, 0)) {
-		return false;
-	}
-	ref = r_anal_ref_new ();
-	if (!ref) {
-		return false;
-	}
-	// set global reference
-// r_cons_printf ("C 0x%llx  0x%llx\n", at, addr);
-	r_anal_xrefs_set (a, type, at, addr);
-	// set per-function reference
-#if FCN_OLD
-// TOO OLD we shouldnt be storing this.. or we do?
-	ref->at = at; // from
-	ref->addr = addr; // to
-	ref->type = type;
-	// TODO: ensure we are not dupping xrefs
-	if (refExists (fcn->refs, ref)) {
-		r_anal_ref_free (ref);
-	} else {
-		r_list_append (fcn->refs, ref);
-	}
-#endif
-#if FCN_SDB
-	sdb_add (DB, sdb_fmt ("fcn.0x%08"PFMT64x ".name", fcn->addr), fcn->name, 0);
-	// encode the name in base64 ?
-	sdb_num_add (DB, sdb_fmt ("fcn.name.%s", fcn->name), fcn->addr, 0);
-	sdb_array_add_num (DB, sdb_fmt ("fcn.0x%08"PFMT64x ".xrefs", fcn->addr), at, 0);
-#endif
-	return true;
-}
-
-R_API int r_anal_fcn_xref_del(RAnal *a, RAnalFunction *fcn, ut64 at, ut64 addr, int type) {
-#if FCN_OLD
-	RAnalRef *ref;
-	RListIter *iter;
-	/* No need for _safe loop coz we return immediately after the delete. */
-	r_list_foreach (fcn->xrefs, iter, ref) {
-		if ((type != -1 || type == ref->type) &&
-		(at == 0LL || at == ref->at) &&
-		(addr == 0LL || addr == ref->addr)) {
-			r_list_delete (fcn->xrefs, iter);
-			return true;
-		}
-	}
-#endif
-#if FCN_SDB
-	// TODO
-	// sdb_array_delete_num (DB, key, at, 0);
-#endif
 	return false;
 }
 
@@ -1150,7 +1084,7 @@ repeat:
 
 		if (op.ptr && op.ptr != UT64_MAX && op.ptr != UT32_MAX) {
 			// swapped parameters wtf
-			r_anal_fcn_xref_add (anal, fcn, op.addr, op.ptr, R_ANAL_REF_TYPE_DATA);
+			r_anal_xrefs_set (anal, op.addr, op.ptr, R_ANAL_REF_TYPE_DATA);
 		}
 
 		switch (op.type & R_ANAL_OP_TYPE_MASK) {
@@ -1289,7 +1223,7 @@ repeat:
 				return R_ANAL_RET_END;
 			}
 			if (anal->opt.jmpref) {
-				(void) r_anal_fcn_xref_add (anal, fcn, op.addr, op.jump, R_ANAL_REF_TYPE_CODE);
+				(void) r_anal_xrefs_set (anal, op.addr, op.jump, R_ANAL_REF_TYPE_CODE);
 			}
 			if (!anal->opt.jmpabove && (op.jump < fcn->addr)) {
 				FITFCNSZ ();
@@ -1384,8 +1318,7 @@ repeat:
 			break;
 		case R_ANAL_OP_TYPE_CJMP:
 			if (anal->opt.cjmpref) {
-				(void) r_anal_fcn_xref_add (anal, fcn,
-					op.addr, op.jump, R_ANAL_REF_TYPE_CODE);
+				(void) r_anal_xrefs_set (anal, op.addr, op.jump, R_ANAL_REF_TYPE_CODE);
 			}
 			if (!overlapped) {
 				bb->jump = op.jump;
@@ -1455,23 +1388,25 @@ repeat:
 		case R_ANAL_OP_TYPE_ICALL:
 		case R_ANAL_OP_TYPE_IRCALL:
 			/* call [dst] */
+			// XXX: this is TYPE_MCALL or indirect-call
+			(void) r_anal_xrefs_set (anal, op.addr, op.ptr, R_ANAL_REF_TYPE_CALL);
+
 			if (op.ptr != UT64_MAX && r_anal_noreturn_at (anal, op.ptr)) {
 				FITFCNSZ ();
 				r_anal_op_fini (&op);
 				return R_ANAL_RET_END;
 			}
-			// XXX: this is TYPE_MCALL or indirect-call
-			(void) r_anal_fcn_xref_add (anal, fcn, op.addr, op.ptr, R_ANAL_REF_TYPE_CALL);
 			break;
 		case R_ANAL_OP_TYPE_CCALL:
 		case R_ANAL_OP_TYPE_CALL:
 			/* call dst */
+			(void) r_anal_xrefs_set (anal, op.addr, op.jump, R_ANAL_REF_TYPE_CALL);
+
 			if (r_anal_noreturn_at (anal, op.jump)) {
 				FITFCNSZ ();
 				r_anal_op_fini (&op);
 				return R_ANAL_RET_END;
 			}
-			(void) r_anal_fcn_xref_add (anal, fcn, op.addr, op.jump, R_ANAL_REF_TYPE_CALL);
 #if CALL_IS_EOB
 			recurseAt (op.jump);
 			recurseAt (op.fail);
@@ -1491,13 +1426,6 @@ repeat:
 			}
 			// switch statement
 			if (anal->opt.jmptbl) {
-				if (fcn->refs->tail) {
-					RAnalRef *last_ref = fcn->refs->tail->data;
-					last_ref->type = R_ANAL_REF_TYPE_NULL;
-					// TODO: walk switch? try_walkthrough_jmptbl?
-					// Why is this a jmp table and what does it look like
-					// walk_switch (anal, fcn, op.addr, op.addr + op.size);
-				}
 				// op.ireg since rip relative addressing produces way too many false positives otherwise
 				// op.ireg is 0 for rip relative, "rax", etc otherwise
 				if (op.ptr != UT64_MAX && op.ireg) {       // direct jump
@@ -1554,7 +1482,7 @@ analopfinish:
 			last_is_push = true;
 			last_push_addr = op.val;
 			if (anal->iob.is_valid_offset (anal->iob.io, op.val, 1)) {
-				(void) r_anal_fcn_xref_add (anal, fcn, op.addr, op.val, R_ANAL_REF_TYPE_DATA);
+				(void) r_anal_xrefs_set (anal, op.addr, op.val, R_ANAL_REF_TYPE_DATA);
 			}
 			break;
 		case R_ANAL_OP_TYPE_RET:
@@ -1666,15 +1594,18 @@ R_API void r_anal_fcn_fit_overlaps(RAnal *anal, RAnalFunction *fcn) {
 	}
 }
 
-R_API void r_anal_trim_jmprefs(RAnalFunction *fcn) {
+R_API void r_anal_trim_jmprefs(RAnal *anal, RAnalFunction *fcn) {
 	RAnalRef *ref;
+	RList *refs = r_anal_fcn_get_refs (anal, fcn);
 	RListIter *iter;
 	RListIter *tmp;
-	r_list_foreach_safe (fcn->refs, iter, tmp, ref) {
+
+	r_list_foreach (refs, iter, ref) {
 		if (ref->type == R_ANAL_REF_TYPE_CODE && r_anal_fcn_is_in_offset (fcn, ref->addr)) {
-			r_list_delete (fcn->refs, iter);
+			r_anal_xrefs_deln (anal, ref->at, ref->addr, ref->type);
 		}
 	}
+	r_list_free (refs);
 }
 
 R_API int r_anal_fcn(RAnal *anal, RAnalFunction *fcn, ut64 addr, ut8 *buf, ut64 len, int reftype) {
@@ -1718,7 +1649,7 @@ R_API int r_anal_fcn(RAnal *anal, RAnalFunction *fcn, ut64 addr, ut8 *buf, ut64 
 		// fcn is not yet in anal => pass NULL
 		r_anal_fcn_resize (NULL, fcn, endaddr - fcn->addr);
 #endif
-		r_anal_trim_jmprefs (fcn);
+		r_anal_trim_jmprefs (anal, fcn);
 	}
 	return ret;
 }
@@ -2318,14 +2249,4 @@ R_API int r_anal_fcn_count_edges(RAnalFunction *fcn, int *ebbs) {
 		}
 	}
 	return edges;
-}
-
-R_API RList *r_anal_fcn_get_refs(RAnal *anal, RAnalFunction *fcn) {
-	RList *ret = (fcn) ? r_list_clone (fcn->refs) : NULL;
-	return ret;
-}
-
-R_API RList *r_anal_fcn_get_xrefs(RAnal *anal, RAnalFunction *fcn) {
-	RList *ret = (fcn) ? r_list_clone (fcn->xrefs) : NULL;
-	return ret;
 }

libr/anal/flirt.c
@@ -593,7 +593,7 @@ static int module_match_buffer(const RAnal *anal, const RFlirtModule *module,
 				}
 				r_anal_fcn_resize (anal, next_module_function, flirt_fcn_size);
 				next_module_function_size = r_anal_fcn_size (next_module_function);
-				r_anal_trim_jmprefs (next_module_function);
+				r_anal_trim_jmprefs ((RAnal *)anal, next_module_function);
 			}
 
 

libr/anal/meson.build
@@ -66,7 +66,6 @@ files = [
   'p/anal_xtensa.c',
   'p/anal_z80.c',
   'pin.c',
-  'ref.c',
   'reflines.c',
   'rtti.c',
   'rtti_msvc.c',

libr/anal/p/anal_java.c
@@ -223,7 +223,7 @@ static int handle_bb_cf_recursive_descent (RAnal *anal, RAnalState *state) {
 	switch (control_type) {
 		case R_ANAL_OP_TYPE_CALL:
 			IFDBG eprintf (" - Handling a call @ 0x%04"PFMT64x".\n", addr);
-			r_anal_fcn_xref_add (anal, state->current_fcn, bb->addr, bb->jump, R_ANAL_REF_TYPE_CALL);
+			r_anal_xrefs_set (anal, bb->addr, bb->jump, R_ANAL_REF_TYPE_CALL);
 			result = R_ANAL_RET_ERROR;
 			break;
 		case R_ANAL_OP_TYPE_JMP:
@@ -406,7 +406,7 @@ static int handle_bb_cf_linear_sweep (RAnal *anal, RAnalState *state) {
 	switch (control_type) {
 		case R_ANAL_OP_TYPE_CALL:
 			IFDBG eprintf (" - Handling a call @ 0x%04"PFMT64x"\n", addr);
-			r_anal_fcn_xref_add (anal, state->current_fcn, bb->addr, bb->jump, R_ANAL_REF_TYPE_CALL);
+			r_anal_xrefs_set (anal, bb->addr, bb->jump, R_ANAL_REF_TYPE_CALL);
 			result = R_ANAL_RET_ERROR;
 			break;
 		case R_ANAL_OP_TYPE_JMP:

commit c2cfa547f1cd2a4eb894aba91f4f4ce8ac423594

Fix leak introduced in  b703dfbf17c0cff54181627fb40da92de3a52b79. (#12452)
Edition in commit b703dfbf17c0cf for compute_calls in libr/core/cmd_anal.c
is wrong, since it leads to memory leak.

If xrefs is null, then r_list_free (xrefs) does nothing;
else, xrefs is set to null in if branch True, then r_list_free dose nothing.
Therefore, call to r_list_free is useless.
However, we should free xrefs in if branch True.

Signed-off-by: Young Xiao <YangX92@hotmail.com>

libr/core/cmd_anal.c
@@ -6934,9 +6934,9 @@ static int compute_calls(RCore *core) {
 		xrefs = r_anal_fcn_get_xrefs (core->anal, fcn);
 		if (xrefs) {
 			cov += r_list_length (xrefs);
+			r_list_free (xrefs);
 			xrefs = NULL;
 		}
-		r_list_free (xrefs);
 	}
 	return cov;
 }