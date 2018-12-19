d2a62ea8bcbbd5c7a8f8cdbd76e386c348988075
Riccardo Schirone <sirmy15@gmail.com>
2018-12-19 10:42:46 +0100

    Fix armass: with r_str_ncpy, NULL char should be included

--- libr/asm/arch/arm/armass.c
+++ libr/asm/arch/arm/armass.c
@@ -665,7 +665,7 @@ static st32 getlistmask(char *input) {
 		for (i = 0; input[i] != ',' && input[i] != '\0'; i++) {
 			;
 		}
-		r_str_ncpy (temp, input, i);
+		r_str_ncpy (temp, input, i + 1);
 
 		input += i;
 		if (*input != '\0') {
