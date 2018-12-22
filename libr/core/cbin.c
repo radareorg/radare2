249899eec854696b63627e51aac8fc3f9e8f9d59
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2018-12-22 17:02:34 +0800

    ie/iee: remove laddr (#12539)

--- libr/core/cbin.c
+++ libr/core/cbin.c
@@ -1173,11 +1173,7 @@ static int bin_entry(RCore *r, int mode, ut64 laddr, int va, bool inifin) {
 			r_cons_printf ("s %s\n", name);
 			free (name);
 		} else {
-			r_cons_printf (
-				 "vaddr=0x%08"PFMT64x
-				" paddr=0x%08"PFMT64x
-				" laddr=0x%08"PFMT64x,
-				at, paddr, laddr);
+			r_cons_printf ("vaddr=0x%08"PFMT64x" paddr=0x%08"PFMT64x, at, paddr);
 			if (is_initfini (entry) && hvaddr != UT64_MAX) {
 				r_cons_printf (" hvaddr=0x%08"PFMT64x, hvaddr);
 			}
