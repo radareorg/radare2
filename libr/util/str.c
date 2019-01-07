38a4dd20639c027bc5132b874854191ff5f1d405
GustavoLCR <gugulcr@gmail.com>
2019-01-07 04:53:11 -0300

    Fix radare#12673 (#12675)

--- libr/util/str.c
+++ libr/util/str.c
@@ -2258,6 +2258,9 @@ R_API const char *r_str_casestr(const char *a, const char *b) {
 	// return strcasestr (a, b);
 	size_t hay_len = strlen (a);
 	size_t needle_len = strlen (b);
+	if (!hay_len || !needle_len) {
+		return NULL;
+	}
 	while (hay_len >= needle_len) {
 		if (!r_str_ncasecmp (a, b, needle_len)) {
 			return (const char *) a;
