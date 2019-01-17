4c3eaf0dc1a8378e9b46c2383b9b12aa21c7ee26
wargio <wargio@libero.it>
2019-01-17 16:51:52 +0100

    meson and makefile fixes

--- libr/parse/p/wasm_pseudo.mk
+++ libr/parse/p/wasm_pseudo.mk
@@ -2,12 +2,17 @@ OBJ_WASMPSEUDO+=parse_wasm_pseudo.o
 
 TARGET_WASMPSEUDO=parse_wasm_pseudo.${EXT_SO}
 STATIC_OBJ+=${OBJ_WASMPSEUDO}
+
 ifeq ($(CC),cccl)
 LIBDEPS=-L../../util -llibr_util
 LIBDEPS+=-L../../flag -llibr_flag
+LDFLAGS+=-L../../reg -llibr_reg
+LDFLAGS+=-L../../cons -llibr_cons
 else
 LIBDEPS=-L../../util -lr_util
 LIBDEPS+=-L../../flag -lr_flag
+LDFLAGS+=-L../../reg -lr_reg
+LDFLAGS+=-L../../cons -lr_cons
 endif
 
 ifeq ($(WITHPIC),1)
