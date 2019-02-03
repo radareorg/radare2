0d089f7f4b4b9615677a96071047c0e3abe7c86b
pancake <pancake@nopcode.org>
2019-02-03 17:14:36 +0100

    Use unsigned format in xref addresses

--- libr/core/cmd_anal.c
+++ libr/core/cmd_anal.c
@@ -4250,9 +4250,8 @@ static void showmem_json (RList *list, PJ *pj) {
 	if (!r_list_empty (list)) {
 		RListIter *iter;
 		AeaMemItem *item;
-
 		r_list_foreach (list, iter, item) {
-				pj_n (pj, item->addr);
+			pj_n (pj, item->addr);
 		}
 	}
 	
