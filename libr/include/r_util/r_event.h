98308a26174154d44f9da0cbb054eea0a48f6cc5
Florian Märkl <info@florianmaerkl.de>
2019-02-04 23:37:06 +0100

    Add user * to REvent Hooking and add all_callbacks for R_EVENT_ALL ##core

--- libr/include/r_util/r_event.h
+++ libr/include/r_util/r_event.h
@@ -8,12 +8,22 @@ extern "C" {
 #endif
 
 #include <sdb/ht_up.h>
+#include <r_vector.h>
 
 typedef struct r_event_t {
 	void *user;
 	bool incall;
 	HtUP *callbacks;
+	RVector *all_callbacks;
+	int next_handle;
 } REvent;
+
+typedef struct r_event_callback_handle_t {
+	int handle;
+	int type;
+} REventCallbackHandle;
+
+typedef void (*REventCallback)(REvent *ev, int type, void *user, void *data);
 
 typedef enum {
 	R_EVENT_ALL = 0,
@@ -29,12 +39,11 @@ typedef struct r_event_meta_t {
 	const char *string;
 } REventMeta;
 
-typedef void (*REventCallback)(REvent *ev, int type, void *user, void *data);
 
 R_API REvent *r_event_new(void *user);
 R_API void r_event_free(REvent *ev);
-R_API void r_event_hook(REvent *ev, int type, REventCallback cb);
-R_API void r_event_unhook(REvent *ev, int type, REventCallback cb);
+R_API REventCallbackHandle r_event_hook(REvent *ev, int type, REventCallback cb, void *user);
+R_API void r_event_unhook(REvent *ev, REventCallbackHandle handle);
 R_API void r_event_send(REvent *ev, int type, void *data);
 
 #ifdef __cplusplus
