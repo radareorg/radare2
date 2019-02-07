ecebfc4379a6fe2a1ce6b07dfcfecee90a14507c
Freakqency <surya.16cs@kct.ac.in>
2019-02-07 15:35:22 +0530

    cleaned up code

--- libr/bin/p/bin_mz.c
+++ libr/bin/p/bin_mz.c
@@ -58,20 +58,20 @@ static bool check_bytes(const ut8 *buf, ut64 length) {
 		}
 
 		// Check for New Executable, LE/LX or Phar Lap executable
-		if (!memcmp(buf, "MZ", 2) && 
-			!memcmp (buf + new_exe_header_offset, "NE", 2) ||
+		if (!memcmp (buf + new_exe_header_offset, "NE", 2) ||
+			!memcmp (buf + new_exe_header_offset, "LE", 2) ||
 			!memcmp (buf + new_exe_header_offset, "LX", 2) ||
 			!memcmp (buf + new_exe_header_offset, "PL", 2)) {
-            	return false;
+			return false;
 		}
 	}
-	
+
+	// Raw plain MZ executable (watcom)
 	if (!checkEntrypoint (buf, length)) {
 		return false;
 	}
-	
+
 	return true;
-	
 }
 
 static void *load(RBinFile *bf, RBuffer *buf, ut64 loadaddr, Sdb *sdb) {
