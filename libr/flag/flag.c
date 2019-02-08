a4e72de11b47630518098751121ae03759a1bc2f
Riccardo Schirone <sirmy15@gmail.com>
2019-02-08 10:48:53 +0100

    tmp com

--- libr/flag/flag.c
+++ libr/flag/flag.c
@@ -515,42 +515,8 @@ R_API RFlagItem *r_flag_get_i(RFlag *f, ut64 off) {
  *
  * XXX: this function is buggy and it's not really clear what's the purpose */
 R_API RFlagItem *r_flag_get_i2(RFlag *f, ut64 off) {
-	r_return_val_if_fail (f, NULL);
-	RFlagItem *oitem = NULL, *item = NULL;
-	RListIter *iter;
-	const RList *list = r_flag_get_list (f, off);
-	if (!list) {
-		return NULL;
-	}
-	r_list_foreach (list, iter, item) {
-		if (!item->name) {
-			continue;
-		}
-		/* catch sym. first */
-		if (!strncmp (item->name, "loc.", 4)) {
-			continue;
-		}
-		if (!strncmp (item->name, "fcn.", 4)) {
-			continue;
-		}
-		if (!strncmp (item->name, "section.", 8)) {
-			continue;
-		}
-		if (!strncmp (item->name, "section_end.", 12)) {
-			continue;
-		}
-		if (r_str_nlen (item->name, 5) > 4 &&
-		    item->name[3] == '.') {
-			oitem = item;
-			break;
-		}
-		oitem = item;
-		if (strlen (item->name) < 5 || item->name[3] != '.') {
-			continue;
-		}
-		oitem = item;
-	}
-	return oitem? evalFlag (f, oitem): NULL;
+	// FIXME: remove if possible
+	return r_flag_get_i (f, off);
 }
 
 static bool isFunctionFlag(const char *n) {
