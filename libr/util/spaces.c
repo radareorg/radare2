33385e5ad6ae4c238e0b2a12310851671beb8d77
Riccardo Schirone <sirmy15@gmail.com>
2019-02-14 10:50:10 +0100

    Do not use _free in a _init method, but free in a _new method

--- libr/util/spaces.c
+++ libr/util/spaces.c
@@ -5,6 +5,7 @@
 R_API RSpaces *r_spaces_new(const char *name) {
 	RSpaces *sp = R_NEW0 (RSpaces);
 	if (!sp || !r_spaces_init (sp, name)) {
+		free (sp);
 		return NULL;
 	}
 	return sp;
@@ -32,7 +33,7 @@ R_API bool r_spaces_init(RSpaces *sp, const char *name) {
 	return true;
 
 fail:
-	r_spaces_free (sp);
+	r_spaces_fini (sp);
 	return false;
 }
 
