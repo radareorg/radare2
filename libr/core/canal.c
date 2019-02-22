radareorg/radare2

commit 6e7e64f48b27b849d85e6f12a5d7ec228373910e

Additional fixes in agd output format
Fix rebase

Fix whitespace

libr/core/canal.c
@@ -1732,11 +1732,11 @@ R_API void r_core_anal_datarefs(RCore *core, ut64 addr) {
 			}
 		}
 		if (!found) {
-			eprintf("No data refs in this function.\n");
+			eprintf ("No data refs in this function.\n");
 		}
 		r_list_free (refs);
 	} else {
-		eprintf("Not in a function. Use 'df' to define it.\n");
+		eprintf ("Not in a function. Use 'df' to define it.\n");
 	}
 }
 

libr/anal/diff.c
@@ -225,7 +225,6 @@ R_API int r_anal_diff_fcn(RAnal *anal, RList *fcns, RList *fcns2) {
 		}
 */
 		if (fcn->diff->type != R_ANAL_DIFF_TYPE_NULL) {
-			eprintf ("Function %s already diffed\n", fcn->name);
 			continue;
 		}
 		ot = 0;

libr/core/cmd_anal.c
@@ -6092,15 +6092,19 @@ static void cmd_anal_graph(RCore *core, const char *input) {
 		case 'k':
 		case '*':
 		case ' ':
-			eprintf ("Currently the only supported formats for the diff graph are 'd' and 'w'\n");
-			break;
 		case 0:
-		case 'd':
-			r_core_anal_graph (core, r_num_math (core->num, input + 2), R_CORE_ANAL_GRAPHBODY | R_CORE_ANAL_GRAPHDIFF);
+			eprintf ("Currently the only supported formats for the diff graph are 'agdd' and 'agdw'\n");
 			break;
+		case 'd': {
+			ut64 addr = input[2]? r_num_math (core->num, input + 2): core->offset;
+			r_core_gdiff_fcn (core, addr, core->offset);
+			r_core_anal_graph (core, addr, R_CORE_ANAL_GRAPHBODY | R_CORE_ANAL_GRAPHDIFF);
+			break;
+			}
 		case 'w': {
-			char *cmdargs = r_str_newf ("agdd %lld", r_num_math (core->num, input + 2));
-			char *cmd = r_core_graph_cmd (cmdargs);
+			char *cmdargs = r_str_newf ("agdd %lld",
+				input[2] ? r_num_math (core->num, input + 2) : core->offset);
+			char *cmd = r_core_graph_cmd (core, cmdargs);
 			if (cmd && *cmd) {
 				r_core_cmd0 (core, cmd);
 			}

commit 65e0d2e49451cf3d0804a9062a7db3d971224da1

Fix NULL pointer dereference in data references analyzer
Data references analyzer assumes that each function reference maps
to a certain section, but this is not the case for raw binary files
like shellcodes/firmware images/etc.

This bug was introduced in #10117.

libr/core/canal.c
@@ -1847,7 +1847,7 @@ R_API void r_core_anal_datarefs(RCore *core, ut64 addr) {
 		r_list_foreach (refs, iter, ref) {
 			RBinObject *obj = r_bin_cur_object (core->bin);
 			RBinSection *binsec = r_bin_get_section_at (obj, ref->addr, true);
-			if (binsec->is_data) {
+			if (binsec && binsec->is_data) {
 				if (!found) {
 					r_cons_printf ("agn %s\n", me);
 					found = true;