20cee698e3d03c5485048198da3d867d1451ad35
radare <pancake@nopcode.org>
2019-03-04 15:23:54 +0100

    PoC: Implement aCef to emulate all calls in the function and add comments (#13266)
    This PoC requires more refinement to use the API and clean the code

--- libr/core/cmd_anal.c
+++ libr/core/cmd_anal.c
@@ -4556,7 +4556,7 @@ static int _aeli_iter(dictkv* kv, void* ud) {
 	return 0;
 }
 
-static void r_anal_aefa (RCore *core, const char *arg) {
+static void r_anal_aefa(RCore *core, const char *arg) {
 	ut64 to = r_num_math (core->num, arg);
 	ut64 at, from = core->offset;
 	RAnalFunction *fcn = r_anal_get_fcn_in (core->anal, to, -1);
@@ -8389,7 +8389,7 @@ static void cmd_anal_classes(RCore *core, const char *input) {
 	}
 }
 
-static void show_reg_args(RCore *core, int nargs) {
+static void show_reg_args(RCore *core, int nargs, RStrBuf *sb) {
 	int i;
 	char regname[8];
 	if (nargs < 0) {
@@ -8398,17 +8398,27 @@ static void show_reg_args(RCore *core, int nargs) {
 	for (i = 0; i < nargs; i++) {
 		snprintf (regname, sizeof (regname), "A%d", i);
 		ut64 v = r_reg_getv (core->anal->reg, regname);
-		r_cons_printf ("A%d 0x%08"PFMT64x"\n", i, v);
+		if (sb) {
+			r_strbuf_appendf (sb, "%s0x%08"PFMT64x, i?", ":"", v);
+		} else {
+			r_cons_printf ("A%d 0x%08"PFMT64x"\n", i, v);
+		}
 	}
 }
 
 // ripped from disasm.c: dupe code from there
 // TODO: Implement aC* and aCj
 static void cmd_anal_aC(RCore *core, const char *input) {
+	bool is_aCer = false;
 	RAnalFuncArg *arg;
 	RListIter *iter;
 	RListIter *nextele;
 	const char *iarg = strchr (input, ' ');
+	if (input[0] == 'e' && input[1] == 'f') { // "aCf"
+		// hacky :D
+		r_core_cmdf (core, ".aCe* $$ @@=`pdr~call`");
+		return;
+	}
 	if (iarg) {
 		iarg++;
 	}
@@ -8416,106 +8426,128 @@ static void cmd_anal_aC(RCore *core, const char *input) {
 		eprintf ("Usage: aC[e] [addr-of-call] # analyze call args (aCe does esil emulation with abte)\n");
 		return;
 	}
+	RStrBuf *sb = r_strbuf_new ("");
 	ut64 pcv = r_num_math (core->num, iarg);
 	if (input[0] == 'e') { // "aCe"
+		is_aCer = (input[1] == '*');
 		r_core_cmdf (core, ".abte 0x%08"PFMT64x, pcv);
 	}
 	RAnalOp* op = r_core_anal_op (core, pcv, -1);
 	if (!op) {
 		return;
 	}
+bool go_on = true;
 	if (op->type != R_ANAL_OP_TYPE_CALL) {
-		show_reg_args (core, -1);
-		return;
+		show_reg_args (core, -1, sb);
+		go_on = false;
 	}
 	const char *fcn_name = NULL;
-	RAnalFunction *fcn = r_anal_get_fcn_at (core->anal, pcv, 0);
-	if (fcn) {
-		fcn_name = fcn->name;
-	} else {
-		RFlagItem *item = r_flag_get_i (core->flags, op->jump);
-		if (item) {
-			fcn_name = item->name;
+	RAnalFunction *fcn;
+	if (go_on) {
+		fcn = r_anal_get_fcn_at (core->anal, pcv, 0);
+		if (fcn) {
+			fcn_name = fcn->name;
+		} else {
+			RFlagItem *item = r_flag_get_i (core->flags, op->jump);
+			if (item) {
+				fcn_name = item->name;
+			}
 		}
-	}
-	char *key = (fcn_name)? resolve_fcn_name (core->anal, fcn_name): NULL;
-	if (key) {
-		const char *fcn_type = r_type_func_ret (core->anal->sdb_types, key);
-		int nargs = r_type_func_args_count (core->anal->sdb_types, key);
-		// remove other comments
-		if (fcn_type) {
-			r_cons_printf ("%s%s%s(", r_str_get (fcn_type),
-					(*fcn_type && fcn_type[strlen (fcn_type) - 1] == '*') ? "" : " ",
-					r_str_get (key));
-			if (!nargs) {
-				r_cons_printf ("void)\n");
+		char *key = (fcn_name)? resolve_fcn_name (core->anal, fcn_name): NULL;
+		if (key) {
+			const char *fcn_type = r_type_func_ret (core->anal->sdb_types, key);
+			int nargs = r_type_func_args_count (core->anal->sdb_types, key);
+			// remove other comments
+			if (fcn_type) {
+				r_strbuf_appendf (sb, "%s%s%s(", r_str_get (fcn_type),
+						(*fcn_type && fcn_type[strlen (fcn_type) - 1] == '*') ? "" : " ",
+						r_str_get (key));
+				if (!nargs) {
+					r_strbuf_appendf (sb, "void)\n");
+				}
+			} else {
+				eprintf ("Cannot find any function type..lets just use some standards?\n");
 			}
 		} else {
-			eprintf ("Cannot find any function type..lets just use some standards?\n");
-		}
-	} else {
-		show_reg_args (core, -1);
-		return;
-	}
-	ut64 s_width = (core->anal->bits == 64)? 8: 4;
-	const char *sp = r_reg_get_name (core->anal->reg, R_REG_NAME_SP);
-	ut64 spv = r_reg_getv (core->anal->reg, sp);
-	r_reg_setv (core->anal->reg, sp, spv + s_width); // temporarily set stack ptr to sync with carg.c
-	RList *list = r_core_get_func_args (core, fcn_name);
-	// show_reg_args (core, -1);
-	if (!r_list_empty (list)) {
-#if 0
-		bool warning = false;
-		bool on_stack = false;
-		r_list_foreach (list, iter, arg) {
-			if (r_str_startswith (arg->cc_source, "stack")) {
-				on_stack = true;
-			}
-			if (!arg->size) {
-				r_cons_printf ("%s: unk_size", arg->c_type);
-				warning = true;
-			}
-#endif
-		r_list_foreach (list, iter, arg) {
-			nextele = r_list_iter_get_next (iter);
-			if (!arg->fmt) {
-				r_cons_printf ("?%s", nextele? ", ": "");
+			if (is_aCer) {
+				show_reg_args (core, -1, sb);
+				go_on = true;
 			} else {
-				// print_fcn_arg (core, arg->orig_c_type, arg->name, arg->fmt, arg->src, on_stack, 0);
-				// const char *fmt = arg->orig_c_type;
-				ut64 addr = arg->src;
-				char *res = r_core_cmd_strf (core, "pfq %s @ 0x%08" PFMT64x, arg->fmt, addr);
-				// r_cons_printf ("pfq *%s @ 0x%08" PFMT64x"\n", arg->fmt, addr);
-				r_str_trim (res);
-				r_cons_printf ("%s", res);
-				free (res);
+				show_reg_args (core, -1, NULL);
+				go_on = false;
 			}
-		}
-		r_cons_printf (")");
-	} else {
-		// function name not resolved
-		int i, nargs = 4; // DEFAULT_NARGS;
-		if (fcn) {
-			// @TODO: fcn->nargs should be updated somewhere and used here instead
-			nargs = r_anal_var_count (core->anal, fcn, 's', 1) +
-				r_anal_var_count (core->anal, fcn, 'b', 1) +
-				r_anal_var_count (core->anal, fcn, 'r', 1);
-		}
-		if (nargs > 0) {
-			if (fcn_name) {
-				r_cons_printf ("; %s(", fcn_name);
-			} else {
-				r_cons_printf ("; 0x%"PFMT64x"(", pcv);
-			}
-			for (i = 0; i < nargs; i++) {
-				ut64 v = r_debug_arg_get (core->dbg, R_ANAL_CC_TYPE_FASTCALL, i);
-				r_cons_printf ("%s0x%"PFMT64x, i?", ":"", v);
-			}
-			r_cons_printf (")");
 		}
 	}
-	r_reg_setv (core->anal->reg, sp, spv); // reset stack ptr
-	r_cons_newline ();
+	if (go_on) {
+		ut64 s_width = (core->anal->bits == 64)? 8: 4;
+		const char *sp = r_reg_get_name (core->anal->reg, R_REG_NAME_SP);
+		ut64 spv = r_reg_getv (core->anal->reg, sp);
+		r_reg_setv (core->anal->reg, sp, spv + s_width); // temporarily set stack ptr to sync with carg.c
+		RList *list = r_core_get_func_args (core, fcn_name);
+		if (!r_list_empty (list)) {
+	#if 0
+			bool warning = false;
+			bool on_stack = false;
+			r_list_foreach (list, iter, arg) {
+				if (r_str_startswith (arg->cc_source, "stack")) {
+					on_stack = true;
+				}
+				if (!arg->size) {
+					r_cons_printf ("%s: unk_size", arg->c_type);
+					warning = true;
+				}
+	#endif
+			r_list_foreach (list, iter, arg) {
+				nextele = r_list_iter_get_next (iter);
+				if (!arg->fmt) {
+					r_strbuf_appendf (sb, "?%s", nextele? ", ": "");
+				} else {
+					// print_fcn_arg (core, arg->orig_c_type, arg->name, arg->fmt, arg->src, on_stack, 0);
+					// const char *fmt = arg->orig_c_type;
+					ut64 addr = arg->src;
+					char *res = r_core_cmd_strf (core, "pfq %s @ 0x%08" PFMT64x, arg->fmt, addr);
+					// r_cons_printf ("pfq *%s @ 0x%08" PFMT64x"\n", arg->fmt, addr);
+					r_str_trim (res);
+					r_strbuf_appendf (sb, "%s", res);
+					free (res);
+				}
+			}
+			r_strbuf_appendf (sb, ")");
+		} else {
+			// function name not resolved
+			int i, nargs = 4; // DEFAULT_NARGS;
+			if (fcn) {
+				// @TODO: fcn->nargs should be updated somewhere and used here instead
+				nargs = r_anal_var_count (core->anal, fcn, 's', 1) +
+					r_anal_var_count (core->anal, fcn, 'b', 1) +
+					r_anal_var_count (core->anal, fcn, 'r', 1);
+			}
+			if (nargs > 0) {
+				if (fcn_name) {
+					r_strbuf_appendf (sb, "; %s(", fcn_name);
+				} else {
+					r_strbuf_appendf (sb, "; 0x%"PFMT64x"(", pcv);
+				}
+				for (i = 0; i < nargs; i++) {
+					ut64 v = r_debug_arg_get (core->dbg, R_ANAL_CC_TYPE_FASTCALL, i);
+					r_strbuf_appendf (sb, "%s0x%"PFMT64x, i?", ":"", v);
+				}
+				r_strbuf_appendf (sb, ")");
+			}
+		}
+		r_reg_setv (core->anal->reg, sp, spv); // reset stack ptr
+	}
+	char *s = r_strbuf_drain (sb);
+	if (is_aCer) {
+		char *u = r_base64_encode_dyn (s, -1);
+		if (u) {
+			r_cons_printf ("CCu base64:%s\n", u);
+			free (u);
+		}
+	} else {
+		r_cons_printf ("%s\n", s);
+	}
+	free (s);
 }
 
 static int cmd_anal(void *data, const char *input) {
