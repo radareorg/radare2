264f4585c7440ee223626222b71934f73e1a9836
Riccardo Schirone <ret2libc@users.noreply.github.com>
2019-03-06 11:13:26 +0100

    Add symbols.objects and symbols.sections flag spaces ##flags

--- libr/include/r_core.h
+++ libr/include/r_core.h
@@ -55,6 +55,8 @@ R_LIB_VERSION_HEADER(r_core);
 #define R_FLAGS_FS_SIGNS "sign"
 #define R_FLAGS_FS_STRINGS "strings"
 #define R_FLAGS_FS_SYMBOLS "symbols"
+#define R_FLAGS_FS_SYMBOLS_OBJECTS "symbols.objects"
+#define R_FLAGS_FS_SYMBOLS_SECTIONS "symbols.sections"
 #define R_FLAGS_FS_SYSCALLS "syscalls"
 
 #define R_GRAPH_FORMAT_NO           0
