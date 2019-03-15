radareorg/radare2

commit 81ad0fe809a45e428abdcb37db7bf83ef09ad26e

Fix #13234 - Allow reopening of webserver ##sync
* Fix hang after ctrl-c

libr/socket/socket.c
@@ -110,7 +110,7 @@ WSACleanup: closes all network connections
 #endif
 #define BUFFER_SIZE 4096
 
-R_API bool r_socket_is_connected (RSocket *s) {
+R_API bool r_socket_is_connected(RSocket *s) {
 #if __WINDOWS__ && !defined(__CYGWIN__) //&& !defined(__MINGW64__)
 	char buf[2];
 	r_socket_block_time (s, 0, 0);
@@ -183,7 +183,7 @@ R_API int r_socket_unix_listen (RSocket *s, const char *file) {
 }
 #endif
 
-R_API RSocket *r_socket_new (bool is_ssl) {
+R_API RSocket *r_socket_new(bool is_ssl) {
 	RSocket *s = R_NEW0 (RSocket);
 	if (!s) {
 		return NULL;
@@ -216,7 +216,7 @@ R_API RSocket *r_socket_new (bool is_ssl) {
 	return s;
 }
 
-R_API bool r_socket_spawn (RSocket *s, const char *cmd, unsigned int timeout) {
+R_API bool r_socket_spawn(RSocket *s, const char *cmd, unsigned int timeout) {
 	// XXX TODO: dont use sockets, we can achieve the same with pipes
 	const int port = 2000 + r_num_rand (2000);
 	int childPid = r_sys_fork ();
@@ -265,7 +265,7 @@ R_API bool r_socket_spawn (RSocket *s, const char *cmd, unsigned int timeout) {
 	return true;
 }
 
-R_API bool r_socket_connect (RSocket *s, const char *host, const char *port, int proto, unsigned int timeout) {
+R_API bool r_socket_connect(RSocket *s, const char *host, const char *port, int proto, unsigned int timeout) {
 #if __WINDOWS__ && !defined(__CYGWIN__) //&& !defined(__MINGW64__)
 	struct sockaddr_in sa;
 	struct hostent *he;
@@ -318,6 +318,9 @@ R_API bool r_socket_connect (RSocket *s, const char *host, const char *port, int
 	iResult = ioctlsocket (s->fd, FIONBIO, &iMode);
 	if (iResult != NO_ERROR) {
 		eprintf ("ioctlsocket error: %d\n", iResult);
+	}
+	if (timeout > 0) {
+		r_socket_block_time (s, 1, timeout);
 	}
 	fd_set Write, Err;
 	FD_ZERO (&Write);
@@ -429,7 +432,7 @@ R_API bool r_socket_connect (RSocket *s, const char *host, const char *port, int
 }
 
 /* close the file descriptor associated with the RSocket s */
-R_API int r_socket_close_fd (RSocket *s) {
+R_API int r_socket_close_fd(RSocket *s) {
 #ifdef _MSC_VER
 	return s->fd != INVALID_SOCKET ? closesocket (s->fd) : false;
 #else
@@ -438,7 +441,7 @@ R_API int r_socket_close_fd (RSocket *s) {
 }
 
 /* shutdown the socket and close the file descriptor */
-R_API int r_socket_close (RSocket *s) {
+R_API int r_socket_close(RSocket *s) {
 	int ret = false;
 	if (!s) {
 		return false;
@@ -450,10 +453,12 @@ R_API int r_socket_close (RSocket *s) {
 #if __WINDOWS__ && !defined(__CYGWIN__) //&& !defined(__MINGW64__)
 		// https://msdn.microsoft.com/en-us/library/windows/desktop/ms740481(v=vs.85).aspx
 		shutdown (s->fd, SD_SEND);
-		do {
-			char buf = 0;
-			ret = recv (s->fd, &buf, 1, 0);
-		} while (ret != 0 && ret != SOCKET_ERROR);
+		if (r_socket_ready (s, 0, 250)) {
+			do {
+				char buf = 0;
+				ret = recv (s->fd, &buf, 1, 0);
+			} while (ret != 0 && ret != SOCKET_ERROR);
+		}
 		ret = closesocket (s->fd);
 #else
 		ret = close (s->fd);
@@ -469,7 +474,7 @@ R_API int r_socket_close (RSocket *s) {
 }
 
 /* shutdown the socket, close the file descriptor and free the RSocket */
-R_API int r_socket_free (RSocket *s) {
+R_API int r_socket_free(RSocket *s) {
 	int res = r_socket_close (s);
 #if HAVE_LIB_SSL
 	if (s && s->is_ssl) {
@@ -490,12 +495,11 @@ R_API int r_socket_port_by_name(const char *name) {
 	return (p && p->s_port) ? ntohs (p->s_port) : r_num_get (NULL, name);
 }
 
-R_API bool r_socket_listen (RSocket *s, const char *port, const char *certfile) {
-#if __UNIX__ || defined(__CYGWIN__)
+R_API bool r_socket_listen(RSocket *s, const char *port, const char *certfile) {
 	int optval = 1;
 	int ret;
 	struct linger linger = { 0 };
-#endif
+
 	if (r_sandbox_enable (0)) {
 		return false;
 	}
@@ -509,7 +513,7 @@ R_API bool r_socket_listen (RSocket *s, const char *port, const char *certfile) 
 	if ((s->fd = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
 		return false;
 	}
-#if __UNIX__ || defined(__CYGWIN__)
+
 	linger.l_onoff = 1;
 	linger.l_linger = 1;
 	ret = setsockopt (s->fd, SOL_SOCKET, SO_LINGER, (void*)&linger, sizeof (linger));
@@ -527,7 +531,7 @@ R_API bool r_socket_listen (RSocket *s, const char *port, const char *certfile) 
 	if (ret < 0) {
 		return false;
 	}
-#endif
+
 	memset (&s->sa, 0, sizeof (s->sa));
 	s->sa.sin_family = AF_INET;
 	s->sa.sin_addr.s_addr = htonl (s->local? INADDR_LOOPBACK: INADDR_ANY);
@@ -645,7 +649,7 @@ R_API RSocket *r_socket_accept_timeout(RSocket *s, unsigned int timeout) {
 	return NULL;
 }
 
-R_API int r_socket_block_time (RSocket *s, int block, int sec) {
+R_API int r_socket_block_time(RSocket *s, int block, int sec) {
 #if __UNIX__ || defined(__CYGWIN__)
 	int ret, flags;
 #endif
@@ -664,8 +668,6 @@ R_API int r_socket_block_time (RSocket *s, int block, int sec) {
 		return false;
 	}
 #elif __WINDOWS__ && !defined(__CYGWIN__) //&& !defined(__MINGW64__)
-	// HACK: nonblocking io on w32 behaves strange
-	return true;
 	ioctlsocket (s->fd, FIONBIO, (u_long FAR*)&block);
 #endif
 	if (sec > 0) {
@@ -702,8 +704,6 @@ R_API int r_socket_ready(RSocket *s, int secs, int usecs) {
 	fds[0].revents = POLLNVAL | POLLHUP | POLLERR;
 	return poll ((struct pollfd *)&fds, 1, msecs);
 #elif __WINDOWS__ && !defined(__CYGWIN__) //&& !defined(__MINGW64__)
-	return 1;
-#if XXX_THIS_IS_NOT_WORKING_WELL
 	fd_set rfds;
 	struct timeval tv;
 	if (s->fd == -1) {
@@ -713,11 +713,7 @@ R_API int r_socket_ready(RSocket *s, int secs, int usecs) {
 	FD_SET (s->fd, &rfds);
 	tv.tv_sec = secs;
 	tv.tv_usec = usecs;
-	if (select (s->fd + 1, &rfds, NULL, NULL, &tv) == -1) {
-		return -1;
-	}
-	return FD_ISSET (0, &rfds);
-#endif
+	return select (s->fd + 1, &rfds, NULL, NULL, &tv);
 #else
 	return true; /* always ready if unknown */
 #endif
@@ -802,6 +798,9 @@ R_API int r_socket_read(RSocket *s, unsigned char *buf, int len) {
 	if (!s) {
 		return -1;
 	}
+	if (r_socket_ready (s, 2, 0) <= 0) {
+		return -1;
+	}
 #if HAVE_LIB_SSL
 	if (s->is_ssl) {
 		if (s->bio) {
@@ -814,9 +813,6 @@ R_API int r_socket_read(RSocket *s, unsigned char *buf, int len) {
 rep:
 	{
 	int ret = recv (s->fd, (void *)buf, len, 0);
-	//if (ret != len)
-	//	return 0;
-	//r_sys_perror ("recv");
 	if (ret == -1) goto rep;
 	return ret;
 	}

libr/core/rtr.c
@@ -445,6 +445,7 @@ static int r_core_rtr_http_run(RCore *core, int launch, int browse, const char *
 	}
 	if (path && atoi (path)) {
 		port = path;
+		r_config_set (core->config, "http.port", port);
 		path = NULL;
 	} else {
 		if (core->file && (!path || !*path)) {

libr/socket/http_server.c
@@ -32,8 +32,10 @@ R_API RSocketHTTPRequest *r_socket_http_accept (RSocket *s, RSocketHTTPOptions *
 	hr->auth = !so->httpauth;
 	for (;;) {
 #if __WINDOWS__
-		if (breaked)
-			break;
+		if (breaked && *breaked) {
+			r_socket_http_close (hr);
+			return NULL;
+		}
 #endif
 		memset (buf, 0, sizeof (buf));
 		xx = r_socket_gets (hr->s, buf, sizeof (buf));

commit c80dbc6e9abc9874d4f2ab635f650e653cb968e1

Fix http webserver regression introduced in 81ad0fe809a45e428abdcb37db7bf83ef09ad26e

libr/socket/socket.c
@@ -798,9 +798,6 @@ R_API int r_socket_read(RSocket *s, unsigned char *buf, int len) {
 	if (!s) {
 		return -1;
 	}
-	if (r_socket_ready (s, 2, 0) <= 0) {
-		return -1;
-	}
 #if HAVE_LIB_SSL
 	if (s->is_ssl) {
 		if (s->bio) {