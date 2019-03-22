059b335bb22f9eaf98a5479200572494a1aea8d4
pancake <pancake@nopcode.org>
2019-03-22 01:14:26 +0100

    Fix invalid json regression for compilerr

--- libr/core/cbin.c
+++ libr/core/cbin.c
@@ -765,8 +765,8 @@ static int bin_info(RCore *r, int mode, ut64 laddr) {
 		}
 		pair_bool ("stripped", R_BIN_DBG_STRIPPED & info->dbg_info, mode, false);
 		pair_str ("subsys", info->subsystem, mode, false);
-		pair_bool ("va", info->has_va, mode, true);
 		pair_str ("compiler", info->compiler, mode, false);
+		pair_bool ("va", info->has_va, mode, true);
 		if (IS_MODE_JSON (mode)) {
 			r_cons_printf (",\"checksums\":{");
 			for (i = 0; info->sum[i].type; i++) {
