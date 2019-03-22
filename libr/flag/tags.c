4000e7bf72249bc8a1d82a6d2f28a57d514ad01a
a1ext <a13x4nd3r.t@gmail.com>
2019-03-22 06:10:00 +0300

    Fix `ft` command crash on Windows (#13457)

--- libr/flag/tags.c
+++ libr/flag/tags.c
@@ -53,11 +53,14 @@ static bool iter_glob_flag(RFlagItem *fi, void *user) {
 R_API RList *r_flag_tags_get(RFlag *f, const char *name) {
 	r_return_val_if_fail (f && name, NULL);
 	const char *k = sdb_fmt ("tag.%s", name);
-	char *words = sdb_get (f->tags, k, NULL);
 	RList *res = r_list_newf (NULL);
-	RList *list = r_str_split_list (words, " ");
-	struct iter_glob_flag_t u = { .res = res, .words = list };
-	r_flag_foreach (f, iter_glob_flag, &u);
-	r_list_free (list);
+	char *words = sdb_get (f->tags, k, NULL);
+	if (words) {
+		RList *list = r_str_split_list (words, " ");
+		struct iter_glob_flag_t u = { .res = res, .words = list };
+		r_flag_foreach (f, iter_glob_flag, &u);
+		r_list_free (list);
+		free (words);
+	}
 	return res;
 }
