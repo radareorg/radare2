e2ab4ef2ceab8aad3f16e1915e28f1b66d5faa5f
Francesco Tamagni <mrmacete@protonmail.ch>
2019-03-27 12:18:45 +0100

    Use entrypoint from LC_UNIXTHREAD for the Kernelcache ##bin

--- libr/bin/p/bin_xnu_kernelcache.c
+++ libr/bin/p/bin_xnu_kernelcache.c
@@ -723,6 +723,15 @@ static RList *entries(RBinFile *bf) {
 	}
 
 	RKernelCacheObj *kobj = (RKernelCacheObj*) obj->bin_obj;
+	ut64 entry_vaddr = kobj->mach0->entry;
+	if (kobj->pa2va_exec <= entry_vaddr) {
+		ut64 entry_paddr = entry_vaddr - kobj->pa2va_exec;
+		RBinAddr *ba = newEntry (entry_paddr, entry_vaddr, 0);
+		if (ba) {
+			r_list_append (ret, ba);
+		}
+	}
+
 	process_constructors (kobj, kobj->mach0, ret, 0, true, R_K_CONSTRUCTOR_TO_ENTRY, NULL);
 
 	return ret;
