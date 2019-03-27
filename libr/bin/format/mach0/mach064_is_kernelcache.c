a893a0f26212620be6dee192ad12f76dbdffa009
pancake <pancake@nopcode.org>
2019-03-27 11:52:56 +0100

    Fix build

--- libr/bin/format/mach0/mach064_is_kernelcache.c
+++ libr/bin/format/mach0/mach064_is_kernelcache.c
@@ -5,11 +5,11 @@ static bool is_kernelcache(const ut8 *buf, ut64 length) {
 
 	const ut8 *end = buf + length;
 	const ut8 *cursor = buf + sizeof (struct MACH0_(mach_header));
-	int ncmds = r_read_le32 (buf + 16);
+	int i, ncmds = r_read_le32 (buf + 16);
 	bool has_unixthread = false;
 	bool has_negative_vaddr = false;
 
-	for (int i = 0; i < ncmds; i++) {
+	for (i = 0; i < ncmds; i++) {
 		if (cursor >= end) {
 			return false;
 		}
@@ -25,16 +25,17 @@ static bool is_kernelcache(const ut8 *buf, ut64 length) {
 		case LC_LOAD_WEAK_DYLIB:
 		case LC_LAZY_LOAD_DYLIB:
 			return false;
-		case LC_SEGMENT_64: {
-			if (has_negative_vaddr) {
-				break;
-			}
-			st64 vmaddr = r_read_le64 (cursor + 24);
-			if (vmaddr < 0) {
-				has_negative_vaddr = true;
+		case LC_SEGMENT_64:
+			{
+				if (has_negative_vaddr) {
+					break;
+				}
+				st64 vmaddr = r_read_le64 (cursor + 24);
+				if (vmaddr < 0) {
+					has_negative_vaddr = true;
+				}
 			}
 			break;
-		}
 		}
 
 		cursor += cmdsize;
