d6d982d791d4fa4a6752cc1679e74f73188fd859
pancake <pancake@nopcode.org>
2019-03-29 11:41:03 +0100

    Blind guess

--- libr/core/cmd_meta.c
+++ libr/core/cmd_meta.c
@@ -734,11 +734,11 @@ static int cmd_meta_others(RCore *core, const char *input) {
 			if (!rep) {
 				rep = strchr (input + len, ' ');
 			}
-#if 0
-			if (rep) {
-				repeat = r_num_math (core->num, rep + 1);
+			if (*input == 'd') {
+				if (rep) {
+					repeat = r_num_math (core->num, rep + 1);
+				}
 			}
-#endif
 		}
 		int repcnt = 0;
 		if (repeat < 1) {
