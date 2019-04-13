b73b76ae8ab2c515e54d81965bcc50bc84d5161d
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2019-04-13 21:33:39 +0800

    Add value and label (if available) to pfj enum (#13728)
    * Fix pfj enum when label is not available

--- libr/util/format.c
+++ libr/util/format.c
@@ -1333,14 +1333,15 @@ static void r_print_format_enum(const RPrint* p, ut64 seeki, char* fmtname,
 		if (mode & R_PRINT_DOT) {
 			p->cb_printf ("%s.%s", fmtname, enumvalue);
 		} else if (MUSTSEEJSON) {
-			p->cb_printf ("\"%s\"}", fmtname);
+			p->cb_printf ("%"PFMT64d",\"label\":\"%s\",\"enum\":\"%s\"}",
+				addr, enumvalue, fmtname);
 		} else if (MUSTSEE) {
 			p->cb_printf ("%s (enum %s) = 0x%"PFMT64x" ; %s\n",
 				fieldname, fmtname, addr, enumvalue);
 		}
 	} else {
 		if (MUSTSEEJSON) {
-			p->cb_printf ("\"`te %s 0x%x`\"}", fmtname, addr);
+			p->cb_printf ("%"PFMT64d",\"enum\":\"%s\"}", addr, fmtname);
 		} else if (MUSTSEE) {
 			p->cb_printf ("%s (enum %s) = 0x%x\n",//`te %s 0x%x`\n",
 				fieldname, fmtname, addr); //enumvalue); //fmtname, addr);
