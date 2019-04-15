c3a39ed7b45d6b8ffccd28eaba391140518a1543
deepakchethan <deepakchethan@outlook.com>
2019-04-15 23:58:14 +0530

    Fixes #13713: rotate call and jmp hints seperately

--- libr/core/cconfig.c
+++ libr/core/cconfig.c
@@ -2760,6 +2760,7 @@ R_API int r_core_config_init(RCore *core) {
 	SETPREF ("asm.jmpsub", "true", "Always substitute jump, call and branch targets in disassembly");
 	SETPREF ("asm.hints", "true", "Disable all asm.hint* if false");
 	SETPREF ("asm.hint.jmp", "true", "Show jump hints [numbers] in disasm");
+	SETPREF ("asm.hint.call", "false", "Show call hints [numbers] in disarm");
 	SETPREF ("asm.hint.lea", "false", "Show LEA hints [numbers] in disasm");
 	SETPREF ("asm.hint.cdiv", "false", "Show CDIV hints optimization hint");
 	SETI ("asm.hint.pos", 1, "Shortcut hint position (-1, 0, 1)");
