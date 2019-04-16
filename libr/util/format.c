ded8ed75bb0cfbc26db4971a2e6a3c2b364a579a
Óscar <oxcabe@gmail.com>
2019-04-17 00:56:59 +0100

    Use normalized types for pfc output

--- libr/util/format.c
+++ libr/util/format.c
@@ -1780,41 +1780,33 @@ static char *get_format_type(const char fmt) {
 	switch (fmt) {
 	case 'b':
 	case 'C':
-		type = strdup ("unsigned char");
+		type = strdup ("uint8_t");
 		break;
 	case 'c':
-		type = strdup ("char");
+		type = strdup ("int8_t");
 		break;
 	case 'd':
-		type = strdup ("unsigned int");
-		break;
 	case 'f':
-		type = strdup ("float");
-		break;
-	case 'F':
-		type = strdup ("double");
-		break;
 	case 'i':
 	case 'o':
-		type = strdup ("int");
+	case 'x':
+		type = strdup ("int32_t");
+		break;
+	case 'F':
+	case 'q':
+		type = strdup ("uint64_t");
 		break;
 	case 'p':
 		type = strdup ("void*");
-		break;
-	case 'q':
-		type = strdup ("long long");
 		break;
 	case 'u':
 		type = strdup ("uleb128_t");
 		break;
 	case 'w':
-		type = strdup ("unsigned short");
-		break;
-	case 'x':
-		type = strdup ("long");
+		type = strdup ("uint16_t");
 		break;
 	case 'X':
-		type = strdup ("unsigned char[]");
+		type = strdup ("uint8_t[]");
 		break;
 	case 'D':
 	case 's':
