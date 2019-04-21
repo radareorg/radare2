081fb79f1401deaefd4995d79d23fa890d157f8a
deepakchethan <deepakchethan@outlook.com>
2019-04-21 18:11:53 +0530

    Assume filetype is unknown if core->bin->cur is null

--- libr/core/cmd_anal.c
+++ libr/core/cmd_anal.c
@@ -7856,7 +7856,7 @@ static bool is_unknown_file(RCore *core) {
 	if (core->bin->cur && core->bin->cur->o) {
 		return (r_list_empty (core->bin->cur->o->sections));
 	}
-	return false;
+	return true;
 }
 
 static int cmd_anal_all(RCore *core, const char *input) {
