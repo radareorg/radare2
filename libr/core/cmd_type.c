c625582181ec1abb0a9563ea29a45268cf5a5c06
Khairul Kasmiran <kazarmy@gmail.com>
2019-04-29 19:18:10 +0800

    Add help msg for tn-

--- libr/core/cmd_type.c
+++ libr/core/cmd_type.c
@@ -136,7 +136,8 @@ static const char *help_msg_tn[] = {
 	"Usage:", "tn [-][0xaddr|symname]", " manage no-return marks",
 	"tn[a]", " 0x3000", "stop function analysis if call/jmp to this address",
 	"tn[n]", " sym.imp.exit", "same as above but for flag/fcn names",
-	"tn", "-*", "remove all no-return references",
+	"tn-", " 0x3000 sym.imp.exit ...", "remove some no-return references",
+	"tn-*", "", "remove all no-return references",
 	"tn", "", "list them all",
 	NULL
 };
