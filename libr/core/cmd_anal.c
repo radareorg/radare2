093716fe45d21f27320746bc72c35d254d963d19
Tilman Sauerbeck <tilman@code-monkey.de>
2019-05-13 09:54:26 +0200

    afu: improve usage text and argument handling (#14044)
    * Tweak documentation for afu to indicate its argument is mandatory.
    * Detect missing argument in afu: Error out if afu is called without its mandatory argument.

--- libr/core/cmd_anal.c
+++ libr/core/cmd_anal.c
@@ -293,7 +293,7 @@ static const char *help_msg_af[] = {
 	"afs", "[!] ([fcnsign])", "get/set function signature at current address (afs! uses cfg.editor)",
 	"afS", "[stack_size]", "set stack frame size for function at current address",
 	"aft", "[?]", "type matching, type propagation",
-	"afu", " [addr]", "resize and analyze function from current address until addr",
+	"afu", " addr", "resize and analyze function from current address until addr",
 	"afv[bsra]", "?", "manipulate args, registers and variables in function",
 	"afx", "", "list function references",
 	NULL
@@ -2491,6 +2491,11 @@ static int cmd_anal_fcn(RCore *core, const char *input) {
 		break;
 	case 'u': // "afu"
 		{
+		if (input[2] != ' ') {
+			eprintf ("Missing argument\n");
+			return false;
+		}
+
 		ut64 addr = core->offset;
 		ut64 addr_end = r_num_math (core->num, input + 2);
 		if (addr_end < addr) {
