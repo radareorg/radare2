de8a39dd5426b9a65a880f046ece1ea617889701
radare <pancake@nopcode.org>
2019-05-14 03:31:04 +0200

    Make r_reg_get O(1). aae is 2x faster! ##optimization (#14061)

--- libr/include/r_reg.h
+++ libr/include/r_reg.h
@@ -106,6 +106,7 @@ typedef struct r_reg_set_t {
 	RRegArena *arena;
 	RList *pool;      /* RRegArena */
 	RList *regs;      /* RRegItem */
+	HtPP *ht_regs;    /* name:RRegItem */
 	RListIter *cur;
 	int maskregstype; /* which type of regs have this reg set (logic mask with RRegisterType  R_REG_TYPE_XXX) */
 } RRegSet;
