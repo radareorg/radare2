f6fb9cb4e5300e10e40ab0210f768486ee21da88
MapleLeaf-X <MapleLeaf-X@users.noreply.github.com>
2019-05-18 01:03:55 +0300

    [WIP] unbreaking msvc build

--- libr/debug/p/debug_native.c
+++ libr/debug/p/debug_native.c
@@ -34,7 +34,7 @@ static int r_debug_native_reg_write (RDebug *dbg, int type, const ut8* buf, int 
 #include <windows.h>
 #include "native/windows/windows_debug.h"
 #define R_DEBUG_REG_T CONTEXT
-// #include "native/w32.c" // to be removed!
+#include "native/w32.c" // to be removed!
 #ifdef NTSTATUS
 #undef NTSTATUS
 #endif
