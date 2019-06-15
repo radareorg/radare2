76509942016ae682ac068eccfdddc6d8f1660401
bart1e <bartolwodzinski@op.pl>
2019-06-15 12:07:44 +0200

    One more indentation fix

--- libr/core/cmd_debug.c
+++ libr/core/cmd_debug.c
@@ -4571,8 +4571,8 @@ static int cmd_debug(void *data, const char *input) {
 				//r_core_cmd0 (core, "pd 1 @@= `dtq`");
 				r_list_foreach (core->dbg->trace->traces, iter, trace) {
 					op = r_core_anal_op (core, trace->addr, R_ANAL_OP_MASK_BASIC | R_ANAL_OP_MASK_DISASM);
-						r_cons_printf ("0x%08"PFMT64x" %s\n", trace->addr, op->mnemonic);
-					}
+					r_cons_printf ("0x%08"PFMT64x" %s\n", trace->addr, op->mnemonic);
+				}
 			}
 			break;
 		case 'g': // "dtg"
