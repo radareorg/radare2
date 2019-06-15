b282620b7a8818910c42a29b8f0855a2d13eec14
radare <pancake@nopcode.org>
2019-06-15 13:24:00 +0200

    Fix #14303 - oob crash in RParse api usage, needs API redesign (#14307)

--- libr/parse/parse.c
+++ libr/parse/parse.c
@@ -91,9 +91,10 @@ R_API int r_parse_assemble(RParse *p, char *data, char *str) {
 	return ret;
 }
 
-// parse 'data' and generate pseudocode disassemble in 'str'
+// data is input disasm, str is output pseudo
 // TODO: refactooring, this should return char * instead
-R_API int r_parse_parse(RParse *p, const char *data, char *str) {
+R_API bool r_parse_parse(RParse *p, const char *data, char *str) {
+	r_return_val_if_fail (p && data && str, false);
 	if (p && data && *data && p->cur && p->cur->parse) {
 		return p->cur->parse (p, data, str);
 	}
