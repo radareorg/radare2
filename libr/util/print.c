aabaa324d4b5c3f8a136d1258e142c5a033b60dd
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2019-06-17 19:05:40 +0800

    Fix #14046 - Don't use ansi with aaa notifications if no ansicon

--- libr/util/print.c
+++ libr/util/print.c
@@ -2224,7 +2224,11 @@ R_API const char* r_print_rowlog(RPrint *print, const char *str) {
 	if (!verbose) {
 		return NULL;
 	}
-	if (use_color) {
+	if (use_color
+#if __WINDOWS__
+	    && r_cons_singleton ()->ansicon
+#endif
+	    ) {
 		eprintf ("[ ] "Color_YELLOW"%s\r["Color_RESET, str);
 	} else {
 		eprintf ("[ ] %s\r[", str);
@@ -2236,7 +2240,11 @@ R_API void r_print_rowlog_done(RPrint *print, const char *str) {
 	int use_color = print->flags & R_PRINT_FLAGS_COLOR;
 	bool verbose =  print->scr_prompt;
 	if (verbose) {
-		if (use_color) {
+		if (use_color
+#if __WINDOWS__
+		    && r_cons_singleton ()->ansicon
+#endif
+		    ) {
 			eprintf ("\r"Color_GREEN"[x]"Color_RESET" %s\n", str);
 		} else {
 			eprintf ("\r[x] %s\n", str);
