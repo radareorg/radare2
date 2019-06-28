fbb3a0569c41484ec4ccf1bdff4791fac06f353e
Óscar <oxcabe@gmail.com>
2019-06-29 00:04:26 +0100

    Implement afsr to change current function return type

--- libr/core/cmd_anal.c
+++ libr/core/cmd_anal.c
@@ -293,6 +293,7 @@ static const char *help_msg_af[] = {
 	"afo", " [fcn.name]", "show address for the function named like this",
 	"afs", "[!] ([fcnsign])", "get/set function signature at current address (afs! uses cfg.editor)",
 	"afS", "[stack_size]", "set stack frame size for function at current address",
+	"afsr", " [function_name] [new_type]", "change type for given function",
 	"aft", "[?]", "type matching, type propagation",
 	"afu", " addr", "resize and analyze function from current address until addr",
 	"afv[bsra]", "?", "manipulate args, registers and variables in function",
@@ -618,7 +619,6 @@ static const char *help_msg_as[] = {
 	"as", " 4", "show syscall 4 based on asm.os and current regs/mem",
 	"asc[a]", " 4", "dump syscall info in .asm or .h",
 	"asf", " [k[=[v]]]", "list/set/unset pf function signatures (see fcnsign)",
-	"asfr", " [function_name] [new_type]", "change type for given function",
 	"asj", "", "list of syscalls in JSON",
 	"asl", "", "list of syscalls by asm.os and asm.arch",
 	"asl", " close", "returns the syscall number for close",
@@ -2780,6 +2780,23 @@ static int cmd_anal_fcn(RCore *core, const char *input) {
 			r_core_cmdf (core, "\"afs %s\"", data);
 			free (sig);
 			free (data);
+		} else if (input[2] == 'r') { // "afsr"
+			if (input[3] == ' ') {
+				const char *ntype = ((input[4]) != '\0')
+					? (input + 4)
+					: NULL;
+				if (ntype) {
+					char *sig = r_core_cmd_str (core, "afs");
+					char *sig_notype = strchr (sig, ' ');
+					char *data = (char *)malloc (strlen (ntype) + strlen (sig_notype) + 1);
+					strcpy (data, ntype);
+					strcat (data, sig_notype);
+					strtok (data, "\n");
+					r_core_cmdf (core, "\"afs %s\"", data);
+					free (sig);
+					free (data);
+				}
+			}
 		} else {
 			ut64 addr = core->offset;
 			RAnalFunction *f;
@@ -5977,39 +5994,7 @@ static void cmd_anal_syscall(RCore *core, const char *input) {
 		}
 		break;
 	case 'f': // "asf"
-		if (input[i] == ' ') {
-			cmd_asf (core, input + 1);
-		}
-		else if (input[1] == 'r') { // "asfr"
-			/*
-				case 't':{ // "afvt"
-				p = strchr (ostr, ' ');
-				if (!p++) {
-				free (ostr);
-				return false;
-				}
-
-				char *type = strchr (p, ' ');
-				if (!type) {
-				free (ostr);
-				return false;
-				}
-			 *type++ = 0;
-			 v1 = r_anal_var_get_byname (core->anal, fcn->addr, p);
-			 if (!v1) {
-			 eprintf ("Cant find get by name %s\n", p);
-			 free (ostr);
-			 return false;
-			 }
-			 r_anal_var_retype (core->anal, fcn->addr,
-			 R_ANAL_VAR_SCOPE_LOCAL, -1, v1->kind, type, -1, v1->isarg, p);
-			 r_anal_var_free (v1);
-			 free (ostr);
-			 return true;
-
-			 }
-			 */
-		}
+		cmd_asf (core, input + 1);
 		break;
 	case 'l': // "asl"
 		if (input[1] == ' ') {
