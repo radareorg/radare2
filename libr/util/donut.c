a6b6fe56ec76cc2e6afc841f788c1d493da3dd6b
pancake <pancake@nopcode.org>
2019-07-11 05:14:41 +0200

    Introduce ?e= and ?ed to print progressbar and 3D donut ##cons

--- libr/util/donut.c
+++ libr/util/donut.c
@@ -34,7 +34,8 @@ in(B),t=c*h*g-f*        e;int x=40+30*D*
 #include <math.h>
 
 // global iterators
-R_API char *r_str_donut(int size) {
+R_API char *r_str_donut(int osize) {
+	int size = osize;
 	static float A= 0;
 	static float B= 0;
 	float i,j,z[1760];
@@ -43,10 +44,19 @@ R_API char *r_str_donut(int size) {
 	char o[1760];
 	memset (b,32,1760);
 	memset (z,0,7040) ;
-	if (size == 0) {
+	if (osize == 0) {
 		size = 100;
+	} else {
+		A=0;
+		B=0;
 	}
 	double Zero = (((double)(100 - size) / 100) * 6);
+	double Width = 30;
+	double Height = 15;
+	double Align = 25; // 40;
+	if (osize != 0) {
+		Align = 40;
+	}
 	for (j=Zero; 6.28>j; j+=0.07) {
 		for (i=Zero;6.28 >i;i+=0.02){
 			float c=sin(i);
@@ -55,20 +65,28 @@ R_API char *r_str_donut(int size) {
 			float f=sin(j);
 			float g=cos(A);
 			float h=d+2;
-			float D=1/(c* h*e+f*g+5),l=cos      (i),m=cos(B),n=sin(B),t=c*h*g-f*        e;
-			int x=40+30*D* (l*h*m-t*n),y=12+15*D*(l*h*n +t*m),o=x+80*y, N=8*((f*e-c*d*g)*m-c*d*e-f*g-l        *d*n);
-			if(22>y&& y>0&&x>0&&80>x&&D>z[o]){
-				z[o]=D;
-				b[o]= ".,-~:;=!*%$@"[N>0?N:0];
+			float D=1/(c* h*e+f*g+5),l=cos(i),m=cos(B),n=sin(B),t=c*h*g-f*e;
+			int x=Align+Width*D*(l*h*m-t*n);
+			int y=12+Height*D*(l*h*n +t*m);
+			int o=x+80*y;
+			int N=8*((f*e-c*d*g)*m-c*d*e-f*g-l*d*n);
+			if (22 >y && y > 0 && x>0&&80>x&&D>z[o]) {
+				z[o] = D;
+				b[o] = " .,-:!/|S$@&"[N > 0? N: 0];
 			}
 		}
 	}
 	for (k=0;k<1760;k++) {
 		o[k] = k%80?b[k]:10;
 	}
-	o[sizeof (o)-1] = 0;
-	A+=0.04;
-	B+= 0.02;
+	o[sizeof (o) - 1] = 0;
+	if (osize == 0)  {
+		A += 0.03;
+		B += 0.02;
+	} else {
+		A += 0.0000;
+		B += 0.01;
+	}
 	char *r = strdup (o);
 	r = r_str_trim_tail (r);
 	return r;
