1f8fefd0076bcf6933c23a7a366ff3b8ade2d40d
Khairul Kasmiran <kazarmy@gmail.com>
2019-07-21 22:32:09 +0800

    ic*: replace ' ' with '_' in flag name

--- libr/core/cbin.c
+++ libr/core/cbin.c
@@ -2883,6 +2883,10 @@ static int bin_classes(RCore *r, int mode) {
 			r_list_foreach (c->methods, iter2, sym) {
 				char *mflags = r_core_bin_method_flags_str (sym->method_flags, mode);
 				char *cmd = r_str_newf ("\"f method%s.%s.%s = 0x%"PFMT64x"\"\n", mflags, c->name, sym->name, sym->vaddr);
+				r_str_replace_char (cmd, ' ', '_');
+				cmd[2] = ' ';
+				char *eq = strrchr (cmd, '=');
+				*(eq - 1) = *(eq + 1) = ' ';
 				r_str_replace_char (cmd, '\n', 0);
 				r_cons_printf ("%s\n", cmd);
 				R_FREE (mflags);
