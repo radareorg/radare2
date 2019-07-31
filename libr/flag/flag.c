65f5de17f3bab18b6e035813fdb72e10a9cb46e7
Paul I <pelijah@users.noreply.github.com>
2019-07-31 23:28:11 +0300

    Update flag.c

--- libr/flag/flag.c
+++ libr/flag/flag.c
@@ -587,7 +587,7 @@ beach:
 static bool isFunctionFlag(const char *n) {
 	return (!strncmp (n, "sym.func.", 9)
 	|| !strncmp (n, "method.", 7)
-	|| !strncmp (n, "sym.", 7)
+	|| !strncmp (n, "sym.", 4)
 	|| !strncmp (n, "func.", 5)
 	|| !strncmp (n, "fcn.0", 5));
 }
