6b3094b9d156100a0ae9883ae686bceaf87e0939
Ayman Khamouma <kamou.k@gmail.com>
2019-08-12 23:03:00 +0200

    cleanup

--- libr/core/canal.c
+++ libr/core/canal.c
@@ -5257,12 +5257,10 @@ R_API void r_core_anal_propagate_noreturn(RCore *core) {
 				continue;
 			}
 
-			if (!r_list_contains(done, f->addr) && is_noreturn_function (core, f)) {
+			if (f->addr && !r_list_contains(done, f->addr) && is_noreturn_function (core, f)) {
 				r_anal_noreturn_add (core->anal, NULL, f->addr);
-				if (f->addr) {
-					r_list_append (todo, f);
-					r_list_append (done, f->addr);
-				}
+				r_list_append (todo, f);
+				r_list_append (done, f->addr);
 			}
 
 			r_anal_op_free (xrefop);
