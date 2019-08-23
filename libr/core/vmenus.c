64783b91264ff412cbd7d87aee825fc415c39315
deepakchethan <deepakchethan@outlook.com>
2019-08-23 17:11:25 +0530

    Added spaces

--- libr/core/vmenus.c
+++ libr/core/vmenus.c
@@ -3362,9 +3362,9 @@ R_API void r_core_visual_anal(RCore *core, const char *input) {
 			}
 			break;
 		case 'g': {
-			r_core_visual_showcursor(core, true);
-			r_core_visual_offset(core);        // change the seek to selected offset
-			RListIter *iter;		   // change the curren option to selected seek
+			r_core_visual_showcursor (core, true);
+			r_core_visual_offset (core);        // change the seek to selected offset
+			RListIter *iter;		   // change the current option to selected seek
 			RAnalFunction *fcn;
 			int i = 0;
 			r_list_foreach (core->anal->fcns, iter, fcn) {
@@ -3373,7 +3373,7 @@ R_API void r_core_visual_anal(RCore *core, const char *input) {
 				}
 				i++;
 			}
-			r_core_visual_showcursor(core, false);
+			r_core_visual_showcursor (core, false);
 		}
 		        break;
 		case 'G':
