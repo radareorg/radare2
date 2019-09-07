d78c83aa3701e9854f08b1be31f6d374b5652a93
GustavoLCR <gugulcr@gmail.com>
2019-09-07 00:31:58 -0300

    Implement Windows message breakpoints

---
+++ libr/debug/p/native/windows/windows_message.h
@@ -0,0 +1,11 @@
+#include <r_types.h>
+
+typedef struct _window {
+	ut32 tid;
+	HANDLE h;
+	char *name;
+	ut64 proc;
+} window;
+
+R_API bool r_w32_add_winmsg_breakpoint(RDebug *dbg, char *name);
+R_API void r_w32_print_windows(RDebug *dbg);
