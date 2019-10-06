c66946955ab7795e7e4152ecb00b9a0e943ceb7a
pancake <pancake@nopcode.org>
2019-10-07 00:50:06 +0200

    Add initial reg profile for sysz arch ##anal

--- libr/anal/p/anal_sysz.c
+++ libr/anal/p/anal_sysz.c
@@ -146,15 +146,54 @@ static int analop(RAnal *a, RAnalOp *op, ut64 addr, const ut8 *buf, int len, RAn
 	return op->size;
 }
 
+static int set_reg_profile(RAnal *anal) {
+	char *p =
+		"=PC	r15\n"
+		"=LR	r14\n"
+		"=SP	r13\n"
+		"=BP	r12\n"
+		"=A0	r0\n"
+		"=A1	r1\n"
+		"=A2	r2\n"
+		"=A3	r3\n"
+		"=SN	r0\n"
+		"gpr	sb	.32	36	0\n" // r9
+		"gpr	sl	.32	40	0\n" // rl0
+		"gpr	fp	.32	44	0\n" // r11
+		"gpr	ip	.32	48	0\n" // r12
+		"gpr	sp	.32	52	0\n" // r13
+		"gpr	lr	.32	56	0\n" // r14
+		"gpr	pc	.32	60	0\n" // r15
+
+		"gpr	r0	.32	0	0\n"
+		"gpr	r1	.32	4	0\n"
+		"gpr	r2	.32	8	0\n"
+		"gpr	r3	.32	12	0\n"
+		"gpr	r4	.32	16	0\n"
+		"gpr	r5	.32	20	0\n"
+		"gpr	r6	.32	24	0\n"
+		"gpr	r7	.32	28	0\n"
+		"gpr	r8	.32	32	0\n"
+		"gpr	r9	.32	36	0\n"
+		"gpr	r10	.32	40	0\n"
+		"gpr	r11	.32	44	0\n"
+		"gpr	r12	.32	48	0\n"
+		"gpr	r13	.32	52	0\n"
+		"gpr	r14	.32	56	0\n"
+		"gpr	r15	.32	60	0\n"
+	;
+	return r_reg_set_profile_string (anal->reg, p);
+}
+
 RAnalPlugin r_anal_plugin_sysz = {
-	.name = "systemz.cs",
+	.name = "sysz",
 	.desc = "Capstone SystemZ microanalysis",
 	.esil = false,
 	.license = "BSD",
 	.arch = "sysz",
 	.bits = 32|64,
 	.op = &analop,
-	//.set_reg_profile = &set_reg_profile,
+	.set_reg_profile = &set_reg_profile,
 };
 
 #ifndef R2_PLUGIN_INCORE
