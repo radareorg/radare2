d7367a7dadf901d24e2f956240a4f2e74f022699
GustavoLCR <gugulcr@gmail.com>
2019-10-14 16:22:58 -0300

    Move UTF-8 setting logic to scr.utf8 config callback on Windows

--- libr/core/cconfig.c
+++ libr/core/cconfig.c
@@ -2273,6 +2273,30 @@ static bool cb_utf8(void *user, void *data) {
 	RCore *core = (RCore *) user;
 	RConfigNode *node = (RConfigNode *) data;
 	core->cons->use_utf8 = node->i_value;
+#if __WINDOWS__
+	if (core->cons->use_utf8) {
+#if UNICODE
+	if (IsValidCodePage (CP_UTF8)) {
+		if (!SetConsoleOutputCP (CP_UTF8) || !SetConsoleCP (CP_UTF8)) {
+			r_sys_perror ("r_cons_new");
+		}
+	} else {
+		R_LOG_WARN ("UTF-8 Codepage not installed.\n");
+	}
+#else
+	UINT CP_IN = GetACP ();
+	UINT CP_OUT = IsValidCodePage (CP_UTF8) ? CP_UTF8 : CP_IN;
+	if (!SetConsoleOutputCP (CP_OUT) || !SetConsoleCP (CP_IN)) {
+		r_sys_perror ("r_cons_new");
+	}
+#endif
+	} else {
+		UINT acp = GetACP ();
+		if (!SetConsoleOutputCP (acp) || !SetConsoleCP (acp)) {
+			r_sys_perror ("r_cons_new");
+		}
+	}
+#endif
 	return true;
 }
 
