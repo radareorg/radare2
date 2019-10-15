9f39d5a92783681d87764555b1c7db533f2370e2
condret <condr3t@protonmail.com>
2019-10-15 21:22:36 +0200

    try r_cons_get_input in r_core_prompt

--- libr/core/core.c
+++ libr/core/core.c
@@ -3084,23 +3084,28 @@ static void set_prompt (RCore *r) {
 }
 
 R_API int r_core_prompt(RCore *r, int sync) {
-	char line[4096];
+//	char line[4096];
 
 	int rnv = r->num->value;
 	set_prompt (r);
-	int ret = r_cons_fgets (line, sizeof (line), 0, NULL);
+	int ret;
+	char *line = r_cons_get_input (&ret);
 	if (ret == -2) {
+		free (line);
 		return R_CORE_CMD_EXIT; // ^D
 	}
 	if (ret == -1) {
+		free (line);
 		return false; // FD READ ERROR
 	}
 	r->num->value = rnv;
 	if (sync) {
+		free (line);
 		return r_core_prompt_exec (r);
 	}
 	free (r->cmdqueue);
-	r->cmdqueue = strdup (line);
+//	r->cmdqueue = strdup (line);
+	r->cmdqueue = line;
         if (r->scr_gadgets && *line && *line != 'q') {
                 r_core_cmd0 (r, "pg");
         }
