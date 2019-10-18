219eefbd5dd5805f17b044ef79daf6c1cfffd965
Riccardo Schirone <ret2libc@users.noreply.github.com>
2019-10-19 01:46:22 +0200

    Simplify r_bin_open_io function (#15307) ##bin
    * remove file_sz field from r_bin_file_new_from_buffer and r_bin_file_xtr_load_buffer
    * remove all the messy buffer size handling and just rely on r_buf_size

--- libr/bin/i/private.h
+++ libr/bin/i/private.h
@@ -37,6 +37,6 @@ R_IPI void r_bin_class_free(RBinClass *c);
 R_IPI RBinSymbol *r_bin_class_add_method(RBinFile *binfile, const char *classname, const char *name, int nargs);
 R_IPI void r_bin_class_add_field(RBinFile *binfile, const char *classname, const char *name);
 
-R_IPI RBinFile *r_bin_file_xtr_load_buffer(RBin *bin, RBinXtrPlugin *xtr, const char *filename, RBuffer *buf, ut64 file_sz, ut64 baseaddr, ut64 loadaddr, int idx, int fd, int rawstr);
-R_IPI RBinFile *r_bin_file_new_from_buffer(RBin *bin, const char *file, RBuffer *buf, ut64 file_sz, int rawstr, ut64 baseaddr, ut64 loadaddr, int fd, const char *pluginname);
+R_IPI RBinFile *r_bin_file_xtr_load_buffer(RBin *bin, RBinXtrPlugin *xtr, const char *filename, RBuffer *buf, ut64 baseaddr, ut64 loadaddr, int idx, int fd, int rawstr);
+R_IPI RBinFile *r_bin_file_new_from_buffer(RBin *bin, const char *file, RBuffer *buf, int rawstr, ut64 baseaddr, ut64 loadaddr, int fd, const char *pluginname);
 #endif
