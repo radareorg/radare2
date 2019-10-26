c64dc87367513abcbf667861c36975fe34fc3e1e
Armin Weihbold <armin.weihbold@gmail.com>
2019-10-26 16:51:07 +0200

    add support for setting xmm registers on linux

--- libr/debug/p/debug_native.c
+++ libr/debug/p/debug_native.c
@@ -1015,6 +1015,12 @@ static int r_debug_native_reg_write (RDebug *dbg, int type, const ut8* buf, int 
 #else
 #warning r_debug_native_reg_write not implemented
 #endif
+	} else if (type == R_REG_TYPE_FPU) {
+#if __linux__
+		return linux_reg_write (dbg, type, buf, size);
+#else
+#warning r_debug_native_reg_write FPU not implemented
+#endif
 	} //else eprintf ("TODO: reg_write_non-gpr (%d)\n", type);
 	return false;
 }
