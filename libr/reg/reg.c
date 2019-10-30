4b2bf8dccd1bb7d2bbadb18f729fef417b13b331
pancake <pancake@nopcode.org>
2019-10-30 23:33:33 +0100

    Fix assert

--- libr/reg/reg.c
+++ libr/reg/reg.c
@@ -88,7 +88,7 @@ R_API void r_reg_item_free(RRegItem *item) {
 }
 
 R_API int r_reg_get_name_idx(const char *type) {
-	r_return_val_if_fail (type && *type, -1);
+	if (type)
 	switch (*type | (type[1] << 8)) {
 	/* flags */
 	case 'Z' + ('F' << 8): return R_REG_NAME_ZF;
