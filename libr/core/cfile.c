c7e232d4feba5a17113fc44e2f659bff5f20c85c
yossizap <yossizap@gmail.com>
2019-11-02 23:24:53 +0000

    Get the debug handle from the URI instead of plugin names
    Plugin names aren't necessarily the same as the handle

--- libr/core/cfile.c
+++ libr/core/cfile.c
@@ -907,9 +907,11 @@ R_API RCoreFile *r_core_file_open(RCore *r, const char *file, int flags, ut64 lo
 			swstep = false;
 		}
 		r_config_set_i (r->config, "dbg.swstep", swstep);
-		// Set the correct debug plugin
-		if (fd->plugin && fd->plugin->name && fd->plugin->isdbg) {
-			r_debug_use (r->dbg, fd->plugin->name);
+		// Set the correct debug handle
+		if (fd->plugin && fd->plugin->isdbg) {
+			const char *dh = strndup(file, (strstr(file, "://") - file));
+			r_debug_use (r->dbg, dh);
+			free(dh);
 		}
 	}
 	//used by r_core_bin_load otherwise won't load correctly
