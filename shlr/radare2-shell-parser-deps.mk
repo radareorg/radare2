827ac7dab8b48916303850f23505389a74475537
Riccardo Schirone <sirmy15@gmail.com>
2019-11-06 09:38:30 +0100

    Switch to radare2-shell-parser

---
+++ shlr/radare2-shell-parser-deps.mk
@@ -0,0 +1,2 @@
+LINK+=$(STOP)/radare2-shell-parser/libshell-parser.$(EXT_AR)
+LINK+=$(STOP)/tree-sitter/libtree-sitter.$(EXT_AR)
--- shlr/tree-sitter-r2cmd-deps.mk
+++
@@ -1,2 +0,0 @@
-LINK+=$(STOP)/tree-sitter-r2cmd/libtree-sitter-r2cmd.$(EXT_AR)
-LINK+=$(STOP)/tree-sitter/libtree-sitter.$(EXT_AR)
