7701093670ea78f355afd1ca16be679bea4551cd
pancake <pancake@nopcode.org>
2019-11-06 17:20:41 +0100

    Fix fox

--- libr/core/cmd_anal.c
+++ libr/core/cmd_anal.c
@@ -1123,7 +1123,7 @@ static void __cmd_afvf(RCore *core, const char *input) {
 		if (p->isarg || p->delta > 0) {
 			continue;
 		}
-		char *pad = r_str_pad (' ', 10 - strlen (p->name));
+		const char *pad = r_str_pad (' ', 10 - strlen (p->name));
 		r_cons_printf ("0x%08"PFMT64x"  %s:%s%s\n", (ut64)-p->delta, p->name, pad, p->type);
 	}
 	r_list_sort (list, delta_cmp);
@@ -1132,7 +1132,7 @@ static void __cmd_afvf(RCore *core, const char *input) {
 			continue;
 		}
 		// TODO: only stack vars if (p->kind == 's') { }
-		char *pad = r_str_pad (' ', 10 - strlen (p->name));
+		const char *pad = r_str_pad (' ', 10 - strlen (p->name));
 		// XXX this 0x6a is a hack
 		r_cons_printf ("0x%08"PFMT64x"  %s:%s%s\n", ((ut64)p->delta) - 0x6a, p->name, pad, p->type);
 	}
