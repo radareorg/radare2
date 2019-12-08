e866f8f21ffdb6463deb54ea94b4a6e0d087ee0c
Florian Märkl <info@florianmaerkl.de>
2019-12-09 00:07:10 +0100

    Add RIntervalTree data structure ##util (#15541)
    * Add RRangeTree
    * Inclusive/Exclusive
    * Add r_interval_tree_delete and r_interval_tree_resize
    * Fix r_interval_tree_all_at()

--- libr/include/r_util.h
+++ libr/include/r_util.h
@@ -31,6 +31,7 @@ int gettimeofday (struct timeval* p, void* tz);
 #include "r_util/r_signal.h"
 #include "r_util/r_alloc.h"
 #include "r_util/r_rbtree.h"
+#include "r_util/r_intervaltree.h"
 #include "r_util/r_big.h"
 #include "r_util/r_base64.h"
 #include "r_util/r_base91.h"
