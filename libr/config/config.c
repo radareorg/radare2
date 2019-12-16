67f0cc0045dfebed35f3a99dca13830a0b6afc1f
Riccardo Schirone <sirmy15@gmail.com>
2019-12-16 19:09:51 +0100

    Pass R2_CONFIG file to ! commands

--- libr/config/config.c
+++ libr/config/config.c
@@ -693,3 +693,26 @@ R_API void r_config_bump(RConfig *cfg, const char *key) {
 		free (orig);
 	}
 }
+
+R_API void r_config_serialize(R_NONNULL RConfig *config, R_NONNULL Sdb *db) {
+	RListIter *iter;
+	RConfigNode *node;
+	r_list_foreach (config->nodes, iter, node) {
+		sdb_set (db, node->name, node->value, 0);
+	}
+}
+
+static int load_config_cb(void *user, const char *k, const char *v) {
+	RConfig *config = user;
+	RConfigNode *node = r_config_node_get (config, k);
+	if (!node) {
+		return 1;
+	}
+	r_config_set (config, k, v);
+	return 1;
+}
+
+R_API bool r_config_unserialize(R_NONNULL RConfig *config, R_NONNULL Sdb *db, R_NULLABLE char **err) {
+	sdb_foreach (db, load_config_cb, config);
+	return true;
+}
