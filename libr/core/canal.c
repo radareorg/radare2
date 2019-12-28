cdbcffe108bd7a58ed8e5b5c93c190159b15b9a1
Florian Märkl <info@florianmaerkl.de>
2019-12-28 12:37:48 +0100

    Remove dupped basic block analysis in core (#15714) ##anal

--- libr/core/canal.c
+++ libr/core/canal.c
@@ -835,16 +835,6 @@ static int __core_anal_fcn(RCore *core, ut64 at, ut64 from, int reftype, int dep
 				r_flag_set (core->flags, fcn->name, fcn->addr, r_anal_fcn_size (fcn));
 				r_flag_space_pop (core->flags);
 			}
-			// XXX fixes overlined function ranges wtf  // fcn->addr = at;
-			/* TODO: Dupped analysis, needs more optimization */
-			fcn->depth = 256;
-			r_core_anal_bb (core, fcn, fcn->addr, true);
-			// hack
-			if (!fcn->depth) {
-				eprintf ("Analysis depth reached at 0x%08"PFMT64x"\n", fcn->addr);
-			} else {
-				fcn->depth = 256 - fcn->depth;
-			}
 
 			/* New function: Add initial xref */
 			if (from != UT64_MAX) {
@@ -1712,87 +1702,6 @@ static int core_anal_graph_nodes(RCore *core, RAnalFunction *fcn, int opts, PJ *
 	free (pal_traced);
 	free (pal_box4);
 	return nodes;
-}
-
-/* analyze a RAnalBlock at the address at and add that to the fcn function. */
-// TODO: move into RAnal.
-R_API int r_core_anal_bb(RCore *core, RAnalFunction *fcn, ut64 addr, int head) {
-	RAnalBlock *bb, *bbi;
-	RListIter *iter;
-	ut64 jump, fail;
-	int rc = true;
-	int ret = R_ANAL_RET_NEW;
-	bool x86 = core->anal->cur->arch && !strcmp (core->anal->cur->arch, "x86");
-
-	if (--fcn->depth <= 0) {
-		return false;
-	}
-
-	bb = r_anal_bb_new ();
-	if (!bb) {
-		return false;
-	}
-
-	r_list_foreach (fcn->bbs, iter, bbi) {
-		if (addr >= bbi->addr && addr < bbi->addr + bbi->size
-		    && (!core->anal->opt.jmpmid || !x86 || r_anal_bb_op_starts_at (bbi, addr))) {
-			ret = r_anal_fcn_split_bb (core->anal, fcn, bbi, addr);
-			break;
-		}
-	}
-	ut8 *buf = NULL;
-	if (ret == R_ANAL_RET_DUP) {
-		/* Dupped basic block */
-		goto error;
-	}
-
-	if (ret == R_ANAL_RET_NEW) { /* New bb */
-		// XXX: use read_ahead and so on, but don't allocate that much in here
-		const int buflen = core->anal->opt.bb_max_size; // OMG THIS IS SO WRONG
-		buf = calloc (1, buflen);
-		if (!buf) {
-			goto error;
-		}
-		int bblen = 0;
-		ut64 at;
-		do {
-			at = addr + bblen;
-			if (!r_io_is_valid_offset (core->io, at, !core->anal->opt.noncode)) {
-				goto error;
-			}
-			if (!r_io_read_at (core->io, at, buf, core->anal->opt.bb_max_size)) { // ETOOSLOW
-				goto error;
-			}
-			bblen = r_anal_bb (core->anal, bb, at, buf, buflen, head);
-			if (bblen == R_ANAL_RET_ERROR || (bblen == R_ANAL_RET_END && bb->size < 1)) { /* Error analyzing bb */
-				goto error;
-			}
-			if (bblen == R_ANAL_RET_END) { /* bb analysis complete */
-				ret = r_anal_fcn_bb_overlaps (fcn, bb);
-				if (ret == R_ANAL_RET_NEW) {
-					r_anal_fcn_bbadd (fcn, bb);
-					fail = bb->fail;
-					jump = bb->jump;
-					if (fail != -1) {
-						r_core_anal_bb (core, fcn, fail, false);
-					}
-					if (jump != -1) {
-						r_core_anal_bb (core, fcn, jump, false);
-					}
-				}
-			}
-		} while (bblen != R_ANAL_RET_END);
-		free (buf);
-		return true;
-	}
-	goto fin;
-error:
-	rc = false;
-fin:
-	r_list_delete_data (fcn->bbs, bb);
-	r_anal_bb_free (bb);
-	free (buf);
-	return rc;
 }
 
 /* seek basic block that contains address addr or just addr if there's no such
