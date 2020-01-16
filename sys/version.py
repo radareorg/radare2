2507a5c4f4ceb45f563ec1b620eec1346b07d220
Riccardo Schirone <sirmy15@gmail.com>
2020-01-16 16:10:06 +0100

    Support major/minor/patch versions in meson build

--- sys/version.py
+++ sys/version.py
@@ -6,4 +6,12 @@ import sys
 
 with open('configure.acr', 'r') as f:
     f.readline()
-    sys.stdout.write(f.readline().split()[1])
+    version = f.readline().split()[1]
+    sys.stdout.write(version + '\n')
+    versions = version.split('.')
+    version_major = versions[0] if len(versions) > 0 else 0
+    version_minor = versions[1] if len(versions) > 1 else 0
+    version_patch = versions[2].replace('-git', '') if len(versions) > 2 else 0
+    sys.stdout.write(version_major + '\n')
+    sys.stdout.write(version_minor + '\n')
+    sys.stdout.write(version_patch + '\n')
