e1298d54fdf33e881d97ca8a0dc66aba7e3f34a5
Riccardo Schirone <sirmy15@gmail.com>
2020-01-17 09:24:31 +0100

    Print the full version only when asked for
    Windows build on appveyor uses sys/version.py, but it does not care
    about the full version.

--- sys/version.py
+++ sys/version.py
@@ -4,14 +4,24 @@
 
 import sys
 
+full_version = False
+
+for arg in sys.argv[1:]:
+    if arg == '--full-version':
+        full_version = True
+    else:
+        print('Option %s not supported' % (arg,))
+        sys.exit(1)
+
 with open('configure.acr', 'r') as f:
     f.readline()
     version = f.readline().split()[1]
     sys.stdout.write(version + '\n')
-    versions = version.split('.')
-    version_major = versions[0] if len(versions) > 0 else 0
-    version_minor = versions[1] if len(versions) > 1 else 0
-    version_patch = versions[2].replace('-git', '') if len(versions) > 2 else 0
-    sys.stdout.write(version_major + '\n')
-    sys.stdout.write(version_minor + '\n')
-    sys.stdout.write(version_patch + '\n')
+    if full_version:
+        versions = version.split('.')
+        version_major = versions[0] if len(versions) > 0 else 0
+        version_minor = versions[1] if len(versions) > 1 else 0
+        version_patch = versions[2].replace('-git', '') if len(versions) > 2 else 0
+        sys.stdout.write(version_major + '\n')
+        sys.stdout.write(version_minor + '\n')
+        sys.stdout.write(version_patch + '\n')
