d5343c8a68d63c8e04862baa903dd21e895493a8
Khairul Kasmiran <kazarmy@gmail.com>
2020-01-20 19:13:06 +0800

    Treat empty string as false when boolifying

--- libr/util/str.c
+++ libr/util/str.c
@@ -3554,7 +3554,8 @@ R_API bool r_str_is_false(const char *s) {
 	return !r_str_casecmp ("no", s)
 		|| !r_str_casecmp ("off", s)
 		|| !r_str_casecmp ("false", s)
-		|| !r_str_casecmp ("0", s);
+		|| !r_str_casecmp ("0", s)
+		|| !*s;
 }
 
 R_API bool r_str_is_bool(const char *val) {
