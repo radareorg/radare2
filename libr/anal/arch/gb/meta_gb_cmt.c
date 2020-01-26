e5269ecba7a6e4bd7893f03d12f672d60a1e8990
Manuel Prinz <manuel@pinguinkiste.de>
2020-01-26 11:59:12 +0100

    Rename Gameboy register comments to commonly used mnemonics

--- libr/anal/arch/gb/meta_gb_cmt.c
+++ libr/anal/arch/gb/meta_gb_cmt.c
@@ -13,29 +13,29 @@ void meta_gb_bankswitch_cmt(RAnal *a, ut64 addr, ut16 ldarg) {
 void meta_gb_hardware_cmt(RAnal *a, const ut8 hw, ut64 addr) {
 	switch(hw)
 	{
-		case 0:
-			r_meta_set_string(a, R_META_TYPE_COMMENT, addr, "JOYPAD");				//Moar context for this (which Key is affected)
+		case 0x00:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "P1 - Joypad info)"); //Moar context for this (which Key is affected)
 			break;
-		case 1:
-			r_meta_set_string(a, R_META_TYPE_COMMENT, addr, "Serial transfer data");
+		case 0x01:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "SB - Serial Transfer Data");
 			break;
-		case 2:
-			r_meta_set_string(a, R_META_TYPE_COMMENT, addr, "Serial transfer data - Ctl");
+		case 0x02:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "SC - Serial I/O Control");
 			break;
-		case 4:
-			r_meta_set_string(a, R_META_TYPE_COMMENT, addr, "DIV");
+		case 0x04:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "DIV - Divider register");
 			break;
-		case 5:
-			r_meta_set_string(a, R_META_TYPE_COMMENT, addr, "TIMA");
+		case 0x05:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "TIMA - Timer Counter");
 			break;
-		case 6:
-			r_meta_set_string(a, R_META_TYPE_COMMENT, addr, "TMA");
+		case 0x06:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "TMA - Timer modulo");
 			break;
-		case 7:
-			r_meta_set_string(a, R_META_TYPE_COMMENT, addr, "TAC");
+		case 0x07:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "TAC - Timer control");
 			break;
 		case 0x0f:
-			r_meta_set_string(a, R_META_TYPE_COMMENT, addr, "Interrupt Flag");
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "IF - Interrupt Flag");
 			break;
 		case 0x10:
 		case 0x11:
@@ -61,78 +61,78 @@ void meta_gb_hardware_cmt(RAnal *a, const ut8 hw, ut64 addr) {
 			r_meta_set_string(a, R_META_TYPE_COMMENT, addr, "SOUND");
 			break;
 		case 0x30:
-			r_meta_set_string(a, R_META_TYPE_COMMENT, addr, "Wave Pattern RAM/SOUND");
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "Wave Pattern RAM/SOUND");
 			break;
 		case 0x40:
-			r_meta_set_string(a, R_META_TYPE_COMMENT, addr, "LCDC");
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "LCDC - LCD Control");
 			break;
 		case 0x41:
-			r_meta_set_string(a, R_META_TYPE_COMMENT, addr, "LCDC - STAT");
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "STAT - LCD Status");
 			break;
 		case 0x42:
-			r_meta_set_string(a, R_META_TYPE_COMMENT, addr, "LCDC - Scroll y");
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "SCY - Scroll Y");
 			break;
 		case 0x43:
-			r_meta_set_string(a, R_META_TYPE_COMMENT, addr, "LCDC - Scroll x");
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "SCX - Scroll X");
 			break;
 		case 0x44:
-			r_meta_set_string(a, R_META_TYPE_COMMENT, addr, "LCDC - y cord");
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "LY - Y-Coordinate");
 			break;
 		case 0x45:
-			r_meta_set_string(a, R_META_TYPE_COMMENT, addr, "LCDC - y cord cmp");
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "LYC - Y-Coordinate Compare");
 			break;
 		case 0x46:
-			r_meta_set_string(a, R_META_TYPE_COMMENT, addr, "DMA");
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "DMA - Transfer and Start Address");
 			break;
-	    case 0x47:
-		    r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "BGP - BG Palette Data");
-		    break;
-	    case 0x48:
-		    r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "OBP0 - Object Palette 0 Data");
-		    break;
-	    case 0x49:
-		    r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "OBP1 - Object Palette 1 Data");
-		    break;
-	    case 0x4a:
-		    r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "WY - Window Y Position");
-		    break;
-	    case 0x4b:
-		    r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "WX - Window X Position");
-		    break;
-	    case 0x4d:
-		    r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "KEY1 - Select CPU Speed");
-		    break;
-	    case 0x4f:
-		    r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "VBK - Select Video RAM Bank");
-		    break;
-	    // DMA registers (0xff51 to 0xff55)
-	    case 0x51:
-	    case 0x52:
-	    case 0x53:
-	    case 0x54:
-	    case 0x55:
-		    r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "HDMA - Horizontal Blanking, General Purpose DMA");
-		    break;
-	    case 0x56:
-		    r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "RP - Infrared Communications Port");
-		    break;
-	    case 0x68:
-		    r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "BCPS - Background Color Palette Specification");
-		    break;
-	    case 0x69:
-		    r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "BCPD - Background Color Palette Data");
-		    break;
-	    case 0x6a:
-		    r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "OCPS - Object Color Palette Specification");
-		    break;
-	    case 0x6b:
-		    r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "OCPD - Object Color Palette Data");
-		    break;
-	    case 0x70:
-		    r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "SVBK - Select Main RAM Bank");
-		    break;
-	    case 0xff:
-		    r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "Interrupt Enable Flag");
-		    break;
-	    }
+		case 0x47:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "BGP - BG Palette Data");
+			break;
+		case 0x48:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "OBP0 - Object Palette 0 Data");
+			break;
+		case 0x49:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "OBP1 - Object Palette 1 Data");
+			break;
+		case 0x4a:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "WY - Window Y Position");
+			break;
+		case 0x4b:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "WX - Window X Position");
+			break;
+		case 0x4d:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "KEY1 - Select CPU Speed");
+			break;
+		case 0x4f:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "VBK - Select Video RAM Bank");
+			break;
+		// DMA registers (0xff51 to 0xff55)
+		case 0x51:
+		case 0x52:
+		case 0x53:
+		case 0x54:
+		case 0x55:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "HDMA - Horizontal Blanking, General Purpose DMA");
+			break;
+		case 0x56:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "RP - Infrared Communications Port");
+			break;
+		case 0x68:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "BCPS - Background Color Palette Specification");
+			break;
+		case 0x69:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "BCPD - Background Color Palette Data");
+			break;
+		case 0x6a:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "OCPS - Object Color Palette Specification");
+			break;
+		case 0x6b:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "OCPD - Object Color Palette Data");
+			break;
+		case 0x70:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "SVBK - Select Main RAM Bank");
+			break;
+		case 0xff:
+			r_meta_set_string (a, R_META_TYPE_COMMENT, addr, "Interrupt Enable Flag");
+			break;
+		}
 }
