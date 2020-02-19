9f620811cd331239ee18af4e4b75a3aab44bb965
pancake <pancake@nopcode.org>
2020-02-19 02:38:53 +0100

    No stdout

--- sys/debian.sh
+++ sys/debian.sh
@@ -14,27 +14,33 @@ fi
 echo "[debian] preparing radare2 package..."
 PKGDIR=sys/debian/radare2/root
 DEVDIR=sys/debian/radare2-dev/root
+
+# clean
 rm -rf "${PKGDIR}" "${DEVDIR}"
 
-./configure --prefix=/usr
-make -j4
-make install DESTDIR="${PWD}/${PKGDIR}"
+# build
+./configure --prefix=/usr > /dev/null
+make -j4 > /dev/null
+make install DESTDIR="${PWD}/${PKGDIR}" > /dev/null
+
+# dev-split
 mkdir -p "${DEVDIR}/usr/include"
 mv "${PKGDIR}/usr/include/"* "${DEVDIR}/usr/include"
 mkdir -p "${DEVDIR}/usr/lib"
 mv "${PKGDIR}/usr/lib/"lib*a "${DEVDIR}/usr/lib"
 mv "${PKGDIR}/usr/lib/pkgconfig" "${DEVDIR}/usr/lib"
 
+# strip
 for a in ${PKGDIR}/usr/bin/* ; do
   echo "[debian] strip $a"
   strip --strip-all "$a" 2> /dev/null || true
 done
-
 for a in ${PKGDIR}/usr/lib/libr*.so.* ; do
   echo "[debian] strip $a"
   strip --strip-unneeded "$a" 2> /dev/null || true
 done
 
+# packages
 echo "[debian] building radare2 package..."
 make -C sys/debian/radare2 ARCH=${ARCH}
 cp -f sys/debian/radare2/*.deb .
