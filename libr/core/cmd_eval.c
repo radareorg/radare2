9ebe9bd376cbfd192d381a653ac68936467b5c4b
Rishi Bhatt <bhattrishi8@gmail.com>
2020-03-03 23:34:15 +0530

    Implement ecHj to list highlight rules in json format

--- libr/core/cmd_eval.c
+++ libr/core/cmd_eval.c
@@ -480,6 +480,10 @@ static int cmd_eval(void *data, const char *input) {
 				r_meta_list (core->anal, R_META_TYPE_HIGHLIGHT, 0);
 				r_str_argv_free (argv);
 				return false;
+			case 'j':
+				r_meta_list (core->anal, R_META_TYPE_HIGHLIGHT, 'j');
+				r_str_argv_free (argv);
+				return false;
 			case '*':
 				r_meta_list (core->anal, R_META_TYPE_HIGHLIGHT, '*');
 				r_str_argv_free (argv);
