1291b70241a36c17e90090faa82ab0f039dd88f5
radare <pancake@nopcode.org>
2020-03-10 14:23:46 +0100

    Drop the check from the hash module

--- libr/hash/hash.c
+++ libr/hash/hash.c
@@ -302,12 +302,8 @@ R_API ut64 r_hash_name_to_bits(const char *name) {
 
 	do {
 		/* Eat everything up to the comma */
-		for (i = 0; *ptr && *ptr != ',' && i < sizeof (tmp) - 1;) {
-			/* Strip minuses */
-			if (*ptr != '-') {
-				tmp[i++] = tolower ((ut8)*ptr);
-			}
-			ptr++;
+		for (i = 0; *ptr && *ptr != ',' && i < sizeof (tmp) - 1; i++) {
+ 			tmp[i] = tolower ((ut8)*ptr++);
 		}
 
 		/* Safety net */
