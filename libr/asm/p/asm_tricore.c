f2aaa72fa29a557c822754852ad5b59b9a5568b1
Rishi Bhatt <bhattrishi8@gmail.com>
2020-03-10 18:40:57 +0530

    Support asm.cpu for Tricore architecture (#16161) ##asm

--- libr/asm/p/asm_tricore.c
+++ libr/asm/p/asm_tricore.c
@@ -15,8 +15,41 @@
 static unsigned long Offset = 0;
 static RStrBuf *buf_global = NULL;
 static ut8 bytes[128];
+enum {
+	TRICORE_GENERIC = 0x00000000,
+	TRICORE_RIDER_A = 0x00000001,
+	TRICORE_RIDER_B = 0x00000002,
+	TRICORE_RIDER_D = TRICORE_RIDER_B,
+	TRICORE_V2      = 0x00000004,
+	TRICORE_PCP     = 0x00000010,
+	TRICORE_PCP2    = 0x00000020
+};
 
-static int tricore_buffer_read_memory (bfd_vma memaddr, bfd_byte *myaddr, ut32 length, struct disassemble_info *info) {
+static int cpu_to_mach(char *cpu_type) {
+	if (cpu_type && *cpu_type) {
+		if (!strcmp (cpu_type, "generic")) {
+			return TRICORE_GENERIC;
+		}
+		if (!strcmp (cpu_type, "rider-a")) {
+			return TRICORE_RIDER_A;
+		}
+		if ((!strcmp (cpu_type, "rider-b")) || (!strcmp (cpu_type, "rider-d"))) {
+			return TRICORE_RIDER_B;
+		}
+		if (!strcmp (cpu_type, "v2")) {
+			return TRICORE_V2;
+		}
+		if (!strcmp (cpu_type, "pcp")) {
+			return TRICORE_PCP;
+		}
+		if (!strcmp (cpu_type, "pcp2")) {
+			return TRICORE_PCP2;
+		}
+	}
+	return TRICORE_RIDER_B;
+}
+
+static int tricore_buffer_read_memory(bfd_vma memaddr, bfd_byte *myaddr, ut32 length, struct disassemble_info *info) {
 	int delta = memaddr - Offset;
 	if (delta >= 0 && length + delta < sizeof(bytes)) {
 		memcpy (myaddr, bytes + delta, length);
@@ -53,7 +86,8 @@ static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 	disasm_obj.fprintf_func = &generic_fprintf_func;
 	disasm_obj.stream = stdout;
 
-	disasm_obj.mach = 2; // select CPU TYPE
+	// cpu type
+	disasm_obj.mach = cpu_to_mach (a->cpu);
 
 	op->size = print_insn_tricore ((bfd_vma)Offset, &disasm_obj);
 	if (op->size == -1) {
