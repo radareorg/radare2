f8a8826b1f79a6c982b27567ca412adc2bf59ad4
pancake <pancake@nopcode.org>
2020-03-19 03:01:28 +0100

    Optimize r_anal_get_bbaddr() to make graph navigation faster ##graph

--- libr/anal/bb.c
+++ libr/anal/bb.c
@@ -133,15 +133,6 @@ R_API ut64 r_anal_bb_size_i(RAnalBlock *bb, int i) {
 /* returns the address of the basic block that contains addr or UT64_MAX if
  * there is no such basic block */
 R_API ut64 r_anal_get_bbaddr(RAnal *anal, ut64 addr) {
-	RAnalBlock *bb;
-	RListIter *iter;
-	RAnalFunction *fcni = r_anal_get_fcn_in_bounds (anal, addr, 0);
-	if (fcni) {
-		r_list_foreach (fcni->bbs, iter, bb) {
-			if (addr >= bb->addr && addr < bb->addr + bb->size) {
-				return bb->addr;
-			}
-		}
-	}
-	return UT64_MAX;
+	RAnalBlock *bb = r_anal_get_block_at (anal, addr);
+	return bb? bb->addr: UT64_MAX;
 }
