9dcb809451cb1c188dfc0e2c414599119cdefde3
Alexis Ehret <alexis.ehret@epita.fr>
2020-03-27 21:29:04 +0100

    Removed useless ref in .h

--- libr/include/r_syscall.h
+++ libr/include/r_syscall.h
@@ -93,7 +93,6 @@ R_API bool r_syscall_setup(RSyscall *s, const char *arch, int bits, const char *
 R_API RSyscallItem *r_syscall_get(RSyscall *ctx, int num, int swi);
 R_API int r_syscall_get_num(RSyscall *ctx, const char *str);
 R_API const char *r_syscall_get_i(RSyscall *ctx, int num, int swi);
-R_API const char *r_syscall_reg(RSyscall *s, int idx, int num);
 R_API const char* r_syscall_sysreg(RSyscall *s, const char *type, ut64 num);
 R_API RList *r_syscall_list(RSyscall *ctx);
 R_API int r_syscall_get_swi(RSyscall *s);
