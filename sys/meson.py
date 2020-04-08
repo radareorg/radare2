a25e008469ab7cf55c44abcc2f6408bbbe0f9531
Paul I <pelijah@users.noreply.github.com>
2020-04-08 18:36:44 +0300

    Make msbuild quieter

--- sys/meson.py
+++ sys/meson.py
@@ -233,7 +233,7 @@ def build(args):
             xp_compat(r2_builddir)
         if not args.project:
             project = os.path.join(r2_builddir, 'radare2.sln')
-            params = ['/m']
+            params = ['/m', '/clp:Summary;Verbosity=minimal']
             if args.backend == 'vs2017' and args.xp:
                 params.append('/p:XPDeprecationWarning=false')
             msbuild(project, *params)
