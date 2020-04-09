2154e13c50f75a1433608e69f55b1fe13e9fd0fa
Liumeo <liumeo@pku.edu.cn>
2020-04-08 23:47:46 -0400

    DEX - Keep original type (#16486)

--- libr/bin/p/bin_dex.c
+++ libr/bin/p/bin_dex.c
@@ -937,8 +937,8 @@ static char *dex_method_name(RBinDexObj *bin, int idx) {
 	if (idx < 0 || idx >= bin->header.method_size) {
 		return NULL;
 	}
-	int cid = bin->methods[idx].class_id;
-	if (cid < 0 || cid >= bin->header.strings_size) {
+	ut16 cid = bin->methods[idx].class_id;
+	if (cid >= bin->header.strings_size) {
 		return NULL;
 	}
 	int tid = bin->methods[idx].name_id;
