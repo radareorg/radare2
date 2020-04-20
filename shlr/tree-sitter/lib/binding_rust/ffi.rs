e00ee621bd974822453a5e4fb7f50554f9d77c6b
Riccardo Schirone <sirmy15@gmail.com>
2020-04-20 12:34:01 +0200

    Avoid importing all binding stuff from tree-sitter

--- shlr/tree-sitter/lib/binding_rust/ffi.rs
+++
@@ -1,9 +0,0 @@
-#![allow(dead_code)]
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-
-include!("./bindings.rs");
-
-extern "C" {
-    pub(crate) fn dup(fd: std::os::raw::c_int) -> std::os::raw::c_int;
-}
--- shlr/tree-sitter/lib/binding_web/prefix.js
+++
@@ -1,9 +0,0 @@
-(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define([], factory);
-  } else if (typeof exports === 'object') {
-    module.exports = factory();
-  } else {
-    window.TreeSitter = factory();
-  }
-}(this, function () {
