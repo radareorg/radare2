10a755785be965e019079424bd2ff823105cd543
mdolmen <mathieu.dolmen@gmail.com>
2020-04-24 11:49:22 +0200

    Remove hardcoded supported encoders names (#16664) ##util

--- libr/include/r_crypto.h
+++ libr/include/r_crypto.h
@@ -48,6 +48,8 @@ typedef struct r_crypto_plugin_t {
 	int (*fini)(RCrypto *cry);
 } RCryptoPlugin;
 
+typedef ut64 RCryptoSelector;
+
 #ifdef R_API
 R_API RCrypto *r_crypto_init(RCrypto *cry, int hard);
 R_API RCrypto *r_crypto_as_new(RCrypto *cry);
@@ -61,7 +63,8 @@ R_API int r_crypto_update(RCrypto *cry, const ut8 *buf, int len);
 R_API int r_crypto_final(RCrypto *cry, const ut8 *buf, int len);
 R_API int r_crypto_append(RCrypto *cry, const ut8 *buf, int len);
 R_API ut8 *r_crypto_get_output(RCrypto *cry, int *size);
-R_API const char *r_crypto_name(ut64 bit);
+R_API const char *r_crypto_name(const RCryptoSelector bit);
+R_API const char *r_crypto_codec_name(const RCryptoSelector bit);
 #endif
 
 /* plugin pointers */
@@ -82,21 +85,27 @@ extern RCryptoPlugin r_crypto_plugin_rc6;
 extern RCryptoPlugin r_crypto_plugin_cps2;
 extern RCryptoPlugin r_crypto_plugin_serpent;
 
-#define R_CRYPTO_NONE 0
-#define R_CRYPTO_RC2 1
-#define R_CRYPTO_RC4 1<<1
-#define R_CRYPTO_RC6 1<<2
-#define R_CRYPTO_AES_ECB 1<<3
-#define R_CRYPTO_AES_CBC 1<<4
-#define R_CRYPTO_ROR 1<<5
-#define R_CRYPTO_ROL 1<<6
-#define R_CRYPTO_ROT 1<<7
-#define R_CRYPTO_BLOWFISH 1<<8
-#define R_CRYPTO_CPS2 1<<9
-#define R_CRYPTO_DES_ECB 1<<10
-#define R_CRYPTO_XOR 1<<11
-#define R_CRYPTO_SERPENT 1<<12
+#define R_CRYPTO_NONE 0ULL
+#define R_CRYPTO_RC2 1ULL
+#define R_CRYPTO_RC4 1ULL<<1
+#define R_CRYPTO_RC6 1ULL<<2
+#define R_CRYPTO_AES_ECB 1ULL<<3
+#define R_CRYPTO_AES_CBC 1ULL<<4
+#define R_CRYPTO_ROR 1ULL<<5
+#define R_CRYPTO_ROL 1ULL<<6
+#define R_CRYPTO_ROT 1ULL<<7
+#define R_CRYPTO_BLOWFISH 1ULL<<8
+#define R_CRYPTO_CPS2 1ULL<<9
+#define R_CRYPTO_DES_ECB 1ULL<<10
+#define R_CRYPTO_XOR 1ULL<<11
+#define R_CRYPTO_SERPENT 1ULL<<12
 #define R_CRYPTO_ALL 0xFFFF
+
+#define R_CODEC_NONE 0ULL
+#define R_CODEC_B64 1ULL
+#define R_CODEC_B91 1ULL<<1
+#define R_CODEC_PUNYCODE 1ULL<<2
+#define R_CODEC_ALL 0xFFFF
 
 #ifdef __cplusplus
 }
