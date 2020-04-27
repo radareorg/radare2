80a01cb4e3e4be9bfe9bd625dee407ead7fdebc5
pancake <pancake@nopcode.org>
2020-04-27 13:10:57 +0200

    Implement r_strbuf_setptr API and improve + test it more ##util (#16649)
    Co-authored-by: Florian Märkl <info@florianmaerkl.de>

--- libr/include/r_util/r_strbuf.h
+++ libr/include/r_util/r_strbuf.h
@@ -7,21 +7,22 @@ extern "C" {
 
 typedef struct {
 	char buf[32];
-	int len;
-	char *ptr;
-	int ptrlen;
+	size_t len; // string length in chars or binary buffer size
+	char *ptr; // ptr replacing buf in case strlen > sizeof(buf)
+	size_t ptrlen; // string length + 1 or binary buffer size
+	bool weakref; // ptr is not owned
 } RStrBuf;
 
 #define R_STRBUF_SAFEGET(sb) (r_strbuf_get (sb) ? r_strbuf_get (sb) : "")
 R_API RStrBuf *r_strbuf_new(const char *s);
 R_API bool r_strbuf_set(RStrBuf *sb, const char *s);
 R_API bool r_strbuf_slice(RStrBuf *sb, int from, int len);
-R_API bool r_strbuf_setbin(RStrBuf *sb, const ut8 *s, int len);
+R_API bool r_strbuf_setbin(RStrBuf *sb, const ut8 *s, size_t len);
 R_API ut8* r_strbuf_getbin(RStrBuf *sb, int *len);
 R_API bool r_strbuf_setf(RStrBuf *sb, const char *fmt, ...);
 R_API bool r_strbuf_vsetf(RStrBuf *sb, const char *fmt, va_list ap);
 R_API bool r_strbuf_append(RStrBuf *sb, const char *s);
-R_API bool r_strbuf_append_n(RStrBuf *sb, const char *s, int l);
+R_API bool r_strbuf_append_n(RStrBuf *sb, const char *s, size_t l);
 R_API bool r_strbuf_prepend(RStrBuf *sb, const char *s);
 R_API bool r_strbuf_appendf(RStrBuf *sb, const char *fmt, ...);
 R_API bool r_strbuf_vappendf(RStrBuf *sb, const char *fmt, va_list ap);
@@ -29,13 +30,15 @@ R_API char *r_strbuf_get(RStrBuf *sb);
 R_API char *r_strbuf_drain(RStrBuf *sb);
 R_API char *r_strbuf_drain_nofree(RStrBuf *sb);
 R_API int r_strbuf_length(RStrBuf *sb);
+R_API int r_strbuf_size(RStrBuf *sb);
 R_API void r_strbuf_free(RStrBuf *sb);
 R_API void r_strbuf_fini(RStrBuf *sb);
 R_API void r_strbuf_init(RStrBuf *sb);
 R_API bool r_strbuf_copy(RStrBuf *dst, RStrBuf *src);
 R_API bool r_strbuf_equals(RStrBuf *sa, RStrBuf *sb);
-R_API bool r_strbuf_reserve(RStrBuf *sb, int len);
+R_API bool r_strbuf_reserve(RStrBuf *sb, size_t len);
 R_API bool r_strbuf_is_empty(RStrBuf *sb);
+R_API bool r_strbuf_setptr(RStrBuf *sb, char *p, int l);
 
 #ifdef __cplusplus
 }
