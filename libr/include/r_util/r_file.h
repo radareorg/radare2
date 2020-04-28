3cde2b1dc0229396e2b714a6734256c36c98bb96
pancake <pancake@nopcode.org>
2020-04-28 06:08:58 +0200

    Boolify some RUtil.file APIs

--- libr/include/r_util/r_file.h
+++ libr/include/r_util/r_file.h
@@ -8,7 +8,7 @@ extern "C" {
 #include <r_util/r_mem.h>
 
 /* is */
-R_API int r_file_is_abspath(const char *file);
+R_API bool r_file_is_abspath(const char *file);
 R_API bool r_file_is_c(const char *file);
 R_API bool r_file_is_directory(const char *str);
 R_API bool r_file_is_regular(const char *str);
@@ -20,7 +20,7 @@ R_API RMmap *r_file_mmap(const char *file, bool rw, ut64 base);
 R_API int r_file_mmap_read(const char *file, ut64 addr, ut8 *buf, int len);
 R_API int r_file_mmap_write(const char *file, ut64 addr, const ut8 *buf, int len);
 R_API void r_file_mmap_free(RMmap *m);
-R_API int r_file_chmod(const char *file, const char *mod, int recursive);
+R_API bool r_file_chmod(const char *file, const char *mod, int recursive);
 R_API char *r_file_temp(const char *prefix);
 R_API char *r_file_path(const char *bin);
 R_API const char *r_file_basename(const char *path);
