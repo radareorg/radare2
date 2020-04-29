07daca36c7dd357e2d5a3949284d89f315dee480
GustavoLCR <gugulcr@gmail.com>
2020-04-29 19:44:09 -0300

    Fix build on VS2017

--- libr/include/r_cons.h
+++ libr/include/r_cons.h
@@ -40,6 +40,9 @@ extern "C" {
 # ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
 # define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
 # endif
+# ifndef ENABLE_VIRTUAL_TERMINAL_INPUT
+# define ENABLE_VIRTUAL_TERMINAL_INPUT 0x0200
+# endif
 #else
 #include <unistd.h>
 #endif
