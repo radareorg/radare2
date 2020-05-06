b07cfd0848c997a824de92796a6deb28ebdd8f52
FXTi <zjxiang1998@gmail.com>
2020-05-07 03:44:52 +0800

    First big num commit, just convert from original impl. Still need more review and correct. Time to sleep.

--- libr/include/r_util/r_big.h
+++ libr/include/r_util/r_big.h
@@ -1,41 +1,100 @@
 #ifndef R_BIG_H
 #define R_BIG_H
 
+#include "../r_types.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #if HAVE_LIB_GMP
+/* Use GMP's data struct */
 #define RNumBig mpz_t
 #elif HAVE_LIB_SSL
+/* Use SSL's data struct */
 #define RNumBig BIGNUM
 #else
-#define	R_BIG_SIZE 10000
+/* Use default impl */
+#ifndef WORD_SIZE
+#define WORD_SIZE 4
+#endif 
+/* Let's support 4096-bit big number */
+#define BN_ARRAY_SIZE (512 / WORD_SIZE)
+#ifndef WORD_SIZE
+#error Must define WORD_SIZE to be 1, 2, 4
+/* If WORD_SIZE == 1, 8 bits long */
+#elif (WORD_SIZE == 1)
+/* Actual element type used during operation */
+#define DTYPE ut8
+#define DTYPE_MSB ((DTYPE_TMP) (0x80))
+/* Middle variable type, must be bigger than DTYPE */
+#define DTYPE_TMP ut32
+/* Type to be passed as variable */
+#define DTYPE_VAR st32
+#define SPRINTF_FORMAT_STR "%.02x"
+#define SSCANF_FORMAT_STR "%2hhx"
+#define MAX_VAL ((DTYPE_TMP)0xFF)
+/* If WORD_SIZE == 2, 16 bits long */
+#elif (WORD_SIZE == 2)
+#define DTYPE ut16
+#define DTYPE_TMP ut32
+#define DTYPE_VAR st32
+#define DTYPE_MSB ((DTYPE_TMP) (0x8000))
+#define SPRINTF_FORMAT_STR "%.04x"
+#define SSCANF_FORMAT_STR "%4hx"
+#define MAX_VAL ((DTYPE_TMP)0xFFFF)
+/* If WORD_SIZE == 4, 32 bits long */
+#elif (WORD_SIZE == 4)
+#define DTYPE ut32
+#define DTYPE_TMP ut64
+#define DTYPE_VAR st64
+#define DTYPE_MSB ((DTYPE_TMP) (0x80000000))
+#define SPRINTF_FORMAT_STR "%.08x"
+#define SSCANF_FORMAT_STR "%8x"
+#define MAX_VAL ((DTYPE_TMP)0xFFFFFFFF)
+#endif
+#ifndef DTYPE
+#error DTYPE must be defined to ut8, ut16 ut32 or whatever
+#endif
+
 typedef struct r_num_big_t {
-	char dgts[R_BIG_SIZE];
-	int sign, last;
+	DTYPE array[BN_ARRAY_SIZE];
+	int sign;
 } RNumBig;
 #endif
 
-R_API RNumBig *r_big_new(RNumBig *b);
+R_API RNumBig *r_big_new();
 R_API void r_big_free(RNumBig *b);
-R_API void r_big_sub(RNumBig *a, RNumBig *b, RNumBig *c);
-R_API void r_big_print(RNumBig *n);
-R_API void r_big_set(RNumBig *a, RNumBig *b);
-R_API void r_big_set_st(RNumBig *n, int v);
-R_API void r_big_set_st64(RNumBig *n, st64 v);
-R_API void r_big_set_str(RNumBig *n, const char *str);
-R_API void r_big_add(RNumBig *c, RNumBig *a, RNumBig *b);
-R_API void r_big_sub(RNumBig *c, RNumBig *a, RNumBig *b);
-R_API int r_big_cmp(RNumBig *a, RNumBig *b);
-R_API int r_big_cmp_st(RNumBig *n, int v);
-R_API void r_big_shift(RNumBig *n, int d);
-R_API void r_big_mul(RNumBig *c, RNumBig *a, RNumBig *b);
-R_API void r_big_mul_ut(RNumBig *c, RNumBig *a, ut32 b);
-R_API void r_big_div(RNumBig *c, RNumBig *a, RNumBig *b);
-R_API void r_big_div_ut(RNumBig *a, RNumBig *b, ut32 c);
-R_API int r_big_divisible_ut(RNumBig *n, ut32 v);
-R_API void r_big_mod(RNumBig *c, RNumBig *a, RNumBig *b);
+
+/* Assignment operations */
+R_API void r_big_from_int(RNumBig *b, DTYPE_VAR v); 
+R_API DTYPE_VAR r_big_to_int(RNumBig *b);
+R_API void r_big_from_hexstr(RNumBig *b, const char *str, int nbytes);
+R_API char *r_big_to_hexstr(RNumBig *b, size_t *size);
+R_API void r_big_assign(RNumBig *dst, RNumBig *src);
+
+/* Basic arithmetic operations */
+R_API void r_big_add(RNumBig *c, RNumBig *a, RNumBig *b); /* c = a + b */
+R_API void r_big_sub(RNumBig *c, RNumBig *a, RNumBig *b); /* c = a - b */
+R_API void r_big_mul(RNumBig *c, RNumBig *a, RNumBig *b); /* c = a * b */
+R_API void r_big_div(RNumBig *c, RNumBig *a, RNumBig *b); /* c = a / b */
+R_API void r_big_mod(RNumBig *c, RNumBig *a, RNumBig *b); /* c = a % b */
+R_API void r_big_divmod(RNumBig *c, RNumBig *d, RNumBig *a, RNumBig *b); /* c = a/b, d = a%b */
+
+/* Bitwise operations */
+R_API void r_big_and(RNumBig *c, RNumBig *a, RNumBig *b); /* c = a & b */
+R_API void r_big_or(RNumBig *c, RNumBig *a, RNumBig *b); /* c = a | b */
+R_API void r_big_xor(RNumBig *c, RNumBig *a, RNumBig *b); /* c = a ^ b */
+R_API void r_big_lshift(RNumBig *c, RNumBig *a, size_t nbits); /* b = a << nbits */
+R_API void r_big_rshift(RNumBig *c, RNumBig *a, size_t nbits); /* b = a >> nbits(ignore sign) */
+
+/* Special operators and comparison */
+R_API int r_big_cmp(RNumBig *a, RNumBig *b); /* Return 1 if a>b, -1 if a<b, else 0 */
+R_API int r_big_is_zero(RNumBig *a); /* For comparison with zero */
+R_API void r_big_inc(RNumBig *a); /* Increment: add one to n */
+R_API void r_big_dec(RNumBig *a); /* Decrement: subtract one from n */
+R_API void r_big_pow(RNumBig *c, RNumBig *a, RNumBig *b); /* Calculate a^b -- e.g. 2^10 => 1024 */
+R_API void r_big_isqrt(RNumBig *c, RNumBig *a); /* Integer square root -- e.g. isqrt(5) => 2*/
 
 #ifdef __cplusplus
 }
