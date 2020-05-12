c599c543d3ab5a7a43a4514f098ceff03ab01e14
FXTi <zjxiang1998@gmail.com>
2020-05-12 13:32:08 +0800

    Remove bignum

--- libr/bin/p/pyc.mk
+++ libr/bin/p/pyc.mk
@@ -2,7 +2,6 @@ OBJ_PYC=bin_pyc.o
 OBJ_PYC+=../format/pyc/marshal.o
 OBJ_PYC+=../format/pyc/pyc_magic.o
 OBJ_PYC+=../format/pyc/pyc.o
-OBJ_PYC+=../format/pyc/bn.o
 
 STATIC_OBJ+=${OBJ_PYC}
 TARGET_PYC=bin_pyc.${EXT_SO}
