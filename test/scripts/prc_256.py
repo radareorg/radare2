eff84d3ec4e1db01df9daba7d35ef40f4fd24e42
Khairul Kasmiran <kazarmy@gmail.com>
2020-05-17 18:36:56 +0800

    Use woe instead

--- test/scripts/prc_256.py
+++ test/scripts/prc_256.py
@@ -1,8 +1,5 @@
 import sys
 
-sys.stdout.write("wx ")
-for i in range(256):
-    sys.stdout.write("%02x" % i)
-sys.stdout.write("\n")
+print("woe 0 0xff 1")
 for i in range(256):
     print("f %02x @ %d" % (i, i))
