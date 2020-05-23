b8b2d5f7e15f33eee208940975b39f17fd7582c2
nirmal <nimmumanoj@gmail.com>
2020-05-24 00:53:29 +0530

    added more documentation and changed the name of core_annotated_code.c

--- libr/include/r_util/r_annotated_code.h
+++ libr/include/r_util/r_annotated_code.h
@@ -1,6 +1,6 @@
 
-#ifndef R2GHIDRA_ANNOTATEDCODE_H
-#define R2GHIDRA_ANNOTATEDCODE_H
+#ifndef R_ANNOTATEDCODE_H
+#define R_ANNOTATEDCODE_H
 
 // #include <r_core.h>
 #include <r_types.h>
@@ -20,6 +20,17 @@ typedef enum r_syntax_highlight_type_t {
 	R_SYNTAX_HIGHLIGHT_TYPE_CONSTANT_VARIABLE,
 	R_SYNTAX_HIGHLIGHT_TYPE_GLOBAL_VARIABLE,
 } RSyntaxHighlightType;
+
+/**
+ * enum r_code_annotation_type_t - typedefed as RCodeAnnotationType and this gives types of annotation
+ *
+ * There are two kinds of RCodeAnnotation. One for offset, which of the type 
+ * R_CODE_ANNOTATION_TYPE_OFFSET and other one is for syntax highlight, which is
+ * of the type R_CODE_ANNOTATION_TYPE_SYNTAX_HIGHLIGHT.
+ * R_CODE_ANNOTATION_TYPE_OFFSET is for representing annotations that gives an offset for 
+ * a range while R_CODE_ANNOTATION_TYPE_SYNTAX_HIGHLIGHT is for representing the
+ * kind of data the range represents. Here, range refers to the range of annotation.
+ */
 
 typedef enum r_code_annotation_type_t {
 	R_CODE_ANNOTATION_TYPE_OFFSET,
@@ -48,76 +59,77 @@ typedef struct r_annotated_code_t {
 } RAnnotatedCode;
 
 /**
-* r_annotated_code_new() - Creates a new RAnnotatedCode structure and returns it's pointer.
-* @code: Literal code for which the RAnnotatedCode structure will be created .
-*
-* This functions creates a new RAnnotatedCode structure.
-* RAnnotatedCode.code will be initialized as the character array passed.
-* This will initialize RVector /*<RCodeAnnotation>*\/ annotations as well.
-* 
-* Return: Pointer to the new RAnnotatedCode structure created.
-*/
+ * r_annotated_code_new() - Creates a new RAnnotatedCode structure and returns it's pointer.
+ * @code: Literal code for which the RAnnotatedCode structure will be created .
+ *
+ * This functions creates a new RAnnotatedCode structure.
+ * RAnnotatedCode.code will be initialized as the character array passed.
+ * Here, code must be a string that can deallocated.
+ * This will initialize RVector /*<RCodeAnnotation>*\/ annotations as well.
+ * 
+ * Return: Pointer to the new RAnnotatedCode structure created.
+ */
 R_API RAnnotatedCode *r_annotated_code_new(char *code);
 /**
-* r_annotated_code_free() - Deallocates *code.
-* @code: Pointer to a RAnnotatedCode.
-*
-* This functions deallocates memory allocated for *code.
-* 
-* Return: Nothing.
-*/
+ * r_annotated_code_free() - Deallocates *code.
+ * @code: Pointer to a RAnnotatedCode.
+ *
+ * This functions deallocates memory allocated for *code.
+ * 
+ * Return: Nothing.
+ */
 R_API void r_annotated_code_free(RAnnotatedCode *code);
 /**
-* r_annotated_code_add_annotation() - Inserts *annotation in *code.
-* @code: Pointer to a RAnnotatedCode.
-* @annotation: Pointer to a annotation.
-*
-* This functions inserts the annotation represented by the pointer 'annotation' to the vector
-* of annotations in the RAnnotatedCode represented by 'code'. To be more precise,
-* annotation will be added to code->annotations, which is a RVector /*<RCodeAnnotation>*\/ annotations.
-* 
-* Return: Nothing.
-*/
+ * r_annotated_code_add_annotation() - Inserts *annotation in *code.
+ * @code: Pointer to a RAnnotatedCode.
+ * @annotation: Pointer to a annotation.
+ *
+ * This functions inserts the annotation represented by the pointer 'annotation' to the vector
+ * of annotations in the RAnnotatedCode represented by 'code'. To be more precise,
+ * annotation will be added to code->annotations, which is a RVector /*<RCodeAnnotation>*\/ annotations.
+ * 
+ * Return: Nothing.
+ */
 R_API void r_annotated_code_add_annotation(RAnnotatedCode *code, RCodeAnnotation *annotation);
 /**
-* r_annotated_code_annotations_in() - Returns all annotations with range that contains the given offset.
-* @code: Pointer to a RAnnotatedCode.
-* @offset: Offset.
-*
-* Creates an RPVector and inserts the pointers to all annotations in which 
-* annotation->start <= offset < annotation->end.
-* This RPVector can have annotations of the type R_CODE_ANNOTATION_TYPE_OFFSET and 
-* R_CODE_ANNOTATION_TYPE_SYNTAX_HIGHLIGHT.
-* 
-* Return: Pointer to the RPVecrtor created.
-*/
+ * r_annotated_code_annotations_in() - Returns all annotations with range that contains the given offset.
+ * @code: Pointer to a RAnnotatedCode.
+ * @offset: Offset.
+ *
+ * Creates an RPVector and inserts the pointers to all annotations in which 
+ * annotation->start <= offset < annotation->end.
+ * This RPVector can have annotations of the type R_CODE_ANNOTATION_TYPE_OFFSET and 
+ * R_CODE_ANNOTATION_TYPE_SYNTAX_HIGHLIGHT.
+ * 
+ * Return: Pointer to the RPVecrtor created.
+ */
 R_API RPVector *r_annotated_code_annotations_in(RAnnotatedCode *code, size_t offset);
 /**
-* r_annotated_code_annotations_range() - Returns all annotations with range that overlap with the given range.
-* @code: Pointer to a RAnnotatedCode.
-* @start: Start of the range(inclusive).
-* @end: End of the range(exclusive).
-*
-* Creates an RPVector and inserts the pointers to all annotations whose 
-* range overlap with range [start, end-1] (both inclusive).
-* 
-* Return: Pointer to the RPVecrtor created.
-*/
+ * r_annotated_code_annotations_range() - Returns all annotations with range that overlap with the given range.
+ * @code: Pointer to a RAnnotatedCode.
+ * @start: Start of the range(inclusive).
+ * @end: End of the range(exclusive).
+ *
+ * Creates an RPVector and inserts the pointers to all annotations whose 
+ * range overlap with range [start, end-1] (both inclusive).
+ * 
+ * Return: Pointer to the RPVecrtor created.
+ */
 R_API RPVector *r_annotated_code_annotations_range(RAnnotatedCode *code, size_t start, size_t end);
 /**
-* r_annotated_code_line_offsets() - Returns the offset for every line of decompiled code in RAnnotatedCode *code.
-* @code: Pointer to a RAnnotatedCode.
-*
-* Creates an RVector and inserts the offsets for every seperate line of decompiled code in
-* code->code (code->code is a character array).
-* If a line of decompiled code doesn't have a unique offset, UT64_MAX is inserted as it's offset.
-* 	
-* Return: Pointer to the RVector created.
-*/
+ * r_annotated_code_line_offsets() - Returns the offset for every line of decompiled code in RAnnotatedCode *code.
+ * @code: Pointer to a RAnnotatedCode.
+ *
+ * Creates an RVector and inserts the offsets for every seperate line of decompiled code in
+ * code->code (code->code is a character array).
+ * If a line of decompiled code doesn't have a unique offset, UT64_MAX is inserted as it's offset.
+ * 	
+ * Return: Pointer to the RVector created.
+ */
 R_API RVector *r_annotated_code_line_offsets(RAnnotatedCode *code);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif //R2GHIDRA_ANNOTATEDCODE_H
+#endif //R_ANNOTATEDCODE_H
