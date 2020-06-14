77870aaa80b1ba342066d7bd40fae4d1d995c6ab
phakeobj <phakeobj@users.noreply.github.com>
2020-06-14 13:51:58 +0300

    Remove redundant conditional free

--- libr/bin/format/mach0/mach0.c
+++ libr/bin/format/mach0/mach0.c
@@ -2051,11 +2051,9 @@ void *MACH0_(mach0_free)(struct MACH0_(obj_t) *mo) {
 		return NULL;
 	}
 
-		if (mo->symbols[i].name) {
-			free (mo->symbols[i].name);
-		}
 	size_t i;
 	for (i = 0; !mo->symbols[i].last; i++) {
+		free (mo->symbols[i].name);
 	}
 	free (mo->symbols);
 	free (mo->segs);
