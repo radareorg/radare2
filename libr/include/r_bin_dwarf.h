c08758cb840bc6ce532547a219cfc42608e5589d
HoundThe <cgkajm@gmail.com>
2020-06-15 16:50:21 +0200

    merge conflict

--- libr/include/r_bin_dwarf.h
+++ libr/include/r_bin_dwarf.h
@@ -806,7 +806,7 @@ typedef struct r_bin_t RBin; // forward declaration so I can keep the functions 
 
 R_API void r_bin_dwarf_free_debug_info(RBinDwarfDebugInfo *inf);
 R_API void r_bin_dwarf_free_debug_abbrev(RBinDwarfDebugAbbrev *da);
-R_API int r_bin_dwarf_parse_info(RBinDwarfDebugAbbrev *da, RBin *a, int mode);
+R_API RBinDwarfDebugInfo *r_bin_dwarf_parse_info(RBinDwarfDebugAbbrev *da, RBin *a, int mode);
 R_API RList *r_bin_dwarf_parse_line(RBin *a, int mode);
 R_API RList *r_bin_dwarf_parse_aranges(RBin *a, int mode);
 R_API RBinDwarfDebugAbbrev *r_bin_dwarf_parse_abbrev(RBin *a, int mode);
