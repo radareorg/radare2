90f72028357ad3a6cb42074173b4a1d967ee3308
meowmeowxw <giovanni.disanti@protonmail.com>
2020-06-16 13:18:53 +0200

    Use r_bin_open instead of r_bin_new

--- libr/core/linux_heap_glibc.c
+++ libr/core/linux_heap_glibc.c
@@ -26,10 +26,12 @@ static GHT GH(get_va_symbol)(RCore *core, const char *path, const char *symname)
 	GHT vaddr = GHT_MAX;
 	RListIter *iter;
 	RBinSymbol *s;
-	RBin *bin = r_bin_new ();
-	RIO *io = r_io_new ();
-	r_io_bind (io, &bin->iob);
-	// RBin *bin = core->bin;
+	const char *bin_file = core->bin->file;
+	ut64 baddr = r_config_get_i (core->config, "bin.baddr");
+	ut64 laddr = r_config_get_i (core->config, "bin.laddr");
+	int fd = r_io_fd_get_current (core->io);
+	int rawstr = core->bin->rawstr;
+	RBin *bin = core->bin;
 	
 	if (!bin) {
 		return vaddr;
@@ -49,8 +51,8 @@ static GHT GH(get_va_symbol)(RCore *core, const char *path, const char *symname)
 		}
 	}
 
-	r_bin_free (bin);
-	r_io_free (io);
+	r_bin_options_init (&opt, fd, baddr, laddr, rawstr);
+	r_bin_open (core->bin, bin_file, &opt);
 	return vaddr;
 }
 
