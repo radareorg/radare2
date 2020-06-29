7a393b64ad0cca0f45e4ff52afb3db89ebf7bf8a
Riccardo Schirone <sirmy15@gmail.com>
2020-06-29 10:04:27 +0200

    Fix comments

--- binr/blob/meson_install_links.sh
+++ binr/blob/meson_install_links.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
+set -e
 
 mkdir -p "${DESTDIR}/${MESON_INSTALL_PREFIX}/bin"
-pushd "${DESTDIR}/${MESON_INSTALL_PREFIX}/bin"
-ln -sf radare2 rahash2
-ln -sf radare2 rarun2
-ln -sf radare2 rasm2
-ln -sf radare2 rabin2
-ln -sf radare2 ragg2
-ln -sf radare2 r2agent
-ln -sf radare2 radiff2
-ln -sf radare2 rafind2
-ln -sf radare2 rasign2
-ln -sf radare2 rax2
-ln -sf radare2 r2
-popd
+cd "${DESTDIR}/${MESON_INSTALL_PREFIX}/bin"
+
+TOOLS="rahash2 rarun2 rasm2 rabin2 ragg2 r2agent radiff2 rafind2 rassign2 rax2 r2"
+
+for TOOL in $TOOLS ; do
+    ln -sf radare2 $TOOL ;
+done
