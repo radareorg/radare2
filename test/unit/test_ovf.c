adc9144ea249a782d46a9fc8511cbcee2f939366
pancake <pancake@nopcode.org>
2020-07-06 14:26:53 +0200

    Cleanup the tests

--- test/unit/test_ovf.c
+++ test/unit/test_ovf.c
@@ -2,121 +2,40 @@
 #include "minunit.h"
 
 int test_overflow_add(void) {
-	int a = 250;
-	int b = 32;
-	if (UT8_ADD_OVFCHK(a, b)) {
-		// ok
-	} else {
-		mu_assert_eq(a, b, "ut8-add 1");
-	}
-	b = 2;
-	if (UT8_ADD_OVFCHK(a, b)) {
-		mu_assert_eq(a, b, "ut8-add 2");
-	} else {
-		// ok
-	}
-	a = ST16_MAX;
-	if (UT16_ADD_OVFCHK(a, b)) {
-		mu_assert_eq(a, b, "ut16-add");
-	} else {
-		// ok
-	}
-	if (ST16_ADD_OVFCHK(a, b)) {
-		// ok
-	} else {
-		mu_assert_eq(a, b, "st16-add");
-	}
-	a = UT16_MAX;
-	b = 1;
-	if (UT16_ADD_OVFCHK(a, b)) {
-		// ok
-	} else {
-		mu_assert_eq(a, b, "ut16-add");
-	}
-	a = 1;
-	b = UT16_MAX;
-	if (UT16_ADD_OVFCHK(a, b)) {
-		// ok
-	} else {
-		mu_assert_eq(a, b, "ut16-add");
-	}
+	mu_assert_true (UT8_ADD_OVFCHK (250, 32), "ut8-add 1");
+	mu_assert_false (UT8_ADD_OVFCHK (250, 2), "ut8-add 2");
+	mu_assert_false (UT16_ADD_OVFCHK (ST16_MAX, 2), "ut16-add 2");
+	mu_assert_true (ST16_ADD_OVFCHK (ST16_MAX, 2), "st16-add 2");
+	mu_assert_true (ST16_ADD_OVFCHK (UT16_MAX, 1), "st16-add 2");
+	mu_assert_true (ST16_ADD_OVFCHK (1, ST16_MAX), "st16-add 3");
 	mu_end;
 }
 
 int test_underflow_sub(void) {
-	int a, b;
-	// underflows
-	a = 10;
-	b = 210;
-	if (ST16_SUB_OVFCHK(a, b)) {
-		// ok
-	} else {
-		mu_assert_eq(a, b, "st16-sub-underflow");
-	}
+	mu_assert_true (ST16_SUB_OVFCHK (10, 210), "st16-sub-underflow");
 	mu_end;
 }
 
 int test_underflow_add(void) {
-	int a, b;
-	// underflows
-	a = 10;
-	b = -210;
-	if (ST16_ADD_OVFCHK(a, b)) {
-		// ok
-	} else {
-		mu_assert_eq(a, b, "st16-add");
-	}
-	a = 10;
-	b = -10;
-	if (ST16_ADD_OVFCHK(a, b)) {
-		mu_assert_eq(a, b, "st16-add");
-	} else {
-		// ok
-	}
-	a = 10;
-	b = -11;
-	if (ST16_ADD_OVFCHK(a, b)) {
-		// ok
-	} else {
-		mu_assert_eq(a, b, "st16-add");
-	}
+	mu_assert_false (ST16_SUB_OVFCHK (10, -210), "st16-add");
+	mu_assert_false (ST16_SUB_OVFCHK (10, 10), "st16-add 10");
+	mu_assert_true (ST16_SUB_OVFCHK (10, 11), "st16-add 10-11");
 	mu_end;
 }
 
 int test_overflow_mul(void) {
-	int a = 16;
-	int b = 32;
-	if (UT8_MUL_OVFCHK(a, b)) {
-		// ok
-	} else {
-		mu_assert_eq(a, b, "ut8-mul");
-	}
-	b = 2;
-	if (UT8_MUL_OVFCHK (a, b)) {
-		mu_assert_eq (a, b, "ut8-mul");
-	} else {
-		// ok
-	}
-	b = 100;
-	if (ST8_MUL_OVFCHK(a, b)) {
-		// ok
-	} else {
-		mu_assert_eq(a, b, "st8-mul");
-	}
-	b = 1;
-	if (ST8_MUL_OVFCHK(a, b)) {
-		mu_assert_eq(a, b, "st8-mul");
-	} else {
-		// ok
-	}
+	mu_assert_true (UT8_MUL_OVFCHK (16, 32), "ut8-mul");
+	mu_assert_false (UT8_MUL_OVFCHK (16, 2), "ut8-mul 2");
+	mu_assert_true (ST8_MUL_OVFCHK (16, 100), "st8-mul 3");
+	mu_assert_false (ST8_MUL_OVFCHK (16, 1), "st8-mul 4");
 	mu_end;
 }
 
 int all_tests() {
-	mu_run_test(test_overflow_add);
-	mu_run_test(test_underflow_add);
-	mu_run_test(test_underflow_sub);
-	mu_run_test(test_overflow_mul);
+	mu_run_test (test_overflow_add);
+	mu_run_test (test_underflow_add);
+	mu_run_test (test_underflow_sub);
+	mu_run_test (test_overflow_mul);
 	return tests_passed != tests_run;
 }
 
