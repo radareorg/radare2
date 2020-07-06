bcf4af12a275a0d20fc472b0110fb2c7f9696253
RHL120 <60626307+RHL120@users.noreply.github.com>
2020-07-06 10:41:55 +0200

    rename r_core_project_save_rdb to r_core_project_save_script (#17208)

--- libr/core/cmd_project.c
+++ libr/core/cmd_project.c
@@ -96,7 +96,7 @@ static int cmd_project(void *data, const char *input) {
 		break;
 	case 'S':
 		if (input[1] == ' ') {
-			r_core_project_save_rdb (core, input + 2, R_CORE_PRJ_ALL);
+			r_core_project_save_script (core, input + 2, R_CORE_PRJ_ALL);
 		} else {
 			eprintf ("Usage: PS [file]\n");
 		}
