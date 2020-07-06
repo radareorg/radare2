d3d6d4e067a7861caf1a273768df0f3c94dd72f0
pancake <pancake@nopcode.org>
2020-07-06 14:15:18 +0200

    dont if0

--- libr/include/r_types_overflow.h
+++ libr/include/r_types_overflow.h
@@ -1,16 +1,11 @@
 #ifndef R_TYPES_OVERFLOW_H
 #define R_TYPES_OVERFLOW_H
 
-/* preventive math overflow checks */
-#if !defined(SZT_ADD_OVFCHK)
-#define SZT_ADD_OVFCHK(x,y) ((SIZE_MAX - (x)) < (y))
-#endif
-
-#if 0
 // ADD
-if ((x > 0) && (a > INT_MAX - x)) /* `a + x` would overflow */;
-if ((x < 0) && (a < INT_MIN - x)) /* `a + x` would underflow */;
-#endif
+// if ((x > 0) && (a > INT_MAX - x)) /* `a + x` would overflow */;
+// if ((x < 0) && (a < INT_MIN - x)) /* `a + x` would underflow */;
+#define SZT_ADD_OVFCHK(x,y) ((SIZE_MAX - (x)) < (y))
+#define SSZT_ADD_OVFCHK(x,y) ((SSIZE_MAX - (x)) < (y))
 #define UT64_ADD_OVFCHK(x,y) ((UT64_MAX - (x)) < (y))
 #define ST64_ADD_OVFCHK(a,x) ((((x) > 0) && ((a) > ST64_MAX - (x))) || ((x) <0 &&(a)< ST32_MIN - (x)))
 #define UT32_ADD_OVFCHK(x,y) ((UT32_MAX - (x)) < (y))
@@ -20,6 +15,10 @@ if ((x < 0) && (a < INT_MIN - x)) /* `a + x` would underflow */;
 #define UT8_ADD_OVFCHK(x,y) ((UT8_MAX - (x)) < (y))
 #define ST8_ADD_OVFCHK(a,x) ((((x) > 0) && ((a) > ST8_MAX - (x))) || ((x) <0 &&(a)< ST8_MIN - (x)))
 // SUB
+// if ((x < 0) && (a > INT_MAX + x)) /* `a - x` would overflow */;
+// if ((x > 0) && (a < INT_MIN + x)) /* `a - x` would underflow */;
+#define SZT_SUB_OVFCHK(a,b) SZT_ADD_OVFCHK(a,-b)
+#define SSZT_SUB_OVFCHK(a,b) SSZT_ADD_OVFCHK(a,-b)
 #define UT64_SUB_OVFCHK(a,b) UT64_ADD_OVFCHK(a,-b)
 #define ST64_SUB_OVFCHK(a,b) ST64_ADD_OVFCHK(a,-b)
 #define UT32_SUB_OVFCHK(a,b) UT32_ADD_OVFCHK(a,-b)
@@ -30,6 +29,12 @@ if ((x < 0) && (a < INT_MIN - x)) /* `a + x` would underflow */;
 #define ST8_SUB_OVFCHK(a,b) ST8_ADD_OVFCHK(a,-b)
 
 // MUL UT64
+//if ((a == -1) && (x == INT_MIN)) /* `a * x` can overflow */
+//if ((x == -1) && (a == INT_MIN)) /* `a * x` (or `a / x`) can overflow */
+//if (a > INT_MAX / x) /* `a * x` would overflow */;
+//if ((a < INT_MIN / x)) /* `a * x` would underflow */;
+#define SZT_MUL_OVFCHK(x,y) ((x) > (SIZE_MAX / (y)))
+#define SSZT_MUL_OVFCHK(x,y) ((x) > (SSIZE_MAX / (y)))
 #define UT64_MUL_OVFCHK(x,y) ((x) > (UT64_MAX / (y)))
 #define ST64_MUL_OVFCHK(x,y) ((x) > (ST64_MAX / (y)))
 #define UT32_MUL_OVFCHK(x,y) ((x) > (UT32_MAX / (y)))
@@ -39,15 +44,5 @@ if ((x < 0) && (a < INT_MIN - x)) /* `a + x` would underflow */;
 #define UT8_MUL_OVFCHK(x,y) ((x) > (UT8_MAX / (y)))
 #define ST8_MUL_OVFCHK(x,y) ((x) > (ST8_MAX / (y)))
 // #define ST8_MUL_OVFCHK(a,x) ((x>0) && (a)>(ST8_MIN / (x)))
-#if 0
-// SUB
-if ((x < 0) && (a > INT_MAX + x)) /* `a - x` would overflow */;
-if ((x > 0) && (a < INT_MIN + x)) /* `a - x` would underflow */;
-// MUL
-if ((a == -1) && (x == INT_MIN)) /* `a * x` can overflow */
-if ((x == -1) && (a == INT_MIN)) /* `a * x` (or `a / x`) can overflow */
-if (a > INT_MAX / x) /* `a * x` would overflow */;
-if ((a < INT_MIN / x)) /* `a * x` would underflow */;
-#endif
 
 #endif
