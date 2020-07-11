9b6a04f7373f78d7f97460bdc64a755b0916c345
GustavoLCR <gugulcr@gmail.com>
2020-07-11 20:54:55 -0300

    Detect stack vars accesses with misc regs with 'aaef' ##anal
    * Follow bbs in r_core_anal_esil for functions
    * Fix ARM PUSH/POP stackop analysis
    * Add tests
    * Fix tests for ARM

--- libr/parse/p/parse_arm_pseudo.c
+++ libr/parse/p/parse_arm_pseudo.c
@@ -324,26 +324,39 @@ static bool varsub(RParse *p, RAnalFunction *f, ut64 addr, int oplen, char *data
 	bool ucase = IS_UPPER (*tstr);
 	RAnalVarField *var;
 	r_list_foreach (bpargs, iter, var) {
-		char *reg = anal->reg->name[R_REG_NAME_BP];
 		char *tmplt = NULL;
-		if (var->delta > -10 && var->delta < 10) {
+		st64 delta = ST64_MAX;
+		if (p->get_ptr_at) {
+			delta = p->get_ptr_at (f, var->delta, addr);
+		}
+		if (delta == ST64_MAX && var->field) {
+			delta = var->delta + f->bp_off;
+		} else if (delta == ST64_MAX) {
+			continue;
+		}
+		const char *reg = NULL;
+		if (p->get_reg_at) {
+			reg = p->get_reg_at (f, var->delta, addr);
+		}
+		if (!reg) {
+			reg = anal->reg->name[R_REG_NAME_BP];
+		}
+		if (delta > -10 && delta < 10) {
 			if (p->pseudo) {
 				char sign = '+';
-				int delta = var->delta;
-				if (var->delta < 0) {
+				if (delta < 0) {
 					sign = '-';
-					delta = -delta;
 				}
-				oldstr = r_str_newf ("[%s %c %d]", reg, sign, delta);
+				oldstr = r_str_newf ("[%s %c %d]", reg, sign, R_ABS (delta));
 			} else {
-				oldstr = r_str_newf ("[%s, %d]", reg, var->delta);
+				oldstr = r_str_newf ("[%s, %d]", reg, delta);
 			}
-		} else if (var->delta > 0) {
+		} else if (delta > 0) {
 			tmplt = p->pseudo ? "[%s + 0x%x]" : (ucase ? "[%s, 0x%X]" : "[%s, 0x%x]");
-			oldstr = r_str_newf (tmplt, reg, var->delta);
+			oldstr = r_str_newf (tmplt, reg, delta);
 		} else {
 			tmplt = p->pseudo ? "[%s - 0x%x]" : (ucase ? "[%s, -0x%X]" : "[%s, -0x%x]");
-			oldstr = r_str_newf (tmplt, reg, -var->delta);
+			oldstr = r_str_newf (tmplt, reg, -delta);
 		}
 		if (ucase) {
 			char *comma = strchr (oldstr, ',');
@@ -358,7 +371,7 @@ static bool varsub(RParse *p, RAnalFunction *f, ut64 addr, int oplen, char *data
 				newstr = r_str_newf ("[%s]", var->name);
 			} else {
 				newstr = r_str_newf ("[%s %c %s]",
-					reg, var->delta > 0 ? '+' : '-', var->name);
+					reg, delta > 0 ? '+' : '-', var->name);
 			}
 			if (ucase) {
 				char *comma = strchr (newstr, ' ');
@@ -376,19 +389,32 @@ static bool varsub(RParse *p, RAnalFunction *f, ut64 addr, int oplen, char *data
 		free (oldstr);
 	}
 	r_list_foreach (spargs, iter, var) {
+		st64 delta = ST64_MAX;
 		if (p->get_ptr_at) {
-			var->delta = p->get_ptr_at (f, var->delta, addr);
+			delta = p->get_ptr_at (f, var->delta, addr);
 		}
-		if (var->delta > -10 && var->delta < 10) {
-			oldstr = r_str_newf ("[sp, %d]", var->delta);
-		} else if (var->delta > 0) {
-			oldstr = r_str_newf ("[sp, 0x%x]", var->delta);
+		if (delta == ST64_MAX && var->field) {
+			delta = var->delta;
+		} else if (delta == ST64_MAX) {
+			continue;
+		}
+		const char *reg = NULL;
+		if (p->get_reg_at) {
+			reg = p->get_reg_at (f, var->delta, addr);
+		}
+		if (!reg) {
+			reg = anal->reg->name[R_REG_NAME_SP];
+		}
+		if (delta > -10 && delta < 10) {
+			oldstr = r_str_newf ("[%s, %d]", reg, delta);
+		} else if (delta > 0) {
+			oldstr = r_str_newf ("[%s, 0x%x]", reg, delta);
 		} else {
-			oldstr = r_str_newf ("[sp, -0x%x]", -var->delta);
+			oldstr = r_str_newf ("[%s, -0x%x]", reg, -delta);
 		}
 		if (strstr (tstr, oldstr)) {
-			newstr = r_str_newf ("[sp %c %s]",
-				var->delta > 0 ? '+' : '-',
+			newstr = r_str_newf ("[%s %c %s]",
+				reg, delta > 0 ? '+' : '-',
 				var->name);
 			tstr = r_str_replace (tstr, oldstr, newstr, 1);
 			free (newstr);
@@ -396,23 +422,23 @@ static bool varsub(RParse *p, RAnalFunction *f, ut64 addr, int oplen, char *data
 			break;
 		}
 		free (oldstr);
-		if (var->delta > -10 && var->delta < 10) {
+		if (delta > -10 && delta < 10) {
 			oldstr = r_str_newf ("[%s, %d]",
-				anal->reg->name[R_REG_NAME_SP],
-				var->delta);
-		} else if (var->delta > 0) {
+				reg,
+				delta);
+		} else if (delta > 0) {
 			oldstr = r_str_newf ("[%s, 0x%x]",
-				anal->reg->name[R_REG_NAME_SP],
-				var->delta);
+				reg,
+				delta);
 		} else {
 			oldstr = r_str_newf ("[%s, -0x%x]",
-				anal->reg->name[R_REG_NAME_SP],
-				-var->delta);
+				reg,
+				-delta);
 		}
 		if (strstr (tstr, oldstr)) {
 			newstr = r_str_newf ("[%s %c %s]",
-				anal->reg->name[R_REG_NAME_BP],
-				var->delta > 0 ? '+' : '-',
+				reg,
+				delta > 0 ? '+' : '-',
 				var->name);
 			tstr = r_str_replace (tstr, oldstr, newstr, 1);
 			free (newstr);
