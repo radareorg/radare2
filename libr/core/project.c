078b3628d1a039257f1c45129af49cdc0733e6ea
rhl120 <rhl120@protonmail.com>
2020-07-13 20:09:01 +0200

    fixed non debug projects by reoppening the binary file

--- libr/core/project.c
+++ libr/core/project.c
@@ -630,7 +630,10 @@ static bool simpleProjectSaveScript(RCore *core, const char *file, int opts) {
 
 static bool projectSaveScript(RCore *core, const char *file, int opts) {
 	char *filename, *hl, *ohl = NULL;
+	char *binpath = r_file_abspath(core->bin->file);
+	char  *reopen = (char *) r_malloc(strlen(binpath) + 32);
 	int fd, fdold;
+	sprintf(reopen, "\"o %s\"\n", binpath);
 
 	if (!file || *file == '\0') {
 		return false;
@@ -653,6 +656,9 @@ static bool projectSaveScript(RCore *core, const char *file, int opts) {
 	r_cons_singleton ()->fdout = fd;
 	r_cons_singleton ()->context->is_interactive = false;
 	r_str_write (fd, "# r2 rdb project file\n");
+        r_str_write(fd, reopen);
+	free(reopen);
+	free(binpath);
 	// Set file.path and file.lastpath to empty string to signal
 	// new behaviour to project load routine (see io maps below).
 	r_config_set (core->config, "file.path", "");
@@ -779,6 +785,7 @@ R_API bool r_core_project_save(RCore *core, const char *prjName) {
 #if TRANSITION
 			r_file_rm (scriptPath);
 			r_sys_mkdirp (prjDir);
+
 			eprintf ("Please remove: rm -rf %s %s.d\n", prjName, prjName);
 			char *rc = r_str_newf ("%s" R_SYS_DIR "rc", prjDir);
 			if (!rc) {
