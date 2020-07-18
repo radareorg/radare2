176f74e205e0a3edd1941a2c37fc0ba4c3a92dac
RHL120 <60626307+RHL120@users.noreply.github.com>
2020-07-18 19:15:45 +0200

    fixed

--- libr/core/project.c
+++ libr/core/project.c
@@ -663,13 +663,12 @@ static bool project_save_script(RCore *core, const char *file, int opts) {
 	r_cons_singleton ()->context->is_interactive = false;
 	r_str_write (fd, "# r2 rdb project file\n");
 	if (!core->bin->is_debugger && !r_config_get_i (core->config, "asm.emu")) {
-		char *fpath = r_file_abspath (core->bin->file);
-		if (fpath != NULL){
-		char *reopen = r_str_newf ("\"o %s\"\n",fpath);
-		r_str_write (fd, reopen);
-		free (reopen);
-		free (fpath);
-
+			char *fpath = r_file_abspath (core->bin->file);
+			if (fpath){
+				char *reopen = r_str_newf ("\"o %s\"\n",fpath);
+				r_str_write (fd, reopen);
+				free (reopen);
+				free (fpath);
 		}
 	}
 	// Set file.path and file.lastpath to empty string to signal
