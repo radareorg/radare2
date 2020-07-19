72b379a658a85df9f122edb3747760ef55a574cd
pancake <pancake@nopcode.org>
2020-07-19 18:11:21 +0200

    r2 doesnt exist for meson

--- test/unit/test_r2pipe.c
+++ test/unit/test_r2pipe.c
@@ -4,7 +4,7 @@
 
 bool test_r2pipe(void) {
 	// R2Pipe *r = r2pipe_open ("/usr/local/bin/r2");
-	R2Pipe *r = r2pipe_open ("r2 -q0 -");
+	R2Pipe *r = r2pipe_open ("radare2 -q0 -");
 	mu_assert("r2pipe can spawn", r);
 	char *hello = r2pipe_cmd (r, "?e hello world");
 	mu_assert_streq(hello, "hello world\n", "r2pipe hello world");
