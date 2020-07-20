03d510d00ba676df637f3f7a26bc096d67d8467a
Paul I <pelijah@users.noreply.github.com>
2020-07-20 09:25:46 +0300

    Don't print reloc addend if it is 0 (#17312)
    * Don't print reloc addend if it is 0
    * Handle negative values

--- libr/core/disasm.c
+++ libr/core/disasm.c
@@ -1870,14 +1870,14 @@ static void ds_show_functions(RDisasmState *ds) {
 		}
 		r_cons_printf ("%d: ", r_anal_function_realsize (f));
 
-		// show function's realname in the signature if realnames are enabled 
+		// show function's realname in the signature if realnames are enabled
 		if (core->flags->realnames) {
 			RFlagItem *flag = r_flag_get (core->flags, fcn_name);
 			if (flag && flag->realname) {
 				fcn_name = flag->realname;
 			}
 		}
-	    
+
 		char *sig = r_anal_fcn_format_sig (core->anal, f, fcn_name, &vars_cache, COLOR (ds, color_fname), COLOR_RESET (ds));
 		if (sig) {
 			r_cons_print (sig);
@@ -4074,7 +4074,7 @@ static void ds_print_ptr(RDisasmState *ds, int len, int idx) {
 					ds_print_str (ds, msg, len, refaddr);
 					string_printed = true;
 				}
-			} else if (!flag_printed && (!ds->opstr || 
+			} else if (!flag_printed && (!ds->opstr ||
 						(!strstr (ds->opstr, f->name) && !strstr (ds->opstr, f->realname)))) {
 				ds_begin_nl_comment (ds);
 				ds_comment (ds, true, "; %s", f->name);
@@ -4209,9 +4209,16 @@ static void ds_print_relocs(RDisasmState *ds) {
 			if (demangle) {
 				demname = r_bin_demangle (core->bin->cur, lang, rel->symbol->name, rel->symbol->vaddr, keep_lib);
 			}
-			r_cons_printf ("; RELOC %d %s @ 0x%08" PFMT64x " + 0x%" PFMT64x,
+			r_cons_printf ("; RELOC %d %s @ 0x%08" PFMT64x,
 					rel->type, demname ? demname : rel->symbol->name,
-					rel->symbol->vaddr, rel->addend);
+					rel->symbol->vaddr);
+			if (rel->addend) {
+				if (rel->addend > 0) {
+					r_cons_printf (" + 0x%" PFMT64x, rel->addend);
+				} else {
+					r_cons_printf (" - 0x%" PFMT64x, -rel->addend);
+				}
+			}
 		} else {
 			r_cons_printf ("; RELOC %d ", rel->type);
 		}
