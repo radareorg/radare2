2e5a336d95a25e1d081aa50e756be5afe7ed0c5d
pancake <pancake@nopcode.org>
2020-07-22 23:30:42 +0200

    Minor improvements

--- test/unit/test_r2pipe.c
+++ test/unit/test_r2pipe.c
@@ -2,30 +2,28 @@
 #include <r_socket.h>
 #include "minunit.h"
 
-bool test_r2pipe(void) {
-	// R2Pipe *r = r2pipe_open ("/usr/local/bin/r2");
+static bool test_r2pipe(void) {
 	R2Pipe *r = r2pipe_open ("radare2 -q0 -");
-	mu_assert("r2pipe can spawn", r);
+	mu_assert ("r2pipe can spawn", r);
 	char *hello = r2pipe_cmd (r, "?e hello world");
-	mu_assert_streq(hello, "hello world\n", "r2pipe hello world");
+	mu_assert_streq (hello, "hello world\n", "r2pipe hello world");
 	free (hello);
 	r2pipe_close (r);
 	mu_end;
 }
 
-bool test_r2pipe_404(void) {
-	// R2Pipe *r = r2pipe_open ("/usr/local/bin/r2");
+static bool test_r2pipe_404(void) {
 	R2Pipe *r = r2pipe_open ("rodoro2 -q0 -");
-	mu_assert("r2pipe can spawn", !r);
+	mu_assert ("r2pipe can spawn", !r);
 	mu_end;
 }
 
-int all_tests() {
-	mu_run_test(test_r2pipe);
-	mu_run_test(test_r2pipe_404);
+static int all_tests() {
+	mu_run_test (test_r2pipe);
+	mu_run_test (test_r2pipe_404);
 	return tests_passed != tests_run;
 }
 
 int main(int argc, char **argv) {
-	return all_tests();
+	return all_tests ();
 }
