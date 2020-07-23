bbefd6d0397b7fcbca6db55cd4839094f29bcf0f
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2020-07-23 21:26:20 +0800

    Set up and use hConnected event object

--- libr/lang/p/pipe.c
+++ libr/lang/p/pipe.c
@@ -36,21 +36,25 @@ static HANDLE myCreateChildProcess(const char * szCmdline) {
 static volatile BOOL bStopPipeLoop = FALSE;
 static HANDLE hPipeInOut = NULL;
 static HANDLE hproc = NULL;
+static HANDLE hConnected = NULL;
 #define PIPE_BUF_SIZE 8192
 
 static DWORD WINAPI WaitForProcThread(LPVOID lParam) {
-	WaitForSingleObject (hproc, INFINITE);
+	HANDLE hEvents[] = { hConnected, hproc };
+	DWORD dwEvent = WaitForMultipleObjects (R_ARRAY_SIZE (hEvents), hEvents, FALSE, INFINITE);
 
-	// Just in case the #!pipe target didn't actually connect to the pipe,
-	// we connect to the pipe here to satisfy the ConnectNamedPipe().
-	LPTSTR pipeName = calloc (128, sizeof (TCHAR));
-	GetEnvironmentVariable (TEXT ("R2PIPE_PATH"), pipeName, 128);
-	HANDLE pipe = CreateFile (pipeName, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
-	if (pipe != INVALID_HANDLE_VALUE) {
-		CloseHandle (pipe);
+	if (dwEvent == WAIT_OBJECT_0 + 1) {
+		// Just in case the #!pipe target didn't actually connect to the pipe,
+		// we connect to the pipe here to satisfy the ConnectNamedPipe().
+		LPTSTR pipeName = calloc (128, sizeof (TCHAR));
+		GetEnvironmentVariable (TEXT ("R2PIPE_PATH"), pipeName, 128);
+		HANDLE pipe = CreateFile (pipeName, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+		if (pipe != INVALID_HANDLE_VALUE) {
+			CloseHandle (pipe);
+		}
+
+		bStopPipeLoop = TRUE;
 	}
-
-	bStopPipeLoop = TRUE;
 	return 0;
 }
 static void lang_pipe_run_win(RLang *lang) {
@@ -231,15 +235,29 @@ static int lang_pipe_run(RLang *lang, const char *code, int len) {
 	hproc = myCreateChildProcess (code);
 	bool connected = false;
 	if (hproc) {
+		hConnected = CreateEvent (NULL, FALSE, FALSE, NULL);
+		if (!hConnected) {
+			eprintf ("CreateEvent failed: %#x\n", (int)GetLastError ());
+			goto pipe_cleanup;
+		}
 		/* a separate thread is created that sets bStopPipeLoop once hproc terminates. */
 		bStopPipeLoop = FALSE;
-		CloseHandle (CreateThread (NULL, 0, WaitForProcThread, NULL, 0, NULL));
+		HANDLE hConnectThread = CreateThread (NULL, 0, WaitForProcThread, NULL, 0, NULL);
 		connected = ConnectNamedPipe (hPipeInOut, NULL) ? true : (GetLastError () == ERROR_PIPE_CONNECTED);
+		if (connected) {
+			if (SetEvent (hConnected)) {
+				WaitForSingleObject (hConnectThread, INFINITE);
+				CloseHandle (CreateThread (NULL, 0, WaitForProcThread, NULL, 0, NULL));
+				/* lang_pipe_run_win has to run in the command thread to prevent deadlock. */
+				lang_pipe_run_win (lang);
+			} else {
+				eprintf ("SetEvent failed: %#x\n", (int)GetLastError ());
+			}
+		}
+		CloseHandle (hConnectThread);
+		CloseHandle (hConnected);
 	}
-	if (hproc && connected) {
-		/* lang_pipe_run_win has to run in the command thread to prevent deadlock. */
-		lang_pipe_run_win (lang);
-	}
+pipe_cleanup:
 	DeleteFile (r2pipe_paz_);
 	CloseHandle (hPipeInOut);
 beach:
