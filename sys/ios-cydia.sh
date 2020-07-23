6ccad1972d63f304e89156faf481298fd1eebf75
pancake <pancake@nopcode.org>
2020-07-23 20:25:02 +0200

    More fixes

--- sys/ios-cydia.sh
+++ sys/ios-cydia.sh
@@ -102,8 +102,7 @@ else
 		RV=$?
 	fi
 	if [ $RV = 0 ]; then
-		time make -j4
-		time make -j4 -C libr
+		time make -j4 || exit 1
 		if [ "$static" = 1 ]; then
 			ls -l libr/util/libr_util.a || exit 1
 			ls -l libr/flag/libr_flag.a || exit 1
