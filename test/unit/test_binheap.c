eb946fb4d07e2bb29fd8cfde975aee38d6a4895e
Riccardo Schirone <sirmy15@gmail.com>
2020-07-31 10:53:51 +0200

    Try to Fix binheap tests on windows

--- test/unit/test_binheap.c
+++ test/unit/test_binheap.c
@@ -2,16 +2,16 @@
 #include "minunit.h"
 
 int basic_cmp(const void *a, const void *b) {
-	ssize_t sa = (ssize_t)a;
-	ssize_t sb = (ssize_t)b;
-	return (sa - sb);
+	size_t sa = (size_t)a;
+	size_t sb = (size_t)b;
+	return sa >= sb? sa - sb: -1;
 }
 
 bool test_basic(void) {
 	RBinHeap *bh = r_binheap_new (basic_cmp);
 	mu_assert_notnull (bh, "binheap is created");
 	mu_assert_true (r_binheap_empty (bh), "binheap is empty");
-	r_binheap_push (bh, (void *)(ssize_t)10);
+	r_binheap_push (bh, (void *)(size_t)10);
 	mu_assert_false (r_binheap_empty (bh), "binheap is not empty anymore");
 	r_binheap_clear (bh);
 	mu_assert_true (r_binheap_empty (bh), "binheap is empty again after clear");
@@ -21,21 +21,21 @@ bool test_basic(void) {
 
 bool test_pushpop(void) {
 	RBinHeap *bh = r_binheap_new (basic_cmp);
-	r_binheap_push (bh, (void *)(ssize_t)10);
-	mu_assert_eq ((ssize_t)r_binheap_top (bh), (ssize_t)10, "10 is the top");
-	mu_assert_eq ((ssize_t)r_binheap_pop (bh), (ssize_t)10, "10 is popped");
+	r_binheap_push (bh, (void *)(size_t)10);
+	mu_assert_eq ((size_t)r_binheap_top (bh), (size_t)10, "10 is the top");
+	mu_assert_eq ((size_t)r_binheap_pop (bh), (size_t)10, "10 is popped");
 	mu_assert_true (r_binheap_empty (bh), "the only element has been popped out");
-	r_binheap_push (bh, (void *)(ssize_t)10);
-	r_binheap_push (bh, (void *)(ssize_t)2);
-	r_binheap_push (bh, (void *)(ssize_t)5);
-	r_binheap_push (bh, (void *)(ssize_t)4);
-	r_binheap_push (bh, (void *)(ssize_t)11);
-	mu_assert_eq ((ssize_t)r_binheap_top (bh), (ssize_t)2, "2 is the top");
-	mu_assert_eq ((ssize_t)r_binheap_pop (bh), (ssize_t)2, "2 is popped");
-	mu_assert_eq ((ssize_t)r_binheap_pop (bh), (ssize_t)4, "4 is popped");
-	mu_assert_eq ((ssize_t)r_binheap_pop (bh), (ssize_t)5, "5 is popped");
-	mu_assert_eq ((ssize_t)r_binheap_pop (bh), (ssize_t)10, "10 is popped");
-	mu_assert_eq ((ssize_t)r_binheap_pop (bh), (ssize_t)11, "11 is popped");
+	r_binheap_push (bh, (void *)(size_t)10);
+	r_binheap_push (bh, (void *)(size_t)2);
+	r_binheap_push (bh, (void *)(size_t)5);
+	r_binheap_push (bh, (void *)(size_t)4);
+	r_binheap_push (bh, (void *)(size_t)11);
+	mu_assert_eq ((size_t)r_binheap_top (bh), (size_t)2, "2 is the top");
+	mu_assert_eq ((size_t)r_binheap_pop (bh), (size_t)2, "2 is popped");
+	mu_assert_eq ((size_t)r_binheap_pop (bh), (size_t)4, "4 is popped");
+	mu_assert_eq ((size_t)r_binheap_pop (bh), (size_t)5, "5 is popped");
+	mu_assert_eq ((size_t)r_binheap_pop (bh), (size_t)10, "10 is popped");
+	mu_assert_eq ((size_t)r_binheap_pop (bh), (size_t)11, "11 is popped");
 	r_binheap_free (bh);
 	mu_end;
 }
