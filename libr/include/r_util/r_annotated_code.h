3528bdedc58ed8c3c840aba82f431c7d316cbcb8
NIRMAL MANOJ C <nimmumanoj@gmail.com>
2020-08-03 20:12:16 +0530

    update docs in r_annotated_code.h

--- libr/include/r_util/r_annotated_code.h
+++ libr/include/r_util/r_annotated_code.h
@@ -79,105 +79,92 @@ typedef struct r_annotated_code_t {
 	char *code; // owned
 	RVector /*<RCodeAnnotation>*/ annotations;
 } RAnnotatedCode;
-
 /**
- * r_annotated_code_new() - Creates a new RAnnotatedCode structure and returns its pointer.
- * @code: Literal code for which the RAnnotatedCode structure will be created .
- *
  * This functions creates a new RAnnotatedCode structure.
  * RAnnotatedCode.code will be initialized as the character array passed.
- * Here, code must be a string that can deallocated.
+ * Here, code must be a string that can be deallocated.
  * This will initialize RVector<RCodeAnnotation> annotations as well.
+ * @brief Creates a new RAnnotatedCode structure and returns its pointer.
  * 
- * Return: Pointer to the new RAnnotatedCode structure created.
+ * @param code Literal code for which the RAnnotatedCode structure will be created.
+ * @return Pointer to the new RAnnotatedCode structure created.
  */
 R_API RAnnotatedCode *r_annotated_code_new(char *code);
 /**
- * r_annotated_code_free() - Deallocates *code.
- * @code: Pointer to a RAnnotatedCode.
- *
  * This functions deallocates memory allocated for *code.
+ * @brief Deallocates *code.
  * 
- * Return: Nothing.
+ * @param code Pointer to a RAnnotatedCode.
  */
 R_API void r_annotated_code_free(RAnnotatedCode *code);
 /**
- * r_annotation_free() - Deallocates dynamically allocated memory for the specified annotation.
- * @e: Pointer to the annotation
- * @user: Always NULL for this function. Present here for this function to be of the type RVectorFree.
- * 
  * This function recognizes the type of the specified annotation and
  * frees memory that is dynamically allocated for it.
+ * @brief Deallocates dynamically allocated memory for the specified annotation.
  * 
- * Return: Nothing.
+ * @param e Pointer to the annotation.
+ * @param user Always NULL for this function. Present here for this function to be of the type RVectorFree.
  */
 R_API void r_annotation_free(void *e, void *user);
 /**
- * r_annotation_is_reference() - Checks if the specified annotation is a reference.
- * @annotation: Pointer to an annotation.
- * 
  * This function recognizes the type of the specified annotation and returns true if its
  * type is any of the following three: R_CODE_ANNOTATION_TYPE_GLOBAL_VARIABLE,
  * R_CODE_ANNOTATION_TYPE_CONSTANT_VARIABLE, R_CODE_ANNOTATION_TYPE_FUNCTION_NAME
+ * @brief Checks if the specified annotation is a reference.
  * 
- * Return: Returns true if the specified annotation is a reference.
+ * @param annotation Pointer to an annotation.
+ * @return Returns true if the specified annotation is a reference.
  */
 R_API bool r_annotation_is_reference(RCodeAnnotation *annotation);
 /**
- * r_annotation_is_variable() - Checks if the specified annotation is a function variable.
- * @annotation: Pointer to an annotation.
- * 
  * This function recognizes the type of the specified annotation and returns true if its
  * type is any of the following two: R_CODE_ANNOTATION_TYPE_LOCAL_VARIABLE,
  * R_CODE_ANNOTATION_TYPE_FUNCTION_PARAMETER
+ * @brief Checks if the specified annotation is a function variable.
  * 
- * Return: Returns true if the specified annotation is a function variable.
+ * @param annotation Pointer to an annotation.
+ * @return Returns true if the specified annotation is a function variable.
  */
 R_API bool r_annotation_is_variable(RCodeAnnotation *annotation);
 /**
- * r_annotated_code_add_annotation() - Inserts *annotation in *code.
- * @code: Pointer to a RAnnotatedCode.
- * @annotation: Pointer to a annotation.
- *
  * This functions inserts the annotation represented by the pointer 'annotation' to the vector
  * of annotations in the RAnnotatedCode represented by 'code'. To be more precise,
  * annotation will be added to code->annotations, which is a RVector<RCodeAnnotation> annotations.
+ * @brief Inserts *annotation in *code.
  * 
- * Return: Nothing.
+ * @param code Pointer to a RAnnotatedCode.
+ * @param annotation Pointer to an annotation.
  */
 R_API void r_annotated_code_add_annotation(RAnnotatedCode *code, RCodeAnnotation *annotation);
 /**
- * r_annotated_code_annotations_in() - Returns all annotations with range that contains the given offset.
- * @code: Pointer to a RAnnotatedCode.
- * @offset: Offset.
- *
  * Creates an RPVector and inserts the pointers to all annotations in which 
  * annotation->start <= offset < annotation->end.
+ * @brief Returns all annotations with range that contains the given offset.
  * 
- * Return: Pointer to the RPVecrtor created.
+ * @param code Pointer to a RAnnotatedCode.
+ * @param offset Offset.
+ * @return Pointer to the RPVector created.
  */
 R_API RPVector *r_annotated_code_annotations_in(RAnnotatedCode *code, size_t offset);
 /**
- * r_annotated_code_annotations_range() - Returns all annotations with range that overlap with the given range.
- * @code: Pointer to a RAnnotatedCode.
- * @start: Start of the range(inclusive).
- * @end: End of the range(exclusive).
- *
  * Creates an RPVector and inserts the pointers to all annotations whose 
  * range overlap with range [start, end-1] (both inclusive).
+ * @brief Returns all annotations with range that overlap with the given range.
  * 
- * Return: Pointer to the RPVecrtor created.
+ * @param code Pointer to a RAnnotatedCode.
+ * @param start Start of the range(inclusive).
+ * @param end End of the range(exclusive).
+ * @return Pointer to the RPVecrtor created.
  */
 R_API RPVector *r_annotated_code_annotations_range(RAnnotatedCode *code, size_t start, size_t end);
 /**
- * r_annotated_code_line_offsets() - Returns the offset for every line of decompiled code in RAnnotatedCode *code.
- * @code: Pointer to a RAnnotatedCode.
- *
  * Creates an RVector and inserts the offsets for every seperate line of decompiled code in
  * code->code (code->code is a character array).
  * If a line of decompiled code doesn't have a unique offset, UT64_MAX is inserted as its offset.
- * 	
- * Return: Pointer to the RVector created.
+ * @brief Returns the offset for every line of decompiled code in RAnnotatedCode *code.
+ * 
+ * @param code Pointer to a RAnnotatedCode.
+ * @return Pointer to the RVector created.
  */
 R_API RVector *r_annotated_code_line_offsets(RAnnotatedCode *code);
 
