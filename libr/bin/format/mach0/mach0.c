4f86bd51c17e9f0a0e971bd437e20a83a6b2501c
phakeobj <phakeobj@users.noreply.github.com>
2020-08-19 00:30:22 +0300

    Use the implementation from RUtil

--- libr/bin/format/mach0/mach0.c
+++ libr/bin/format/mach0/mach0.c
@@ -34,53 +34,17 @@ typedef struct {
 // USE THIS: int ws = bf->o->info->big_endian;
 #define mach0_endian 1
 
-// TODO: Use the implementation from RUtil
 static ut64 read_uleb128(ulebr *r, ut8 *end) {
-	ut64 result = 0;
-	int bit = 0;
-	ut64 slice = 0;
-	ut8 *p = r->p;
-	do {
-		if (p == end) {
-			eprintf ("malformed uleb128\n");
-			return UT64_MAX;
-		}
-		slice = *p & 0x7f;
-		if (bit > 63) {
-			eprintf ("uleb128 too big for uint64, bit=%d, result=0x%"PFMT64x"\n", bit, result);
-			return UT64_MAX;
-		} else {
-			result |= (slice << bit);
-			bit += 7;
-		}
-	} while (*p++ & 0x80);
-	r->p = p;
-	return result;
+	int datalen = end - r->p;
+	ut64 v;
+	r->p = (ut8 *)r_uleb128 (r->p, datalen, &v);
+	return v;
 }
 
 static st64 read_sleb128(ulebr *r, ut8 *end) {
-	st64 result = 0;
-	int bit = 0;
-	ut8 byte = 0;
-	ut8 *p = r->p;
-	do {
-		if (p == end) {
-			eprintf ("malformed sleb128\n");
-			break;
-		}
-		byte = *p++;
-		result |= (((st64)(byte & 0x7f)) << bit);
-		bit += 7;
-		if (bit > 63) {
-			eprintf ("too large sleb128 shift\n");
-			return result;
-		}
-	} while (byte & 0x80);
-	// sign extend negative numbers
-	if ((byte & 0x40)) {
-		result |= (-1LL) << bit;
-	}
-	r->p = p;
+	const ut8 *t = r->p;
+	st64 result = r_sleb128 (&t, end);
+	r->p = (ut8 *)t;
 	return result;
 }
 
