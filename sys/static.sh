d1efd489010ae9220bed2241c84be80f10b02e45
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2020-08-20 21:03:04 +0800

    Revert "Pass -fno-lto to Debian package build"
    This reverts commit 84a70181eefff26eb8985c6edfb1365f74343841.

--- sys/static.sh
+++ sys/static.sh
@@ -5,8 +5,8 @@
 case "$(uname)" in
 Linux)
 	LDFLAGS="${LDFLAGS} -lpthread -ldl -lutil -lm"
-	CFLAGS="-flto ${CFLAGS}"
-	LDFLAGS="-flto ${LDFLAGS}"
+	CFLAGS="${CFLAGS} -flto"
+	LDFLAGS="${LDFLAGS} -flto"
 	if [ -n "`gcc -v 2>&1 | grep gcc`" ]; then
 		export AR=gcc-ar
 	fi
