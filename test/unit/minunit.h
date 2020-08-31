3786d01ca75e3e6604b85da6663ef6e4d3060546
Riccardo Schirone <sirmy15@gmail.com>
2020-08-31 10:01:27 +0200

    Revert "Add the r_json json parser based on nxjson ##util (#17439)"
    This reverts commit 3ef9c81da91a51ffe542c9b9c109e8ac4335e912.

--- test/unit/minunit.h
+++ test/unit/minunit.h
@@ -176,14 +176,12 @@ void sprint_mem(char *out, const ut8 *buf, size_t len) {
 		mu_assert(_meqstr, memcmp((exp__), (act__), (len)) == 0); \
 } while(0)
 
-#define mu_run_test_named(test, name, ...) do { int result; \
-		printf(TBOLD "%s" TRESET " ", name); \
-		result = test(__VA_ARGS__); \
+#define mu_run_test(test) do { int result; \
+		printf(TBOLD #test TRESET " "); \
+		result = test(); \
 		tests_run++; \
 		tests_passed += result; \
 } while (0)
-
-#define mu_run_test(test, ...) mu_run_test_named (test, #test, __VA_ARGS__)
 
 #define mu_cleanup_fail(label, message) do { mu_perror(message); retval = MU_ERR; goto label; } while(0)
 #define mu_cleanup_sysfail(label, message) do { mu_psyserror(message); retval = MU_ERR; goto label; } while(0)
