151d41d38cae9b1267ab3d979890b367cc1b4532
David Carlier <devnexen@gmail.com>
2020-09-01 22:28:35 +0000

    Fixes r_sys_pid_to_path for Haiku using proper team_id/pid_t ##util

--- libr/util/sys.c
+++ libr/util/sys.c
@@ -1172,7 +1172,7 @@ R_API char *r_sys_pid_to_path(int pid) {
 	int32_t group = 0;
 	image_info ii;
 
-	while (get_next_image_info (0, &group, &ii) == B_OK) {
+	while (get_next_image_info ((team_id)pid, &group, &ii) == B_OK) {
 		if (ii.type == B_APP_IMAGE) {
 			break;
 		}
