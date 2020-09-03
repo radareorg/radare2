c54b37d22434f3dd3fb1f81d1f0707f169c847b3
yala <junchao_zhao@yeah.net>
2020-09-03 09:24:06 +0800

    update indent

--- libr/anal/p/anal_mips_cs.c
+++ libr/anal/p/anal_mips_cs.c
@@ -228,18 +228,18 @@ static int analop_esil(RAnal *a, RAnalOp *op, ut64 addr, const ut8 *buf, int len
 			r_strbuf_appendf (&op->esil, "%s,%s,==", ARG (1), ARG (0));
 			break;
 		case MIPS_INS_DSRA:
-			r_strbuf_appendf (&op->esil, 
-							"%s,%s,>>,31,%s,>>,?{,32,%s,32,-,0xffffffff,<<,0xffffffff,&,<<,}{,0,},|,%s,=",
-							ARG (2), ARG (1), ARG (1), ARG (2), ARG (0));
+			r_strbuf_appendf (&op->esil,
+				"%s,%s,>>,31,%s,>>,?{,32,%s,32,-,0xffffffff,<<,0xffffffff,&,<<,}{,0,},|,%s,=",
+				ARG (2), ARG (1), ARG (1), ARG (2), ARG (0));
 			break;
 		case MIPS_INS_SHRAV:
 		case MIPS_INS_SHRAV_R:
 		case MIPS_INS_SHRA:
 		case MIPS_INS_SHRA_R:
 		case MIPS_INS_SRA:
-			r_strbuf_appendf (&op->esil, 
-							"%s,%s,>>,31,%s,>>,?{,%s,32,-,0xffffffff,<<,0xffffffff,&,}{,0,},|,0xffffffff,&,%s,=",
-							ARG (2), ARG (1), ARG (1), ARG (2), ARG (0));
+			r_strbuf_appendf (&op->esil,
+				"%s,%s,>>,31,%s,>>,?{,%s,32,-,0xffffffff,<<,0xffffffff,&,}{,0,},|,0xffffffff,&,%s,=",
+				ARG (2), ARG (1), ARG (1), ARG (2), ARG (0));
 			break;
 		case MIPS_INS_SHRL:
 			// suffix 'S' forces conditional flag to be updated
