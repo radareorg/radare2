fe5b80d615881bd1c31e0a99d655f4af90acf126
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2020-09-11 18:47:27 +0800

    Fix asan r2pipe test by updating libasan and using LD_PRELOAD (#17594)
    * Set LD_PRELOAD for unit tests
    * Declare failed debug tests as broken if no assertion or Sanitizer in output
    * Check for ASAN=1 in cmd_pipe test
    * Set asan: true in the GitHubCI matrix
    * Fix ttyname-related runtime error
    * Upgrade asan build to Ubuntu 20.04
    * Quick fix for reg/arena.c runtime error

--- libr/reg/arena.c
+++ libr/reg/arena.c
@@ -43,7 +43,7 @@ R_API ut8 *r_reg_get_bytes(RReg *reg, int type, int *size) {
 		*size = sz;
 	}
 	buf = malloc (sz);
-	if (buf) {
+	if (buf && reg->regset[type].arena->bytes) { // TODO change 2nd check to something more reasonable
 		memcpy (buf, reg->regset[type].arena->bytes, sz);
 	}
 	return buf;
