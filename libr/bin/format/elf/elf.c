8801624edfb1e176b66e07d7ff9a26926619011f
Riccardo Schirone <ret2libc@users.noreply.github.com>
2020-09-11 10:26:33 +0200

    Apply suggestions from code review

--- libr/bin/format/elf/elf.c
+++ libr/bin/format/elf/elf.c
@@ -83,7 +83,6 @@ static inline int UTX_MUL(ut64 *r, ut64 a, ut64 b) {
  * Return non-zero if HDR identifies an MIPS n64 ELF binary.
  */
 static bool is_elfclass64(Elf_(Ehdr) *h) {
-
 	return h->e_ident[EI_CLASS] == ELFCLASS64;
 }
 
@@ -91,7 +90,6 @@ static bool is_elfclass64(Elf_(Ehdr) *h) {
  * Return non-zero if HDR identifies an MIPS o32 ELF binary.
  */
 static bool is_mips_o32(Elf_(Ehdr) *h) {
-
 	if (h->e_ident[EI_CLASS] != ELFCLASS32) {
 		return false;
 	}
@@ -110,7 +108,6 @@ static bool is_mips_o32(Elf_(Ehdr) *h) {
  * Return non-zero if HDR identifies an MIPS n32 ELF binary.
  */
 static bool is_mips_n32(Elf_(Ehdr) *h) {
-
 	int res = 1;
 
 	if (h->e_ident[EI_CLASS] != ELFCLASS32) {
