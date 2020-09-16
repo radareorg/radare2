5a1f6fda736e95729689dba45291054fbac8d16c
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2020-09-16 15:27:58 +0800

    Return false if type != R_REG_TYPE_ALL

--- libr/debug/dreg.c
+++ libr/debug/dreg.c
@@ -49,6 +49,8 @@ R_API int r_debug_reg_sync(RDebug *dbg, int type, int write) {
 				if (!i) {
 					eprintf ("r_debug_reg: error writing "
 						"registers %d to %d\n", i, dbg->tid);
+				}
+				if (type != R_REG_TYPE_ALL) {
 					free (buf);
 					return false;
 				}
