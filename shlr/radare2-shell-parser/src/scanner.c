daf012fae59ea62cde725e81c2dc93551eacde2b
pancake <pancake@nopcode.org>
2020-09-18 02:58:35 +0200

    Fix

--- shlr/radare2-shell-parser/src/scanner.c
+++ shlr/radare2-shell-parser/src/scanner.c
@@ -51,9 +51,9 @@ static bool is_special_start(const int32_t ch) {
 }
 
 static bool is_start_of_command(const int32_t ch) {
-	return isalpha (ch) || ch == '$' || ch == '?' || ch == ':' || ch == '+' ||
-		ch == '=' || ch == '/' || ch == '_' || ch == '#' || ch == '\\' ||
-		ch == '-' || is_special_start (ch);
+	return isalpha (ch) || ch == '$' || ch == '?' || ch == ':' || ch == '+' || ch == '-'
+		|| ch == '#' || ch == ',' || ch == '=' || ch == '/' || ch == '_' || ch == '\\'
+		|| is_special_start (ch);
 }
 
 static bool is_mid_command(const char *res, int len, const int32_t ch) {
