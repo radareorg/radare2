5e7c4b8ec93a68d1a58d5329f15db5cb7a181267
Alexis Ehret <08a@riseup.net>
2020-09-21 19:39:25 +0200

    Add r_asctime_r: a cross platform asctime_r

--- libr/include/r_util/r_time.h
+++ libr/include/r_util/r_time.h
@@ -9,6 +9,8 @@
 #define R_NSEC_PER_USEC 1000
 #define R_USEC_PER_MSEC 1000
 
+#define ASCTIME_BUF_MINLEN (26)
+
 // wall clock time in microseconds
 R_API ut64 r_time_now(void);
 
@@ -19,6 +21,9 @@ R_API char *r_time_stamp_to_str(ut32 timeStamp);
 R_API ut32 r_time_dos_time_stamp_to_posix(ut32 timeStamp);
 R_API bool r_time_stamp_is_dos_format(const ut32 certainPosixTimeStamp, const ut32 possiblePosixOrDosTimeStamp);
 R_API const char *r_time_to_string(ut64 ts);
+
+// Cross platform asctime_r
+R_API char *r_asctime_r(const struct tm *tm, char *buf, size_t size);
 
 #define R_TIME_PROFILE_ENABLED 0
 
