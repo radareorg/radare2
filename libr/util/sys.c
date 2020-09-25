d9c6009949a6c4d3ea10224a0fca41ff1d6ab4d2
Riccardo Schirone <sirmy15@gmail.com>
2020-09-25 10:03:15 +0200

    Fix return type of `signal`

--- libr/util/sys.c
+++ libr/util/sys.c
@@ -171,13 +171,13 @@ R_API int r_sys_sigaction(int *sig, void (*handler) (int)) {
 	return 0;
 }
 #else
-R_API int r_sys_sigaction(int *sig, void (*handler) (int)) {
+R_API int r_sys_sigaction(int *sig, void (*handler)(int)) {
 	if (!sig) {
 		return -EINVAL;
 	}
 	size_t i;
 	for (i = 0; sig[i] != 0; i++) {
-		sig_t ret = signal (sig[i], handler);
+		void (*ret)(int) = signal (sig[i], handler);
 		if (ret == SIG_ERR) {
 			eprintf ("Failed to set signal handler for signal '%d': %s\n", sig[i], strerror(errno));
 			return -1;
