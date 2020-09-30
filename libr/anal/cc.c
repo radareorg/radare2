f56046e32aafc4024a1591a34e5dac9ac2ee59b4
Florian Märkl <info@florianmaerkl.de>
2020-09-30 07:39:20 +0200

    Add setters for CC self and error and Tests ##anal (#17704)
    * Add setters for CC self and error and Tests ##anal
    * Purge sdb_cc on anal purge ##anal
    * Add non-nonsensical syntax for self/err CC

--- libr/anal/cc.c
+++ libr/anal/cc.c
@@ -7,12 +7,16 @@
 
 R_API void r_anal_cc_del(RAnal *anal, const char *name) {
 	size_t i;
-	sdb_unset (DB, sdb_fmt ("%s", name), 0);
-	sdb_unset (DB, sdb_fmt ("cc.%s.ret", name), 0);
-	sdb_unset (DB, sdb_fmt ("cc.%s.argn", name), 0);
+	RStrBuf sb;
+	sdb_unset (DB, r_strbuf_initf (&sb, "%s", name), 0);
+	sdb_unset (DB, r_strbuf_setf (&sb, "cc.%s.ret", name), 0);
+	sdb_unset (DB, r_strbuf_setf (&sb, "cc.%s.argn", name), 0);
 	for (i = 0; i < R_ANAL_CC_MAXARG; i++) {
-		sdb_unset (DB, sdb_fmt ("cc.%s.arg%d", name, i), 0);
+		sdb_unset (DB, r_strbuf_setf (&sb, "cc.%s.arg%d", name, i), 0);
 	}
+	sdb_unset (DB, r_strbuf_setf (&sb, "cc.%s.self", name), 0);
+	sdb_unset (DB, r_strbuf_setf (&sb, "cc.%s.error", name), 0);
+	r_strbuf_fini (&sb);
 }
 
 R_API bool r_anal_cc_set(RAnal *anal, const char *expr) {
@@ -77,7 +81,8 @@ R_API char *r_anal_cc_get(RAnal *anal, const char *name) {
 		return NULL;
 	}
 	RStrBuf *sb = r_strbuf_new (NULL);
-	r_strbuf_appendf (sb, "%s %s (", ret, name);
+	const char *self = r_anal_cc_self (anal, name);
+	r_strbuf_appendf (sb, "%s %s%s%s (", ret, self? self: "", self? ".": "", name);
 	bool isFirst = true;
 	for (i = 0; i < R_ANAL_CC_MAXARG; i++) {
 		const char *k = sdb_fmt ("cc.%s.arg%d", name, i);
@@ -92,16 +97,14 @@ R_API char *r_anal_cc_get(RAnal *anal, const char *name) {
 	if (argn) {
 		r_strbuf_appendf (sb, "%s%s", isFirst? "": ", ", argn);
 	}
-	const char *self = r_anal_cc_self (anal, name);
-	if (self) {
-		r_strbuf_appendf (sb, "%s%s", isFirst? "": ", ", self);
-	}
+	r_strbuf_append (sb, ")");
+
 	const char *error = r_anal_cc_error (anal, name);
 	if (error) {
-		r_strbuf_appendf (sb, "%s%s", isFirst? "": ", ", error);
+		r_strbuf_appendf (sb, " %s", error);
 	}
 
-	r_strbuf_appendf (sb, ");");
+	r_strbuf_append (sb, ";");
 	return r_strbuf_drain (sb);
 }
 
@@ -134,11 +137,29 @@ R_API const char *r_anal_cc_self(RAnal *anal, const char *convention) {
 	return self? r_str_constpool_get (&anal->constpool, self): NULL;
 }
 
+R_API void r_anal_cc_set_self(RAnal *anal, const char *convention, const char *self) {
+	if (!r_anal_cc_exist (anal, convention)) {
+		return;
+	}
+	RStrBuf sb;
+	sdb_set (anal->sdb_cc, r_strbuf_initf (&sb, "cc.%s.self", convention), self, 0);
+	r_strbuf_fini (&sb);
+}
+
 R_API const char *r_anal_cc_error(RAnal *anal, const char *convention) {
 	r_return_val_if_fail (anal && convention, NULL);
 	const char *query = sdb_fmt ("cc.%s.error", convention);
 	const char *error = sdb_const_get (DB, query, 0);
 	return error? r_str_constpool_get (&anal->constpool, error): NULL;
+}
+
+R_API void r_anal_cc_set_error(RAnal *anal, const char *convention, const char *error) {
+	if (!r_anal_cc_exist (anal, convention)) {
+		return;
+	}
+	RStrBuf sb;
+	sdb_set (anal->sdb_cc, r_strbuf_initf (&sb, "cc.%s.error", convention), error, 0);
+	r_strbuf_fini (&sb);
 }
 
 R_API int r_anal_cc_max_arg(RAnal *anal, const char *cc) {
