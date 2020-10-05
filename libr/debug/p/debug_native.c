16a91fbe5ac930575ee6b24d3a3b25d0085688c1
Florian Märkl <info@florianmaerkl.de>
2020-10-05 14:50:23 +0200

    Fix eprinf -> eprintf

--- libr/debug/p/debug_native.c
+++ libr/debug/p/debug_native.c
@@ -96,7 +96,7 @@ static int r_debug_handle_signals(RDebug *dbg) {
 #if __KFBSD__
 	return bsd_handle_signals (dbg);
 #else
-	eprinf ("Warning: signal handling is not supported on this platform\n");
+	eprintf ("Warning: signal handling is not supported on this platform\n");
 	return 0;
 #endif
 }
