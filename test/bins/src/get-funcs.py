c611b986cb0dd6551001faed7f0d362d373397e3
valdaarhun <icegambit91@gmail.com>
2020-10-08 13:21:10 +0530

    Removed test/bins

--- test/bins/src/get-funcs.py
+++
@@ -1,8 +0,0 @@
-import r2pipe
-r2 = r2pipe.open()
-r2.cmd('aa')
-print('\nFunction names:')
-for func in r2.cmdj('aflj'):
-    print(func['name'])
-print('\nDisassembly of entry0:')
-print(r2.cmd('pdf'))
