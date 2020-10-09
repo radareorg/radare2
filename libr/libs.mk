85e684d5b8a1443f7813a8b4fce70a7dc6023c58
pancake <pancake@youterm.com>
2020-10-09 18:36:10 +0200

    Fix build race condition introduced in 0ba897f5c9f7dbc65bd812798531bed538447f42 (#17773)
    Co-authored-by: pancake <pancake@nopcode.org>

--- libr/libs.mk
+++ libr/libs.mk
@@ -3,9 +3,9 @@
 ifeq ($(LIBS0),)
 LIBS0=util
 LIBS1=socket hash reg cons magic bp search config
-LIBS2=syscall lang crypto io flag
-LIBS3=fs parse bin
-LIBS4=asm
+LIBS2=syscall lang crypto flag
+LIBS3=parse io
+LIBS4=fs asm bin
 LIBS5=anal egg
 LIBS6=debug
 LIBS7=core
