1641cf2b2709952a3481df57388d69bb64e34a65
pancake <pancake@nopcode.org>
2020-10-09 15:43:44 +0200

    fs needs io

--- libr/libs.mk
+++ libr/libs.mk
@@ -4,8 +4,8 @@ ifeq ($(LIBS0),)
 LIBS0=util
 LIBS1=socket hash reg cons magic bp search config
 LIBS2=syscall lang crypto flag
-LIBS3=fs parse io
-LIBS4=asm bin
+LIBS3=parse io
+LIBS4=fs asm bin
 LIBS5=anal egg
 LIBS6=debug
 LIBS7=core
