radareorg/radare2

commit 0ba897f5c9f7dbc65bd812798531bed538447f42

Add network support for WinDbg/KD (KDNET) ##debug (#17340)
* Add network support for WinDbg/KD (KDNET) ##debug
* Add iob_net io_backend plugin for shlr/winkd
* Add Windows 10 (May 2019 Update) build 18362 profile
* Add r_hash_do_hmac_sha256 function to r_hash
* Change r_socket_connect to bind source port for UDP connection

libr/libs.mk
@@ -3,7 +3,7 @@
 ifeq ($(LIBS0),)
 LIBS0=util
 LIBS1=socket hash reg cons magic bp search config
-LIBS2=syscall lang io crypto flag
+LIBS2=syscall lang crypto io flag
 LIBS3=fs parse bin
 LIBS4=asm
 LIBS5=anal egg

libr/debug/Makefile
@@ -1,7 +1,7 @@
 include ../config.mk
 
 NAME=r_debug
-R2DEPS=r_reg r_anal r_bp r_io r_parse r_cons r_syscall r_hash r_flag r_util r_egg
+R2DEPS=r_reg r_anal r_bp r_io r_parse r_cons r_syscall r_hash r_flag r_util r_egg r_crypto
 R2DEPS+=r_socket
 CFLAGS+=-DR2_PLUGIN_INCORE
 

libr/hash/state.c
@@ -132,3 +132,52 @@ R_API ut8 *r_hash_do_md4(RHash *ctx, const ut8 *input, int len) {
 	}
 	return NULL;
 }
+
+R_API ut8 *r_hash_do_hmac_sha256(RHash *ctx, const ut8 *input, int len, const ut8 *key, int klen) {
+	if (len < 0 || klen < 0) {
+		return NULL;
+	}
+
+	size_t i;
+	ut8 bskey[SHA256_BLOCK_LENGTH]; // block-sized key
+	ut8 kpad[SHA256_BLOCK_LENGTH]; // keypad for opad, ipad
+
+	// If klen > block-size, bskey = Hash(key)
+	memset (bskey, 0, SHA256_BLOCK_LENGTH);
+	if (klen > SHA256_BLOCK_LENGTH) {
+		SHA256_Init (&ctx->sha256);
+		SHA256_Update (&ctx->sha256, key, klen);
+		SHA256_Final (ctx->digest, &ctx->sha256);
+		memcpy (bskey, ctx->digest, R_HASH_SIZE_SHA256);
+	} else {
+		memcpy (bskey, key, klen);
+	}
+
+	// XOR block-sized key with ipad 0x36
+	memset (kpad, 0, SHA256_BLOCK_LENGTH);
+	memcpy (kpad, bskey, SHA256_BLOCK_LENGTH);
+	for (i = 0; i < SHA256_BLOCK_LENGTH; i++) {
+		kpad[i] ^= 0x36;
+	}
+
+	// Inner hash (key ^ ipad || input)
+	SHA256_Init (&ctx->sha256);
+	SHA256_Update (&ctx->sha256, kpad, SHA256_BLOCK_LENGTH);
+	SHA256_Update (&ctx->sha256, input, len);
+	SHA256_Final (ctx->digest, &ctx->sha256);
+
+	// XOR block-sized key with opad 0x5c
+	memset (kpad, 0, SHA256_BLOCK_LENGTH);
+	memcpy (kpad, bskey, SHA256_BLOCK_LENGTH);
+	for (i = 0; i < SHA256_BLOCK_LENGTH; i++) {
+		kpad[i] ^= 0x5c;
+	}
+
+	// Outer hash (key ^ opad || Inner hash)
+	SHA256_Init (&ctx->sha256);
+	SHA256_Update (&ctx->sha256, kpad, SHA256_BLOCK_LENGTH);
+	SHA256_Update (&ctx->sha256, ctx->digest, R_HASH_SIZE_SHA256);
+	SHA256_Final (ctx->digest, &ctx->sha256);
+
+	return ctx->digest;
+}

libr/include/r_hash.h
@@ -490,6 +490,7 @@ R_API ut8 *r_hash_do_sha1(RHash *ctx, const ut8 *input, int len);
 R_API ut8 *r_hash_do_sha256(RHash *ctx, const ut8 *input, int len);
 R_API ut8 *r_hash_do_sha384(RHash *ctx, const ut8 *input, int len);
 R_API ut8 *r_hash_do_sha512(RHash *ctx, const ut8 *input, int len);
+R_API ut8 *r_hash_do_hmac_sha256(RHash *ctx, const ut8 *input, int len, const ut8 *key, int klen);
 
 R_API char *r_hash_to_string(RHash *ctx, const char *name, const ut8 *data, int len);
 

libr/io/Makefile
@@ -3,7 +3,9 @@ include ../config.mk
 NAME=r_io
 R2DEPS+=r_util
 R2DEPS+=r_socket
+R2DEPS+=r_hash
 R2DEPS+=r_cons
+R2DEPS+=r_crypto
 STATIC_OBJS=$(subst ..,p/..,$(subst io_,p/io_,$(STATIC_OBJ)))
 OBJS=${STATIC_OBJS}
 OBJS+=io.o io_plugin.o io_map.o io_desc.o io_cache.o p_cache.o undo.o ioutils.o io_fd.o

libr/io/p/io_winkd.c
@@ -29,19 +29,34 @@ static RIODesc *__open(RIO *io, const char *file, int rw, int mode) {
 		return NULL;
 	}
 
-	if (!iob_select ("pipe")) {
-		eprintf("Could not initialize the IO backend\n");
+	// net  - host:ip:key
+	// pipe - \\.\pipe\com_1 /tmp/windbg.pipe
+	io_backend_t *iob = NULL;
+	if (strchr (file + 8, ':')) {
+		iob = &iob_net;
+	} else {
+		iob = &iob_pipe;
+	}
+
+	if (!iob) {
+		eprintf ("Error: Invalid WinDBG path\n");
 		return NULL;
 	}
 
-	void *io_ctx = iob_open (file + 9);
+	void *io_ctx = iob->open (file + 8);
 	if (!io_ctx) {
-		eprintf ("Could not open the pipe\n");
+		eprintf ("Error: Could not open the %s\n", iob->name);
 		return NULL;
 	}
-	eprintf ("Opened pipe %s with fd %p\n", file + 9, io_ctx);
+	eprintf ("Opened %s %s with fd %p\n", iob->name, file + 8, io_ctx);
 
-	WindCtx *ctx = winkd_ctx_new (io_ctx);
+	io_desc_t *desc = io_desc_new (iob, io_ctx);
+	if (!desc) {
+		eprintf ("Error: Could not create io_desc_t\n");
+		return NULL;
+	}
+
+	WindCtx *ctx = winkd_ctx_new (desc);
 	if (!ctx) {
 		eprintf ("Failed to initialize winkd context\n");
 		return NULL;

libr/io/p/winkd.mk
@@ -9,10 +9,16 @@ CFLAGS+=-I$(SHLR)/winkd
 LDFLAGS+=$(SHLR)/winkd/libr_winkd.$(EXT_AR)
 
 ifeq (${WITHPIC},0)
-LINKFLAGS+=../../util/libr_util.a
+LINKFLAGS=../../util/libr_util.a
+LINKFLAGS+= ../../util/libr_socket.a
+LINKFLAGS+= ../../util/libr_hash.a
+LINKFLAGS+=../../util/libr_crypto.a
 LINKFLAGS+=../../io/libr_io.a
 else
-LINKFLAGS+=-L../../util -lr_util
+LINKFLAGS=-L../../util -lr_util
+LINKFLAGS+=-L../../socket -lr_socket
+LINKFLAGS+=-L../../hash -lr_hash
+LINKFLAGS+=-L../../crypto -lr_crypto
 LINKFLAGS+=-L.. -lr_io
 endif
 

libr/socket/socket.c
@@ -301,16 +301,44 @@ R_API bool r_socket_connect(RSocket *s, const char *host, const char *port, int 
 				perror ("socket");
 				continue;
 			}
-			ret = setsockopt (s->fd, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof (flag));
-			if (ret < 0) {
-				perror ("setsockopt");
-				close (s->fd);
-				s->fd = -1;
-				continue;
-			}
 
-			r_socket_block_time (s, 0, 0, 0);
-			ret = connect (s->fd, rp->ai_addr, rp->ai_addrlen);
+			switch (proto) {
+			case R_SOCKET_PROTO_TCP:
+				ret = setsockopt (s->fd, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof (flag));
+				if (ret < 0) {
+					perror ("setsockopt");
+					close (s->fd);
+					s->fd = -1;
+					continue;
+				}
+				r_socket_block_time (s, 0, 0, 0);
+				ret = connect (s->fd, rp->ai_addr, rp->ai_addrlen);
+				break;
+			case R_SOCKET_PROTO_UDP:
+				memset (&s->sa, 0, sizeof (s->sa));
+				s->sa.sin_family = AF_INET;
+				s->sa.sin_addr.s_addr = htonl (s->local? INADDR_LOOPBACK: INADDR_ANY);
+				s->port = r_socket_port_by_name (port);
+				if (s->port < 1) {
+					continue;
+				}
+				s->sa.sin_port = htons (s->port);
+				if (bind (s->fd, (struct sockaddr *)&s->sa, sizeof (s->sa)) < 0) {
+					r_sys_perror ("bind");
+#ifdef __WINDOWS__
+					closesocket (s->fd);
+#else
+					close (s->fd);
+#endif
+					continue;
+				}
+				ret = connect (s->fd, rp->ai_addr, rp->ai_addrlen);
+				break;
+			default:
+				r_socket_block_time (s, 0, 0, 0);
+				ret = connect (s->fd, rp->ai_addr, rp->ai_addrlen);
+				break;
+			}
 
 			if (ret == 0) {
 				freeaddrinfo (res);
@@ -469,8 +497,20 @@ R_API bool r_socket_listen(RSocket *s, const char *port, const char *certfile) {
 		return false;
 	}
 #endif
-	if ((s->fd = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP)) == R_INVALID_SOCKET) {
-		return false;
+
+	switch (s->proto) {
+	case R_SOCKET_PROTO_TCP:
+		if ((s->fd = socket (AF_INET, SOCK_STREAM, R_SOCKET_PROTO_TCP)) == R_INVALID_SOCKET) {
+			return false;
+		}
+		break;
+	case R_SOCKET_PROTO_UDP:
+		if ((s->fd = socket (AF_INET, SOCK_DGRAM, R_SOCKET_PROTO_UDP)) == R_INVALID_SOCKET) {
+			return false;
+		}
+		break;
+	default:
+		break;
 	}
 
 	linger.l_onoff = 1;
@@ -511,13 +551,15 @@ R_API bool r_socket_listen(RSocket *s, const char *port, const char *certfile) {
 #if __UNIX__
 	r_sys_signal (SIGPIPE, SIG_IGN);
 #endif
-	if (listen (s->fd, 32) < 0) {
+	if (s->proto == R_SOCKET_PROTO_TCP) {
+		if (listen (s->fd, 32) < 0) {
 #ifdef _MSC_VER
-		closesocket (s->fd);
+			closesocket (s->fd);
 #else
-		close (s->fd);
+			close (s->fd);
 #endif
-		return false;
+			return false;
+		}
 	}
 #if HAVE_LIB_SSL
 	if (s->is_ssl) {

commit d828ff86724ac8b2cb9d23524680f3aef13b28d4

Fix build race condition introduced in 0ba897f5c9f7dbc65bd812798531bed538447f42

libr/libs.mk
@@ -3,9 +3,9 @@
 ifeq ($(LIBS0),)
 LIBS0=util
 LIBS1=socket hash reg cons magic bp search config
-LIBS2=syscall lang crypto io flag
-LIBS3=fs parse bin
-LIBS4=asm
+LIBS2=syscall lang crypto flag
+LIBS3=fs parse io
+LIBS4=asm bin
 LIBS5=anal egg
 LIBS6=debug
 LIBS7=core