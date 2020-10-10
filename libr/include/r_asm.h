c521a6290c0bb3c7f7a1c7b78e6e0fc13c76fe34
pancake <pancake@youterm.com>
2020-10-10 06:11:29 +0200

    Move spc700 asm,bin plugins to radare2-extras ##asm (#17762)
    Co-authored-by: pancake <pancake@nopcode.org>

--- libr/include/r_asm.h
+++ libr/include/r_asm.h
@@ -249,7 +249,6 @@ extern RAsmPlugin r_asm_plugin_sh;
 extern RAsmPlugin r_asm_plugin_snes;
 extern RAsmPlugin r_asm_plugin_sparc_cs;
 extern RAsmPlugin r_asm_plugin_sparc_gnu;
-extern RAsmPlugin r_asm_plugin_spc700;
 extern RAsmPlugin r_asm_plugin_sysz;
 extern RAsmPlugin r_asm_plugin_tms320;
 extern RAsmPlugin r_asm_plugin_tms320c64x;
