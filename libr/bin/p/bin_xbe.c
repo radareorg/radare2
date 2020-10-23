2cfa1fc3fe126113e90e4c32c89527631685cf86
x1nixmzeng <x1nixmzeng@live.co.uk>
2020-10-23 23:43:57 +0100

    Fix xbe symbol name lookup

--- libr/bin/p/bin_xbe.c
+++ libr/bin/p/bin_xbe.c
@@ -297,9 +297,9 @@ static RList *symbols(RBinFile *bf) {
 		}
 		const ut32 thunk_index = thunk_addr[i] ^ 0x80000000;
 		// Basic sanity checks
-		if (thunk_addr[i] & 0x80000000 && thunk_index < XBE_MAX_THUNK) {
-			eprintf ("%d\n", thunk_index);
-			sym->name = r_str_newf ("kt.%s", kt_name[thunk_index]);
+		if (thunk_addr[i] & 0x80000000 && thunk_index > 0 && thunk_index <= XBE_MAX_THUNK) {
+			eprintf ("thunk_index %d\n", thunk_index);
+			sym->name = r_str_newf ("kt.%s", kt_name[thunk_index - 1]);
 			sym->vaddr = (h->kernel_thunk_addr ^ obj->kt_key) + (4 * i);
 			sym->paddr = sym->vaddr - h->base;
 			sym->size = 4;
