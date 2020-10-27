b0228ca85b958891a220b862dcf9411d71cda697
pancake <pancake@youterm.com>
2020-10-27 19:05:57 +0100

    Autocomplete `e <tab>` ##shell (#17855)

--- libr/core/core.c
+++ libr/core/core.c
@@ -1292,14 +1292,11 @@ static void autocomplete_evals(RCore *core, RLineCompletion *completion, const c
 	r_return_if_fail (str);
 	RConfigNode *bt;
 	RListIter *iter;
-	char *tmp = strrchr (str, ' ');
+	const char *tmp = strrchr (str, ' ');
 	if (tmp) {
 		str = tmp + 1;
 	}
-	int n = strlen (str);
-	if (n < 1) {
-		return;
-	}
+	size_t n = strlen (str);
 	r_list_foreach (core->config->nodes, iter, bt) {
 		if (!strncmp (bt->name, str, n)) {
 			r_line_completion_push (completion, bt->name);
