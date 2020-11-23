bdbb6d068dc8110163a77e844f947e9f0a3cb5d5
GustavoLCR <gugulcr@gmail.com>
2020-11-23 01:37:33 -0300

    Fix taking over variable use on the last instruction of block ##anal (#17957)
    * Fix taking over variable use on the last instruction of block
    * Add test

--- libr/anal/fcn.c
+++ libr/anal/fcn.c
@@ -416,7 +416,7 @@ static bool fcn_takeover_block_recursive_followthrough_cb(RAnalBlock *block, voi
 		}
 		// Steal vars from this block
 		size_t i;
-		for (i = 0; i + 1 < block->ninstr; i++) {
+		for (i = 0; i < block->ninstr; i++) {
 			const ut64 addr = r_anal_bb_opaddr_i (block, i);
 			RPVector *vars_used = r_anal_function_get_vars_used_at (other_fcn, addr);
 			if (!vars_used) {
