4a76e8df0e49337f6d80a14e19af556ea60f6736
pancake <pancake@nopcode.org>
2020-12-06 23:10:01 +0100

    Honor :csv in aflt

--- libr/core/canal.c
+++ libr/core/canal.c
@@ -3227,8 +3227,7 @@ static int fcn_list_table(RCore *core, const char *q, int fmt) {
 	if (r_table_query (t, q)) {
 		char *s = (fmt== 'j')
 			? r_table_tojson (t)
-			: r_table_tofancystring (t);
-		// char *s = r_table_tostring (t);
+			: r_table_tostring (t);
 		r_cons_printf ("%s\n", s);
 		free (s);
 	}
@@ -3316,7 +3315,7 @@ R_API int r_core_anal_fcn_list(RCore *core, const char *input, const char *rad) 
 		}
 	case 't': // "aflt" "afltj"
 		if (rad[1] == 'j') {
-			fcn_list_table (core, r_str_trim_head_ro (rad+ 2), 'j');
+			fcn_list_table (core, r_str_trim_head_ro (rad + 2), 'j');
 		} else {
 			fcn_list_table (core, r_str_trim_head_ro (rad + 1), rad[1]);
 		}
