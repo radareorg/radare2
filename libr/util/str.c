4755b7bb1cc9fc7aef90908fc4f035172e188ad7
eagleoflqj <liumeo@pku.edu.cn>
2020-12-18 11:41:40 -0500

    don't use sprintf

--- libr/util/str.c
+++ libr/util/str.c
@@ -1566,7 +1566,7 @@ R_API char *r_str_encoded_json(const char *buf, int buf_size, int encoding) {
 
 		const char *format = encoding == PJ_ENCODING_STR_ARRAY ? "%03u," : "%02X";
 		while (buf[loop] != '\0' && i < (new_sz - 1)) {
-			sprintf (encoded_str + i, format, (ut8) buf[loop]);
+			snprintf (encoded_str + i, new_sz - i, format, (ut8) buf[loop]);
 			loop++;
 			i += increment;
 		}
