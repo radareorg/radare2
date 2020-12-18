1dd639546006047b163ca7e85866094d54c867be
eagleoflqj <liumeo@pku.edu.cn>
2020-12-18 01:36:54 -0500

    fix arm32 db/cmd/cmd_i

--- libr/bin/bin.c
+++ libr/bin/bin.c
@@ -1116,7 +1116,7 @@ R_API void r_bin_list_archs(RBin *bin, int mode) {
 			default:
 				str_fmt = h_flag && strcmp (h_flag, "unknown_flag")? sdb_fmt ("%s_%i %s", arch, bits, h_flag) \
 					: sdb_fmt ("%s_%i", arch, bits);
-				r_table_add_rowf (table, "nsnss", i, sdb_fmt ("0x%08" PFMT64x , boffset), obj_size, str_fmt, "");
+				r_table_add_rowf (table, "dsnss", i, sdb_fmt ("0x%08" PFMT64x , boffset), obj_size, str_fmt, "");
 				bin->cb_printf ("%s\n", r_table_tostring(table));
 			}
 			snprintf (archline, sizeof (archline),
