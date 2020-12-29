a4de76f74092ad86581546f54c0257f064ee0d48
pancake <pancake@nopcode.org>
2020-12-29 12:07:39 +0100

    One more fix

--- libr/rules.mk
+++ libr/rules.mk
@@ -87,7 +87,7 @@ $(LIBSO): $(EXTRA_TARGETS) ${WFD} ${OBJS} ${SHARED_OBJ}
 	    echo "LD $(LIBSO)" || \
 	    echo "\"${CC_LIB} ${LIBNAME} ${OBJS} ${SHARED_OBJ} ${LINK} ${LDFLAGS}\"" ; \
 	    ${CC_LIB} ${LIBNAME} ${CFLAGS} ${OBJS} ${SHARED_OBJ} ${LINK} ${LDFLAGS} || exit 1; \
-	    [ -f "$(LIBR)/stripsyms.sh" ] && sh $(LIBR)/stripsyms.sh "${LIBSO}" ${NAME} ; \
+	    [ -f "$(LIBR)/stripsyms.sh" ] && sh "$(LIBR)/stripsyms.sh" "${LIBSO}" ${NAME} ; \
 	  break ; \
 	fi ; done
 else
