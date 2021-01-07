4441d9e333fc8bac8cd3f1f3a5451ef4bb1bcf11
pancake <pancake@nopcode.org>
2021-01-07 18:58:29 +0100

    More termux patches

--- sys/termux.sh
+++ sys/termux.sh
@@ -1,4 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
-bash ./configure --with-compiler=termux --prefix=/data/data/com.termux/files/usr
+if [ -z "${PREFIX}" ]; then
+	echo "PREFIX env var not set, are you running this script from Termux?"
+fi
+export ANDROID=1
+
+bash ./configure --with-compiler=termux --prefix=${PREFIX}
+rm -f ${PREFIX}/lib/libr_*
 make -j2
 make symstall
