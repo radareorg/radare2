528e6598a17b11796c936c62c6aaca2cffb89cea
Dennis Goodlett <dennis+git_swoops@hurricanelabs.com>
2021-01-10 11:34:38 -0500

    Add arall:// and liball:// open_many plugins #io (#18196)

--- libr/io/p/io_ar.c
+++ libr/io/p/io_ar.c
@@ -6,14 +6,34 @@
 #include "ar.h"
 
 
+static const char *r_io_get_individual_schema(const char *file) {
+	if (r_str_startswith (file, "arall://")) {
+		return "ar://";
+	}
+	if (r_str_startswith (file, "liball://")) {
+		return "lib://";
+	}
+	return NULL;
+}
+
 static bool r_io_ar_plugin_open(RIO *io, const char *file, bool many) {
+	r_return_val_if_fail (io && file, NULL);
+	if (many) {
+		return (r_io_get_individual_schema (file) != NULL);
+	}
 	return !strncmp ("ar://", file, 5) || !strncmp ("lib://", file, 6);
 }
 
+static int r_io_ar_close(RIODesc *fd) {
+	if (!fd || !fd->data) {
+		return -1;
+	}
+	return ar_close ((RArFp *)fd->data);
+}
+
 static RIODesc *r_io_ar_open(RIO *io, const char *file, int rw, int mode) {
-	RIODesc *res = NULL;
-	char *url = strdup (file);
-	char *arname = strstr (url, "://") + 3;
+	r_return_val_if_fail (r_io_ar_plugin_open (io, file, false), NULL);
+	const char *arname = strstr (file, "://") + 3;
 	char *filename = strstr (arname, "//");
 	if (filename) {
 		*filename = 0;
@@ -21,16 +41,61 @@ static RIODesc *r_io_ar_open(RIO *io, const char *file, int rw, int mode) {
 	}
 
 	RArFp *arf = ar_open_file (arname, filename);
+	RIODesc *res = NULL;
 	if (arf) {
-		res = r_io_desc_new (io, &r_io_plugin_ar, filename, rw, mode, arf);
+		res = r_io_desc_new (io, &r_io_plugin_ar, file, rw, mode, arf);
+		if (res) {
+			res->name = strdup (filename);
+		}
 	}
-	free (url);
 	return res;
 }
 
+typedef struct ar_many_data {
+	const char *schema;
+	const char *arname;
+	RIO *io;
+	bool rw;
+	int mode;
+	RList *list;
+} ar_many_data;
+
+static int __io_ar_list(RArFp *arf, void *user) {
+	ar_many_data *data = (ar_many_data *)user;
+	char *uri = r_str_newf ("%s%s//%s", data->schema, data->arname, arf->name);
+	RIODesc *des = r_io_desc_new (data->io, &r_io_plugin_ar, uri, data->rw, data->mode, arf);
+	free (uri);
+
+	if (!des) {
+		ar_close (arf);
+		return -1; // stop error
+	}
+
+	des->name = strdup (arf->name);
+	if (!r_list_append (data->list, des)) {
+		r_io_ar_close (des);
+		return -1; // stop error
+	}
+	return 0; // continue
+}
+
 static RList *r_io_ar_open_many(RIO *io, const char *file, int rw, int mode) {
-	eprintf ("Not implemented\n");
-	return NULL;
+	r_return_val_if_fail (io && file, NULL);
+	ar_many_data data;
+	if ((data.schema = r_io_get_individual_schema (file)) == NULL) {
+		r_warn_if_reached ();
+		return NULL;
+	}
+	data.io = io;
+	data.rw = rw;
+	data.mode = mode;
+	data.arname = strstr (file, "://") + 3;
+	data.list = r_list_newf ((RListFree)r_io_ar_close);
+	if (data.list && ar_open_all_cb (data.arname, (RArOpenManyCB)__io_ar_list, (void *)&data) < 0) {
+		r_list_free (data.list);
+		return NULL;
+	}
+	return data.list;
 }
 
 static ut64 r_io_ar_lseek(RIO *io, RIODesc *fd, ut64 offset, int whence) {
@@ -69,18 +134,11 @@ static int r_io_ar_write(RIO *io, RIODesc *fd, const ut8 *buf, int count) {
 	return ar_write_at ((RArFp *) fd->data, io->off, (void *) buf, count);
 }
 
-static int r_io_ar_close(RIODesc *fd) {
-	if (!fd || !fd->data) {
-		return -1;
-	}
-	return ar_close ((RArFp *) fd->data);
-}
-
 RIOPlugin r_io_plugin_ar = {
 	.name = "ar",
 	.desc = "Open ar/lib files",
 	.license = "LGPL3",
-	.uris = "ar://,lib://",
+	.uris = "ar://,lib://,arall://,liball://",
 	.open = r_io_ar_open,
 	.open_many = r_io_ar_open_many,
 	.write = r_io_ar_write,
--- shlr/ar/ar.h
+++ shlr/ar/ar.h
@@ -10,9 +10,13 @@ typedef struct RARFP {
 	ut32 *refcount;
 } RArFp;
 
+typedef int (*RArOpenManyCB) (RArFp *arf, void *user);
+
 /* Offset passed is always the real io->off of the inspected file,
  * the functions automatically translate it to relative offset within the archive */
 R_API RArFp *ar_open_file(const char *arname, const char *filename);
+R_API RList *ar_open_all(const char *arname);
+R_API int ar_open_all_cb(const char *arname, RArOpenManyCB cb, void *user);
 R_API int ar_close(RArFp *f);
 R_API int ar_read_at(RArFp *f, ut64 off, void *buf, int count);
 R_API int ar_write_at(RArFp *f, ut64 off, void *buf, int count);
