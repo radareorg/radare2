d1ad8b8ca0c4bb455cf563219261bcc0675cc247
murphy <dark.cancerbero@gmail.com>
2021-01-22 16:48:39 +0100

    check return value if file cannot be created or truncated

--- libr/core/cmd_mount.c
+++ libr/core/cmd_mount.c
@@ -331,7 +331,9 @@ static int cmd_mount(void *data, const char *_input) {
 			int total_bytes_read = 0;
 			int blocksize = file->size < core->blocksize ? file->size : core->blocksize;
 			size = size > 0 ? size : file->size;
-			r_sys_truncate (localFile, 0);
+			if (!r_sys_truncate (localFile, 0)) {
+				eprintf ("Cannot create file %s\n", localFile);
+			}
 			while (total_bytes_read < size && ptr < file->size) {
 				int bytes_read = 0;
 				if (size - total_bytes_read < blocksize) {
