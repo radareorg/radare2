c3e725a387dd1fd91b5851006fc3c25f58ab7237
pancake <pancake@nopcode.org>
2021-01-26 23:07:48 +0100

    Extra fix for empty string

--- libr/bin/p/bin_mach0.c
+++ libr/bin/p/bin_mach0.c
@@ -36,8 +36,9 @@ static char *entitlements(RBinFile *bf, bool json) {
 	r_return_val_if_fail (bf && bf->o && bf->o->bin_obj, NULL);
 	struct MACH0_(obj_t) *bin = bf->o->bin_obj;
 	if (json) {
+		const char *s = R_STR_ISEMPTY (bin->signature)?"":bin->signature;
 		PJ *pj = pj_new ();
-		pj_s (pj, (const char*)bin->signature);
+		pj_s (pj, s);
 		return pj_drain (pj);
 	}
 	return r_str_dup (NULL, (const char*)bin->signature);
