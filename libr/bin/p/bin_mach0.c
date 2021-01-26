404d0a18666c4c7a43208e81f1d5a7b04f376e68
pancake <pancake@nopcode.org>
2021-01-27 00:20:26 +0100

    Fix warning

--- libr/bin/p/bin_mach0.c
+++ libr/bin/p/bin_mach0.c
@@ -36,7 +36,7 @@ static char *entitlements(RBinFile *bf, bool json) {
 	r_return_val_if_fail (bf && bf->o && bf->o->bin_obj, NULL);
 	struct MACH0_(obj_t) *bin = bf->o->bin_obj;
 	if (json) {
-		const char *s = R_STR_ISEMPTY (bin->signature)?"":bin->signature;
+		const char *s = r_str_get ((const char *)bin->signature);
 		PJ *pj = pj_new ();
 		pj_s (pj, s);
 		return pj_drain (pj);
