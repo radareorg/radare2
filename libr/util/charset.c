c7f7584347479b4d75c03f1f1d242fe7019f8478
gogo <gogo246475@gmail.com>
2021-01-31 21:42:51 +0100

    r_charset_encode_str ignores now the unknow chars and stop on these.

--- libr/util/charset.c
+++ libr/util/charset.c
@@ -84,7 +84,14 @@ R_API size_t r_charset_encode_str(RCharset *rc, ut8 *out, size_t out_len, const 
 		ut8 ch_in = in[i];
 		snprintf (k, sizeof (k), "0x%02x", ch_in);
 		const char *v = sdb_const_get (rc->db, k, 0);
-		strcpy (o, r_str_get_fail (v, "?"));
+
+		const char *ret = r_str_get_fail (v, "?");
+		if (ret[0] == '?') {
+			return o - (char*)out;
+		} else {
+			strcpy (o, ret);
+		}
+
 		o += strlen (o);
 	}
 	return o - (char*)out;
