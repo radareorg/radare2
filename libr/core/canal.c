90ded38ac5443cab247be994be50159e85490dfd
pancake <pancake@nopcode.org>
2021-02-03 12:03:56 +0100

    Reduce stackframe usage in fcn_recurse ##anal
    * This function is called recursively for each basic block
    * Limits the size of functions that can be analized (anal.depth)
    * The r2 -A Ch34 almost works now.
    * Moving from stack to heap is not the right solution

--- libr/core/canal.c
+++ libr/core/canal.c
@@ -77,14 +77,14 @@ static int cmpcalls(const void *_a, const void *_b) {
 	return (as > bs)? 1: (as < bs)? -1: 0;
 }
 
-static int cmpnbbs (const void *_a, const void *_b) {
+static int cmpnbbs(const void *_a, const void *_b) {
 	const RAnalFunction *a = _a, *b = _b;
 	ut64 as = r_list_length (a->bbs);
 	ut64 bs = r_list_length (b->bbs);
 	return (as> bs)? 1: (as< bs)? -1: 0;
 }
 
-static int cmpaddr (const void *_a, const void *_b) {
+static int cmpaddr(const void *_a, const void *_b) {
 	const RAnalFunction *a = _a, *b = _b;
 	return (a->addr > b->addr)? 1: (a->addr < b->addr)? -1: 0;
 }
