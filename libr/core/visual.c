c20d67b61fab13525f65e16e05ed8e2ea612bcce
pancake <pancake@nopcode.org>
2021-02-14 12:06:27 +0100

    Oops, fix remaining ?0/?1 -> ?t0/?t1 in debugger

--- libr/core/visual.c
+++ libr/core/visual.c
@@ -4313,7 +4313,7 @@ dodo:
 
 			if (cmdvhex && *cmdvhex) {
 				snprintf (debugstr, sizeof (debugstr),
-					"?t0;f tmp;ssr %s;%s;?1;%s;?1;"
+					"?t0;f tmp;ssr %s;%s;?t1;%s;?t1;"
 					"ss tmp;f-tmp;pd $r", reg, cmdvhex,
 					ref? "drr": "dr=");
 				debugstr[sizeof (debugstr) - 1] = 0;
@@ -4322,7 +4322,7 @@ dodo:
 				const char sign = (delta < 0)? '+': '-';
 				const int absdelta = R_ABS (delta);
 				snprintf (debugstr, sizeof (debugstr),
-					"diq;?0;f tmp;ssr %s;%s %d@$$%c%d;"
+					"diq;?t0;f tmp;ssr %s;%s %d@$$%c%d;"
 					"?t1;%s;"
 					"?t1;ss tmp;f-tmp;afal;pd $r",
 					reg, pxa? "pxa": pxw, size, sign, absdelta,
