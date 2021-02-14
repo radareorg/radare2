f214b0c8af0af11d419b8975f8c6d9ce76d0fa03
pancake <pancake@nopcode.org>
2021-02-14 02:30:16 +0100

    Fixes to make the r2blob shine again ##build

--- binr/blob/r2blob.c
+++ binr/blob/r2blob.c
@@ -1,9 +1,9 @@
-/* radare - LGPL - Copyright 2012-2020 - pancake */
+/* radare - LGPL - Copyright 2012-2021 - pancake */
 
 #include <r_main.h>
 #include <r_util.h>
 
-int main(int argc, char **argv) {
+int main(int argc, const char **argv) {
 	int rc = 1;
 	const char *prog_name = r_file_basename (argv[0]);
 	RMain *m = r_main_new (prog_name);
