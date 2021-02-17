d9364d5e7f818bf2dcf421b0cb78cd5c63459c98
condret <condr3t@protonmail.com>
2021-02-17 18:29:39 +0100

    Add dummy_op to esil_dummy

--- libr/anal/p/esil_dummy.c
+++ libr/anal/p/esil_dummy.c
@@ -5,13 +5,23 @@
 #include <r_lib.h>
 #include <r_anal.h>
 
+static bool esil_dummy_operation (RAnalEsil *esil) {
+	eprintf ("Dummy: Operation executed\n");
+	return true;
+}
 
 static void *r_esil_dummy_init (RAnalEsil *esil) {
+	r_anal_esil_set_op (esil, "dummy_op", esil_dummy_operation,
+				0, 0, R_ANAL_ESIL_OP_TYPE_CUSTOM);
 	eprintf ("Dummy: Activated\n");
 	return NULL;
 }
 
 static void r_esil_dummy_fini (RAnalEsil *esil, void *user) {
+	RAnalEsilOp *op = r_anal_esil_get_op (esil, "dummy_op");
+	if (op && op->code == esil_dummy_operation) {
+		r_anal_esil_del_op (esil, "dummy_op");
+	}
 	eprintf ("Dummy: Deactivated\n");
 }
 
