dcd584a0ad6032969c7a852e9f511a93a0b3f37b
pancake <pancake@nopcode.org>
2021-03-08 03:08:22 +0100

    Implement slurp:// uri handler plugin ##io
    * Same as oom but without the extra step

--- libr/io/p/io_malloc.c
+++ libr/io/p/io_malloc.c
@@ -1,4 +1,4 @@
-/* radare - LGPL - Copyright 2008-2017 - pancake */
+/* radare - LGPL - Copyright 2008-2021 - pancake */
 
 #include "r_lib.h"
 #include <stdio.h>
@@ -6,7 +6,9 @@
 #include "../io_memory.h"
 
 static bool __check(RIO *io, const char *pathname, bool many) {
-	return (!strncmp (pathname, "malloc://", 9)) || (!strncmp (pathname, "hex://", 6));
+	return (!strncmp (pathname, "slurp://", 8)
+		|| !strncmp (pathname, "malloc://", 9))
+		|| (!strncmp (pathname, "hex://", 6));
 }
 
 static RIODesc *__open(RIO *io, const char *pathname, int rw, int mode) {
@@ -15,14 +17,22 @@ static RIODesc *__open(RIO *io, const char *pathname, int rw, int mode) {
 		if (!mal) {
 			return NULL;
 		}
-		if (!strncmp (pathname, "hex://", 6)) {
+		if (!strncmp (pathname, "slurp://", 8)) {
+			size_t size;
+			char *buf = r_file_slurp (pathname + 8, &size);
+			if (!buf || size < 1) {
+				free (mal);
+				return NULL;
+			}
+			mal->size = size;
+			mal->buf = (ut8*)buf;
+		} else if (!strncmp (pathname, "hex://", 6)) {
 			mal->size = strlen (pathname);
 			mal->buf = calloc (1, mal->size + 1);
 			if (!mal->buf) {
 				free (mal);
 				return NULL;
 			}
-			mal->offset = 0;
 			mal->size = r_hex_str2bin (pathname + 6, mal->buf);
 			if ((int)mal->size < 1) {
 				R_FREE (mal->buf);
@@ -34,9 +44,9 @@ static RIODesc *__open(RIO *io, const char *pathname, int rw, int mode) {
 				eprintf ("Cannot allocate (%s) 0 bytes\n", pathname + 9);
 				return NULL;
 			}
-			mal->offset = 0;
 			mal->buf = calloc (1, mal->size + 1);
 		}
+		mal->offset = 0;
 		if (mal->buf) {
 			return r_io_desc_new (io, &r_io_plugin_malloc, pathname, R_PERM_RW | rw, mode, mal);
 		}
@@ -49,7 +59,7 @@ static RIODesc *__open(RIO *io, const char *pathname, int rw, int mode) {
 RIOPlugin r_io_plugin_malloc = {
 	.name = "malloc",
 	.desc = "Memory allocation plugin",
-	.uris = "malloc://,hex://",
+	.uris = "malloc://,hex://,slurp://",
 	.license = "LGPL3",
 	.open = __open,
 	.close = io_memory_close,
