f8ea69e594de4b6f6cc96733c15b500ec77624dc
condret <condr3t@protonmail.com>
2021-03-14 19:16:51 +0100

    Implement r_io_bank_{free,init,fini] (siol_eternal) ##io

--- libr/io/io_bank.c
+++ libr/io/io_bank.c
@@ -3,7 +3,7 @@
 #include <r_io.h>
 #include <r_util.h>
 
-R_API RIOBank *r_io_bank_new() {
+R_API RIOBank *r_io_bank_new(void) {
 	RIOBank *bank = R_NEW0 (RIOBank);
 	if (!bank) {
 		return NULL;
@@ -27,4 +27,34 @@ R_API RIOBank *r_io_bank_new() {
 		return NULL;
 	}
 	return bank;
+}
+
+R_API void r_io_bank_free(RIOBank *bank) {
+	if (!bank) {
+		return;
+	}
+	r_queue_free (bank->todo);
+	r_list_free (bank->maprefs);
+	r_rbtree_cont_free (bank->submaps);
+	free (bank);
+}
+
+static bool _bank_free_cb(void *user, void *data, ut32 id) {
+	r_io_bank_free ((RIOBank *)data);
+	return true;
+}
+
+R_API void r_io_bank_init(RIO *io) {
+	r_return_if_fail (io);
+	r_io_bank_fini (io);
+	io->banks = r_id_storage_new (0, UT32_MAX);
+}
+
+R_API void r_io_bank_fini(RIO *io) {
+	r_return_if_fail (io);
+	if (io->banks) {
+		r_id_storage_foreach (io->banks, _bank_free_cb, NULL);
+		r_id_storage_free (io->banks);
+		io->banks = NULL;
+	}
 }
