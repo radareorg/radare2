ff370a97e41a2bebe5619feeaf87fbfeee5094bc
pancake <pancake@nopcode.org>
2021-03-18 00:12:00 +0100

    Add more tests

--- test/unit/test_ssdeep.c
+++ test/unit/test_ssdeep.c
@@ -8,22 +8,24 @@ static const char *input_2 = "abcdefghijklmnopqrstuvwxyz01234567890______GHIJKLM
 
 static const char *hash_0 = "1:EHGBADCNMPOJILKVUXWRQTSdcfVUXWRQTSdcVknmhgjitsvuporq1032xwzy98/n:EHGBADCNMPOJILKVUXWRQTSdcf+XXTSP";
 static const char *hash_1 = "1:EHGBADCNMPOJILKVUXWRQTSdcfVUXWRQTSdcVknmhgjitsvuporq1032xwzy986n:uWmCNMPOJILKVUXWRQTSdcf+XXTSdcVT";
-static const char *hash_2 = "hash2";
+static const char *hash_2 = "1:EHGBADCNMPOJILKVUXWRQTSdcfVUXWRQTSdcV666666itsvuporq1032xwzy986n:uWmCNMPOJILKVUXWRQTSdcf+XXTSdcVE";
 
 int test_ssdeep(void) {
+#define d r_str_distance
 	char *h0 = r_hash_ssdeep ((const ut8*)input_0, strlen (input_0));
 	char *h1 = r_hash_ssdeep ((const ut8*)input_1, strlen (input_1));
 	char *h2 = r_hash_ssdeep ((const ut8*)input_2, strlen (input_2));
 	mu_assert_streq (h0, hash_0, "hash0 is fine");
 	mu_assert_streq (h1, hash_1, "hash1 is fine");
-	int distance = r_str_distance (h0, h1);
-	mu_assert_eq (88, distance, "distance"); // 80% equal
-	int distance2 = r_str_distance (h0, h0);
-	mu_assert_eq (100, distance2, "distance2");
-	int distance3 = r_str_distance (h0, "123");
-	mu_assert_eq (2, distance3, "distance3");
-	int distance4 = r_str_distance (h0, h2);
-	mu_assert_eq (82, distance4, "distance4");
+	mu_assert_streq (h2, hash_2, "hash2 is fine");
+	mu_assert_eq (100, d(h0, h0), "distance2");
+	mu_assert_eq (88, d(h0, h1), "distance"); // 80% equal
+	mu_assert_eq (82, d(h0, h2), "distance4");
+	mu_assert_eq (88, d(h1, h0), "distance5");
+	mu_assert_eq (100, d(h1, h1), "distance6");
+	mu_assert_eq (92, d(h1, h2), "distance6");
+	mu_assert_eq (92, d(h2, h1), "distance6");
+	mu_assert_eq (2, d(h0, "123"), "distance3");
 	free (h0);
 	free (h1);
 	free (h2);
