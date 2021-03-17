0b4cfc1dbbcb5d43db7ff502cc729dc91795c253
pancake <pancake@nopcode.org>
2021-03-17 19:05:09 +0100

    Fix version not being updated after running sys/termux.sh ##build

--- sys/termux.sh
+++ sys/termux.sh
@@ -4,8 +4,10 @@ if [ -z "${PREFIX}" ]; then
 	PREFIX=/data/data/com.termux/files/usr
 fi
 export ANDROID=1
-
+# make clean > /dev/null 2>&1
+rm -f libr/include/r_version.h
 bash ./configure --with-compiler=termux --prefix=${PREFIX}
+touch -t 19700101 libr/include/r_version.h
 rm -f "${PREFIX}/lib/"libr_*
 make -j2
 make symstall
