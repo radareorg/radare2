05225f267d8f25691e95a73f288c3c5f178519df
rhl120 <rhl120@protonmail.com>
2021-03-20 16:41:09 +0100

    Finlized branching

--- libr/core/rvcs.c
+++ libr/core/rvcs.c
@@ -21,7 +21,9 @@ struct rvc {
 	uint branch_num;
 	struct branch **branches;
 };
-static inline bool copy_commits (const char *dpath, const char *spath, struct rvc *repo) {
+static inline bool copy_commits(const char *dpath, const char *sname, const struct rvc *repo) {
+	char *spath = malloc (r_str_len_utf8 (repo->path) + r_str_len_utf8("branches/commits") + r_str_len_utf8(sname) + 1);
+	sprintf (spath, "%s/branches/commits/%s/commits", repo->path, sname);
 	RList *files = r_sys_dir (spath);
 	if (!files)
 		return false;
@@ -32,55 +34,65 @@ static inline bool copy_commits (const char *dpath, const char *spath, struct rv
 		path = malloc (r_str_len_utf8 (spath) + r_str_len_utf8 (name) + 1);
 		if (!path)
 			return false;
-		if (!r_file_copy (dpath, path))
+		if (!r_file_copy (dpath, path)) {
+			free (path);
 			return false;
+		}
 		free (path);
 	}
 }
-R_API bool rvc_branch (char *name, struct branch *parent, struct rvc *repo) {
+static inline char *branch_mkdir(struct rvc *repo, struct branch *b) {
+	char *path;
+	path = malloc (r_str_len_utf8(repo->path) + r_str_len_utf8("/branches/") + r_str_len_utf8(b->name) + 1);
+	if (!path) {
+		return NULL;
+	}
+	if (!r_sys_mkdirp (path)) {
+		free (path);
+		return NULL;
+	}
+	return path;
+}
+R_API bool rvc_branch(const char *name, const struct branch *parent, struct rvc *repo) {
 	struct branch *nb;
 	struct branch **tmp;
 	char *bpath;
 	nb = malloc (sizeof (struct branch));
-	bool ret = false;
 	if (!nb)
-		return ret;
+		return false;
+	nb->head = NULL;
 	nb->name = r_str_new (name);
 	if (!nb->name) {
 		free (nb);
-		return ret;
+		return false;
 	}
-	bpath = malloc (r_str_len_utf8 (repo->path) + r_str_len_utf8 ("branches/") + r_str_len_utf8 (name) + r_str_len_utf8 ("/commits/") + 1);
-	if (!bpath) {
-		free (nb->name);
-		free (nb);
-		return ret;
-	}
-	sprintf (bpath, "%sbranches/%s/commits", repo->path, name);
-	if (!parent) {
-		nb->head = NULL;
-		ret = r_sys_mkdirp (bpath);
-		goto ret;
-	}
-	ret = copy_commits (bpath, parent->name, repo);
-	if (!ret)
-		goto ret;
 	repo->branch_num++;
-	tmp = realloc (repo->branches, repo->branch_num * sizeof (struct branch));
+	tmp = realloc (repo->branches, repo->branch_num * sizeof (struct branch *));
 	if (!tmp) {
 		repo->branch_num--;
-		goto ret;
+		free (nb->name);
+		free (nb);
+		free (bpath);
+		return false;
 	}
+	repo->branches = tmp;
 	repo->branches[repo->branch_num - 1] = nb;
-	ret = true;
-	goto ret;
-ret:
-	free (nb->name);
-	free (nb);
-	free (bpath);
-	return ret;
+	bpath = branch_mkdir (repo, nb);
+	if (!bpath)
+		return false;
+	if (parent) {
+		nb->head = parent->head;
+		if (!copy_commits (bpath, parent->name, repo)) {
+			free (nb->name);
+			free (nb);
+			repo->branch_num--;
+			repo->branches = realloc (repo->branches, repo->branch_num * sizeof (struct branch *));
+			free (bpath);
+		}
+	}
+	return true;
 }
-R_API struct rvc *rvc_init (const char *path) {
+R_API struct rvc *rvc_init(const char *path) {
 	struct rvc *repo;
 	repo = malloc (sizeof (struct rvc));
 	if (!repo)
