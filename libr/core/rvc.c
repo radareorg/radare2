c70cc12e7ba68781fefae3f59284527ce5cb9a1c
rhl120 <rhl120@protonmail.com>
2021-03-22 22:31:04 +0100

    Added log messages

--- libr/core/rvc.c
+++ libr/core/rvc.c
@@ -42,20 +42,26 @@ static char *branch_mkdir(Rvc *repo, RvcBranch *b) {
 R_API bool rvc_branch(Rvc *repo, const char *name, const RvcBranch *parent) {
 	char *bpath;
 	RvcBranch *nb = R_NEW0 (RvcBranch);
-	r_return_val_if_fail (nb, false);
+	if (!nb) {
+		eprintf ("Failed To Allocate Branch Struct\n");
+		return false;
+	}
 	nb->head = NULL;
 	nb->name = r_str_new (name);
 	if (!nb->name) {
+		eprintf ("Failed To Allocate Branch Name\n");
 		free (nb);
 		return false;
 	}
 	if (!r_list_append (repo->branches, nb)) {
+		eprintf ("Failed To Allocate Branch Struct\n");
 		free (nb->name);
 		free (nb);
 		return false;
 	}
 	bpath = branch_mkdir (repo, nb);
 	if (!bpath) {
+		eprintf ("Failed To Create Branch Directory\n");
 		free (nb->name);
 		free (nb);
 		r_list_pop (repo->branches);
@@ -64,8 +70,11 @@ R_API bool rvc_branch(Rvc *repo, const char *name, const RvcBranch *parent) {
 	if (parent) {
 		nb->head = parent->head;
 		if (!copy_commits (repo, parent->name, bpath)) {
+			eprintf ("Failed To Copy Commits From Parent\n");
 			free (nb->name);
 			free (nb);
+			free (bpath);
+			return false;
 		}
 	}
 	free (bpath);
@@ -75,21 +84,32 @@ R_API bool rvc_branch(Rvc *repo, const char *name, const RvcBranch *parent) {
 R_API Rvc *rvc_new(const char *path) {
 	Rvc *repo;
 	repo = R_NEW (Rvc);
-	r_return_val_if_fail (repo, NULL);
+	if (!repo) {
+		eprintf ("Failed To Allocate Repoistory Path\n");
+	}
 	repo->path = r_str_newf ("%s" R_SYS_DIR ".rvc" R_SYS_DIR, path);
 	if (!repo->path) {
+		eprintf ("Failed To Allocate Repository Path\n");
 		free (repo);
+		return NULL;
 	}
 	if (!r_sys_mkdir (repo->path)) {
+		eprintf ("Failed To Create Repo Directory\n");
 		free (repo->path);
 		free (repo);
 	}
 	repo->branches = r_list_new ();
 	if (!repo->branches) {
+		eprintf ("Failed To Allocate Branches List\n");
 		free (repo);
 		free (repo->path);
 		return NULL;
 	}
-	rvc_branch (repo, "master", NULL);
+	if (!rvc_branch (repo, "master", NULL)) {
+		free (repo->path);
+		r_list_free (repo->branches);
+		free (repo);
+		return NULL;
+	}
 	return repo;
 }
