radareorg/radare2

commit 765f2b6bd4098b8e90e46a45c174a71099bc0d56

Fix #2818 - Fix proper chopping of trailing spaces after comments (#17090)
* Fix #2818 - Fix proper chopping of trailing spaces after comments
* Handle the hashbang
* Add test

Co-authored-by: pancake <pancake@nopcode.org>

libr/core/cmd.c
@@ -2624,9 +2624,25 @@ static int r_core_cmd_subst(RCore *core, char *cmd) {
 	if (!icmd || (cmd[0] == '#' && cmd[1] != '!' && cmd[1] != '?')) {
 		goto beach;
 	}
-	cmt = *icmd ? (char *)r_str_firstbut (icmd, '#', "\""): NULL;
-	if (cmt && (cmt[1] == ' ' || cmt[1] == '\t')) {
-		*cmt = 0;
+	if (*icmd && !strchr (icmd, '"')) {
+		char *hash = icmd;
+		cmt = NULL;
+		for (hash = icmd + 1; *hash; hash++) {
+			if (*hash == '\\') {
+				hash++;
+				if (*hash == '#') {
+					hash++;
+				}
+			}
+			if (*hash == '#') {
+				break;
+			}
+		}
+		if (hash && *hash) {
+			*hash = 0;
+			r_str_trim_tail (icmd);
+			cmt = hash + 1;
+		}
 	}
 	if (*cmd != '"') {
 		if (!strchr (cmd, '\'')) { // allow | awk '{foo;bar}' // ignore ; if there's a single quote

test/db/cmd/cmd_search
@@ -1,3 +1,12 @@
+NAME=trailing comment
+FILE=-
+ARGS=-n
+CMDS=<<EOF
+/ #
+EOF
+EXPECT=
+RUN
+
 NAME=pm olf
 FILE=bins/elf/ioli/crackme0x00
 ARGS=-n

commit 198ef7e997d30fcc3c78d6552330d09a3cae8c52

Fix proper hash comments & quotes mix (#18551) ##shell
The problem was that the quotes are not checked to be before or after
the hash comment.

Fixes: 765f2b6
Signed-off-by: Anthoine Bourgeois <anthoine.bourgeois@gmail.com>

Co-authored-by: Anthoine Bourgeois <anthoine.bourgeois@blade-group.com>

libr/core/cmd.c
@@ -3062,24 +3062,28 @@ static int r_core_cmd_subst(RCore *core, char *cmd) {
 	if (!icmd || (cmd[0] == '#' && cmd[1] != '!' && cmd[1] != '?')) {
 		goto beach;
 	}
-	if (*icmd && !strchr (icmd, '"')) {
+	if (*cmd) {
+		int inside_quote = 0;
 		char *hash;
-		for (hash = icmd + 1; *hash; hash++) {
+		for (hash = cmd; *hash; hash++) {
 			if (*hash == '\\') {
 				hash++;
-				if (*hash == '#') {
+				if (*hash == '#' || *hash == '"') {
 					continue;
 				} else if (!*hash) {
 					break;
 				}
 			}
-			if (*hash == '#') {
+			if (*hash == '"') {
+				inside_quote ^= 1;
+			}
+			if (*hash == '#' && hash != cmd && !inside_quote) {
 				break;
 			}
 		}
-		if (hash && *hash) {
+		if (hash && *hash && !inside_quote) {
 			*hash = 0;
-			r_str_trim_tail (icmd);
+			r_str_trim_tail (cmd);
 		}
 	}
 	if (*cmd != '"') {

test/db/cmd/cmd_interpret
@@ -32,6 +32,7 @@ EOF
 RUN
 
 NAME=$foo=#!pipe
+ARGS=-ecfg.newshell=true
 FILE=-
 CMDS=<<EOF
 #!pipe echo hello
@@ -42,7 +43,6 @@ $foo
 $foo
 ?e --
 $-*
-e cfg.newshell=true
 $foo=\#!pipe echo hello
 $foo
 ?e --

test/db/cmd/cmds
@@ -154,3 +154,13 @@ EXPECT=<<EOF
 b821000000
 EOF
 RUN
+
+NAME=hash comment and double quote
+FILE=malloc://1024
+CMDS=<<EOF
+?? # ;o"
+EOF
+EXPECT=<<EOF
+0
+EOF
+RUN