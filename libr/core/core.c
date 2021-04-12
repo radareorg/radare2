bbc2fbbbb0d7a98358bef489e658fbe60147d528
murphy <dark.cancerbero@gmail.com>
2021-04-12 15:49:58 +0200

    Fix 1450830 Resource leak

--- libr/core/core.c
+++ libr/core/core.c
@@ -1597,6 +1597,7 @@ static void autocomplete_vars(RCore *core, RLineCompletion *completion, const ch
 			r_line_completion_push (completion, var->name);
 		}
 	}
+	r_list_free (vars);
 }
 
 static void autocomplete_macro(RCore *core, RLineCompletion *completion, const char *str) {
