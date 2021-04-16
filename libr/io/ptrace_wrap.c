71dc8b715c4bb8c5261bbdc25ebba5b01fda3546
pancake <pancake@youterm.com>
2021-04-16 19:19:56 +0200

    Move shlr/ptrace-wrap into libr/io, simplify build (#18587)

--- libr/io/ptrace_wrap.c
+++ libr/io/ptrace_wrap.c
@@ -1,5 +1,5 @@
 /*
- * This file is part of ptrace-wrap.
+ * This file is part of ptrace-wrap. (github.com/thestr4ng3r/ptrace-wrap)
  *
  * ptrace-wrap is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
--- shlr/ptrace-wrap/deps.mk
+++
@@ -1,3 +0,0 @@
-CFLAGS+=-I$(STOP)/ptrace-wrap/include
-LINK+=$(STOP)/ptrace-wrap/libptrace_wrap.$(EXT_AR)
-PTRACEWRAP_OBJS=$(STOP)/ptrace-wrap/src/ptrace_wrap.o
