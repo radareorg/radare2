40c7adb428a1afd428223fe43d5f99516502b7e0
rhl120 <rhl120@protonmail.com>
2021-04-18 21:12:36 +0200

    Added an rvc_git git_commit_interactive

--- libr/core/rvc.c
+++ libr/core/rvc.c
@@ -527,3 +527,6 @@ R_API int git_add (const char *path, const char *fname) {
 R_API int git_commit (const char *path, const char *message) {
 	return r_sys_cmdf ("git -C %s commit -m %s", path, message);
 }
+R_API int git_commit_interactive (const char *path) {
+	return r_sys_cmdf ("git -C %s commit", path);
+}
