085f1f1f4bd1d4fc1cbf64dfd31effcab35c035f
condret <condr3t@protonmail.com>
2021-04-20 15:54:46 +0200

    Moar codemeta speedup

--- libr/anal/codemeta.c
+++ libr/anal/codemeta.c
@@ -85,6 +85,36 @@ static int cmp_ins(void *incoming, void *in, void *user) {
 	return ((int)mi2->type) - ((int)mi->type);	// avoid weird things
 }
 
+// cmp to find the lowest mid, that is bigger than or equal to search_mid
+// consider adding mod-bit to search_mid
+static int cmp_find_min_mid(void *incoming, void *in, void *user) {
+	RCodeMetaItem **min = (RCodeMetaItem **)user;
+	RCodeMetaItem *mi = (RCodeMetaItem *)in;
+	size_t *search_mid = (size_t *)incoming;
+	const size_t mid = mi->start + (mi->end - mi->start) / 2;
+	if (mid > search_mid[0]) {
+		if (!min[0]) {
+			min[0] = mi;
+			return -1;
+		}
+		const size_t min_mid = min[0]->start + (min[0]->end - min[0]->start) / 2;
+		if (mid < min_mid) {
+			min[0] = mi;
+		} else if (mid == min_mid) {
+			const ut32 mod = (mi->end - mi->start) & 0x1;
+			const ut32 min_mod = (min[0]->end - min[0]->start) & 0x1;
+			if (mod < min_mod) {
+				min[0] = mi;
+			}
+		}
+		return -1;
+	} else if (mid == search_mid[0]) {
+		min[0] = mi;
+		return 0;
+	}
+	return 1;
+}
+
 static int cm_add(void *incoming, void *in, void *user) {
 	RCodeMetaItem *mi = in;
 	RCodeMetaItem *mi2 = incoming;
@@ -113,7 +143,7 @@ static int cm_cmp(void *incoming, void *in, void *user) {
 R_API void r_codemeta_add_annotation(RCodeMeta *code, RCodeMetaItem *mi) {
 	r_return_if_fail (code && mi);
 	r_vector_push (&code->annotations, mi);
-	r_rbtree_cont_insert (code->tree, mi, cm_add, NULL);
+	r_rbtree_cont_insert (code->tree, mi, cmp_ins, NULL);
 }
 
 R_API RPVector *r_codemeta_at(RCodeMeta *code, size_t offset) {
@@ -127,8 +157,31 @@ R_API RPVector *r_codemeta_in(RCodeMeta *code, size_t start, size_t end) {
 		return NULL;
 	}
 #if USE_TRI
-	RCodeMetaItem my = { .start = start, .end = end };
-	r_rbtree_cont_find (code->tree, &my, cm_cmp, r);
+	size_t search_start = start / 2;
+	RCodeMetaItem *min = NULL;
+	r_rbtree_cont_find (code->tree, &search_start, cmp_find_min_mid, &min);
+	if (min) {
+		RContRBNode *node = r_rbtree_cont_find (code->tree, min, cmp_ins, NULL);	//get node for min
+		RContRBNode *prev = r_rbtree_cont_node_prev (node);
+		while (prev)
+			RCodeMetaItem *mi = (RCodeMetaItem *)prev->data;
+			if (mi->end <= start) {
+				break;
+			}
+			node = prev;
+			prev = r_rbtree_cont_node_prev (node);
+		}
+		while (node) {
+			r_pvector_push (r, node->data);
+			node = r_rbtree_cont_node_next (node);
+			if (node) {
+				RCodeMetaItem *mi = (RCodeMetaItem *)node->data;
+				if (end < mi->start) {
+					break;
+				}
+			}
+		}
+	}
 	return r;
 #else
 	RCodeMetaItem *mi;
