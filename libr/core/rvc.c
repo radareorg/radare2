8ca8a77869440843efac01d6f8872e2a2e257cf2
rhl120 <rhl120@protonmail.com>
2021-04-25 02:01:47 +0200

    Wrap %s in quotes r_vc_git

--- libr/core/rvc.c
+++ libr/core/rvc.c
@@ -513,15 +513,15 @@ R_API bool r_vc_checkout(Rvc *repo, const char *name) {
 }
 
 R_API int r_vc_git_init (const char *path) {
-	return r_sys_cmdf ("git init %s", path);
+	return r_sys_cmdf ("git init '%s'", path);
 }
 
 R_API bool r_vc_git_branch (const char *path, const char *name) {
-	return !r_sys_cmdf ("git -C %s branch %s", path, name);
+	return !r_sys_cmdf ("git -C '%s' branch '%s'", path, name);
 }
 
 R_API bool r_vc_git_checkout (const char *path, const char *name) {
-	return !r_sys_cmdf ("git -C %s checkout %s", path, name);
+	return !r_sys_cmdf ("git -C '%s' checkout '%s'", path, name);
 }
 
 R_API int r_vc_git_add (const char *path, const char *fname) {
@@ -535,7 +535,7 @@ R_API int r_vc_git_add (const char *path, const char *fname) {
 		free (cwd);
 		return -2;
 	}
-	ret = r_sys_cmdf ("pwd; git add %s", fname);
+	ret = r_sys_cmdf ("pwd; git add '%s'", fname);
 	if (!r_sys_chdir (cwd)) {
 		free (cwd);
 		return -3;
@@ -545,6 +545,6 @@ R_API int r_vc_git_add (const char *path, const char *fname) {
 }
 
 R_API int r_vc_git_commit (const char *path, const char *message) {
-	return message ? r_sys_cmdf ("git -C %s commit -m %s", path, message) :
-		r_sys_cmdf ("git -C %s commit", path);
+	return message ? r_sys_cmdf ("git -C '%s' commit -m '%s'", path, message) :
+		r_sys_cmdf ("git -C '%s' commit", path);
 }
