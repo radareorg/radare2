fbe1020b1a04e98e8df8e19cfede70be5cfa0b19
pancake <pancake@nowsecure.com>
2021-05-21 10:15:00 +0200

    Update libr/core/cmd_hash.c

--- libr/core/cmd_hash.c
+++ libr/core/cmd_hash.c
@@ -326,7 +326,7 @@ static void handle_fletcher16 (const ut8 *block, int len) {
 	r_cons_printf ("%02x\n", r_hash_fletcher16 (block, len));
 }
 
-static void handle_fletcher32 (const ut8 *block, int len) {
+static void handle_fletcher32(const ut8 *block, int len) {
 	r_cons_printf ("%" PFMT32x "\n", r_hash_fletcher32 (block, len));
 }
 
