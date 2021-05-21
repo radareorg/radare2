525dac6644db444b2009993d044fb9812507f115
pancake <pancake@nowsecure.com>
2021-05-21 10:15:18 +0200

    Update libr/core/cmd_hash.c

--- libr/core/cmd_hash.c
+++ libr/core/cmd_hash.c
@@ -65,7 +65,7 @@ static void handle_xorpair(const ut8 *block, int len) {
 	r_cons_printf ("%04x\n", r_hash_xorpair (block, len));
 }
 
-static void handle_entropy (const ut8 *block, int len) {
+static void handle_entropy(const ut8 *block, int len) {
 	r_cons_printf ("%f\n", r_hash_entropy (block, len));
 }
 
