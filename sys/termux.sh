9833d827a81141e55c1446a7c693bed391bd26d2
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2021-06-04 18:02:55 +0200

    Improve error checking in termux build script

--- sys/termux.sh
+++ sys/termux.sh
@@ -6,9 +6,9 @@ fi
 export ANDROID=1
 # make clean > /dev/null 2>&1
 rm -f libr/include/r_version.h
-bash ./configure --with-compiler=termux --prefix=${PREFIX}
+bash ./configure --with-compiler=termux --prefix=${PREFIX} || exit 1
 make libr/include/r_version.h
 touch -t 19700101 libr/include/r_version.h
 rm -f "${PREFIX}/lib/"libr_*
-make -j2
+make -j2 || exit 1
 make symstall
