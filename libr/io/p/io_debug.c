6b7c15ce0a79bdc40e8879cf19f7fbe9b2abcb03
gogo <gogo246475@gmail.com>
2021-06-12 03:45:59 +0200

    refactor WIFSTOPPED

--- libr/io/p/io_debug.c
+++ libr/io/p/io_debug.c
@@ -410,9 +410,8 @@ static int fork_and_ptraceme_for_unix(RIO *io, int bits, const char *cmd) {
 		usleep (100000);
 		r_cons_sleep_end (bed);
 	} while (ret != child_pid && !r_cons_is_breaked ());
-	if (WIFSTOPPED (status)) {
-		eprintf ("Process with PID %d started...\n", (int)child_pid);
-		return child_pid;
+	if (!WIFSTOPPED (status)) {
+		return -1;
 	}
 	if (WEXITSTATUS (status) == MAGIC_EXIT || r_cons_is_breaked ()) {
 		eprintf ("Killing child process %d due to an error\n", (int)child_pid);
