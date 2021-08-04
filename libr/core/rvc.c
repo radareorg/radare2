5a5c8b80afa33b5d57ebf3eec18ef4eb5b21cd3b
rhl120 <rhl120@protonmail.com>
2021-08-04 18:02:14 +0200

    Fixed the repo check in repo_files
    Co-authored-by: pancake <pancake@nopcode.org>

--- libr/core/rvc.c
+++ libr/core/rvc.c
@@ -404,13 +404,15 @@ static bool traverse_files(RList *dst, const char *dir) {
 }
 
 static RList *repo_files(const char *dir) {
-	RList *ret = r_list_new ();
-	if (!ret && !traverse_files (ret, dir)) {
-		r_list_free (ret);
-		return NULL;
-	}
-	return ret;
-}
+ 	RList *ret = r_list_newf (free);
+ 	if (ret) {
+           if (!traverse_files (ret, dir)) {
+ 		r_list_free (ret);
+ 		ret = NULL;
+           }
+ 	}
+ 	return ret;
+ }
 
 //shit function:
 static RList *get_uncommitted(const char *rp) {
