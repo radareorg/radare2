7c2edb1cdd7916ce7fff456ac5fcdbbc8713b05b
pancake <pancake@nopcode.org>
2021-08-19 11:44:57 +0200

    Fix #18783 - Support ELFs with phnum > 0xFFFF ##bin
    * Useful for loading qemu coredumps

--- libr/bin/format/elf/elf.h
+++ libr/bin/format/elf/elf.h
@@ -210,6 +210,7 @@ bool Elf_(r_bin_elf_section_perms)(RBinFile *bf, const char *name, int perms);
 bool Elf_(r_bin_elf_entry_write)(RBinFile *bf, ut64 addr);
 bool Elf_(r_bin_elf_del_rpath)(RBinFile *bf);
 
+ut64 Elf_(r_bin_elf_get_phnum)(ELFOBJ *bin);
 bool Elf_(r_bin_elf_is_executable)(ELFOBJ *bin);
 int Elf_(r_bin_elf_has_relro)(struct Elf_(r_bin_elf_obj_t) *bin);
 int Elf_(r_bin_elf_has_nx)(struct Elf_(r_bin_elf_obj_t) *bin);
