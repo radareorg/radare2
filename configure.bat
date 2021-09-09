9d18eb1a3af2d86f01825573baf90b5cf0b8ddc1
pancake <pancake@nowsecure.com>
2021-09-09 11:59:11 -0700

    Fix crash on windows `dd`, still wip ##debug

--- configure.bat
+++ configure.bat
@@ -2,9 +2,18 @@
 REM call preconfigure.bat
 
 set PATH=%CD%\prefix\bin;%PATH%
+if EXIST vs (
+meson vs -Dsdb_cgen=false --backend vs --reconfigure
+) else (
 meson vs -Dsdb_cgen=false --backend vs
+)
 
+if EXIST b (
 meson b -Dsdb_cgen=false --reconfigure
+) else (
+meson b -Dsdb_cgen=false
+)
+
 if %ERRORLEVEL% == 0 (
   echo Done
   exit /b 0
