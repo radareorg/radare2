6576366ca02190301c6173737b95bee5e4aa3369
pancake <pancake@nopcode.org>
2021-10-25 00:18:57 +0200

    Add termux-specific plugins configuration file

--- sys/termux.sh
+++ sys/termux.sh
@@ -6,6 +6,8 @@ fi
 export ANDROID=1
 # make clean > /dev/null 2>&1
 rm -f libr/include/r_version.h
+cp -f dist/plugins/plugins.termux.cfg plugins.cfg
+./configure-plugins
 bash ./configure --with-compiler=termux --prefix=${PREFIX} || exit 1
 make libr/include/r_version.h
 touch -t 19700101 libr/include/r_version.h
