1675a6ccc91f20796630d6752960408a29bb9bb8
pancake <pancake@nopcode.org>
2021-10-26 17:01:50 +0200

    Indent make.bat

--- make.bat
+++ make.bat
@@ -3,21 +3,20 @@
 ninja --version > NUL 2> NUL
 if %ERRORLEVEL% == 0 (
   if EXIST b (
-  ninja -C b
-
-  if %ERRORLEVEL% == 0 (
-    echo Installing r2 in %CD%\prefix
-    set DESTDIR=%CD%\prefix
-    ninja -C b install > NUL
-    copy /y %DESTDIR%\bin\radare2.exe %DESTDIR%\bin\r2.exe
-    copy /y C:\WINDOWS\System32\vcruntime140.dll %DESTDIR%\bin\vcruntime140.dll
+    ninja -C b
+    if %ERRORLEVEL% == 0 (
+      echo Installing r2 in %CD%\prefix
+      set DESTDIR=%CD%\prefix
+      ninja -C b install > NUL
+      copy /y %DESTDIR%\bin\radare2.exe %DESTDIR%\bin\r2.exe
+      copy /y C:\WINDOWS\System32\vcruntime140.dll %DESTDIR%\bin\vcruntime140.dll
+    ) else (
+      exit /b 1
+    )
   ) else (
+    echo Please run configure before make
     exit /b 1
   )
-) else (
-  echo Please run configure before make
-  exit /b 1
-)
 ) else (
   echo Please run preconfigure
   exit /b 1
