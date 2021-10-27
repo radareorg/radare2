884a7055e2e6add10b33170eb2bd3a3f0de3ec1b
Francesco Tamagni <mrmacete@protonmail.ch>
2021-10-27 10:29:13 +0200

    Fix signed overflow in r_buf_fread_at ##util

--- libr/util/buf.c
+++ libr/util/buf.c
@@ -562,7 +562,7 @@ R_API st64 r_buf_fread(RBuffer *b, ut8 *buf, const char *fmt, int n) {
 R_API st64 r_buf_fread_at(RBuffer *b, ut64 addr, ut8 *buf, const char *fmt, int n) {
 	r_return_val_if_fail (b && buf && fmt, -1);
 	st64 o_addr = r_buf_seek (b, 0, R_BUF_CUR);
-	int r = r_buf_seek (b, addr, R_BUF_SET);
+	st64 r = r_buf_seek (b, addr, R_BUF_SET);
 	if (r < 0) {
 		return r;
 	}
