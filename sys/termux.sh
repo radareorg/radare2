3b078eab3dae21c1e41823236c566273513651b9
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2021-10-27 16:26:35 +0200

    preconfigure and pull on termux.sh

--- sys/termux.sh
+++ sys/termux.sh
@@ -7,6 +7,8 @@ export ANDROID=1
 # make clean > /dev/null 2>&1
 rm -f libr/include/r_version.h
 cp -f dist/plugins-cfg/plugins.termux.cfg plugins.cfg
+git pull
+./preconfigure
 ./configure-plugins
 bash ./configure --with-compiler=termux --prefix=${PREFIX} || exit 1
 make libr/include/r_version.h
