b6e57c1f72f1e84e45c503cd88ff19608138b902
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2021-10-29 23:55:10 +0200

    Fix oobread crash in RAnal.hexagon (tests_64900) ##crash
    Reported by giantbranch of NSFOCUS TIANJI Lab

--- libr/anal/p/anal_hexagon.c
+++ libr/anal/p/anal_hexagon.c
@@ -10,8 +10,11 @@
 #include "hexagon_anal.h"
 
 static int hexagon_v6_op(RAnal *anal, RAnalOp *op, ut64 addr, const ut8 *buf, int len, RAnalOpMask mask) {
-	HexInsn hi = {0};;
+	HexInsn hi = {0};
 	ut32 data = 0;
+	if (len < 4) {
+		return 0;
+	}
 	data = r_read_le32 (buf);
 	int size = hexagon_disasm_instruction (data, &hi, (ut32) addr);
 	op->size = size;
