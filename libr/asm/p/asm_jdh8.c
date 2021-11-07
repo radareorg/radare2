67db06009f9589b28d80bf595facaada0e6f961d
condret <condr3t@protonmail.com>
2021-11-07 21:31:28 +0100

    Add jdh-8 asm-plugin template

---
+++ libr/asm/p/asm_jdh8.c
@@ -0,0 +1,31 @@
+/* radare - <TODO> - Copyright 2021 - <TODO> */
+
+#include <r_types.h>
+#include <r_util.h>
+#include <r_asm.h>
+#include <r_lib.h>
+#include "../arch/jdh8/jdh8dis.c"
+
+static int disassemble(RAsm *a, RAsmOp *r_op, const ut8 *buf, int len) {
+	int dlen = jdh8Disass(r_op, buf, len);
+	return r_op->size = R_MAX (0, dlen);
+}
+
+RAsmPlugin r_asm_plugin_jdh8 = {
+	.name = "jdh8",
+	.desc = "jdh-8 toy architecture",
+	.arch = "jdh-8",
+	.author = "TODO",
+	.license = "TODO",
+	.bits = 16,
+	.endian = R_SYS_ENDIAN_LITTLE,
+	.disassemble = &disassemble,
+};
+
+#ifndef R2_PLUGIN_INCORE
+R_API RLibStruct radare_plugin = {
+	.type = R_LIB_TYPE_ASM,
+	.data = &r_asm_plugin_jdh8,
+	.version = R2_VERSION
+};
+#endif
