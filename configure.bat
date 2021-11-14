38c22fa199d451ccd63c2b345c1fa7d44f3c5b85
pancake <pancake@nopcode.org>
2021-11-14 17:59:38 +0100

    Properly check for %ERRORLEVEL% in batch scripts

--- configure.bat
+++ configure.bat
@@ -4,30 +4,32 @@ REM call preconfigure.bat
 set MESON_FLAGS=-Dsdb_cgen=false
 
 if "%*" == "asan" (
-set MESON_FLAGS=%MESON_FLAGS% -Dwasan=true
+  set MESON_FLAGS=%MESON_FLAGS% -Dwasan=true
 )
 
 set PATH=%CD%\prefix\bin;%PATH%
+set WORKS=0
 if EXIST vs (
-meson vs %MESON_FLAGS% --backend vs --reconfigure
+  meson vs %MESON_FLAGS% --backend vs --reconfigure && set WORKS=1
 ) else (
-meson vs %MESON_FLAGS% --backend vs
+  meson vs %MESON_FLAGS% --backend vs && set WORKS=1
 )
 
-if %ERRORLEVEL% == 0 (
+if %WORKS% EQU 1 (
   echo Done
 ) else (
   echo VS failed Try running 'preconfigure'
   exit /b 1
 )
 
+set WORKS=0
 if EXIST b (
-meson b %MESON_FLAGS% --reconfigure
+  meson b %MESON_FLAGS% --reconfigure && set WORKS=1
 ) else (
-meson b %MESON_FLAGS% --buildtype=release
+  meson b %MESON_FLAGS% --buildtype=release && set WORKS=1
 )
 
-if %ERRORLEVEL% == 0 (
+if %WORKS% EQU 1 (
   echo Done
   exit /b 0
 ) else (
--- make.bat
+++ make.bat
@@ -4,29 +4,28 @@ if "%*" == "clean" (
 	wsl rm -rf b vs
 	exit /b 0
 )
-ninja --version > NUL 2> NUL
-if %ERRORLEVEL% == 0 (
+call ninja.exe --version > NUL 2> NUL && (
   if EXIST b (
     REM meson compile -C b
-    ninja -C b
-    if %ERRORLEVEL% == 0 (
+    call ninja.exe -C b && (
       echo Installing r2 in %CD%\prefix
       set DESTDIR=%CD%\prefix
-      rmdir /s prefix
+      rmdir /q /s prefix
       REM meson install -C b
-      ninja -C b install > NUL
+      call ninja -C b install > NUL
       copy /y %DESTDIR%\bin\radare2.exe %DESTDIR%\bin\r2.exe
       REM COPY ASAN DLL FROM HERE
       REM "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64"
       copy /y C:\WINDOWS\System32\vcruntime140.dll %DESTDIR%\bin\vcruntime140.dll
-    ) else (
+    ) || (
+      echo Compilation has failed
       exit /b 1
     )
   ) else (
     echo Please run configure before make
     exit /b 1
   )
-) else (
-  echo Please run preconfigure
+) || (
+  echo Cannot find the ninja. Please run preconfigure
   exit /b 1
 )
