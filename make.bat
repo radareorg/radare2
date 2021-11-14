8b227868b848de93197d98f76e23c6ec918fe7f5
pancake <pancake@nowsecure.com>
2021-11-14 21:09:42 +0100

    Fix the windows CI workflows
    * Use ninja -j2 in the CI, seems like there are less resources for windows
    * Add windows configure+make+run CI workflow
    * Use more join_paths()

--- make.bat
+++ make.bat
@@ -7,18 +7,19 @@ if "%*" == "clean" (
 call ninja.exe --version > NUL 2> NUL && (
   if EXIST b (
     REM meson compile -C b
-    call ninja.exe -C b && (
+    call ninja.exe -C b -j 2 && (
       echo Installing r2 in %CD%\prefix
       set DESTDIR=%CD%\prefix
-      rmdir /q /s prefix
+      rmdir /q /s prefix 2> NUL
       REM meson install -C b
       call ninja -C b install > NUL
       copy /y %DESTDIR%\bin\radare2.exe %DESTDIR%\bin\r2.exe
       REM COPY ASAN DLL FROM HERE
       REM "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64"
       copy /y C:\WINDOWS\System32\vcruntime140.dll %DESTDIR%\bin\vcruntime140.dll
+      exit /b 0
     ) || (
-      echo Compilation has failed
+      echo Ninja compilation has failed
       exit /b 1
     )
   ) else (
