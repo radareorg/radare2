e62d0086139dbb3867e08530f54b9bc8157217f8
pancake <pancake@nopcode.org>
2021-11-14 20:58:03 +0100

    -j2

--- make.bat
+++ make.bat
@@ -7,7 +7,7 @@ if "%*" == "clean" (
 call ninja.exe --version > NUL 2> NUL && (
   if EXIST b (
     REM meson compile -C b
-    call ninja.exe -C b -j 1 && (
+    call ninja.exe -C b -j 2 && (
       echo Installing r2 in %CD%\prefix
       set DESTDIR=%CD%\prefix
       rmdir /q /s prefix 2> NUL
