592e6407d6cf26ea3dcf977d17965277783a8d78
meme <meme@users.noreply.github.com>
2021-11-21 12:44:34 -0500

    Improve Plan 9 support ##bin
    Currently support for Plan 9 binaries is limited to i386 and even then
    it doesn't work properly (certain sections won't load, the base address
    is wrong, etc.)
    
    With these changes, all architectures including ones from 9front are
    supported.

--- libr/bin/format/p9/p9bin.h
+++ libr/bin/format/p9/p9bin.h
@@ -1,56 +1,54 @@
+/* radare2 - LGPL - Copyright 2011-2021 - pancake, keegan */
+
 #ifndef P9BIN_H
 #define P9BIN_H
 
 #include <r_util.h>
 
-/*
- * Binary loader for Plan 9's a.out executable format
- * 
- * Copyright (C) 2008 Anant Narayanan
- */
+R_PACKED(
 struct plan9_exec {
-	unsigned long magic;	/* magic number */
-	unsigned long text;	/* size of text segment */
-	unsigned long data;	/* size of initialized data */
-	unsigned long bss;	/* size of uninitialized data */
-	unsigned long syms;	/* size of symbol table */
-	unsigned long entry;	/* entry point */
-	unsigned long spsz;	/* size of pc/sp offset table */
-	unsigned long pcsz;	/* size of pc/line number table */
-};
+	ut32 magic;
+	ut32 text;
+	ut32 data;
+	ut32 bss;
+	ut32 syms;
+	ut32 entry;
+	ut32 spsz;
+	ut32 pcsz;
+});
 
-#define HDR_MAGIC	0x00008000	/* header expansion */
+/* Flag for extended header. This means that an additional 64-bit integer follows
+ * the standard header which specifies the 64-bit entrypoint. */
+#define HDR_MAGIC 0x00008000
 
-#define	_MAGIC(f, b)	((f)|((((4*(b))+0)*(b))+7))
-#define	A_MAGIC		_MAGIC(0, 8)	/* 68020 */
-#define	I_MAGIC		_MAGIC(0, 11)	/* intel 386 */
-#define	J_MAGIC		_MAGIC(0, 12)	/* intel 960 (retired) */
-#define	K_MAGIC		_MAGIC(0, 13)	/* sparc */
-#define	V_MAGIC		_MAGIC(0, 16)	/* mips 3000 BE */
-#define	X_MAGIC		_MAGIC(0, 17)	/* att dsp 3210 (retired) */
-#define	M_MAGIC		_MAGIC(0, 18)	/* mips 4000 BE */
-#define	D_MAGIC		_MAGIC(0, 19)	/* amd 29000 (retired) */
-#define	E_MAGIC		_MAGIC(0, 20)	/* arm */
-#define	Q_MAGIC		_MAGIC(0, 21)	/* powerpc */
-#define	N_MAGIC		_MAGIC(0, 22)	/* mips 4000 LE */
-#define	L_MAGIC		_MAGIC(0, 23)	/* dec alpha */
-#define	P_MAGIC		_MAGIC(0, 24)	/* mips 3000 LE */
-#define	U_MAGIC		_MAGIC(0, 25)	/* sparc64 */
-#define	S_MAGIC		_MAGIC(HDR_MAGIC, 26)	/* amd64 */
-#define	T_MAGIC		_MAGIC(HDR_MAGIC, 27)	/* powerpc64 */
+#define	_MAGIC(flags, b) ((flags) | ((((4 * (b)) + 0) * (b)) + 7))
 
-#define TOS_SIZE	14	/* Size of Top of Stack: 56 / 4 */
-#define HDR_SIZE	0x20
-#define STR_ADDR	0x1000	/* Start Address */
-#define TXT_ADDR	HDR_SIZE + ex.text	/* TEXT Address */
-#define DAT_ADDR	STR_ADDR + PAGE_ALIGN(TXT_ADDR)	/* DATA&BSS Address */
+#define	MAGIC_68020 _MAGIC(0, 8)
 
-/*---*/
+#define	MAGIC_INTEL_386 _MAGIC(0, 11)
+#define	MAGIC_AMD64 _MAGIC(HDR_MAGIC, 26)
 
-#define p9bin_open(x) fopen(x,"r")
-#define p9bin_close(x) fclose(x)
+#define	MAGIC_SPARC _MAGIC(0, 13)
+#define	MAGIC_SPARC64 _MAGIC(0, 25)
+
+#define	MAGIC_MIPS_3000BE _MAGIC(0, 16)
+#define	MAGIC_MIPS_4000BE _MAGIC(0, 18)
+#define	MAGIC_MIPS_4000LE _MAGIC(0, 22)
+#define	MAGIC_MIPS_3000LE _MAGIC(0, 24)
+
+#define	MAGIC_ARM _MAGIC(0, 20)
+#define	MAGIC_ARM64 _MAGIC(HDR_MAGIC, 28)
+
+#define	MAGIC_PPC _MAGIC(0, 21)
+#define	MAGIC_PPC64 _MAGIC(HDR_MAGIC, 27)
+
+/* Retired, and subsequently unsupported, architectures. */
+#define	MAGIC_INTEL_960 _MAGIC(0, 12)
+#define	MAGIC_ATT_DSP_3210 _MAGIC(0, 17)
+#define	MAGIC_AMD_29000 _MAGIC(0, 19)
+#define	MAGIC_DEC_ALPHA _MAGIC(0, 23)
 
 /* Reads four bytes from b. */
-int r_bin_p9_get_arch(RBuffer *b, int *bits, int *big_endian);
+bool r_bin_p9_get_arch(R_NONNULL RBuffer *b, R_NONNULL RSysArch *arch, R_NONNULL int *bits, R_NONNULL int *big_endian);
 
 #endif
