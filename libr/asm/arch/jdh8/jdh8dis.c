55412db060e700e7fb653b76ffc0b97f30e2afbc
condret <condr3t@protonmail.com>
2021-11-23 03:51:13 +0100

    Use r_strbuf_setf instead of sdb_fmt in jd8dis.c

--- libr/asm/arch/jdh8/jdh8dis.c
+++ libr/asm/arch/jdh8/jdh8dis.c
@@ -26,115 +26,117 @@ static int jdh8Disass(RAsmOp *op, const ut8 *buf, int len) {
 	int ilen = jdh_get_ins_len (*buf);
 	const ut8 high = (*buf & 0xf0) >> 4;
 	const ut8 low = (*buf & 0xf);
-	const char *buf_asm = "invalid";
 	if (ilen > len) {
 		return op->size = 0;
 	}
 	switch (high) {
 	case 0:
-		buf_asm = (low & 8)? snprintf (buf, sizeof (buf), "mw %c, %c", reg[low & 7], reg[buf[1]]):
-			snprintf (buf, sizeof (buf), "mw %c, 0x%02x", reg[low & 7], buf[1]);
+		if (low & 8) {
+			r_strbuf_setf (&op->buf_asm, "mw %c, %c", reg[low & 7], reg[buf[1]]);
+		} else {
+			r_strbuf_setf (&op->buf_asm, "mw %c, 0x%02x", reg[low & 7], buf[1]);
+		}
 		break;
 	case 1:
 		if (ilen == 2) {
-			buf_asm = snprintf (buf, sizeof (buf), "lw %c, %c", reg[low & 7], reg[buf[1]]);
+			r_strbuf_setf (&op->buf_asm, "lw %c, %c", reg[low & 7], reg[buf[1]]);
 		} else {
-			buf_asm = snprintf (buf, sizeof (buf), "lw %c, 0x%02x", reg[low & 7], buf[1]);
+			r_strbuf_setf (&op->buf_asm, "lw %c, 0x%02x", reg[low & 7], buf[1]);
 		}
 		break;
 	case 2:
 		if (ilen == 2) {
-			buf_asm = snprintf (buf, sizeof (buf), "sw %c, %c", reg[low & 7], reg[buf[1]]);
+			r_strbuf_setf (&op->buf_asm, "sw %c, %c", reg[low & 7], reg[buf[1]]);
 		} else {
-			buf_asm = snprintf (buf, sizeof (buf), "sw 0x%04x, %c", ((ut16)(buf[1] << 8) | buf[2]), reg[low & 7]);
+			r_strbuf_setf (&op->buf_asm, "sw 0x%04x, %c", ((ut16)(buf[1] << 8) | buf[2]), reg[low & 7]);
 		}
 		break;
 	case 3:
 		if (ilen == 1) {
-			buf_asm = snprintf (buf, sizeof (buf), "push %c", reg[low & 7]);
+			r_strbuf_setf (&op->buf_asm, "push %c", reg[low & 7]);
 		} else {
-			buf_asm = snprintf (buf, sizeof (buf), "push 0x%02x", buf[1]);
+			r_strbuf_setf (&op->buf_asm, "push 0x%02x", buf[1]);
 		}
 		break;
 	case 4:
-		buf_asm = snprintf (buf, sizeof (buf), "pop %c", reg[low & 7]);
+		r_strbuf_setf (&op->buf_asm, "pop %c", reg[low & 7]);
 		break;
 	case 5:
-		buf_asm = snprintf (buf, sizeof (buf), "lda 0x%03x", ((ut16)(buf[1] << 8) | buf[2]));
+		r_strbuf_setf (&op->buf_asm, "lda 0x%03x", ((ut16)(buf[1] << 8) | buf[2]));
 		break;
 	case 6:
 		if (ilen == 1) {
-			buf_asm = snprintf (buf, sizeof (buf), "jnz %c", reg[low & 7]);
+			r_strbuf_setf (&op->buf_asm, "jnz %c", reg[low & 7]);
 		} else {
-			buf_asm = snprintf (buf, sizeof (buf), "jnz 0x%02x", buf[1]);
+			r_strbuf_setf (&op->buf_asm, "jnz 0x%02x", buf[1]);
 		}
 		break;
 	case 7:
 		if (low & 8) {
-			buf_asm = snprintf (buf, sizeof (buf), "inb %c, %c", reg[low & 7], reg[buf[1]]);
+			r_strbuf_setf (&op->buf_asm, "inb %c, %c", reg[low & 7], reg[buf[1]]);
 		} else {
-			buf_asm = snprintf (buf, sizeof (buf), "inb %c, 0x%02x", reg[low & 7], buf[1]);
+			r_strbuf_setf (&op->buf_asm, "inb %c, 0x%02x", reg[low & 7], buf[1]);
 		}
 		break;
 	case 8:
 		if (low & 8) {
-			buf_asm = snprintf (buf, sizeof (buf), "outb %c, %c", reg[low & 7], reg[buf[1]]);
+			r_strbuf_setf (&op->buf_asm, "outb %c, %c", reg[low & 7], reg[buf[1]]);
 		} else {
-			buf_asm = snprintf (buf, sizeof (buf), "outb %c, 0x%02x", reg[low & 7], buf[1]);
+			r_strbuf_setf (&op->buf_asm, "outb %c, 0x%02x", reg[low & 7], buf[1]);
 		}
 		break;
 	case 9:
 		if (low & 8) {
-			buf_asm = snprintf (buf, sizeof (buf), "add %c, %c", reg[low & 7], reg[buf[1]]);
+			r_strbuf_setf (&op->buf_asm, "add %c, %c", reg[low & 7], reg[buf[1]]);
 		} else {
-			buf_asm = snprintf (buf, sizeof (buf), "add %c, 0x%02x", reg[low & 7], buf[1]);
+			r_strbuf_setf (&op->buf_asm, "add %c, 0x%02x", reg[low & 7], buf[1]);
 		}
 		break;
 	case 10:
 		if (low & 8) {
-			buf_asm = snprintf (buf, sizeof (buf), "adc %c, %c", reg[low & 7], reg[buf[1]]);
+			r_strbuf_setf (&op->buf_asm, "adc %c, %c", reg[low & 7], reg[buf[1]]);
 		} else {
-			buf_asm = snprintf (buf, sizeof (buf), "adc %c, 0x%02x", reg[low & 7], buf[1]);
+			r_strbuf_setf (&op->buf_asm, "adc %c, 0x%02x", reg[low & 7], buf[1]);
 		}
 		break;
 	case 11:
 		if (low & 8) {
-			buf_asm = snprintf (buf, sizeof (buf), "and %c, %c", reg[low & 7], reg[buf[1]]);
+			r_strbuf_setf (&op->buf_asm, "and %c, %c", reg[low & 7], reg[buf[1]]);
 		} else {
-			buf_asm = snprintf (buf, sizeof (buf), "and %c, 0x%02x", reg[low & 7], buf[1]);
+			r_strbuf_setf (&op->buf_asm, "and %c, 0x%02x", reg[low & 7], buf[1]);
 		}
 		break;
 	case 12:
 		if (low & 8) {
-			buf_asm = snprintf (buf, sizeof (buf), "or %c, %c", reg[low & 7], reg[buf[1]]);
+			r_strbuf_setf (&op->buf_asm, "or %c, %c", reg[low & 7], reg[buf[1]]);
 		} else {
-			buf_asm = snprintf (buf, sizeof (buf), "or %c, 0x%02x", reg[low & 7], buf[1]);
+			r_strbuf_setf (&op->buf_asm, "or %c, 0x%02x", reg[low & 7], buf[1]);
 		}
 		break;
 	case 13:
 		if (low & 8) {
-			buf_asm = snprintf (buf, sizeof (buf), "nor %c, %c", reg[low & 7], reg[buf[1]]);
+			r_strbuf_setf (&op->buf_asm, "nor %c, %c", reg[low & 7], reg[buf[1]]);
 		} else {
-			buf_asm = snprintf (buf, sizeof (buf), "nor %c, 0x%02x", reg[low & 7], buf[1]);
+			r_strbuf_setf (&op->buf_asm, "nor %c, 0x%02x", reg[low & 7], buf[1]);
 		}
 		break;
 	case 14:
 		if (low & 8) {
-			buf_asm = snprintf (buf, sizeof (buf), "cmp %c, %c", reg[low & 7], reg[buf[1]]);
+			r_strbuf_setf (&op->buf_asm, "cmp %c, %c", reg[low & 7], reg[buf[1]]);
 		} else {
-			buf_asm = snprintf (buf, sizeof (buf), "cmp %c, 0x%02x", reg[low & 7], buf[1]);
+			r_strbuf_setf (&op->buf_asm, "cmp %c, 0x%02x", reg[low & 7], buf[1]);
 		}
 		break;
 	case 15:
 		if (low & 8) {
-			buf_asm = snprintf (buf, sizeof (buf), "sbb %c, %c", reg[low & 7], reg[buf[1]]);
+			r_strbuf_setf (&op->buf_asm, "sbb %c, %c", reg[low & 7], reg[buf[1]]);
 		} else {
-			buf_asm = snprintf (buf, sizeof (buf), "sbb %c, 0x%02x", reg[low & 7], buf[1]);
+			r_strbuf_setf (&op->buf_asm, "sbb %c, 0x%02x", reg[low & 7], buf[1]);
 		}
 		break;
 	default:
+		r_strbuf_set (&op->buf_asm, "invalid");
 		break;
 	}
-	r_strbuf_set (&op->buf_asm, buf_asm);
 	return op->size = ilen;
 }
