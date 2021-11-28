a7889a4c5187ac783c2c1bb531f8f7c19aaf06c2
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2021-11-28 21:34:21 +0100

    exit 1 in ming

--- sys/mingw32.sh
+++ sys/mingw32.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 cp -f dist/plugins-cfg/plugins.mingw.cfg plugins.cfg
 export CC=i686-w64-mingw32-gcc
-./configure --with-ostype=windows --with-compiler=i686-w64-mingw32-gcc --prefix=/ --without-libuv
-make -j4
+./configure --with-ostype=windows --with-compiler=i686-w64-mingw32-gcc --prefix=/ --without-libuv || exit 1
+make -j4 || exit 1
 # install
 rm -rf prefix
-make install DESTDIR=$PWD/prefix
+make install DESTDIR=$PWD/prefix || exit 1
 cp -f prefix/lib/*.dll prefix/bin/
 D=radare2-`./configure -qV`-mingw32
 rm -rf $D
 mkdir -p $D
 cp -rf prefix/bin/*.exe $D
 cp -rf prefix/lib/*.dll $D
-zip -r $D.zip $D
+exec zip -r $D.zip $D
--- sys/mingw64.sh
+++ sys/mingw64.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 cp -f dist/plugins-cfg/plugins.mingw.cfg plugins.cfg
 export CC=x86_64-w64-mingw32-gcc
-./configure --with-ostype=windows --with-compiler=x86_64-w64-mingw32-gcc --prefix=/ --without-libuv
-make -j4
+./configure --with-ostype=windows --with-compiler=x86_64-w64-mingw32-gcc --prefix=/ --without-libuv || exit 1
+make -j4 || exit 1
 # install
 rm -rf prefix
-make install DESTDIR=$PWD/prefix
+make install DESTDIR=$PWD/prefix || exit 1
 cp -f prefix/lib/*.dll prefix/bin/
 D=radare2-`./configure -qV`-mingw32
 rm -rf $D
-mkdir -p $D
+mkdir -p $D || exit 1
 cp -rf prefix/bin/*.exe $D
 cp -rf prefix/lib/*.dll $D
-zip -r $D.zip $D
+zip -r $D.zip $D || exit 1
