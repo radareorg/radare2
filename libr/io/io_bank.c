37897226a1a31f982bfefdc4aeefc2e50355c73c
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2022-01-06 19:33:28 +0100

    Fix use-after-free in iobank rbtree usage ##io
    * See havoc4 bin for reproducer
    * Reported via huntr.dev by 'Cen Zhang'

--- libr/io/io_bank.c
+++ libr/io/io_bank.c
@@ -230,7 +230,10 @@ R_API bool r_io_bank_map_add_top(RIO *io, const ut32 bankid, const ut32 mapid) {
 		//delete all submaps that are completly included in sm
 		RRBNode *next = r_rbnode_next (entry);
 		// this can be optimized, there is no need to do search here
-		r_crbtree_delete (bank->submaps, entry->data, _find_sm_by_from_vaddr_cb, NULL);
+		bool a = r_crbtree_delete (bank->submaps, entry->data, _find_sm_by_from_vaddr_cb, NULL);
+		if (!a) {
+			break;
+		}
 		entry = next;
 	}
 	if (entry && r_io_submap_from (((RIOSubMap *)entry->data)) <= r_io_submap_to (sm)) {
