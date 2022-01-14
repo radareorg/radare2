5a024f10febbede7106996c2dfd51a1e4333ddcf
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2022-01-14 12:42:04 +0100

    Fix UBSAN in armass64 when shifting a signed variable
    * runtime error: left shift of 128 by 29 places cannot be represented in type 'int'

--- libr/asm/arch/arm/armass64.c
+++ libr/asm/arch/arm/armass64.c
@@ -894,10 +894,10 @@ static ut32 exception(ArmOp *op, ut32 k) {
 	ut32 data = UT32_MAX;
 
 	if (op->operands[0].type == ARM_CONSTANT) {
-		int n = op->operands[0].immediate;
+		ut32 n = op->operands[0].immediate;
 		data = k;
 		data += (((n / 8) & 0xff) << 16);
-		data += n << 29;//((n >> 8) << 8);
+		data += (n << 29);
 	}
 	return data;
 }
