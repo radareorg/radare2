b8585ad9e8106e794abe0da4fda11e8dc74eee3a
Sylvain Pelissier <sylvain.pelissier@gmail.com>
2022-02-01 17:01:59 +0100

    Improve EVM analysis and update tests ##esil

--- test/db/asm/evm.cs
+++ test/db/asm/evm.cs
@@ -8,8 +8,8 @@ d "callvalue" 34
 d "pop" 50
 d "mstore" 52
 d "jumpi" 57
-d "push1 40" 6040
-d "push4 ffffffff" 63ffffffff
+d "push1 0x40" 6040
+d "push4 0xffffffff" 63ffffffff
 d "dup1" 80
 d "swap1" 90
 d "return" f3
