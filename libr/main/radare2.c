606bdfb89f323b6ce2c21c324c2ee6b67826d14c
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2022-02-12 11:37:43 +0100

    Remove the `file.offset` unused config variable
    * It was set for a while, RBinFile holds this info in loadaddr
    * file.offset was conceptually wrong because its 'global'

--- libr/main/radare2.c
+++ libr/main/radare2.c
@@ -658,7 +658,6 @@ R_API int r_main_radare2(int argc, const char **argv) {
 			r_config_set_i (r->config, "io.va", 1);
 			mapaddr = r_num_math (r->num, opt.arg);
 			s_seek = opt.arg;
-			r_config_set_i (r->config, "file.offset", mapaddr);
 			break;
 		case 'M':
 			r_config_set (r->config, "bin.demangle", "false");
