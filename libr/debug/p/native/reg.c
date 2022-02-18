6e4819b0543947916c21a15050c3d11b0eef6ae1
pkubaj <pkubaj@anongoth.pl>
2022-02-18 18:25:53 +0000

    Add support for powerpc, powerpc64, powerpc64le and riscv64 on FreeBSD ##debug
    * Also correct issue in vmenus.c, where ch is checked later whether it's -1, but on ARM and POWER, char is unsigned.

--- libr/debug/p/native/reg.c
+++ libr/debug/p/native/reg.c
@@ -49,6 +49,12 @@ static char *r_debug_native_reg_profile(RDebug *dbg) {
 #include "reg/kfbsd-x64.h"
 #elif __aarch64__
 #include "reg/kfbsd-arm64.h"
+#elif __powerpc64__
+#include "reg/kfbsd-ppc64.h"
+#elif __powerpc__
+#include "reg/kfbsd-ppc.h"
+#elif __riscv
+#include "reg/kfbsd-riscv64.h"
 #else
 #error "Unsupported BSD architecture"
 #endif
