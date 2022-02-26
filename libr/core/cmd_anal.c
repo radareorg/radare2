877ec5a2e369f2d84d90124a3662db666324ab3c
aandersonl <22428720+AandersonL@users.noreply.github.com>
2022-02-26 15:48:29 -0300

    fix get_config as boolean and use expressions for analyze all functions

--- libr/core/cmd_anal.c
+++ libr/core/cmd_anal.c
@@ -10782,9 +10782,9 @@ static int cmd_anal_all(RCore *core, const char *input) {
 			r_core_task_yield (&core->tasks);
 			
 			// Run afvn in all fcns 
-			if (r_config_get_i (core->config, "anal.vars")) {
+			if (r_config_get_b (core->config, "anal.vars")) {
 				oldstr = r_print_rowlog (core->print, "Analyze all functions arguments/locals");
-				r_core_cmd0 (core, "afva @@ fcn.*");
+				r_core_cmd0 (core, "afva @@ f");
 				r_print_rowlog_done (core->print, oldstr);
 			}
 
