2bcfbd6aef5174c1f1a530c2c448d5821ace63be
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2022-03-26 11:23:05 +0100

    Fix help message for pxd

--- libr/core/cmd_print.c
+++ libr/core/cmd_print.c
@@ -180,10 +180,11 @@ static const char *help_msg_p[] = {
 
 static const char *help_msg_pxd[] = {
 	"Usage:", "pxd[1248] ([len])", "show decimal byte/short/word/dword dumps",
-	"pxd", "", "show decimal hexdumps",
-	"pxd2", "", "show shorts hexdump",
-	"pxd4", "", "show dword hexdump (int)",
-	"pxd8", "", "show qword hexdump (int)",
+	"pxd", "", "show base10 signed decimal hexdumps",
+	"pxd1", "", "show byte hexdump (int8_t)",
+	"pxd2", "", "show short hexdump (int16_t)",
+	"pxd4", "", "show dword hexdump (int32_t)",
+	"pxd8", "", "show qword hexdump (int64_t)",
 	NULL
 };
 
