e4c53e5ae51387abdb21308d48d67b55b7545d8b
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2022-04-01 19:56:37 +0200

    use strstr instead of rstr.glob for now in @@

--- libr/core/cmd.c
+++ libr/core/cmd.c
@@ -360,7 +360,8 @@ struct duplicate_flag_t {
 static bool duplicate_flag(RFlagItem *flag, void *u) {
 	struct duplicate_flag_t *user = (struct duplicate_flag_t *)u;
 	/* filter per flag spaces */
-	if (r_str_glob (flag->name, user->word)) {
+	// if (r_str_glob (flag->name, user->word)) {
+	if (strstr (flag->name, user->word)) {
 		RFlagItem *cloned_item = r_flag_item_clone (flag);
 		if (!cloned_item) {
 			return false;
