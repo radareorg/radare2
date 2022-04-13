50acd6521907888d15c4c6ab334a8e9383946120
rhl120 <rhl120@protonmail.com>
2022-04-13 11:21:26 +0200

    Fix a styling issue

--- libr/core/project.c
+++ libr/core/project.c
@@ -87,7 +87,7 @@ R_API bool r_core_is_project(RCore *core, const char *name) {
 }
 
 R_API void r_core_project_cat(RCore *core, const char *name) {
-				r_core_return_code (core, R_CMD_RC_FAILURE);
+	r_core_return_code (core, R_CMD_RC_FAILURE);
 	char *path = get_project_script_path (core, name);
 	if (path) {
 		char *data = r_file_slurp (path, NULL);
