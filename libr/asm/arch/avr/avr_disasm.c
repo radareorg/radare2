0da98904c1a396e68b234bcf82ae806bf14e9b97
pancake <pancake@nopcode.org>
2022-05-01 21:33:21 +0200

    Fix null derefs in anal.avr plugin and improve defaults ##anal

--- libr/asm/arch/avr/avr_disasm.c
+++ libr/asm/arch/avr/avr_disasm.c
@@ -38,106 +38,6 @@
 static int disassembleOperands(disassembledInstruction *dInstruction);
 /* Extracts certain bits of data from a mask, used to extract operands from their encoding in the opcode. */
 static uint16_t extractDataFromMask(uint16_t data, uint16_t mask);
-/* Look up an instruction by it's opcode in the instructionSet,
- * starting from index offset. Always returns a valid instruction
- * index because the last instruction in the instruction set database
- * is set to be a generic data word (.DW). */
-static int lookupInstruction(uint16_t opcode, int offset);
-
-
-/* Disassembles an assembled instruction, including its operands. */
-int disassembleInstruction(avrDisassembleContext *context, disassembledInstruction *dInstruction, const assembledInstruction aInstruction) {
-	int insidx, i;
-	
-	if (!dInstruction || !context)
-		return ERROR_INVALID_ARGUMENTS;
-	
-	
-	/* Look up the instruction */
-	insidx = lookupInstruction(aInstruction.opcode, 0);
-	if (insidx == AVR_TOTAL_INSTRUCTIONS) {
-		// invalid instruction
-		return 0;
-	}
-
-	/*** AVR SPECIFIC */
-	/* If a long instruction was found in the last instruction disassembly,
-	 * extract the rest of the address, and indicate that it is to be printed */
-	if (context->status == AVR_LONG_INSTRUCTION_FOUND) {
-		context->status = AVR_LONG_INSTRUCTION_PRINT;
-		context->longAddress |= aInstruction.opcode;
-		/* We must multiply by two, because every instruction is 2 bytes,
-		 * so in order to jump/call to the right address (which increments by
-		 * two for every instruction), we must multiply this distance by two. */
-		//printf ("ii=%d\n", insidx);
-        if(!strcmp(context->longInstruction.instruction->mnemonic, "call")||
-           !strcmp(context->longInstruction.instruction->mnemonic, "jmp")) {
-			context->longAddress *= 2;
-        }
-		*dInstruction = context->longInstruction;
-		return 0;
-	/* If a long instruction was printed in the last instruction disassembly,
-	 * reset the AVR_Call_Instruction variable back to zero. */
-	} else if (context->status == AVR_LONG_INSTRUCTION_PRINT) {
-		context->status = 0;
-	}
-
-	/* Copy over the address, and reference to the instruction, set
-	 * the equivilant-encoded but different instruction to NULL for now. */
-	dInstruction->address = aInstruction.address;
-	dInstruction->instruction = &instructionSet[insidx];
-	dInstruction->alternateInstruction = NULL;
-	
-	/* Copy out each operand, extracting the operand data from the original
-	 * opcode using the operand mask. */
-	for (i = 0; i < instructionSet[insidx].numOperands; i++) {
-		dInstruction->operands[i] = extractDataFromMask(aInstruction.opcode, dInstruction->instruction->operandMasks[i]);
-		/*** AVR SPECIFIC */
-		/* If this is an instruction with a long absolute operand, indicate that a long instruction has been found,
-		 * and extract the first part of the long address. */
-		if (dInstruction->instruction->operandTypes[i] == OPERAND_LONG_ABSOLUTE_ADDRESS) {
-			context->status = AVR_LONG_INSTRUCTION_FOUND;
-			context->longAddress = dInstruction->operands[i] << 16;
-			context->longInstruction = *dInstruction;
-		}
-	}
-	
-	/* Disassemble operands */
-	if (disassembleOperands(dInstruction) < 0)
-		return ERROR_INVALID_ARGUMENTS; /* Only possible error for disassembleOperands() */
-
-	if (context->status == AVR_LONG_INSTRUCTION_FOUND) {
-		/* If we found a long instruction (32-bit one),
-		 * Copy this instruction over to our special longInstruction variable, that
-		 * will exist even after we move onto the next 16-bits */
-		context->longInstruction = *dInstruction;
-	}
-
-	return 0;
-}
-
-/* Extracts certain bits of data from a mask, used to extract operands from their encoding in the opcode. */
-static uint16_t extractDataFromMask(uint16_t data, uint16_t mask) {
-	int i, j;
-	uint16_t result = 0;
-	
-	/* i counts through every bit of the data,
-	 * j counts through every bit of the data we're copying out. */
-	for (i = 0, j = 0; i < 16; i++) {
-		/* If the mask has a bit in this position */
-		if (mask & (1<<i)) {
-			/* If there is a data bit with this mask bit,
-			 * then toggle that bit in the extracted data (result).
-			 * Notice that it uses its own bit counter j. */
-			if (((mask & (1<<i)) & data) != 0)
-				result |= (1<<j);
-			/* Increment the extracted data bit count. */
-			j++;
-		}
-	}
-	
-	return result;
-}
 
 /* Look up an instruction by it's opcode in the instructionSet,
  * starting from index offset. Always returns a valid instruction
@@ -173,11 +73,12 @@ static int lookupInstruction(uint16_t opcode, int offset) {
 		/* If we encountered a ghost register and were unable confirm that
 		 * all register operands were equal (in this case ghostRegisterConfirmed
 		 * would have changed), then move the match-search onto the next instruction. */
-		if (ghostRegisterConfirmed == 0)
+		if (ghostRegisterConfirmed == 0) {
 			continue;
-
-		if (opcodeSearch == instructionSet[insidx].opcodeMask)
+		}
+		if (opcodeSearch == instructionSet[insidx].opcodeMask) {
 			break;
+		}
 	}
 	/* It's impossible not to find an instruction, because the last instruction ".DW",
 	 * specifies a word of data at the addresses, instead of an instruction.
@@ -186,16 +87,108 @@ static int lookupInstruction(uint16_t opcode, int offset) {
 	return insidx;
 }
 
+
+/* Disassembles an assembled instruction, including its operands. */
+int disassembleInstruction(avrDisassembleContext *context, disassembledInstruction *dInstruction, const assembledInstruction aInstruction) {
+	int insidx, i;
+	if (!dInstruction || !context) {
+		return ERROR_INVALID_ARGUMENTS;
+	}
+	/* Look up the instruction */
+	insidx = lookupInstruction (aInstruction.opcode, 0);
+	if (insidx == AVR_TOTAL_INSTRUCTIONS) {
+		// invalid instruction
+		return 0;
+	}
+
+	/*** AVR SPECIFIC */
+	/* If a long instruction was found in the last instruction disassembly,
+	 * extract the rest of the address, and indicate that it is to be printed */
+	if (context->status == AVR_LONG_INSTRUCTION_FOUND) {
+		context->status = AVR_LONG_INSTRUCTION_PRINT;
+		context->longAddress |= aInstruction.opcode;
+		/* We must multiply by two, because every instruction is 2 bytes,
+		 * so in order to jump/call to the right address (which increments by
+		 * two for every instruction), we must multiply this distance by two. */
+		//printf ("ii=%d\n", insidx);
+		if(!strcmp(context->longInstruction.instruction->mnemonic, "call") ||
+		   !strcmp(context->longInstruction.instruction->mnemonic, "jmp")) {
+				context->longAddress *= 2;
+		}
+		*dInstruction = context->longInstruction;
+		return 0;
+	/* If a long instruction was printed in the last instruction disassembly,
+	 * reset the AVR_Call_Instruction variable back to zero. */
+	} else if (context->status == AVR_LONG_INSTRUCTION_PRINT) {
+		context->status = 0;
+	}
+
+	/* Copy over the address, and reference to the instruction, set
+	 * the equivilant-encoded but different instruction to NULL for now. */
+	dInstruction->address = aInstruction.address;
+	dInstruction->instruction = &instructionSet[insidx];
+	dInstruction->alternateInstruction = NULL;
+	
+	/* Copy out each operand, extracting the operand data from the original
+	 * opcode using the operand mask. */
+	for (i = 0; i < instructionSet[insidx].numOperands; i++) {
+		dInstruction->operands[i] = extractDataFromMask(aInstruction.opcode, dInstruction->instruction->operandMasks[i]);
+		/*** AVR SPECIFIC */
+		/* If this is an instruction with a long absolute operand, indicate that a long instruction has been found,
+		 * and extract the first part of the long address. */
+		if (dInstruction->instruction->operandTypes[i] == OPERAND_LONG_ABSOLUTE_ADDRESS) {
+			context->status = AVR_LONG_INSTRUCTION_FOUND;
+			context->longAddress = dInstruction->operands[i] << 16;
+			context->longInstruction = *dInstruction;
+		}
+	}
+	
+	/* Disassemble operands */
+	if (disassembleOperands(dInstruction) < 0) {
+		return ERROR_INVALID_ARGUMENTS; /* Only possible error for disassembleOperands() */
+	}
+
+	if (context->status == AVR_LONG_INSTRUCTION_FOUND) {
+		/* If we found a long instruction (32-bit one),
+		 * Copy this instruction over to our special longInstruction variable, that
+		 * will exist even after we move onto the next 16-bits */
+		context->longInstruction = *dInstruction;
+	}
+
+	return 0;
+}
+
+/* Extracts certain bits of data from a mask, used to extract operands from their encoding in the opcode. */
+static uint16_t extractDataFromMask(uint16_t data, uint16_t mask) {
+	int i, j;
+	uint16_t result = 0;
+	
+	/* i counts through every bit of the data,
+	 * j counts through every bit of the data we're copying out. */
+	for (i = 0, j = 0; i < 16; i++) {
+		/* If the mask has a bit in this position */
+		if (mask & (1<<i)) {
+			/* If there is a data bit with this mask bit,
+			 * then toggle that bit in the extracted data (result).
+			 * Notice that it uses its own bit counter j. */
+			if (((mask & (1<<i)) & data) != 0)
+				result |= (1<<j);
+			/* Increment the extracted data bit count. */
+			j++;
+		}
+	}
+	
+	return result;
+}
+
 /* Disassembles/decodes operands back to their original form. */
 static int disassembleOperands(disassembledInstruction *dInstruction) {
 	int i;
 	
 	/* This should never happen */
-	if (!dInstruction)
+	if (!dInstruction || !dInstruction->instruction) {
 		return ERROR_INVALID_ARGUMENTS;
-	if (!dInstruction->instruction)
-		return ERROR_INVALID_ARGUMENTS;
-	
+	}
 	/* For each operand, decode its original value. */
 	for (i = 0; i < dInstruction->instruction->numOperands; i++) {
 		switch (dInstruction->instruction->operandTypes[i]) {
