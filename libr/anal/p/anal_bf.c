fd6f81602b819fcd68f673755b7338e658c02f7f
pancake <pancake@nopcode.org>
2022-05-08 11:01:49 +0200

    Remove asm.bf, and move its .opasm to the anal.bf ##anal

--- libr/anal/p/anal_bf.c
+++ libr/anal/p/anal_bf.c
@@ -1,4 +1,4 @@
-/* radare2 - LGPL - Copyright 2011-2016 - pancake */
+/* radare2 - LGPL - Copyright 2011-2022 - pancake */
 
 #include <string.h>
 #include <r_types.h>
@@ -22,14 +22,127 @@ static int getid(char ch) {
 	return cidx? cidx - keys + 1: 0;
 }
 
+static int disassemble(RAnalOp *op, const ut8 *buf, int len) {
+	const ut8 *b;
+	size_t rep = 1;
+
+	/* Count repetitions of the current instruction, unless it's a trap. */
+	if (*buf != 0x00 && *buf != 0xff) {
+		for (b = &buf[1]; b < buf + len && *b == *buf; b++) {
+			rep++;
+		}
+	}
+	const char *buf_asm = "invalid";
+	switch (*buf) {
+	case '[':
+		buf_asm = "while [ptr]";
+		break;
+	case ']':
+		buf_asm = "loop";
+		break;
+	case '>':
+		buf_asm = (rep > 1)? "add ptr": "inc ptr";
+		break;
+	case '<':
+		buf_asm = (rep > 1)? "sub ptr": "dec ptr";
+		break;
+	case '+':
+		buf_asm = (rep > 1)? "add [ptr]": "inc [ptr]";
+		break;
+	case '-':
+		buf_asm = (rep > 1)? "sub [ptr]": "dec [ptr]";
+		break;
+	case ',':
+		buf_asm = "in [ptr]";
+		break;
+	case '.':
+		buf_asm = "out [ptr]";
+		break;
+	case 0xff:
+	case 0x00:
+		buf_asm = "trap";
+		break;
+	default:
+		buf_asm = "nop";
+		break;
+	}
+
+	char buf_asm_local[256];
+	if (rep > 1) {
+		/* Note: snprintf's source and destination buffers may not overlap. */
+		const char *fmt = strchr (buf_asm, ' ')? "%s, %d": "%s %d";
+		snprintf (buf_asm_local, sizeof (buf_asm_local), fmt, buf_asm, rep);
+		buf_asm = buf_asm_local;
+	}
+	op->mnemonic = strdup (buf_asm);
+	op->size = rep;
+	return rep;
+}
+
+static bool _write_asm(ut8 *outbuf, int outbufsz, int value, int n) {
+	memset (outbuf, value, R_MIN (n, outbufsz));
+	return n > outbufsz;
+}
+
+static int assemble(const char *buf, ut8 *outbuf, int outbufsz) {
+	int n = 0;
+	if (buf[0] && buf[1] == ' ') {
+		buf += 2;
+	}
+	const char *arg = strchr (buf, ',');
+	const char *ref = strchr (buf, '[');
+	bool write_err = false;
+	if (arg) {
+		n = atoi (arg + 1);
+	} else {
+		n = 1;
+	}
+	if (!strncmp (buf, "trap", 4)) {
+		write_err = _write_asm (outbuf, outbufsz, 0xcc, n);
+	} else if (!strncmp (buf, "nop", 3)) {
+		write_err = _write_asm (outbuf, outbufsz, 0x90, n);
+	} else if (!strncmp (buf, "inc", 3)) {
+		char ch = ref? '+': '>';
+		n = 1;
+		write_err = _write_asm (outbuf, outbufsz, ch, n);
+	} else if (!strncmp (buf, "dec", 3)) {
+		char ch = ref? '-': '<';
+		n = 1;
+		write_err = _write_asm (outbuf, outbufsz, ch, n);
+	} else if (!strncmp (buf, "sub", 3)) {
+		char ch = ref? '-': '<';
+		write_err = _write_asm (outbuf, outbufsz, ch, n);
+	} else if (!strncmp (buf, "add", 3)) {
+		char ch = ref? '+': '>';
+		write_err = _write_asm (outbuf, outbufsz, ch, n);
+	} else if (!strncmp (buf, "while", 5)) {
+		n = 1;
+		write_err = _write_asm (outbuf, outbufsz, '[', 1);
+	} else if (!strncmp (buf, "loop", 4)) {
+		n = 1;
+		write_err = _write_asm (outbuf, outbufsz, ']', 1);
+	} else if (!strncmp (buf, "in", 2)) {
+		write_err = _write_asm (outbuf, outbufsz, ',', n);
+	} else if (!strncmp (buf, "out", 3)) {
+		write_err = _write_asm (outbuf, outbufsz, '.', n);
+	} else {
+		n = 0;
+	}
+	if (write_err) {
+		return 0;
+	}
+	return n;
+}
+
 #define BUFSIZE_INC 32
 static int bf_op(RAnal *anal, RAnalOp *op, ut64 addr, const ut8 *buf, int len, RAnalOpMask mask) {
 	ut64 dst = 0LL;
 	if (!op) {
 		return 1;
 	}
-	/* Ayeeee! What's inside op? Do we have an initialized RAnalOp? Are we going to have a leak here? :-( */
-	memset (op, 0, sizeof (RAnalOp)); /* We need to refactorize this. Something like r_anal_op_init would be more appropriate */
+	if (mask & R_ANAL_OP_MASK_DISASM) {
+		(void) disassemble (op, buf, len);
+	}
 	r_strbuf_init (&op->esil);
 	op->size = 1;
 	op->id = getid (buf[0]);
@@ -145,14 +258,20 @@ static char *get_reg_profile(RAnal *anal) {
 	);
 }
 
+static int bf_opasm(RAnal *a, ut64 addr, const char *str, ut8 *outbuf, int outsize) {
+	return assemble (str, outbuf, outsize);
+}
+
 RAnalPlugin r_anal_plugin_bf = {
 	.name = "bf",
 	.desc = "brainfuck code analysis plugin",
 	.license = "LGPL3",
 	.arch = "bf",
 	.bits = 8,
+	.endian = R_SYS_ENDIAN_NONE,
 	.esil = true,
 	.op = &bf_op,
+	.opasm = &bf_opasm,
 	.get_reg_profile = get_reg_profile,
 };
 
