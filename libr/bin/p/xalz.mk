7fff6fc42de38de0d21e81ee9a9044046331c2d4
pancake <pancake@nopcode.org>
2022-05-16 12:01:12 +0200

    Remove the bin.xalz plugin as its meant to be io or bin.xtr ##bin

--- libr/bin/p/xalz.mk
+++
@@ -1,9 +0,0 @@
-OBJ_XALZ=bin_xalz.o
-
-STATIC_OBJ+=${OBJ_XALZ}
-TARGET_XALZ=bin_xalz.${EXT_SO}
-
-ALL_TARGETS+=${TARGET_XALZ}
-
-${TARGET_XALZ}: ${OBJ_XALZ}
-	-${CC} $(call libname,bin_xalz) ${CFLAGS} ${OBJ_XALZ}
