c6e32b35d4a8a82eff50d16cd86f63f6efab493b
pancake <pancake@nopcode.org>
2022-05-17 18:08:17 +0200

    Fix memleaks in bin.xtr.xalz

--- libr/bin/p/bin_xtr_xalz.c
+++ libr/bin/p/bin_xtr_xalz.c
@@ -25,6 +25,7 @@ static RBinXtrData *get_the_meta(RBin *bin, RBuffer *buf) {
 	int datalen = r_buf_size (buf) - 0xc;
 	ut8 *data = malloc (datalen);
 	if (!data) {
+		free (meta);
 		return NULL;
 	}
 	r_buf_read_at (buf, 0xc, data, datalen);
@@ -34,9 +35,13 @@ static RBinXtrData *get_the_meta(RBin *bin, RBuffer *buf) {
 	if (obuf && outsize == osz) {
 		buf = r_buf_new_with_pointers (obuf, outsize, true);
 		RBinXtrData *res = r_bin_xtrdata_new (buf, 0, r_buf_size (buf), 0, meta);
+		free (data);
 		return res;
 	}
 	R_LOG_ERROR ("LZ4 decompression failed");
+	free (data);
+	free (meta);
+	free (obuf);
 	return NULL;
 }
 
