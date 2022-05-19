25b9c447a049d82cad9e5655b02631565d41a6de
Dennis Goodlett <dennis+git_swoops@hurricanelabs.com>
2022-05-19 04:12:30 -0400

    Fix wasm section parsing ##bin
    * Rename wasm var max to bound
    * Fix bug in consume_str_r
    * Add consume_str_new helper function
    * Allow imports with arbitrarily long field names
    * Allow exports with arbitrary length fields
    * Fix leak in wasm sections
    * Update formating of wasm format
    
    Co-authored-by: Dennis Goodlett <dennis@hurricanelabs.com>

--- libr/bin/format/wasm/wasm.h
+++ libr/bin/format/wasm/wasm.h
@@ -107,9 +107,9 @@ struct r_bin_wasm_memory_type_t {
 
 typedef struct r_bin_wasm_import_t {
 	ut32 module_len;
-	char module_str[R_BIN_WASM_STRING_LENGTH];
+	char *module_str;
 	ut32 field_len;
-	char field_str[R_BIN_WASM_STRING_LENGTH];
+	char *field_str;
 	ut8 kind;
 	union {
 		ut32 type_f;
@@ -141,7 +141,7 @@ typedef struct r_bin_wasm_global_t {
 
 typedef struct r_bin_wasm_export_t {
 	ut32 field_len;
-	char field_str[R_BIN_WASM_STRING_LENGTH];
+	char *field_str;
 	ut8 kind;
 	ut32 index;
 } RBinWasmExportEntry;
