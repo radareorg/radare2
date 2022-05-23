6bf4f8db06bcf4d6dd8c1198c4d8708ce49e33aa
Dennis Goodlett <dennis@hurricanelabs.com>
2022-05-23 04:13:45 -0400

    Fix off-by-one bound check in wasm format ##bin

--- libr/bin/format/wasm/wasm.c
+++ libr/bin/format/wasm/wasm.c
@@ -76,7 +76,7 @@ static bool consume_str_r(RBuffer *b, ut64 bound, size_t len, char *out) {
 	r_return_val_if_fail (b && bound > 0 && bound < r_buf_size (b), 0);
 	*out = 0;
 
-	if (r_buf_tell (b) + len <= bound) {
+	if (r_buf_tell (b) + len <= bound + 1) {
 		if (r_buf_read (b, (ut8 *)out, len) == len) {
 			out[len] = 0;
 			return true;
