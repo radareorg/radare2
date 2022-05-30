4a7b17f828261558bff32975d76ed5c8438e790a
pancake <pancake@nowsecure.com>
2022-05-30 14:24:06 +0200

    Move the asm.ppc.gnu into the anal ##asm

--- libr/include/r_asm.h
+++ libr/include/r_asm.h
@@ -219,7 +219,6 @@ extern RAsmPlugin r_asm_plugin_or1k;
 extern RAsmPlugin r_asm_plugin_pic;
 extern RAsmPlugin r_asm_plugin_ppc_as;
 extern RAsmPlugin r_asm_plugin_ppc_cs;
-extern RAsmPlugin r_asm_plugin_ppc_gnu;
 extern RAsmPlugin r_asm_plugin_propeller;
 extern RAsmPlugin r_asm_plugin_riscv;
 extern RAsmPlugin r_asm_plugin_riscv_cs;
