ffaf2db97045a1b8640078f74755ee2f59991be5
pancake <pancake@nopcode.org>
2022-06-09 21:05:23 +0200

    vmode fixes visual shift issue in cmd.exe ##windows

--- libr/core/visual.c
+++ libr/core/visual.c
@@ -773,7 +773,7 @@ R_API void r_core_visual_prompt_input(RCore *core) {
 	r_cons_reset_colors ();
 	//r_cons_printf ("\nPress <enter> to return to Visual mode.\n");
 	r_cons_show_cursor (true);
-	core->vmode = false;
+	core->vmode = 0;
 
 	int curbs = core->blocksize;
 	if (autoblocksize) {
@@ -790,7 +790,7 @@ R_API void r_core_visual_prompt_input(RCore *core) {
 	}
 
 	r_cons_show_cursor (false);
-	core->vmode = true;
+	core->vmode = 1;
 	r_cons_enable_mouse (mouse_state && r_config_get_i (core->config, "scr.wheel"));
 	r_cons_show_cursor (true);
 }
@@ -4398,7 +4398,7 @@ R_API int r_core_visual(RCore *core, const char *input) {
 		// show V? help message, disables oneliner to open visual help
 		return 0;
 	}
-	core->vmode = false;
+	core->vmode = 0;
 	/* honor vim */
 	if (!strncmp (input, "im", 2)) {
 		char *cmd = r_str_newf ("!v%s", input);
@@ -4413,8 +4413,10 @@ R_API int r_core_visual(RCore *core, const char *input) {
 		}
 		input += len;
 	}
-	core->vmode = true;
 
+	int vtmode = r_config_get_i (core->config, "scr.vtmode");
+	r_config_set_i (core->config, "scr.vtmode", core->vmode);
+	core->vmode = 2;
 	// disable tee in cons
 	teefile = r_cons_singleton ()->teefile;
 	r_cons_singleton ()->teefile = "";
@@ -4549,10 +4551,11 @@ dodo:
 	r_cons_singleton ()->teefile = teefile;
 	r_cons_set_cup (false);
 	r_cons_clear00 ();
-	core->vmode = false;
+	core->vmode = 0;
 	core->cons->event_resize = NULL;
 	core->cons->event_data = NULL;
 	r_cons_show_cursor (true);
+	r_config_set_i (core->config, "scr.vtmode", vtmode);
 	return 0;
 }
 
