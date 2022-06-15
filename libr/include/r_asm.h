b90f5c8e95de25b0243c04212290e77273488604
Richard Patel <me@terorie.dev>
2022-06-15 21:40:53 +0100

    Merge asm.mips into anal.mips ##refactor

--- libr/include/r_asm.h
+++ libr/include/r_asm.h
@@ -203,7 +203,6 @@ extern RAsmPlugin r_asm_plugin_java;
 extern RAsmPlugin r_asm_plugin_lanai_gnu;
 extern RAsmPlugin r_asm_plugin_m680x_cs;
 extern RAsmPlugin r_asm_plugin_mcs96;
-extern RAsmPlugin r_asm_plugin_mips_cs;
 extern RAsmPlugin r_asm_plugin_mips_gnu;
 extern RAsmPlugin r_asm_plugin_nios2;
 extern RAsmPlugin r_asm_plugin_or1k;
