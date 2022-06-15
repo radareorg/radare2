00bf3dd91ebe3ce6893529e79e6c574ffa4a180f
Richard Patel <me@terorie.dev>
2022-06-15 23:28:56 +0100

    Merge asm.pdp11 into anal.pdp11 ##refactor

--- libr/anal/p/anal_pdp11_gnu.c
+++ libr/anal/p/anal_pdp11_gnu.c
@@ -33,20 +33,15 @@ static void memory_error_func(int status, bfd_vma memaddr, struct disassemble_in
 DECLARE_GENERIC_PRINT_ADDRESS_FUNC()
 DECLARE_GENERIC_FPRINTF_FUNC()
 
-static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
-	char options[64];
-	struct disassemble_info disasm_obj;
-	if (len < 4) {
-		return -1;
+static int pdp11_op(RAnal *a, RAnalOp *op, ut64 addr, const ut8 *buf, int len, RAnalOpMask mask) {
+	struct disassemble_info disasm_obj = {0};
+	if (mask & R_ANAL_OP_MASK_DISASM) {
+		buf_global = r_strbuf_new (NULL);
 	}
-	buf_global = &op->buf_asm;
-	Offset = a->pc;
-	memcpy (bytes, buf, 4); // TODO handle thumb
+	Offset = addr;
+	memcpy (bytes, buf, R_MIN (sizeof (bytes), len)); // TODO handle thumb
 
 	/* prepare disassembler */
-	memset (&disasm_obj, '\0', sizeof (struct disassemble_info));
-	*options = 0;
-	disasm_obj.disassembler_options = options;
 	disasm_obj.buffer = bytes;
 	disasm_obj.read_memory_func = &pdp11_buffer_read_memory;
 	disasm_obj.symbol_at_address_func = &symbol_at_address;
@@ -56,10 +51,21 @@ static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
 	disasm_obj.fprintf_func = &generic_fprintf_func;
 	disasm_obj.stream = stdout;
 	op->size = print_insn_pdp11 ((bfd_vma)Offset, &disasm_obj);
+
+	if (mask & R_ANAL_OP_MASK_DISASM) {
+		op->mnemonic = r_strbuf_drain (buf_global);
+		for (char *c = op->mnemonic; *c != 0; c++) {
+			if (*c == '\t') {
+				*c = ' ';
+			}
+		}
+		buf_global = NULL;
+	}
+
 	return op->size;
 }
 
-RAsmPlugin r_asm_plugin_pdp11_gnu = {
+RAnalPlugin r_anal_plugin_pdp11_gnu = {
 	.name = "pdp11",
 	.arch = "pdp11",
 	.license = "GPL3",
@@ -67,13 +73,13 @@ RAsmPlugin r_asm_plugin_pdp11_gnu = {
 	.bits = 16,
 	.endian = R_SYS_ENDIAN_LITTLE | R_SYS_ENDIAN_BIG,
 	.desc = "PDP-11",
-	.disassemble = &disassemble
+	.op = &pdp11_op
 };
 
 #ifndef R2_PLUGIN_INCORE
 R_API RLibStruct radare_plugin = {
-	.type = R_LIB_TYPE_ASM,
-	.data = &r_asm_plugin_pdp11_gnu,
+	.type = R_LIB_TYPE_ANAL,
+	.data = &r_anal_plugin_pdp11_gnu,
 	.version = R2_VERSION
 };
 #endif
