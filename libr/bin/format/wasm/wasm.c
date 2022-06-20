b4ca66f5d4363d68a6379e5706353b3bde5104a4
pancake <pancake@nopcode.org>
2022-06-21 00:49:41 +0200

    Fix #20336 - wasm bin parser ##crash

--- libr/bin/format/wasm/wasm.c
+++ libr/bin/format/wasm/wasm.c
@@ -137,14 +137,19 @@ static bool consume_encoded_name_new(RBuffer *b, ut64 bound, ut32 *len_out, char
 	}
 
 	size_t i, oi = 0;
-	for (i = 0; i < len && oi + 4 < maxsize; i++) {
+	for (i = 0; i < len && oi + 6 < maxsize; i++) {
 		if (WASM_IS_OK (orig, i, len)) {
 			sout[oi++] = orig[i];
 		} else {
-			oi += snprintf (sout + oi, maxsize - oi, "_%02x_", orig[i]);
+			int res = snprintf (sout + oi, maxsize - oi, "_%02x_", orig[i]);
+			oi += res;
 		}
 	}
-	sout[oi++] = '\0';
+	if (oi >= maxsize) {
+		sout[maxsize - 1] = '\0';
+	} else {
+		sout[oi++] = '\0';
+	}
 	free (orig);
 
 	char *tmp = realloc (sout, oi);
