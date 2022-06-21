0422f04c91cdaa3044e91b5d4780f12e3a43114f
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2022-06-21 12:05:53 +0200

    werror dislikes

--- libr/include/r_util/r_str.h
+++ libr/include/r_util/r_str.h
@@ -235,7 +235,7 @@ R_API bool r_str_glob(const char *str, const char *glob);
 R_API int r_str_binstr2bin(const char *str, ut8 *out, int outlen);
 R_API char *r_str_between(const char *str, const char *prefix, const char *suffix);
 R_API bool r_str_startswith(const char *str, const char *needle);
-#define r_str_startswith(x,y) ((x&&y)&&(x==y||!strncmp (x,y,strlen(y))))
+#define r_str_startswith(x,y) (((x)&&(y))&&(!strncmp ((x),(y),strlen((y)))))
 R_API bool r_str_endswith(const char *str, const char *needle);
 R_API bool r_str_isnumber(const char *str);
 R_API const char *r_str_last(const char *in, const char *ch);
