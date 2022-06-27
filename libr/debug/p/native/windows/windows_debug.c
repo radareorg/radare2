484fc65b8379017d1bdfa65f1ea9a9c797167108
Richard Patel <me@terorie.dev>
2022-06-27 15:22:25 +0200

    fixes

--- libr/debug/p/native/windows/windows_debug.c
+++ libr/debug/p/native/windows/windows_debug.c
@@ -242,8 +242,8 @@ static void __printwincontext(HANDLE th, CONTEXT *ctx) {
 #else
 	eprintf ("ControlWord   = %08x StatusWord   = %08x\n", (ut32)ctx->FloatSave.ControlWord, (ut32)ctx->FloatSave.StatusWord);
 	eprintf ("MxCsr         = %08x TagWord      = %08x\n", *(ut32 *)&ctx->ExtendedRegisters[24], (ut32)ctx->FloatSave.TagWord);
-	R_LOG_ERROR ("ErrorOffset   = %08x DataOffset   = %08x", (ut32)ctx->FloatSave.ErrorOffset, (ut32)ctx->FloatSave.DataOffset);
-	R_LOG_ERROR ("ErrorSelector = %08x DataSelector = %08x", (ut32)ctx->FloatSave.ErrorSelector, (ut32)ctx->FloatSave.DataSelector);
+	eprintf ("ErrorOffset   = %08x DataOffset   = %08x\n", (ut32)ctx->FloatSave.ErrorOffset, (ut32)ctx->FloatSave.DataOffset);
+	eprintf ("ErrorSelector = %08x DataSelector = %08x\n", (ut32)ctx->FloatSave.ErrorSelector, (ut32)ctx->FloatSave.DataSelector);
 	for (x = 0; x < 8; x++) {
 		st[x].High = (ut16) *((ut16 *)(&ctx->FloatSave.RegisterArea[x * 10] + 8));
 		st[x].Low = (ut64) *((ut64 *)&ctx->FloatSave.RegisterArea[x * 10]);
