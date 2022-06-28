e5783272d6317b9d3dc7b30784dc6edaf3fe36f2
pancake <pancake@nopcode.org>
2022-06-28 11:19:47 +0200

    Merge asm.cris into anal.cris ##refactor

--- libr/anal/p/cris.mk
+++ libr/anal/p/cris.mk
@@ -1,10 +1,11 @@
 OBJ_CRIS=anal_cris.o
+OBJ_CRIS+=../../asm/arch/cris/gnu/cris-dis.o
+OBJ_CRIS+=../../asm/arch/cris/gnu/cris-opc.o
 
 STATIC_OBJ+=$(OBJ_CRIS)
-TARGET_CRIS=anal_cris.${EXT_SO}
+TARGET_CRIS=anal_cris.$(EXT_SO)
 
-ALL_TARGETS+=${TARGET_CRIS}
+ALL_TARGETS+=$(TARGET_CRIS)
 
-${TARGET_CRIS}: ${OBJ_CRIS}
-	${CC} ${CFLAGS} $(call libname,anal_cris) \
-		-o anal_cris.${EXT_SO} ${OBJ_CRIS}
+$(TARGET_CRIS): $(OBJ_CRIS)
+	$(CC) $(CFLAGS) $(call libname,anal_cris) -o anal_cris.$(EXT_SO) $(OBJ_CRIS)
