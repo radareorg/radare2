39cd506acf0b12f250528bda9319915bf1f8732e
condret <condr3t@protonmail.com>
2022-06-30 02:52:11 +0200

    Merge asm_propeller into anal_propeller ##refactor

--- libr/anal/p/propeller.mk
+++ libr/anal/p/propeller.mk
@@ -1,8 +1,8 @@
 OBJ_propeller=anal_propeller.o
-CFLAGS+=-I../asm/arch/propeller/
+CFLAGS+=-Iarch/propeller/
 
 STATIC_OBJ+=${OBJ_propeller}
-OBJ_propeller+=../../asm/arch/propeller/propeller_disas.o
+OBJ_propeller+=../arch/propeller/propeller_disas.o
 TARGET_propeller=anal_propeller.${EXT_SO}
 
 ALL_TARGETS+=${TARGET_propeller}
