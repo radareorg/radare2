f42d88c00ce9819725aec61160dbca549c2d8dc3
pancake <pancake@nopcode.org>
2022-07-15 12:59:43 +0200

    Handle arm64's BTI instruction as a nop ##analysis

--- libr/anal/p/anal_arm_cs.c
+++ libr/anal/p/anal_arm_cs.c
@@ -3426,6 +3426,11 @@ static void anop64(csh handle, RAnalOp *op, cs_insn *insn) {
 	case ARM64_INS_CINC:
 		op->type = R_ANAL_OP_TYPE_CMOV;
 		break;
+	case ARM64_INS_BFI:
+	// case ARM64_INS_BTI:
+		op->type = R_ANAL_OP_TYPE_NOP;
+		op->family = R_ANAL_OP_FAMILY_SECURITY;
+		break;
 	case ARM64_INS_MOV:
 		if (REGID64(0) == ARM64_REG_SP) {
 			op->stackop = R_ANAL_STACK_RESET;
@@ -3445,7 +3450,6 @@ static void anop64(csh handle, RAnalOp *op, cs_insn *insn) {
 	case ARM64_INS_SBFIZ:
 	case ARM64_INS_UBFIZ:
 	case ARM64_INS_BIC:
-	case ARM64_INS_BFI:
 	case ARM64_INS_BFXIL:
 		op->type = R_ANAL_OP_TYPE_MOV;
 		if (ISIMM64 (1)) {
