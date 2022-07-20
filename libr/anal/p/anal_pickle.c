73278b2addd766d5135d7a270b1b6de8b299894f
Dennis Goodlett <dennis+git_swoops@hurricanelabs.com>
2022-07-20 12:54:47 -0400

    Change op enum nmae to make windows happy

--- libr/anal/p/anal_pickle.c
+++ libr/anal/p/anal_pickle.c
@@ -6,82 +6,82 @@
 #define MAXSTRLEN 128
 
 enum opcode {
-	MARK = '(',
-	STOP = '.',
-	POP = '0',
-	POP_MARK = '1',
-	DUP = '2',
-	FLOAT = 'F',
-	INT = 'I',
-	BININT = 'J',
-	BININT1 = 'K',
-	LONG = 'L',
-	BININT2 = 'M',
-	NONE = 'N',
-	PERSID = 'P',
-	BINPERSID = 'Q',
-	REDUCE = 'R',
-	STRING = 'S',
-	BINSTRING = 'T',
-	SHORT_BINSTRING = 'U',
-	UNICODE = 'V',
-	BINUNICODE = 'X',
-	APPEND = 'a',
-	BUILD = 'b',
-	GLOBAL = 'c',
-	DICT = 'd',
-	EMPTY_DICT = '}',
-	APPENDS = 'e',
-	GET = 'g',
-	BINGET = 'h',
-	INST = 'i',
-	LONG_BINGET = 'j',
-	LIST = 'l',
-	EMPTY_LIST = ']',
-	OBJ = 'o',
-	PUT = 'p',
-	BINPUT = 'q',
-	LONG_BINPUT = 'r',
-	SETITEM = 's',
-	TUPLE = 't',
-	EMPTY_TUPLE = ')',
-	SETITEMS = 'u',
-	BINFLOAT = 'G',
+	OP_MARK = '(',
+	OP_STOP = '.',
+	OP_POP = '0',
+	OP_POP_MARK = '1',
+	OP_DUP = '2',
+	OP_FLOAT = 'F',
+	OP_INT = 'I',
+	OP_BININT = 'J',
+	OP_BININT1 = 'K',
+	OP_LONG = 'L',
+	OP_BININT2 = 'M',
+	OP_NONE = 'N',
+	OP_PERSID = 'P',
+	OP_BINPERSID = 'Q',
+	OP_REDUCE = 'R',
+	OP_STRING = 'S',
+	OP_BINSTRING = 'T',
+	OP_SHORT_BINSTRING = 'U',
+	OP_UNICODE = 'V',
+	OP_BINUNICODE = 'X',
+	OP_APPEND = 'a',
+	OP_BUILD = 'b',
+	OP_GLOBAL = 'c',
+	OP_DICT = 'd',
+	OP_EMPTY_DICT = '}',
+	OP_APPENDS = 'e',
+	OP_GET = 'g',
+	OP_BINGET = 'h',
+	OP_INST = 'i',
+	OP_LONG_BINGET = 'j',
+	OP_LIST = 'l',
+	OP_EMPTY_LIST = ']',
+	OP_OBJ = 'o',
+	OP_PUT = 'p',
+	OP_BINPUT = 'q',
+	OP_LONG_BINPUT = 'r',
+	OP_SETITEM = 's',
+	OP_TUPLE = 't',
+	OP_EMPTY_TUPLE = ')',
+	OP_SETITEMS = 'u',
+	OP_BINFLOAT = 'G',
 
 	// Protocol 2.
-	PROTO = '\x80',
-	NEWOBJ = '\x81',
-	EXT1 = '\x82',
-	EXT2 = '\x83',
-	EXT4 = '\x84',
-	TUPLE1 = '\x85',
-	TUPLE2 = '\x86',
-	TUPLE3 = '\x87',
-	NEWTRUE = '\x88',
-	NEWFALSE = '\x89',
-	LONG1 = '\x8a',
-	LONG4 = '\x8b',
+	OP_PROTO = '\x80',
+	OP_NEWOBJ = '\x81',
+	OP_EXT1 = '\x82',
+	OP_EXT2 = '\x83',
+	OP_EXT4 = '\x84',
+	OP_TUPLE1 = '\x85',
+	OP_TUPLE2 = '\x86',
+	OP_TUPLE3 = '\x87',
+	OP_NEWTRUE = '\x88',
+	OP_NEWFALSE = '\x89',
+	OP_LONG1 = '\x8a',
+	OP_LONG4 = '\x8b',
 
 	// Protocol 3 (Python 3.x)
-	BINBYTES = 'B',
-	SHORT_BINBYTES = 'C',
+	OP_BINBYTES = 'B',
+	OP_SHORT_BINBYTES = 'C',
 
 	// Protocol 4
-	SHORT_BINUNICODE = '\x8c',
-	BINUNICODE8 = '\x8d',
-	BINBYTES8 = '\x8e',
-	EMPTY_SET = '\x8f',
-	ADDITEMS = '\x90',
-	FROZENSET = '\x91',
-	NEWOBJ_EX = '\x92',
-	STACK_GLOBAL = '\x93',
-	MEMOIZE = '\x94',
-	FRAME = '\x95',
+	OP_SHORT_BINUNICODE = '\x8c',
+	OP_BINUNICODE8 = '\x8d',
+	OP_BINBYTES8 = '\x8e',
+	OP_EMPTY_SET = '\x8f',
+	OP_ADDITEMS = '\x90',
+	OP_FROZENSET = '\x91',
+	OP_NEWOBJ_EX = '\x92',
+	OP_STACK_GLOBAL = '\x93',
+	OP_MEMOIZE = '\x94',
+	OP_FRAME = '\x95',
 
 	// Protocol 5
-	BYTEARRAY8 = '\x96',
-	NEXT_BUFFER = '\x97',
-	READONLY_BUFFER = '\x98'
+	OP_BYTEARRAY8 = '\x96',
+	OP_NEXT_BUFFER = '\x97',
+	OP_READONLY_BUFFER = '\x98'
 };
 
 static inline int handle_int(RAnalOp *op, const char *name, int sz, const ut8 *buf, int buflen) {
@@ -202,7 +202,7 @@ static inline int cnt_str(RAnal *a, RAnalOp *op, const char *name, int sz, const
 }
 
 #define trivial_op(x) \
-	op->mnemonic = strdup (#x); \
+	op->mnemonic = strdup (x); \
 	return op->size;
 
 static int analop(RAnal *a, RAnalOp *op, ut64 addr, const ut8 *buf, int len, RAnalOpMask mask) {
@@ -217,146 +217,146 @@ static int analop(RAnal *a, RAnalOp *op, ut64 addr, const ut8 *buf, int len, RAn
 	buf++;
 	len--;
 	switch (opcode) {
-	case MARK:
-		trivial_op (MARK);
-	case STOP:
-		trivial_op (STOP);
-	case POP:
-		trivial_op (POP);
-	case POP_MARK:
-		trivial_op (POP_MARK);
-	case DUP:
-		trivial_op (DUP);
-	case FLOAT:
+	case OP_MARK:
+		trivial_op ("MARK");
+	case OP_STOP:
+		trivial_op ("STOP");
+	case OP_POP:
+		trivial_op ("POP");
+	case OP_POP_MARK:
+		trivial_op ("POP_MARK");
+	case OP_DUP:
+		trivial_op ("DUP");
+	case OP_FLOAT:
 		return handle_n_lines (op, "FLOAT", 1, buf, len);
-	case INT:
+	case OP_INT:
 		return handle_n_lines (op, "INT", 1, buf, len);
-	case BININT:
+	case OP_BININT:
 		op->sign = true;
 		return handle_int (op, "BININT", 4, buf, len);
-	case BININT1:
+	case OP_BININT1:
 		return handle_int (op, "BININT1", 1, buf, len);
-	case LONG:
+	case OP_LONG:
 		return handle_n_lines (op, "LONG", 1, buf, len);
-	case BININT2:
+	case OP_BININT2:
 		return handle_int (op, "BININT2", 2, buf, len);
-	case NONE:
-		trivial_op (NONE);
-	case PERSID:
+	case OP_NONE:
+		trivial_op ("NONE");
+	case OP_PERSID:
 		// TODO: validate
 		return handle_n_lines (op, "PERSID", 1, buf, len);
-	case BINPERSID:
-		trivial_op (BINPERSID);
-	case REDUCE:
-		trivial_op (REDUCE);
-	case STRING:
+	case OP_BINPERSID:
+		trivial_op ("BINPERSID");
+	case OP_REDUCE:
+		trivial_op ("REDUCE");
+	case OP_STRING:
 		return handle_n_lines (op, "STRING", 1, buf, len);
-	case BINSTRING:
+	case OP_BINSTRING:
 		return cnt_str (a, op, "BINSTRING", 4, buf, len);
-	case SHORT_BINSTRING:
+	case OP_SHORT_BINSTRING:
 		return cnt_str (a, op, "SHORT_BINSTRING", 1, buf, len);
-	case UNICODE:
+	case OP_UNICODE:
 		return handle_n_lines (op, "UNICODE", 1, buf, len);
-	case BINUNICODE:
+	case OP_BINUNICODE:
 		return cnt_str (a, op, "BINUNICODE", 4, buf, len);
-	case APPEND:
-		trivial_op (APPEND);
-	case BUILD:
-		trivial_op (BUILD);
-	case GLOBAL:
+	case OP_APPEND:
+		trivial_op ("APPEND");
+	case OP_BUILD:
+		trivial_op ("BUILD");
+	case OP_GLOBAL:
 		return handle_n_lines (op, "GLOBAL", 2, buf, len);
-	case DICT:
-		trivial_op (DICT);
-	case EMPTY_DICT:
-		trivial_op (EMPTY_DICT);
-	case APPENDS:
-		trivial_op (APPENDS);
-	case GET:
+	case OP_DICT:
+		trivial_op ("DICT");
+	case OP_EMPTY_DICT:
+		trivial_op ("EMPTY_DICT");
+	case OP_APPENDS:
+		trivial_op ("APPENDS");
+	case OP_GET:
 		return handle_n_lines (op, "GET", 1, buf, len);
-	case BINGET:
+	case OP_BINGET:
 		op->sign = true; // I think
 		return handle_int (op, "BINGET", 1, buf, len);
-	case INST:
+	case OP_INST:
 		return handle_n_lines (op, "INST", 2, buf, len);
-	case LONG_BINGET:
+	case OP_LONG_BINGET:
 		return handle_int (op, "LONG_BINGET", 4, buf, len);
-	case LIST:
-		trivial_op (LIST);
-	case EMPTY_LIST:
-		trivial_op (EMPTY_LIST);
-	case OBJ:
-		trivial_op (OBJ);
-	case PUT:
+	case OP_LIST:
+		trivial_op ("LIST");
+	case OP_EMPTY_LIST:
+		trivial_op ("EMPTY_LIST");
+	case OP_OBJ:
+		trivial_op ("OBJ");
+	case OP_PUT:
 		return handle_n_lines (op, "PUT", 1, buf, len);
-	case BINPUT:
+	case OP_BINPUT:
 		return handle_int (op, "BINPUT", 1, buf, len);
-	case LONG_BINPUT:
+	case OP_LONG_BINPUT:
 		return handle_int (op, "LONG_BINPUT", 4, buf, len);
-	case SETITEM:
-		trivial_op (SETITEM);
-	case TUPLE:
-		trivial_op (TUPLE);
-	case EMPTY_TUPLE:
-		trivial_op (EMPTY_TUPLE);
-	case SETITEMS:
-		trivial_op (SETITEMS);
-	case BINFLOAT:
+	case OP_SETITEM:
+		trivial_op ("SETITEM");
+	case OP_TUPLE:
+		trivial_op ("TUPLE");
+	case OP_EMPTY_TUPLE:
+		trivial_op ("EMPTY_TUPLE");
+	case OP_SETITEMS:
+		trivial_op ("SETITEMS");
+	case OP_BINFLOAT:
 		return handle_float (op, "BINFLOAT", 8, buf, len);
-	case PROTO:
+	case OP_PROTO:
 		return handle_int (op, "PROTO", 1, buf, len);
-	case NEWOBJ:
-		trivial_op (NEWOBJ);
-	case EXT1:
+	case OP_NEWOBJ:
+		trivial_op ("NEWOBJ");
+	case OP_EXT1:
 		// I don't *think* it's signed
 		return handle_int (op, "EXT1", 1, buf, len);
-	case EXT2:
+	case OP_EXT2:
 		return handle_int (op, "EXT2", 2, buf, len);
-	case EXT4:
+	case OP_EXT4:
 		return handle_int (op, "EXT4", 4, buf, len);
-	case TUPLE1:
-		trivial_op (TUPLE1);
-	case TUPLE2:
-		trivial_op (TUPLE2);
-	case TUPLE3:
-		trivial_op (TUPLE3);
-	case NEWTRUE:
-		trivial_op (NEWTRUE);
-	case NEWFALSE:
-		trivial_op (NEWFALSE);
-	case LONG1:
+	case OP_TUPLE1:
+		trivial_op ("TUPLE1");
+	case OP_TUPLE2:
+		trivial_op ("TUPLE2");
+	case OP_TUPLE3:
+		trivial_op ("TUPLE3");
+	case OP_NEWTRUE:
+		trivial_op ("NEWTRUE");
+	case OP_NEWFALSE:
+		trivial_op ("NEWFALSE");
+	case OP_LONG1:
 		return handle_int (op, "LONG1", 1, buf, len);
-	case LONG4:
+	case OP_LONG4:
 		return handle_int (op, "LONG4", 4, buf, len);
-	case BINBYTES:
+	case OP_BINBYTES:
 		return cnt_str (a, op, "BINBYTES", 4, buf, len);
-	case SHORT_BINBYTES:
+	case OP_SHORT_BINBYTES:
 		return cnt_str (a, op, "SHORT_BINBYTES", 1, buf, len);
-	case SHORT_BINUNICODE:
+	case OP_SHORT_BINUNICODE:
 		return cnt_str (a, op, "SHORT_BINUNICODE", 1, buf, len);
-	case BINUNICODE8:
+	case OP_BINUNICODE8:
 		return cnt_str (a, op, "BINUNICODE8", 8, buf, len);
-	case BINBYTES8:
+	case OP_BINBYTES8:
 		return cnt_str (a, op, "BINBYTES8", 8, buf, len);
-	case EMPTY_SET:
-		trivial_op (EMPTY_SET);
-	case ADDITEMS:
-		trivial_op (ADDITEMS);
-	case FROZENSET:
-		trivial_op (FROZENSET);
-	case NEWOBJ_EX:
-		trivial_op (NEWOBJ_EX);
-	case STACK_GLOBAL:
-		trivial_op (STACK_GLOBAL);
-	case MEMOIZE:
-		trivial_op (MEMOIZE);
-	case FRAME:
+	case OP_EMPTY_SET:
+		trivial_op ("EMPTY_SET");
+	case OP_ADDITEMS:
+		trivial_op ("ADDITEMS");
+	case OP_FROZENSET:
+		trivial_op ("FROZENSET");
+	case OP_NEWOBJ_EX:
+		trivial_op ("NEWOBJ_EX");
+	case OP_STACK_GLOBAL:
+		trivial_op ("STACK_GLOBAL");
+	case OP_MEMOIZE:
+		trivial_op ("MEMOIZE");
+	case OP_FRAME:
 		return handle_int (op, "FRAME", 8, buf, len);
-	case BYTEARRAY8:
+	case OP_BYTEARRAY8:
 		return cnt_str (a, op, "BYTEARRAY8", 8, buf, len);
-	case NEXT_BUFFER:
-		trivial_op (NEXT_BUFFER);
-	case READONLY_BUFFER:
-		trivial_op (READONLY_BUFFER);
+	case OP_NEXT_BUFFER:
+		trivial_op ("NEXT_BUFFER");
+	case OP_READONLY_BUFFER:
+		trivial_op ("READONLY_BUFFER");
 	}
 
 	// bad opcode, must be at bad addr
