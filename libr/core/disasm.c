5be46d0affc06737fd981499076541facd7aacdb
pancake <pancake@nopcode.org>
2022-07-21 18:00:21 +0200

    je

--- libr/core/disasm.c
+++ libr/core/disasm.c
@@ -3281,12 +3281,13 @@ static bool ds_print_meta_infos(RDisasmState *ds, ut8* buf, int len, int idx, in
 						int remaining = size - delta;
 						remaining = R_MAX (remaining, 0);
 						if (remaining > (len - delta)) {
-							size_t calloc_size = R_MIN (len, size - delta);
+							size_t calloc_size = R_MAX (len, size - delta);
 							if (idx < calloc_size) {
-								ut8 *b = r_mem_dup (buf, calloc_size);
+								ut8 *b = calloc (1, calloc_size);
 								if (b) {
+									memcpy (b, buf, len);
 									r_print_hexdump (core->print, ds->at,
-											b + idx, calloc_size - idx, 16, 1, 1);
+											b + idx, remaining, 16, 1, 1);
 									free (b);
 								}
 							}
