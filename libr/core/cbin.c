70f934afc77d9c290d420588f9c8ad8669001b81
Paul B Mahol <onemda@gmail.com>
2022-07-25 21:22:57 +0200

    Fix leak of anal_arch in r_core_anal_cc_init()

--- libr/core/cbin.c
+++ libr/core/cbin.c
@@ -827,10 +827,10 @@ R_API void r_core_anal_cc_init(RCore *core) {
 	if (anal_arch && sdb_isempty (core->anal->sdb_cc)) {
 		R_LOG_WARN ("Missing calling conventions for '%s' %d. Deriving it from the regprofile", anal_arch, bits);
 	}
-	free (anal_arch);
 	free (dbpath);
 	free (dbhomepath);
 #endif
+	free (anal_arch);
 }
 
 static int bin_info(RCore *r, PJ *pj, int mode, ut64 laddr) {
