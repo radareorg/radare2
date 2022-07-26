bff427af0576209bf91b88c56596a3313bac1fb6
Paul B Mahol <onemda@gmail.com>
2022-07-26 07:32:54 +0200

    Check for memory allocation failure in backtrace_fuzzy()

--- libr/debug/p/native/bt/fuzzy_all.c
+++ libr/debug/p/native/bt/fuzzy_all.c
@@ -49,6 +49,9 @@ static RList *backtrace_fuzzy(RDebug *dbg, ut64 at) {
 
 	stacksize = 1024*512; // 512KB .. should get the size from the regions if possible
 	stack = malloc (stacksize);
+	if (!stack) {
+		return NULL;
+	}
 	if (at == UT64_MAX) {
 		RRegItem *ri;
 		RReg *reg = dbg->reg;
