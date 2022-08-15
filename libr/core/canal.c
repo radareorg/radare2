9629e910844e47cbf2a33825da18277a8f0cb55f
pancake <pancake@nopcode.org>
2022-08-15 13:48:32 +0200

    Don't show the linearsize in the `afl` output ##anal

--- libr/core/canal.c
+++ libr/core/canal.c
@@ -2733,14 +2733,19 @@ static void __fcn_print_default(RCore *core, RAnalFunction *fcn, bool quiet) {
 	} else {
 		char *name = r_core_anal_fcn_name (core, fcn);
 		ut64 realsize = r_anal_function_realsize (fcn);
+#if 0
 		ut64 size = r_anal_function_linear_size (fcn);
 		char *msg = (realsize == size)
 			? r_str_newf ("%-12"PFMT64u, size)
 			: r_str_newf ("%-4"PFMT64u" -> %-4"PFMT64u, size, realsize);
 		r_cons_printf ("0x%08"PFMT64x" %4d %4s %s\n",
 				fcn->addr, r_list_length (fcn->bbs), msg, name);
-		free (name);
 		free (msg);
+#else
+		r_cons_printf ("0x%08"PFMT64x" %4d %6"PFMT64d" %s\n",
+				fcn->addr, r_list_length (fcn->bbs), realsize, name);
+#endif
+		free (name);
 	}
 }
 
