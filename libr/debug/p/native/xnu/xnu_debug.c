radareorg/radare2

commit d2d241a5c05829cc28ddf4679548f7d28f6da17f

Fix arm64 register access in xnu debugger ##debug
* Inspired by commit e893d2fb66916734fa71589058f7de9bb8c8fbf5
* in rizinorg/rizin by Author: Florian Märkl <info@florianmaerkl.de>
* Date:   Sat Aug 6 19:09:11 2022 +0200

    Fix arm64 register access in xnu debugger

    ARM_THREAD_STATE64 fetches arm_thread_state64_t but our struct was
    arm_unified_thread_state_t, which adds a header. The pc in the register
    profile was hacked to account for the shift, but all the x-regs were
    not, so x7 had the value of x8 for example. Now we just use the specific
    state for 32 or 64.

libr/debug/p/native/xnu/xnu_debug.c
@@ -448,8 +448,16 @@ int xnu_reg_write(RDebug *dbg, int type, const ut8 *buf, int size) {
 #if __POWERPC__
 #warning TODO powerpc support here
 #else
-		memcpy (&th->gpr.uts, buf, R_MIN (size, sizeof (th->gpr.uts)));
+		// memcpy (&th->gpr.uts, buf, R_MIN (size, sizeof (th->gpr.uts)));
+		{
+		size_t buf_size = R_MIN (size, sizeof (th->gpr));
+#if __x86_64__ || __i386__
+		memcpy (&th->gpr.uts, buf, buf_size);
+#else
+		memcpy (&th->gpr, buf, buf_size);
 #endif
+#endif
+		}
 		ret = xnu_thread_set_gpr (dbg, th);
 		break;
 	}
@@ -594,7 +602,8 @@ static void xnu_free_threads_ports(RDebugPid *p) {
 RList *xnu_thread_list(RDebug *dbg, int pid, RList *list) {
 #if __arm__ || __arm64__ || __aarch_64__
 	#define CPU_PC (dbg->bits == R_SYS_BITS_64) ? \
-		state.ts_64.__pc : state.ts_32.__pc
+		state.arm64.__pc : state.arm32.__pc
+		/* state.ts_64.__pc : state.ts_32.__pc */
 #elif __POWERPC__
 	#define CPU_PC state.srr0
 #elif __x86_64__ || __i386__

libr/debug/p/native/xnu/xnu_threads.c
@@ -138,7 +138,7 @@ static bool xnu_thread_set_gpr(RDebug *dbg, xnu_thread_t *thread) {
 	// thread->flavor is used in a switch+case but in regs->tsh.flavor we
 	// specify
 	thread->state = &regs->uts;
-	//thread->state = regs;
+	thread->state = &regs;
 	thread->flavor = x86_THREAD_STATE;
 	thread->count = x86_THREAD_STATE_COUNT;
 	if (dbg->bits == R_SYS_BITS_64) {
@@ -154,8 +154,8 @@ static bool xnu_thread_set_gpr(RDebug *dbg, xnu_thread_t *thread) {
 	thread->flavor = ARM_UNIFIED_THREAD_STATE;
 	thread->count = ARM_UNIFIED_THREAD_STATE_COUNT;
 #endif
-	//thread->state = regs;
-	thread->state = &regs->uts;
+	//thread->state = &regs->uts;
+	thread->state = &regs;
 	if (dbg->bits == R_SYS_BITS_64) {
 		thread->flavor = ARM_THREAD_STATE64;
 		thread->count = ARM_THREAD_STATE64_COUNT;
@@ -191,8 +191,8 @@ static bool xnu_thread_get_gpr(RDebug *dbg, xnu_thread_t *thread) {
 #if __POWERPC__
 	thread->state = regs;
 #elif  __arm64 || __aarch64 || __arch64__ || __arm64__
-	//thread->state = regs;
-	thread->state = &regs->uts;
+	thread->state = regs;
+	// thread->state = &regs->uts;
 	if (dbg->bits == R_SYS_BITS_64) {
 		thread->flavor = ARM_THREAD_STATE64;
 		thread->count = ARM_THREAD_STATE64_COUNT;

libr/debug/p/native/xnu/xnu_threads.h
@@ -15,7 +15,13 @@
 #	ifndef ARM_THREAD_STATE64
 #		define ARM_THREAD_STATE64 6
 #	endif
-#	define R_REG_T arm_unified_thread_state_t
+typedef union rz_xnu_arm_reg_state_t {
+	// which one is used here is determined by RzXnuDebug.cpu
+	arm_thread_state32_t arm32;
+	arm_thread_state64_t arm64;
+} RXnuArmRegState;
+// #	define R_REG_T arm_unified_thread_state_t
+#define R_REG_T        RXnuArmRegState
 #	define R_REG_STATE_T MACHINE_THREAD_STATE
 #	define R_REG_STATE_SZ MACHINE_THREAD_STATE_COUNT
 #elif __x86_64__ || __i386__

libr/debug/p/native/xnu/reg/darwin-arm64.h
@@ -86,10 +86,9 @@ return strdup (
 // special registers
 "gpr	fp	.64	232	0\n" // FP
 "gpr	lr	.64	236	0\n" // LR
-"gpr	sp	.64	240	0\n" // SP
-"gpr	pc	.64	248	0\n" // PC
-// "gpr	pstate	.64	256	0\n" // FLAGS
-"gpr	pstate	.64	256	0   _____tfiae_____________j__qvczn\n" // x0
+"gpr	sp	.64	248	0\n" // SP
+"gpr	pc	.64	256	0\n" // PC
+"gpr	pstate	.64	264	0   _____tfiae_____________j__qvczn\n" // x0
 "gpr	vf	.1	256.28	0	overflow\n" // set if overflows
 "gpr	cf	.1	256.29	0	carry\n" // set if last op carries
 "gpr	zf	.1	256.30	0	zero\n" // set if last op is 0

commit 4e4053d4f4c53d4e8821f6039d20c5acc77188c0

Fix build for 32bit iOS debugger ##debug
* Regression introduced in d2d241a5c05829cc28ddf4679548f7d28f6da17f
* May require extra testing

libr/debug/p/native/xnu/xnu_debug.c
@@ -605,10 +605,12 @@ static void xnu_free_threads_ports(RDebugPid *p) {
 */
 
 RList *xnu_thread_list(RDebug *dbg, int pid, RList *list) {
-#if __arm__ || __arm64__ || __aarch_64__
+#if __arm64__ || __aarch_64__
 	#define CPU_PC (dbg->bits == R_SYS_BITS_64) ? \
 		state.arm64.__pc : state.arm32.__pc
-		/* state.ts_64.__pc : state.ts_32.__pc */
+#elif __arm__ || __arm
+	#define CPU_PC (dbg->bits == R_SYS_BITS_64) ? \
+		state.ts_64.__pc : state.ts_32.__pc
 #elif __POWERPC__
 	#define CPU_PC state.srr0
 #elif __x86_64__ || __i386__

libr/debug/p/native/xnu/xnu_threads.c
@@ -103,9 +103,11 @@ static bool xnu_thread_set_drx(RDebug *dbg, xnu_thread_t *thread) {
 #elif __arm__ || __arm || __armv7__
 	thread->count = ARM_DEBUG_STATE_COUNT;
 	thread->flavor = ARM_DEBUG_STATE;
+	mach_msg_type_number_t count;
 	rc = thread_set_state (thread->port, thread->flavor,
 			       (thread_state_t)&thread->debug.drx,
-			       &thread->count);
+				&count);
+	thread->count = count;
 #elif __POWERPC__
 /* not supported */
 # ifndef PPC_DEBUG_STATE32
@@ -138,7 +140,6 @@ static bool xnu_thread_set_gpr(RDebug *dbg, xnu_thread_t *thread) {
 	// thread->flavor is used in a switch+case but in regs->tsh.flavor we
 	// specify
 	thread->state = &regs->uts;
-	thread->state = &regs;
 	thread->flavor = x86_THREAD_STATE;
 	thread->count = x86_THREAD_STATE_COUNT;
 	if (dbg->bits == R_SYS_BITS_64) {

libr/debug/p/native/xnu/xnu_threads.h
@@ -6,8 +6,7 @@
 #	define R_REG_T ppc_thread_state_t
 #	define R_REG_STATE_T PPC_THREAD_STATE
 #	define R_REG_STATE_SZ PPC_THREAD_STATE_SZ
-
-#elif __arm || __arm64 || __aarch64
+#elif __arm64 || __aarch64
 #	include <mach/arm/thread_status.h>
 #	ifndef ARM_THREAD_STATE
 #		define ARM_THREAD_STATE 1
@@ -15,13 +14,16 @@
 #	ifndef ARM_THREAD_STATE64
 #		define ARM_THREAD_STATE64 6
 #	endif
-typedef union rz_xnu_arm_reg_state_t {
+typedef union r_xnu_arm_reg_state_t {
 	// which one is used here is determined by RzXnuDebug.cpu
 	arm_thread_state32_t arm32;
 	arm_thread_state64_t arm64;
 } RXnuArmRegState;
-// #	define R_REG_T arm_unified_thread_state_t
+#undef R_REG_T
 #define R_REG_T        RXnuArmRegState
+#elif __arm || __arm__
+#undef R_REG_T
+#	define R_REG_T arm_unified_thread_state_t
 #	define R_REG_STATE_T MACHINE_THREAD_STATE
 #	define R_REG_STATE_SZ MACHINE_THREAD_STATE_COUNT
 #elif __x86_64__ || __i386__