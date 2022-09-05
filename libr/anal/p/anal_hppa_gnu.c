27556a3f74a2e9dd43747d096ca2dcb5be17df97
pancake <pancake@nopcode.org>
2022-09-05 16:17:05 +0200

    Move the hppa plugin from asm to anal ##refactor

--- libr/anal/p/anal_hppa_gnu.c
+++ libr/anal/p/anal_hppa_gnu.c
@@ -1,14 +1,6 @@
-/* radare - LGPL - Copyright 2015 - pancake */
+/* radare - LGPL - Copyright 2015-2022 - pancake */
 
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
-
-#include <r_types.h>
-#include <r_lib.h>
-#include <r_util.h>
 #include <r_asm.h>
-
 #include "disas-asm.h"
 
 static R_TH_LOCAL unsigned long Offset = 0;
@@ -16,16 +8,6 @@ static R_TH_LOCAL RStrBuf *buf_global = NULL;
 static R_TH_LOCAL unsigned char bytes[4];
 
 static int hppa_buffer_read_memory(bfd_vma memaddr, bfd_byte *myaddr, ut32 length, struct disassemble_info *info) {
-#if 0 // XXX wtf ?!
-	if (length == 4)  {
-		// swap
-		myaddr[0] = bytes[3];
-		myaddr[1] = bytes[2];
-		myaddr[2] = bytes[1];
-		myaddr[3] = bytes[0];
-		return 0;
-	}
-#endif
 	int delta = (memaddr - Offset);
 	if (delta < 0) {
 		return -1;      // disable backward reads
@@ -48,48 +30,53 @@ static void memory_error_func(int status, bfd_vma memaddr, struct disassemble_in
 DECLARE_GENERIC_PRINT_ADDRESS_FUNC()
 DECLARE_GENERIC_FPRINTF_FUNC()
 
-static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
-	struct disassemble_info disasm_obj;
-	if (len < 4) {
-		return -1;
+static int hppa_op(RAnal *a, RAnalOp *op, ut64 addr, const ut8 *buf, int len, RAnalOpMask mask) {
+	struct disassemble_info disasm_obj = {0};
+	if (mask & R_ANAL_OP_MASK_DISASM) {
+		buf_global = r_strbuf_new (NULL);
 	}
-	buf_global = &op->buf_asm;
-	Offset = a->pc;
-	memcpy (bytes, buf, 4); // TODO handle thumb
+	Offset = addr;
+	memcpy (bytes, buf, R_MIN (sizeof (bytes), len)); // TODO handle thumb
 
 	/* prepare disassembler */
-	memset (&disasm_obj, '\0', sizeof (struct disassemble_info));
-	disasm_obj.disassembler_options = (a->config->bits == 64)?"64":"";
 	disasm_obj.buffer = bytes;
 	disasm_obj.read_memory_func = &hppa_buffer_read_memory;
 	disasm_obj.symbol_at_address_func = &symbol_at_address;
 	disasm_obj.memory_error_func = &memory_error_func;
 	disasm_obj.print_address_func = &generic_print_address_func;
-	disasm_obj.endian = BFD_ENDIAN_BIG;
+	disasm_obj.endian = !a->config->big_endian;
 	disasm_obj.fprintf_func = &generic_fprintf_func;
 	disasm_obj.stream = stdout;
-
 	op->size = print_insn_hppa ((bfd_vma)Offset, &disasm_obj);
-	if (op->size == -1) {
-		r_strbuf_set (&op->buf_asm, "(data)");
+
+	if (mask & R_ANAL_OP_MASK_DISASM) {
+		op->mnemonic = r_strbuf_drain (buf_global);
+		for (char *c = op->mnemonic; *c != 0; c++) {
+			if (*c == '\t') {
+				*c = ' ';
+			}
+		}
+		buf_global = NULL;
 	}
+
 	return op->size;
 }
 
-RAsmPlugin r_asm_plugin_hppa_gnu = {
+RAnalPlugin r_anal_plugin_hppa_gnu = {
 	.name = "hppa",
 	.arch = "hppa",
 	.license = "GPL3",
-	.bits = 32,
+	.cpus = "",
+	.bits = 16,
 	.endian = R_SYS_ENDIAN_BIG,
 	.desc = "HP PA-RISC",
-	.disassemble = &disassemble
+	.op = &hppa_op
 };
 
 #ifndef R2_PLUGIN_INCORE
 R_API RLibStruct radare_plugin = {
-	.type = R_LIB_TYPE_ASM,
-	.data = &r_asm_plugin_hppa_gnu,
+	.type = R_LIB_TYPE_ANAL,
+	.data = &r_anal_plugin_hppa_gnu,
 	.version = R2_VERSION
 };
 #endif
