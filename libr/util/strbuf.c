c391be63929d24bd300ca6ef02edc828a14d1eb6
condret <condr3t@protonmail.com>
2022-09-07 16:24:00 +0200

    Add r_strbuf_replace{f} ##util

--- libr/util/strbuf.c
+++ libr/util/strbuf.c
@@ -342,6 +342,45 @@ R_API char *r_strbuf_drain_nofree(RStrBuf *sb) {
 	return ret;
 }
 
+R_API bool r_strbuf_replace(RStrBuf *sb, const char *key, const char *val) {
+	r_return_val_if_fail (sb && key && val, false);
+	char *tmp = r_str_replace (strdup (r_strbuf_get (sb)), key, val, 0);
+	if (!tmp) {
+		return false;
+	}
+	free (r_strbuf_drain_nofree (sb));
+	return r_strbuf_setptr (sb, tmp, 0);
+}
+
+R_API bool r_strbuf_replacef(RStrBuf *sb, const char *key, const char *fmt, ...) {
+	r_return_val_if_fail (sb && key && fmt, false);
+	RStrBuf *sb_tmp = r_strbuf_new (NULL);
+	if (!sb_tmp) {
+		return false;
+	}
+	char *tmp = strdup (r_strbuf_get (sb));
+	if (!tmp) {
+		r_strbuf_free (sb_tmp);
+		return false;
+	}
+	va_list ap;
+	va_start (ap, fmt);
+	const bool vsret = r_strbuf_vsetf (sb_tmp, fmt, ap);
+	va_end (ap);
+	if (!vsret) {
+		r_strbuf_free (sb_tmp);
+		free (tmp);
+		return false;
+	}
+	tmp = r_str_replace (tmp, key, r_strbuf_get (sb_tmp), 0);
+	r_strbuf_free (sb_tmp);
+	if (!tmp) {
+		return false;
+	}
+	free (r_strbuf_drain_nofree (sb));
+	return r_strbuf_setptr (sb, tmp, 0);
+}
+
 R_API void r_strbuf_free(RStrBuf *sb) {
 	if (sb) {
 		r_strbuf_fini (sb);
