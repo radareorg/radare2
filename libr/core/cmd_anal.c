radareorg/radare2

commit 96ca94e8929fa3474a5ad7af07dc114f822fdea4

Include bb instruction addresses in an array for `abj` ##analysis

libr/core/cmd_anal.c
@@ -2874,6 +2874,8 @@ static void print_bb(PJ *pj, const RAnalBlock *b, const RAnalFunction *fcn, cons
 	ut64 opaddr = __opaddr (b, addr);
 	if (pj) {
 		pj_o (pj);
+		pj_kn (pj, "addr", b->addr);
+		pj_ki (pj, "size", b->size);
 		if (b->jump != UT64_MAX) {
 			pj_kn (pj, "jump", b->jump);
 		}
@@ -2904,11 +2906,18 @@ static void print_bb(PJ *pj, const RAnalBlock *b, const RAnalFunction *fcn, cons
 			pj_end (pj);
 		}
 		pj_kn (pj, "opaddr", opaddr);
-		pj_kn (pj, "addr", b->addr);
-		pj_ki (pj, "size", b->size);
 		pj_ki (pj, "inputs", inputs);
 		pj_ki (pj, "outputs", outputs);
 		pj_ki (pj, "ninstr", b->ninstr);
+		pj_ko (pj, "instrs");
+		{
+			int i;
+			for (i = 0; i <= b->ninstr; i++) {
+				int delta = (i > 0)? b->op_pos[i-1]: 0;
+				pj_n (pj, b->addr + delta);
+			}
+		}
+		pj_end (pj);
 		pj_kb (pj, "traced", b->traced);
 		pj_end (pj);
 	} else {

test/db/cmd/cmd_afb
@@ -42,14 +42,31 @@ traced: false
 opaddr: 0x1000011e9
 opaddr: 0x1000011e9
 {
+  "addr": "0x1000011e8",
+  "size": 41,
   "jump": "0x100001216",
   "fail": "0x100001211",
   "opaddr": "0x1000011e9",
-  "addr": "0x1000011e8",
-  "size": 41,
   "inputs": 0,
   "outputs": 2,
   "ninstr": 14,
+  "instrs": {
+    4294971880,
+    4294971881,
+    4294971884,
+    4294971886,
+    4294971888,
+    4294971890,
+    4294971892,
+    4294971893,
+    4294971900,
+    4294971903,
+    4294971906,
+    4294971913,
+    4294971917,
+    4294971919,
+    4294971880
+  },
   "traced": false
 }
 opaddr: 0x1000011e9

commit 1e86c6dcfc873024b2b7905cc73ff6e7f92cf4c6

Fix bug introduced in 96ca94e8929fa3474a5ad7af07dc114f822fdea4 for abj

libr/core/cmd_anal.c
@@ -2910,11 +2910,11 @@ static void print_bb(PJ *pj, const RAnalBlock *b, const RAnalFunction *fcn, cons
 		pj_ki (pj, "inputs", inputs);
 		pj_ki (pj, "outputs", outputs);
 		pj_ki (pj, "ninstr", b->ninstr);
-		pj_ko (pj, "instrs");
+		pj_ka (pj, "instrs");
 		{
 			int i;
-			for (i = 0; i <= b->ninstr; i++) {
-				int delta = (i > 0)? b->op_pos[i-1]: 0;
+			for (i = 0; i < b->ninstr; i++) {
+				int delta = (i > 0)? b->op_pos[i - 1]: 0;
 				pj_n (pj, b->addr + delta);
 			}
 		}

test/db/cmd/cmd_afb
@@ -50,7 +50,7 @@ opaddr: 0x1000011e9
   "inputs": 0,
   "outputs": 2,
   "ninstr": 14,
-  "instrs": {
+  "instrs": [
     4294971880,
     4294971881,
     4294971884,
@@ -64,9 +64,8 @@ opaddr: 0x1000011e9
     4294971906,
     4294971913,
     4294971917,
-    4294971919,
-    4294971880
-  },
+    4294971919
+  ],
   "traced": false
 }
 opaddr: 0x1000011e9