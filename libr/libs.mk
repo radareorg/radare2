f6383a05f314bfc6b3479327325b3ab1d2e38c7b
pancake <pancake@nopcode.org>
2022-09-18 00:55:33 +0200

    Introduce the new r_arch library, just the skeleton ##arch

--- libr/libs.mk
+++ libr/libs.mk
@@ -3,7 +3,7 @@
 ifeq ($(LIBS0),)
 LIBS0=util
 LIBS1=socket reg cons magic bp config
-LIBS2=syscall lang crypto flag
+LIBS2=syscall lang crypto flag arch
 LIBS3=parse io search
 LIBS4=asm fs
 LIBS5=anal egg bin
