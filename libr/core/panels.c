53d645c25bf1d42f4edcea6098a99cd67394460c
pancake <pancake@nopcode.org>
2022-09-25 22:16:03 +0200

    Override scr.maxpage in panels to avoid undesired prompts ##visual

--- libr/core/panels.c
+++ libr/core/panels.c
@@ -7302,6 +7302,8 @@ R_API bool r_core_panels_root(RCore *core, RPanelsRoot *panels_root) {
 			}
 		}
 	}
+	int maxpage = r_config_get_i (core->config, "scr.maxpage");
+	r_config_set_i (core->config, "scr.maxpage", 0);
 	while (panels_root->n_panels) {
 		__set_root_state (core, DEFAULT);
 		__panels_process (core, panels_root->panels[panels_root->cur_panels]);
@@ -7312,6 +7314,7 @@ R_API bool r_core_panels_root(RCore *core, RPanelsRoot *panels_root) {
 			break;
 		}
 	}
+	r_config_set_i (core->config, "scr.maxpage", maxpage);
 	if (fromVisual) {
 		r_core_visual (core, "");
 	} else {
