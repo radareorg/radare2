f2934eb19ad4e1b100924e91eb28d1a8f1a8d74a
rhl120 <rhl120@protonmail.com>
2022-09-26 08:48:58 +0200

    Fix some stuff in ravc2
    Co-authored-by: pancake <pancake@nopcode.org>

--- libr/main/ravc2.c
+++ libr/main/ravc2.c
@@ -1,30 +1,36 @@
 /* radare - LGPL - Copyright 2022 - pancake, rhl120 */
 
+#define R_LOG_ORIGIN "ravc"
+
 #include <rvc.h>
 #include <r_list.h>
 
 static void usage(void) {
-	printf ("Usage: ravc2 [action] [args ...]\n");
+	printf ("Usage: ravc2 [-qvh] [action] [args ...]\n");
 }
 
 static void help(void) {
 	usage ();
 	printf (
+		"Flags:\n"
+		" -q       quiet mode\n"
+		" -v       show version\n"
+		" -h       display this help message\n"
 		"Actions:\n"
-		" init     [git | rvc]          initialize a repository with the given vc.\n" 
-		" branch   [name]               if a name is provided, create a branch with that name otherwise list branches."
-		" commit   [message] [files...] commit the files with the message."
-		" checkout [branch]             set the current branch to the given branch."
-		" status                        print a status message."
-		" reset                         remove all uncommitted changes."
-		" log                           print all commits."
-		" RAVC2_USER=[n]                override cfg.user value to author commit.\n"
+		" init     [git | rvc]          initialize a repository with the given vc\n"
+		" branch   [name]               if a name is provided, create a branch with that name otherwise list branches\n"
+		" commit   [message] [files...] commit the files with the message\n"
+		" checkout [branch]             set the current branch to the given branch\n"
+		" status                        print a status message\n"
+		" reset                         remove all uncommitted changes\n"
+		" log                           print all commits\n"
+		" RAVC2_USER=[n]                override cfg.user value to author commit\n"
 	);
 }
 
 static char *get_author(void) {
 	char *author = r_sys_getenv ("RAVC2_USER");
-	if (R_STR_ISEMPTY(author)) {
+	if (R_STR_ISEMPTY (author)) {
 		free (author);
 		return r_sys_whoami ();
 	}
@@ -40,6 +46,9 @@ R_API int r_main_ravc2(int argc, const char **argv) {
 	if (argc < 2) {
 		usage ();
 		return 1;
+	}
+	if (!r_cons_is_initialized ()) {
+		r_cons_new ();
 	}
 	r_getopt_init (&opt, argc, argv, "gqvh");
 	while ((c = r_getopt_next (&opt)) != -1) {
@@ -66,9 +75,14 @@ R_API int r_main_ravc2(int argc, const char **argv) {
 		}
 		return r_main_version_print ("ravc2");
 	}
+	if (opt.ind >= argc) {
+		R_LOG_ERROR ("Try ravc2 -h");
+		return 1;
+	}
 
 	const char *action = opt.argv[opt.ind];
 	if (!action) {
+		R_LOG_ERROR ("Unknown action");
 		return 1;
 	}
 	char *rp = r_sys_getdir ();
@@ -79,7 +93,7 @@ R_API int r_main_ravc2(int argc, const char **argv) {
 	if (!strcmp (action, "init")) {
 		Rvc *rvc = NULL;
 		if (opt.argc <= 2) {
-			R_LOG_ERROR("Usage: ravc2 <git | rvc>");
+			R_LOG_ERROR ("Usage: ravc2 <git | rvc>");
 		} else if (!strcmp (opt.argv[opt.ind + 1], "git")) {
 			rvc = r_vc_git_init (rp);
 		} else if (!strcmp (opt.argv[opt.ind + 1], "rvc")) {
@@ -88,21 +102,23 @@ R_API int r_main_ravc2(int argc, const char **argv) {
 			R_LOG_ERROR ("unknown option %s", opt.argv[opt.ind + 1]);
 		}
 		free (rp);
-		return rvc? !r_vc_save(rvc) : 1;
+		return rvc? !r_vc_save (rvc) : 1;
 	}
 	Rvc *rvc = rvc_git_open (rp);
-	R_FREE (rp);
 	if (!rvc) {
+		R_LOG_ERROR ("Invalid action or repository in %s", rp);
+		R_FREE (rp);
 		return 1;
 	}
+	R_FREE (rp);
 	bool save = false; // only save the db if the command ran successfully
 	// commands that need Rvc *
 	if (!strcmp (action, "branch")) {
 		if (opt.argc <= 2) {
-			RList *branches = rvc_git_get_branches(rvc);
+			RList *branches = rvc_git_get_branches (rvc);
 			RListIter *iter;
 			char *branch;
-			r_list_foreach(branches, iter, branch) {
+			r_list_foreach (branches, iter, branch) {
 				printf ("%s\n", branch);
 			}
 			r_list_free(branches);
@@ -146,9 +162,9 @@ R_API int r_main_ravc2(int argc, const char **argv) {
 			printf ("Branch: %s\n", current_branch);
 			RList *uncommitted = rvc->get_uncommitted (rvc);
 			if (r_list_empty (uncommitted)) {
-				printf("All files are committed\n");
+				printf ("All files are committed\n");
 			} else {
-				printf ("The follwing files are uncommitted:\n");
+				printf ("The following files were NOT committed:\n");
 				RListIter *iter;
 				char *file;
 				r_list_foreach (uncommitted, iter, file) {
@@ -158,13 +174,13 @@ R_API int r_main_ravc2(int argc, const char **argv) {
 			r_list_free (uncommitted);
 		}
 	} else if (!strcmp (action, "reset")) {
-		save = rvc->reset(rvc);
+		save = rvc->reset (rvc);
 	} else if (!strcmp (action, "log")) {
-		save = rvc->print_commits(rvc);
+		save = rvc->print_commits (rvc);
 	} else {
 		R_LOG_ERROR ("Incorrect command");
 	}
 ret:
-	rvc_git_close(rvc, save);
+	rvc_git_close (rvc, save);
 	return !save;
 }
