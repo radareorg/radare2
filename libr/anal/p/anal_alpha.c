2ab4471cc2f6ed8cb531ed74ce465d36b7a330cd
pancake <pancake@nopcode.org>
2022-09-27 17:51:44 +0200

    Remove 3 globals from anal.alpha ##globals

--- libr/anal/p/anal_alpha.c
+++ libr/anal/p/anal_alpha.c
@@ -14,17 +14,13 @@
 #include "disas-asm.h"
 #include "../../asm/arch/include/opcode/alpha.h"
 
-static R_TH_LOCAL unsigned long Offset = 0;
-static R_TH_LOCAL RStrBuf *buf_global = NULL;
-static R_TH_LOCAL const ut8 *bytes = NULL;
-static R_TH_LOCAL int bytes_size = 0;
-
 static int alpha_buffer_read_memory(bfd_vma memaddr, bfd_byte *myaddr, ut32 length, struct disassemble_info *info) {
-	int delta = (memaddr - Offset);
+	int delta = (memaddr - info->buffer_vma);
 	if (delta < 0) {
 		return -1; // disable backward reads
 	}
-	memcpy (myaddr, bytes + delta, R_MIN (length, bytes_size));
+	ut8 *bytes = info->buffer;
+	memcpy (myaddr, bytes + delta, length); // R_MIN (length, bytes_size));
 	return 0;
 }
 
@@ -36,20 +32,20 @@ static void memory_error_func(int status, bfd_vma memaddr, struct disassemble_in
 	//--
 }
 
-DECLARE_GENERIC_PRINT_ADDRESS_FUNC()
-DECLARE_GENERIC_FPRINTF_FUNC()
+DECLARE_GENERIC_PRINT_ADDRESS_FUNC_NOGLOBALS()
+DECLARE_GENERIC_FPRINTF_FUNC_NOGLOBALS()
 
 static int alpha_op(RAnal *a, RAnalOp *op, ut64 addr, const ut8 *buf, int len, RAnalOpMask mask) {
+	ut8 bytes [8] = {0};
+	RStrBuf *sb = NULL;
 	struct disassemble_info disasm_obj = {0};
 	if (len < 4) {
 		return -1;
 	}
 	if (mask & R_ANAL_OP_MASK_DISASM) {
-		buf_global = r_strbuf_new (NULL);
+		sb = r_strbuf_new (NULL);
 	}
-	bytes = buf;
-	bytes_size = len;
-	Offset = addr;
+	// bytes_size = len;
 
 	/* prepare disassembler */
 	disasm_obj.buffer = (ut8*)bytes;
@@ -59,19 +55,19 @@ static int alpha_op(RAnal *a, RAnalOp *op, ut64 addr, const ut8 *buf, int len, R
 	disasm_obj.print_address_func = &generic_print_address_func;
 	disasm_obj.endian = BFD_ENDIAN_LITTLE;
 	disasm_obj.fprintf_func = &generic_fprintf_func;
-	disasm_obj.stream = stdout;
-	op->size = print_insn_alpha ((bfd_vma)Offset, &disasm_obj);
+	disasm_obj.stream = sb;
+	op->size = print_insn_alpha ((bfd_vma)addr, &disasm_obj);
 
 	if (mask & R_ANAL_OP_MASK_DISASM) {
 		if (op->size > 0) {
-			op->mnemonic = r_strbuf_drain (buf_global);
-			buf_global = NULL;
+			op->mnemonic = r_strbuf_drain (sb);
+			sb = NULL;
 			r_str_replace_char (op->mnemonic, '\t', ' ');
 		} else {
 			op->mnemonic = strdup ("(data)");
 		}
-		r_strbuf_free (buf_global);
-		buf_global = NULL;
+		r_strbuf_free (sb);
+		sb = NULL;
 	}
 
 	return op->size;
