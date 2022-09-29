adb7c1388adaa3995a1489a7c1fa2927d011b5c9
pancake <pancake@nopcode.org>
2022-09-29 11:37:06 +0200

    Initial support for XDG env vars and paths ##core

---
+++ libr/util/xdg.c
@@ -0,0 +1,30 @@
+/* radare2 - LGPL - Copyright 2022 - pancake */
+
+// move into file.c ?
+#include <r_util.h>
+
+static char *xdg(const char *env, const char *a, const char *s) {
+	char *dir = r_sys_getenv (env);
+	if (R_STR_ISEMPTY (dir)) {
+		free (dir);
+		dir = r_str_home (a);
+	}
+	char *res = r_file_new (dir, "radare2", s, NULL);
+	free (dir);
+	return res;
+}
+
+// XDG_CONFIG_HOME	User-specific configuration files	~/.var/app/<app-id>/config
+R_API char *r_xdg_configdir(const char *s) {
+	return xdg ("XDG_CONFIG_HOME", ".config", s);
+}
+
+// XDG_DATA_HOME	User-specific data	~/.var/app/<app-id>/data
+R_API char *r_xdg_datadir(const char *s) {
+	return xdg ("XDG_DATA_HOME", ".local/share", s);
+}
+
+// XDG_CACHE_HOME	Non-essential user-specific data	~/.var/app/<app-id>/cache
+R_API char *r_xdg_cachedir(const char *s) {
+	return xdg ("XDG_CACHE_HOME", ".cache", s);
+}
