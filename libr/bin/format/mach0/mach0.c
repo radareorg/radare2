2af08597f938ecd9a86080b97f1b1d63bd37a9cf
pancake <pancake@nopcode.org>
2022-10-04 15:51:40 +0200

    Expose the macho reloc fixups and use internal buffer for parsing ##bin
    * 7x faster, remove 4 globals, fix uaf, stack exhaustion and race condition bugs

--- libr/bin/format/mach0/mach0.c
+++ libr/bin/format/mach0/mach0.c
@@ -4622,46 +4622,46 @@ struct MACH0_(mach_header) *MACH0_(get_hdr)(RBuffer *buf) {
 	return macho_hdr;
 }
 
-void MACH0_(iterate_chained_fixups)(struct MACH0_(obj_t) *bin, ut64 limit_start, ut64 limit_end, ut32 event_mask, RFixupCallback callback, void * context) {
+void MACH0_(iterate_chained_fixups)(struct MACH0_(obj_t) *mo, ut64 limit_start, ut64 limit_end, ut32 event_mask, RFixupCallback callback, void * context) {
 	int i = 0;
-	for (; i < bin->nsegs && i < bin->segs_count; i++) {
-		if (!bin->chained_starts[i]) {
+	for (; i < mo->nsegs && i < mo->segs_count; i++) {
+		if (!mo->chained_starts[i]) {
 			continue;
 		}
-		int page_size = bin->chained_starts[i]->page_size;
+		int page_size = mo->chained_starts[i]->page_size;
 		if (page_size < 1) {
 			page_size = 4096;
 		}
-		ut64 start = bin->segs[i].fileoff;
-		ut64 end = start + bin->segs[i].filesize;
+		ut64 start = mo->segs[i].fileoff;
+		ut64 end = start + mo->segs[i].filesize;
 		if (end >= limit_start && start <= limit_end) {
 			ut64 page_idx = (R_MAX (start, limit_start) - start) / page_size;
 			ut64 page_end_idx = (R_MIN (limit_end, end) - start) / page_size;
 			for (; page_idx <= page_end_idx; page_idx++) {
-				if (page_idx >= bin->chained_starts[i]->page_count) {
+				if (page_idx >= mo->chained_starts[i]->page_count) {
 					break;
 				}
-				if (!bin->chained_starts[i]->page_start) {
+				if (!mo->chained_starts[i]->page_start) {
 					break;
 				}
-				ut16 page_start = bin->chained_starts[i]->page_start[page_idx];
+				ut16 page_start = mo->chained_starts[i]->page_start[page_idx];
 				if (page_start == DYLD_CHAINED_PTR_START_NONE) {
 					continue;
 				}
 				ut64 cursor = start + page_idx * page_size + page_start;
 				while (cursor < limit_end && cursor < end) {
 					ut8 tmp[8];
-					bool previous_rebasing = bin->rebasing_buffer;
-					bin->rebasing_buffer = true;
-					if (r_buf_read_at (bin->b, cursor, tmp, 8) != 8) {
-						bin->rebasing_buffer = previous_rebasing;
+					bool previous_rebasing = mo->rebasing_buffer;
+					mo->rebasing_buffer = true;
+					if (r_buf_read_at (mo->b, cursor, tmp, 8) != 8) {
+						mo->rebasing_buffer = previous_rebasing;
 						break;
 					}
-					bin->rebasing_buffer = previous_rebasing;
+					mo->rebasing_buffer = previous_rebasing;
 					ut64 raw_ptr = r_read_le64 (tmp);
 					ut64 ptr_value = raw_ptr;
 					ut64 delta, stride, addend;
-					ut16 pointer_format = bin->chained_starts[i]->pointer_format;
+					ut16 pointer_format = mo->chained_starts[i]->pointer_format;
 					RFixupEvent event = R_FIXUP_EVENT_NONE;
 					ut8 key = 0, addr_div = 0;
 					ut16 diversity = 0;
@@ -4693,7 +4693,7 @@ void MACH0_(iterate_chained_fixups)(struct MACH0_(obj_t) *bin, ut64 limit_start,
 									(struct dyld_chained_ptr_arm64e_auth_rebase *) &raw_ptr;
 							event = R_FIXUP_EVENT_REBASE_AUTH;
 							delta = p->next;
-							ptr_value = p->target + bin->baddr;
+							ptr_value = p->target + mo->baddr;
 							key = p->key;
 							addr_div = p->addrDiv;
 							diversity = p->diversity;
@@ -4732,7 +4732,7 @@ void MACH0_(iterate_chained_fixups)(struct MACH0_(obj_t) *bin, ut64 limit_start,
 										(struct dyld_chained_ptr_arm64e_auth_rebase *) &raw_ptr;
 								event = R_FIXUP_EVENT_REBASE_AUTH;
 								delta = p->next;
-								ptr_value = p->target + bin->baddr;
+								ptr_value = p->target + mo->baddr;
 								key = p->key;
 								addr_div = p->addrDiv;
 								diversity = p->diversity;
@@ -4741,7 +4741,7 @@ void MACH0_(iterate_chained_fixups)(struct MACH0_(obj_t) *bin, ut64 limit_start,
 									(struct dyld_chained_ptr_arm64e_rebase *) &raw_ptr;
 								event = R_FIXUP_EVENT_REBASE;
 								delta = p->next;
-								ptr_value = bin->baddr + (((ut64)p->high8 << 56) | p->target);
+								ptr_value = mo->baddr + (((ut64)p->high8 << 56) | p->target);
 							}
 						}
 						}
@@ -4764,7 +4764,7 @@ void MACH0_(iterate_chained_fixups)(struct MACH0_(obj_t) *bin, ut64 limit_start,
 							delta = p->next;
 							ptr_value = ((ut64)p->high8 << 56) | p->target;
 							if (pointer_format == DYLD_CHAINED_PTR_64_OFFSET) {
-								ptr_value += bin->baddr;
+								ptr_value += mo->baddr;
 							}
 						}
 						}
@@ -4780,7 +4780,7 @@ void MACH0_(iterate_chained_fixups)(struct MACH0_(obj_t) *bin, ut64 limit_start,
 							RFixupBindEventDetails event_details;
 
 							event_details.type = event;
-							event_details.bin = bin;
+							event_details.bin = mo;
 							event_details.offset = cursor;
 							event_details.raw_ptr = raw_ptr;
 							event_details.ordinal = ordinal;
@@ -4793,7 +4793,7 @@ void MACH0_(iterate_chained_fixups)(struct MACH0_(obj_t) *bin, ut64 limit_start,
 							RFixupBindAuthEventDetails event_details;
 
 							event_details.type = event;
-							event_details.bin = bin;
+							event_details.bin = mo;
 							event_details.offset = cursor;
 							event_details.raw_ptr = raw_ptr;
 							event_details.ordinal = ordinal;
@@ -4808,7 +4808,7 @@ void MACH0_(iterate_chained_fixups)(struct MACH0_(obj_t) *bin, ut64 limit_start,
 							RFixupRebaseEventDetails event_details;
 
 							event_details.type = event;
-							event_details.bin = bin;
+							event_details.bin = mo;
 							event_details.offset = cursor;
 							event_details.raw_ptr = raw_ptr;
 							event_details.ptr_value = ptr_value;
@@ -4820,7 +4820,7 @@ void MACH0_(iterate_chained_fixups)(struct MACH0_(obj_t) *bin, ut64 limit_start,
 							RFixupRebaseAuthEventDetails event_details;
 
 							event_details.type = event;
-							event_details.bin = bin;
+							event_details.bin = mo;
 							event_details.offset = cursor;
 							event_details.raw_ptr = raw_ptr;
 							event_details.ptr_value = ptr_value;
