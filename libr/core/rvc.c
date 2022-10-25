fbd2e5a5750bf324d8122c40bd09f9743ac1fbcf
pancake <pancake@nopcode.org>
2022-10-25 13:59:33 +0200

    Fix last covs

--- libr/core/rvc.c
+++ libr/core/rvc.c
@@ -1201,13 +1201,13 @@ R_API bool r_vc_git_commit(Rvc *vc, const char *_message, const char *author, co
 		return false;
 	}
 	if (R_STR_ISEMPTY (message)) {
-		free (message);
+		R_FREE (message);
 		if (!r_cons_is_interactive ()) {
 			message = strdup ("default message");
 		}
 	}
 	if (R_STR_ISEMPTY (message)) {
-		free (message);
+		R_FREE (message);
 		char *epath = r_str_escape (vc->path);
 		if (epath) {
 			// XXX ensure CWD in the same line?
