da4b23ec287f565564cf9c4a080a4919bf4e382b
pancake <pancake@nopcode.org>
2022-11-03 20:10:20 +0100

    Move jdh8 from asm/anal to arch ##arch

--- libr/arch/p/arch_jdh8.c
+++ libr/arch/p/arch_jdh8.c
@@ -2,9 +2,9 @@
 
 #include <r_anal.h>
 #include <r_lib.h>
-#include "../../asm/arch/jdh8/jdh8dis.c"
+#include "../i/jdh8/jdh8dis.c"
 
-static int jdh8_op(RAnal *anal, RAnalOp *op, ut64 addr, const ut8 *buf, int len, RAnalOpMask mask) {
+static int decode(RArchConfig *cfg, RAnalOp *op, ut64 addr, const ut8 *buf, int len, ut32 mask, void *user) {
 	int dlen = 0;
 	char *o = jdh8Disass (buf, len, &dlen);
 	op->mnemonic = strdup (o);
@@ -13,20 +13,20 @@ static int jdh8_op(RAnal *anal, RAnalOp *op, ut64 addr, const ut8 *buf, int len,
 	return dlen;
 }
 
-RAnalPlugin r_anal_plugin_jdh8 = {
+RArchPlugin r_arch_plugin_jdh8 = {
 	.name = "jdh8",
 	.desc = "jdh-8 toy architecture",
 	.license = "LGPL3",
 	.arch = "jdh8",
 	.bits = 16,
 	.endian = R_SYS_ENDIAN_LITTLE,
-	.op = &jdh8_op,
+	.decode = &decode,
 };
 
 #ifndef R2_PLUGIN_INCORE
 R_API RLibStruct radare_plugin = {
-	.type = R_LIB_TYPE_ANAL,
-	.data = &r_anal_plugin_jdh8,
+	.type = R_LIB_TYPE_ARCH,
+	.data = &r_arch_plugin_jdh8,
 	.version = R2_VERSION
 };
 #endif
--- sys/zig.sh
+++ sys/zig.sh
@@ -2,6 +2,9 @@
 
 # cross compile to linux-arm64
 #export CC="zig cc -target aarch64-linux"
-export CC="zig cc"
-./configure
+export CC="zig cc -std=c11"
+export LD="zig cc"
+export AR="zig ar"
+export RANLIB="zig ranlib"
+./configure --without-threads
 make -j
