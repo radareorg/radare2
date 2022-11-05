ab8e25f3887a431f1386c73cd9a51c88c9953165
pancake <pancake@nopcode.org>
2022-11-05 22:10:00 +0100

    ejej

--- sys/zig.sh
+++ sys/zig.sh
@@ -5,15 +5,18 @@ ARG="$1"
 TARGETS="
 	arm64-linux
 	amd64-linux
+	riscv64-linux
 	native
 "
 
 if [ -z "$ARG" ]; then
 	echo "Usage: sys/zig.sh [cpu] [os]"
-	echo "CPUS: x86_64 arm aarch64 i386 riscv64 wasm32"
-	echo "OSS: linux macos windows freebsd netbsd dragonfly UEFI"
+	echo "$TARGETS"
+#echo "CPUS: x86_64 arm aarch64 i386 riscv64 wasm32"
+#echo "OSS: linux macos windows freebsd netbsd dragonfly UEFI"
 	exit 1
 fi
+TARGET="$ARG"
 #export CC="zig cc -std=c11"
 #export LD="zig cc"
 
@@ -22,20 +25,24 @@ arm64-linux|aarch64-linux)
 	TARGET="aarch64-linux"
 	;;
 amd64-linux|x86_64-linux|x64-linux)
-	TARGET="aarch64-linux"
+	TARGET="x86_64-linux"
+	;;
+riscv-linux|riscv64-linux)
+	TARGET="riscv64-linux"
 	;;
 amd64-freebsd|x86_64-freebsd|x64-freebsd)
 	TARGET="x86_64-freebsd"
+	;;
+amd64-netbsd)
+	TARGET="x86_64-netbsd"
 	;;
 native)
 	TARGET=""
 	;;
 *)
-	echo "Unknown target"
+	echo "Unknown target $TARGET"
 	;;
 esac
-
-TARGET="riscv64-windows"
 
 export CFLAGS="-Oz"
 export LDFLAGS="-flto"
@@ -47,10 +54,13 @@ else
 	export CC="zig cc -target ${TARGET}"
 	export LD="zig cc -target ${TARGET}"
 fi
+# nollvm
+#export CC="$CC -fstage1 -fno-LLVM"
+#export LD="$LD -fstage1 -fno-LLVM"
 export EXT_SO=so
 export AR="zig ar"
 export RANLIB="zig ranlib"
 rm -f libr/include/r_version.h
 # ./configure --host=aarch64-gnu-linux --with-ostype=linux
-./configure --with-ostype=gnulinux
+./configure --with-ostype=gnulinux || exit 1
 time make -j
