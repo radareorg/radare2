44dc0d849965d728c6beb46e8b5325749f3cb6f9
pancake <pancake@nopcode.org>
2022-11-05 21:52:03 +0100

    Add support for crosscompilation with zig cc ##build

--- sys/zig.sh
+++ sys/zig.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-export CC="zig cc -std=c11"
-export LD="zig cc"
+#export CC="zig cc -std=c11"
+#export LD="zig cc"
 
 # cross compile to linux-arm64
-#export CC="zig cc -std=c11 -target aarch64-linux"
-#export LD="zig cc -target aarch64-linux"
-
+export CC="zig cc -target aarch64-linux -Oz"
+export LD="zig cc -target aarch64-linux -Oz"
+export EXT_SO=so
 export AR="zig ar"
 export RANLIB="zig ranlib"
-./configure
-time make -j
+# ./configure --host=aarch64-gnu-linux --with-ostype=linux
+rm -f libr/include/r_version.h
+./configure --with-ostype=gnulinux
+time make -j EXT_SO=so
