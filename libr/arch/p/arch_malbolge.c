2b50c94284638465c5b52d904b8c698325705c4d
pancake <pancake@nopcode.org>
2022-11-13 01:22:32 +0100

    Move anal.esil into the new esil ##esil

---
+++ libr/arch/p/arch_malbolge.c
@@ -0,0 +1,81 @@
+/* radare - LGPL - Copyright 2015-2022 - condret */
+
+#include <r_anal.h>
+#include <r_lib.h>
+
+static bool mal_decode(RArchSession *s, RAnalOp *op, RArchEncodeMask mask) {
+	if (op->size < 1 || !op->bytes) {
+		return false;
+	}
+	const ut8 *data = op->bytes;
+	switch ((data[0] + op->addr) % 94) {
+	case 4:
+		op->type = R_ANAL_OP_TYPE_UJMP;
+		if (mask & R_ARCH_OP_MASK_DISASM) {
+			op->mnemonic = strdup ("jmp [d]");
+		}
+		break;
+	case 5:
+		op->type = R_ANAL_OP_TYPE_IO;
+		if (mask & R_ARCH_OP_MASK_DISASM) {
+			op->mnemonic = strdup ("out a");
+		}
+		break;
+	case 23:
+		op->type = R_ANAL_OP_TYPE_IO;
+		if (mask & R_ARCH_OP_MASK_DISASM) {
+			op->mnemonic = strdup ("in a");
+		}
+		break;
+	case 39:
+		op->type = R_ANAL_OP_TYPE_ROR;
+		op->type2 = R_ANAL_OP_TYPE_LOAD;
+		if (mask & R_ARCH_OP_MASK_DISASM) {
+			op->mnemonic = strdup ("rotr [d], mov a, [d]");
+		}
+		break;
+	case 40:
+		op->type = R_ANAL_OP_TYPE_LOAD;
+		if (mask & R_ARCH_OP_MASK_DISASM) {
+			op->mnemonic = strdup ("mov d, [d]");
+		}
+		break;
+	case 62:
+		op->type = R_ANAL_OP_TYPE_XOR;
+		op->type2 = R_ANAL_OP_TYPE_LOAD;
+		if (mask & R_ARCH_OP_MASK_DISASM) {
+			op->mnemonic = strdup ("crz [d], a, mov a, [d]");
+		}
+		break;
+	case 81:
+		op->type = R_ANAL_OP_TYPE_TRAP;
+		if (mask & R_ARCH_OP_MASK_DISASM) {
+			op->mnemonic = strdup ("end");
+		}
+		break;
+	default:
+		op->type = R_ANAL_OP_TYPE_NOP;
+		if (mask & R_ARCH_OP_MASK_DISASM) {
+			op->mnemonic = strdup ("nop");
+		}
+	}
+	op->size = 1;
+	return true;
+}
+
+RArchPlugin r_arch_plugin_malbolge = {
+	.name = "malbolge",
+	.desc = "Malbolge analysis plugin",
+	.arch = "malbolge",
+	.license = "LGPL3",
+	.bits = 32,
+	.decode = &mal_decode,
+};
+
+#ifndef R2_PLUGIN_INCORE
+R_API RLibStruct radare_plugin = {
+	.type = R_LIB_TYPE_ANAL,
+	.data = &r_arch_plugin_malbolge,
+	.version = R2_VERSION
+};
+#endif
