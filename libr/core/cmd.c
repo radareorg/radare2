e5679d21ad991c9f557df30c9fec7cafb3e5f949
pancake <pancake@nopcode.org>
2022-11-25 01:51:14 +0100

    Fix null deref on main router inside the r2 shell

--- libr/core/cmd.c
+++ libr/core/cmd.c
@@ -2471,7 +2471,7 @@ static int __runMain(RMainCallback cb, const char *arg) {
 	char *a = r_str_trim_dup (arg);
 	int argc = 0;
 	char **args = r_str_argv (a, &argc);
-	int res = cb (argc, (const char **)args);
+	int res = cb? cb (argc, (const char **)args): -1;
 	free (args);
 	free (a);
 	return res;
