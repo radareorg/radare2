ad8a1ce3a7d809a819a1710db6979974cfb45b1a
condret <condr3t@protonmail.com>
2022-11-26 14:22:05 +0100

    Some more improvements to esil_dfg ##anal

--- test/unit/test_esil_dfg_filter.c
+++ test/unit/test_esil_dfg_filter.c
@@ -18,29 +18,29 @@ bool test_filter_regs(void) {
 	const ut64 al = r_reg_getv (anal->reg, "al");
 	r_reg_setv (anal->reg, "eax", 0);
 
-	REsilDFG *dfg = r_esil_dfg_expr (anal, NULL, "0x9090,ax,:=,0xff,ah,:=");
+	RAnalEsilDFG *dfg = r_anal_esil_dfg_expr (anal, NULL, "0x9090,ax,:=,0xff,ah,:=");
 
 	// filter for ax register
-	RStrBuf *filtered_expr = r_esil_dfg_filter (dfg, "ax");
+	RStrBuf *filtered_expr = r_anal_esil_dfg_filter (dfg, "ax");
 	r_esil_parse (esil, r_strbuf_get (filtered_expr));
 	const ut64 filtered_ax = r_reg_getv (anal->reg, "ax");
 	r_strbuf_free (filtered_expr);
 	r_reg_setv (anal->reg, "eax", 0);
 
 	// filter for ah register
-	filtered_expr = r_esil_dfg_filter (dfg, "ah");
+	filtered_expr = r_anal_esil_dfg_filter (dfg, "ah");
 	r_esil_parse (esil, r_strbuf_get (filtered_expr));
 	const ut64 filtered_ah = r_reg_getv (anal->reg, "ah");
 	r_strbuf_free (filtered_expr);
 	r_reg_setv (anal->reg, "eax", 0);
 
 	// filter for al register
-	filtered_expr = r_esil_dfg_filter (dfg, "al");
+	filtered_expr = r_anal_esil_dfg_filter (dfg, "al");
 	r_esil_parse (esil, r_strbuf_get (filtered_expr));
 	const ut64 filtered_al = r_reg_getv (anal->reg, "al");
 	r_strbuf_free (filtered_expr);
 
-	r_esil_dfg_free (dfg);
+	r_anal_esil_dfg_free (dfg);
 	r_esil_free (esil);
 	r_anal_free (anal);
 
@@ -56,12 +56,12 @@ bool test_lemon_const_folder(void) {
 	r_anal_set_bits (anal, 32);
 	r_anal_set_reg_profile (anal, NULL);
 
-	REsilDFG *dfg = r_esil_dfg_expr (anal, NULL, "4,!,3,ebx,:=,!,1,+,eax,:=");
-	r_esil_dfg_fold_const (anal, dfg);
-	RStrBuf *filtered = r_esil_dfg_filter (dfg, "eax");
+	RAnalEsilDFG *dfg = r_anal_esil_dfg_expr (anal, NULL, "4,!,3,ebx,:=,!,1,+,eax,:=");
+	r_anal_esil_dfg_fold_const (anal, dfg);
+	RStrBuf *filtered = r_anal_esil_dfg_filter (dfg, "eax");
 	const bool cmp_result = !strcmp (r_strbuf_get(filtered), "0x2,eax,:=");
 	r_strbuf_free (filtered);
-	r_esil_dfg_free (dfg);
+	r_anal_esil_dfg_free (dfg);
 	r_anal_free (anal);
 
 	mu_assert_true (cmp_result, "esil dfg const folding is broken");
