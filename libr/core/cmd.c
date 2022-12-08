0f45f2cadd57403edec447600e758be54931bf3d
pancake <pancake@nopcode.org>
2022-12-08 19:59:27 +0100

    Add 'js' and 'js:' commands, as well as improve help for #!? ##lang

--- libr/core/cmd.c
+++ libr/core/cmd.c
@@ -101,6 +101,8 @@ static RCoreHelpMessage help_msg_j = {
 	"Usage:", "j[:o]in", "run command with json facilities or join two files",
 	"j:", "?e", "run '?e' command and show the result stats in json",
 	"ji:", "[cmd]", "run command and indent it as json like (cmd~{})",
+	"js", " [expr]", "run given javascript expression",
+	"js:", "[file]", "interpret javascript file",
 	"join", " f1 f2", "join the contents of two files",
 	NULL
 };
@@ -1567,6 +1569,20 @@ static int cmd_join(void *data, const char *input) { // "join"
 		r_cons_printf ("%s\n", indented);
 		free (indented);
 		free (res);
+		return R_CMD_RC_SUCCESS;
+	}
+	if (input[0] == 's') {
+		if (input[1] == ':') {
+			r_core_cmdf (core, ". %s", input + 1);
+		} else if (input[1] == ' ') {
+			if (r_lang_use (core->lang, "mujs")) {
+				r_lang_run (core->lang, input + 1, -1);
+			} else {
+				r_core_cmdf (core, "#!pipe node -e '%s'", input + 1);
+			}
+		} else {
+			r_core_cmd_help_match (core, help_msg_j, "js", false);
+		}
 		return R_CMD_RC_SUCCESS;
 	}
 	if (input[0] == ':') {
--- libr/core/cmd_hash.c
+++ libr/core/cmd_hash.c
@@ -1,4 +1,4 @@
-/* radare - LGPL - Copyright 2009-2021 - pancake, nibble */
+/* radare - LGPL - Copyright 2009-2022 - pancake, nibble */
 
 #include <r_core.h>
 
@@ -9,12 +9,10 @@ const char *help_msg_hash[] = {
 	"#!?", "", "show this help message",
 	"#!?j", "", "list all available interpreters in JSON",
 	"#!?q", "", "list all available lang plugin names (See Ll?)",
-	"#!v?", "", "show vlang script example",
-	"#!python?", "", "show python script example",
-	"#!python", "", "run python commandline",
-	"#!python", " foo.py", "run foo.py python script (same as '. foo.py')",
-	//"#!python <<EOF        get python code until 'EOF' mark\n"
-	"#!python", " arg0 a1 <<q", "set arg0 and arg1 and read until 'q'",
+	"#!<lang>?", "", "show help for <lang> (v, python, mujs, ..)",
+	"#!<lang>", " [file]", "interpret the given file with lang plugin",
+	"#!<lang>", "", "enter interactive prompt for given language plugin",
+	"#!pipe", " node -e 'console.log(123)''", "run program with arguments inside an r2pipe environment",
 	NULL
 };
 
