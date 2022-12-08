58dbbc7fb40858e329a5c557da584e3da459f861
pancake <pancake@nowsecure.com>
2022-12-08 16:03:31 +0100

    Move anal.xap into the arch ##arch

--- libr/anal/p/xap.mk
+++
@@ -1,9 +0,0 @@
-OBJ_XAP=anal_xap.o
-
-STATIC_OBJ+=${OBJ_XAP}
-TARGET_XAP=anal_xap.${EXT_SO}
-
-ALL_TARGETS+=${TARGET_XAP}
-
-${TARGET_XAP}: ${OBJ_XAP}
-	${CC} $(call libname,anal_xap) ${CFLAGS} -o anal_xap.${EXT_SO} ${OBJ_XAP}
