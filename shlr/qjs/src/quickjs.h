e9a6be1767ad8a7d9591dd435c1ab35f001c8829
pancake <pancake@nopcode.org>
2022-12-10 16:47:10 +0100

    ll

--- shlr/qjs/src/quickjs.h
+++ shlr/qjs/src/quickjs.h
@@ -41,7 +41,7 @@ extern "C" {
 #else
 #define js_likely(x)     (x)
 #define js_unlikely(x)   (x)
-#define js_force_inline  __forceinline
+#define js_force_inline  inline
 #define __js_printf_like(a, b)
 #endif
 
