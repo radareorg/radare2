9a9177736d4a0b9dcbc2695880864ae46956bfaf
pancake <pancake@nopcode.org>
2022-12-11 02:19:16 +0100

    Initial import of the interactive QJS repl ##lang

--- libr/lang/p/qjs.c
+++ libr/lang/p/qjs.c
@@ -88,20 +88,9 @@ static JSValue r2cmdj(JSContext *ctx, JSValueConst this_val, int argc, JSValueCo
 	return ns;
 }
 
-static const JSCFunctionListEntry js_r2_funcs[] = {
-	JS_CFUNC_DEF ("cmd", 1, r2cmd),
-	JS_CFUNC_DEF ("cmdj", 1, r2cmdj),
-	JS_CFUNC_DEF ("log", 1, r2log),
-	JS_CFUNC_DEF ("error", 1, r2error),
-};
 
-static int js_r2_init(JSContext *ctx, JSModuleDef *m) {
-	return JS_SetModuleExportList (ctx, m, js_r2_funcs, countof (js_r2_funcs));
-}
 
-static JSContext *JS_NewCustomContext(JSRuntime *rt);
-
-static JSValue js_print(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
+static JSValue js_write(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
 	int i;
 	const char *str;
 	size_t len;
@@ -117,8 +106,83 @@ static JSValue js_print(JSContext *ctx, JSValueConst this_val, int argc, JSValue
 		fwrite (str, 1, len, stdout);
 		JS_FreeCString (ctx, str);
 	}
-	putchar ('\n');
 	return JS_UNDEFINED;
+}
+
+static JSValue js_flush(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
+	r_cons_flush ();
+	fflush (stdout);
+	return JS_UNDEFINED;
+}
+
+static JSValue js_print(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
+	JSValue a = js_write (ctx, this_val, argc, argv);
+	putchar ('\n');
+	return a;
+}
+
+static JSValue js_os_read_write(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv, int magic) {
+	int fd;
+	uint64_t pos, len;
+	size_t size;
+	ssize_t ret;
+	uint8_t *buf;
+
+	if (JS_ToInt32(ctx, &fd, argv[0]))
+		return JS_EXCEPTION;
+	if (JS_ToIndex(ctx, &pos, argv[2]))
+		return JS_EXCEPTION;
+	if (JS_ToIndex(ctx, &len, argv[3]))
+		return JS_EXCEPTION;
+	buf = JS_GetArrayBuffer(ctx, &size, argv[1]);
+	if (!buf)
+		return JS_EXCEPTION;
+	if (pos + len > size)
+		return JS_ThrowRangeError(ctx, "read/write array buffer overflow");
+	if (magic)
+		ret = write(fd, buf + pos, len);
+	else
+		ret = read(fd, buf + pos, len);
+	return JS_NewInt64(ctx, ret);
+}
+
+
+static const JSCFunctionListEntry js_r2_funcs[] = {
+	JS_CFUNC_DEF ("cmd", 1, r2cmd),
+	JS_CFUNC_DEF ("cmdj", 1, r2cmdj),
+	JS_CFUNC_DEF ("log", 1, r2log),
+	JS_CFUNC_DEF ("error", 1, r2error),
+};
+
+static int js_r2_init(JSContext *ctx, JSModuleDef *m) {
+	return JS_SetModuleExportList (ctx, m, js_r2_funcs, countof (js_r2_funcs));
+}
+
+
+static const JSCFunctionListEntry js_os_funcs[] = {
+	JS_CFUNC_MAGIC_DEF("read", 4, js_os_read_write, 0 ),
+	JS_CFUNC_MAGIC_DEF("write", 4, js_os_read_write, 1 ),
+#if 0
+	JS_CFUNC_MAGIC_DEF("setReadHandler", 2, js_os_setReadHandler, 0 ),
+	JS_CFUNC_DEF("setTimeout", 2, js_os_setTimeout ),
+	JS_CFUNC_DEF("clearTimeout", 1, js_os_clearTimeout ),
+#endif
+	// JS_CFUNC_DEF("open", 2, js_os_open ),
+	// OS_FLAG(O_RDONLY),
+};
+
+static int js_os_init(JSContext *ctx, JSModuleDef *m) {
+	return JS_SetModuleExportList(ctx, m, js_os_funcs,
+			countof(js_os_funcs));
+}
+
+JSModuleDef *js_init_module_os(JSContext *ctx, const char *module_name) {
+	JSModuleDef *m;
+	m = JS_NewCModule(ctx, module_name, js_os_init);
+	if (!m)
+		return NULL;
+	JS_AddModuleExportList(ctx, m, js_os_funcs, countof(js_os_funcs));
+	return m;
 }
 
 static void register_helpers(JSContext *ctx) {
@@ -134,20 +198,25 @@ static void register_helpers(JSContext *ctx) {
 		return;
 	}
 	js_r2_init (ctx, m);
+	js_init_module_os (ctx, "os");
 	JS_AddModuleExportList (ctx, m, js_r2_funcs, countof (js_r2_funcs));
 	JSValue global_obj = JS_GetGlobalObject (ctx);
-	JS_SetPropertyStr (ctx, global_obj, "print",
-		JS_NewCFunction (ctx, js_print, "print", 1));
 	JS_SetPropertyStr (ctx, global_obj, "r2cmd",
-		JS_NewCFunction (ctx, r2cmd, "r2cmd", 1));
+			JS_NewCFunction (ctx, r2cmd, "r2cmd", 1));
 	JS_SetPropertyStr (ctx, global_obj, "r2log",
-		JS_NewCFunction (ctx, r2log, "r2log", 1));
+			JS_NewCFunction (ctx, r2log, "r2log", 1));
+	JS_SetPropertyStr (ctx, global_obj, "write",
+			JS_NewCFunction (ctx, js_write, "write", 1));
+	JS_SetPropertyStr (ctx, global_obj, "flush", // write + newline
+			JS_NewCFunction (ctx, js_flush, "flush", 1));
+	JS_SetPropertyStr (ctx, global_obj, "print", // write + newline
+			JS_NewCFunction (ctx, js_print, "print", 1));
 	eval (ctx, "function dir(x) {"
-		"console.log(JSON.stringify(x).replace(/,/g,',\\n '));"
-		"for (var i in x) {console.log(i);}}");
+			"console.log(JSON.stringify(x).replace(/,/g,',\\n '));"
+			"for (var i in x) {console.log(i);}}");
 	eval(ctx, "var console = { log:print, error:print, debug:print };");
 	eval(ctx, "var r2 = { log:r2log, cmd:r2cmd, cmdj:(x)=>JSON.parse(r2cmd(x))};");
-	eval(ctx, "var global = globalThis;");
+	eval(ctx, "var global = globalThis; var G = globalThis;");
 }
 
 static JSContext *JS_NewCustomContext(JSRuntime *rt) {
@@ -178,6 +247,8 @@ static void eval_jobs(JSContext *ctx) {
 }
 
 static bool eval(JSContext *ctx, const char *code) {
+	// set raw console
+			r_cons_set_raw (true);
 	JSValue v = JS_Eval (ctx, code, strlen (code), "-", 0);
 	if (JS_IsException (v)) {
 		js_std_dump_error (ctx);
@@ -185,6 +256,8 @@ static bool eval(JSContext *ctx, const char *code) {
 		js_dump_obj (ctx, stderr, e);
 	}
 	eval_jobs (ctx);
+			r_cons_set_raw (false);
+	// restore raw console
 	JS_FreeValue (ctx, v);
 	return true;
 }
