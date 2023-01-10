2b12410fe903c922b45acdee03e59157f30b50ac
pancake <pancake@nopcode.org>
2023-01-10 19:07:47 +0100

    Use more R_LOG in RDebug

--- libr/debug/p/native/xnu/reg/darwin-arm64.h
+++ libr/debug/p/native/xnu/reg/darwin-arm64.h
@@ -19,36 +19,38 @@ return strdup (
 "=SF	nf\n"
 "=OF	vf\n"
 "=CF	cf\n"
-"gpr	x0	.64	0	0\n" // r14
-"gpr	x1	.64	8	0\n" // r14
-"gpr	x2	.64	16	0\n" // r14
-"gpr	x3	.64	24	0\n" // r14
-"gpr	x4	.64	32	0\n" // r14
-"gpr	x5	.64	40	0\n" // r14
-"gpr	x6	.64	48	0\n" // r14
-"gpr	x7	.64	56	0\n" // r14
-"gpr	x8	.64	64	0\n" // r14
-"gpr	x9	.64	72	0\n" // r14
-"gpr	x10	.64	80	0\n" // r14
-"gpr	x11	.64	88	0\n" // r14
-"gpr	x12	.64	96	0\n" // r14
-"gpr	x13	.64	104	0\n" // r14
-"gpr	x14	.64	112	0\n" // r14
-"gpr	x15	.64	120	0\n" // r14
-"gpr	x16	.64	128	0\n" // r14
-"gpr	x17	.64	136	0\n" // r14
-"gpr	x18	.64	144	0\n" // r14
-"gpr	x19	.64	152	0\n" // r14
-"gpr	x20	.64	160	0\n" // r14
-"gpr	x21	.64	168	0\n" // r14
-"gpr	x22	.64	176	0\n" // r14
-"gpr	x23	.64	184	0\n" // r14
-"gpr	x24	.64	192	0\n" // r14
-"gpr	x25	.64	200	0\n" // r14
-"gpr	x26	.64	208	0\n" // r14
-"gpr	x27	.64	216	0\n" // r14
-"gpr	x28	.64	224	0\n" // r14
-"gpr	x29	.64	232	0\n" // r14
+"gpr	x0	.64	0	0\n"
+"gpr	x1	.64	8	0\n"
+"gpr	x2	.64	16	0\n"
+"gpr	x3	.64	24	0\n"
+"gpr	x4	.64	32	0\n"
+"gpr	x5	.64	40	0\n"
+"gpr	x6	.64	48	0\n"
+"gpr	x7	.64	56	0\n"
+"gpr	x8	.64	64	0\n"
+"gpr	x9	.64	72	0\n"
+"gpr	x10	.64	80	0\n"
+"gpr	x11	.64	88	0\n"
+"gpr	x12	.64	96	0\n"
+"gpr	x13	.64	104	0\n"
+"gpr	x14	.64	112	0\n"
+"gpr	x15	.64	120	0\n"
+"gpr	x16	.64	128	0\n"
+"gpr	x17	.64	136	0\n"
+"gpr	x18	.64	144	0\n"
+"gpr	x19	.64	152	0\n"
+"gpr	x20	.64	160	0\n"
+"gpr	x21	.64	168	0\n"
+"gpr	x22	.64	176	0\n"
+"gpr	x23	.64	184	0\n"
+"gpr	x24	.64	192	0\n"
+"gpr	x25	.64	200	0\n"
+"gpr	x26	.64	208	0\n"
+"gpr	x27	.64	216	0\n"
+"gpr	x28	.64	224	0\n"
+"gpr	x29	.64	232	0\n"
+// "gpr	x30	.64	240	0\n"
+// "gpr	x31	.64	248	0\n" // LR
 // words (32bit lower part of x
 "gpr	w0	.32	0	0\n" // w0
 "gpr	w1	.32	8	0\n" // w0
@@ -85,7 +87,7 @@ return strdup (
 // TODO complete w list ...
 // special registers
 "gpr	fp	.64	232	0\n" // FP
-"gpr	lr	.64	236	0\n" // LR
+"gpr	lr	.64	240	0\n" // LR X30
 "gpr	sp	.64	248	0\n" // SP
 "gpr	pc	.64	256	0\n" // PC
 "gpr	pstate	.64	264	0   _____tfiae_____________j__qvczn\n" // x0
