12c6271e7e11c5f0a083f7fdc86d565f3706ada1
pancake <pancake@nopcode.org>
2023-01-10 19:07:00 +0100

    Reestructure lib dependencies, add lang.asm plugin ##lang

--- libr/libs.mk
+++ libr/libs.mk
@@ -3,15 +3,14 @@
 ifeq ($(LIBS0),)
 LIBS0=util
 LIBS1=socket reg cons magic bp config crypto
-LIBS2=syscall lang search flag arch esil io
+LIBS2=syscall search flag arch esil io
 LIBS3=asm fs anal
-LIBS4=egg bin
-LIBS5=debug
+LIBS4=lang bin
+LIBS5=debug egg
 LIBS6=core
-LIBS7=core
-LIBS8=main
+LIBS7=main
 
-LIBS=$(LIBS0) $(LIBS1) $(LIBS2) $(LIBS3) $(LIBS4) $(LIBS5) $(LIBS6) $(LIBS7) $(LIBS8)
+LIBS=$(LIBS0) $(LIBS1) $(LIBS2) $(LIBS3) $(LIBS4) $(LIBS5) $(LIBS6) $(LIBS7)
 endif
 
 .PHONY: $(LIBS)
