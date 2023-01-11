a740698298113d3b4c18c45a9543616d27cd7218
pancake <pancake@nopcode.org>
2023-01-12 00:24:04 +0100

    Improve r2slides with title, colors and 2 column mode ##slides:xa

--- libr/core/vslides.c
+++ libr/core/vslides.c
@@ -1,8 +1,8 @@
-/* radare - LGPL - Copyright 2021 - pancake */
+/* radare - LGPL - Copyright 2021-2023 - pancake */
 
 #include <r_core.h>
 
-static char *keys[256] = {0};
+static R_TH_LOCAL char *keys[256] = {0};
 
 static void clearkeys(void) {
 	int i;
@@ -17,7 +17,7 @@ static int findpage(RList *list, const char *pagename) {
 	RListIter *iter;
 	char *s;
 	r_list_foreach (list, iter, s) {
-		if (!strncmp (s, "# ", 2)) {
+		if (r_str_startswith (s, "# ")) {
 			count++;
 		} else if (r_str_startswith (s, "--label:")) {
 			const char *label = s + strlen ("--label:");
@@ -39,24 +39,52 @@ static int gotokey(RList *list, int ch, int page) {
 	return page;
 }
 
-static void render(RCore *core, RList *list, int page) {
+static int count_pages(RList *list) {
+	int pages = 0;
+	char *s;
+	RListIter *iter;
+	r_list_foreach (list, iter, s) {
+		if (r_str_startswith (s, "# ")) {
+			pages++;
+		}
+	}
+	return pages;
+}
+
+static void render(RCore *core, RList *list, int mode, int page) {
 	char *s;
 	if (page < 0) {
 		page = 0;
 	}
 	int count = 0;
+	int notch = r_config_get_i (core->config, "scr.notch");
+	if (notch < 1) {
+		notch = 1;
+	}
 	RListIter *iter;
+	RStrBuf *sb = r_strbuf_new (NULL);
 	r_list_foreach (list, iter, s) {
-		if (!strncmp (s, "# ", 2)) {
+		if (r_str_startswith (s, "# ")) {
 			count++;
 		}
 		if (count == page) {
 			if (*s == '`') {
 				char *cmd = r_str_ndup (s + 1, strlen (s) -2);
 				char *res = r_core_cmd_str (core, cmd);
-				r_cons_printf ("%s", res);
+				// r_cons_printf ("%s", res);
+				r_strbuf_append (sb, res);
 				free (res);
 				free (cmd);
+			} else if (r_str_startswith (s, "--color=")) {
+				char *kv = strdup (s + strlen ("--color="));
+				if (*kv) {
+					char *k = r_cons_pal_parse (kv, NULL);
+					r_strbuf_append (sb, k);
+					free (k);
+				} else {
+					r_strbuf_append (sb, Color_RESET);
+				}
+				free (kv);
 			} else if (r_str_startswith (s, "--gotokey:")) {
 				char *kv = strdup (s + strlen ("--gotokey:"));
 				if (kv[0] && kv[1]) {
@@ -73,13 +101,41 @@ static void render(RCore *core, RList *list, int page) {
 
 			} else if (*s == '#') {
 				char *ss = r_str_ss (s, 0, 0);
-				r_cons_printf ("%s\n", ss);
+				r_strbuf_append (sb, ss);
+				// r_cons_printf ("%s\n", ss);
 				free (ss);
 			} else {
-				r_cons_printf ("     %s\n", s);
+				// r_cons_printf ("     %s\n", s);
+				r_strbuf_appendf (sb, "     %s\n", s);
 			}
 		}
 	}
+	char *o = r_strbuf_drain (sb);
+	char *oo = r_str_newf ("%s%s", r_str_pad ('\n', notch), o);
+	free (o);
+	o = oo;
+	int h, w = r_cons_get_size (&h);
+	if (mode == 2) {
+		w /= 2;
+		char *o2 = r_str_ansi_crop (o, 0, 0, w, h);
+		const char *prefix = r_str_pad (' ', w);
+		char *no = r_str_prefix_all (o2, prefix);
+		free (o);
+		free (o2);
+		o = no;
+		r_cons_print (o);
+	} else {
+		char *no = r_str_ansi_crop (o, 0, 0, w, h);
+		r_cons_print (no);
+		free (no);
+	}
+	free (o);
+}
+
+static void render_title(int page, int mode, int total) {
+	r_cons_gotoxy (0, 0);
+	r_cons_printf (Color_BLACK""Color_BGYELLOW""R_CONS_CLEAR_LINE"\r [r2slides] [%s:%d/%d]", 
+			(mode==2)?"pages":"page", page, total);
 }
 
 R_API void r_core_visual_slides(RCore *core, const char *file) {
@@ -97,28 +153,58 @@ R_API void r_core_visual_slides(RCore *core, const char *file) {
 
 	int ch;
 	int page = 1;
+	int mode = 1;
+	r_cons_set_raw (1);
+	r_cons_show_cursor (false);
+	r_cons_enable_mouse (false);
+	int total_pages = count_pages (list);
 	while (having_fun) {
+		if (page > total_pages) {
+			page = total_pages;
+		}
 		clearkeys ();
 		r_cons_clear00 ();
-		render (core, list, page);
+		if (mode == 2) {
+			render (core, list, 2, page + 1);
+		}
+		r_cons_gotoxy (0, 0);
+		render (core, list, 1, page);
+		render_title (page, mode, total_pages);
 		r_cons_flush ();
-		r_cons_enable_mouse (false);
 		ch = r_cons_readchar ();
 		ch = r_cons_arrow_to_hjkl (ch);
 		switch (ch) {
+		case '1':
+			mode = 1;
+			break;
+		case '2':
+			mode = 2;
+			break;
 		case 'q':
 			having_fun = false;
 			break;
 		case ' ':
 		case 'n':
-			page++;
+		case 'P':
+			page+=mode;
 			break;
 		case 'p':
-			page--;
+		case 'N':
+			page-=mode;
 			if (page < 1) {
 				page = 1;
 			}
 			break;
+		case '?':
+			eprintf ("Keys:\n");
+			eprintf (" np = next/prev slide\n");
+			eprintf (" q  = quit the slides\n");
+			eprintf (" e  = open vim to edit the current slide\n");
+			eprintf (" 12 = show 1 or two pages\n");
+			eprintf (" :  = enter command\n");
+			r_cons_any_key (NULL);
+			break;
+		case 'e':
 		case '!':
 			//
 			{
@@ -129,12 +215,14 @@ R_API void r_core_visual_slides(RCore *core, const char *file) {
 					free (data);
 					data = ndata;
 					list = r_str_split_list (data, "\n", 0);
+					total_pages = count_pages (list);
 				}
 			}
 			break;
 		case ':':
 			r_cons_show_cursor (true);
 			r_cons_set_raw (0);
+			r_cons_flush ();
 			while (1) {
 				char cmd[1024];
 				*cmd = 0;
@@ -157,6 +245,8 @@ R_API void r_core_visual_slides(RCore *core, const char *file) {
 			break;
 		}
 	}
+	r_cons_set_raw (0);
+	r_cons_show_cursor (true);
 	r_list_free (list);
 	free (data);
 }
