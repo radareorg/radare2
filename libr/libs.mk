6733031dbc6f7d877b86ba608b4b2b13de11b60b
pancake <pancake@nopcode.org>
2023-01-11 01:05:41 +0100

    Fix another library dependency issue

--- libr/libs.mk
+++ libr/libs.mk
@@ -5,12 +5,13 @@ LIBS0=util
 LIBS1=socket reg cons magic bp config crypto syscall
 LIBS2=search flag arch esil io
 LIBS3=asm fs anal
-LIBS4=lang bin
-LIBS5=debug egg
-LIBS6=core
-LIBS7=main
+LIBS4=bin lang
+LIBS5=egg
+LIBS6=debug
+LIBS7=core
+LIBS8=main
 
-LIBS=$(LIBS0) $(LIBS1) $(LIBS2) $(LIBS3) $(LIBS4) $(LIBS5) $(LIBS6) $(LIBS7)
+LIBS=$(LIBS0) $(LIBS1) $(LIBS2) $(LIBS3) $(LIBS4) $(LIBS5) $(LIBS6) $(LIBS7) $(LIBS8)
 endif
 
 .PHONY: $(LIBS)
