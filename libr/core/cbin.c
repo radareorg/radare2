983d7dab73563a02516192a1fb9f4cbdec53d5c3
pancake <pancake@nopcode.org>
2023-01-22 19:06:05 +0100

    Massage the bottleneck that was making r2dec super slow ##perf

--- libr/core/cbin.c
+++ libr/core/cbin.c
@@ -4363,7 +4363,8 @@ R_API bool r_core_bin_info(RCore *core, int action, PJ *pj, int mode, int va, RC
 	if (filter && filter->name) {
 		name = filter->name;
 	}
-	r_core_bin_export_info (core, R_MODE_SET);
+	// XXX this makes r2dec very slow (25s vs 80s)
+	// r_core_bin_export_info (core, R_MODE_SET);
 
 	// use our internal values for va
 	va = va ? VA_TRUE : VA_FALSE;
