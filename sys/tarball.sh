56379ce71334c51485695d3927f901ec796e30e6
pancake <pancake@nopcode.org>
2023-01-23 17:56:45 +0100

    Fixes for the offline tarball generation ##build

--- sys/tarball.sh
+++ sys/tarball.sh
@@ -11,9 +11,17 @@ V=`./configure -qV`
 git clone . radare2-${V}
 cd radare2-${V}
 ./preconfigure
+git status .
 rm -rf .git
 find * | grep /.git$ | xargs rm -rf
+rm -f .test.c
+rm -f config-user.mk
+rm -f pkgcfg/*.pc
+rm -f libr/include/r_userconf.h
+mkdir shlr/capstone/.git
 cd ..
+rm -f radare2-${V}.tar.xz
 tar cJvf radare2-${V}.tar.xz radare2-${V}
+rm -f radare2-${V}.zip
 zip -r radare2-${V}.zip radare2-${V}
 rm -rf radare2-${V}
