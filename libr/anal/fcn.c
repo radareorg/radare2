b5f8621812adce36a24fc5a9e9306275865cc5be
pancake <pancake@nopcode.org>
2023-02-15 12:37:10 +0100

    Fix null deref spotted in native rv64 analysis

--- libr/anal/fcn.c
+++ libr/anal/fcn.c
@@ -570,6 +570,10 @@ static int fcn_recurse(RAnal *anal, RAnalFunction *fcn, ut64 addr, ut64 len, int
 	} delay = {
 		0
 	};
+	if (anal->cur == NULL) {
+		R_LOG_WARN ("fcn.recurse found anal.cur is nul");
+		return R_ANAL_RET_ERROR;
+	}
 	bool arch_destroys_dst = does_arch_destroys_dst (anal->cur->arch);
 	const bool is_arm = anal->cur->arch && !strncmp (anal->cur->arch, "arm", 3);
 	const bool is_v850 = is_arm ? false: (anal->cur->arch && (!strncmp (anal->cur->arch, "v850", 4) || !strncmp (anal->coreb.cfgGet (anal->coreb.core, "asm.cpu"), "v850", 4)));
