a90ad913aef787d53a6c404f86b97639677b1728
pancake <pancake@nopcode.org>
2023-02-20 22:14:56 +0100

    Remove broken github counters and introduce the sys/counters.sh ##ci

---
+++ sys/counters.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "# Counters"
+
+printf -- "XXX\t"
+git grep XXX libr | wc -l
+printf -- "TODO\t"
+git grep TODO libr | wc -l
+printf -- "GLOBALS\t"
+git grep R_TH_LOCAL libr | grep -v include | wc -l
+
+printf -- "strcpy\t"
+git grep 'strcpy (' libr | wc -l
+printf -- "sprintf\t"
+git grep 'sprintf (' libr | wc -l
