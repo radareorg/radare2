f9e42259f5a68b50c3c36a9deadbf7c9f1452a6c
pancake <pancake@nowsecure.com>
2023-02-25 02:29:14 +0100

    Update gen.py

--- libr/syscall/d/gen.py
+++ libr/syscall/d/gen.py
@@ -11,17 +11,17 @@ try:
 	_output = sys.argv[3]
 	_tmpfile = _input + ".tmp"
 	with open(_input) as lines:
-	res = ""
-	for line in lines:
-		kv = line.rstrip().split("=", 1)
-		if kv[0] == "_":
-			res += line
-		else:
-			vv = kv[1].split(",")
-			res += vv[0] + "." + vv[1] + "=" + kv[0] + "\n"
-			res += line
-	with open(_tmpfile, "w") as file:
-		file.write(res)
+		res = ""
+		for line in lines:
+			kv = line.rstrip().split("=", 1)
+			if kv[0] == "_":
+				res += line
+			else:
+				vv = kv[1].split(",")
+				res += vv[0] + "." + vv[1] + "=" + kv[0] + "\n"
+				res += line
+		with open(_tmpfile, "w") as file:
+			file.write(res)
 	subprocess.call([sdb_exe, _output, "==", _tmpfile])
 except Exception as e:
 	print(e)
