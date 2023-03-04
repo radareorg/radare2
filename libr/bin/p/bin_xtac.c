c16d89d8ddb2be0de97e950f3b5b6a1af5f5fd7e
pancake <pancake@nopcode.org>
2023-03-04 23:01:24 +0100

    Fix two more ooms in wasm and xtac

--- libr/bin/p/bin_xtac.c
+++ libr/bin/p/bin_xtac.c
@@ -285,6 +285,9 @@ static bool r_bin_xtac_read_address_pairs(RBinXtacObj *bin) {
 	const ut32 n_addr_pairs = bin->header->num_of_addr_pairs;
 	const ut32 addr_pair_size = n_addr_pairs * sizeof (X86ArmAddrPair);
 	const ut32 p_addr_pair = bin->header->ptr_to_addr_pairs;
+	if (n_addr_pairs > 0xfffff) {
+		return false;
+	}
 	if (!(bin->address_pairs = R_NEWS0 (X86ArmAddrPair, n_addr_pairs))) {
 		return false;
 	}
