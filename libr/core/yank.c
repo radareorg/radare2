d9e2ec009d1a326ae34b544a28370c234fe29666
Lazula <26179473+Lazula@users.noreply.github.com>
2023-03-08 16:25:42 -0600

    yank.c

--- libr/core/yank.c
+++ libr/core/yank.c
@@ -213,7 +213,6 @@ R_API int r_core_yank_to(RCore *core, const char *_arg) {
 		str[0] = ' ';
 	}
 	if (!str || pos == -1 || len == 0) {
-		eprintf ("Usage: yt [len] [dst-addr]\n");
 		free (arg);
 		return res;
 	}
