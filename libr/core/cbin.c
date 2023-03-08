2cdb1952abc1bfc22d0cd6e9aa188006278db041
pancake <pancake@nopcode.org>
2023-03-08 17:53:04 +0100

    Make aab even faster (39s -> 32s) ##anal

--- libr/core/cbin.c
+++ libr/core/cbin.c
@@ -350,11 +350,8 @@ R_API bool r_core_bin_set_env(RCore *r, RBinFile *binfile) {
 		r_config_set (r->config, "asm.arch", arch);
 		r_config_set_i (r->config, "asm.bits", bits);
 		r_config_set (r->config, "anal.arch", arch);
-		if (info->cpu && *info->cpu) {
-			r_config_set (r->config, "asm.cpu", info->cpu);
-		} else {
-			r_config_set (r->config, "asm.cpu", arch);
-		}
+		const char *cpu = R_STR_ISNOTEMPTY (info->cpu)? info->cpu: arch;
+		r_config_set (r->config, "asm.cpu", cpu);
 		r_asm_use (r->rasm, arch);
 		r_core_bin_info (r, R_CORE_BIN_ACC_ALL, NULL, R_MODE_SET, va, NULL, NULL);
 		r_core_bin_set_cur (r, binfile);
