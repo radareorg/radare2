236f65fe478929df70904b00d53dfb679e9096e9
Princess-Sunset-Shimmer <102773277+Princess-Sunset-Shimmer@users.noreply.github.com>
2023-03-13 01:17:00 -0500

    Update trap-darwin-x86-32.asm

--- binr/preload/trap-darwin-x86-32.asm
+++ binr/preload/trap-darwin-x86-32.asm
@@ -3,9 +3,11 @@
 .bits 32
 
 mov eax, $sys.getpid
-sub esp, 4
+add esp, -0x4
 int 0x80
 mov ebx, $sys.kill
-xchg eax, ebx
-sub esp, 8
+xor eax, ebx
+xor rbx, eax
+xor eax, ebx
+add esp, -0x8
 int 0x80
