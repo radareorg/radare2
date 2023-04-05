8ea488202b19a3587287d69cf7f2bbd017906812
pancake <pancake@nopcode.org>
2023-04-05 11:38:08 +0200

    Aim to fix crosscompilation

--- libr/arch/p/arm_v35.mk
+++ libr/arch/p/arm_v35.mk
@@ -1,12 +1,11 @@
 include ../config.mk
-N=anal_arm_v35
+N=arch_arm_v35
 V35ARM64_HOME=$(LIBR)/arch/p/arm/v35/
 
 include ../arch/p/arm/v35/deps-arm64.mk
 
 OBJ_ARM_V35=p/arm/plugin_v35.o
-# OBJ_ARM_V35+=${V35ARM64_LINK}
-OBJ_ARM_V35+=p/arm/v35/arch-arm64/disassembler/*.o
+OBJ_ARM_V35+=${V35ARM64_LINK}
 # OBJ_ARM_V35 += ../../arch/p/arm/v35/arm64dis.$(EXT_AR)
 # OBJ_ARM_V35+=../../arch/p/arm/v35/armv7dis.$(EXT_AR)
 # assembler should be resolved externally
