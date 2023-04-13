e943a97b261e8c98b5a70e5847c85efe6f7e4aee
Luc Tielen <ltielen@nowsecure.com>
2023-04-13 15:30:43 +0200

    Conditionally parse mach0 start symbols ##bin

--- libr/bin/p/bin_mach0.c
+++ libr/bin/p/bin_mach0.c
@@ -48,6 +48,8 @@ static bool load_buffer(RBinFile *bf, void **bin_obj, RBuffer *buf, ut64 loadadd
 	r_return_val_if_fail (bf && bin_obj && buf, false);
 	struct MACH0_(opts_t) opts;
 	MACH0_(opts_set_default) (&opts, bf);
+	opts.parse_start_symbols = true;
+
 	struct MACH0_(obj_t) *res = MACH0_(new_buf) (buf, &opts);
 	if (res) {
 		if (res->chained_starts) {
