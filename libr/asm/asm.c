9d427f39ab38cada3b5265332091e8cb60c185f9
pancake <pancake@nopcode.org>
2023-04-25 00:14:09 +0200

    Live endian change propagation ##arch

--- libr/asm/asm.c
+++ libr/asm/asm.c
@@ -535,6 +535,7 @@ static int r_asm_assemble(RAsm *a, RAnalOp *op, const char *buf) {
 		//RAnalOp aop;
 		//a->analb.opinit (&aop);
 		ut8 buf[256] = {0};
+		a->analb.anal->arch->cfg->endian = a->config->endian;
 		// XXX we shuold use just RArch and ecur/dcur
 		ret = a->analb.encode (a->analb.anal, a->pc, b, buf, sizeof (buf));
 		if (ret > 0) {
