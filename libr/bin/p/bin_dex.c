62006c0adff051ca584649ba16fb5134d9d7c9b1
pancake <pancake@nopcode.org>
2023-04-26 21:37:09 +0200

    Move the memset to fix the dex regression

--- libr/bin/p/bin_dex.c
+++ libr/bin/p/bin_dex.c
@@ -1607,7 +1607,6 @@ static bool dex_loadcode(RBinFile *bf) {
 	int *methods = NULL;
 	size_t methods_size = 0;
 	int sym_count = 0;
-	memset (&dex->header, 0, sizeof (dex->header));
 	// doublecheck??
 	if (dex->methods_list) {
 		return false;
