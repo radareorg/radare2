d7361366868743ac666ddf27e33f33c9df1305bb
pancake <pancake@nopcode.org>
2023-05-04 18:15:38 +0200

    Fix #17523 - Resolve internal symbols referenced from the PLT ##bin

--- libr/bin/p/bin_elf.inc.c
+++ libr/bin/p/bin_elf.inc.c
@@ -403,11 +403,13 @@ static RList* imports(RBinFile *bf) {
 
 	RBinElfSymbol *import_symbol;
 	r_vector_foreach (import_symbols, import_symbol) {
+		if (import_symbol->is_internal) {
+			continue;
+		}
 		RBinImport *ptr = R_NEW0 (RBinImport);
 		if (!ptr) {
 			break;
 		}
-
 		ptr->name = strdup (import_symbol->name);
 		ptr->bind = import_symbol->bind;
 		ptr->type = import_symbol->type;
