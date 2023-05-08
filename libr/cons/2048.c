46ba4a2a2bc42ab014802e04ae61e4cc33ad5c59
Luc Tielen <ltielen@nowsecure.com>
2023-05-08 20:56:00 +0200

    Remove globals from 2048 ##cons

--- libr/cons/2048.c
+++ libr/cons/2048.c
@@ -3,51 +3,48 @@
 #include <r_cons.h>
 #include <r_th.h>
 
-static R_TH_LOCAL ut8 twok_buf[4][4];
-static R_TH_LOCAL int score = 0;
-static R_TH_LOCAL int moves = 0;
+typedef struct game_state_t {
+	ut8 twok_buf[4][4];
+	int score;
+	int moves;
+} GameState;
 
-static void twok_init(void) {
-	int i, j;
-	score = 0;
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 4; j++) {
-			twok_buf[i][j] = 0;
-		}
-	}
+
+static void twok_init(GameState *state) {
+	memset (state, 0, sizeof (GameState));
 }
 
-static void twok_add(void) {
+static void twok_add(GameState *state) {
 	int i, j;
 	while (true) {
 		i = r_num_rand (4);
 		j = r_num_rand (4);
-		if (!twok_buf[i][j]) {
-			twok_buf[i][j] = 1 + (r_num_rand (10) == 1);
+		if (!state->twok_buf[i][j]) {
+			state->twok_buf[i][j] = 1 + (r_num_rand (10) == 1);
 			break;
 		}
 	}
 }
 
-static bool twok_fin(void) {
+static bool twok_fin(GameState *state) {
 	int i, j;
 	for (i = 0; i < 4; i++) {
 		for (j = 0; j < 4; j++) {
-			if (!twok_buf[i][j]) {
+			if (!state->twok_buf[i][j]) {
 				return true;
 			}
 		}
 	}
 	for (i = 0; i < 4; i++) {
 		for (j = 0; j < 3; j++) {
-			if (twok_buf[i][j] == twok_buf[i][j + 1]) {
+			if (state->twok_buf[i][j] == state->twok_buf[i][j + 1]) {
 				return true;
 			}
 		}
 	}
 	for (i = 0; i < 3; i++) {
 		for (j = 0; j < 4; j++) {
-			if (twok_buf[i][j] == twok_buf[i + 1][j]) {
+			if (state->twok_buf[i][j] == state->twok_buf[i + 1][j]) {
 				return true;
 			}
 		}
@@ -55,13 +52,13 @@ static bool twok_fin(void) {
 	return false;
 }
 
-static void twok_move(int u, int v) {
+static void twok_move(GameState *state, int u, int v) {
 	int i, j, k;
 	int nKI = 0, nKJ = 0, nIK = 0, nJK = 0;
 	int moved = 0;
 	for (k = 0; k < 4; k++) {
 		for (i = 0; i < 4; i++) {
-			for (j = i + 1; j < 4 && !twok_buf[nKJ = u ? k : v ? j : 3 - j][nJK = !u ? k : v ? j : 3 - j]; j++) {
+			for (j = i + 1; j < 4 && !state->twok_buf[nKJ = u ? k : v ? j : 3 - j][nJK = !u ? k : v ? j : 3 - j]; j++) {
 				;
 			}
 			if (j == 4) {
@@ -69,25 +66,25 @@ static void twok_move(int u, int v) {
 			}
 			nKI = u? k: v? i: 3 - i;
 			nIK = !u? k: v? i: 3 - i;
-			if (!twok_buf[nKI][nIK]) {
-				twok_buf[nKI][nIK] = twok_buf[nKJ][nJK];
-				twok_buf[nKJ][nJK] = 0;
+			if (!state->twok_buf[nKI][nIK]) {
+				state->twok_buf[nKI][nIK] = state->twok_buf[nKJ][nJK];
+				state->twok_buf[nKJ][nJK] = 0;
 				--i;
 				moved = 1;
-			} else if (twok_buf[nKI][nIK] == twok_buf[nKJ][nJK]) {
-				score += 1 << ++twok_buf[nKI][nIK];
-				twok_buf[nKJ][nJK] = 0;
+			} else if (state->twok_buf[nKI][nIK] == state->twok_buf[nKJ][nJK]) {
+				state->score += 1 << ++state->twok_buf[nKI][nIK];
+				state->twok_buf[nKJ][nJK] = 0;
 				moved = 1;
 			}
 		}
 	}
 	if (moved) {
-		twok_add ();
-		moves++;
+		twok_add (state);
+		state->moves++;
 	}
 }
 
-static void getval(bool color, char *val0, int i, int x) {
+static void getval(GameState *state, bool color, char *val0, int i, int x) {
 	const char * colorarray[] = {
 		Color_WHITE,
 		Color_RED,
@@ -98,18 +95,18 @@ static void getval(bool color, char *val0, int i, int x) {
 		Color_BLUE,
 		Color_GRAY
 	};
-	if (twok_buf[i][x]) {
+	if (state->twok_buf[i][x]) {
 		if (color) {
-			snprintf (val0, 31, "%s%4d"Color_RESET, colorarray [twok_buf [i][x] % 8 ], 1 << twok_buf[i][x]);
+			snprintf (val0, 31, "%s%4d"Color_RESET, colorarray [state->twok_buf [i][x] % 8 ], 1 << state->twok_buf[i][x]);
 		} else {
-			snprintf (val0, 31, "%4d", 1 << twok_buf[i][x]);
+			snprintf (val0, 31, "%4d", 1 << state->twok_buf[i][x]);
 		}
 	} else {
 		strcpy (val0, "    ");
 	}
 }
 
-static void twok_print(bool color) {
+static void twok_print(GameState *state, bool color) {
 	char val0[32];
 	char val1[32];
 	char val2[32];
@@ -121,10 +118,10 @@ static void twok_print(bool color) {
 		printf ("  +------+------+------+------+\n");
 	}
 	for (i = 0; i < 4; i++) {
-		getval (color, val0, i, 0);
-		getval (color, val1, i, 1);
-		getval (color, val2, i, 2);
-		getval (color, val3, i, 3);
+		getval (state, color, val0, i, 0);
+		getval (state, color, val1, i, 1);
+		getval (state, color, val2, i, 2);
+		getval (state, color, val3, i, 3);
 		if (color) {
 			printf (Color_BBLUE"  |      |      |      |      |\n");
 			printf ("  |"Color_RESET" %s "Color_BBLUE"|"Color_RESET" %s "
@@ -143,40 +140,41 @@ static void twok_print(bool color) {
 	printf ("Hexboard:     'hjkl' and 'q'uit\n");
 	for (i = 0; i < 4; i++) {
 		printf ("  %02x %02x %02x %02x\n",
-			twok_buf[i][0], twok_buf[i][1],
-			twok_buf[i][2], twok_buf[i][3]);
+			state->twok_buf[i][0], state->twok_buf[i][1],
+			state->twok_buf[i][2], state->twok_buf[i][3]);
 	}
 }
 
 R_API void r_cons_2048(bool color) {
+	GameState state;
 	int ch;
 	r_cons_set_raw (1);
-	twok_init ();
-	twok_add ();
-	twok_add ();
-	while (twok_fin ()) {
+	twok_init (&state);
+	twok_add (&state);
+	twok_add (&state);
+	while (twok_fin (&state)) {
 		r_cons_clear00 ();
 		if (color) {
-			r_cons_printf (Color_GREEN"[r2048]"Color_BYELLOW" score: %d   moves: %d\n"Color_RESET, score, moves);
+			r_cons_printf (Color_GREEN"[r2048]"Color_BYELLOW" score: %d   moves: %d\n"Color_RESET, state.score, state.moves);
 		} else {
-			r_cons_printf ("[r2048] score: %d   moves: %d\n", score, moves);
+			r_cons_printf ("[r2048] score: %d   moves: %d\n", state.score, state.moves);
 		}
 		r_cons_flush ();
-		twok_print (color);
+		twok_print (&state, color);
 		ch = r_cons_readchar ();
 		ch = r_cons_arrow_to_hjkl (ch);
 		switch (ch) {
 		case 'h':
-			twok_move (1, 1);
+			twok_move (&state, 1, 1);
 			break;
 		case 'j':
-			twok_move (0, 0);
+			twok_move (&state, 0, 0);
 			break;
 		case 'k':
-			twok_move (0, 1);
+			twok_move (&state, 0, 1);
 			break;
 		case 'l':
-			twok_move (1, 0);
+			twok_move (&state, 1, 0);
 			break;
 		}
 		if (ch < 1 || ch == 'q') {
@@ -184,11 +182,11 @@ R_API void r_cons_2048(bool color) {
 		}
 	}
 	r_cons_clear00 ();
-	r_cons_printf ("[r2048] score: %d\n", score);
+	r_cons_printf ("[r2048] score: %d\n", state.score);
 	r_cons_flush ();
-	twok_print (color);
+	twok_print (&state, color);
 
-	r_cons_printf ("\n  [r2048.score] %d\n", score);
+	r_cons_printf ("\n  [r2048.score] %d\n", state.score);
 	do {
 		ch = r_cons_any_key ("Press 'q' to quit.");
 	} while (ch != 'q' && ch >= 1);
