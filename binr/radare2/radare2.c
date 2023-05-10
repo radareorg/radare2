ab053bff3d659febed471169a5c63e28f6f490c9
pancake <pancake@nopcode.org>
2023-05-10 18:13:51 +0200

    Fix the r2p tool and add a test ##shell

--- binr/radare2/radare2.c
+++ binr/radare2/radare2.c
@@ -59,9 +59,10 @@ static void r2cmd(int in, int out, const char *cmd) {
 	}
 	while (1) {
 		int n = read (in, buf, bufsz);
-		if (n != bufsz) {
+		if (n < 1) {
 			break;
 		}
+		buf[n] = '\0';
 		buf[bufsz - 1] = '\0';
 		int len = strlen ((const char *)buf);
 		n = len;
