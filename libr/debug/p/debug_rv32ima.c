85fac5da024e5ff39c1b5425b3d7968f02082fd2
pancake <pancake@nopcode.org>
2023-05-18 01:05:26 +0200

    Initial import of the rv32ima debug plugin ##debug
    * https://github.com/cnlohr/mini-rv32ima

---
+++ libr/debug/p/debug_rv32ima.c
@@ -0,0 +1,243 @@
+/* radare - LGPL - Copyright 2023 pancake */
+// r2 -a riscv -b 32 -Drv32ima -
+// wx b700000037a10000b711000037803e001300000013000000130000001300000013000000e39420fe
+// ds / drp / pd
+
+#include <r_asm.h>
+#include <r_debug.h>
+
+uint32_t ram_amt = 64*1024*1024;
+
+#define MINIRV32_RAM_IMAGE_OFFSET 0
+#define MINIRV32WARN(x...) printf((x));
+#define MINIRV32_DECORATE static
+#define MINI_RV32_RAM_SIZE ram_amt
+#define MINIRV32_IMPLEMENTATION
+#if 0
+#define MINIRV32_POSTEXEC(pc, ir, retval) { if (retval > 0) { if (fail_on_all_faults) { printf ("FAULT\n"); return 3; } else { retval = HandleException( ir, retval ); } } }
+#define MINIRV32_HANDLE_MEM_STORE_CONTROL( addy, val ) if (HandleControlStore(addy, val)) return val;
+#define MINIRV32_HANDLE_MEM_LOAD_CONTROL( addy, rval ) rval = HandleControlLoad( addy);
+#define MINIRV32_OTHERCSR_WRITE(csrno, value) HandleOtherCSRWrite (image, csrno, value);
+#define MINIRV32_OTHERCSR_READ(csrno, value) value = HandleOtherCSRRead (image, csrno);
+#endif
+#include "./mini-rv32ima.h"
+
+static R_TH_LOCAL int elapsed = 0;
+static R_TH_LOCAL struct MiniRV32IMAState rv32state = {0};
+#if 0
+static bool is_io_rv32ima(RDebug *dbg) {
+	RIODesc *d = dbg->iob.io->desc;
+	if (d && d->plugin && d->plugin->name)
+		if (!strcmp ("rv32ima", d->plugin->name))
+			return true;
+	return false;
+}
+#endif
+
+static bool __rv32ima_step(RDebug *dbg) {
+	ut8 buf[0xffff];
+
+	r_debug_reg_sync (dbg, R_REG_TYPE_GPR, false);
+	ut64 pc = r_debug_reg_get (dbg, "PC");
+	// eprintf ("PC = 0x%" PFMT64x "\n", pc);
+/// XXX. hack to trick vaddr issue
+//pc = 0x100001478;
+	//memset (buf, 0, sizeof (buf));
+	// dbg->iob.read_at (dbg->iob.io, pc, buf, sizeof (buf));
+	dbg->iob.read_at (dbg->iob.io, 0, buf, sizeof (buf));
+	// eprintf ("READ 0x%08"PFMT64x" %02x %02x %02x\n", pc, buf[0], buf[1], buf[2]);
+	elapsed += 1;
+	// uint32_t vProcAddress = pc;
+	rv32state.pc = pc;
+	MiniRV32IMAStep( &rv32state, buf, pc, elapsed, 1);
+	// eprintf ("POST PC 0x%"PFMT64x"\n", rv32state.pc);
+// int32_t MiniRV32IMAStep( &rv32state, buf, uint32_t vProcAddress, uint32_t elapsedUs, int count );
+	return true;
+}
+
+static bool __rv32ima_init(RDebug *dbg) {
+	dbg->swstep = false;
+	dbg->tid = dbg->pid = 1;
+	// aeim
+	// aei
+	return true;
+}
+
+static bool __rv32ima_continue(RDebug *dbg, int pid, int tid, int sig) {
+	R_LOG_TODO ("continue");
+	return true;
+}
+
+static bool __rv32ima_continue_syscall(RDebug *dbg, int pid, int num) {
+	R_LOG_TODO ("rv32ima continue until syscall");
+	return true;
+}
+
+static RDebugReasonType __rv32ima_wait(RDebug *dbg, int pid) {
+	/* do nothing */
+	return R_DEBUG_REASON_NONE;
+}
+
+static bool __rv32ima_attach(RDebug *dbg, int pid) {
+	dbg->swstep = false;
+	eprintf ("OK attach\n");
+	return true;
+#if 0
+	if (!is_io_rv32ima (dbg))
+		return false;
+#endif
+#if 0
+	RIOBdescbg *o;
+	o = dbg->iob.io->desc->data;
+	eprintf ("base = %llx\n", o->bfvm->base);
+	eprintf ("screen = %llx\n", o->bfvm->screen);
+	eprintf ("input = %llx\n", o->bfvm->input);
+#endif
+	return true;
+}
+
+static bool __rv32ima_detach(RDebug *dbg, int pid) {
+	// reset vm?
+	return true;
+}
+
+static char *__rv32ima_reg_profile(RDebug *dbg) {
+	const char *p = "=PC	pc\n"
+				 "=A0	a0\n"
+				 "=A1	a1\n"
+				 "=A2	a2\n"
+				 "=A3	a3\n"
+				 "=A4	a4\n"
+				 "=A5	a5\n"
+				 "=A6	a6\n"
+				 "=A7	a7\n"
+				 "=R0	a0\n"
+				 "=R1	a1\n"
+				 "=SP	sp\n" // ABI: stack pointer
+				 "=LR	ra\n" // ABI: return address
+				 "=BP	s0\n" // ABI: frame pointer
+				 "=SN	a7\n" // ABI: syscall numer
+				 "gpr	pc	.32	0	0\n"
+				 // RV32I regs (ABI names)
+				 // From user-Level ISA Specification, section 2.1
+				 // "zero" has been left out as it ignores writes and always reads as zero
+				 "gpr	ra	.32	4	0\n" // =x1
+				 "gpr	sp	.32	8	0\n" // =x2
+				 "gpr	gp	.32	12	0\n" // =x3
+				 "gpr	tp	.32	16	0\n" // =x4
+				 "gpr	t0	.32	20	0\n" // =x5
+				 "gpr	t1	.32	24	0\n" // =x6
+				 "gpr	t2	.32	28	0\n" // =x7
+				 "gpr	s0	.32	32	0\n" // =x8
+				 "gpr	s1	.32	36	0\n" // =x9
+				 "gpr	a0	.32	40	0\n" // =x10
+				 "gpr	a1	.32	44	0\n" // =x11
+				 "gpr	a2	.32	48	0\n" // =x12
+				 "gpr	a3	.32	52	0\n" // =x13
+				 "gpr	a4	.32	56	0\n" // =x14
+				 "gpr	a5	.32	60	0\n" // =x15
+				 "gpr	a6	.32	64	0\n" // =x16
+				 "gpr	a7	.32	68	0\n" // =x17
+				 "gpr	s2	.32	72	0\n" // =x18
+				 "gpr	s3	.32	76	0\n" // =x19
+				 "gpr	s4	.32	80	0\n" // =x20
+				 "gpr	s5	.32	84	0\n" // =x21
+				 "gpr	s6	.32	88	0\n" // =x22
+				 "gpr	s7	.32	92	0\n" // =x23
+				 "gpr	s8	.32	96	0\n" // =x24
+				 "gpr	s9	.32	100	0\n" // =x25
+				 "gpr	s10	.32	104	0\n" // =x26
+				 "gpr	s11	.32	108	0\n" // =x27
+				 "gpr	t3	.32	112	0\n" // =x28
+				 "gpr	t4	.32	116	0\n" // =x29
+				 "gpr	t5	.32	120	0\n" // =x30
+				 "gpr	t6	.32	124	0\n" // =x31
+#if 0
+				 // RV32F/D regs (ABI names)
+				 // From user-Level ISA Specification, section 8.1 and 9.1
+				 "flg	nx	.1	3072	0\n"
+				 "flg	uf	.1	3073	0\n"
+				 "flg	of	.1	3074	0\n"
+				 "flg	dz	.1	3075	0\n"
+				 "flg	nv	.1	3076	0\n"
+				 "flg	frm	.3	3077	0\n"
+#endif
+				 ;
+	return strdup (p);
+}
+
+static int __rv32ima_breakpoint(RBreakpoint *bp, RBreakpointItem *b, bool set) {
+	//r_io_system (dbg->iob.io, "db");
+	return false;
+}
+
+static bool __rv32ima_kill(RDebug *dbg, int pid, int tid, int sig) {
+	// TODO: ESIL reset
+	return true;
+}
+
+static int __rv32ima_stop(RDebug *dbg) {
+	eprintf ("ESIL: stop\n");
+	return true;
+}
+
+// static bool __reg_write(RDebug *dbg, int type, ut8 *buf, int size) {
+static bool __reg_write(RDebug *dbg, int type, const ut8* buf, int size) {
+	if (type != R_REG_TYPE_GPR || size < 4) {
+		return false;
+	}
+	int sz = 128; // sizeof (rv32state.regs);
+	/* do nothing */
+	ut8 *bytes = r_reg_get_bytes (dbg->reg, type, &sz);
+	sz = sizeof (rv32state.regs);
+	memcpy (&(rv32state.pc), buf, 4);
+	memcpy (&(rv32state.regs), buf + 4, R_MIN (size, sz));
+	// memcpy (buf, &rv32state.regs, R_MIN (size, sz));
+	free (bytes);
+	return true;
+}
+
+static bool __reg_read(RDebug *dbg, int type, ut8 *buf, int size) {
+	if (type != R_REG_TYPE_GPR || size < 1) {
+		return false;
+	}
+	dbg->swstep = false;
+	int sz = sizeof (rv32state.regs);
+	/* do nothing */
+	ut8 *bytes = r_reg_get_bytes (dbg->reg, type, &sz);
+	memcpy (buf, &rv32state.pc, 4);
+	memcpy (buf + 4, &rv32state.regs, R_MIN (size, sz));
+	free (bytes);
+	return true;
+}
+
+RDebugPlugin r_debug_plugin_rv32ima = {
+	.name = "rv32ima",
+	.author = "pancake",
+	// .description = "experimental riscv32ima emulator",
+	.license = "MIT",
+	.arch = "riscv",
+	.bits = R_SYS_BITS_32,
+	.init = __rv32ima_init,
+	.step = __rv32ima_step,
+	.cont = __rv32ima_continue,
+	.contsc = __rv32ima_continue_syscall,
+	.attach = &__rv32ima_attach,
+	.detach = &__rv32ima_detach,
+	.wait = &__rv32ima_wait,
+	.stop = __rv32ima_stop,
+	.kill = __rv32ima_kill,
+	.breakpoint = __rv32ima_breakpoint,
+ 	.reg_profile = __rv32ima_reg_profile,
+	.reg_read = __reg_read,
+	.reg_write = __reg_write,
+};
+
+#ifndef R2_PLUGIN_INCORE
+R_API RLibStruct radare_plugin = {
+	.type = R_LIB_TYPE_DBG,
+	.data = &r_debug_plugin_rv32ima,
+	.version = R2_VERSION
+};
+#endif
+
---
+++ libr/debug/p/mini-rv32ima.h
@@ -0,0 +1,553 @@
+// Copyright 2022 Charles Lohr, you may use this file or any portions herein under any of the BSD, MIT, or CC0 licenses.
+
+#ifndef _MINI_RV32IMAH_H
+#define _MINI_RV32IMAH_H
+
+/**
+    To use mini-rv32ima.h for the bare minimum, the following:
+
+	#define MINI_RV32_RAM_SIZE ram_amt
+	#define MINIRV32_IMPLEMENTATION
+
+	#include "mini-rv32ima.h"
+
+	Though, that's not _that_ interesting. You probably want I/O!
+
+
+	Notes:
+		* There is a dedicated CLNT at 0x10000000.
+		* There is free MMIO from there to 0x12000000.
+		* You can put things like a UART, or whatever there.
+		* Feel free to override any of the functionality with macros.
+*/
+
+#ifndef MINIRV32WARN
+	#define MINIRV32WARN( x... );
+#endif
+
+#ifndef MINIRV32_DECORATE
+	#define MINIRV32_DECORATE static
+#endif
+
+#ifndef MINIRV32_RAM_IMAGE_OFFSET
+	#define MINIRV32_RAM_IMAGE_OFFSET  0x80000000
+#endif
+
+#ifndef MINIRV32_POSTEXEC
+	#define MINIRV32_POSTEXEC(...);
+#endif
+
+#ifndef MINIRV32_HANDLE_MEM_STORE_CONTROL
+	#define MINIRV32_HANDLE_MEM_STORE_CONTROL(...);
+#endif
+
+#ifndef MINIRV32_HANDLE_MEM_LOAD_CONTROL
+	#define MINIRV32_HANDLE_MEM_LOAD_CONTROL(...);
+#endif
+
+#ifndef MINIRV32_OTHERCSR_WRITE
+	#define MINIRV32_OTHERCSR_WRITE(...);
+#endif
+
+#ifndef MINIRV32_OTHERCSR_READ
+	#define MINIRV32_OTHERCSR_READ(...);
+#endif
+
+#ifndef MINIRV32_CUSTOM_MEMORY_BUS
+	#define MINIRV32_STORE4( ofs, val ) *(uint32_t*)(image + ofs) = val
+	#define MINIRV32_STORE2( ofs, val ) *(uint16_t*)(image + ofs) = val
+	#define MINIRV32_STORE1( ofs, val ) *(uint8_t*)(image + ofs) = val
+	#define MINIRV32_LOAD4( ofs ) *(uint32_t*)(image + ofs)
+	#define MINIRV32_LOAD2( ofs ) *(uint16_t*)(image + ofs)
+	#define MINIRV32_LOAD1( ofs ) *(uint8_t*)(image + ofs)
+	#define MINIRV32_LOAD2_SIGNED( ofs ) *(int16_t*)(image + ofs)
+	#define MINIRV32_LOAD1_SIGNED( ofs ) *(int8_t*)(image + ofs)
+#endif
+
+// As a note: We quouple-ify these, because in HLSL, we will be operating with
+// uint4's.  We are going to uint4 data to/from system RAM.
+//
+// We're going to try to keep the full processor state to 12 x uint4.
+struct MiniRV32IMAState
+{
+	uint32_t regs[32];
+
+	uint32_t pc;
+	uint32_t mstatus;
+	uint32_t cyclel;
+	uint32_t cycleh;
+
+	uint32_t timerl;
+	uint32_t timerh;
+	uint32_t timermatchl;
+	uint32_t timermatchh;
+
+	uint32_t mscratch;
+	uint32_t mtvec;
+	uint32_t mie;
+	uint32_t mip;
+
+	uint32_t mepc;
+	uint32_t mtval;
+	uint32_t mcause;
+
+	// Note: only a few bits are used.  (Machine = 3, User = 0)
+	// Bits 0..1 = privilege.
+	// Bit 2 = WFI (Wait for interrupt)
+	// Bit 3+ = Load/Store reservation LSBs.
+	uint32_t extraflags;
+};
+
+#ifndef MINIRV32_STEPPROTO
+MINIRV32_DECORATE int32_t MiniRV32IMAStep( struct MiniRV32IMAState * state, uint8_t * image, uint32_t vProcAddress, uint32_t elapsedUs, int count );
+#endif
+
+#ifdef MINIRV32_IMPLEMENTATION
+
+#ifndef MINIRV32_CUSTOM_INTERNALS
+#define CSR( x ) state->x
+#define SETCSR( x, val ) { state->x = val; }
+#define REG( x ) state->regs[x]
+#define REGSET( x, val ) { state->regs[x] = val; }
+#endif
+
+#ifndef MINIRV32_STEPPROTO
+MINIRV32_DECORATE int32_t MiniRV32IMAStep( struct MiniRV32IMAState * state, uint8_t * image, uint32_t vProcAddress, uint32_t elapsedUs, int count )
+#else
+MINIRV32_STEPPROTO
+#endif
+{
+	uint32_t new_timer = CSR( timerl ) + elapsedUs;
+	if( new_timer < CSR( timerl ) ) CSR( timerh )++;
+	CSR( timerl ) = new_timer;
+
+	// Handle Timer interrupt.
+	if( ( CSR( timerh ) > CSR( timermatchh ) || ( CSR( timerh ) == CSR( timermatchh ) && CSR( timerl ) > CSR( timermatchl ) ) ) && ( CSR( timermatchh ) || CSR( timermatchl ) ) )
+	{
+		CSR( extraflags ) &= ~4; // Clear WFI
+		CSR( mip ) |= 1<<7; //MTIP of MIP // https://stackoverflow.com/a/61916199/2926815  Fire interrupt.
+	}
+	else
+		CSR( mip ) &= ~(1<<7);
+
+	// If WFI, don't run processor.
+	if( CSR( extraflags ) & 4 )
+		return 1;
+
+	uint32_t trap = 0;
+	uint32_t rval = 0;
+	uint32_t pc = CSR( pc );
+	uint32_t cycle = CSR( cyclel );
+
+	int icount;
+	if( ( CSR( mip ) & (1<<7) ) && ( CSR( mie ) & (1<<7) /*mtie*/ ) && ( CSR( mstatus ) & 0x8 /*mie*/) )
+	{
+		// Timer interrupt.
+		trap = 0x80000007;
+		pc -= 4;
+	}
+	else // No timer interrupt?  Execute a bunch of instructions.
+	for (icount = 0; icount < count; icount++)
+	{
+		uint32_t ir = 0;
+		rval = 0;
+		cycle++;
+		uint32_t ofs_pc = pc - MINIRV32_RAM_IMAGE_OFFSET;
+
+		if( ofs_pc >= MINI_RV32_RAM_SIZE )
+		{
+			trap = 1 + 1;  // Handle access violation on instruction read.
+			break;
+		}
+		else if( ofs_pc & 3 )
+		{
+			trap = 1 + 0;  //Handle PC-misaligned access
+			break;
+		}
+		else
+		{
+			ir = MINIRV32_LOAD4( ofs_pc );
+			uint32_t rdid = (ir >> 7) & 0x1f;
+
+			switch( ir & 0x7f )
+			{
+				case 0x37: // LUI (0b0110111)
+					rval = ( ir & 0xfffff000 );
+					break;
+				case 0x17: // AUIPC (0b0010111)
+					rval = pc + ( ir & 0xfffff000 );
+					break;
+				case 0x6F: // JAL (0b1101111)
+				{
+					int32_t reladdy = ((ir & 0x80000000)>>11) | ((ir & 0x7fe00000)>>20) | ((ir & 0x00100000)>>9) | ((ir&0x000ff000));
+					if( reladdy & 0x00100000 ) reladdy |= 0xffe00000; // Sign extension.
+					rval = pc + 4;
+					pc = pc + reladdy - 4;
+					break;
+				}
+				case 0x67: // JALR (0b1100111)
+				{
+					uint32_t imm = ir >> 20;
+					int32_t imm_se = imm | (( imm & 0x800 )?0xfffff000:0);
+					rval = pc + 4;
+					pc = ( (REG( (ir >> 15) & 0x1f ) + imm_se) & ~1) - 4;
+					break;
+				}
+				case 0x63: // Branch (0b1100011)
+				{
+					uint32_t immm4 = ((ir & 0xf00)>>7) | ((ir & 0x7e000000)>>20) | ((ir & 0x80) << 4) | ((ir >> 31)<<12);
+					if( immm4 & 0x1000 ) immm4 |= 0xffffe000;
+					int32_t rs1 = REG((ir >> 15) & 0x1f);
+					int32_t rs2 = REG((ir >> 20) & 0x1f);
+					immm4 = pc + immm4 - 4;
+					rdid = 0;
+					switch( ( ir >> 12 ) & 0x7 )
+					{
+						// BEQ, BNE, BLT, BGE, BLTU, BGEU
+						case 0: if( rs1 == rs2 ) pc = immm4; break;
+						case 1: if( rs1 != rs2 ) pc = immm4; break;
+						case 4: if( rs1 < rs2 ) pc = immm4; break;
+						case 5: if( rs1 >= rs2 ) pc = immm4; break; //BGE
+						case 6: if( (uint32_t)rs1 < (uint32_t)rs2 ) pc = immm4; break;   //BLTU
+						case 7: if( (uint32_t)rs1 >= (uint32_t)rs2 ) pc = immm4; break;  //BGEU
+						default: trap = (2+1);
+					}
+					break;
+				}
+				case 0x03: // Load (0b0000011)
+				{
+					uint32_t rs1 = REG((ir >> 15) & 0x1f);
+					uint32_t imm = ir >> 20;
+					int32_t imm_se = imm | (( imm & 0x800 )?0xfffff000:0);
+					uint32_t rsval = rs1 + imm_se;
+
+					rsval -= MINIRV32_RAM_IMAGE_OFFSET;
+					if( rsval >= MINI_RV32_RAM_SIZE-3 )
+					{
+						rsval += MINIRV32_RAM_IMAGE_OFFSET;
+						if( rsval >= 0x10000000 && rsval < 0x12000000 )  // UART, CLNT
+						{
+							if( rsval == 0x1100bffc ) // https://chromitem-soc.readthedocs.io/en/latest/clint.html
+								rval = CSR( timerh );
+							else if( rsval == 0x1100bff8 )
+								rval = CSR( timerl );
+							else
+								MINIRV32_HANDLE_MEM_LOAD_CONTROL( rsval, rval );
+						}
+						else
+						{
+							trap = (5+1);
+							rval = rsval;
+						}
+					}
+					else
+					{
+						switch( ( ir >> 12 ) & 0x7 )
+						{
+							//LB, LH, LW, LBU, LHU
+							case 0: rval = MINIRV32_LOAD1_SIGNED( rsval ); break;
+							case 1: rval = MINIRV32_LOAD2_SIGNED( rsval ); break;
+							case 2: rval = MINIRV32_LOAD4( rsval ); break;
+							case 4: rval = MINIRV32_LOAD1( rsval ); break;
+							case 5: rval = MINIRV32_LOAD2( rsval ); break;
+							default: trap = (2+1);
+						}
+					}
+					break;
+				}
+				case 0x23: // Store 0b0100011
+				{
+					uint32_t rs1 = REG((ir >> 15) & 0x1f);
+					uint32_t rs2 = REG((ir >> 20) & 0x1f);
+					uint32_t addy = ( ( ir >> 7 ) & 0x1f ) | ( ( ir & 0xfe000000 ) >> 20 );
+					if( addy & 0x800 ) addy |= 0xfffff000;
+					addy += rs1 - MINIRV32_RAM_IMAGE_OFFSET;
+					rdid = 0;
+
+					if( addy >= MINI_RV32_RAM_SIZE-3 )
+					{
+						addy += MINIRV32_RAM_IMAGE_OFFSET;
+						if( addy >= 0x10000000 && addy < 0x12000000 )
+						{
+							// Should be stuff like SYSCON, 8250, CLNT
+							if( addy == 0x11004004 ) //CLNT
+								CSR( timermatchh ) = rs2;
+							else if( addy == 0x11004000 ) //CLNT
+								CSR( timermatchl ) = rs2;
+							else if( addy == 0x11100000 ) //SYSCON (reboot, poweroff, etc.)
+							{
+								SETCSR( pc, pc + 4 );
+								return rs2; // NOTE: PC will be PC of Syscon.
+							}
+							else
+								MINIRV32_HANDLE_MEM_STORE_CONTROL( addy, rs2 );
+						}
+						else
+						{
+							trap = (7+1); // Store access fault.
+							rval = addy;
+						}
+					}
+					else
+					{
+						switch( ( ir >> 12 ) & 0x7 )
+						{
+							//SB, SH, SW
+							case 0: MINIRV32_STORE1( addy, rs2 ); break;
+							case 1: MINIRV32_STORE2( addy, rs2 ); break;
+							case 2: MINIRV32_STORE4( addy, rs2 ); break;
+							default: trap = (2+1);
+						}
+					}
+					break;
+				}
+				case 0x13: // Op-immediate 0b0010011
+				case 0x33: // Op           0b0110011
+				{
+					uint32_t imm = ir >> 20;
+					imm = imm | (( imm & 0x800 )?0xfffff000:0);
+					uint32_t rs1 = REG((ir >> 15) & 0x1f);
+					uint32_t is_reg = !!( ir & 0x20 );
+					uint32_t rs2 = is_reg ? REG(imm & 0x1f) : imm;
+
+					if( is_reg && ( ir & 0x02000000 ) )
+					{
+						switch( (ir>>12)&7 ) //0x02000000 = RV32M
+						{
+							case 0: rval = rs1 * rs2; break; // MUL
+#ifndef CUSTOM_MULH // If compiling on a system that doesn't natively, or via libgcc support 64-bit math.
+							case 1: rval = ((int64_t)((int32_t)rs1) * (int64_t)((int32_t)rs2)) >> 32; break; // MULH
+							case 2: rval = ((int64_t)((int32_t)rs1) * (uint64_t)rs2) >> 32; break; // MULHSU
+							case 3: rval = ((uint64_t)rs1 * (uint64_t)rs2) >> 32; break; // MULHU
+#else
+							CUSTOM_MULH
+#endif
+							case 4: if( rs2 == 0 ) rval = -1; else rval = ((int32_t)rs1 == INT32_MIN && (int32_t)rs2 == -1) ? rs1 : ((int32_t)rs1 / (int32_t)rs2); break; // DIV
+							case 5: if( rs2 == 0 ) rval = 0xffffffff; else rval = rs1 / rs2; break; // DIVU
+							case 6: if( rs2 == 0 ) rval = rs1; else rval = ((int32_t)rs1 == INT32_MIN && (int32_t)rs2 == -1) ? 0 : ((uint32_t)((int32_t)rs1 % (int32_t)rs2)); break; // REM
+							case 7: if( rs2 == 0 ) rval = rs1; else rval = rs1 % rs2; break; // REMU
+						}
+					}
+					else
+					{
+						switch( (ir>>12)&7 ) // These could be either op-immediate or op commands.  Be careful.
+						{
+							case 0: rval = (is_reg && (ir & 0x40000000))? (rs1 - rs2) : (rs1 + rs2); break;
+							case 1: rval = rs1 << (rs2 & 0x1F); break;
+							case 2: rval = (int32_t)rs1 < (int32_t)rs2; break;
+							case 3: rval = rs1 < rs2; break;
+							case 4: rval = rs1 ^ rs2; break;
+							case 5: rval = (ir & 0x40000000 ) ? ( ((int32_t)rs1) >> (rs2 & 0x1F) ) : ( rs1 >> (rs2 & 0x1F) ); break;
+							case 6: rval = rs1 | rs2; break;
+							case 7: rval = rs1 & rs2; break;
+						}
+					}
+					break;
+				}
+				case 0x0f: // 0b0001111
+					rdid = 0;   // fencetype = (ir >> 12) & 0b111; We ignore fences in this impl.
+					break;
+				case 0x73: // Zifencei+Zicsr  (0b1110011)
+				{
+					uint32_t csrno = ir >> 20;
+					uint32_t microop = ( ir >> 12 ) & 0x7;
+					if( (microop & 3) ) // It's a Zicsr function.
+					{
+						int rs1imm = (ir >> 15) & 0x1f;
+						uint32_t rs1 = REG(rs1imm);
+						uint32_t writeval = rs1;
+
+						// https://raw.githubusercontent.com/riscv/virtual-memory/main/specs/663-Svpbmt.pdf
+						// Generally, support for Zicsr
+						switch( csrno )
+						{
+						case 0x340: rval = CSR( mscratch ); break;
+						case 0x305: rval = CSR( mtvec ); break;
+						case 0x304: rval = CSR( mie ); break;
+						case 0xC00: rval = cycle; break;
+						case 0x344: rval = CSR( mip ); break;
+						case 0x341: rval = CSR( mepc ); break;
+						case 0x300: rval = CSR( mstatus ); break; //mstatus
+						case 0x342: rval = CSR( mcause ); break;
+						case 0x343: rval = CSR( mtval ); break;
+						case 0xf11: rval = 0xff0ff0ff; break; //mvendorid
+						case 0x301: rval = 0x40401101; break; //misa (XLEN=32, IMA+X)
+						//case 0x3B0: rval = 0; break; //pmpaddr0
+						//case 0x3a0: rval = 0; break; //pmpcfg0
+						//case 0xf12: rval = 0x00000000; break; //marchid
+						//case 0xf13: rval = 0x00000000; break; //mimpid
+						//case 0xf14: rval = 0x00000000; break; //mhartid
+						default:
+							MINIRV32_OTHERCSR_READ( csrno, rval );
+							break;
+						}
+
+						switch( microop )
+						{
+							case 1: writeval = rs1; break;  			//CSRRW
+							case 2: writeval = rval | rs1; break;		//CSRRS
+							case 3: writeval = rval & ~rs1; break;		//CSRRC
+							case 5: writeval = rs1imm; break;			//CSRRWI
+							case 6: writeval = rval | rs1imm; break;	//CSRRSI
+							case 7: writeval = rval & ~rs1imm; break;	//CSRRCI
+						}
+
+						switch( csrno )
+						{
+						case 0x340: SETCSR( mscratch, writeval ); break;
+						case 0x305: SETCSR( mtvec, writeval ); break;
+						case 0x304: SETCSR( mie, writeval ); break;
+						case 0x344: SETCSR( mip, writeval ); break;
+						case 0x341: SETCSR( mepc, writeval ); break;
+						case 0x300: SETCSR( mstatus, writeval ); break; //mstatus
+						case 0x342: SETCSR( mcause, writeval ); break;
+						case 0x343: SETCSR( mtval, writeval ); break;
+						//case 0x3a0: break; //pmpcfg0
+						//case 0x3B0: break; //pmpaddr0
+						//case 0xf11: break; //mvendorid
+						//case 0xf12: break; //marchid
+						//case 0xf13: break; //mimpid
+						//case 0xf14: break; //mhartid
+						//case 0x301: break; //misa
+						default:
+							MINIRV32_OTHERCSR_WRITE( csrno, writeval );
+							break;
+						}
+					}
+					else if( microop == 0x0 ) // "SYSTEM" 0b000
+					{
+						rdid = 0;
+						if( csrno == 0x105 ) //WFI (Wait for interrupts)
+						{
+							CSR( mstatus ) |= 8;    //Enable interrupts
+							CSR( extraflags ) |= 4; //Infor environment we want to go to sleep.
+							SETCSR( pc, pc + 4 );
+							return 1;
+						}
+						else if( ( ( csrno & 0xff ) == 0x02 ) )  // MRET
+						{
+							//https://raw.githubusercontent.com/riscv/virtual-memory/main/specs/663-Svpbmt.pdf
+							//Table 7.6. MRET then in mstatus/mstatush sets MPV=0, MPP=0, MIE=MPIE, and MPIE=1. La
+							// Should also update mstatus to reflect correct mode.
+							uint32_t startmstatus = CSR( mstatus );
+							uint32_t startextraflags = CSR( extraflags );
+							SETCSR( mstatus , (( startmstatus & 0x80) >> 4) | ((startextraflags&3) << 11) | 0x80 );
+							SETCSR( extraflags, (startextraflags & ~3) | ((startmstatus >> 11) & 3) );
+							pc = CSR( mepc ) -4;
+						}
+						else
+						{
+							switch( csrno )
+							{
+							case 0: trap = ( CSR( extraflags ) & 3) ? (11+1) : (8+1); break; // ECALL; 8 = "Environment call from U-mode"; 11 = "Environment call from M-mode"
+							case 1:	trap = (3+1); break; // EBREAK 3 = "Breakpoint"
+							default: trap = (2+1); break; // Illegal opcode.
+							}
+						}
+					}
+					else
+						trap = (2+1); 				// Note micrrop 0b100 == undefined.
+					break;
+				}
+				case 0x2f: // RV32A (0b00101111)
+				{
+					uint32_t rs1 = REG((ir >> 15) & 0x1f);
+					uint32_t rs2 = REG((ir >> 20) & 0x1f);
+					uint32_t irmid = ( ir>>27 ) & 0x1f;
+
+					rs1 -= MINIRV32_RAM_IMAGE_OFFSET;
+
+					// We don't implement load/store from UART or CLNT with RV32A here.
+
+					if( rs1 >= MINI_RV32_RAM_SIZE-3 )
+					{
+						trap = (7+1); //Store/AMO access fault
+						rval = rs1 + MINIRV32_RAM_IMAGE_OFFSET;
+					}
+					else
+					{
+						rval = MINIRV32_LOAD4( rs1 );
+
+						// Referenced a little bit of https://github.com/franzflasch/riscv_em/blob/master/src/core/core.c
+						uint32_t dowrite = 1;
+						switch( irmid )
+						{
+							case 2: //LR.W (0b00010)
+								dowrite = 0;
+								CSR( extraflags ) = (CSR( extraflags ) & 0x07) | (rs1<<3);
+								break;
+							case 3:  //SC.W (0b00011) (Make sure we have a slot, and, it's valid)
+								rval = ( CSR( extraflags ) >> 3 != ( rs1 & 0x1fffffff ) );  // Validate that our reservation slot is OK.
+								dowrite = !rval; // Only write if slot is valid.
+								break;
+							case 1: break; //AMOSWAP.W (0b00001)
+							case 0: rs2 += rval; break; //AMOADD.W (0b00000)
+							case 4: rs2 ^= rval; break; //AMOXOR.W (0b00100)
+							case 12: rs2 &= rval; break; //AMOAND.W (0b01100)
+							case 8: rs2 |= rval; break; //AMOOR.W (0b01000)
+							case 16: rs2 = ((int32_t)rs2<(int32_t)rval)?rs2:rval; break; //AMOMIN.W (0b10000)
+							case 20: rs2 = ((int32_t)rs2>(int32_t)rval)?rs2:rval; break; //AMOMAX.W (0b10100)
+							case 24: rs2 = (rs2<rval)?rs2:rval; break; //AMOMINU.W (0b11000)
+							case 28: rs2 = (rs2>rval)?rs2:rval; break; //AMOMAXU.W (0b11100)
+							default: trap = (2+1); dowrite = 0; break; //Not supported.
+						}
+						if( dowrite ) MINIRV32_STORE4( rs1, rs2 );
+					}
+					break;
+				}
+				default: trap = (2+1); // Fault: Invalid opcode.
+			}
+
+			// If there was a trap, do NOT allow register writeback.
+			if( trap )
+				break;
+
+			if( rdid )
+			{
+				REGSET( rdid, rval ); // Write back register.
+			}
+		}
+
+		MINIRV32_POSTEXEC( pc, ir, trap );
+
+		pc += 4;
+	}
+
+	// Handle traps and interrupts.
+	if( trap )
+	{
+		if( trap & 0x80000000 ) // If prefixed with 1 in MSB, it's an interrupt, not a trap.
+		{
+			SETCSR( mcause, trap );
+			SETCSR( mtval, 0 );
+			pc += 4; // PC needs to point to where the PC will return to.
+		}
+		else
+		{
+			SETCSR( mcause,  trap - 1 );
+			SETCSR( mtval, (trap > 5 && trap <= 8)? rval : pc );
+		}
+		SETCSR( mepc, pc ); //TRICKY: The kernel advances mepc automatically.
+		//CSR( mstatus ) & 8 = MIE, & 0x80 = MPIE
+		// On an interrupt, the system moves current MIE into MPIE
+		SETCSR( mstatus, (( CSR( mstatus ) & 0x08) << 4) | (( CSR( extraflags ) & 3 ) << 11) );
+		pc = (CSR( mtvec ) - 4);
+
+		// If trapping, always enter machine mode.
+		CSR( extraflags ) |= 3;
+
+		trap = 0;
+		pc += 4;
+	}
+
+	if( CSR( cyclel ) > cycle ) CSR( cycleh )++;
+	SETCSR( cyclel, cycle );
+	SETCSR( pc, pc );
+	return 0;
+}
+
+#endif
+
+#endif
+
+
