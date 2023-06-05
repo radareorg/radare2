0498d80d3518f184658461dc709552d9df9af375
Luc Tielen <ltielen@nowsecure.com>
2023-06-05 14:01:53 +0200

    Fix undefined behavior when reading java uints ##shlr

--- shlr/java/class.h
+++ shlr/java/class.h
@@ -19,16 +19,16 @@
 
 #define USHORT(x,y) ((ut16)(x[y+1]|(x[y]<<8)))
 #if 1
-#define UINT(x,y) (ut32)(((x[y]&0xff)<<24) \
-| ((x[y+1]&0xff)<<16)  \
-| ((x[y+2]&0xff)<<8)   \
+#define UINT(x,y) (ut32)(((ut32)((x[y]&0xff))<<24) \
+| ((ut32)((x[y+1]&0xff))<<16)  \
+| ((ut32)((x[y+2]&0xff))<<8)   \
 | (x[y+3]&0xff))
 #else
 static inline ut32 UINT(const ut8 *x, const int y) {
 	ut32 ret = 0;
-	ret  = (x[y]&0xff)   << 24;
-	ret |= (x[y+1]&0xff) << 16;
-	ret |= (x[y+2]&0xff) << 8;
+	ret  = ((ut32)(x[y]&0xff))   << 24;
+	ret |= ((ut32)(x[y+1]&0xff)) << 16;
+	ret |= ((ut32)(x[y+2]&0xff)) << 8;
 	ret |= (x[y+3]&0xff);
 	return ret;
 }
@@ -38,7 +38,7 @@ static inline ut32 UINT(const ut8 *x, const int y) {
 
 #define R_BIN_JAVA_USHORT(x,y) ((ut16)(((0xff&x[y+1])|((x[y]&0xff)<<8)) & 0xffff))
 
-#define R_BIN_JAVA_UINT(x,y) ((ut32)(((x[y]&0xff)<<24)|((x[y+1]&0xff)<<16)|((x[y+2]&0xff)<<8)|(x[y+3]&0xff)))
+#define R_BIN_JAVA_UINT(x,y) ((ut32)((((ut32)(x[y]&0xff))<<24)|(((ut32)(x[y+1]&0xff))<<16)|((ut32)((x[y+2]&0xff))<<8)|(x[y+3]&0xff)))
 #define R_BIN_JAVA_FLOAT(x,y) ((float)R_BIN_JAVA_UINT(x,y))
 
 #define R_BIN_JAVA_LONG(x,y) (((ut64) R_BIN_JAVA_UINT (x, y) << 32) | ((ut64)R_BIN_JAVA_UINT (x, y + 4) & UT32_MAX))
