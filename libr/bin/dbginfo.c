82f5cbb4646f765b9b0654bd08fd840572b03682
pancake <pancake@nopcode.org>
2023-06-12 02:32:41 +0200

    The addr2line function, now also returns the column ##bin

--- libr/bin/dbginfo.c
+++ libr/bin/dbginfo.c
@@ -4,8 +4,7 @@
 
 // XXX this api must return a struct instead of pa
 // R_API RBinDwarfRow *r_bin_addr2line(RBin *bin, ut64 addr) {}
-// R2_590 R_API bool r_bin_addr2line(RBin *bin, ut64 addr, char *file, int len, int *line, int *column) {
-R_API bool r_bin_addr2line(RBin *bin, ut64 addr, char *file, int len, int *line) {
+R_API bool r_bin_addr2line(RBin *bin, ut64 addr, char *file, int len, int *line, int *column) {
 	r_return_val_if_fail (bin, false);
 	RBinFile *binfile = r_bin_cur (bin);
 	RBinObject *o = r_bin_cur_object (bin);
@@ -17,8 +16,7 @@ R_API bool r_bin_addr2line(RBin *bin, ut64 addr, char *file, int len, int *line)
 	if (cp && cp->dbginfo) {
 		if (o && addr >= baddr && addr < baddr + bin->cur->o->size) {
 			if (cp->dbginfo->get_line) {
-				return cp->dbginfo->get_line (
-					bin->cur, addr, file, len, line);
+				return cp->dbginfo->get_line (bin->cur, addr, file, len, line, column);
 			}
 		}
 	}
@@ -26,7 +24,7 @@ R_API bool r_bin_addr2line(RBin *bin, ut64 addr, char *file, int len, int *line)
 }
 
 // XXX R2_590 why dupe
-R_API bool r_bin_addr2line2(RBin *bin, ut64 addr, char *file, int len, int *line) {
+R_API bool r_bin_addr2line2(RBin *bin, ut64 addr, char *file, int len, int *line, int *column) {
 	r_return_val_if_fail (bin, false);
 	if (!bin->cur || !bin->cur->sdb_addrinfo) {
 		return false;
@@ -37,6 +35,17 @@ R_API bool r_bin_addr2line2(RBin *bin, ut64 addr, char *file, int len, int *line
 		char *token = strchr (file_line, '|');
 		if (token) {
 			*token++ = 0;
+			char *token2 = strchr (token, ':'); // 0xaddr=file.c|line:column
+			if (token2) {
+				*token2++ = 0;
+				if (column) {
+					*column = atoi (token2);
+				}
+			} else {
+				if (column) {
+					*column = 0;
+				}
+			}
 			if (line) {
 				*line = atoi (token);
 			}
@@ -53,10 +62,8 @@ R_API bool r_bin_addr2line2(RBin *bin, ut64 addr, char *file, int len, int *line
 R_API char *r_bin_addr2text(RBin *bin, ut64 addr, int origin) {
 	r_return_val_if_fail (bin, NULL);
 	char file[4096];
-	int line;
-#if R2_590
-	int colu;
-#endif
+	int line = 0;
+	int colu = -1;
 	char *out = NULL, *out2 = NULL;
 	char *file_nopath = NULL;
 	if (!bin->cur) {
@@ -69,12 +76,10 @@ R_API char *r_bin_addr2text(RBin *bin, ut64 addr, int origin) {
 		if (token) {
 			*token++ = 0;
 			line = atoi (token);
-#if R2_590
 			char *colupos = strchr (token, ':');
 			if (colupos) {
 				colu = atoi (colupos + 1);
 			}
-#endif
 			bool found = true;
 			const char *filename = file_line;
 			char *nf = NULL;
@@ -116,17 +121,18 @@ R_API char *r_bin_addr2text(RBin *bin, ut64 addr, int origin) {
 			}
 		}
 		if (origin) {
-#if R2_590
-			char *res = r_str_newf ("%s:%d:%d%s%s",
-					r_str_get (file_nopath),
-					line, colu, file_nopath? " ": "",
-					r_str_get (out));
-#else
-			char *res = r_str_newf ("%s:%d%s%s",
+			char *res;
+			if (colu == -1) {
+				res = r_str_newf ("%s:%d%s%s",
 					r_str_get (file_nopath),
 					line, file_nopath? " ": "",
 					r_str_get (out));
-#endif
+			} else {
+				res = r_str_newf ("%s:%d:%d%s%s",
+					r_str_get (file_nopath),
+					line, colu, file_nopath? " ": "",
+					r_str_get (out));
+			}
 			free (out);
 			out = res;
 		}
@@ -136,7 +142,7 @@ R_API char *r_bin_addr2text(RBin *bin, ut64 addr, int origin) {
 	R_FREE (file_line);
 
 	file[0] = 0;
-	if (r_bin_addr2line (bin, addr, file, sizeof (file), &line)) {
+	if (r_bin_addr2line (bin, addr, file, sizeof (file), &line, &colu)) {
 		if (bin->srcdir && *bin->srcdir) {
 			char *slash = strrchr (file, '/');
 			char *nf = r_str_newf ("%s/%s", bin->srcdir, slash? slash + 1: file);
@@ -156,30 +162,24 @@ R_API char *r_bin_addr2text(RBin *bin, ut64 addr, int origin) {
 					file_nopath = file;
 				}
 			}
-#if R2_590
+			if (colu == -1) {
+				return r_str_newf ("%s:%d", r_str_get (file_nopath), line);
+			}
 			return r_str_newf ("%s:%d:%d", r_str_get (file_nopath), line, colu);
-#else
-			return r_str_newf ("%s:%d", r_str_get (file_nopath), line);
-#endif
-		}
-		out2 = malloc ((strlen (file) + 64 + strlen (out)) * sizeof (char));
-		if (origin > 1) {
-			file_nopath = NULL;
-		} else {
-			file_nopath = strrchr (file, '/');
 		}
 		if (origin) {
-#if R2_590
-			snprintf (out2, strlen (file) + 63 + strlen (out), "%s:%d:%d%s%s",
-				file_nopath? file_nopath + 1: file, line, colu, *out? " ": "", out);
-#else
-			snprintf (out2, strlen (file) + 63 + strlen (out), "%s:%d%s%s",
-				file_nopath? file_nopath + 1: file, line, *out? " ": "", out);
-#endif
+			file_nopath = (origin < 2)? strrchr (file, '/'): NULL;
+			if (colu != -1) {
+				out2 = r_str_newf ("%s:%d:%d%s%s", file_nopath? file_nopath + 1: file,
+					line, colu, *out? " ": "", out);
+			} else {
+				out2 = r_str_newf ("%s:%d:%s%s", file_nopath? file_nopath + 1: file,
+					line, *out? " ": "", out);
+			}
+			free (out);
 		} else {
-			snprintf (out2, 64, "%s", out);
+			out2 = out;
 		}
-		free (out);
 	}
 	return out2;
 }
@@ -188,15 +188,13 @@ R_API char *r_bin_addr2fileline(RBin *bin, ut64 addr) {
 	r_return_val_if_fail (bin, NULL);
 	char file[1024];
 	int line = 0;
-
-	if (r_bin_addr2line (bin, addr, file, sizeof (file) - 1, &line)) {
+	int colu = -1;
+	if (r_bin_addr2line (bin, addr, file, sizeof (file) - 1, &line, &colu)) {
 		char *file_nopath = strrchr (file, '/');
-#if R2_590
-		int colu = 0;
-		return r_str_newf ("%s:%d:%d", file_nopath? file_nopath + 1: file, line, colu);
-#else
+		if (colu != -1) {
+			return r_str_newf ("%s:%d:%d", file_nopath? file_nopath + 1: file, line, colu);
+		}
 		return r_str_newf ("%s:%d", file_nopath? file_nopath + 1: file, line);
-#endif
 	}
 	return NULL;
 }
