02a3d44e6f6963e362362768d0560574b62fd7da
pancake <pancake@nopcode.org>
2023-06-16 13:44:43 +0200

    Fix #21915 - UB in the mips.gnu plugin

--- libr/arch/p/mips_gnu/plugin.c
+++ libr/arch/p/mips_gnu/plugin.c
@@ -1544,7 +1544,7 @@ static bool decode(RArchSession *as, RAnalOp *op, RAnalOpMask mask) {
 				break;
 			case 17: // bal  bgezal
 				if (rs == 0) {
-					op->jump = addr + (imm << 2) + 4;
+					op->jump = addr + ((ut64)imm << 2) + 4;
 					snprintf ((char *)insn.i_reg.jump, REG_BUF_MAX, "0x%" PFMT64x, op->jump);
 					insn.id = MIPS_INS_BAL;
 				} else {
@@ -1584,7 +1584,7 @@ static bool decode(RArchSession *as, RAnalOp *op, RAnalOpMask mask) {
 				insn.id = MIPS_INS_BGTZ;
 			}
 			op->type = R_ANAL_OP_TYPE_CJMP;
-			op->jump = addr + (imm << 2) + 4;
+			op->jump = addr + ((ut64)imm << 2) + 4;
 			op->fail = addr + 8;
 			op->delay = 1;
 
