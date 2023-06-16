86dc6912accda66d5e56b4dffc2f446e6574c67c
pancake <pancake@nopcode.org>
2023-06-16 20:45:53 +0200

    Faster RAnal.bbTrace() ##analysis

--- libr/anal/anal.c
+++ libr/anal/anal.c
@@ -432,18 +432,11 @@ R_API ut8 *r_anal_mask(RAnal *anal, int size, const ut8 *data, ut64 at) {
 
 R_API void r_anal_trace_bb(RAnal *anal, ut64 addr) {
 	r_return_if_fail (anal);
-	RAnalBlock *bbi;
-	RListIter *iter2;
-	RAnalFunction *fcni = r_anal_get_fcn_in (anal, addr, 0);
-	if (fcni) {
-		r_list_foreach (fcni->bbs, iter2, bbi) {
-			if (addr >= bbi->addr && addr < (bbi->addr + bbi->size)) {
-				bbi->traced = true;
-				break;
-			}
-		}
+	RAnalBlock *bb = r_anal_get_block_at (anal, addr);
+	if (bb && !bb->traced) {
+		bb->traced = true;
+		R_DIRTY (anal);
 	}
-	R_DIRTY (anal);
 }
 
 R_API RList* r_anal_get_fcns(RAnal *anal) {
