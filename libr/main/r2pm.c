6efa8ea82506fa479f4d702b36a3ec6a575f20ed
pancake <pancake@nowsecure.com>
2023-07-07 10:58:58 +0200

    R2PM_SUDO + SUDO just in case ##r2pm
    * Some packages and scripts still use 'SUDO' and maybe better not to do the full transition yet

--- libr/main/r2pm.c
+++ libr/main/r2pm.c
@@ -819,10 +819,12 @@ static int r2pm_install(RList *targets, bool uninstall, bool clean, bool force, 
 			sudo = strdup ("sudo");
 		}
 		r_sys_setenv ("R2PM_SUDO", sudo);
+		r_sys_setenv ("SUDO", sudo);
 		free (sudo);
 	} else {
 		r_sys_setenv ("GLOBAL", "0");
 		r_sys_setenv ("R2PM_SUDO", "");
+		r_sys_setenv ("SUDO", "");
 	}
 	if (!r2pm_have_packages ()) {
 		R_LOG_ERROR ("Please run r2pm -U to initialize/update the database");
