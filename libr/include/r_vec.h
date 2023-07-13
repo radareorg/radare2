5d5d550060d1aa3b5ed8db9db78f4a3f9b7a0ef7
Luc Tielen <luc.tielen@gmail.com>
2023-07-13 12:31:20 +0200

    Fix vec compilation warnings

--- libr/include/r_vec.h
+++ libr/include/r_vec.h
@@ -114,9 +114,10 @@ extern "C" {
 #define R_VEC_CAPACITY(vec) (vec)->_capacity
 
 // Helper macros for doing a foreach-style loop over the elements of a vector.
-#define R_VEC_FOREACH(vec, iter) if (vec) for (iter = (vec)->_start; iter != (vec)->_end; iter++)
+#define R_VEC_FOREACH(vec, iter) \
+	if (vec && (vec)->_start != (vec)->_end) for (iter = (vec)->_start; iter != (vec)->_end; iter++)
 #define R_VEC_FOREACH_PREV(vec, iter) \
-	if (vec) for (iter = (vec)->_end - 1; iter >= (vec)->_start; iter--)
+	if (vec && (vec)->_start != (vec)->_end) for (iter = (vec)->_end - 1; iter >= (vec)->_start; iter--)
 
 #define R_CONCAT_INNER(a, b) a ## b
 #define R_CONCAT(a, b) R_CONCAT_INNER(a, b)
