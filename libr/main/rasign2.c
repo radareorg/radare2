a60b1bf347272e9f11247c7e4a6804384f29b742
pancake <pancake@nopcode.org>
2023-07-25 17:08:59 +0200

    Swap -a and -A in rasign2 ##tools

--- libr/main/rasign2.c
+++ libr/main/rasign2.c
@@ -13,8 +13,8 @@ struct rasignconf {
 
 static void rasign_show_help(void) {
 	printf ("Usage: rasign2 [options] [file]\n"
-		" -a [-a]          add extra 'a' to analysis command\n"
-		" -A               make signatures from all .o files in the provided .a file\n"
+		" -a               make signatures from all .o files in the provided .a file\n"
+		" -A[AAA]          same as r2 -A, the more 'A's the more analysis is performed\n"
 		" -f               interpret the file as a FLIRT .sig file and dump signatures\n"
 		" -h               help menu\n"
 		" -j               show signatures in json\n"
@@ -66,6 +66,7 @@ static void find_functions(RCore *core, size_t count) {
 	case 0: cmd = "aa"; break;
 	case 1: cmd = "aaa"; break;
 	case 2: cmd = "aaaa"; break;
+	case 3: cmd = "aaaaa"; break;
 	}
 	r_core_cmd0 (core, cmd);
 }
@@ -239,10 +240,10 @@ R_API int r_main_rasign2(int argc, const char **argv) {
 	r_getopt_init (&opt, argc, argv, "Aafhjmo:qrSs:cv");
 	while ((c = r_getopt_next (&opt)) != -1) {
 		switch (c) {
-		case 'A':
+		case 'a':
 			conf.ar = true;
 			break;
-		case 'a':
+		case 'A':
 			conf.a_cnt++;
 			break;
 		case 'o':
