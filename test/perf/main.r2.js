abd71f483011d0d2854afbe3ace1eed4050f8842
pancake <pancake@nopcode.org>
2023-08-16 01:16:28 +0200

    Add more colorful indicators

--- test/perf/main.r2.js
+++ test/perf/main.r2.js
@@ -65,7 +65,7 @@ function parseLogs() {
 	for (let i = 1; i < logs.length; i++) {
 		const log = logs[i];
 	//	console.log(logs[i].total, '-', logs[i-1].total);
-		log.diff = 0 | (log.total - logs[i-1].total);
+		log.diff = 0 | (log.total - logs[i - 1].total);
 	}
 	logs.reverse();
 	return logs;
@@ -85,9 +85,78 @@ console.log('<canvas id="myChart" style="width:100%;max-width:600px"></canvas>')
 console.log('<canvas id="myChart2" style="width:100%;max-width:600px"></canvas>');
 const totals = logs.map((x) => x.total);
 const yvalues = '['+totals.reverse().join(',')+']';
-const averages = logs.map((x) => (4.5)*x.average);
+const averages = logs.map((x) => (11)*x.average);
 const avalues = '['+averages.reverse().join(',')+']';
 const xvalues = '[' + logs.map((x) => x.count).reverse().join(',')+']';
+let datasets = '';
+function getdata(log, two, log2) {
+	if (!log) {
+		return [];
+	}
+	// const keys = (log && log.tests)? Object.keys(log.tests) : []; // segfault
+	const keys = Object.keys(log.tests).sort();
+	const ret = [];
+	const o = {};
+	for (let key of keys) {
+		const t = log.tests[key];
+		for (const tt of t) {
+			if (o[key]) {
+				o[key] += tt.time_elapsed;
+				o[key] /= 2;
+			} else {
+				o[key] = tt.time_elapsed;
+			}
+		}
+	}
+	const log2data = (two && log2)? getdata(log2): [];
+	for (let k = 0 ; k < keys.length; k++) {
+		const key = keys[k];
+		const num = 0 | (o[key] / 1000);
+		if (two) {
+			const dk = log2data[k];
+			console.log(dk, num, "<br>");
+			const kolor = (dk > num)?"green": "red";
+			ret.push('<td style="background-color:'+kolor+'" title='+key+'>'+num+'</td>');
+		} else {
+			ret.push(num);
+		}
+	}
+	return ret;
+}
+const res = {};
+for (const kount of Object.keys(logs)) {
+	const log = logs[kount];
+	const data = getdata(log);
+	for (const d of Object.keys(data)) {
+		if (res[d]) {
+			res[d].data.push(data[d]);
+		} else {
+			res[d] = {name: d, data : [data[d]]};
+		}
+	}
+}
+let ares = [];
+for (const r of Object.keys(res)) {
+	res[r].data = res[r].data.reverse();
+	ares.push(res[r]);
+}
+ares.sort((x) => x.count - this.count);
+
+for (const are of ares) {
+	const r = 0|(Math.random()*255);
+	const g = 0|(Math.random()*255);
+	const b = 0|(Math.random()*255);
+	let ds = `
+	datasets.push({
+	      label: "'${are.name}'",
+	      fill: false,
+	      backgroundColor:"rgba(${r},${g},${b},0.4)",
+	      borderColor:"rgba(${r},${g},${b},0.8)",
+	      data: [ ${are.data} ]
+	    });
+	`;
+	datasets += ds;
+}
 const msg = `
 <script>
 const xValues = ${xvalues}; // [50,60,70,80,90,100,110,120,130,140,150];
@@ -112,16 +181,13 @@ const aValues = ${avalues}; // [7,8,8,9,9,9,10,11,14,14,15];
   },
   options:{}
 });
+const datasets = [];
+${datasets}
 new Chart("myChart2", {
   type: "line",
   data: {
     labels: xValues,
-    datasets: [{
-      label: 'time',
-      backgroundColor:"rgba(0,200,0,0.4)",
-      borderColor: "rgba(0,250,0,0.8)",
-      data: aValues
-    }]
+    datasets: datasets
   },
   options:{}
 });
@@ -132,7 +198,7 @@ console.log(msg);
 console.log("<table border=1>");
 let line = "<tr style='background-color:#404040'>\n  ";
 line += "<td>count</td>";
-line += "<td>commit</td>";
+// line += "<td>commit</td>";
 line += "<td>diff</td>";
 line += "<td>total</td>";
 line += "<td>average</td>";
@@ -140,17 +206,29 @@ line += "<td>tests</td>";
 line += "</tr>";
 console.log(line);
 // todo add log.diff computing it with the aveage
-for (const kount of Object.keys(logs)) {
+const logKeys = Object.keys(logs).sort();
+for (let n = 0; n < logKeys.length; n++) {
+	const kount = logKeys[n];
 	const log = logs[kount];
+	const log2 = logs[logKeys[n + 1]];
 	// console.log(JSON.stringify(log, null, 2));
 	let line = "<tr>";
-	line += "<td>"+log.count+"</td>";
-	line += "<td><a href='https://github.com/radareorg/radare2/commit/"+log.commit+"'>"+log.commit+"</a></td>";
-	const bg = log.diff > 0? "red": "green";
+	// line += "<td>"+log.count+"</td>";
+	line += "<td><a href='https://github.com/radareorg/radare2/commit/"+log.commit+"'>"+log.count+"</a></td>";
+	var bg = log.diff > 10? "red": "green";
 	line += "<td style='background-color:"+bg+"'>"+log.diff+"</td>";
 	line += "<td>"+log.total+"</td>";
 	line += "<td>"+log.average+"</td>";
-	line += "<td>"+Object.keys(log.tests).join('<br />')+"</td>";
+	const lldata_a = getdata(log);
+	const lldata_b = getdata(log2);
+	const lldata = [];
+	for (let i = 0; i<lldata_a.length; i++) {
+		const ka = lldata_a[i] || 0;
+		const kb = lldata_b[i] || 0;
+		var bg = (ka <= kb)? "green": "red";
+		lldata.push('<td style="background-color:'+bg+'">'+ka+'</td>\n');
+	}
+	line += lldata.join('');
 	line += "</tr>";
 	console.log(line);
 }
