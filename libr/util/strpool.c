ea6ae0fac4955f41b58fd7353b087cf22a0d79e3
pancake <pancake@nopcode.org>
2023-08-17 23:03:38 +0200

    Rename RStrpool.ansiChop to ansiTrim() ##api

--- libr/util/strpool.c
+++ libr/util/strpool.c
@@ -72,8 +72,7 @@ R_API int r_strpool_append(RStrpool *p, const char *s) {
 	return r_strpool_memcat (p, s, l);
 }
 
-// R2_590 Rename to ansi_trim() not chop
-R_API int r_strpool_ansi_chop(RStrpool *p, int n) {
+R_API int r_strpool_ansi_trim(RStrpool *p, int n) {
 	/* p->str need not be a c-string */
 	int i = r_str_ansi_trim (p->str, p->len, n);
 	p->len = i;
