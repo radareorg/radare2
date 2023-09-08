ba6103455c5e5a2cb5a1caf230e215796a55beeb
pancake <pancake@nowsecure.com>
2023-09-08 19:15:44 +0200

    Use a hashtable fo the .equ rasm2 directive ##asm

--- libr/asm/acode.c
+++ libr/asm/acode.c
@@ -8,53 +8,39 @@ R_API RAsmCode *r_asm_code_new(void) {
 
 R_API void r_asm_code_free(RAsmCode *acode) {
 	if (acode) {
-		r_list_free (acode->equs);
+		ht_pp_free (acode->equs);
 		free (acode->bytes);
 		free (acode->assembly);
 		free (acode);
 	}
 }
 
-R_API void r_asm_equ_item_free(RAsmEqu *equ) {
-	if (equ) {
-		free (equ->key);
-		free (equ->value);
-		free (equ);
-	}
-}
-
-static RAsmEqu *__asm_equ_new(const char *key, const char *value) {
-	r_return_val_if_fail (key && value, NULL);
-	RAsmEqu *equ = R_NEW0 (RAsmEqu);
-	if (equ) {
-		equ->key = strdup (key);
-		equ->value = strdup (value);
-	}
-	return equ;
-}
-
 R_API void r_asm_code_set_equ(RAsmCode *code, const char *key, const char *value) {
 	r_return_if_fail (code && key && value);
-
-	if (code->equs) {
-		RAsmEqu *equ = r_asm_code_equ_get (code, key);
-		if (equ) {
-			free (equ->value);
-			equ->value = strdup (value);
-		}
-	} else {
-		code->equs = r_list_newf ((RListFree)r_asm_equ_item_free);
+	if (!code->equs) {
+		code->equs = ht_pp_new0 ();
 	}
-	r_list_append (code->equs, __asm_equ_new (key, value));
+	ht_pp_insert (code->equs, key, strdup (value));
+}
+
+typedef struct {
+	RAsmCode *code;
+	char *str;
+} UserData;
+
+static bool replace_cb(void *user, const void *key, const void *value) {
+	UserData *data = user;
+	data->str = r_str_replace (data->str, key, value, true);
+	return true;
 }
 
 R_API char *r_asm_code_equ_replace(RAsmCode *code, char *str) {
 	r_return_val_if_fail (code && str, NULL);
-	RAsmEqu *equ;
-	RListIter *iter;
-	r_list_foreach (code->equs, iter, equ) {
-		str = r_str_replace (str, equ->key, equ->value, true);
-	}
+	UserData data = {
+		.code = code,
+		.str = str
+	};
+	ht_pp_foreach (code->equs, replace_cb, &data);
 	return str;
 }
 
@@ -67,15 +53,8 @@ R_API char* r_asm_code_get_hex(RAsmCode *acode) {
 	return str;
 }
 
-R_API RAsmEqu *r_asm_code_equ_get(RAsmCode *code, const char *key) { // R2_590
-	// TODO: use a hashtable or sdb
-	void *equ;
-	RListIter *iter;
-	r_list_foreach (code->equs, iter, equ) {
-		RAsmEqu *e = (RAsmEqu*) equ;
-		if (!strcmp (e->key, key)) {
-			return e;
-		}
-	}
-	return NULL;
+R_API char *r_asm_code_equ_get(RAsmCode *code, const char *key) {
+	r_return_val_if_fail (code && key, NULL);
+	bool found = false;
+	return ht_pp_find (code->equs, key, &found);
 }
