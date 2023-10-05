bf1a3b2ab0883e6388d40634cfe378fa3764475d
pancake <pancake@nopcode.org>
2023-10-05 17:02:20 +0200

    Fix #22293 - uaf in bin.avr file

--- libr/bin/p/bin_avr.c
+++ libr/bin/p/bin_avr.c
@@ -86,8 +86,8 @@ static bool load(RBinFile *bf, RBuffer *buf, ut64 loadaddr) {
 
 static void destroy(RBinFile *bf) {
 	AvrPriv *ap = (AvrPriv*)(bf->bo->bin_obj);
-	r_buf_free (ap->b);
-	R_FREE (bf->bo->bin_obj);
+	// r_buf_free (ap->b); // this is freed by RBinFile.free()
+	free (ap);
 }
 
 static RBinInfo* info(RBinFile *bf) {
