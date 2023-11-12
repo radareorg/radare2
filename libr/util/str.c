68bcd67c0d7fba60edca3a0ed61129a55dac8395
solid-snail <solidsnail@tuta.io>
2023-11-12 15:17:21 +0200

    fixed indentation

--- libr/util/str.c
+++ libr/util/str.c
@@ -1964,10 +1964,10 @@ R_API size_t r_str_ansi_nlen(const char *str, size_t slen) {
 static size_t __str_ansi_sanitize_length(char const *str) {
 	size_t i = 0;
 	if (str[0] == 0x1b || str[0] == 0x07 || str[0] == 0x05 || str[0] == 0x7f) { // ESC, BEL, ENQ, DEL
-        i++;
+		i++;
 	} else if (str[0] == -0x3e && str[1] >= -0x80 && str[1] <= -0x61) { // C1 control codes U+0080 - U+009F
-        i += 2;
-    }
+		i += 2;
+	}
 	return i;
 }
 
@@ -1977,10 +1977,10 @@ R_API size_t r_str_ansi_strip(char *str) {
 	size_t i = 0;
 	while (str[i]) {
 		size_t chlen = __str_ansi_length (str + i);
-        size_t sanitize_len = __str_ansi_sanitize_length(str + i);
+		size_t sanitize_len = __str_ansi_sanitize_length (str + i);
 		if (chlen > 1) {
 			r_str_cpy (str + i, str + i + chlen);
-        } else if (sanitize_len > 0) {
+		} else if (sanitize_len > 0) {
 			r_str_cpy (str + i, str + i + sanitize_len);
 		} else {
 			i++;
