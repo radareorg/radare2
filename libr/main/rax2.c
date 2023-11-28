06b30fc19bef2944381733f4aff56a4fd2b9cb99
overmyerD <dalton@raptorpowersystems.com>
2023-11-28 11:36:51 -0600

    Change -B and -b option to use new r_str_str2binstr and functions.

--- libr/main/rax2.c
+++ libr/main/rax2.c
@@ -313,12 +313,8 @@ dotherax:
 		}
 		return true;
 	} else if (flags & (1 << 3)) { // -b
-		int i;
-		ut8 buf[4096];
-		const int n = r_str_binstr2bin (str, buf, sizeof (buf));
-		for (i = 0; i < n; i++) {
-			printf ("%c", buf[i]);
-		}
+    char* newstr = r_str_binstr2str(str);
+		printf("%s", newstr);
 		return true;
 	} else if (flags & (1 << 4)) { // -x
 		int h = r_str_hash (str);
@@ -389,7 +385,8 @@ dotherax:
 		}
 		return true;
 	} else if (flags & (1 << 17)) { // -B (bin -> str)
-		r_print_bin_from_str (NULL, str);
+    char* binstr = r_str_str2binstr(str);
+		printf("%s", binstr);
 		return true;
 	} else if (flags & (1 << 16)) { // -w
 		ut64 n = r_num_calc (num, str, &errstr);
