89924b494c07ff142726878098c7258e3f4a0cbd
pancake <pancake@nopcode.org>
2024-02-03 13:03:07 +0900

    A bit faster internal_esil_reg_write

--- libr/esil/esil.c
+++ libr/esil/esil.c
@@ -426,7 +426,13 @@ static bool internal_esil_reg_read(REsil *esil, const char *regname, ut64 *num, 
 }
 
 static bool internal_esil_reg_write(REsil *esil, const char *regname, ut64 num) {
-	r_return_val_if_fail (esil && esil->anal, false);
+	R_RETURN_VAL_IF_FAIL (esil && esil->anal, false);
+#if 1
+	if (r_reg_setv (esil->anal->reg, regname, num)) {
+		return true;
+	}
+	R_LOG_DEBUG ("Register %s does not exist", regname);
+#else
 	RRegItem *ri = r_reg_get (esil->anal->reg, regname, -1);
 	if (ri) {
 		r_reg_set_value (esil->anal->reg, ri, num);
@@ -434,6 +440,7 @@ static bool internal_esil_reg_write(REsil *esil, const char *regname, ut64 num) 
 		r_unref (ri);
 		return true;
 	}
+#endif
 	return false;
 }
 
