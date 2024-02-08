fa24e5c8eac87b4a5e62fb947682d719539d2ce4
pancake <pancake@nopcode.org>
2024-02-08 10:31:00 +0100

    fix for msvc

--- libr/util/rprintf.c
+++ libr/util/rprintf.c
@@ -109,7 +109,15 @@ redo_spec:
 				 flags |= FILL_ZERO;
 			 }
 			 // fall through
-		case '1'...'9':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
 			 width = width * 10 + c - '0';
 			 goto redo_spec;
 		case '*':
