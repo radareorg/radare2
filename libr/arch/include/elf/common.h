8eed32f97510085a5e96bdddab29ef9b70bc9a6f
pancake <pancake@nowsecure.com>
2024-02-14 17:08:50 +0100

    Update libr/arch/include/elf/common.h

--- libr/arch/include/elf/common.h
+++ libr/arch/include/elf/common.h
@@ -638,7 +638,6 @@
 #define DTF_1_PARINIT	0x00000001
 /* From
 
-   https://docs.sun.com:80/ab2/coll.45.13/LLM/@Ab2PageView/21165?Ab2Lang=C&Ab2Enc=iso-8859-1
 
    DTF_1_CONFEXP is the same as DTF_1_PARINIT. It is a typo. The value
    defined here is the same as the one in <sys/link.h> on Solaris 8.  */
