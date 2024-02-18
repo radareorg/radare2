3e4110a5afb05f50b3c5225264d955b10445b66a
ripatel-fd <ripatel+git@jumptrading.com>
2024-02-18 00:23:49 -0600

    Fix #22269 - RAP seek over 2GiB ##io
    Co-authored-by: Richard Patel <me@terorie.dev>

--- libr/socket/socket_rap_client.c
+++ libr/socket/socket_rap_client.c
@@ -176,7 +176,7 @@ R_API int r_socket_rap_client_read(RSocket *s, ut8 *buf, int count) {
 	return count;
 }
 
-R_API int r_socket_rap_client_seek(RSocket *s, ut64 offset, int whence) {
+R_API ut64 r_socket_rap_client_seek(RSocket *s, ut64 offset, int whence) {
 	ut8 tmp[10];
 	tmp[0] = RAP_PACKET_SEEK;
 	tmp[1] = (ut8)whence;
@@ -185,19 +185,19 @@ R_API int r_socket_rap_client_seek(RSocket *s, ut64 offset, int whence) {
 	if (ret != 10) {
 		R_LOG_ERROR ("Truncated socket write %d vs %d", ret, 10);
 		r_sys_backtrace ();
-		return -1;
+		return UT64_MAX;
 	}
 	r_socket_flush (s);
 	ret = r_socket_read_block (s, (ut8*)&tmp, 9);
 	if (ret != 9) {
 		R_LOG_ERROR ("Truncated socket read %d vs %d", ret, 9);
-		return -1;
+		return UT64_MAX;
 	}
 	if (tmp[0] != (RAP_PACKET_SEEK | RAP_PACKET_REPLY)) {
 		// eprintf ("%d %d  - %02x %02x %02x %02x %02x %02x %02x\n",
 		// ret, whence, tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5], tmp[6]);
 		R_LOG_WARN ("Unexpected seek reply (%02x -> %02x)", tmp[0], (RAP_PACKET_SEEK | RAP_PACKET_REPLY));
-		return -1;
+		return UT64_MAX;
 	}
 	return r_read_at_be64 (tmp, 1);
 }
