c158fae44e2048f985780275d3977550224babb5
ashamedbit <muralianiruddhan@gmail.com>
2024-02-24 03:41:39 -0500

    Fix leaks in bin_elf.inc.c

--- libr/bin/p/bin_elf.inc.c
+++ libr/bin/p/bin_elf.inc.c
@@ -239,6 +239,7 @@ static void process_constructors(RBinFile *bf, RList *ret, int bits) {
 		}
 		free (buf);
 	}
+	r_list_free (secs);
 }
 
 static RList* entries(RBinFile *bf) {
@@ -1021,6 +1022,7 @@ static void lookup_sections(RBinFile *bf, RBinInfo *ret) {
 			break;
 		}
 	}
+	r_list_free (secs);
 }
 
 static bool has_sanitizers(RBinFile *bf) {
@@ -1197,6 +1199,7 @@ static ut64 size(RBinFile *bf) {
 				len = section->size;
 			}
 		}
+		r_list_free (secs);
 	}
 	return off + len;
 }
