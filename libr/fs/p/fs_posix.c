d3d2be771a9c07270bdd15c70eff20117cc6bdbf
pancake <pancake@nopcode.org>
2024-03-15 15:34:19 +0100

    try to fix windows

--- libr/fs/p/fs_posix.c
+++ libr/fs/p/fs_posix.c
@@ -6,6 +6,9 @@
 #ifdef _MSC_VER
 #define S_ISREG(m) (((m) & S_IFMT) == S_IFREG)
 #define S_ISDIR(m) (((m) & S_IFMT) == S_IFDIR)
+#define S_ISLNK(m) (((m) & S_IFMT) == S_IFLNK)
+#define S_ISCHR(m) (((m) & S_IFMT) == S_IFCHR)
+#define S_ISBLK(m) (((m) & S_IFMT) == S_IFBLK)
 #define MAXPATHLEN 255
 #endif
 static RFSFile* fs_posix_open(RFSRoot *root, const char *path, bool create) {
