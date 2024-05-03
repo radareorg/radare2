15d820fb7ed7b0e6435398cb584279648fc9e410
pancake <pancake@nopcode.org>
2024-05-03 03:02:45 +0200

    Fix #22831 - Proper baddr on the FreeBSD debugger ##debug

--- libr/core/cfile.c
+++ libr/core/cfile.c
@@ -290,6 +290,16 @@ R_API char *r_core_sysenv_begin(RCore *core, const char *cmd) {
 }
 
 #if !__linux__ && !R2__WINDOWS__
+static bool is_base_map(RDebugMap *map) {
+	if ((map->perm & 1) == 1) { // "--x"
+		// only used by FreeBSD (and PS5)
+		return true;
+	}
+	if ((map->perm & 5) == 5) { // "r-x"
+		return true;
+	}
+	return false;
+}
 static ut64 get_base_from_maps(RCore *core, const char *file) {
 	RDebugMap *map;
 	RListIter *iter;
@@ -297,7 +307,7 @@ static ut64 get_base_from_maps(RCore *core, const char *file) {
 
 	r_debug_map_sync (core->dbg); // update process memory maps
 	r_list_foreach (core->dbg->maps, iter, map) {
-		if ((map->perm & 5) == 5) {
+		if (is_base_map (map)) {
 			// TODO: make this more flexible
 			// XXX - why "copy/" here?
 			if (map->name && strstr (map->name, "copy/")) {
@@ -317,7 +327,7 @@ static ut64 get_base_from_maps(RCore *core, const char *file) {
 	}
 	// fallback resolution copied from cmd_debug.c:r_debug_get_baddr
 	r_list_foreach (core->dbg->maps, iter, map) {
-		if (map->perm == 5) { // r-x
+		if (is_base_map (map)) {
 			return map->addr;
 		}
 	}
