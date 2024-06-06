308d6dd0143ae32fe3658bc2821c3b2179ca39ef
pancake <pancake@nopcode.org>
2024-06-06 12:42:15 +0200

    Remove non-id code from the gml graph output ##graph

--- libr/core/canal.c
+++ libr/core/canal.c
@@ -2276,7 +2276,7 @@ R_API int r_core_print_bb_custom(RCore *core, RAnalFunction *fcn) {
 	return true;
 }
 
-#define USE_ID 1
+// R2_600 - return bool
 R_API int r_core_print_bb_gml(RCore *core, RAnalFunction *fcn) {
 	RAnalBlock *bb;
 	RListIter *iter;
@@ -2292,19 +2292,12 @@ R_API int r_core_print_bb_gml(RCore *core, RAnalFunction *fcn) {
 		RFlagItem *flag = r_flag_get_i (core->flags, bb->addr);
 		char *msg = flag? strdup (flag->name): r_str_newf ("0x%08"PFMT64x, bb->addr);
 		// TODO char *str = r_str_escape_dot (msg);
-#if USE_ID
 		ht_uu_insert (ht, bb->addr, id);
 		r_cons_printf ("  node [\n"
 				"    id  %d\n"
 				"    label  \"%s\"\n"
 				"  ]\n", id, msg);
 		id++;
-#else
-		r_cons_printf ("  node [\n"
-				"    id  %"PFMT64d"\n"
-				"    label  \"%s\"\n"
-				"  ]\n", bb->addr, msg);
-#endif
 		free (msg);
 	}
 
@@ -2312,7 +2305,6 @@ R_API int r_core_print_bb_gml(RCore *core, RAnalFunction *fcn) {
 		if (bb->addr == UT64_MAX) {
 			continue;
 		}
-#if USE_ID
 		if (bb->jump != UT64_MAX) {
 			bool found;
 			int i = ht_uu_find (ht, bb->addr, &found);
@@ -2356,33 +2348,6 @@ R_API int r_core_print_bb_gml(RCore *core, RAnalFunction *fcn) {
 				}
 			}
 		}
-#else
-		if (bb->jump != UT64_MAX) {
-			r_cons_printf ("  edge [\n"
-				"    source  %"PFMT64d"\n"
-				"    target  %"PFMT64d"\n"
-				"  ]\n", bb->addr, bb->jump
-				);
-		}
-		if (bb->fail != UT64_MAX) {
-			r_cons_printf ("  edge [\n"
-				"    source  %"PFMT64d"\n"
-				"    target  %"PFMT64d"\n"
-				"  ]\n", bb->addr, bb->fail
-				);
-		}
-		if (bb->switch_op) {
-			RListIter *it;
-			RAnalCaseOp *cop;
-			r_list_foreach (bb->switch_op->cases, it, cop) {
-				r_cons_printf ("  edge [\n"
-					"    source  %"PFMT64d"\n"
-					"    target  %"PFMT64d"\n"
-					"  ]\n", bb->addr, cop->addr
-					);
-			}
-		}
-#endif
 	}
 	r_cons_printf ("]\n");
 	ht_uu_free (ht);
