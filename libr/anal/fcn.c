a62d97cca8b5a5e46e045d976ba1756cac796745
pancake <pancake@nopcode.org>
2024-07-16 18:21:43 +0200

    A bit better experimental nopskip ##analysis

--- libr/anal/fcn.c
+++ libr/anal/fcn.c
@@ -1,4 +1,4 @@
-/* radare - LGPL - Copyright 2010-2023 - nibble, alvaro, pancake */
+/* radare - LGPL - Copyright 2010-2024 - nibble, alvaro, pancake */
 
 #define R_LOG_ORIGIN "fcn"
 
@@ -771,6 +771,20 @@ repeat:
 		src1 = r_vector_at (&op->srcs, 1);
 
 		if (anal->opt.nopskip && fcn->addr == at) {
+			if (at == 0) {
+				goto noskip;
+			}
+			if (anal->config->bits == 64) {
+				if (at % 8) {
+					goto noskip;
+				}
+			} else {
+				if (r_anal_archinfo (anal, R_ARCH_INFO_CODE_ALIGN) == 4) {
+					if (at % 4) {
+						goto noskip;
+					}
+				}
+			}
 			RFlagItem *fi = anal->flb.get_at (anal->flb.f, addr, false);
 			if (!fi || strstr (fi->name, "sym.")) {
 				if ((addr + delay.un_idx - oplen) == fcn->addr) {
@@ -798,6 +812,7 @@ repeat:
 				}
 			}
 		}
+noskip:
 		if (op->hint.new_bits) {
 			r_anal_hint_set_bits (anal, op->jump, op->hint.new_bits);
 		}
