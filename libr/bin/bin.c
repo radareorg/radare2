bbeb41cee139cec495a549b13179481b92cba1bb
pancake <pancake@nopcode.org>
2024-08-04 11:47:38 +0200

    Implement RBinPlugin Commands in the new abi via i: ##bin

--- libr/bin/bin.c
+++ libr/bin/bin.c
@@ -35,9 +35,11 @@ static int __getoffset(RBin *bin, int type, int idx) {
 
 static const char *__getname(RBin *bin, int type, int idx, bool sd) {
 	RBinFile *a = r_bin_cur (bin);
-	RBinPlugin *plugin = r_bin_file_cur_plugin (a);
-	if (plugin && plugin->get_name) {
-		return plugin->get_name (a, type, idx, sd);
+	if (a) {
+		RBinPlugin *plugin = r_bin_file_cur_plugin (a);
+		if (plugin && plugin->get_name) {
+			return plugin->get_name (a, type, idx, sd);
+		}
 	}
 	return NULL;
 }
@@ -1691,3 +1693,16 @@ R_API char *r_bin_attr_tostring(ut64 attr, bool singlechar) {
 R_API ut64 r_bin_attr_fromstring(const char *s) {
 	return 0ULL;
 }
+
+#if R2_USE_NEW_ABI
+R_API bool r_bin_command(RBin *bin, const char *input) {
+	RBinFile *a = r_bin_cur (bin);
+	if (a) {
+		RBinPlugin *plugin = r_bin_file_cur_plugin (a);
+		if (plugin && plugin->cmd) {
+			return plugin->cmd(a, input);
+		}
+	}
+	return false;
+}
+#endif
