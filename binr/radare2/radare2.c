245779383ead51f3b5847a852ae6fb37d26d31d7
pancake <pancake@nowsecure.com>
2024-09-13 17:54:09 +0200

    Fix a 1 byte overflow in r2cmd and improve logic checks ##crash

--- binr/radare2/radare2.c
+++ binr/radare2/radare2.c
@@ -1,4 +1,4 @@
-/* radare - LGPL - Copyright 2009-2023 - pancake */
+/* radare - LGPL - Copyright 2009-2024 - pancake */
 
 #include <r_main.h>
 
@@ -43,38 +43,39 @@ void r2_asmjs_openurl(void *kore, const char *url) {
 	}
 }
 #else
+
+#ifndef STDOUT_FILENO
+#define STDOUT_FILENO 1
+#endif
+
 static void r2cmd(int in, int out, const char *cmd) {
-	size_t cmd_len = strlen (cmd) + 1;
-	if (write (out, cmd, cmd_len) != cmd_len) {
+	int cmd_len = strlen (cmd) + 1;
+	if ((int)write (out, cmd, cmd_len) != cmd_len) {
 		return;
 	}
+#if 0
 	if (write (out, "\n", 1) != 1) {
 		return;
 	}
-	int bufsz = (1024 * 64);
-	unsigned char *buf = malloc (bufsz);
-	if (!buf) {
+#endif
+	int bufsz = (1024 * 64) - 1;
+	ut8 *buf = malloc (bufsz + 1);
+	if (R_UNLIKELY (!buf)) {
 		return;
 	}
-	while (1) {
-		int n = read (in, buf, bufsz);
-		if (n < 1) {
-			break;
-		}
-		buf[n] = '\0';
-		buf[bufsz - 1] = '\0';
+	int n = read (in, buf, bufsz);
+	if (R_LIKELY (n > 0)) {
+		buf[R_MIN (n, bufsz)] = 0;
 		int len = strlen ((const char *)buf);
-		n = len;
-		if (n < 1) {
-			break;
-		}
-		n = write (1, buf, n);
-		if (n != bufsz) {
-			break;
+		if (len > 0) {
+			n = write (STDOUT_FILENO, buf, len);
+			if (n != len) {
+				R_LOG_ERROR ("Truncated output");
+			}
 		}
 	}
 	free (buf);
-	write (1, "\n", 1);
+	write (STDOUT_FILENO, "\n", 1);
 }
 
 static int r_main_r2pipe(int argc, const char **argv) {
