f006158460be1a0e6ec8560eee1ed5b18c7b5803
satk0 <satko_a@op.pl>
2024-09-18 21:51:40 +0200

    Undevilize preconfigure.bat and add MSBuild 2022 Community support ##build

--- preconfigure.bat
+++ preconfigure.bat
@@ -100,39 +100,30 @@ echo === Finding Visual Studio...
 cl --help > NUL 2> NUL
 if %ERRORLEVEL% == 0 (
   echo FOUND
+) else if EXIST "C:\Program Files\Microsoft Visual Studio\2022\Enterprise" (
+  echo "Found 2022 Enterprise edition"
+  call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
+) else if EXIST "C:\Program Files\Microsoft Visual Studio\2022\Community" (
+  echo "Found 2022 Community edition"
+  call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
+) else if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" (
+  echo "Found 2022 BuildTools"
+  call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
+) else if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community" (
+  echo "Found 2019 community edition"
+  call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
+) else if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" (
+  echo "Found 2019 Enterprise edition"
+  call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
+) else if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" (
+  echo "Found 2019 Professional edition"
+  call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
+) else if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" (
+  echo "Found 2019 BuildTools"
+  call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
 ) else (
-  if EXIST "C:\Program Files\Microsoft Visual Studio\2022\Enterprise" (
-    echo "Found 2022 Enterprise edition"
-    call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
-  ) else (
-    if EXIST "C:\Program Files\Microsoft Visual Studio\2022\Community" (
-      echo "Found 2022 Community edition"
-      call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
-    ) else (
-      if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community" (
-        echo "Found 2019 community edition"
-        call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
-      ) else (
-        if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" (
-          echo "Found 2019 Enterprise edition"
-          call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
-        ) else (
-          if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" (
-            echo "Found 2019 Professional edition"
-            call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
-          ) else (
-            if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" (
-              echo "Found 2019 BuildTools"
-              call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
-            ) else (
-              echo "Not Found"
-              exit /b 1
-            )
-          )
-        )
-      )
-    )
-  )
+  echo "Not Found"
+  exit /b 1
 )
 
 if EXIST "libr\arch\p\arm\v35\arch-arm64" (
