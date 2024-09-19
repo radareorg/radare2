9ea449cee4a69f01a5c4643450f26b9fef29d54a
pancake <pancake@nowsecure.com>
2024-09-19 11:59:15 +0200

    Assume x27 as global pointer only in dart binaries ##analysis

--- libr/core/canal.c
+++ libr/core/canal.c
@@ -5834,8 +5834,10 @@ R_API void r_core_anal_esil(RCore *core, const char *str /* len */, const char *
 		gp_reg = "gp";
 		arch = R2_ARCH_MIPS;
 	} else if (arch == R2_ARCH_ARM64) {
-		// Dart binaries only
-		gp_reg = "x27";
+		RBinInfo *info = r_bin_get_info (core->bin);
+		if (info && info->lang && !strcmp (info->lang, "dart")) {
+			gp_reg = "x27";
+		}
 	}
 
 	r_reg_arena_push (core->anal->reg);
