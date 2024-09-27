0738d61eb38f4760c14e5da88874d954d95786dd
pancake <pancake@nopcode.org>
2024-09-27 03:16:30 +0200

    ai

--- libr/main/rapatch2.c
+++ libr/main/rapatch2.c
@@ -6,10 +6,13 @@
 #include <r_main.h>
 
 static int show_help(int v) {
-	printf ("Usage: rapatch2 [-R] [origfile] ([patchfile])\n");
+	printf ("Usage: rapatch2 [-p N] [-sv] [-R] [origfile] ([patchfile])\n");
 	if (v) {
 		printf (
+			"  -p N       patch level, skip N directories\n"
 			"  -R         reverse patch\n"
+			"  -s         be silent\n"
+			"  -v         show version\n"
 		       );
 	}
 	return 1;
@@ -20,15 +23,25 @@ R_API int r_main_rapatch2(int argc, const char **argv) {
 	int o;
 
 	bool reverse = false;
+	bool silent = false;
+	int patchlevel = 0;
 
-	r_getopt_init (&opt, argc, argv, "hR");
+	r_getopt_init (&opt, argc, argv, "hRvsp:");
 	while ((o = r_getopt_next (&opt)) != -1) {
 		switch (o) {
 		case 'h':
 			return show_help (1);
+		case 's':
+			silent = true;
+			break;
+		case 'p':
+			patchlevel = atoi (opt.arg);
+			break;
 		case 'R':
 			reverse = true;
 			break;
+		case 'v':
+			return r_main_version_print ("rapatch2", 0);
 		default:
 			return show_help (0);
 		}
@@ -46,6 +59,12 @@ R_API int r_main_rapatch2(int argc, const char **argv) {
 	}
 	if (reverse) {
 		R_LOG_TODO ("reverse patch not yet supported");
+	}
+	if (silent) {
+		R_LOG_TODO ("silent not yet supported");
+	}
+	if (patchlevel) {
+		R_LOG_TODO ("patchlevel not yet supported");
 	}
 	const char *r2argv[5] = {
 		"radare2",
