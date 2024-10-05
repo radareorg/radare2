1f5defe34fcabec1b3041914be189d04435df174
Sylvain Pelissier <sylvain.pelissier@gmail.com>
2024-10-05 15:31:50 -0700

    Split signature write in a new command

--- libr/core/cmd_write.inc.c
+++ libr/core/cmd_write.inc.c
@@ -132,6 +132,7 @@ static RCoreHelpMessage help_msg_wo = {
 	"wor", " [val]", ">>= shift right",
 	"woR", "", "random bytes (alias for 'wr $b')",
 	"wos", " [val]", "-= substraction",
+	"woS", " [algo] [key]", "sign the current block with given algo and key",
 	"wow", " [val]", "== write looped value (alias for 'wb')",
 	"wox", " [val]", "^= xor (f.ex: wox 0x90)",
 	NULL
@@ -240,10 +241,8 @@ R_API int cmd_write_hexpair(RCore* core, const char* pairs) {
 	return len;
 }
 
-static bool encrypt_or_decrypt_block(RCore *core, const char *algo, const char *key, int direction, const char *iv) {
-	//TODO: generalise no_key_mode for all non key encoding/decoding.
+static void write_encrypted_block(RCore *core, const char *algo, const char *key, int direction, const char *iv) {
 	int keylen = 0;
-	bool no_key_mode = !strcmp ("base64", algo) || !strcmp ("base91", algo) || !strcmp ("punycode", algo);
 	ut8 *binkey = NULL;
 	if (!strncmp (key, "s:", 2)) {
 		binkey = (ut8*)strdup (key + 2);
@@ -253,16 +252,16 @@ static bool encrypt_or_decrypt_block(RCore *core, const char *algo, const char *
 		keylen = r_hex_str2bin (key, binkey);
 	}
 	if (!binkey) {
-		return false;
+		return;
 	}
-	if (!no_key_mode && keylen < 1) {
+	if (keylen < 1) {
 		const char *mode = (!direction)? "Encryption": "Decryption";
 		R_LOG_ERROR ("%s key not defined. Use -S [key]", mode);
 		free (binkey);
-		return false;
+		return;
 	}
 	RCryptoJob *cj = r_crypto_use (core->crypto, algo);
-	if (cj) {
+	if (cj && cj->h->type == R_CRYPTO_TYPE_ENCRYPT) {
 		if (r_crypto_job_set_key (cj, binkey, keylen, 0, direction)) {
 			if (iv) {
 				ut8 *biniv = malloc (strlen (iv) + 1);
@@ -273,7 +272,7 @@ static bool encrypt_or_decrypt_block(RCore *core, const char *algo, const char *
 				}
 				if (!r_crypto_job_set_iv (cj, biniv, ivlen)) {
 					R_LOG_ERROR ("Invalid IV");
-					return 0;
+					return;
 				}
 			}
 			r_crypto_job_update (cj, (const ut8*)core->block, core->blocksize);
@@ -289,11 +288,51 @@ static bool encrypt_or_decrypt_block(RCore *core, const char *algo, const char *
 			}
 		}
 		free (binkey);
-		return 0;
+		return;
 	} else {
-		R_LOG_ERROR ("Unknown %s algorithm '%s'", ((!direction) ? "encryption" : "decryption") ,algo);
+		R_LOG_ERROR ("Unknown %s algorithm '%s'", ((!direction)? "encryption": "decryption"), algo);
 	}
-	return 1;
+	return;
+}
+
+static void write_block_signature(RCore *core, const char *algo, const char *key) {
+	int keylen = 0;
+	ut8 *binkey = NULL;
+	if (!strncmp (key, "s:", 2)) {
+		binkey = (ut8 *)strdup (key + 2);
+		keylen = strlen (key + 2);
+	} else {
+		binkey = (ut8 *)strdup (key);
+		keylen = r_hex_str2bin (key, binkey);
+	}
+	if (!binkey) {
+		return;
+	}
+	if (keylen < 1) {
+		R_LOG_ERROR ("Private key not defined. Use -S [key]");
+		free (binkey);
+		return;
+	}
+	RCryptoJob *cj = r_crypto_use (core->crypto, algo);
+	if (cj && cj->h->type == R_CRYPTO_TYPE_SIGNATURE) {
+		if (r_crypto_job_set_key (cj, binkey, keylen, 0, R_CRYPTO_DIR_ENCRYPT)) {
+			r_crypto_job_update (cj, (const ut8 *)core->block, core->blocksize);
+			int result_size = 0;
+			ut8 *result = r_crypto_job_get_output (cj, &result_size);
+			if (result) {
+				if (!r_core_write_at (core, core->offset, result, result_size)) {
+					R_LOG_ERROR ("write failed at 0x%08" PFMT64x, core->offset);
+				}
+				R_LOG_INFO ("Written %d byte(s)", result_size);
+				free (result);
+			}
+		}
+		free (binkey);
+		return;
+	} else {
+		R_LOG_ERROR ("Unknown signature algorithm '%s'", algo);
+	}
+	return;
 }
 
 static void cmd_write_bits(RCore *core, int set, ut64 val) {
@@ -389,7 +428,7 @@ static int cmd_wo(void *data, const char *input) {
 			}
 			algo = args;
 			if (R_STR_ISNOTEMPTY (algo) && key) {
-				encrypt_or_decrypt_block (core, algo, key, direction, iv);
+				write_encrypted_block (core, algo, key, direction, iv);
 			} else {
 				r_crypto_list (core->crypto, r_cons_printf, 0 | (int)R_CRYPTO_TYPE_ENCRYPT << 8);
 				r_core_cmd_help_match_spec (core, help_msg_wo, "wo", input[0]);
@@ -397,6 +436,26 @@ static int cmd_wo(void *data, const char *input) {
 			free (args);
 		}
 		break;
+		case 'S': // "woS" sign
+		{
+			const char *algo = NULL;
+			const char *key = NULL;
+			char *space, *args = strdup (r_str_trim_head_ro (input + 1));
+			space = strchr (args, ' ');
+			if (space) {
+				*space++ = 0;
+				key = space;
+				space = strchr (key, ' ');
+			}
+			algo = args;
+			if (R_STR_ISNOTEMPTY (algo) && key) {
+				write_block_signature (core, algo, key);
+			} else {
+				r_crypto_list (core->crypto, r_cons_printf, 0 | (int)R_CRYPTO_TYPE_SIGNATURE << 8);
+				r_core_cmd_help_match_spec (core, help_msg_wo, "wo", input[0]);
+			}
+			free (args);
+		} break;
 	case 'p': // debruijn patterns
 		switch (input[1]) {
 		case 'D': // "wopD"
