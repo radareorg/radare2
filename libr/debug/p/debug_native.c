e7341a55d8a351eb3fedfb969eb2b84c2a2c3351
Chédotal Julien <rjdi@wanadoo.fr>
2024-10-07 01:44:37 +0200

    Add xmm registers for the FreeBSD debugger ##debug

--- libr/debug/p/debug_native.c
+++ libr/debug/p/debug_native.c
@@ -661,13 +661,19 @@ static int bsd_reg_read(RDebug *dbg, int type, ut8* buf, int size) {
 		return true;
 		break;
 	case R_REG_TYPE_FPU:
+	case R_REG_TYPE_VEC64: // MMX
 	   ret = ptrace (PT_GETFPREGS, pid, (caddr_t)buf, sizeof (struct fpreg));
 	   break;
-	case R_REG_TYPE_VEC64: // MMX
 	case R_REG_TYPE_VEC128: // XMM
 	case R_REG_TYPE_VEC256: // YMM
 	case R_REG_TYPE_VEC512: // ZMM
-		// not implemented
+#if __KFBSD__
+		struct ptrace_xstate_info info;
+		ret = ptrace (PT_GETXSTATE_INFO, pid, (caddr_t)&info, sizeof (info));
+		if (info.xsave_len != 0) {
+		ret = ptrace (PT_GETXSTATE, pid, (caddr_t)buf, info.xsave_len);
+		}
+#endif
 		break;
 	case R_REG_TYPE_SEG:
 	case R_REG_TYPE_FLG:
--- libr/debug/p/native/bsd/bsd_debug.c
+++ libr/debug/p/native/bsd/bsd_debug.c
@@ -118,8 +118,20 @@ bool bsd_reg_write(RDebug *dbg, int type, const ut8 *buf, int size) {
 #endif
 		break;
 	case R_REG_TYPE_FPU:
+	case R_REG_TYPE_VEC64: // MMX
 		r = ptrace (PT_SETFPREGS, dbg->pid, (caddr_t)buf, sizeof (struct fpreg));
 		break;
+	case R_REG_TYPE_VEC128: // XMM
+	case R_REG_TYPE_VEC256: // YMM
+	case R_REG_TYPE_VEC512: // ZMM
+#if __KFBSD__
+		struct ptrace_xstate_info info;
+		r = ptrace (PT_GETXSTATE_INFO, dbg->pid, (caddr_t)&info, sizeof (info));
+		if (info.xsave_len != 0) {
+			r = ptrace (PT_SETXSTATE, dbg->pid,  (caddr_t)buf, info.xsave_len);
+		}
+#endif
+	      break;
 	}
 	return r == 0;
 }
