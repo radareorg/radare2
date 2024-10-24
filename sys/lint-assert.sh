5d177df6b8e8f3c05007ae9d3c45d03811a51b19
astralia <astraliaz@gmail.com>
2024-10-24 17:47:43 +0200

    Exclude functions taking no arguments

--- sys/lint-assert.sh
+++ sys/lint-assert.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git grep -C1 R_API libr | awk '/R_API/ && !/NULL/ {getline nextLine; if (nextLine !~ /R_RETURN/) print $0}' | grep 'c:'
+git grep -C1 R_API libr | awk '/R_API/ && !/NULL/ && !/(void)/ {getline nextLine; if (nextLine !~ /R_RETURN/) print $0}' | grep 'c:'
