547bfbb0cde100a6087cfc313eb043304a5b0f0b
pancake <pancake@nopcode.org>
2024-11-07 03:08:50 +0100

    Change macro syntax to be more lispy

--- libr/core/cmd.c
+++ libr/core/cmd.c
@@ -3814,6 +3814,7 @@ static char *parse_tmp_evals(RCore *core, const char *str) {
 }
 
 static bool is_macro_command(const char *ptr) {
+return false;
 	if (!strchr (ptr, ')')) {
 		return false;
 	}
