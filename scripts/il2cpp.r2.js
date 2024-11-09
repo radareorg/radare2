59e21269fa4e7fc326c5f7f5e5905545b9547183
pancake <pancake@nopcode.org>
2024-11-09 16:09:21 +0100

    Use il.baddr in the il2cpp script

--- scripts/il2cpp.r2.js
+++ scripts/il2cpp.r2.js
@@ -2,27 +2,33 @@
  function flagName(s) {
    return r2.call('fD ' + s).trim();
  }
+ const baddr = r2.cmd("?vx il.baddr 2> /dev/null");
  const script = JSON.parse(r2.cmd("cat script.json"));
  const commands = [];
+ console.error("Using il.baddr = " + baddr);
  console.error("Loading methods...");
  for (const method of script.ScriptMethod) {
    const fname = flagName(method.Name);
-   commands.push("f sym.il." + fname + " = " + method.Address);
+   const faddr = method.Address + baddr;
+   commands.push("f sym.il." + fname + " = " + faddr);
  }
  console.error("Loading strings...");
  for (const str of script.ScriptString) {
    const fname = flagName(str.Value);
-   commands.push("f str.il." + fname + " = " + str.Address);
+   const faddr = str.Address + baddr;
+   commands.push("f str.il." + fname + " = " + faddr);
  }
  console.error("Loading IL metadata...");
  for (const meta of script.ScriptMetadata) {
    const fname = flagName(meta.Name) + (meta.Address & 0xfff);
-   commands.push("f il.meta." + fname + " = " + meta.Address);
+   const faddr = meta.Address + baddr;
+   commands.push("f il.meta." + fname + " = " + faddr);
  }
  console.error("Loading IL methods metadata...");
  for (const meta of script.ScriptMetadataMethod) {
    const fname = flagName(meta.Name) + (meta.Address & 0xfff);
-   commands.push("f il.meta.method." + fname + " = " + meta.Address);
+   const faddr = meta.Address + baddr;
+   commands.push("f il.meta.method." + fname + " = " + faddr);
  }
  console.error("Importing flags...");
  for (const cmd of commands) {
