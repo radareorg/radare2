edbc14b86505701d2907203b24a36e0838c0c0ba
Sylvain Pelissier <sylvain.pelissier@gmail.com>
2024-11-10 21:23:34 +0100

    Add ssl builds in the CI and add the SipHash SSL plugin ##crypto

--- libr/crypto/hash/deps.mk
+++ libr/crypto/hash/deps.mk
@@ -1,14 +1,17 @@
 
-OBJS+=hash/state.o hash/hash.o hash/hamdist.o hash/crca.o hash/fletcher.o hash/sip.o
+OBJS+=hash/state.o hash/hash.o hash/hamdist.o hash/crca.o hash/fletcher.o
 OBJS+=hash/entropy.o hash/hcalc.o hash/adler32.o hash/luhn.o hash/ssdeep.o
 
 ifeq ($(WANT_SSL_CRYPTO),1)
 CFLAGS+=${SSL_CFLAGS}
 LDFLAGS+=${SSL_LDFLAGS}
+LDFLAGS+=-lcrypto
 LINK+=${SSL_LDFLAGS}
-#else
-# OBJS+=hash/md4.o hash/md5.o hash/sha1.o hash/sha2.o
+OBJS+=hash/sip_ssl.o
+else
+OBJS+=hash/sip.o
 endif
+
 OBJS+=hash/md4.o hash/md5.o hash/sha1.o hash/sha2.o
 
 ifeq ($(USE_LIB_XXHASH),1)
