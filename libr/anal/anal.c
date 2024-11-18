386e94dd5f89b9845504d106e32e869b062e27ef
condret <condr3t@protonmail.com>
2024-11-18 23:24:31 +0100

    Remove needless uses of r_io_bind_init

--- libr/anal/anal.c
+++ libr/anal/anal.c
@@ -125,7 +125,6 @@ R_API RAnal *r_anal_new(void) {
 	anal->diff_thbb = R_ANAL_THRESHOLDBB;
 	anal->diff_thfcn = R_ANAL_THRESHOLDFCN;
 	anal->syscall = r_syscall_new ();
-	r_io_bind_init (anal->iob);
 	r_flag_bind_init (anal->flb);
 	anal->reg = r_reg_new ();
 	anal->last_disasm_reg = NULL;
