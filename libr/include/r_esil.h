b31dbafc8e004edad6d507d3f3b2ca005bc9a3a5
condret <condr3t@protonmail.com>
2024-11-26 06:42:57 +0100

    Add esil voyeurism api ##esil

--- libr/include/r_esil.h
+++ libr/include/r_esil.h
@@ -153,6 +153,32 @@ typedef struct r_esil_register_interface_t {
 	REsilRegSize reg_size;
 } REsilRegInterface;
 
+typedef void (*REsilVoyeurRegRead)(void *user, const char *name, ut64 val);
+typedef void (*REsilVoyeurRegWrite)(void *user, const char *name, ut64 old, ut64 val);
+typedef void (*REsilVoyeurMemRead)(void *user, ut64 addr, const ut8 *buf, int len);
+typedef	void (*REsilVoyeurMemWrite)(void *user, ut64 addr, const ut8 *buf, const ut8 *old, int len);
+
+typedef struct r_esil_voyeur_t {
+	void *user;
+	union {
+		REsilVoyeurRegRead reg_read;
+		REsilVoyeurRegWrite reg_write;
+		REsilVoyeurMemRead mem_read;
+		REsilVoyeurMemWrite mem_write;
+		void *vfn;
+	};
+} REsilVoyeur;
+
+typedef enum {
+	R_ESIL_VOYEUR_REG_READ = 0,
+	R_ESIL_VOYEUR_REG_WRITE,
+	R_ESIL_VOYEUR_MEM_READ,
+	R_ESIL_VOYEUR_MEM_WRITE,
+	R_ESIL_VOYEUR_LAST,
+} REsilVoyeurType;
+
+#define	MAX_VOYEURS	(UT32_MAX ^ (0x7 << 29))
+
 typedef struct r_esil_options_t {
 	int nowrite;
 	int iotrap;
@@ -203,6 +229,7 @@ typedef struct r_esil_t {
 	REsilTrace *trace;
 	REsilRegInterface reg_if;
 	REsilMemInterface mem_if;
+	RIDStorage voyeur[R_ESIL_VOYEUR_LAST];
 	REsilCallbacks cb;
 	REsilCallbacks ocb;
 	bool ocb_set;
@@ -256,6 +283,14 @@ R_API REsil *r_esil_new_ex(int stacksize, bool iotrap, ut32 addrsize,
 //this should replace existing r_esil_new
 R_API REsil *r_esil_new_simple(ut32 addrsize, void *reg, void *iob);
 //R_API REsil *r_esil_new_simple(ut32 addrsize, struct r_reg_t *reg, struct r_io_bind_t *iob);
+R_API st32 r_esil_add_voyeur(REsil *esil, void *user, void *vfn, REsilVoyeurType vt);
+#define	R_ESIL_ADD_VOYEUR(x, y, z)	_Generic((z),							\
+	REsilVoyeurRegRead:	r_esil_add_voyeur ((x), (y), (void *)(z), R_ESIL_VOYEUR_REG_READ),	\
+	REsilVoyeurRegWrite:	r_esil_add_voyeur ((x), (y), (void *)(z), R_ESIL_VOYEUR_REG_WRITE),	\
+	REsilVoyeurMemRead:	r_esil_add_voyeur ((x), (y), (void *)(z), R_ESIL_VOYEUR_MEM_READ),	\
+	REsilVoyeurMemWrite:	r_esil_add_voyeur ((x), (y), (void *)(z), R_ESIL_VOYEUR_MEM_WRITE),	\
+	default: -1)
+R_API void r_esil_del_voyeur(REsil *esil, st32 vid);
 R_API void r_esil_reset(REsil *esil);
 R_API void r_esil_set_pc(REsil *esil, ut64 addr);
 R_API bool r_esil_setup(REsil *esil, struct r_anal_t *anal, bool romem, bool stats, bool nonull);
