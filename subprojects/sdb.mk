cd9eca4da9dc66fcab5638faed0a1c90160bb4bd
pancake <pancake@nopcode.org>
2024-11-27 20:16:23 +0100

    Initial support for subproject's (acr|meson)-wrap files ##build

---
+++ subprojects/sdb.mk
@@ -0,0 +1,15 @@
+WRAP_wrap_git_url:=https://github.com/radareorg/sdb.git
+WRAP_wrap_git_revision:=2.0.1
+WRAP_wrap_git_directory:=sdb
+WRAP_wrap_git_depth:=1
+
+sdb_all: sdb
+	@echo "Nothing to do"
+
+sdb:
+	git clone --no-checkout --depth=1 https://github.com/radareorg/sdb.git sdb
+	cd sdb && git fetch --depth=1 origin 2.0.1
+	cd sdb && git checkout
+
+sdb_clean:
+	rm -rf sdb
