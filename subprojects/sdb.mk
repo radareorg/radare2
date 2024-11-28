017140c2ecb4294dc6af7a53612b4e235aaebdcc
pancake <pancake@nopcode.org>
2024-11-28 12:49:55 +0100

    hu

--- subprojects/sdb.mk
+++ subprojects/sdb.mk
@@ -1,5 +1,6 @@
 WRAP_wrap_git_url:=https://github.com/radareorg/sdb.git
-WRAP_wrap_git_revision:=2.0.1
+WRAP_wrap_git_revision:=3ad531a5413d2c75fcfbf80f3d405e3a2c420a33
+# revision = 2.0.1
 WRAP_wrap_git_directory:=sdb
 WRAP_wrap_git_depth:=1
 
@@ -8,7 +9,7 @@ sdb_all: sdb
 
 sdb:
 	git clone --no-checkout --depth=1 https://github.com/radareorg/sdb.git sdb
-	cd sdb && git fetch --depth=1 origin 2.0.1
+	cd sdb && git fetch --depth=1 origin 3ad531a5413d2c75fcfbf80f3d405e3a2c420a33
 	cd sdb && git checkout
 
 sdb_clean:
