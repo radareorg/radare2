92b84b957540efccab5c39ada0871529d6815279
pancake <pancake@nopcode.org>
2024-12-03 14:00:42 +0100

    flex

--- libr/arch/p/arm/v35/deps-arm64.mk
+++ libr/arch/p/arm/v35/deps-arm64.mk
@@ -2,7 +2,6 @@ V35ARM64_HOME=$(SPRJ)/v35arm64
 V35ARM64_SRCDIR=$(V35ARM64_HOME)/disassembler/
 
 V35ARM64_CFLAGS=-I$(V35ARM64_SRCDIR)
-# V35ARM64_OBJS+=arm64dis.o
 # V35ARM64_OBJS+=encodings.o
 # V35ARM64_OBJS+=test.o
 V35ARM64_OBJS+=decode.o
@@ -20,16 +19,16 @@ V35ARM64_OBJS+=pcode.o
 V35ARM64_OBJS+=regs.o
 V35ARM64_OBJS+=sysregs.o
 V35ARM64_LINK=$(addprefix $(V35ARM64_SRCDIR),$(V35ARM64_OBJS))
-V35ARM64_LIBS=$(V35ARM64_HOME)/arm64dis.a
+V35ARM64_LIBS=$(V35ARM64_HOME)/v35arm64.a
 
 ${V35ARM64_LINK}: $(V35ARM64_SRCDIR)
 $(V35ARM64_SRCDIR):
 
 #	$(MAKE) -C $(V35ARM64_HOME) arch-arm64
-#	$(MAKE) $(V35ARM64_HOME)/arm64dis.a
+#	$(MAKE) $(V35ARM64_HOME)/v35arm64.a
 #	$(MAKE) git-clone-arm64v35
 
-git-clone-arm64v35: # $(V35ARM64_HOME)/arm64dis.a:
+git-clone-arm64v35:
 	$(MAKE) -C $(V35ARM64_HOME) arch-arm64
 
 .PHONY: git-clone-arm64v35
