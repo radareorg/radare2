c562c0e3da2fdef1db9f7533c9624fb0ce163958
satk0 <satko_a@op.pl>
2024-12-17 21:09:19 +0100

    Try to patch qjs with meson.build

--- subprojects/qjs.mk
+++ subprojects/qjs.mk
@@ -2,15 +2,18 @@
 
 WRAP_wrap_git_url:=https://github.com/quickjs-ng/quickjs
 WRAP_wrap_git_revision:=7e292050a21d3dd5076f70116ae95cc5200c40c1
+WRAP_wrap_git_patch_directory:=qjs-patches
 WRAP_wrap_git_directory:=qjs
 WRAP_wrap_git_depth:=1
 
 qjs_all: qjs
-	@echo jeje
+	@echo "Nothing to do"
 
 qjs:
-	git clone https://github.com/quickjs-ng/quickjs qjs
-	cd qjs && git checkout 7e292050a21d3dd5076f70116ae95cc5200c40c1
+	git clone --no-checkout --depth=1 https://github.com/quickjs-ng/quickjs qjs
+	cd qjs && git fetch --depth=1 origin 7e292050a21d3dd5076f70116ae95cc5200c40c1
+	cd qjs && git checkout
+	cp -f qjs-patches/* qjs
 
 qjs_clean:
 	rm -rf qjs
