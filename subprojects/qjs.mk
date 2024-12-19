deed636804c8c58c5d5e07e82b37ad597d346dc5
satk0 <satko_a@op.pl>
2024-12-19 21:48:51 +0100

    Replace win ci/cd frida meson with standard meson ##build

--- subprojects/qjs.mk
+++ subprojects/qjs.mk
@@ -13,7 +13,7 @@ qjs:
 	git clone --no-checkout --depth=1 https://github.com/quickjs-ng/quickjs qjs
 	cd qjs && git fetch --depth=1 origin 7e292050a21d3dd5076f70116ae95cc5200c40c1
 	cd qjs && git checkout
-	cp -f qjs/* qjs
+	cp -f packagefiles/qjs/* qjs
 
 qjs_clean:
 	rm -rf qjs
