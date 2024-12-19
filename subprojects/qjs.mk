e80ae7874d24e1f9cc9d25f9027f24c550855899
satk0 <satko_a@op.pl>
2024-12-19 21:39:03 +0100

    Generate qjs.mk

--- subprojects/qjs.mk
+++ subprojects/qjs.mk
@@ -3,8 +3,8 @@
 WRAP_wrap_git_url:=https://github.com/quickjs-ng/quickjs
 WRAP_wrap_git_revision:=7e292050a21d3dd5076f70116ae95cc5200c40c1
 WRAP_wrap_git_directory:=qjs
+WRAP_wrap_git_patch_directory:=qjs
 WRAP_wrap_git_depth:=1
-WRAP_wrap_git_diff_files:=qjs/meson.patch
 
 qjs_all: qjs
 	@echo "Nothing to do"
@@ -13,6 +13,7 @@ qjs:
 	git clone --no-checkout --depth=1 https://github.com/quickjs-ng/quickjs qjs
 	cd qjs && git fetch --depth=1 origin 7e292050a21d3dd5076f70116ae95cc5200c40c1
 	cd qjs && git checkout
+	cp -f qjs/* qjs
 
 qjs_clean:
 	rm -rf qjs
