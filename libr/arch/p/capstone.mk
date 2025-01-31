daf039e7f849e11ba63b096e431b85af1f9e0f63
satk0 <satko_a@op.pl>
2025-01-31 23:34:20 +0100

    Fix cloning capstone with make subproject

--- libr/arch/p/capstone.mk
+++ libr/arch/p/capstone.mk
@@ -1,19 +1,29 @@
-ifeq ($(WANT_CAPSTONE),1)
-ifeq ($(USE_CAPSTONE),1)
-CS_CFLAGS=${CAPSTONE_CFLAGS}
-CS_LDFLAGS=${CAPSTONE_LDFLAGS}
+ifeq ($(USE_CSNEXT),1)
+  CS_ROOT=../../../subprojects/capstone-next
 else
-CS_CFLAGS=-I../../shlr/capstone/include
-CS_CFLAGS+=-I../../../shlr/capstone/include
-CS_CFLAGS+=-I../../shlr/capstone/include/capstone
-CS_CFLAGS+=-I../../../shlr/capstone/include/capstone
-CS_LDFLAGS=$(SHLR)/capstone/libcapstone.a
+ifeq ($(USE_CS4),1)
+  CS_ROOT=../../../subprojects/capstone-v4
+else
+  CS_ROOT=../../../subprojects/capstone-v5
+endif
 endif
 
-ifeq ($(CS_CFLAGS_INCLUDED),)
-CFLAGS+=$(CS_CFLAGS)
-CS_CFLAGS_INCLUDED=Yes
-endif
+ifeq ($(WANT_CAPSTONE),1)
+  ifeq ($(USE_CAPSTONE),1)
+    CS_CFLAGS=${CAPSTONE_CFLAGS}
+    CS_LDFLAGS=${CAPSTONE_LDFLAGS}
+  else
+    CS_CFLAGS=-I$(CS_ROOT)/include
+    CS_CFLAGS+=-I$(CS_ROOT)/include/capstone
+    CS_LDFLAGS=$(CS_ROOT)/libcapstone.a
+  endif
+
+  $(info [cs_flags] - $(CS_CFLAGS))
+
+  ifeq ($(CS_CFLAGS_INCLUDED),)
+    CFLAGS+=$(CS_CFLAGS)
+    CS_CFLAGS_INCLUDED=Yes
+  endif
 else
-CS_CFLAGS=
+  CS_CFLAGS=
 endif
