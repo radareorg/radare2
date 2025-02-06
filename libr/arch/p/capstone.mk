30851527aa062f9bd31698c3f4b22698eff46afd
satk0 <satko_a@op.pl>
2025-02-06 19:57:04 +0100

    Support cs4

--- libr/arch/p/capstone.mk
+++ libr/arch/p/capstone.mk
@@ -3,16 +3,9 @@ ifeq ($(WANT_CAPSTONE),1)
     CS_CFLAGS=${CAPSTONE_CFLAGS}
     CS_LDFLAGS=${CAPSTONE_LDFLAGS}
   else
-    #CS_CFLAGS=-I../../shlr/capstone/include
     CS_CFLAGS=-I$(CS_ROOT)/include
-    #CS_CFLAGS+=-I../$(CS_ROOT)/include
-    #CS_CFLAGS+=-I../../shlr/capstone/include/capstone
-    #CS_CFLAGS+=-I$(CS_ROOT)/include/capstone
-    #CS_CFLAGS+=-I$(CS_ROOT)/include/capstone
     CS_LDFLAGS=$(CS_ROOT)/libcapstone.a
   endif
-
-  $(info [cs_flags] - $(CS_CFLAGS))
 
   ifeq ($(CS_CFLAGS_INCLUDED),)
     CFLAGS+=$(CS_CFLAGS)
