348437bb8d25eb3524767b50b2bcc1af895a6564
Adam Satko <54475808+satk0@users.noreply.github.com>
2025-02-06 22:12:17 +0100

    Make Capstone a subproject ##build
    * Deprecate support for capstone v3

--- libr/arch/p/capstone.mk
+++ libr/arch/p/capstone.mk
@@ -1,19 +1,16 @@
 ifeq ($(WANT_CAPSTONE),1)
-ifeq ($(USE_CAPSTONE),1)
-CS_CFLAGS=${CAPSTONE_CFLAGS}
-CS_LDFLAGS=${CAPSTONE_LDFLAGS}
-else
-CS_CFLAGS=-I../../shlr/capstone/include
-CS_CFLAGS+=-I../../../shlr/capstone/include
-CS_CFLAGS+=-I../../shlr/capstone/include/capstone
-CS_CFLAGS+=-I../../../shlr/capstone/include/capstone
-CS_LDFLAGS=$(SHLR)/capstone/libcapstone.a
-endif
+  ifeq ($(USE_CAPSTONE),1)
+    CS_CFLAGS=${CAPSTONE_CFLAGS}
+    CS_LDFLAGS=${CAPSTONE_LDFLAGS}
+  else
+    CS_CFLAGS=-I$(CS_ROOT)/include
+    CS_LDFLAGS=$(CS_ROOT)/libcapstone.a
+  endif
 
-ifeq ($(CS_CFLAGS_INCLUDED),)
-CFLAGS+=$(CS_CFLAGS)
-CS_CFLAGS_INCLUDED=Yes
-endif
+  ifeq ($(CS_CFLAGS_INCLUDED),)
+    CFLAGS+=$(CS_CFLAGS)
+    CS_CFLAGS_INCLUDED=Yes
+  endif
 else
-CS_CFLAGS=
+  CS_CFLAGS=
 endif
--- subprojects/v35armv7.mk
+++ subprojects/v35armv7.mk
@@ -11,7 +11,7 @@ v35armv7_all: v35armv7
 v35armv7:
 	git clone --no-checkout --depth=1 https://github.com/radareorg/vector35-arch-armv7 v35armv7
 	cd v35armv7 && git fetch --depth=1 origin radare2-2024
-	cd v35armv7 && git checkout
+	cd v35armv7 && git checkout FETCH_HEAD
 
 v35armv7_clean:
 	rm -rf v35armv7
