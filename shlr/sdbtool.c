8d4b6399389759d4ebd1122184c25afc0e43143e
pancake <pancake@nopcode.org>
2025-03-24 01:17:00 +0100

    ii

--- shlr/sdbtool.c
+++ shlr/sdbtool.c
@@ -1,5 +1,22 @@
 #include <r_util.h>
 
+static bool dothec(const char *file_txt, const char *file_c) {
+	// TODO: there's no api in sdb to do that yet, its all hardcoded in sdb.c
+	// Sdb *db = sdb_new (NULL, file_c, 0);
+	// sdb_free (db);
+}
+
+static bool dothesdb(const char *file_txt, const char *file_sdb) {
+	Sdb *db = sdb_new (NULL, file_sdb, 0);
+	if (sdb_text_load (db, file_txt)) {
+		eprintf ("maked %s\n", file_sdb);
+		sdb_sync (db);
+	} else {
+		eprintf ("Failed to parse %s\n", file_txt);
+	}
+	sdb_free (db);
+}
+
 static bool dothething(const char *basedir, const char *file_txt) {
 	char *file_sdb = r_str_ndup (file_txt, strlen (file_txt) - 4);
 	char *file_c = strdup (file_sdb);
@@ -7,12 +24,12 @@ static bool dothething(const char *basedir, const char *file_txt) {
 
 	if (!r_file_exists (file_c) || r_file_is_newer (file_txt, file_c)) {
 		R_LOG_INFO ("newer %s", file_c);
+		dothec (file_txt, file_c);
 	}
 	if (!r_file_exists (file_sdb) || r_file_is_newer (file_txt, file_sdb)) {
 		R_LOG_INFO ("newer %s", file_sdb);
+		dothesdb (file_txt, file_sdb);
 	}
-
-	// R_LOG_INFO ("%s (%s) (%s)", file_txt, file_sdb, file_c);
 	free (file_c);
 	free (file_sdb);
 	return true;
