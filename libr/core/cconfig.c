3f1ad5e4cecd28e1f4da88798fbd2d0db341d4b8
pancake <pancake@nopcode.org>
2025-04-22 00:24:30 +0200

    fi

--- libr/core/cconfig.c
+++ libr/core/cconfig.c
@@ -1591,9 +1591,7 @@ static bool cb_color(void *user, void *data) {
 	}
 	r_cons_singleton ()->context->color_mode = (node->i_value > COLOR_MODE_16M)
 		? COLOR_MODE_16M: node->i_value;
-	if (node->i_value) {
-		r_cons_pal_reload ();
-	}
+	r_cons_pal_reload ();
 	r_print_set_flags (core->print, core->print->flags);
 	r_log_set_colors (node->i_value);
 	return true;
