235c8e703030150a5cc308ce9baf159350b05f1b
pancake <pancake@nopcode.org>
2025-04-22 12:59:08 +0200

    fi

--- libr/arch/include/symcat.h
+++ libr/arch/include/symcat.h
@@ -11,7 +11,7 @@
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
- 
+
    You should have received a copy of the GNU General Public License along
    with this program; if not, write to the Free Software Foundation, Inc.,
    51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.  */
@@ -44,12 +44,5 @@
 #define XCONCAT4(a,b,c,d) CONCAT4(a,b,c,d)
 #define XCONCAT5(a,b,c,d,e) CONCAT5(a,b,c,d,e)
 #define XCONCAT6(a,b,c,d,e,f) CONCAT6(a,b,c,d,e,f)
-
-/* Note the layer of indirection here is typically used to allow
-   stringification of the expansion of macros.  I.e. "#define foo
-   bar", "XSTRING(foo)", to yield "bar".  Be aware that this only
-   works for __STDC__, not for traditional C which will still resolve
-   to "foo".  */
-#define XSTRING(s) STRINGX(s) 
 
 #endif /* SYM_CAT_H */
