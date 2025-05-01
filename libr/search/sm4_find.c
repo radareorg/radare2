e4a26ce5d7f9b462565eb21a256822709d1a0fcc
pancake <trufae@gmail.com>
2025-05-01 13:04:30 +0200

    RSearch /j uses 'addr' instead of 'offset' ##search

--- libr/search/sm4_find.c
+++ libr/search/sm4_find.c
@@ -12,14 +12,14 @@ static bool sm4_key_test(const unsigned char *buf) {
 }
 
 // Display the corresponding master key which is not directly in memory for SM4.
-static void sm4_master_key(const unsigned char *buf, ut64 offset) {
+static void sm4_master_key(const unsigned char *buf, ut64 addr) {
 	ut32 *ptr = (ut32 *)buf;
 	ut32 master_key[4] = { 0 };
 	master_key[3] = ptr[3] ^ (sm4_RK (ptr[2] ^ ptr[1] ^ ptr[0] ^ sm4_CK[3]));
 	master_key[2] = ptr[2] ^ (sm4_RK (ptr[1] ^ ptr[0] ^ master_key[3] ^ sm4_CK[2]));
 	master_key[1] = ptr[1] ^ (sm4_RK (ptr[0] ^ master_key[3] ^ master_key[2] ^ sm4_CK[1]));
 	master_key[0] = ptr[0] ^ (sm4_RK (master_key[3] ^ master_key[2] ^ master_key[1] ^ sm4_CK[0]));
-	R_LOG_INFO ("Master key found: %08x%08x%08x%08x @%#8"PFMT64x, FK[0] ^ master_key[0], FK[1] ^ master_key[1], FK[2] ^ master_key[2], FK[3] ^ master_key[3], offset);
+	R_LOG_INFO ("Master key found: %08x%08x%08x%08x @%#8"PFMT64x, FK[0] ^ master_key[0], FK[1] ^ master_key[1], FK[2] ^ master_key[2], FK[3] ^ master_key[3], addr);
 	return;
 }
 
