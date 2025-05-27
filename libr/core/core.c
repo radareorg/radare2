66427fa63a54896c1b5ce6797a7324ab8c0b0928
condret <condr3t@protonmail.com>
2025-05-27 14:20:24 +0200

    Initialize core_esil on core init and autoupdate on arch config changes

--- libr/core/core.c
+++ libr/core/core.c
@@ -2754,6 +2754,9 @@ R_API bool r_core_init(RCore *core) {
 	core->dbg->cb_printf = r_cons_printf;
 	core->dbg->bp->cb_printf = r_cons_printf;
 	core->dbg->ev = core->ev;
+#if USE_NEW_ESIL
+	r_core_esil_init (core);
+#endif
 	r_core_config_init (core);
 	r_core_loadlibs_init (core);
 	//r_core_loadlibs (core);
@@ -2859,6 +2862,9 @@ R_API void r_core_fini(RCore *c) {
 	/*
 	r_unref (c->anal->config);
 	*/
+#if USE_NEW_ESIL
+	r_core_esil_fini (&c->esil);
+#endif
 	if (c->anal->esil) {
 		c->anal->esil->anal = NULL;
 	}
