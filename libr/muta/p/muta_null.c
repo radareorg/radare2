4f11a7a2daa5caa8c68870021d3229068329d058
pancake <pancake@nopcode.org>
2025-06-02 20:47:52 +0200

    compila

---
+++ libr/muta/p/muta_null.c
@@ -0,0 +1,21 @@
+/* radare - LGPL - Copyright 2025 - pancake */
+
+#include <r_muta.h>
+
+RMutaPlugin r_muta_plugin_ror = {
+	.type = R_MUTA_TYPE_HASH,
+	.meta = {
+		.name = "null",
+		.desc = "mutate nothing",
+		.author = "pancake",
+		.license = "MIT",
+	}
+};
+
+#ifndef R2_PLUGIN_INCORE
+R_API RLibStruct radare_plugin = {
+	.type = R_LIB_TYPE_CRYPTO,
+	.data = &r_muta_plugin_ror,
+	.version = R2_VERSION
+};
+#endif
