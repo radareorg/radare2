12bb247d6a73b5e5ae0f0a2503e2a3c3b5b03474
l0kh <122721783+l0kh@users.noreply.github.com>
2025-06-04 15:52:07 +0200

    Fix att imm color display ##disasm

--- libr/util/print.c
+++ libr/util/print.c
@@ -2346,15 +2346,7 @@ R_API char* r_print_colorize_opcode(RPrint *print, char *p, const char *reg, con
 
 	memset (o, 0, COLORIZE_BUFSIZE);
 	for (i = j = 0; p[i]; i++, j++) {
-		if (i > 0 && p[i - 1] == ' ' && (p[i] == '$')) {
-			if (p[i + 1] == '0') {
-				snprintf (o + j, COLORIZE_BUFSIZE - j, "%s$0", num);
-			} else {
-				snprintf (o + j, COLORIZE_BUFSIZE - j, "%s$", num);
-			}
-			i += strlen (o + j);
-			j += strlen (o + j);
-		} else if (i > 0 && p[i - 1] == ' ' && (p[i] == '0' && p[i + 1] == 0)) {
+		if (i > 0 && p[i - 1] == ' ' && (p[i] == '0' && p[i + 1] == 0)) {
 			snprintf (o + j, COLORIZE_BUFSIZE - j, "%s0", num);
 			j += strlen (o + j);
 			i++;
@@ -2367,7 +2359,7 @@ R_API char* r_print_colorize_opcode(RPrint *print, char *p, const char *reg, con
 		}
 		/* colorize numbers */
 		if ((ishexprefix (p + i) && previous != ':') \
-		     || (p[i] == '$' || (isdigit (p[i] & 0xff) && issymbol (previous)))) {
+		     || (isdigit (p[i] & 0xff) && issymbol (previous))) {
 			const char *num2 = num;
 			ut64 n = r_num_get (NULL, p + i);
 			const char *name = print->offname (print->user, n)? color_flag: NULL;
@@ -2407,6 +2399,9 @@ R_API char* r_print_colorize_opcode(RPrint *print, char *p, const char *reg, con
 			o[j++] = p[i++];
 #endif
 		case '$':
+			snprintf (o + j, COLORIZE_BUFSIZE - j, "%s$", num);
+			j += strlen (o + j);
+			i++;
 			break;
 		case '+':
 		case '-':
