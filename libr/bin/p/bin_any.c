d85bab8f030d4d9142278180128eb8e734c0d904
pancake <pancake@nopcode.org>
2025-06-11 17:48:25 +0200

    fm

--- libr/bin/p/bin_any.c
+++ libr/bin/p/bin_any.c
@@ -38,7 +38,8 @@ static void fini(RBinFile *bf) {
 RBinPlugin r_bin_plugin_any = {
 	.meta = {
 		.name = "any",
-		.desc = "RMagic based loader",
+		.desc = "Dummy loader based on RMagic",
+		.author = "pancake",
 		.license = "LGPL-3.0-only",
 	},
 	.load = load,
