dfaaa0afccd0844493345c6ab22c7be9ab2c4912
pancake <pancake@nopcode.org>
2025-06-20 20:45:20 +0200

    Move back the codemeta json function

--- libr/include/r_codemeta.h
+++ libr/include/r_codemeta.h
@@ -79,6 +79,7 @@ R_API void r_codemeta_add_item(RCodeMeta *code, RCodeMetaItem *annotation);
 R_API void r_codemeta_print(RCodeMeta *code, RVector *line_offsets);
 R_API void r_codemeta_print_disasm(RCodeMeta *code, RVector *line_offsets, void *anal);
 R_API void r_codemeta_print_comment_cmds(RCodeMeta *code);
+R_API char *r_codemeta_print_json(RCodeMeta *code);
 
 // compatibility with 5.2.0
 #define r_codemeta_add_annotation r_codemeta_add_item
