c6d120577fb86604485fd2963bd47dc76ddb2c74
pancake <pancake@nopcode.org>
2025-06-25 16:53:28 +0200

    fi

--- libr/include/r_cons.h
+++ libr/include/r_cons.h
@@ -874,6 +874,7 @@ R_API void r_kons_break_end(RCons *cons);
 // TODO all the w32 apis must be ipi
 R_IPI int win_is_vtcompat(void);
 R_API void r_kons_clear(RCons *cons);
+R_API void r_cons_clear_buffer(RCons *cons);
 R_API void r_cons_win_gotoxy(RCons *cons, int fd, int x, int y);
 R_API int r_cons_win_print(RCons *cons, const char *ptr, int len, bool vmode);
 R_API int r_cons_win_printf(RCons *cons, bool vmode, const char *fmt, ...) R_PRINTF_CHECK(3, 4);
