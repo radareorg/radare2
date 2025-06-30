d6e1594d9618a7a09d04641e943f3693c73fc6b7
pancake <pancake@nopcode.org>
2025-06-30 19:57:06 +0200

    jilj

--- test/unit/test_thread_channel.c
+++ test/unit/test_thread_channel.c
@@ -13,10 +13,10 @@ static RThreadFunctionRet consumer_function(RThread *th) {
 		if (!msg) {
 			break;
 		}
-		
+
 		// Increment counter based on received message
 		consumer_count += *(int *)msg->msg;
-		
+
 		// Send response back (this is crucial for promise-based synchronization)
 		r_th_channel_post(tc, msg);
 	}
@@ -26,118 +26,119 @@ static RThreadFunctionRet consumer_function(RThread *th) {
 bool test_thread_channel_basic(void) {
 	// Reset test data
 	consumer_count = 0;
-	
+
 	// Create a channel with consumer thread
 	RThreadChannel *tc = r_th_channel_new(consumer_function, NULL);
 	mu_assert_notnull(tc, "Channel creation failed");
-	
+
 	// Set channel as thread user data
 	tc->consumer->user = tc;
-	
+
 	// Create and send a message with value 5
 	int value = 5;
 	RThreadChannelMessage *msg = r_th_channel_message_new(tc, (const ut8*)&value, sizeof(int));
 	mu_assert_notnull(msg, "Failed to create channel message");
-	
+
 	// Start the consumer thread
 	r_th_start(tc->consumer);
-	
+
 	// Write message to channel
 	r_th_channel_write(tc, msg);
-	
+
 	// Give some time for processing
 	r_sys_usleep(100000);  // 100ms
-	
+
 	// Clean up
 	r_th_channel_free(tc);
-	
+
 	// Verify consumer processed the message
 	mu_assert_eq(consumer_count, 5, "Consumer didn't process message correctly");
-	
+
 	mu_end;
 }
 
 bool test_thread_channel_multiple_messages(void) {
 	// Reset test data
 	consumer_count = 0;
-	
+
 	// Create a channel with consumer thread
 	RThreadChannel *tc = r_th_channel_new(consumer_function, NULL);
 	mu_assert_notnull(tc, "Channel creation failed");
-	
+
 	// Set channel as thread user data
 	tc->consumer->user = tc;
-	
+
 	// Start the consumer thread
 	r_th_start(tc->consumer);
-	
+
 	// Send multiple messages and collect promises
 	RThreadChannelPromise *promises[5];
-	for (int i = 1; i <= 5; i++) {
+	int i;
+	for (i = 1; i <= 5; i++) {
 		// Create message with value i
 		RThreadChannelMessage *msg = r_th_channel_message_new(tc, (const ut8*)&i, sizeof(int));
 		mu_assert_notnull(msg, "Failed to create channel message");
-		
+
 		// Use query to get a promise for the response
 		promises[i-1] = r_th_channel_query(tc, msg);
 		mu_assert_notnull(promises[i-1], "Failed to create channel promise");
 	}
-	
+
 	// Wait for all promises to be fulfilled (ensures all messages are processed)
-	for (int i = 0; i < 5; i++) {
+	for (i = 0; i < 5; i++) {
 		RThreadChannelMessage *response = r_th_channel_promise_wait(promises[i]);
 		mu_assert_notnull(response, "Failed to get response from promise");
 		r_th_channel_message_free(response);
 		r_th_channel_promise_free(promises[i]);
 	}
-	
+
 	// Clean up
 	r_th_channel_free(tc);
-	
+
 	// Verify consumer processed all messages (1+2+3+4+5 = 15)
 	mu_assert_eq(consumer_count, 15, "Consumer didn't process all messages correctly");
-	
+
 	mu_end;
 }
 
 bool test_thread_channel_query(void) {
 	// Reset test data
 	consumer_count = 0;
-	
+
 	// Create a channel with consumer thread
 	RThreadChannel *tc = r_th_channel_new(consumer_function, NULL);
 	mu_assert_notnull(tc, "Channel creation failed");
-	
+
 	// Set channel as thread user data
 	tc->consumer->user = tc;
-	
+
 	// Start the consumer thread
 	r_th_start(tc->consumer);
-	
+
 	// Create a message with value 10
 	int value = 10;
 	RThreadChannelMessage *msg = r_th_channel_message_new(tc, (const ut8*)&value, sizeof(int));
 	mu_assert_notnull(msg, "Failed to create channel message");
-	
+
 	// Send query and get promise
 	RThreadChannelPromise *promise = r_th_channel_query(tc, msg);
 	mu_assert_notnull(promise, "Failed to create channel promise");
-	
+
 	// Wait for response
 	RThreadChannelMessage *response = r_th_channel_promise_wait(promise);
 	mu_assert_notnull(response, "Failed to get response from promise");
-	
+
 	// Verify response content
 	mu_assert_eq(*(int*)response->msg, 10, "Incorrect response value");
-	
+
 	// Clean up
 	r_th_channel_message_free(response);
 	r_th_channel_promise_free(promise);
 	r_th_channel_free(tc);
-	
+
 	// Verify consumer processed the message
 	mu_assert_eq(consumer_count, 10, "Consumer didn't process message correctly");
-	
+
 	mu_end;
 }
 
--- test/unit/test_thread_lock.c
+++ test/unit/test_thread_lock.c
@@ -7,119 +7,119 @@ static RThreadLock *test_lock = NULL;
 
 // Thread function that increments counter with lock protection
 static RThreadFunctionRet increment_with_lock(RThread *th) {
-    // Acquire the lock
-    r_th_lock_enter(test_lock);
-    
-    // Critical section - increment counter
-    shared_counter++;
-    
-    // Release the lock
-    r_th_lock_leave(test_lock);
-    
-    return R_TH_STOP;
+	// Acquire the lock
+	r_th_lock_enter(test_lock);
+
+	// Critical section - increment counter
+	shared_counter++;
+
+	// Release the lock
+	r_th_lock_leave(test_lock);
+
+	return R_TH_STOP;
 }
 
 bool test_lock_basic(void) {
-    // Initialize counter and create lock
-    shared_counter = 0;
-    test_lock = r_th_lock_new(false); // non-recursive lock
-    mu_assert_notnull(test_lock, "Lock creation failed");
-    
-    // Create thread
-    RThread *th = r_th_new(increment_with_lock, NULL, 0);
-    mu_assert_notnull(th, "Thread creation failed");
-    
-    // Start thread
-    r_th_start(th);
-    
-    // Wait for thread to complete
-    r_th_wait(th);
-    
-    // Check that thread incremented counter
-    mu_assert_eq(shared_counter, 1, "Thread didn't increment counter correctly");
-    
-    // Clean up
-    r_th_free(th);
-    r_th_lock_free(test_lock);
-    
-    mu_end;
+	// Initialize counter and create lock
+	shared_counter = 0;
+	test_lock = r_th_lock_new(false); // non-recursive lock
+	mu_assert_notnull(test_lock, "Lock creation failed");
+
+	// Create thread
+	RThread *th = r_th_new(increment_with_lock, NULL, 0);
+	mu_assert_notnull(th, "Thread creation failed");
+
+	// Start thread
+	r_th_start(th);
+
+	// Wait for thread to complete
+	r_th_wait(th);
+
+	// Check that thread incremented counter
+	mu_assert_eq(shared_counter, 1, "Thread didn't increment counter correctly");
+
+	// Clean up
+	r_th_free(th);
+	r_th_lock_free(test_lock);
+
+	mu_end;
 }
 
 // Thread function for testing recursion
 static RThreadFunctionRet test_recursion(RThread *th) {
-    bool first_enter = r_th_lock_enter(test_lock);
-    mu_assert("First lock enter should succeed", first_enter);
-    
-    // In recursive mode, this should succeed
-    // In non-recursive mode, this would deadlock, but we'll check tryenter instead
-    bool second_enter = r_th_lock_tryenter(test_lock);
-    
-    if (second_enter) {
-        // We were able to recursively lock, so increment and release inner lock
-        shared_counter++;
-        r_th_lock_leave(test_lock);
-    }
-    
-    // Always release outer lock
-    r_th_lock_leave(test_lock);
-    
-    return R_TH_STOP;
+	bool first_enter = r_th_lock_enter(test_lock);
+	mu_assert("First lock enter should succeed", first_enter);
+
+	// In recursive mode, this should succeed
+	// In non-recursive mode, this would deadlock, but we'll check tryenter instead
+	bool second_enter = r_th_lock_tryenter(test_lock);
+
+	if (second_enter) {
+		// We were able to recursively lock, so increment and release inner lock
+		shared_counter++;
+		r_th_lock_leave(test_lock);
+	}
+
+	// Always release outer lock
+	r_th_lock_leave(test_lock);
+
+	return R_TH_STOP;
 }
 
 bool test_lock_recursive(void) {
-    // Initialize counter
-    shared_counter = 0;
-    
-    // Create recursive lock
-    test_lock = r_th_lock_new(true); // recursive lock
-    mu_assert_notnull(test_lock, "Recursive lock creation failed");
-    
-    // Create thread
-    RThread *th = r_th_new(test_recursion, NULL, 0);
-    mu_assert_notnull(th, "Thread creation failed");
-    
-    // Start thread
-    r_th_start(th);
-    
-    // Wait for thread to complete
-    r_th_wait(th);
-    
-    // Check that inner lock was acquired
-    mu_assert_eq(shared_counter, 1, "Recursive lock didn't allow inner lock acquisition");
-    
-    // Clean up
-    r_th_free(th);
-    r_th_lock_free(test_lock);
-    
-    mu_end;
+	// Initialize counter
+	shared_counter = 0;
+
+	// Create recursive lock
+	test_lock = r_th_lock_new(true); // recursive lock
+	mu_assert_notnull(test_lock, "Recursive lock creation failed");
+
+	// Create thread
+	RThread *th = r_th_new(test_recursion, NULL, 0);
+	mu_assert_notnull(th, "Thread creation failed");
+
+	// Start thread
+	r_th_start(th);
+
+	// Wait for thread to complete
+	r_th_wait(th);
+
+	// Check that inner lock was acquired
+	mu_assert_eq(shared_counter, 1, "Recursive lock didn't allow inner lock acquisition");
+
+	// Clean up
+	r_th_free(th);
+	r_th_lock_free(test_lock);
+
+	mu_end;
 }
 
 bool test_lock_non_recursive(void) {
-    // Initialize counter
-    shared_counter = 0;
-    
-    // Create non-recursive lock
-    test_lock = r_th_lock_new(false); // non-recursive lock
-    mu_assert_notnull(test_lock, "Non-recursive lock creation failed");
-    
-    // Create thread
-    RThread *th = r_th_new(test_recursion, NULL, 0);
-    mu_assert_notnull(th, "Thread creation failed");
-    
-    // Start thread
-    r_th_start(th);
-    
-    // Wait for thread to complete
-    r_th_wait(th);
-    
-    // Check that inner lock was NOT acquired (tryenter should have failed)
-    mu_assert_eq(shared_counter, 0, "Non-recursive lock shouldn't allow inner lock acquisition");
-    
-    // Clean up
-    r_th_free(th);
-    r_th_lock_free(test_lock);
-    
-    mu_end;
+	// Initialize counter
+	shared_counter = 0;
+
+	// Create non-recursive lock
+	test_lock = r_th_lock_new(false); // non-recursive lock
+	mu_assert_notnull(test_lock, "Non-recursive lock creation failed");
+
+	// Create thread
+	RThread *th = r_th_new(test_recursion, NULL, 0);
+	mu_assert_notnull(th, "Thread creation failed");
+
+	// Start thread
+	r_th_start(th);
+
+	// Wait for thread to complete
+	r_th_wait(th);
+
+	// Check that inner lock was NOT acquired (tryenter should have failed)
+	mu_assert_eq(shared_counter, 0, "Non-recursive lock shouldn't allow inner lock acquisition");
+
+	// Clean up
+	r_th_free(th);
+	r_th_lock_free(test_lock);
+
+	mu_end;
 }
 
 // Multiple threads synchronizing with a lock
@@ -127,116 +127,117 @@ bool test_lock_non_recursive(void) {
 static RThread *threads[NUM_THREADS];
 
 static RThreadFunctionRet thread_increment_lock(RThread *th) {
-    // Acquire lock
-    r_th_lock_enter(test_lock);
-    
-    // Critical section
-    shared_counter++;
-    
-    // Release lock
-    r_th_lock_leave(test_lock);
-    
-    return R_TH_STOP;
+	// Acquire lock
+	r_th_lock_enter(test_lock);
+
+	// Critical section
+	shared_counter++;
+
+	// Release lock
+	r_th_lock_leave(test_lock);
+
+	return R_TH_STOP;
 }
 
 bool test_lock_multiple_threads(void) {
-    // Initialize counter and create lock
-    shared_counter = 0;
-    test_lock = r_th_lock_new(false); // non-recursive lock
-    mu_assert_notnull(test_lock, "Lock creation failed");
-    
-    // Create and start threads
-    for (int i = 0; i < NUM_THREADS; i++) {
-        threads[i] = r_th_new(thread_increment_lock, NULL, 0);
-        mu_assert_notnull(threads[i], "Thread creation failed");
-        r_th_start(threads[i]);
-    }
-    
-    // Wait for all threads to finish
-    for (int i = 0; i < NUM_THREADS; i++) {
-        r_th_wait(threads[i]);
-        r_th_free(threads[i]);
-    }
-    
-    // Verify count
-    mu_assert_eq(shared_counter, NUM_THREADS, "Thread synchronization failed");
-    
-    // Clean up
-    r_th_lock_free(test_lock);
-    
-    mu_end;
+	// Initialize counter and create lock
+	shared_counter = 0;
+	test_lock = r_th_lock_new(false); // non-recursive lock
+	mu_assert_notnull(test_lock, "Lock creation failed");
+
+	// Create and start threads
+	int i;
+	for (i = 0; i < NUM_THREADS; i++) {
+		threads[i] = r_th_new(thread_increment_lock, NULL, 0);
+		mu_assert_notnull(threads[i], "Thread creation failed");
+		r_th_start(threads[i]);
+	}
+
+	// Wait for all threads to finish
+	for (i = 0; i < NUM_THREADS; i++) {
+		r_th_wait(threads[i]);
+		r_th_free(threads[i]);
+	}
+
+	// Verify count
+	mu_assert_eq(shared_counter, NUM_THREADS, "Thread synchronization failed");
+
+	// Clean up
+	r_th_lock_free(test_lock);
+
+	mu_end;
 }
 
 bool test_lock_wait(void) {
-    // Initialize counter
-    shared_counter = 0;
-    
-    // Create lock
-    test_lock = r_th_lock_new(false);
-    mu_assert_notnull(test_lock, "Lock creation failed");
-    
-    // Acquire lock from main thread
-    r_th_lock_enter(test_lock);
-    
-    // Create thread that will wait on lock
-    RThread *th = r_th_new(increment_with_lock, NULL, 0);
-    mu_assert_notnull(th, "Thread creation failed");
-    
-    // Start thread (it will block on lock)
-    r_th_start(th);
-    
-    // Counter should still be 0 as thread is blocked
-    r_sys_usleep(100000); // 100ms
-    mu_assert_eq(shared_counter, 0, "Counter should still be 0 while thread is blocked");
-    
-    // Release lock to allow thread to proceed
-    r_th_lock_leave(test_lock);
-    
-    // Wait for thread to complete
-    r_th_wait(th);
-    
-    // Counter should now be 1
-    mu_assert_eq(shared_counter, 1, "Counter should be 1 after thread completes");
-    
-    // Clean up
-    r_th_free(th);
-    r_th_lock_free(test_lock);
-    
-    mu_end;
+	// Initialize counter
+	shared_counter = 0;
+
+	// Create lock
+	test_lock = r_th_lock_new(false);
+	mu_assert_notnull(test_lock, "Lock creation failed");
+
+	// Acquire lock from main thread
+	r_th_lock_enter(test_lock);
+
+	// Create thread that will wait on lock
+	RThread *th = r_th_new(increment_with_lock, NULL, 0);
+	mu_assert_notnull(th, "Thread creation failed");
+
+	// Start thread (it will block on lock)
+	r_th_start(th);
+
+	// Counter should still be 0 as thread is blocked
+	r_sys_usleep(100000); // 100ms
+	mu_assert_eq(shared_counter, 0, "Counter should still be 0 while thread is blocked");
+
+	// Release lock to allow thread to proceed
+	r_th_lock_leave(test_lock);
+
+	// Wait for thread to complete
+	r_th_wait(th);
+
+	// Counter should now be 1
+	mu_assert_eq(shared_counter, 1, "Counter should be 1 after thread completes");
+
+	// Clean up
+	r_th_free(th);
+	r_th_lock_free(test_lock);
+
+	mu_end;
 }
 
 bool test_atomic_operations(void) {
-    // Test atomic exchange
-    volatile R_ATOMIC_BOOL data = 0;
-    
-    bool prev = r_atomic_exchange(&data, true);
-    mu_assert_eq(prev, false, "Initial value should be false");
-    mu_assert("Atomic value should now be true", data);
-    
-    prev = r_atomic_exchange(&data, false);
-    mu_assert_eq(prev, true, "Previous value should be true");
-    mu_assert("Atomic value should now be false", !data);
-    
-    // Test atomic store
-    r_atomic_store(&data, true);
-    mu_assert("Atomic value should be true after store", data);
-    
-    r_atomic_store(&data, false);
-    mu_assert("Atomic value should be false after store", !data);
-    
-    mu_end;
+	// Test atomic exchange
+	volatile R_ATOMIC_BOOL data = 0;
+
+	bool prev = r_atomic_exchange(&data, true);
+	mu_assert_eq(prev, false, "Initial value should be false");
+	mu_assert("Atomic value should now be true", data);
+
+	prev = r_atomic_exchange(&data, false);
+	mu_assert_eq(prev, true, "Previous value should be true");
+	mu_assert("Atomic value should now be false", !data);
+
+	// Test atomic store
+	r_atomic_store(&data, true);
+	mu_assert("Atomic value should be true after store", data);
+
+	r_atomic_store(&data, false);
+	mu_assert("Atomic value should be false after store", !data);
+
+	mu_end;
 }
 
 int all_tests(void) {
-    mu_run_test(test_lock_basic);
-    mu_run_test(test_lock_recursive);
-    mu_run_test(test_lock_non_recursive);
-    mu_run_test(test_lock_multiple_threads);
-    mu_run_test(test_lock_wait);
-    mu_run_test(test_atomic_operations);
-    return tests_passed != tests_run;
+	mu_run_test(test_lock_basic);
+	mu_run_test(test_lock_recursive);
+	mu_run_test(test_lock_non_recursive);
+	mu_run_test(test_lock_multiple_threads);
+	mu_run_test(test_lock_wait);
+	mu_run_test(test_atomic_operations);
+	return tests_passed != tests_run;
 }
 
 int main(int argc, char **argv) {
-    return all_tests();
+	return all_tests();
 }
