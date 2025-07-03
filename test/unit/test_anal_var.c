eec9ba826435768e68890dbba08b88c12f8cefeb
pancake <pancake@nopcode.org>
2025-07-03 19:53:11 +0200

    fii

--- test/unit/test_anal_var.c
+++ test/unit/test_anal_var.c
@@ -55,7 +55,7 @@ bool test_r_anal_var(void) {
 	RAnalVar *a = r_anal_function_set_var (fcn, -8, R_ANAL_VAR_KIND_BPV, "char *", 8, false, "random_name");
 	mu_assert_notnull (a, "create a var");
 	mu_assert_streq (a->name, "random_name", "var name");
-	bool succ = r_anal_var_rename (a, "var_a", false);
+	bool succ = r_anal_var_rename (anal, a, "var_a");
 	mu_assert ("rename success", succ);
 	mu_assert_streq (a->name, "var_a", "var name after rename");
 
@@ -64,13 +64,13 @@ bool test_r_anal_var(void) {
 	b = r_anal_function_set_var (fcn, -0x10, R_ANAL_VAR_KIND_SPV, "char *", 8, false, "new_var");
 	mu_assert_notnull (b, "create a var with another name");
 	mu_assert_streq (b->name, "new_var", "var name");
-	succ = r_anal_var_rename (b, "random_name", false);
+	succ = r_anal_var_rename (anal, b, "random_name");
 	mu_assert ("rename success", succ);
 	mu_assert_streq (b->name, "random_name", "var name after rename");
-	succ = r_anal_var_rename (b, "var_a", false);
+	succ = r_anal_var_rename (anal, b, "var_a");
 	mu_assert ("rename failed", !succ);
 	mu_assert_streq (b->name, "random_name", "var name after failed rename");
-	succ = r_anal_var_rename (b, "var_b", false);
+	succ = r_anal_var_rename (anal, b, "var_b");
 	mu_assert ("rename success", succ);
 	mu_assert_streq (b->name, "var_b", "var name after rename");
 
@@ -95,10 +95,10 @@ bool test_r_anal_var(void) {
 
 	// accesses
 
-	r_anal_var_set_access (a, "rsp", 0x120, R_PERM_R, 42);
-	r_anal_var_set_access (a, "rbp", 0x130, R_PERM_W, 13);
-	r_anal_var_set_access (b, "rsp", 0x120, R_PERM_W, 123);
-	r_anal_var_set_access (b, "rbp", 0x10, R_PERM_W, -100);
+	r_anal_var_set_access (anal, a, "rsp", 0x120, R_PERM_R, 42);
+	r_anal_var_set_access (anal, a, "rbp", 0x130, R_PERM_W, 13);
+	r_anal_var_set_access (anal, b, "rsp", 0x120, R_PERM_W, 123);
+	r_anal_var_set_access (anal, b, "rbp", 0x10, R_PERM_W, -100);
 
 	st64 stackptr = r_anal_function_get_var_stackptr_at (fcn, -0x10, 0x12345);
 	mu_assert_eq (stackptr, ST64_MAX, "unset stackptr");
@@ -135,14 +135,14 @@ bool test_r_anal_var(void) {
 
 	used_vars = r_anal_function_get_vars_used_at (fcn, 0xffffffffffff0130UL); // addresses should stay the same
 	mu_assert ("no used vars", !used_vars || r_pvector_length (used_vars));
-	r_anal_var_set_access (a, "rbp", 0xffffffffffff0130UL, R_PERM_R, 42);
+	r_anal_var_set_access (anal, a, "rbp", 0xffffffffffff0130UL, R_PERM_R, 42);
 	used_vars = r_anal_function_get_vars_used_at (fcn, 0xffffffffffff0130UL);
 	mu_assert_eq (r_pvector_length (used_vars), 1, "used vars count");
 	mu_assert ("used vars", r_pvector_contains (used_vars, a));
 
 	used_vars = r_anal_function_get_vars_used_at (fcn, 0x123);
 	mu_assert ("no used vars", !used_vars || r_pvector_length (used_vars));
-	r_anal_var_set_access (a, "rbp" , 0x123, R_PERM_R, 42);
+	r_anal_var_set_access (anal, a, "rbp" , 0x123, R_PERM_R, 42);
 	used_vars = r_anal_function_get_vars_used_at (fcn, 0x123);
 	mu_assert_eq (r_pvector_length (used_vars), 1, "used vars count");
 	mu_assert ("used vars", r_pvector_contains (used_vars, a));
@@ -167,7 +167,7 @@ bool test_r_anal_var(void) {
 
 	used_vars = r_anal_function_get_vars_used_at (fcn, 0x8000000000000100);
 	mu_assert ("no used vars", !used_vars || r_pvector_length (used_vars));
-	r_anal_var_set_access (a, "rbp", 0x8000000000000100, R_PERM_R, 987321);
+	r_anal_var_set_access (anal, a, "rbp", 0x8000000000000100, R_PERM_R, 987321);
 	used_vars = r_anal_function_get_vars_used_at (fcn, 0x8000000000000100);
 	mu_assert_eq (r_pvector_length (used_vars), 1, "used vars count");
 	mu_assert ("used vars", r_pvector_contains (used_vars, a));
@@ -176,7 +176,7 @@ bool test_r_anal_var(void) {
 
 	used_vars = r_anal_function_get_vars_used_at (fcn, 0x7ffffffffffffe00);
 	mu_assert ("no used vars", !used_vars || r_pvector_length (used_vars));
-	r_anal_var_set_access (a, "rbp", 0x7ffffffffffffe00, R_PERM_R, 777);
+	r_anal_var_set_access (anal, a, "rbp", 0x7ffffffffffffe00, R_PERM_R, 777);
 	used_vars = r_anal_function_get_vars_used_at (fcn, 0x7ffffffffffffe00);
 	mu_assert_eq (r_pvector_length (used_vars), 1, "used vars count");
 	mu_assert ("used vars", r_pvector_contains (used_vars, a));
@@ -203,7 +203,7 @@ bool test_r_anal_var(void) {
 
 	assert_sane (anal);
 
-	r_anal_var_delete (a);
+	r_anal_var_delete (anal, a);
 	assert_sane (anal);
 
 	used_vars = r_anal_function_get_vars_used_at (fcn, 0xffffffffffff0130UL);
@@ -260,8 +260,8 @@ bool test_r_anal_var(void) {
 	vps = r_anal_var_deserialize ("ts-16:v,r_name:char **");
 	mu_assert ("No ',' in serialized name", !vps);
 
-	r_anal_var_delete (b);
-	r_anal_var_delete (c);
+	r_anal_var_delete (anal, b);
+	r_anal_var_delete (anal, c);
 
 	r_anal_free (anal);
 	mu_end;
