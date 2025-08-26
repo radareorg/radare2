bbb7d27badf861068618e4b0dac097ba39c5dc8e
pancake <pancake@nowsecure.com>
2025-08-26 11:23:53 +0200

    Update libr/util/math.c

--- libr/util/math.c
+++ libr/util/math.c
@@ -178,7 +178,7 @@ static RNumCalcValue prim(RNum *num, RNumCalc *nc, int get) {
 		get_token (num, nc);
 		return Bnot (expr (num, nc, 1));
 	case RNCBNEQ:
-		return Bneq (expr (number, nc, 1));
+		return Bneq (expr (num, nc, 1));
 	case RNCNEG:
 		get_token (num, nc);
 		return Nneg (expr (num, nc, 1));
