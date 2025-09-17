e11534dc4034af785b87f738a35c90b35514934a
pancake <pancake@nowsecure.com>
2025-09-17 17:55:47 +0200

    Implement support for bpf pseudocode (alias sbpf) ##arch

--- libr/arch/p/bpf/pseudo.c
+++ libr/arch/p/bpf/pseudo.c
@@ -1,85 +1,105 @@
-/* radare - LGPL - Copyright 2022-2024 - pancake */
+/* radare - LGPL - Copyright 2022-2025 - pancake */
 
 #include <r_asm.h>
 
-#define MAXARGS 4
-#define BUFSIZE 64
+/* Pseudo rules for classic and extended BPF (eBPF/sBPF) */
+static const char *pseudo_rules[] = {
+    /* moves / immediates */
+    "mov/2/$1 = $2",
+    "mov64/2/$1 = $2",
+    "lddw/2/$1 = $2",
 
-static void concat(char *buf, size_t len, const char** args) {
-	const char *arg;
-	char *dest = buf;
-	int arg_len;
+    /* arithmetic */
+    "add/2/$1 += $2",
+    "add64/2/$1 += $2",
+    "sub/2/$1 -= $2",
+    "sub64/2/$1 -= $2",
+    "mul/2/$1 *= $2",
+    "mul64/2/$1 *= $2",
+    "div/2/$1 /= $2",
+    "div64/2/$1 /= $2",
+    "mod/2/$1 %= $2",
+    "mod64/2/$1 %= $2",
+    "neg/1/$1 = -$1",
+    "neg64/1/$1 = -$1",
 
-	while ((arg = *args++)) {
-		if (snprintf (dest, len, "%s", arg) >= len) {
-			break;
-		}
-		arg_len = strlen (arg);
-		dest += arg_len;
-		len -= arg_len;
-	}
-}
+    /* bitwise */
+    "and/2/$1 &= $2",
+    "and64/2/$1 &= $2",
+    "or/2/$1 |= $2",
+    "or64/2/$1 |= $2",
+    "xor/2/$1 ^= $2",
+    "xor64/2/$1 ^= $2",
 
-static int replace(int argc, char *argv[], char *newstr, size_t len) {
-	int i;
-	struct {
-		const char *op;
-		const char **res;
-	} ops[] = {
-		{ "add",  (const char*[]){ argv[1], " += ", argv[2], NULL } },
-#if 0
-		{ "subn", (const char*[]){ argv[1], " = ", argv[2], " - ", argv[1], NULL } },
-		{ "xor",  (const char*[]){ argv[1], " ^= ", argv[2], NULL } },
-#endif
-		{ NULL }
-	};
+    /* shifts */
+    "lsh/2/$1 <<= $2",
+    "lsh64/2/$1 <<= $2",
+    "rsh/2/$1 >>= $2",
+    "rsh64/2/$1 >>= $2",
+    "arsh/2/$1 >>= $2",
+    "arsh64/2/$1 >>= $2",
 
-	for (i = 0; ops[i].op; i++) {
-		if (!strcmp (ops[i].op, argv[0]) && newstr) {
-			concat (newstr, len, ops[i].res);
-			return true;
-		}
-	}
+    /* byteswaps */
+    "be16/1/$1 = bswap16($1)",
+    "be32/1/$1 = bswap32($1)",
+    "be64/1/$1 = bswap64($1)",
+    "le16/1/$1 = (ut16)$1",
+    "le32/1/$1 = (ut32)$1",
+    "le64/1/$1 = (ut64)$1",
 
-	return false;
-}
+    /* classic BPF xfers */
+    "tax/0/x = a",
+    "txa/0/a = x",
 
-static int tokenize(const char* in, char* out[]) {
-	int len = strlen (in), count = 0, i = 0, tokenlen = 0, seplen = 0;
-	char *token, *buf = (char*) in;
-	const char* tokcharset = ", \t\n";
+    /* loads */
+    "ldxw/2/$1 = $2",
+    "ldxh/2/$1 = $2",
+    "ldxb/2/$1 = $2",
+    "ldxdw/2/$1 = $2",
+    "ldw/2/$1 = $2",
+    "ldh/2/$1 = $2",
+    "ldb/2/$1 = $2",
 
-	while (i < len) {
-		tokenlen = strcspn (buf, tokcharset);
-		token = calloc (tokenlen + 1, sizeof (char));
-		memcpy (token, buf, tokenlen);
-		out[count] = token;
-		i += tokenlen;
-		buf += tokenlen;
-		count++;
+    /* stores */
+    "stxw/2/$1 = $2",
+    "stxh/2/$1 = $2",
+    "stxb/2/$1 = $2",
+    "stxdw/2/$1 = $2",
+    "stw/2/$1 = $2",
+    "sth/2/$1 = $2",
+    "stb/2/$1 = $2",
 
-		seplen = strspn (buf, tokcharset);
-		i += seplen;
-		buf += seplen;
-	}
+    /* atomics */
+    "xaddw/2/$1 += $2",
+    "xadddw/2/$1 += $2",
 
-	return count;
-}
+    /* control flow */
+    "call/1/$1 ()",
+    "exit/0/return",
+    "ret/0/return",
+    "ja/1/goto $1",
+    "jmp/1/goto $1",
+    "goto/1/goto $1",
+
+    /* conditional branches */
+    "jeq/3/if ($1 == $2) goto $3",
+    "jne/3/if ($1 != $2) goto $3",
+    "jgt/3/if ($1 > $2) goto $3",
+    "jge/3/if ($1 >= $2) goto $3",
+    "jlt/3/if ($1 < $2) goto $3",
+    "jle/3/if ($1 <= $2) goto $3",
+    "jset/3/if ($1 & $2) goto $3",
+    /* signed variants */
+    "jsgt/3/if ($1 > $2) goto $3",
+    "jsge/3/if ($1 >= $2) goto $3",
+    "jslt/3/if ($1 < $2) goto $3",
+    "jsle/3/if ($1 <= $2) goto $3",
+
+    NULL
+};
 
 static char *parse(RAsmPluginSession *aps, const char *data) {
-	int i;
-	char *argv[MAXARGS] = { NULL, NULL, NULL, NULL };
-	int argc = tokenize (data, argv);
-
-	char *str = malloc (strlen (data) + 128);
-	strcpy (str, data);
-	if (!replace (argc, argv, str, BUFSIZE)) {
-		strcpy (str, data);
-	}
-	for (i = 0; i < MAXARGS; i++) {
-		free (argv[i]);
-	}
-	return r_str_fixspaces (str);
+    return r_str_pseudo_transform (pseudo_rules, data);
 }
 
 RAsmPlugin r_asm_plugin_bpf = {
