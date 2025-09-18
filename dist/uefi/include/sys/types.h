d81269bccf5fbf007e53b0b2a630142d14a250b6
pancake <pancake@nopcode.org>
2025-09-18 20:11:04 +0200

    Add dummy headers

---
+++ dist/uefi/include/sys/types.h
@@ -0,0 +1,15 @@
+#ifndef _SYS_TYPES_H
+#define _SYS_TYPES_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+typedef int pid_t;
+typedef long off_t;
+typedef int mode_t;
+typedef unsigned int uid_t;
+typedef unsigned int gid_t;
+typedef long time_t;
+typedef long suseconds_t;
+
+#endif
