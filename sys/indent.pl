6fb2fa4173d1e0edeb873d9dea8f842f94c80ca1
pancake <pancake@nopcode.org>
2025-09-20 08:32:55 +0200

    The indent.pl now takes files as argument

--- sys/indent.pl
+++ sys/indent.pl
@@ -2,14 +2,37 @@
 use strict;
 use warnings;
 
-while (<>) {
-	s/^ +/\t/g;
-	if (/^[A-Za-z0-9]/) {
-		s/\s*\(/(/g;
-	} else {
-		s/\s*\(/ (/g;
+if (@ARGV) {
+	foreach my $file (@ARGV) {
+		open my $fh, '<', $file or die "Cannot open $file: $!";
+		my @lines = <$fh>;
+		close $fh;
+
+		foreach my $line (@lines) {
+			$line =~ s/^ +/\t/g;
+			if ($line =~ /^[A-Za-z0-9]/) {
+				$line =~ s/\s*\(/(/g;
+			} else {
+				$line =~ s/\s*\(/ (/g;
+			}
+			$line =~ s/'\s\(/'(/g;
+			$line =~ s/\(\s\(/((/g;
+		}
+
+		open my $out, '>', $file or die "Cannot write to $file: $!";
+		print $out @lines;
+		close $out;
 	}
-	s/'\s\(/'(/g;
-	s/\(\s\(/((/g;
-	print;
+} else {
+	while (<>) {
+		s/^ +/\t/g;
+		if (/^[A-Za-z0-9]/) {
+			s/\s*\(/(/g;
+		} else {
+			s/\s*\(/ (/g;
+		}
+		s/'\s\(/'(/g;
+		s/\(\s\(/((/g;
+		print;
+	}
 }
