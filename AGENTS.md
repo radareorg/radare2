98cab1aa80fd288b682e265d1a4c59adfc1ecd4d
pancake <pancake@nopcode.org>
2025-09-23 10:11:09 +0200

    Improve the 6502 pseudo disassembler ##arch

--- AGENTS.md
+++ AGENTS.md
@@ -6,18 +6,27 @@
 - API manpages in the `./man/3` directory provide a good quick start
 - Plugins are located under the `/p` subdirectories on each `./libr/*`
 
-## Coding Rules
+## Coding Style
 
 - `R_NEW`/`R_NEW0` macros never return NULL
 - Do not check for NULL before calling `free` or any other `*_free` function
 - The `r_json_parse` does not own the string passed, we must free it after freeing the parser
 - Function calls require a space before the parenthesis. (p.ex: Use `foo ()` instead of `foo()`)
-- Use tabs instead of spaces to indent the code
+- Always indent code using **TABS**
 - Follow the `radare2` coding style (see `./third_party/radare2/DEVELOPERS.md`)
 - Prefer `!strcmp ()` over `strcmp () == 0`
 - Always use `{}` braces, even one line conditionals
 - Use `R_RETURN_*` macros in the public APIs (those marked with `R_API` to specify preconditions for null parameters.
 - The `case` lines under the `switch` statements must be indented at the same column.
+- Always define loop variables before the `for` statements. For example: `int i;\nfor (i = 0; i ..)`
+
+## Coding Rules
+
+- Keep changes minimal and take smart decisions
+- When implementing commands, handle the `?` subcommand to show its help
+- Do not use `r_str_append`, better use an `r_strbuf_new` and concatenate for loops
+- Use `r_str_newf` instead of manual malloc+snprintf
+- Use `r_str_pad2` to create a string containing a character repeated many times
 
 ## Actions
 
--- libr/arch/p/6502/pseudo.c
+++ libr/arch/p/6502/pseudo.c
@@ -1,7 +1,8 @@
-/* radare - LGPL - Copyright 2015-2024 - pancake, qnix */
+/* radare - LGPL - Copyright 2015-2025 - pancake, qnix */
 
 #include <r_lib.h>
 #include <r_asm.h>
+#include <r_util.h>
 
 typedef enum {
 	IND_IDX = 0,
@@ -86,8 +87,6 @@ static char *replace(int argc, const char *argv[], ADDR_TYPE type) {
 			if (argc == 4 && argv[2][0] == '[') {
 				r_strbuf_append (sb, "+");
 				r_strbuf_append (sb, argv[2]); // wtf+3?
-				// strcat (newstr + k, "+");
-				// strcat (newstr + k + 3, argv[2]);
 			}
 			return r_strbuf_drain (sb);
 		}
@@ -101,7 +100,7 @@ static char *replace(int argc, const char *argv[], ADDR_TYPE type) {
 }
 
 static ADDR_TYPE addr_type(const char *str) {
-	if (strchr(str, '(')) {
+	if (strchr (str, '(')) {
 		char *e = strchr (str, ')');
 		if (!e) {
 			return NORM;
@@ -112,66 +111,237 @@ static ADDR_TYPE addr_type(const char *str) {
 	return NORM;
 }
 
-static char *parse(RAsmPluginSession *s, const char *data) {
-	char w0[256], w1[256], w2[256];
-	int i, len = strlen (data);
-	char *ptr, *optr;
-	ADDR_TYPE atype;
-	char *str = NULL;
+static const char *pseudo_rules[] = {
+	"lda/1/a = $1",
+	"ldx/1/x = $1",
+	"ldy/1/y = $1",
+	"sta/1/$1 = a",
+	"stx/1/$1 = x",
+	"sty/1/$1 = y",
+	"dec/1/$1--",
+	"inc/1/$1++",
+	"inx/0/x++",
+	"iny/0/y++",
+	"tax/0/x = a",
+	"tay/0/y = a",
+	"txa/0/a = x",
+	"tya/0/a = y",
+	"tsx/0/x = s",
+	"txs/0/s = x",
+	"brk/0/break",
+	"clc/0/clear_carry",
+	"cld/0/clear_decimal",
+	"cli/0/clear_interrupt",
+	"clv/0/clear_overflow",
+	"sec/0/set_carry",
+	"sed/0/set_decimal",
+	"sei/0/set_interrupt",
+	"jsr/1/$1 ()",
+	"jmp/1/goto $1",
+	"bne/1/if (!z) goto $1",
+	"beq/1/if (z) goto $1",
+	"bcc/1/if (!c) goto $1",
+	"bcs/1/if (c) goto $1",
+	"bpl/1/if (!n) goto $1",
+	"bmi/1/if (n) goto $1",
+	"bvc/1/if (!v) goto $1",
+	"bvs/1/if (v) goto $1",
+	"pha/0/push a",
+	"pla/0/a = pop()",
+	"php/0/push p",
+	"plp/0/p = pop()",
+	"adc/1/a += $1",
+	"sbc/1/a -= $1",
+	"and/1/a &= $1",
+	"eor/1/a ^= $1",
+	"ora/1/a |= $1",
+	NULL
+};
 
-	if (len >= sizeof (w0)) {
-		return NULL;
-	}
-	// malloc can be slow here :?
-	char *buf = malloc (len + 1);
-	if (!buf) {
-		return NULL;
-	}
-	memcpy (buf, data, len + 1);
-
-	if (*buf) {
-		atype = addr_type (buf);
-		r_str_replace_char (buf, '(', ' ');
-		r_str_replace_char (buf, ')', ' ');
-		*w0 = *w1 = *w2 = '\0';
-		ptr = strchr (buf, ' ');
-		if (!ptr) {
-			ptr = strchr (buf, '\t');
+static bool is_mem_access(const char *op) {
+	const char *memops[] = {"lda","ldx","ldy","sta","stx","sty","adc","sbc","and","eor","ora","inc","dec","asl","lsr","rol","ror", NULL};
+	int i = 0;
+	for (; memops[i]; i++) {
+		if (r_str_casecmp (memops[i], op) == 0) {
+			return true;
 		}
-		if (ptr) {
-			*ptr = '\0';
-			for (ptr++; *ptr == ' '; ptr++) {
-				;
-			}
-			strncpy (w0, buf, sizeof (w0) - 1);
-			strncpy (w1, ptr, sizeof (w1) - 1);
-			optr = ptr;
-			ptr = strchr (ptr, ',');
-			if (ptr) {
-				*ptr = '\0';
-				for (ptr++; *ptr == ' '; ptr++) {
-					;
-				}
-				strncpy (w1, optr, sizeof (w1) - 1);
-				strncpy (w2, ptr, sizeof (w2) - 1);
-			}
+	}
+	return false;
+}
+
+// transform one operand: immediate (#value) -> value, indexed/indirect -> [expr]
+static char *transform_operand(const char *op, const char *opcode) {
+	if (!op) {
+		return NULL;
+	}
+	char *s = r_str_trim_dup (op);
+	if (!s) {
+		return NULL;
+	}
+	// immediate
+	if (s[0] == '#') {
+		char *ret = strdup (s + 1);
+		free (s);
+		return ret;
+	}
+	// indirect indexed (addr),Y -> [addr+Y]
+	char *p = strchr (s, ')');
+	if (strchr (s, '(') && p) {
+		// remove parentheses
+		char *open = strchr (s, '(');
+		char inside[128] = {0};
+		int len = p - open - 1;
+		if (len > 0 && len < (int)sizeof (inside)) {
+			strncpy (inside, open + 1, len);
+			inside[len] = '\0';
+		}
+		// check for comma separated index inside parentheses
+		char *comma = strchr (inside, ',');
+		char idx = 0;
+		if (comma) {
+			// e.g. (addr,X)
+			idx = *(comma + 1);
+			*comma = '\0';
 		} else {
-			strncpy (w0, buf, sizeof (w0) - 1);
-		}
-
-		const char *wa[] = {w0, w1, w2};
-		int nw = 0;
-		for (i = 0; i < 3; i++) {
-			if (wa[i][0]) {
-				nw++;
+			// (addr) followed by ,X after )
+			char *after = r_str_trim_head_ro (p + 1);
+			if (*after == ',') {
+				after = r_str_trim_head_ro (after + 1);
+				idx = *after;
 			}
 		}
-		str = replace (nw, wa, atype);
+		char buf[256];
+		if (idx) {
+			snprintf (buf, sizeof (buf), "[%s+%c]", inside, idx);
+		} else {
+			snprintf (buf, sizeof (buf), "[%s]", inside);
+		}
+		free (s);
+		return strdup (buf);
+	}
+	// indexed without parentheses: addr,X -> [addr+X]
+	char *comma = strchr (s, ',');
+	if (comma) {
+		char addr[128] = {0};
+		char idx = '\0';
+		strncpy (addr, s, sizeof (addr) - 1);
+		// split
+		char *c = strchr (addr, ',');
+		if (c) {
+			*c = '\0';
+			// find index after comma in original string to preserve it
+			char *ci = r_str_trim_head_ro (comma + 1);
+			idx = *ci;
+		}
+		char buf[256];
+		if (idx) {
+			snprintf (buf, sizeof (buf), "[%s+%c]", addr, idx);
+		} else {
+			snprintf (buf, sizeof (buf), "[%s]", addr);
+		}
+		free (s);
+		return strdup (buf);
+	}
+	// bare symbol/number: decide based on opcode
+	if (is_mem_access (opcode)) {
+		char *buf = r_str_newf ("[%s]", s);
+		free (s);
+		return buf;
+	}
+	// otherwise keep as-is (addresses for branches, jsr...)
+	return s;
+}
+
+static char *parse(RAsmPluginSession *s, const char *data) {
+	if (!data) {
+		return NULL;
+	}
+	// copy and normalize
+	char *copy = strdup (data);
+	if (!copy) {
+		return NULL;
+	}
+	r_str_trim (copy);
+	// split opcode and operands
+	char opcode[64] = {0};
+	char *sp = strchr (copy, ' ');
+	char *oper = NULL;
+	if (!sp) {
+		strncpy (opcode, copy, sizeof (opcode) - 1);
+	} else {
+		int olen = sp - copy;
+		if (olen >= (int)sizeof (opcode)) {
+			olen = sizeof (opcode) - 1;
+		}
+		strncpy (opcode, copy, olen);
+		opcode[olen] = '\0';
+		oper = sp + 1;
+	}
+	(void)opcode; // opcode comparisons are case-insensitive via r_str_casecmp
+
+	// If there are no operands, delegate to str_pseudo
+	if (!oper || !*oper) {
+		free (copy);
+		return r_str_pseudo_transform (pseudo_rules, opcode);
 	}
 
-	free (buf);
+	// Now split operands by commas but some addressing modes contain commas
+	// We will manually parse operands and transform them into canonical form
+	char *ops[4] = {0};
+	int nops = 0;
+	const char *p = oper;
+	const char *start = p;
+	bool in_paren = false;
+	while (*p && nops < 4) {
+		if (*p == '(') {
+			in_paren = true;
+		} else if (*p == ')') {
+			in_paren = false;
+		} else if (*p == ',' && !in_paren) {
+			// split
+			int len = p - start;
+			char tmp[128] = {0};
+			if (len > 0) {
+				if (len >= (int)sizeof (tmp)) {
+					len = sizeof (tmp) - 1;
+				}
+				strncpy (tmp, start, len);
+				ops[nops++] = r_str_trim_dup (tmp);
+			}
+			start = p + 1;
+		}
+		p++;
+	}
+	// last operand
+	if (nops < 4 && *start) {
+		ops[nops++] = r_str_trim_dup (start);
+	}
 
-	return str;
+	// transform each operand depending on opcode
+	RStrBuf *sb = r_strbuf_new ("");
+	r_strbuf_append (sb, opcode);
+	r_strbuf_append (sb, " ");
+	int i;
+	for (i = 0; i < nops; i++) {
+		char *t = transform_operand (ops[i], opcode);
+		if (t) {
+			r_strbuf_append (sb, t);
+			free (t);
+		}
+		if (i != nops - 1) {
+			r_strbuf_append (sb, ",");
+		}
+	}
+	char *canon = r_strbuf_drain (sb);
+	free (copy);
+	for (i = 0; i < nops; i++) {
+		free (ops[i]);
+	}
+
+	// Use the general-purpose transform with canonical operands
+	char *out = r_str_pseudo_transform (pseudo_rules, canon);
+	free (canon);
+	return out;
 }
 
 RAsmPlugin r_asm_plugin_6502 = {
