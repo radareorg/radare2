96f5af33cf5117830c8a357551186bb4a34caf60
Ignacio Sanmillan <ulexec@gmail.com>
2025-09-25 19:06:29 +0200

    sBPF: Properly integrate sbpf analysis plugin and add specific sBPF sys types

--- libr/anal/p/sbpf.mk
+++ libr/anal/p/sbpf.mk
@@ -2,8 +2,9 @@ OBJ_SBPF=anal_sbpf.o
 
 STATIC_OBJ+=${OBJ_SBPF}
 TARGET_SBPF=anal_sbpf.${EXT_SO}
+
 ALL_TARGETS+=${TARGET_SBPF}
 
 ${TARGET_SBPF}: ${OBJ_SBPF}
-	-${CC} $(call libname,anal_sbpf) ${CFLAGS} \
-	${OBJ_SBPF} -o ${TARGET_SBPF} ${LINKFLAGS}
+	${CC} $(call libname,anal_sbpf) ${LDFLAGS} \
+		${CFLAGS} -o anal_sbpf.${EXT_SO} ${OBJ_SBPF}
