25a810f2b5173c76797143eb7c8f0936c3dbaf7e
pancake <pancake@nopcode.org>
2025-10-05 20:25:21 +0200

    vitamina

--- libr/core/clippy.inc.c
+++ libr/core/clippy.inc.c
@@ -1,114 +1,126 @@
-static const char *avatar_orangg[] = {
-	"      _______\n"
-	"     /       \\      .-%s-.\n"
-	"   _| ( o) (o)\\_    | %s |\n"
-	"  / _     .\\. | \\  <| %s |\n"
-	"  \\| \\   ____ / 7`  | %s |\n"
-	"  '|\\|  `---'/      `-%s-'\n"
-	"     | /----. \\\n"
-	"     | \\___/  |___\n"
-	"     `-----'`-----'\n"
+typedef struct {
+	const char *name;
+	int w;
+	int h;
+	int count;
+	const char *lines[];
+} Avatar;
+
+static Avatar avatar_orangg = {
+	.name = "orangg",
+	.w = 19,
+	.h = 9,
+	.count = 1,
+	.lines = {
+		"      _______      ",
+		"     /       \\     ",
+		"   _| ( o) (o)\\_   ",
+		"  / _     .\\. | \\  ",
+		"  \\| \\   ____ / 7` ",
+		"  '|\\|  `---'/     ",
+		"     | /----. \\    ",
+		"     | \\___/  |___ ",
+		"     `-----'`-----'"
+	}
 };
 
-static const char *avatar_croco[] = {
-	" __   __          .-%s-.\n"
-	"(o |_| o)_____    | %s |\n"
-	"|  ___________)  <  %s |\n"
-	"\\            /    | %s |\n"
-	" \\          /     `-%s-'\n"
-	"  \\________/\n"
-};
-#if 0
-
-static const char *avatar_clippy[] = {
-	" .--.     .-%s-.\n"
-	" | _|     | %s |\n"
-	" | O O   <  %s |\n"
-	" |  |  |  | %s |\n"
-	" || | /   `-%s-'\n"
-	" |`-'|\n"
-	" `---'\n",
-	" .--.     .-%s-.\n"
-	" |   \\    | %s |\n"
-	" | O o   <  %s |\n"
-	" |   | /  | %s |\n"
-	" |  ( /   `-%s-'\n"
-	" |   / \n"
-	" `--'\n",
-	" .--.     .-%s-.\n"
-	" | _|_    | %s |\n"
-	" | O O   <  %s |\n"
-	" |  ||    | %s |\n"
-	" | _:|    `-%s-'\n"
-	" |   |\n"
-	" `---'\n",
-};
-#endif
-
-static const char *avatar_clippy_utf8[] = {
-	" ╭──╮    ╭─%s─╮\n"
-	" │ _│    │ %s │\n"
-	" │ O O  <  %s │\n"
-	" │  │╭   │ %s │\n"
-	" ││ ││   ╰─%s─╯\n"
-	" │└─┘│\n"
-	" ╰───╯\n",
-	" ╭──╮    ╭─%s─╮\n"
-	" │ ╶│╶   │ %s │\n"
-	" │ O o  <  %s │\n"
-	" │  │  ╱ │ %s │\n"
-	" │ ╭┘ ╱  ╰─%s─╯\n"
-	" │ ╰ ╱\n"
-	" ╰──'\n",
-	" ╭──╮    ╭─%s─╮\n"
-	" │ _│_   │ %s │\n"
-	" │ O O  <  %s │\n"
-	" │  │╷   │ %s │\n"
-	" │  ││   ╰─%s─╯\n"
-	" │ ─╯│\n"
-	" ╰───╯\n",
+static Avatar avatar_croco = {
+	.name = "croco",
+	.w = 16,
+	.h = 6,
+	.count = 1,
+	.lines = {
+		" __   __        ",
+		"(o |_| o)_____  ",
+		"|  ___________) ",
+		"\\            /  ",
+		" \\          /   ",
+		"  \\________/    "
+	}
 };
 
-static const char *avatar_cybcat[] = {
-"     /\\.---./\\       .-%s-.\n"
-" '--           --'   | %s |\n"
-"----   ^   ^   ---- <  %s |\n"
-"  _.-    Y    -._    | %s |\n"
-"                     `-%s-'\n",
-"     /\\.---./\\       .-%s-.\n"
-" '--   @   @   --'   | %s |\n"
-"----     Y     ---- <  %s |\n"
-"  _.-    O    -._    | %s |\n"
-"                     `-%s-'\n",
-"     /\\.---./\\       .-%s-.\n"
-" '--   =   =   --'   | %s |\n"
-"----     Y     ---- <  %s |\n"
-"  _.-    U    -._    | %s |\n"
-"                     `-%s-'\n",
+static Avatar avatar_cybcat = {
+	.name = "cybercat",
+	.w = 19,
+	.h = 5,
+	.count = 3,
+	.lines = {
+		"     /\\.---./\\     ",
+		" '--           --' ",
+		"----   ^   ^   ----",
+		"  _.-    Y    -._  ",
+		"                   ",
+		"     /\\.---./\\     ",
+		" '--   @   @   --' ",
+		"----     Y     ----",
+		"  _.-    O    -._  ",
+		"                   ",
+		"     /\\.---./\\     ",
+		" '--   =   =   --' ",
+		"----     Y     ----",
+		"  _.-    U    -._  ",
+		"                   "
+	}
 };
 
-static const char *avatar_clippy[] = {
-	" .--.     .-%s-.\n"
-	" | _|     | %s |\n"
-	" | O O   <  %s |\n"
-	" |  |  |  | %s |\n"
-	" || | /   `-%s-'\n"
-	" |`-'|\n"
-	" `---'\n",
-	" .--.     .-%s-.\n"
-	" |   \\    | %s |\n"
-	" | O o   <  %s |\n"
-	" |   | /  | %s |\n"
-	" |  ( /   `-%s-'\n"
-	" |   / \n"
-	" `--'\n",
-	" .--.     .-%s-.\n"
-	" | _|_    | %s |\n"
-	" | O O   <  %s |\n"
-	" |  ||    | %s |\n"
-	" | _:|    `-%s-'\n"
-	" |   |\n"
-	" `---'\n",
+static Avatar avatar_clippy_utf8 = {
+	.name = "clippy",
+	.w = 16, // utf8 hacks
+	.h = 7,
+	.count = 3,
+	.lines = {
+		" ╭──╮  ",
+		" │ _│  ",
+		" │ O O ",
+		" │  │╭ ",
+		" ││ ││ ",
+		" │└─┘│ ",
+		" ╰───╯ ",
+		" ╭──╮   ",
+		" │ ╶│╶  ",
+		" │ O o  ",
+		" │  │  ╱",
+		" │ ╭┘ ╱ ",
+		" │ ╰ ╱  ",
+		" ╰──'   ",
+		" ╭──╮  ",
+		" │ _│_ ",
+		" │ O O ",
+		" │  │╷ ",
+		" │  ││ ",
+		" │ ─╯│ ",
+		" ╰───╯ "
+	}
+};
+
+static Avatar avatar_clippy = {
+	.name = "clippy",
+	.w = 8,
+	.h = 7,
+	.count = 3,
+	.lines = {
+		" .--.   ",
+		" | _|   ",
+		" | O O  ",
+		" |  |  |",
+		" || | / ",
+		" |`-'|  ",
+		" `---'  ",
+		" .--.   ",
+		" |   \\  ",
+		" | O o  ",
+		" |   | /",
+		" |  ( / ",
+		" |   /  ",
+		" `--'   ",
+		" .--.   ",
+		" | _|_  ",
+		" | O O  ",
+		" |  ||  ",
+		" | _:|  ",
+		" |   |  ",
+		" `---'  "
+	}
 };
 
 enum {
@@ -118,8 +130,7 @@ enum {
 	R_AVATAR_CLIPPY,
 };
 
-R_API void r_core_clippy (RCore *core, const char *msg) {
-	/* choose avatar type */
+R_API void r_core_clippy(RCore *core, const char *msg) {
 	int type = R_AVATAR_CLIPPY;
 	switch (*msg) {
 	case '+':
@@ -136,239 +147,109 @@ R_API void r_core_clippy (RCore *core, const char *msg) {
 		break;
 	}
 
-	/* wrap message at 50 columns */
 	int w = r_cons_get_size (core->cons, NULL);
-	const int WRAP_COL = (w > 50)? w - 20: 50;
-	char *wrapped = r_str_wrap (msg, WRAP_COL - 1);
-	if (!wrapped) {
-		return;
-	}
 
-	/* split wrapped text into lines */
-	int nlines = 0;
 	int i;
-	for (i = 0; wrapped[i]; i++) {
-		if (wrapped[i] == '\n') {
-			nlines++;
-		}
-	}
-	if (i > 0 && wrapped[i - 1] != '\n') {
-		nlines++;
-	}
-	char **tlines = NULL;
-	if (nlines > 0) {
-		tlines = (char **)malloc (sizeof (char *) * nlines);
-	}
-	int idx = 0;
-	char *wp = wrapped;
-	char *line = wp;
-	for (; *wp; wp++) {
-		if (*wp == '\n') {
-			*wp = '\0';
-			if (idx < nlines) {
-				tlines[idx++] = line;
-			}
-			line = wp + 1;
-		}
-	}
-	if (*line && idx < nlines) {
-		tlines[idx++] = line;
-	}
+	Avatar *avatar;
+	const bool utf8 = r_config_get_b (core->config, "scr.utf8");
 
-	/* compute bubble width from lines (max, capped at WRAP_COL) */
-	int width = 0;
-	for (i = 0; i < nlines; i++) {
-		int len = (int)r_str_len_utf8 (tlines[i]);
-		if (len > width) {
-			width = len;
-		}
-	}
-	if (width < 0) {
-		width = 0;
-	}
-	if (width > WRAP_COL) {
-		width = WRAP_COL;
-	}
-
-	/* choose avatar frame and border char */
-	const char *f;
-	char *l;
 	switch (type) {
 	case R_AVATAR_ORANGG:
-		l = strdup (r_str_pad ('-', width));
-		f = avatar_orangg[0];
+		avatar = &avatar_orangg;
 		break;
 	case R_AVATAR_CROCO:
-		l = strdup (r_str_pad ('-', width));
-		f = avatar_croco[0];
+		avatar = &avatar_croco;
 		break;
 	case R_AVATAR_CYBCAT:
-		l = strdup (r_str_pad ('-', width));
-		f = avatar_cybcat[r_num_rand (R_ARRAY_SIZE (avatar_cybcat))];
+		avatar = &avatar_cybcat;
 		break;
 	default:
-		if (r_config_get_b (core->config, "scr.utf8")) {
-			l = (char *)r_str_repeat ("─", width);
-			f = avatar_clippy_utf8[r_num_rand (R_ARRAY_SIZE (avatar_clippy_utf8))];
-		} else {
-			l = strdup (r_str_pad ('-', width));
-			f = avatar_clippy[r_num_rand (R_ARRAY_SIZE (avatar_clippy))];
-		}
+		avatar = utf8? &avatar_clippy_utf8 : &avatar_clippy;
 		break;
 	}
-	char *s = strdup (r_str_pad (' ', width));
-
-	/* split avatar frame into lines and extract bubble line templates */
-	char *fc = strdup (f);
-	int fav_lines_n = 0;
-	int flen = (int)strlen (fc);
-	for (i = 0; i < flen; i++) {
-		if (fc[i] == '\n') {
-			fav_lines_n++;
-		}
+	int baseline = 0;
+	if (avatar->count > 1) {
+		baseline += r_num_rand (avatar->count) * avatar->h;
 	}
-	if (flen > 0 && fc[flen - 1] != '\n') {
-		fav_lines_n++;
-	}
-	char **fav = NULL;
-	if (fav_lines_n > 0) {
-		fav = (char **)malloc (sizeof (char *) * fav_lines_n);
-	}
-	idx = 0;
-	char *sp = fc;
-	char *start = sp;
-	for (; *sp; sp++) {
-		if (*sp == '\n') {
-			*sp = '\0';
-			if (idx < fav_lines_n) {
-				fav[idx++] = start;
-			}
-			start = sp + 1;
-		}
-	}
-	if (*start && idx < fav_lines_n) {
-		fav[idx++] = start;
-	}
-
-	/* collect indices of lines with %s */
-	int fmt_idx[8];
-	int fmt_n = 0;
-	for (i = 0; i < fav_lines_n; i++) {
-		if (strstr (fav[i], "%s")) {
-			if (fmt_n < (int)(sizeof (fmt_idx) / sizeof (*fmt_idx))) {
-				fmt_idx[fmt_n++] = i;
-			}
-		}
-	}
-
-	/* render: top border, top pad, text lines, bottom pad, bottom border, tail */
-	if (fmt_n >= 5) {
-		/* top border */
-		{
-			const char *tmpl = fav[fmt_idx[0]];
-			const char *p = strstr (tmpl, "%s");
-			if (p) {
-				int prelen = (int)(p - tmpl);
-				char *pre = r_str_ndup (tmpl, prelen);
-				const char *post = p + 2;
-				char *out = r_str_newf ("%s%s%s", pre, l, post);
-				r_cons_printf (core->cons, "%s\n", out);
-				free (pre);
-				free (out);
-			} else {
-				r_cons_printf (core->cons, "%s\n", tmpl);
-			}
-		}
-		/* top padding */
-		{
-			const char *tmpl = fav[fmt_idx[1]];
-			const char *p = strstr (tmpl, "%s");
-			if (p) {
-				int prelen = (int)(p - tmpl);
-				char *pre = r_str_ndup (tmpl, prelen);
-				const char *post = p + 2;
-				char *out = r_str_newf ("%s%s%s", pre, s, post);
-				r_cons_printf (core->cons, "%s\n", out);
-				free (pre);
-				free (out);
-			} else {
-				r_cons_printf (core->cons, "%s\n", tmpl);
-			}
-		}
-		/* text lines */
-		for (i = 0; i < nlines; i++) {
-			int llen = (int)r_str_len_utf8 (tlines[i]);
-			int pad = width - R_MAX (0, llen);
-			char *right = strdup (r_str_pad (' ', pad));
-			char *payload = r_str_newf ("%s%s", tlines[i], right);
-			/* use arrow line for first text row if available */
-			int body_tmpl = (i == 0 && fmt_n >= 3)? fmt_idx[2]: fmt_idx[3];
-			{
-				const char *tmpl = fav[body_tmpl];
-				const char *p = strstr (tmpl, "%s");
-				if (p) {
-					int prelen = (int)(p - tmpl);
-					char *pre = r_str_ndup (tmpl, prelen);
-					const char *post = p + 2;
-					char *out = r_str_newf ("%s%s%s", pre, payload, post);
-					r_cons_printf (core->cons, "%s\n", out);
-					free (pre);
-					free (out);
-				} else {
-					r_cons_printf (core->cons, "%s\n", tmpl);
-				}
-			}
-			free (right);
-			free (payload);
-		}
-		/* bottom padding */
-		{
-			const char *tmpl = fav[fmt_idx[3]];
-			const char *p = strstr (tmpl, "%s");
-			if (p) {
-				int prelen = (int)(p - tmpl);
-				char *pre = r_str_ndup (tmpl, prelen);
-				const char *post = p + 2;
-				char *out = r_str_newf ("%s%s%s", pre, s, post);
-				r_cons_printf (core->cons, "%s\n", out);
-				free (pre);
-				free (out);
-			} else {
-				r_cons_printf (core->cons, "%s\n", tmpl);
-			}
-		}
-		/* bottom border */
-		{
-			const char *tmpl = fav[fmt_idx[4]];
-			const char *p = strstr (tmpl, "%s");
-			if (p) {
-				int prelen = (int)(p - tmpl);
-				char *pre = r_str_ndup (tmpl, prelen);
-				const char *post = p + 2;
-				char *out = r_str_newf ("%s%s%s", pre, l, post);
-				r_cons_printf (core->cons, "%s\n", out);
-				free (pre);
-				free (out);
-			} else {
-				r_cons_printf (core->cons, "%s\n", tmpl);
-			}
-		}
-		/* tail (avatar-only) */
-		for (i = fmt_idx[4] + 1; i < fav_lines_n; i++) {
-			r_cons_printf (core->cons, "%s\n", fav[i]);
-		}
+	int margin_right = avatar->w * 4;
+	char *m = r_str_wrap (msg, w - margin_right - 1);
+	RList *lines = r_str_split_list (m, "\n", 0);
+	const int lines_length = r_list_length (lines);
+	int bubble_w;
+	if (lines_length == 1) {
+		bubble_w = strlen (m);
 	} else {
-		/* fallback: original single-line rendering using first line of wrapped */
-		const char *single = (nlines > 0)? tlines[0]: "";
-		char *s1 = strdup (r_str_pad (' ', (int)r_str_len_utf8 (single)));
-		r_cons_printf (core->cons, f, l, s1, single, s1, l);
-		free (s1);
+		bubble_w = (w < margin_right) ? 10: w - margin_right;
 	}
-
-	free (fc);
-	free (fav);
-	free (l);
-	free (s);
-	free (tlines);
-	free (wrapped);
+	int rows = R_MAX (lines_length + 4, avatar->h);
+	for (i = 0; i < rows; i++) {
+		// draw clippy
+		if (i < avatar->h) {
+			const char *avatar_line = avatar->lines[baseline + i];
+			r_cons_printf (core->cons, "%s ", avatar_line);
+		} else {
+			r_cons_printf (core->cons, r_str_pad (' ', avatar->w + 1));
+		}
+		// draw bubble
+		const char *bubble_begin = "";
+		const char *bubble_end = "";
+		if (i == 0) {
+			if (utf8) {
+				bubble_begin = " ╭─";
+				bubble_end = "─╮";
+			} else {
+				bubble_begin = " .-";
+				bubble_end = "-. ";
+			}
+		} else if (i == 2) {
+			bubble_begin = "<  ";
+			if (utf8) {
+				bubble_end = " │ ";
+			} else {
+				bubble_end = " | ";
+			}
+		} else if (i == lines_length + 3) {
+			if (utf8) {
+				bubble_begin = " ╰─";
+				bubble_end = "─╯";
+			} else {
+				bubble_begin = " `-";
+				bubble_end = "-' ";
+			}
+		} else if (i < lines_length + 3) {
+			if (utf8) {
+				bubble_begin = " │ ";
+				bubble_end = " │ ";
+			} else {
+				bubble_begin = " | ";
+				bubble_end = " | ";
+			}
+		}
+		r_cons_print (core->cons, bubble_begin);
+		// print text
+		if (i > 1 && i < lines_length + 2) {
+			RListIter *line = r_list_get_nth (lines, i - 2);
+			if (line) {
+				r_cons_printf (core->cons, "%s", line->data);
+				const int tw = strlen (line->data);
+				r_cons_printf (core->cons, r_str_pad (' ', bubble_w - tw));
+			}
+		} else {
+			if (i == 0 || i == lines_length + 3) {
+				// pad with lines
+				if (utf8) {
+					int j;
+					for (j = 0; j < bubble_w; j++) {
+						r_cons_printf (core->cons, "─");
+					}
+				} else {
+				r_cons_printf (core->cons, r_str_pad ('-', bubble_w));
+				}
+			} else {
+				r_cons_printf (core->cons, r_str_pad (' ', bubble_w));
+			}
+		}
+		// print bubble_end
+		r_cons_println (core->cons, bubble_end);
+	}
 }
