4c45078aecd2f41038b8079cba4ca66459e53efe
pancake <pancake@nowsecure.com>
2025-10-14 20:36:47 +0200

    Apply suggestion from @trufae

--- libr/arch/p/arm/pseudo.c
+++ libr/arch/p/arm/pseudo.c
@@ -320,7 +320,7 @@ static char *parse(RAsmPluginSession *aps, const char *data) {
 						r_str_ncpy (reg, start, reg_len);
 						// Check for "(unsigned) reg /" pattern (special case for division)
 						char *check_pos = eq + 3;
-						if (i == 1 && !strncmp (check_pos, "(unsigned) ", 11)) {
+						if (i == 1 && r_str_startswith (check_pos, "(unsigned) ")) {
 							check_pos += 11; // skip "(unsigned) "
 						}
 						// Check if same register appears after '='
