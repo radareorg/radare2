7ef24422051625130fc9b84e1965cff6343afd4c
Eduardo Novella <foro.dudu@gmail.com>
2025-11-11 13:55:05 +0100

    Enhance #24747: Respect TLS directory image types

--- libr/bin/format/pe/pe.c
+++ libr/bin/format/pe/pe.c
@@ -1750,16 +1750,15 @@ static int read_tls_directory(RBuffer *b, ut64 addr, PE_(image_tls_directory) *t
 	tls_directory->EndAddressOfRawData = r_buf_read_le64 (b);
 	tls_directory->AddressOfIndex = r_buf_read_le64 (b);
 	tls_directory->AddressOfCallBacks = r_buf_read_le64 (b);
-	tls_directory->SizeOfZeroFill = r_buf_read_le32 (b);
-	tls_directory->Characteristics = r_buf_read_le32 (b);
 #else
 	tls_directory->StartAddressOfRawData = r_buf_read_le32 (b);
 	tls_directory->EndAddressOfRawData = r_buf_read_le32 (b);
 	tls_directory->AddressOfIndex = r_buf_read_le32 (b);
 	tls_directory->AddressOfCallBacks = r_buf_read_le32 (b);
+#endif
 	tls_directory->SizeOfZeroFill = r_buf_read_le32 (b);
 	tls_directory->Characteristics = r_buf_read_le32 (b);
-#endif
+
 	r_buf_seek (b, o_addr, R_BUF_SET);
 	return sizeof (PE_(image_tls_directory));
 }
