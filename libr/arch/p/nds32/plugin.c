012637e8f1844d3fb7a289bfb87bdbed56848207
pancake <pancake@nopcode.org>
2025-11-14 19:12:45 +0100

    apo

--- libr/arch/p/nds32/plugin.c
+++ libr/arch/p/nds32/plugin.c
@@ -676,24 +676,13 @@ static bool nds32_encode(RArchSession *as, RAnalOp *op, RArchEncodeMask mask) {
 		const char *space = strchr (arg, ' ');
 		if (space) {
 			ut64 num = r_num_get (NULL, space + 1);
-			ut8 bytes[2] = { 0xf8, 0x00 };
-			if (num > op->addr) {
-				ut64 n = (num - op->addr) >> 1;
-				ut8 n8 = n & 0x7f;
-				if (n != n8) {
-					R_LOG_ERROR ("Out of range");
-				} else {
-					bytes[1] = n8;
-				}
-			} else {
-				ut64 n = (op->addr - num) >> 1;
-				st8 n8 = n & 0x7f;
-				if (n != n8) {
-					R_LOG_ERROR ("Out of range");
-				} else {
-					bytes[1] = -n8;
-				}
+			st64 disp = ((st64)num - op->addr) >> 1;
+			if (disp < -256 || disp > 255) {
+				R_LOG_ERROR ("Out of range");
+				return false;
 			}
+			ut16 imm = (ut16)disp & 0x1FF;
+			ut8 bytes[2] = { 0xf8 | ((imm >> 8) & 1), imm & 0xFF };
 			op->size = 2;
 			free (op->bytes);
 			op->bytes = r_mem_dup (bytes, 2);
