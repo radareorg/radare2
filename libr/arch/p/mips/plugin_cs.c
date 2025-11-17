f69adedd5073a89834af7cb7d39e6a109c8248f9
pancake <pancake@nowsecure.com>
2025-11-17 20:28:55 +0100

    Fix uaf in mips spotted by clang-analyzer

--- libr/arch/p/mips/plugin_cs.c
+++ libr/arch/p/mips/plugin_cs.c
@@ -896,6 +896,9 @@ static csh cs_handle_for_session(RArchSession *as) {
 
 static bool plugin_changed(RArchSession *as) {
 	PluginData *cpd = as->data;
+	if (!cpd) {
+		return true;
+	}
 	if (R_ARCH_CONFIG_IS_BIG_ENDIAN (as->config) != cpd->bigendian) {
 		return true;
 	}
@@ -907,14 +910,11 @@ static bool plugin_changed(RArchSession *as) {
 }
 
 static bool decode(RArchSession *as, RAnalOp *op, RArchDecodeMask mask) {
-	PluginData *pd = as->data;
 	ut64 addr = op->addr;
 	const ut8 *buf = op->bytes;
 	const int len = op->size;
 	csh handle = cs_handle_for_session (as);
-	if (!pd || handle == 0) {
-		return false;
-	}
+	PluginData *pd;
 	cs_insn *insn = NULL;
 	if (as->config->syntax == R_ARCH_SYNTAX_REGNUM) {
 		cs_option (handle, CS_OPT_SYNTAX, CS_OPT_SYNTAX_NOREGNAME);
@@ -926,6 +926,10 @@ static bool decode(RArchSession *as, RAnalOp *op, RArchDecodeMask mask) {
 		fini (as);
 		init (as);
 		handle = cs_handle_for_session (as);
+	}
+	pd = as->data;
+	if (!pd || handle == 0) {
+		return false;
 	}
 	int n, opsize = -1;
 
