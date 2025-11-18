3ef4f46159d6a8e8213d714c154528789fc5d771
pancake <pancake@nopcode.org>
2025-11-18 02:17:08 +0100

    afij

--- test/fuzz/fuzz_ia.c
+++ test/fuzz/fuzz_ia.c
@@ -7,13 +7,16 @@ int LLVMFuzzerInitialize(int *lf_argc, char ***lf_argv) {
 }
 
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-	RCore *r = r_core_new();
-
+	RCore *r = r_core_new ();
+	if (Size == 0) {
+		return 0;
+	}
 	r_core_cmdf (r, "o malloc://%zu", Size);
 	r_io_write_at (r->io, 0, Data, Size);
 
 	r_core_cmd0 (r, "oba 0");
 	r_core_cmd0 (r, "ia");
+	r_core_cmd0 (r, "ii;is;il");
 
 	r_core_free (r);
 	return 0;
