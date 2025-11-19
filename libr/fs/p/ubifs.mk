269704fa2a2a58bb54a566363f40ee61244a711c
MiKi <miki@mikelloc.com>
2025-11-19 12:10:44 +0100

    Check if lzo is available in the platform to use it

--- libr/fs/p/ubifs.mk
+++ libr/fs/p/ubifs.mk
@@ -3,8 +3,11 @@
 OBJ_UBIFS=fs_ubifs.o
 
 # Add LZO support if available
-CFLAGS+=$(shell pkg-config --cflags lzo2 2>/dev/null)
-LDFLAGS+=$(shell pkg-config --libs lzo2 2>/dev/null)
+HAVE_LZO2=$(shell pkg-config --exists lzo2 2>/dev/null && echo 1 || echo 0)
+ifeq ($(HAVE_LZO2),1)
+CFLAGS+=$(shell pkg-config --cflags lzo2)
+LDFLAGS+=$(shell pkg-config --libs lzo2)
+endif
 
 STATIC_OBJ+=${OBJ_UBIFS}
 TARGET_UBIFS=fs_ubifs.${EXT_SO}
