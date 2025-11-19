eb2cf2eac738839628e503ba9a0c1f1269db6696
astralia <astralia@nopcode.org>
2025-11-19 20:12:10 +0100

    Fix buffer ovf at r_str_scale ##crash

--- libr/util/str.c
+++ libr/util/str.c
@@ -4008,27 +4008,15 @@ R_API char *r_str_nextword(char *s, char ch) {
 }
 
 R_API char *r_str_scale(const char *s, int w, int h) {
-	// count lines and rows in (s) string
-	// compute how many lines we should remove or combine
-	// return a string containing
-	// for now this function is ascii only (no utf8 or ansi escapes)
-	RListIter *iter;
-	char *line;
+	R_RETURN_VAL_IF_FAIL (s && w > 0 && h > 0, NULL);
 	char *str = strdup (s);
 	RList *lines = r_str_split_list (str, "\n", 0);
 	int i, j;
-	int rows = 0;
-	int maxcol = 0;
-
-	rows = r_list_length (lines);
-	r_list_foreach (lines, iter, line) {
-		maxcol = R_MAX (strlen (line), maxcol);
-	}
-
+	int rows = r_list_length (lines);
 	RList *out = r_list_newf (free);
 
 	int curline = -1;
-	char *linetext = (char*)r_str_pad (' ', w);
+	char *linetext = (char*)r_str_pad2 (NULL, 0, ' ', w);
 	for (i = 0; i < h; i++) {
 		int zoomedline = (int)(i * ((double)rows / h));
 		const char *srcline = r_list_get_n (lines, zoomedline);
@@ -4043,6 +4031,7 @@ R_API char *r_str_scale(const char *s, int w, int h) {
 		}
 		memset (linetext, ' ', w);
 	}
+	free (linetext);
 	free (str);
 	return r_str_list_join (out, "\n");
 }
