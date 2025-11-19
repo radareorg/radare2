b1f0a335b221d47176733a1976a7ade8e3bcc938
pancake <pancake@nopcode.org>
2025-11-19 10:57:49 +0100

    asdifsaif

--- test/fuzz/config.mk
+++ test/fuzz/config.mk
@@ -1,7 +1,7 @@
 
-CC ?= clang-18
-CXX ?= clang++-18
-CFLAGS ?= -g -fsanitize=fuzzer,address,undefined -fsanitize-coverage=trace-pc
-CXXFLAGS ?= -g -fsanitize=fuzzer,address,undefined -fsanitize-coverage=trace-pc
-LDFLAGS ?= -fsanitize=fuzzer,address,undefined
+CC= clang-18
+CXX= clang++-18
+CFLAGS= -g -fsanitize=fuzzer,address,undefined -fsanitize-coverage=trace-pc
+CXXFLAGS = -g -fsanitize=fuzzer,address,undefined -fsanitize-coverage=trace-pc
+LDFLAGS = -fsanitize=fuzzer,address,undefined
 
