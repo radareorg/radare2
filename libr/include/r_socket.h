59bdce06c896beb4edd2943b2e1abe432c375055
pancake <pancake@nopcode.org>
2025-11-26 10:12:19 +0100

    Use RSocket.download from idld to fix command injection ##crash

--- libr/include/r_socket.h
+++ libr/include/r_socket.h
@@ -155,6 +155,7 @@ R_API int r_socket_proc_ready(RSocketProc *sp, int secs, int usecs);
 /* HTTP */
 R_API char *r_socket_http_get(const char *url, const char **headers, int *code, int *rlen);
 R_API char *r_socket_http_post(const char *url, const char **headers, const char *data, int *code, int *rlen);
+R_API bool r_socket_http_download(const char *url, const char **headers, const char *filepath);
 R_API void r_socket_http_server_set_breaked(bool *b);
 
 typedef struct r_socket_http_request {
