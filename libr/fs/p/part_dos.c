1cb6927bdf6cb2527e1df8918b551bdc804a6cc8
pancake <pancake@nopcode.org>
2025-12-01 12:51:17 +0100

    Add support for EBR (extended boot records) ##fs

--- libr/fs/p/part_dos.c
+++ libr/fs/p/part_dos.c
@@ -23,6 +23,40 @@ typedef struct {
 	ut16 aa55; // the signature
 }) MBR;
 
+static void parse_ebr(RFS *fs, RFSPartitionIterator iterate, RList *list, ut64 extended_start, ut64 ebr_sector, int *part_index) {
+	MBR ebr;
+	ut64 addr, aend;
+	DOS_ENTRY *e;
+
+	memset (&ebr, 0, sizeof (ebr));
+	fs->iob.read_at (fs->iob.io, ebr_sector * 512, (ut8*)&ebr, sizeof (ebr));
+	if (ebr.aa55 != 0xaa55) {
+		R_LOG_ERROR ("Invalid EBR signature at sector 0x%" PFMT64x, ebr_sector);
+		return;
+	}
+
+	// First entry: logical partition
+	e = &ebr.entries[0];
+	if (e->type != 0) {
+		addr = extended_start + e->start;
+		addr *= 512;
+		aend = e->length;
+		aend *= 512;
+		RFSPartition *par = r_fs_partition_new (*part_index, addr, aend);
+		par->index = 0;
+		par->type = e->type;
+		iterate (fs, par, list);
+		(*part_index)++;
+	}
+
+	// Second entry: next EBR
+	e = &ebr.entries[1];
+	if (e->type != 0) {
+		ut64 next_ebr_sector = extended_start + e->start;
+		parse_ebr (fs, iterate, list, extended_start, next_ebr_sector, part_index);
+	}
+}
+
 static int fs_part_dos(void *disk, void *ptr, void *closure) {
 	int i;
 	MBR mbr;
@@ -30,6 +64,7 @@ static int fs_part_dos(void *disk, void *ptr, void *closure) {
 	RFSPartition *par = NULL;
 	RFSPartitionIterator iterate = (RFSPartitionIterator)ptr;
 	RList *list = (RList*)closure;
+	int part_index = 0;
 
 	memset (&mbr, 0, sizeof (mbr));
 	fs->iob.read_at (fs->iob.io, 0, (ut8*)&mbr, sizeof (mbr));
@@ -41,17 +76,23 @@ static int fs_part_dos(void *disk, void *ptr, void *closure) {
 		ut64 addr, aend;
 		DOS_ENTRY *e = &mbr.entries[i];
 		if (e->type != 0) {
-			//int is_active = (e->flag == 0x80);
-			addr = e->start;
-			addr *= 512;
+			if (e->type == 0x05 || e->type == 0x0F) { // Extended partition
+				// Parse EBR chain
+				ut64 extended_start = e->start;
+				parse_ebr (fs, iterate, list, extended_start, extended_start, &part_index);
+			} else {
+				// Primary partition
+				addr = e->start;
+				addr *= 512;
 
-			aend = 0; //e->start;
-			aend += e->length;
-			aend *= 512;
-			par = r_fs_partition_new (i, addr, aend);
-			par->index = 0;
-			par->type = e->type;
-			iterate (disk, par, list);
+				aend = e->length;
+				aend *= 512;
+				par = r_fs_partition_new (part_index, addr, aend);
+				par->index = 0;
+				par->type = e->type;
+				iterate (disk, par, list);
+				part_index++;
+			}
 		} else {
 			// TODO: make deleted entries accessible?
 		}
