7506a26449a74fd59a4299b03162096119a9d057
pancake <pancake@nopcode.org>
2025-12-01 19:43:25 +0100

    asdfi

---
+++ sys/wasm-browser.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# find root
+cd `dirname $PWD/$0` ; cd ..
+#TODO: add support for ccache
+
+# XXX. fails with >1
+[ -z "${MAKE_JOBS}" ] && MAKE_JOBS=8
+
+OLD_LDFLAGS="${LDFLAGS}"
+unset LDFLAGS
+
+export CC="emcc -Os -s WASM=1 -s SIDE_MODULE=1"
+export AR="emar"
+
+CFGFLAGS="./configure --prefix=/usr --disable-debugger --with-compiler=wasm --with-static-themes --with-libr --with-wasm-browser"
+
+make mrproper
+cp -f dist/plugins-cfg/plugins.emscripten.cfg plugins.cfg
+./configure-plugins
+
+./configure ${CFGFLAGS} --host=wasm && \
+	make -s -j ${MAKE_JOBS} DEBUG=0
