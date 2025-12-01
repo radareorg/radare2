cc4e13c59ab54950f65fcf6b5322b96a30b91d56
pancake <pancake@nopcode.org>
2025-12-01 13:47:50 +0100

    missing wtf

--- libr/fs/p/part_apm.c
+++ libr/fs/p/part_apm.c
@@ -38,45 +38,45 @@ static int fs_part_apm(void *disk, void *ptr, void *closure) {
 
 	// Read first APM entry at sector 1 to get number of partitions
 	APMEntry first_entry;
-	fs->iob.read_at(fs->iob.io, 512, (ut8 *)&first_entry, sizeof(first_entry));
+	fs->iob.read_at (fs->iob.io, 512, (ut8 *)&first_entry, sizeof (first_entry));
 
-	if (memcmp(first_entry.signature, APM_SIGNATURE, APM_SIGNATURE_LEN) != 0) {
-		R_LOG_ERROR("Invalid APM signature");
+	if (memcmp (first_entry.signature, APM_SIGNATURE, APM_SIGNATURE_LEN) != 0) {
+		R_LOG_ERROR ("Invalid APM signature");
 		return 0;
 	}
 
 	ut32 num_partitions = first_entry.number_of_partitions;
 	if (num_partitions == 0 || num_partitions > 1024) {
-		R_LOG_ERROR("Invalid number of APM partitions: %u", num_partitions);
+		R_LOG_ERROR ("Invalid number of APM partitions: %u", num_partitions);
 		return 0;
 	}
 
 	// Allocate memory for all partition entries
-	size_t alloc_size = (size_t)num_partitions * sizeof(APMEntry);
+	size_t alloc_size = (size_t)num_partitions * sizeof (APMEntry);
 	if (alloc_size > (4 * 1024 * 1024)) { // 4MB sanity check
-		R_LOG_ERROR("APM partition entries allocation size too large: %zu", alloc_size);
+		R_LOG_ERROR ("APM partition entries allocation size too large: %zu", alloc_size);
 		return 0;
 	}
-	APMEntry *entries = malloc(alloc_size);
+	APMEntry *entries = malloc (alloc_size);
 	if (!entries) {
-		R_LOG_ERROR("Failed to allocate memory for APM partition entries");
+		R_LOG_ERROR ("Failed to allocate memory for APM partition entries");
 		return 0;
 	}
 
 	// Read all partition entries starting from sector 1
-	fs->iob.read_at(fs->iob.io, 512, (ut8 *)entries, alloc_size);
+	fs->iob.read_at (fs->iob.io, 512, (ut8 *)entries, alloc_size);
 
 	int i;
 	for (i = 0; i < num_partitions; i++) {
 		APMEntry *e = &entries[i];
 
 		// Check signature
-		if (memcmp(e->signature, APM_SIGNATURE, APM_SIGNATURE_LEN) != 0) {
+		if (memcmp (e->signature, APM_SIGNATURE, APM_SIGNATURE_LEN) != 0) {
 			continue; // Skip invalid entries
 		}
 
 		// Check if partition is valid (status bit 0 set means valid)
-		if (!(e->partition_status & 1)) {
+		if (! (e->partition_status & 1)) {
 			continue; // Skip invalid partitions
 		}
 
@@ -88,13 +88,13 @@ static int fs_part_apm(void *disk, void *ptr, void *closure) {
 		ut64 start = (ut64)e->partition_start * 512;
 		ut64 size = (ut64)e->partition_size * 512;
 
-		par = r_fs_partition_new(i, start, size);
+		par = r_fs_partition_new (i, start, size);
 		par->index = i;
 		par->type = 0; // APM doesn't have byte type, maybe use index
 
-		iterate(fs, par, list);
+		iterate (fs, par, list);
 	}
 
-	free(entries);
+	free (entries);
 	return 0;
 }
