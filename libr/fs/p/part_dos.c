cf206aff531f5981cc9b7a0538d805538fa9b199
pancake <pancake@nopcode.org>
2025-12-01 13:23:47 +0100

    mrefm

--- libr/fs/p/part_dos.c
+++ libr/fs/p/part_dos.c
@@ -3,25 +3,27 @@
 #include <r_fs.h>
 #include <r_types.h>
 
-R_PACKED (
-typedef struct {
-	ut8 flag; // 0x80 if active
-	ut8 start_head;
-	ut8 start_sector;
-	ut8 start_cylinder;
-	ut8 type;
-	ut8 end_head;
-	ut8 end_sector;
-	ut8 end_cylinder;
-	ut32 start; // sector count (starting from 0)
-	ut32 length; // in sector
-}) DOS_ENTRY;
-R_PACKED (
-typedef struct {
-	ut8 code[446]; // code
-	DOS_ENTRY entries[4];
-	ut16 aa55; // the signature
-}) MBR;
+R_PACKED(
+	typedef struct {
+		ut8 flag; // 0x80 if active
+		ut8 start_head;
+		ut8 start_sector;
+		ut8 start_cylinder;
+		ut8 type;
+		ut8 end_head;
+		ut8 end_sector;
+		ut8 end_cylinder;
+		ut32 start; // sector count (starting from 0)
+		ut32 length; // in sector
+	})
+DOS_ENTRY;
+R_PACKED(
+	typedef struct {
+		ut8 code[446]; // code
+		DOS_ENTRY entries[4];
+		ut16 aa55; // the signature
+	})
+MBR;
 
 static void parse_ebr(RFS *fs, RFSPartitionIterator iterate, RList *list, ut64 extended_start, ut64 ebr_sector, int *part_index) {
 	MBR ebr;
@@ -29,7 +31,7 @@ static void parse_ebr(RFS *fs, RFSPartitionIterator iterate, RList *list, ut64 e
 	DOS_ENTRY *e;
 
 	memset (&ebr, 0, sizeof (ebr));
-	fs->iob.read_at (fs->iob.io, ebr_sector * 512, (ut8*)&ebr, sizeof (ebr));
+	fs->iob.read_at (fs->iob.io, ebr_sector * 512, (ut8 *)&ebr, sizeof (ebr));
 	if (ebr.aa55 != 0xaa55) {
 		R_LOG_ERROR ("Invalid EBR signature at sector 0x%" PFMT64x, ebr_sector);
 		return;
@@ -60,14 +62,14 @@ static void parse_ebr(RFS *fs, RFSPartitionIterator iterate, RList *list, ut64 e
 static int fs_part_dos(void *disk, void *ptr, void *closure) {
 	int i;
 	MBR mbr;
-	RFS *fs = (RFS*)disk;
+	RFS *fs = (RFS *)disk;
 	RFSPartition *par = NULL;
 	RFSPartitionIterator iterate = (RFSPartitionIterator)ptr;
-	RList *list = (RList*)closure;
+	RList *list = (RList *)closure;
 	int part_index = 0;
 
 	memset (&mbr, 0, sizeof (mbr));
-	fs->iob.read_at (fs->iob.io, 0, (ut8*)&mbr, sizeof (mbr));
+	fs->iob.read_at (fs->iob.io, 0, (ut8 *)&mbr, sizeof (mbr));
 	if (mbr.aa55 != 0xaa55) {
 		R_LOG_ERROR ("Invalid DOS signature at 0x%x", (int)r_offsetof (MBR, aa55));
 		return 0;
