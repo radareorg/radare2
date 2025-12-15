91952f1609bef461b4dbe8711d32417306722e44
pancake <pancake@nopcode.org>
2025-12-15 17:09:18 +0100

    fiv

--- libr/util/str.c
+++ libr/util/str.c
@@ -4114,8 +4114,8 @@ R_API char *r_str_version(const char *program) {
 #if WITH_GPL
 	gplstr = " gpl";
 #endif
-	char *s = r_str_newf ("%s " R2_VERSION " abi:%d commit:%d @ " R_SYS_OS "-" R_SYS_ARCH "-%d\n",
-		program, R2_ABIVERSION, R2_VERSION_COMMIT,
+	char *s = r_str_newf ("%s " R2_VERSION " +%d abi:%d @ " R_SYS_OS "-" R_SYS_ARCH "-%d\n",
+		program, R2_VERSION_COMMIT, R2_ABIVERSION,
 		(R_SYS_BITS_CHECK (R_SYS_BITS, 64))? 64: 32);
 	int csv = R2_CSVERSION;
 	s = r_str_appendf (s, "birth: git.%s " R2_BIRTH "\n",
