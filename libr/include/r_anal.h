9fae5973c4f0fb6d86613ab52fc0ccb1cb2e316b
pancake <pancake@nopcode.org>
2025-12-22 13:29:17 +0100

    mis

--- libr/include/r_anal.h
+++ libr/include/r_anal.h
@@ -1684,6 +1684,7 @@ R_API void r_anal_backtrace_list(RAnal *a, ut64 addr, int opt);
 extern RAnalPlugin r_anal_plugin_null;
 extern RAnalPlugin r_anal_plugin_sbpf;
 extern RAnalPlugin r_anal_plugin_tcc;
+extern RAnalPlugin r_anal_plugin_blaze;
 extern RAnalPlugin r_anal_plugin_a2f;
 extern RAnalPlugin r_anal_plugin_path;
 extern RAnalPlugin r_anal_plugin_tp;
