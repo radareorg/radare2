eb3e81091a205c359d72ab62ec5fed4ef045d683
pancake <pancake@nopcode.org>
2026-01-04 11:27:19 +0100

    rmD

--- libr/anal/block.c
+++ libr/anal/block.c
@@ -58,8 +58,6 @@ static int __bb_addr_cmp(const void *incoming, const RBNode *in_tree, void *user
 	return 0;
 }
 
-#define D if(anal && anal->verbose)
-
 R_API void r_anal_block_ref(RAnalBlock *bb) {
 	// XXX we have R_REF for this
 	if (bb) {
