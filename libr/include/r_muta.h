0a747b0e235ccf7233b040afde9f87f61d808923
pancake <pancake@nopcode.org>
2026-01-04 08:19:51 +0100

    ifi

--- libr/include/r_muta.h
+++ libr/include/r_muta.h
@@ -107,17 +107,9 @@ R_API RMuta *R_NONNULL r_muta_new(void);
 R_API void r_muta_free(RMuta *cry);
 R_API char *r_muta_list(RMuta *cry, RMutaType type, int mode);
 
-// R_API RMutaHash *r_muta_hash(RMuta *cry, bool rst, const char *name);
-
-#if 1
-// DEPRECATE
 R_API RMutaSession *r_muta_use(RMuta *cry, const char *algo);
 R_API bool r_muta_session_set_key(RMutaSession *cry, const ut8* key, int keylen, int mode, int direction);
 R_API bool r_muta_session_set_iv(RMutaSession *cry, const ut8 *iv, int ivlen);
-#endif
-
-R_API RMutaSession *r_muta_new_session(RMuta *mu, const char *algo, RMutaOptions *opt);
-
 
 R_API RMutaSession *r_muta_session_new(RMuta *cry, RMutaPlugin *cp);
 R_API void r_muta_session_free(RMutaSession *cj);
@@ -132,7 +124,6 @@ R_API ut8 *r_muta_session_get_output(RMutaSession *cry, int *size);
 typedef int (*RMutaDecodeCallback)(void *, const ut8 *, int, ut8 **, int *);
 R_API ut8 *r_muta_session_decode_string(RMutaSession *session, const ut8 *input, int len, RMutaDecodeCallback decode_fn, void *decode_ctx);
 
-/* Bindings API */
 R_API void r_muta_bind(RMuta *muta, RMutaBind *bnd);
 
 #endif
