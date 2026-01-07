308cd6fc6d12ccaa2c0f9e42042cd0019fa5b243
pancake <pancake@nopcode.org>
2026-01-07 11:14:13 +0100

    asf

--- libr/util/thread.c
+++ libr/util/thread.c
@@ -50,9 +50,8 @@ static void *_r_th_launcher(void *_th) {
 			ut32 delay = th->delay;
 			th->delay = 0;
 			r_th_lock_leave (th->lock);
-			// Sleep in small intervals to check breaked flag
 			while (delay > 0 && !th->breaked) {
-				r_sys_usleep (10000); // 10ms intervals
+				r_sys_usleep (10000);
 				delay = (delay > 10) ? delay - 10 : 0;
 			}
 			r_th_lock_enter (th->lock);
