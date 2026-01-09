ae9753c38f2cadedec1819ee315773565c49b1a7
pancake <pancake@nopcode.org>
2026-01-09 08:11:36 +0100

    fi

--- binr/shlr.mk
+++ binr/shlr.mk
@@ -1,4 +1,3 @@
-
 # required for the static builds
 include ../../libr/socket/deps.mk
 include ../../libr/main/deps.mk
@@ -7,6 +6,5 @@ include ../../shlr/gdb/deps.mk
 include ../../shlr/java/deps.mk
 include ../../shlr/bochs/deps.mk
 include ../../shlr/qnx/deps.mk
-include ../../shlr/ar/deps.mk
 include ../../shlr/capstone.mk
 LDFLAGS+=$(LINK)
