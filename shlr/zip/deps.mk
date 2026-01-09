1bd652fbdef0b4ffac2232b0b8bb07eb54aa378a
pancake <pancake@nopcode.org>
2026-01-09 06:03:12 +0100

    ffb

--- shlr/zip/deps.mk
+++ shlr/zip/deps.mk
@@ -7,10 +7,10 @@ else
 ifeq ($(_INCLUDE_OTEZIP_MK_),)
 _INCLUDE_OTEZIP_MK_=1
 OTEZIP_ROOT=$(SHLR)/../subprojects/otezip
-OTEZIP_LIBA=$(OTEZIP_ROOT)/libotezip.a
+OTEZIP_LIBA=$(OTEZIP_ROOT)/libotezip.$(EXT_AR)
 CFLAGS+=-I$(OTEZIP_ROOT)/src/include/otezip
 $(OTEZIP_LIBA):
-	$(MAKE) -C $(OTEZIP_ROOT) CC="$(CC)" AR="$(AR)" RANLIB="$(RANLIB)" CFLAGS="$(CFLAGS) -fPIC" libotezip.a
+	$(MAKE) -C $(OTEZIP_ROOT) CC="$(CC)" EXT_AR="$(EXT_AR)" AR="$(AR)" RANLIB="$(RANLIB)" CFLAGS="$(CFLAGS) -fPIC" libotezip.$(EXT_AR)
 endif
 LINK+=$(OTEZIP_LIBA)
 endif
