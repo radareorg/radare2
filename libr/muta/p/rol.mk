ac6033cc9b44ba90911edf2490b244185290d225
pancake <pancake@nopcode.org>
2026-01-09 09:02:01 +0100

    more

--- libr/muta/p/rol.mk
+++
@@ -1,9 +0,0 @@
-OBJ_ROL=muta_rol.o
-
-STATIC_OBJ+=${OBJ_ROL}
-TARGET_ROL=muta_rol.${EXT_SO}
-
-ALL_TARGETS+=${TARGET_ROL}
-
-${TARGET_ROL}: ${OBJ_ROL}
-	${CC} $(call libname,muta_rol) ${LDFLAGS} ${CFLAGS} -o ${TARGET_ROL} ${OBJ_ROL}
