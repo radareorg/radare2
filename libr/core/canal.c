d44e4170b23b0030fa329310d8ccbedc115670c4
pancake <pancake@nowsecure.com>
2026-01-25 17:28:10 +0100

    Update libr/core/canal.c

--- libr/core/canal.c
+++ libr/core/canal.c
@@ -6227,7 +6227,7 @@ R_API void r_core_anal_esil(RCore *core, const char *str /* len */, const char *
 				}
 #endif
 			} else if (archIsX86) {
-				ut64 dst = op.ptr? op.ptr: ESIL->cur;
+				const ut64 dst = op.ptr? op.ptr: ESIL->cur;
 				if ((target && dst == ntarget) || !target) {
 					if (CHECKREF (dst)) {
 						if (dst && r_io_is_valid_offset (core->io, dst, !core->anal->opt.noncode)) {
