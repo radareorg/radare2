d627fca967fb402b6ecc5f4865ba53fa4a004511
pancake <pancake@nopcode.org>
2026-01-27 23:53:36 +0100

    Fix UAF in mdmp

--- libr/bin/format/mdmp/mdmp.c
+++ libr/bin/format/mdmp/mdmp.c
@@ -87,7 +87,6 @@ ut32 r_bin_mdmp_get_perm(struct r_bin_mdmp_obj *obj, ut64 vaddr) {
 static void r_bin_mdmp_free_pe32_bin(void *pe_bin_) {
 	struct Pe32_r_bin_mdmp_pe_bin *pe_bin = pe_bin_;
 	if (pe_bin) {
-		sdb_free (pe_bin->bin->kv);
 		Pe32_r_bin_pe_free (pe_bin->bin);
 		R_FREE (pe_bin);
 	}
@@ -96,7 +95,6 @@ static void r_bin_mdmp_free_pe32_bin(void *pe_bin_) {
 static void r_bin_mdmp_free_pe64_bin(void *pe_bin_) {
 	struct Pe64_r_bin_mdmp_pe_bin *pe_bin = pe_bin_;
 	if (pe_bin) {
-		sdb_free (pe_bin->bin->kv);
 		Pe64_r_bin_pe_free (pe_bin->bin);
 		R_FREE (pe_bin);
 	}
