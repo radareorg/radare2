0e9a84f341930f26842ec7c8fe7e2ee6299762a2
pancake <pancake@nopcode.org>
2026-02-23 13:41:10 +0100

    fi

--- libr/egg/egg_lang.c
+++ libr/egg/egg_lang.c
@@ -108,7 +108,7 @@ R_API void r_egg_lang_init(REgg *egg) {
 	/* do call or inline it? */ // BOOL
 	egg->lang.docall = 1;
 	egg->lang.line = 1;
-	egg->lang.file = "stdin";
+	egg->lang.file = strdup ("stdin");
 	egg->lang.oc = '\n';
 	egg->lang.mode = LANG_MODE_NORMAL;
 }
