9f0302f7b15f00d15ea4d9b3da5a9c4f5ea2e282
pancake <pancake@nopcode.org>
2026-03-05 13:11:52 +0100

    Fix overlapping registers in the dalvik profile ##arch

--- libr/arch/p/dalvik/plugin.c
+++ libr/arch/p/dalvik/plugin.c
@@ -1636,55 +1636,24 @@ static bool decode(RArchSession *as, RAnalOp *op, RArchDecodeMask mask) {
 }
 
 static char *regs(RArchSession *as) {
-	const char * const p =
-	"=PC	ip\n"
-	"=SP	sp\n"
-	"=BP	bp\n"
-	"=A0	v0\n"
-	"=A1	v1\n"
-	"=A2	v2\n"
-	"=A3	v3\n"
-	"=SN	v0\n"
-	"gpr	v0	.32	0	0\n"
-	"gpr	v1	.32	4	0\n"
-	"gpr	v2	.32	8	0\n"
-	"gpr	v3	.32	12	0\n"
-	"gpr	v4	.32	16	0\n"
-	"gpr	v5	.32	20	0\n"
-	"gpr	v6	.32	24	0\n"
-	"gpr	v7	.32	28	0\n"
-	"gpr	v8	.32	32	0\n"
-	"gpr	v9	.32	36	0\n"
-	"gpr	v10	.32	40	0\n"
-	"gpr	v11	.32	44	0\n"
-	"gpr	v12	.32	48	0\n"
-	"gpr	v13	.32	52	0\n"
-	"gpr	v14	.32	56	0\n"
-	"gpr	v15	.32	60	0\n"
-	"gpr	v16	.32	40	0\n"
-	"gpr	v17	.32	44	0\n"
-	"gpr	v18	.32	48	0\n"
-	"gpr	v19	.32	52	0\n"
-	"gpr	v20	.32	56	0\n"
-	"gpr	v21	.32	60	0\n"
-	"gpr	v22	.32	64	0\n"
-	"gpr	v23	.32	68	0\n"
-	"gpr	v24	.32	72	0\n"
-	"gpr	v25	.32	76	0\n"
-	"gpr	v26	.32	80	0\n"
-	"gpr	v27	.32	84	0\n"
-	"gpr	v28	.32	88	0\n"
-	"gpr	v29	.32	92	0\n"
-	"gpr	v30	.32	96	0\n"
-	"gpr	v31	.32	100	0\n"
-	"gpr	v32	.32	104	0\n"
-	"gpr	v33	.32	108	0\n"
-	"gpr	v34	.32	112	0\n"
-	"gpr	ip	.32	116	0\n"
-	"gpr	sp	.32	120	0\n"
-	"gpr	bp	.32	124	0\n"
-	;
-	return strdup (p);
+	RStrBuf *sb = r_strbuf_new (
+		"=PC	ip\n"
+		"=SP	sp\n"
+		"=BP	bp\n"
+		"=A0	v0\n"
+		"=A1	v1\n"
+		"=A2	v2\n"
+		"=A3	v3\n"
+		"=SN	v0\n"
+	);
+	int i;
+	for (i = 0; i < 256; i++) {
+		r_strbuf_appendf (sb, "gpr\tv%d\t.32\t%d\t0\n", i, i * 4);
+	}
+	r_strbuf_appendf (sb, "gpr\tip\t.32\t%d\t0\n", 256 * 4);
+	r_strbuf_appendf (sb, "gpr\tsp\t.32\t%d\t0\n", 256 * 4 + 4);
+	r_strbuf_appendf (sb, "gpr\tbp\t.32\t%d\t0\n", 256 * 4 + 8);
+	return r_strbuf_drain (sb);
 }
 
 static int archinfo(RArchSession *as, ut32 q) {
