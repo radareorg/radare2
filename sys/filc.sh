f3f33cce6d9d974b4aa23c58f7f24e21c5cef61c
pancake <pancake@nopcode.org>
2026-03-21 10:32:26 +0100

    Ledockisback

--- sys/filc.sh
+++ sys/filc.sh
@@ -7,7 +7,11 @@ FILC_VERSION=${FILC_VERSION:-0.678}
 FILC_NAME="filc-${FILC_VERSION}-linux-x86_64"
 FILC_ROOT=${FILC_ROOT:-$FILC_DIR/$FILC_NAME}
 
-[ "$(uname -s)-$(uname -m)" = "Linux-x86_64" ] || exit 1
+if [ "$(uname -s)-$(uname -m)" != "Linux-x86_64" ]; then
+	echo "filc only supports linux-amd64, falling back to Docker..."
+	docker build -t r2filc -f "$FILC_DIR/Dockerfile" "$FILC_DIR"
+	exec docker run --rm -v "$ROOT:/r2" -w /r2 r2filc ./sys/filc.sh "$@"
+fi
 
 command -v filcc || [ -x "$FILC_ROOT/build/bin/filcc" ] || {
 	mkdir -p "$FILC_DIR"
