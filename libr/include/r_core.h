1af322873943cbb99b995977a192e2c8c2b41247
pancake <trufae@gmail.com>
2026-04-09 03:02:32 +0200

    Fix command injection in abe command and add r_core_callf_at ##security

--- libr/include/r_core.h
+++ libr/include/r_core.h
@@ -552,6 +552,7 @@ R_API int r_core_cmd_call_at(RCore *core, ut64 addr, const char *cmd);
 R_API char *r_core_cmd_call_str_at(RCore *core, ut64 addr, const char *cmd);
 R_API int r_core_cmd_call(RCore *core, const char *cmd);
 R_API int r_core_cmd_callf(RCore *core, const char *cmd, ...);
+R_API int r_core_callf_at(RCore *core, ut64 addr, const char *fmt, ...) R_PRINTF_CHECK(3, 4);
 R_API int r_core_cmd_pipe(RCore *core, char *radare_cmd, char *shell_cmd);
 R_API R_MUSTUSE char *r_core_cmd_str(RCore *core, const char *cmd);
 R_API R_MUSTUSE char *r_core_cmd_str_r(RCore *core, const char *cmd);
