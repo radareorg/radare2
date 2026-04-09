radareorg/radare2

commit 27a298f31573cee24604330a814bf53ecfa50fea

Honor cfg.bigendian in pp2, pp4 and pp8 commands ##print

libr/core/cmd_print.inc.c
@@ -2875,12 +2875,6 @@ static void annotated_hexdump(RCore *core, const char *str, int len) {
 							free (flagname);
 							flagname = fnear->name;
 						}
-#if 0
-// TODO missing color here?
-						if (fnear->color) {
-							curflag = fnear;
-						}
-#endif
 						if (!curflag) {
 							curflag = fnear;
 						}
@@ -5463,6 +5457,7 @@ static void cmd_pp(RCore *core, const char *_input) {
 		return;
 	}
 	RStrBuf *sb = r_strbuf_new ("");
+	const bool be = r_config_get_b (core->config, "cfg.bigendian");
 	switch (input[0]) {
 	case 'd': // "ppd"
 		// debruijn pattern
@@ -5488,30 +5483,48 @@ static void cmd_pp(RCore *core, const char *_input) {
 	case '2': // "pp2"
 		// incremental half word sequences
 		{
-			// TODO: honor cfg.bigendian
 			int min = (core->addr & 0xffff);
 			for (i = 0; i < len; i++) {
-				r_strbuf_appendf (sb, "%04x", (int) (i + min) & 0xffff);
+				ut16 val = (ut16)((i + min) & 0xffff);
+				if (be) {
+					r_strbuf_appendf (sb, "%04x", val);
+				} else {
+					r_strbuf_appendf (sb, "%02x%02x", val & 0xff, (val >> 8) & 0xff);
+				}
 			}
 		}
 		break;
 	case '4': // "pp4"
-		// incremental half word sequences
+		// incremental word sequences
 		{
-			// TODO: honor cfg.bigendian
-			int min = (core->addr & UT32_MAX);
+			ut32 min = (core->addr & UT32_MAX);
 			for (i = 0; i < len; i++) {
-				r_strbuf_appendf (sb, "%08x", (int) (i + min) & UT32_MAX);
+				ut32 val = (ut32)((i + min) & UT32_MAX);
+				if (be) {
+					r_strbuf_appendf (sb, "%08x", val);
+				} else {
+					r_strbuf_appendf (sb, "%02x%02x%02x%02x",
+						val & 0xff, (val >> 8) & 0xff,
+						(val >> 16) & 0xff, (val >> 24) & 0xff);
+				}
 			}
 		}
 		break;
 	case '8': // "pp8"
-		// incremental half word sequences
+		// incremental qword sequences
 		{
-			// TODO: honor cfg.bigendian
 			ut64 min = (core->addr);
 			for (i = 0; i < len; i++) {
-				r_strbuf_appendf (sb, "%016" PFMT64x, i + min);
+				ut64 val = i + min;
+				if (be) {
+					r_strbuf_appendf (sb, "%016" PFMT64x, val);
+				} else {
+					r_strbuf_appendf (sb, "%02x%02x%02x%02x%02x%02x%02x%02x",
+						(ut32)(val & 0xff), (ut32)((val >> 8) & 0xff),
+						(ut32)((val >> 16) & 0xff), (ut32)((val >> 24) & 0xff),
+						(ut32)((val >> 32) & 0xff), (ut32)((val >> 40) & 0xff),
+						(ut32)((val >> 48) & 0xff), (ut32)((val >> 56) & 0xff));
+				}
 			}
 		}
 		break;

commit fae6c1a865f1a611939f87334edc7789f833a184

Fix pp2/pp4/pp8 regression: always use natural hex format ##print
Commit 27a298f introduced endian-aware byte swapping for pp2, pp4, and
pp8, but these pattern commands should always display values in natural
hex format regardless of cfg.bigendian. The byte-swapping caused pp2 2
to output 00000100 instead of the expected 00000001.

https://claude.ai/code/session_01Ls8VknTcXE7pd9ieMKDP3p

libr/core/cmd_print.inc.c
@@ -5415,7 +5415,6 @@ static void cmd_pp(RCore *core, const char *_input) {
 		return;
 	}
 	RStrBuf *sb = r_strbuf_new ("");
-	const bool be = r_config_get_b (core->config, "cfg.bigendian");
 	switch (input[0]) {
 	case 'd': // "ppd"
 		// debruijn pattern
@@ -5443,12 +5442,7 @@ static void cmd_pp(RCore *core, const char *_input) {
 		{
 			int min = (core->addr & 0xffff);
 			for (i = 0; i < len; i++) {
-				ut16 val = (ut16)((i + min) & 0xffff);
-				if (be) {
-					r_strbuf_appendf (sb, "%04x", val);
-				} else {
-					r_strbuf_appendf (sb, "%02x%02x", val & 0xff, (val >> 8) & 0xff);
-				}
+				r_strbuf_appendf (sb, "%04x", (int)(i + min) & 0xffff);
 			}
 		}
 		break;
@@ -5457,14 +5451,7 @@ static void cmd_pp(RCore *core, const char *_input) {
 		{
 			ut32 min = (core->addr & UT32_MAX);
 			for (i = 0; i < len; i++) {
-				ut32 val = (ut32)((i + min) & UT32_MAX);
-				if (be) {
-					r_strbuf_appendf (sb, "%08x", val);
-				} else {
-					r_strbuf_appendf (sb, "%02x%02x%02x%02x",
-						val & 0xff, (val >> 8) & 0xff,
-						(val >> 16) & 0xff, (val >> 24) & 0xff);
-				}
+				r_strbuf_appendf (sb, "%08x", (ut32)((i + min) & UT32_MAX));
 			}
 		}
 		break;
@@ -5473,16 +5460,7 @@ static void cmd_pp(RCore *core, const char *_input) {
 		{
 			ut64 min = (core->addr);
 			for (i = 0; i < len; i++) {
-				ut64 val = i + min;
-				if (be) {
-					r_strbuf_appendf (sb, "%016" PFMT64x, val);
-				} else {
-					r_strbuf_appendf (sb, "%02x%02x%02x%02x%02x%02x%02x%02x",
-						(ut32)(val & 0xff), (ut32)((val >> 8) & 0xff),
-						(ut32)((val >> 16) & 0xff), (ut32)((val >> 24) & 0xff),
-						(ut32)((val >> 32) & 0xff), (ut32)((val >> 40) & 0xff),
-						(ut32)((val >> 48) & 0xff), (ut32)((val >> 56) & 0xff));
-				}
+				r_strbuf_appendf (sb, "%016" PFMT64x, i + min);
 			}
 		}
 		break;