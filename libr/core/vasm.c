491b3821cbe180fb88b3c44664d821b8c3737518
pancake <pancake@nopcode.org>
2026-04-10 11:34:09 +0200

    Avoid strcpy on fixed size buffer in visual assembler

--- libr/core/vasm.c
+++ libr/core/vasm.c
@@ -20,23 +20,22 @@ typedef struct {
 static int readline_callback(RCons *cons, void *_a, const char *str) {
 	RCoreVisualAsm *a = _a;
 	RCore *core = a->core;
+	RLine *line = cons->line;
 	r_cons_clear00 (cons);
 	r_cons_printf (cons, "Write %s-%" PFMT64d " %s... (! for %s, ^C to quit)\n\n",
-		r_config_get (a->core->config, "asm.arch"),
-		r_config_get_i (a->core->config, "asm.bits"),
+		r_config_get (core->config, "asm.arch"),
+		r_config_get_i (core->config, "asm.bits"),
 		a->amode? "assembly": "hexpairs",
-		a->amode? "hexpairs": "assembly"
-		);
-	r_asm_set_pc (a->core->rasm, a->off);
-	RLine *line = cons->line;
+		a->amode? "hexpairs": "assembly");
+	r_asm_set_pc (core->rasm, a->off);
 	if (*str == '!') {
 		a->amode = !a->amode;
 		line->buffer.data[0] = 0;
 		line->buffer.length = 0;
 	} else if (r_str_endswith (str, "!")) {
 		a->amode = !a->amode;
-		strcpy (line->buffer.data, a->otherstr);
-		line->buffer.length = strlen (a->otherstr);
+		const char *src = a->otherstr? a->otherstr: "";
+		line->buffer.length = r_str_ncpy (line->buffer.data, src, sizeof (line->buffer.data));
 		line->buffer.index = line->buffer.length;
 	} else if (*str == '?') {
 		r_cons_printf (core->cons, "[VA]> ?\n\n"
@@ -46,60 +45,49 @@ static int readline_callback(RCons *cons, void *_a, const char *str) {
 			"  this assembler supports various directives like .hex ...\n"
 			"!       toggle between hexpairs or assembly as input\n"
 			"RETURN  write the bytes/assembly in place\n"
-			"^C      leave this mode\n"
-			);
+			"^C      leave this mode\n");
 	} else if (a->amode) {
 		r_asm_code_free (a->acode);
-		a->acode = r_asm_assemble (a->core->rasm, str);
+		a->acode = r_asm_assemble (core->rasm, str);
 	} else {
-#if 0
-		r_asm_code_free (a->acode);
-		char *fmthex = r_str_newf (".hex %s", str);
-		a->acode = r_asm_assemble (a->core->rasm, fmthex);
-		free (fmthex);
-#else
 		ut8 out[1024];
 		int len = r_hex_str2bin (str, out);
-		if (len > 0) {
+		if (len > 0 && a->acode) {
 			free (a->acode->bytes);
 			a->acode->bytes = r_mem_dup (out, len);
 			a->acode->len = len;
 		}
 		a->codebuf[0] = 0;
-#endif
 	}
-	const char *hex = (a->acode)? r_asm_code_get_hex (a->acode): "";
+	const char *hex = a->acode? r_asm_code_get_hex (a->acode): "";
 	r_cons_printf (core->cons, "[%s:%d]> %s\n",
-			a->amode? "ASM": "HEX",
-			a->acode? a->acode->len: 0, str);
+		a->amode? "ASM": "HEX",
+		a->acode? a->acode->len: 0, str);
 	if (a->acode) {
 		int xlen = R_MIN ((2 * a->acode->len), R_VISUAL_ASM_BUFSIZE - 2);
-		strcpy (a->codebuf, a->blockbuf);
+		r_str_ncpy (a->codebuf, a->blockbuf, R_VISUAL_ASM_BUFSIZE);
 		memcpy (a->codebuf, hex, xlen);
 		if (xlen >= strlen (a->blockbuf)) {
 			a->codebuf[xlen] = '\0';
 		}
 	}
+	free (a->otherstr);
 	if (a->amode) {
-		free (a->otherstr);
 		a->otherstr = strdup (hex);
 	} else {
-		free (a->otherstr);
-		a->otherstr = r_core_cmd_strf (a->core, "pad %s", hex);
+		a->otherstr = r_core_cmd_strf (core, "pad %s", hex);
 		r_str_replace_char (a->otherstr, '\n', ';');
 	}
-	{
-		int rows = 0;
-		int cols = r_cons_get_size (core->cons, &rows);
-		core->print->cur_enabled = true;
-		core->print->ocur = 0;
-		core->print->cur = (a->acode && a->acode->len) ? a->acode->len - 1: 0;
-		char *res = r_core_cmd_strf (a->core, "pd %d @x:%s @0x%08"PFMT64x, rows - 11, a->codebuf, a->off);
-		char *msg = r_str_ansi_crop (res, 0, 0, cols - 2, rows - 5);
-		r_cons_printf (core->cons, "%s\n", msg);
-		free (msg);
-		free (res);
-	}
+	int rows = 0;
+	int cols = r_cons_get_size (core->cons, &rows);
+	core->print->cur_enabled = true;
+	core->print->ocur = 0;
+	core->print->cur = (a->acode && a->acode->len)? a->acode->len - 1: 0;
+	char *res = r_core_cmd_strf (core, "pd %d @x:%s @0x%08"PFMT64x, rows - 11, a->codebuf, a->off);
+	char *msg = r_str_ansi_crop (res, 0, 0, cols - 2, rows - 5);
+	r_cons_println (core->cons, msg);
+	free (msg);
+	free (res);
 	r_cons_flush (core->cons);
 	return 1;
 }
