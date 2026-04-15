59db360bac3cf9c451863d8c7d6570f35dd34ae5
s-zaizen <180300122+s-zaizen@users.noreply.github.com>
2026-04-15 21:01:51 +0900

    Avoid shell-evaluated seek in afsv arg printing ##anal

--- libr/core/cmd_anal.inc.c
+++ libr/core/cmd_anal.inc.c
@@ -5068,8 +5068,10 @@ static char *print_fcn_arg(RCore *core, const char *type, const char *name, cons
 	char *argstr = NULL;
 	if (addr != UT32_MAX && addr != UT64_MAX  && addr != 0) {
 		char *safe_name = r_str_sanitize_r2 (name);
-		char *res = r_core_cmd_strf (core, "pfq %s%s %s @ 0x%08" PFMT64x,
-			(on_stack == 1) ? "*" : "", fmt, safe_name, addr);
+		char *cmd = r_str_newf ("pfq %s%s %s",
+			(on_stack == 1) ? "*" : "", fmt, safe_name);
+		char *res = r_core_call_str_at (core, addr, cmd);
+		free (cmd);
 		free (safe_name);
 		r_str_trim (res);
 		if (r_str_startswith (res, "\"\\xff\\xff")) {
