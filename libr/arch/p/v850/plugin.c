6a9403dc32da114f6393ff7cf3b4fadcd0934193
brainstorm <brainstorm@nopcode.org>
2026-04-16 19:34:06 +0200

    V850 ESIL: Fix flags, sign-extension, bit ops, and shifts
    - Add tmp scratch register for esil->cur after non-compound ops
    - Fix TST/SAR flag computation using tmp register
    - Fix SHL/SHR/SAR carry flag (CY = last bit shifted out)
    - Fix SAR S/Z flag ordering (was swapped on stack)
    - Fix CMP overflow flag (OV)
    - Fix andi S flag (always cleared per V850 spec)
    - Fix subr flag computation
    - Fix add/addi flag computation
    - Add sign-extension for ld.b, ld.h, sld.b, sld.h
    - Fix bit operations (set1, clr1, not1, tst1) to use byte access [1]
    - Add ESIL for di/ei instructions
    - Add ESIL for sxb/sxh sign-extension instructions
    - Enforce r0=0 by appending ,0,r0,:= to all instructions
    - Fix %= operator pushing remainder before assignment
    - Fix >>= operator removing >64bit shift check

--- libr/arch/p/v850/plugin.c
+++ libr/arch/p/v850/plugin.c
@@ -449,7 +449,6 @@ static int v850e0_op(RArchSession *a, RAnalOp *op, ut64 addr, const ut8 *buf, in
 		break;
 	case V850_TST:
 		op->type = R_ANAL_OP_TYPE_CMP;
-		// compare (a & b) against 0 to set esil->cur so $s/$z reflect the AND
 		r_strbuf_appendf (&op->esil, "0,%s,%s,&,==", F1_RN1(word1), F1_RN2(word1));
 		update_flags (op, V850_FLAG_S | V850_FLAG_Z);
 		clear_flags (op, V850_FLAG_OV);
@@ -495,10 +494,11 @@ static int v850e0_op(RArchSession *a, RAnalOp *op, ut64 addr, const ut8 *buf, in
 			ut8 shift = (ut8)F2_IMM (word1);
 			reg2 = F2_RN2 (word1);
 			if (shift) {
-				r_strbuf_appendf (&op->esil, "%s,%u,>>,1,&,cy,:=,%u,%s,>>=", reg2, shift - 1, shift, reg2);
+				r_strbuf_appendf (&op->esil, "%s,%u,>>,1,&,cy,:=,", reg2, shift - 1);
 			} else {
-				r_strbuf_appendf (&op->esil, "0,cy,:=,0,%s,>>=", reg2);
+				r_strbuf_append (&op->esil, "0,cy,:=,");
 			}
+			r_strbuf_appendf (&op->esil, "%u,%s,>>=", shift, reg2);
 		}
 		update_flags (op, V850_FLAG_S | V850_FLAG_Z);
 		clear_flags (op, V850_FLAG_OV);
@@ -525,10 +525,11 @@ static int v850e0_op(RArchSession *a, RAnalOp *op, ut64 addr, const ut8 *buf, in
 			ut8 shift = (ut8)F2_IMM(word1);
 			reg2 = F2_RN2(word1);
 			if (shift) {
-				r_strbuf_appendf (&op->esil, "%s,%u,>>,1,&,cy,:=,%u,%s,<<=", reg2, 32 - shift, shift, reg2);
+				r_strbuf_appendf (&op->esil, "%s,%u,>>,1,&,cy,:=,", reg2, 32 - shift);
 			} else {
-				r_strbuf_appendf (&op->esil, "0,cy,:=,0,%s,<<=", reg2);
+				r_strbuf_append (&op->esil, "0,cy,:=,");
 			}
+			r_strbuf_appendf (&op->esil, "%u,%s,<<=", shift, reg2);
 		}
 		update_flags (op, V850_FLAG_S | V850_FLAG_Z);
 		clear_flags (op, V850_FLAG_OV);
@@ -656,15 +657,15 @@ static int v850e0_op(RArchSession *a, RAnalOp *op, ut64 addr, const ut8 *buf, in
 			break;
 		case V850_EXT_SHL:
 			op->type = R_ANAL_OP_TYPE_SHL;
-			r_strbuf_appendf (&op->esil, "%s,32,%s,-,>>,1,&,cy,:=,%s,%s,<<=",
-				F9_RN2 (word1), F9_RN1 (word1), F9_RN1 (word1), F9_RN2 (word1));
+			r_strbuf_appendf (&op->esil, "%s,32,-,%s,>>,1,&,cy,:=,%s,%s,<<=",
+				F9_RN1 (word1), F9_RN2 (word1), F9_RN1 (word1), F9_RN2 (word1));
 			update_flags (op, V850_FLAG_S | V850_FLAG_Z);
 			clear_flags (op, V850_FLAG_OV);
 			break;
 		case V850_EXT_SHR:
 			op->type = R_ANAL_OP_TYPE_SHR;
-			r_strbuf_appendf (&op->esil, "%s,%s,1,-,>>,1,&,cy,:=,%s,%s,>>=",
-				F9_RN2 (word1), F9_RN1 (word1), F9_RN1 (word1), F9_RN2 (word1));
+			r_strbuf_appendf (&op->esil, "%s,--,%s,>>,1,&,cy,:=,%s,%s,>>=",
+				F9_RN1 (word1), F9_RN2 (word1), F9_RN1 (word1), F9_RN2 (word1));
 			update_flags (op, V850_FLAG_S | V850_FLAG_Z);
 			clear_flags (op, V850_FLAG_OV);
 			break;
@@ -672,8 +673,7 @@ static int v850e0_op(RArchSession *a, RAnalOp *op, ut64 addr, const ut8 *buf, in
 			op->type = R_ANAL_OP_TYPE_SAR;
 			reg1 = F9_RN1 (word1);
 			reg2 = F9_RN2 (word1);
-			// CY = bit(reg1-1) of original reg2; compute first (before reg2 is overwritten)
-			r_strbuf_appendf (&op->esil, "%s,%s,1,-,>>,1,&,cy,:=,", reg2, reg1);
+			r_strbuf_appendf (&op->esil, "%s,--,%s,>>,1,&,cy,:=,", reg1, reg2);
 			r_strbuf_appendf (&op->esil, "31,%s,>>,?{,%s,32,-,%s,1,<<,--,<<,}{,0,},%s,%s,>>,|,%s,=",
 				reg2, reg1, reg1, reg1, reg2, reg2);
 			update_flags (op, V850_FLAG_S | V850_FLAG_Z);
