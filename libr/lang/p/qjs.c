radareorg/radare2

commit 431b3dffae9866ca222f555e3f35e995a8cfac21

Fix #25872 - null deref in r2fload ##crash

libr/lang/p/qjs.c
@@ -458,8 +458,11 @@ static JSValue r2fload(JSContext *ctx, JSValueConst this_val, int argc, JSValueC
 	size_t plen;
 	const char *n = JS_ToCStringLen2 (ctx, &plen, argv[0], false);
 	size_t sz = 0;
-	char *s = r_file_slurp (n, &sz);
-	JSValue v = JS_NewString (ctx, s);
+	char *s = NULL;
+	if (R_STR_ISNOTEMPTY (n)) {
+		s = r_file_slurp (n, &sz);
+	}
+	JSValue v = JS_NewString (ctx, r_str_get (s));
 	free (s);
 	return v;
 }

commit 91fa9ddd154ac9683bc1fd743e0c9dd57978a732

Improve error handling in r2fload ##qjs
Throw an error if slurp fails as suggested in #25873

libr/lang/p/qjs.c
@@ -462,6 +462,9 @@ static JSValue r2fload(JSContext *ctx, JSValueConst this_val, int argc, JSValueC
 	if (R_STR_ISNOTEMPTY (n)) {
 		s = r_file_slurp (n, &sz);
 	}
+	if (!s) {
+		return JS_ThrowPlainError (ctx, "Failed to load file %s", n);
+	}
 	JSValue v = JS_NewString (ctx, r_str_get (s));
 	free (s);
 	return v;