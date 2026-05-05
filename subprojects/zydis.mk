9f419c6f068ffff7977cfcda0d060a31317074fd
Priyanshu Kumar <priyanshu.kumar@broadcom.com>
2026-05-05 06:59:47 +0000

    Fix bundled Zydis CI builds ##ci

--- subprojects/zydis.mk
+++ subprojects/zydis.mk
@@ -1,10 +1,12 @@
 .PHONY: zydis zydis_clean
 
+ZYDIS_BUILD_CFLAGS=$(filter-out -Werror%,$(CFLAGS))
+
 zydis:
 	rm -rf zydis.tmp
 	mkdir -p zydis.tmp
 	cp -rf packagefiles/zydis/* zydis.tmp
-	${CC} ${CFLAGS} -fPIC -DZYDIS_STATIC_BUILD -Izydis.tmp/amalgamated-dist -c zydis.tmp/amalgamated-dist/Zydis.c -o zydis.tmp/Zydis.o
+	${CC} ${ZYDIS_BUILD_CFLAGS} -fPIC -DZYDIS_STATIC_BUILD -Izydis.tmp/amalgamated-dist -c zydis.tmp/amalgamated-dist/Zydis.c -o zydis.tmp/Zydis.o
 	${AR} rcs zydis.tmp/libzydis.a zydis.tmp/Zydis.o
 	rm -rf zydis
 	mv zydis.tmp zydis
