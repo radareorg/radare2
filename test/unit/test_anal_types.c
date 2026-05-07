3cfbe770fd11f2aad92905abcff03ea85d6e6d61
Priyanshu Kumar <priyanshu.kumar@broadcom.com>
2026-05-07 21:12:14 +0530

    Add r2sleigh native analysis integration ##analysis

--- test/unit/test_anal_types.c
+++ test/unit/test_anal_types.c
@@ -338,6 +338,145 @@ static bool test_anal_get_base_type_not_found(void) {
 	mu_end;
 }
 
+static bool test_anal_types_snapshot_epoch_and_context_hash(void) {
+	RAnal *anal = r_anal_new ();
+	mu_assert_notnull (anal, "Couldn't create new RAnal");
+
+	ut64 epoch0 = r_anal_types_dirty_epoch (anal);
+	ut64 hash0 = r_anal_types_context_hash (anal);
+	mu_assert_neq (hash0, 0, "initial type context hash");
+
+	RList *snapshot0 = r_anal_types_snapshot (anal);
+	mu_assert_notnull (snapshot0, "initial type snapshot");
+	r_anal_types_snapshot_free (snapshot0);
+
+	RAnalBaseType *base = r_anal_base_type_new (R_ANAL_BASE_TYPE_KIND_ATOMIC);
+	base->name = strdup ("codex_u8");
+	base->type = strdup ("u");
+	base->size = 8;
+	r_anal_save_base_type (anal, base);
+	r_anal_base_type_free (base);
+
+	ut64 epoch1 = r_anal_types_dirty_epoch (anal);
+	ut64 hash1 = r_anal_types_context_hash (anal);
+	mu_assert_neq (epoch1, epoch0, "saving a base type bumps the dirty epoch");
+	mu_assert_neq (hash1, hash0, "saving a base type changes the type context hash");
+	mu_assert_eq (r_anal_types_context_hash (anal), hash1, "type context hash is cached until the next epoch");
+
+	bool found = false;
+	RList *snapshot1 = r_anal_types_snapshot (anal);
+	mu_assert_notnull (snapshot1, "updated type snapshot");
+	RAnalBaseType *type;
+	RListIter *iter;
+	r_list_foreach (snapshot1, iter, type) {
+		if (type && type->name && !strcmp (type->name, "codex_u8")) {
+			found = true;
+			break;
+		}
+	}
+	r_anal_types_snapshot_free (snapshot1);
+	mu_assert_true (found, "updated type snapshot contains saved base type");
+
+	r_anal_free (anal);
+	mu_end;
+}
+
+static bool test_anal_types_link_epoch_and_context_hash(void) {
+	RAnal *anal = r_anal_new ();
+	mu_assert_notnull (anal, "Couldn't create new RAnal");
+	mu_assert_notnull (anal->sdb_types, "Couldn't create new RAnal.sdb_types");
+	sdb_set (anal->sdb_types, "codex_link_type", "type", 0);
+
+	ut64 epoch0 = r_anal_types_dirty_epoch (anal);
+	ut64 hash0 = r_anal_types_context_hash (anal);
+	mu_assert_true (r_anal_types_set_link (anal, "codex_link_type", 0x401000), "type link applies through typed API");
+	ut64 epoch1 = r_anal_types_dirty_epoch (anal);
+	ut64 hash1 = r_anal_types_context_hash (anal);
+	mu_assert_neq (epoch1, epoch0, "type link bumps dirty epoch");
+	mu_assert_neq (hash1, hash0, "type link changes context hash");
+	mu_assert_streq (sdb_const_get (anal->sdb_types, "link.00401000", 0), "codex_link_type", "type link stored in sdb");
+
+	mu_assert_true (r_anal_types_set_link_offset (anal, "codex_link_type", 0x401008), "type offset link applies through typed API");
+	ut64 epoch2 = r_anal_types_dirty_epoch (anal);
+	ut64 hash2 = r_anal_types_context_hash (anal);
+	mu_assert_neq (epoch2, epoch1, "type offset link bumps dirty epoch");
+	mu_assert_neq (hash2, hash1, "type offset link changes context hash");
+	mu_assert_streq (sdb_const_get (anal->sdb_types, "offset.00401008", 0), "codex_link_type", "type offset link stored in sdb");
+
+	mu_assert_true (r_anal_types_unlink (anal, 0x401000), "type link removal applies through typed API");
+	ut64 epoch3 = r_anal_types_dirty_epoch (anal);
+	ut64 hash3 = r_anal_types_context_hash (anal);
+	mu_assert_neq (epoch3, epoch2, "type unlink bumps dirty epoch");
+	mu_assert_neq (hash3, hash2, "type unlink changes context hash");
+	mu_assert_null (sdb_const_get (anal->sdb_types, "link.00401000", 0), "type link removed from sdb");
+
+	r_anal_free (anal);
+	mu_end;
+}
+
+static bool test_anal_types_link_context_hash_is_order_independent(void) {
+	RAnal *left = r_anal_new ();
+	RAnal *right = r_anal_new ();
+	mu_assert_notnull (left, "Couldn't create left RAnal");
+	mu_assert_notnull (right, "Couldn't create right RAnal");
+	sdb_set (left->sdb_types, "codex_link_type", "type", 0);
+	sdb_set (right->sdb_types, "codex_link_type", "type", 0);
+
+	mu_assert_true (r_anal_types_set_link (left, "codex_link_type", 0x401000), "left first link");
+	mu_assert_true (r_anal_types_set_link_offset (left, "codex_link_type", 0x401008), "left second link");
+	mu_assert_true (r_anal_types_set_link_offset (right, "codex_link_type", 0x401008), "right first link");
+	mu_assert_true (r_anal_types_set_link (right, "codex_link_type", 0x401000), "right second link");
+
+	mu_assert_eq (r_anal_types_dirty_epoch (left), r_anal_types_dirty_epoch (right), "same mutation count produces same type epoch");
+	mu_assert_eq (r_anal_types_context_hash (left), r_anal_types_context_hash (right), "type context hash is independent of link insertion order");
+
+	r_anal_free (left);
+	r_anal_free (right);
+	mu_end;
+}
+
+static bool test_anal_mutation_type_link_bumps_type_context(void) {
+	RAnal *anal = r_anal_new ();
+	mu_assert_notnull (anal, "Couldn't create new RAnal");
+	sdb_set (anal->sdb_types, "codex_mut_type", "type", 0);
+	ut64 epoch0 = r_anal_types_dirty_epoch (anal);
+	ut64 hash0 = r_anal_types_context_hash (anal);
+	RAnalMutation mutation = {
+		.kind = R_ANAL_MUTATION_TYPE_LINK,
+		.type = "codex_mut_type",
+		.addr = 0x402000,
+	};
+	RAnalMutationResult result = {0};
+	mu_assert_true (r_anal_apply_mutations (anal, &mutation, 1, &result), "type link mutation batch succeeds");
+	mu_assert_eq (result.attempted, 1, "one mutation attempted");
+	mu_assert_eq (result.applied, 1, "one mutation applied");
+	mu_assert_eq (result.failed, 0, "no mutation failed");
+	mu_assert_neq (r_anal_types_dirty_epoch (anal), epoch0, "type link mutation bumps dirty epoch");
+	mu_assert_neq (r_anal_types_context_hash (anal), hash0, "type link mutation changes context hash");
+	mu_assert_streq (sdb_const_get (anal->sdb_types, "link.00402000", 0), "codex_mut_type", "type link mutation stored in sdb");
+
+	r_anal_free (anal);
+	mu_end;
+}
+
+static bool test_anal_mutation_type_link_accepts_zero_addr(void) {
+	RAnal *anal = r_anal_new ();
+	mu_assert_notnull (anal, "Couldn't create new RAnal");
+	sdb_set (anal->sdb_types, "codex_zero_type", "type", 0);
+	RAnalMutation mutation = {
+		.kind = R_ANAL_MUTATION_TYPE_LINK,
+		.type = "codex_zero_type",
+		.addr = 0,
+	};
+	RAnalMutationResult result = {0};
+	mu_assert_true (r_anal_apply_mutations (anal, &mutation, 1, &result), "zero-address type link mutation succeeds");
+	mu_assert_eq (result.applied, 1, "zero-address type link mutation applied");
+	mu_assert_streq (sdb_const_get (anal->sdb_types, "link.00000000", 0), "codex_zero_type", "zero-address type link stored in sdb");
+
+	r_anal_free (anal);
+	mu_end;
+}
+
 int all_tests(void) {
 	mu_run_test (test_anal_get_base_type_struct);
 	mu_run_test (test_anal_save_base_type_struct);
@@ -350,6 +489,11 @@ int all_tests(void) {
 	mu_run_test (test_anal_get_base_type_atomic);
 	mu_run_test (test_anal_save_base_type_atomic);
 	mu_run_test (test_anal_get_base_type_not_found);
+	mu_run_test (test_anal_types_snapshot_epoch_and_context_hash);
+	mu_run_test (test_anal_types_link_epoch_and_context_hash);
+	mu_run_test (test_anal_types_link_context_hash_is_order_independent);
+	mu_run_test (test_anal_mutation_type_link_bumps_type_context);
+	mu_run_test (test_anal_mutation_type_link_accepts_zero_addr);
 	return tests_passed != tests_run;
 }
 
