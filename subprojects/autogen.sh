7f78315c93e921690a9e7172a499bb3b1806e39f
pancake <pancake@nopcode.org>
2026-05-22 13:11:09 +0200

    Skip the zydis mk because its handcrafted for make

--- subprojects/autogen.sh
+++ subprojects/autogen.sh
@@ -3,6 +3,10 @@ ACR_WRAP=acr-wrap
 
 for i in *.wrap ; do
 	o=`echo $i | sed -e 's,.wrap,.mk,'`
+	if [ -n "`echo $o | grep zydis`" ]; then
+		echo "[ACR] Skipping $i"
+		continue
+	fi
 	echo "[ACR] Wrapping $i"
 	${ACR_WRAP} $i > $o
 done
