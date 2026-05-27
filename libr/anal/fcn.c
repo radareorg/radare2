5a1c7d5025fc0ae7892563cea7f3fc09b1935478
pancake <pancake@nopcode.org>
2026-05-27 18:33:33 +0200

    Clean up function analysis helpers

--- libr/anal/fcn.c
+++ libr/anal/fcn.c
@@ -88,6 +88,16 @@ static bool cond_is_inverse(RAnalCondType a, RAnalCondType b) {
 		|| (a == R_ANAL_CONDTYPE_NE && b == R_ANAL_CONDTYPE_EQ);
 }
 
+static void fcn_stack_delta(RAnalFunction *fcn, RAnalBlock *bb, st64 delta) {
+	if (R_ABS (delta) < R_ANAL_MAX_INCSTACK) {
+		fcn->stack += delta;
+		if (fcn->stack > fcn->maxstack) {
+			fcn->maxstack = fcn->stack;
+		}
+	}
+	bb->stackptr += delta;
+}
+
 R_API int r_anal_function_resize(RAnalFunction *fcn, int newsize) {
 	RAnal *anal = fcn->anal;
 	RAnalBlock *bb;
@@ -149,7 +159,9 @@ static bool is_symbol_flag(const char *name) {
 }
 
 static bool next_instruction_is_symbol(RAnal *anal, RAnalOp *op) {
-	R_RETURN_VAL_IF_FAIL (anal && op && anal->flb.get_at, false);
+	if (!anal->flb.get_at) {
+		return false;
+	}
 	RFlagItem *fi = anal->flb.get_at (anal->flb.f, op->addr + op->size, false);
 	return (fi && fi->name && is_symbol_flag (fi->name));
 }
@@ -264,12 +276,11 @@ static bool is_delta_pointer_table(ReadAhead *ra, RAnal *anal, RAnalFunction *fc
 	return true;
 }
 
-static ut64 try_get_cmpval_from_parents(RAnal *anal, RAnalFunction *fcn, RAnalBlock *my_bb, const char *cmp_reg) {
+static ut64 try_get_cmpval_from_parents(RAnalFunction *fcn, RAnalBlock *my_bb, const char *cmp_reg) {
 	if (!cmp_reg) {
 		R_LOG_DEBUG ("try_get_cmpval_from_parents: cmp_reg not defined");
 		return UT64_MAX;
 	}
-	R_RETURN_VAL_IF_FAIL (fcn && fcn->bbs, UT64_MAX);
 	RListIter *iter;
 	RAnalBlock *tmp_bb;
 	r_list_foreach (fcn->bbs, iter, tmp_bb) {
@@ -425,9 +436,6 @@ static RAnalBlock *bbget(RAnal *anal, ut64 addr, bool jumpmid) {
 }
 
 static bool block_belongs_to_function(const RAnalBlock *block, const RAnalFunction *fcn) {
-	if (!block || !fcn) {
-		return false;
-	}
 	return r_list_contains ((RList *)block->fcns, (void *)fcn);
 }
 
@@ -464,7 +472,6 @@ static bool add_switch_case_block(RAnal *anal, RAnalFunction *fcn, ut64 case_add
 }
 
 static bool reset_switch_case_stub(RAnal *anal, RAnalBlock *block) {
-	R_RETURN_VAL_IF_FAIL (anal && block, false);
 	RList *fcns = r_list_new ();
 	if (!fcns) {
 		return false;
@@ -1051,13 +1058,7 @@ noskip:
 		// compiler bug or junk or something it wont get treated as a delay
 		switch (op->stackop) {
 		case R_ANAL_STACK_INC:
-			if (R_ABS (op->stackptr) < R_ANAL_MAX_INCSTACK) {
-				fcn->stack += op->stackptr;
-				if (fcn->stack > fcn->maxstack) {
-					fcn->maxstack = fcn->stack;
-				}
-			}
-			bb->stackptr += op->stackptr;
+			fcn_stack_delta (fcn, bb, op->stackptr);
 			break;
 		case R_ANAL_STACK_RESET:
 			bb->stackptr = 0;
@@ -1812,7 +1813,7 @@ noskip:
 						// skip inlined jumptable
 						// idx += table_size;
 					} else if (op->ptrsize == 1) { // TBB
-						ut64 pred_cmpval = try_get_cmpval_from_parents (anal, fcn, bb, op->ireg);
+						ut64 pred_cmpval = try_get_cmpval_from_parents (fcn, bb, op->ireg);
 						ut64 table_size = 0;
 						if (pred_cmpval != UT64_MAX) {
 							table_size += pred_cmpval;
@@ -1824,7 +1825,7 @@ noskip:
 						// skip inlined jumptable
 						idx += table_size;
 					} else if (op->ptrsize == 2) { // LDRH on thumb/arm
-						ut64 pred_cmpval = try_get_cmpval_from_parents(anal, fcn, bb, op->ireg);
+						ut64 pred_cmpval = try_get_cmpval_from_parents (fcn, bb, op->ireg);
 						int tablesize = 1;
 						if (pred_cmpval != UT64_MAX) {
 							tablesize += pred_cmpval;
