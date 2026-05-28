de06aa2d1b91026e16ed077a12c11b3a9a6a072c
pancake <pancake@nopcode.org>
2026-05-28 20:34:16 +0200

    Fix sm4 search on big endian

--- libr/search/sm4_find.c
+++ libr/search/sm4_find.c
@@ -7,19 +7,32 @@
 #define SM4_SEARCH_LENGTH 24
 #define SM4_KEY_LENGTH    16
 
-static bool sm4_key_test(const unsigned char *buf) {
-	ut32 *ptr = (ut32 *)buf;
-	return (ptr[4] == (ptr[0] ^ (sm4_RK (ptr[1] ^ ptr[2] ^ ptr[3] ^ sm4_CK[4])))) && (ptr[5] == (ptr[1] ^ (sm4_RK (ptr[2] ^ ptr[3] ^ ptr[4] ^ sm4_CK[5]))));
+static ut32 sm4_word(const ut8 *buf, int index) {
+	return r_read_at_le32 (buf, index * sizeof (ut32));
+}
+
+static bool sm4_key_test(const ut8 *buf) {
+	ut32 w0 = sm4_word (buf, 0);
+	ut32 w1 = sm4_word (buf, 1);
+	ut32 w2 = sm4_word (buf, 2);
+	ut32 w3 = sm4_word (buf, 3);
+	ut32 w4 = sm4_word (buf, 4);
+	ut32 w5 = sm4_word (buf, 5);
+	return w4 == (w0 ^ sm4_RK (w1 ^ w2 ^ w3 ^ sm4_CK[4]))
+		&& w5 == (w1 ^ sm4_RK (w2 ^ w3 ^ w4 ^ sm4_CK[5]));
 }
 
 // Display the corresponding master key which is not directly in memory for SM4.
-static void sm4_master_key(const unsigned char *buf, ut64 addr) {
-	ut32 *ptr = (ut32 *)buf;
+static void sm4_master_key(const ut8 *buf, ut64 addr) {
+	ut32 w0 = sm4_word (buf, 0);
+	ut32 w1 = sm4_word (buf, 1);
+	ut32 w2 = sm4_word (buf, 2);
+	ut32 w3 = sm4_word (buf, 3);
 	ut32 master_key[4] = { 0 };
-	master_key[3] = ptr[3] ^ (sm4_RK (ptr[2] ^ ptr[1] ^ ptr[0] ^ sm4_CK[3]));
-	master_key[2] = ptr[2] ^ (sm4_RK (ptr[1] ^ ptr[0] ^ master_key[3] ^ sm4_CK[2]));
-	master_key[1] = ptr[1] ^ (sm4_RK (ptr[0] ^ master_key[3] ^ master_key[2] ^ sm4_CK[1]));
-	master_key[0] = ptr[0] ^ (sm4_RK (master_key[3] ^ master_key[2] ^ master_key[1] ^ sm4_CK[0]));
+	master_key[3] = w3 ^ (sm4_RK (w2 ^ w1 ^ w0 ^ sm4_CK[3]));
+	master_key[2] = w2 ^ (sm4_RK (w1 ^ w0 ^ master_key[3] ^ sm4_CK[2]));
+	master_key[1] = w1 ^ (sm4_RK (w0 ^ master_key[3] ^ master_key[2] ^ sm4_CK[1]));
+	master_key[0] = w0 ^ (sm4_RK (master_key[3] ^ master_key[2] ^ master_key[1] ^ sm4_CK[0]));
 	R_LOG_INFO ("Master key found: %08x%08x%08x%08x @%#8"PFMT64x, FK[0] ^ master_key[0], FK[1] ^ master_key[1], FK[2] ^ master_key[2], FK[3] ^ master_key[3], addr);
 	return;
 }
