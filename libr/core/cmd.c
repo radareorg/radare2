dfbe2c40edd29b61c1a04ed560ef747878fcdea3
pancake <pancake@nopcode.org>
2026-06-07 19:25:24 +0200

    Fix 'join' command argument parsing ##shell

--- libr/core/cmd.c
+++ libr/core/cmd.c
@@ -1732,29 +1732,22 @@ static int cmd_join(void *data, const char *input) { // "join"
 		goto beach;
 	}
 	arg1 = r_str_trim_head_ro (arg1);
-	if (!arg1) {
-		goto beach;
+	if (!*arg1) {
+		goto out;
 	}
 	char *end = strchr (arg1, ' ');
 	if (!end) {
-		goto beach;
+		goto out;
 	}
 	*end = '\0';
-	const char *arg2 = end + 1;
-	if (!arg2) {
-		goto beach;
+	const char *arg2 = r_str_trim_head_ro (end + 1);
+	if (!*arg2) {
+		goto out;
 	}
-	arg2 = r_str_trim_head_ro (arg2);
 	switch (*input) {
 	case '?': // "join?"
 		goto beach;
 	default: // "join"
-		if (!arg1) {
-			arg1 = "";
-		}
-		if (!arg2) {
-			arg2 = "";
-		}
 		if (!r_fs_check (core->fs, arg1) && !r_fs_check (core->fs, arg2)) {
 			char *res = r_syscmd_join (arg1, arg2);
 			if (res) {
@@ -1764,6 +1757,7 @@ static int cmd_join(void *data, const char *input) { // "join"
 		}
 		break;
 	}
+out:
 	free (tmp);
 	return R_CMD_RC_SUCCESS;
 beach:
