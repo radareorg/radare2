Link-Start/radare2_radareorg

Use RVec in RFlag ##abi

libr/core/cbin.c
@@ -2544,13 +2544,13 @@ static char *unique_symflag_for_addr(RCore *core, const char *pfx, const char *l
 	if (!base) {
 		return NULL;
 	}
-	const RList *lst = r_flag_get_list (core->flags, vaddr);
+	const RVecFlagItemPtr *lst = r_flag_get_list (core->flags, vaddr);
 	if (lst) {
 		RFlagItem *match = NULL;
 		RFlagItem *firstsym = NULL;
-		RListIter *it;
+		RFlagItem **it;
 		RFlagItem *f_at;
-		r_list_foreach (lst, it, f_at) {
+		r_flag_item_vec_foreach (lst, it, f_at) {
 			if (!f_at || !f_at->name || !r_str_startswith (f_at->name, "sym.")) {
 				continue;
 			}
@@ -2638,11 +2638,11 @@ static void set_symbol_flag(RCore *core, RBinSymbol *symbol, const SymName *sn, 
 		return;
 	}
 	if (IS_MODE_RAD (mode) && r_str_startswith (fn, "sym.")) {
-		const RList *lst = r_flag_get_list (core->flags, addr);
+		const RVecFlagItemPtr *lst = r_flag_get_list (core->flags, addr);
 		if (lst) {
 			RFlagItem *fi;
-			RListIter *it;
-			r_list_foreach (lst, it, fi) {
+			RFlagItem **it;
+			r_flag_item_vec_foreach (lst, it, fi) {
 				if (fi && fi->name && r_str_startswith (fi->name, "sym.")) {
 					return;
 				}
@@ -3010,11 +3010,11 @@ static bool bin_symbols(RCore *core, PJ *pj, int mode, ut64 laddr, int va, ut64 
 				/* Ignore duplicate sym.* names if the target address already holds one. */
 				if (IS_MODE_RAD (mode) && r_str_startswith (fn, "sym.")) {
 					RFlagItem *fi_at = NULL;
-					const RList *lst = r_flag_get_list (core->flags, addr);
+					const RVecFlagItemPtr *lst = r_flag_get_list (core->flags, addr);
 					if (lst) {
-						RListIter *it;
+						RFlagItem **it;
 						RFlagItem *fi;
-						r_list_foreach (lst, it, fi) {
+						r_flag_item_vec_foreach (lst, it, fi) {
 							if (fi && fi->name && r_str_startswith (fi->name, "sym.")) {
 								fi_at = fi;
 								break;

libr/core/cmd_anal.inc.c
@@ -5691,8 +5691,7 @@ static void cmd_afsv(RCore *core, ut64 pcv, int mode) {
 				pj_o (pj);
 				ut64 v = arg->src;
 				pj_kn (pj, "num", v);
-				const RList *list = r_flag_get_list (core->flags, v);
-				RFlagItem *item = r_list_last (list);
+				RFlagItem *item = r_flag_item_vec_last (r_flag_get_list (core->flags, v));
 				if (item) {
 					pj_ks (pj, "name", item->name);
 				}
@@ -5760,12 +5759,9 @@ static void cmd_afsv(RCore *core, ut64 pcv, int mode) {
 				// TODO: show value (string, flag if any in that address)
 				pj_o (pj);
 				pj_kn (pj, "num", v);
-				const RList *list = r_flag_get_list (core->flags, v);
-				if (list) {
-					RFlagItem *item = r_list_last (list);
-					if (item) {
-						pj_ks (pj, "name", item->name);
-					}
+				RFlagItem *item = r_flag_item_vec_last (r_flag_get_list (core->flags, v));
+				if (item) {
+					pj_ks (pj, "name", item->name);
 				}
 				char *s = r_core_cmd_strf (core, "ps0 @ 0x%08"PFMT64x, v);
 				r_str_trim (s);
@@ -8977,12 +8973,12 @@ R_IPI void cmd_aei(RCore *core) {
 }
 
 R_IPI int core_type_by_addr(RCore *core, ut64 addr) {
-	RListIter *iter;
+	RFlagItem **iter;
 	RFlagItem *item;
 	bool has_flag = false;
 	int type = R_ANAL_REF_TYPE_DATA;
-	const RList *list = r_flag_get_list (core->flags, addr);
-	r_list_foreach (list, iter, item) {
+	const RVecFlagItemPtr *list = r_flag_get_list (core->flags, addr);
+	r_flag_item_vec_foreach (list, iter, item) {
 		if (strchr (item->name, '.')) {
 			has_flag = true;
 			if (r_str_startswith (item->name, "str")) {

libr/core/cmd_debug.inc.c
@@ -1500,7 +1500,6 @@ end:
 
 static bool step_until_flag(RCore *core, const char *instr) {
 	R_RETURN_VAL_IF_FAIL (core && instr && core->dbg, false);
-	RListIter *iter;
 	RFlagItem *f;
 
 	bool honorbps = r_config_get_b (core->config, "dbg.bpforuntil");
@@ -1520,8 +1519,9 @@ static bool step_until_flag(RCore *core, const char *instr) {
 				break;
 			}
 		}
-		const RList *list = r_flag_get_list (core->flags, pc);
-		r_list_foreach (list, iter, f) {
+		const RVecFlagItemPtr *list = r_flag_get_list (core->flags, pc);
+		RFlagItem **viter;
+		r_flag_item_vec_foreach (list, viter, f) {
 			if (R_STR_ISEMPTY (instr) || (f->realname && strstr (f->realname, instr))) {
 				r_cons_printf (core->cons, "[ 0x%08"PFMT64x" ] %s\n", f->addr, f->realname);
 				r_cons_break_pop (core->cons);

libr/core/cmd_print.inc.c
@@ -2828,15 +2828,15 @@ static void annotated_hexdump(RCore *core, const char *str, int len) {
 					r_cons_printf (core->cons, "/fcn.%s\n", fcn->name);
 				}
 			}
-			const RList *list = r_flag_get_list (core->flags, addr + j);
-			RListIter *iter;
+			const RVecFlagItemPtr *list = r_flag_get_list (core->flags, addr + j);
+			RFlagItem **iter;
 			RFlagItem *fi;
 			ut64 flagsize = 0;
 			ut64 flagaddr = 0;
 			bool found = false;
 			char *flagname = NULL;
 			ut64 at = addr + j;
-			if (r_list_empty (list)) {
+			if (!list || RVecFlagItemPtr_empty (list)) {
 				// get flag fnear and check for size
 				RFlagItem *fnear = r_flag_get_at (core->flags, at, true);
 				if (fnear) {
@@ -2858,7 +2858,7 @@ static void annotated_hexdump(RCore *core, const char *str, int len) {
 					}
 				}
 			} else {
-				r_list_foreach (list, iter, fi) {
+				r_flag_item_vec_foreach (list, iter, fi) {
 					flagsize = R_MAX (flagsize, fi->size);
 					const char *fi_color = r_flag_item_set_color (core->flags, fi, NULL);
 					if (fi_color) {

libr/core/cmd_search.inc.c
@@ -885,20 +885,20 @@ R_API RList *r_core_get_boundaries_prot(RCore *core, R_UNUSED int perm, const ch
 		perm = R_PERM_RWX;
 	}
 	if (!strcmp (mode, "flag")) {
-		const RList *ls = r_flag_get_list (core->flags, core->addr);
+		const RVecFlagItemPtr *ls = r_flag_get_list (core->flags, core->addr);
 		RFlagItem *fi;
-		RListIter *iter;
-		r_list_foreach (ls, iter, fi) {
+		RFlagItem **iter;
+		r_flag_item_vec_foreach (ls, iter, fi) {
 			if (fi->size > 1) {
 				append_bound (list, core->io, search_itv, fi->addr, fi->size, 7);
 			}
 		}
 	} else if (r_str_startswith (mode, "flag:")) {
 		const char *match = mode + 5;
-		const RList *ls = r_flag_get_list (core->flags, core->addr);
+		const RVecFlagItemPtr *ls = r_flag_get_list (core->flags, core->addr);
 		RFlagItem *fi;
-		RListIter *iter;
-		r_list_foreach (ls, iter, fi) {
+		RFlagItem **iter;
+		r_flag_item_vec_foreach (ls, iter, fi) {
 			if (fi->size > 1 && r_str_glob (fi->name, match)) {
 				append_bound (list, core->io, search_itv, fi->addr, fi->size, 7);
 			}

libr/core/core.c
@@ -1852,12 +1852,12 @@ R_API char *r_core_anal_hasrefs_to_depth(RCore *core, ut64 value, PJ *pj, int de
 	}
 	if (value != 0 && value != UT64_MAX) {
 		if (pj) {
-			RListIter *iter;
+			RFlagItem **iter;
 			RFlagItem *f;
-			const RList *flags = r_flag_get_list (core->flags, value);
-			if (flags && !r_list_empty (flags)) {
+			const RVecFlagItemPtr *flags = r_flag_get_list (core->flags, value);
+			if (flags && !RVecFlagItemPtr_empty (flags)) {
 				pj_ka (pj, "flags");
-				r_list_foreach (flags, iter, f) {
+				r_flag_item_vec_foreach (flags, iter, f) {
 					pj_s (pj, f->name);
 				}
 				pj_end (pj);

libr/include/r_flag.h
@@ -4,6 +4,7 @@
 #include <r_types.h>
 #include <r_util.h>
 #include <r_list.h>
+#include <r_vec.h>
 #include <r_skiplist.h>
 
 #ifdef __cplusplus
@@ -20,11 +21,36 @@ typedef struct r_flag_zone_item_t {
 	char *name;
 } RFlagZoneItem;
 
+static inline void r_flag_zone_item_fini(RFlagZoneItem *zi) {
+	if (zi) {
+		free (zi->name);
+	}
+}
+
+R_VEC_TYPE_WITH_FINI (RVecFlagZoneItem, RFlagZoneItem, r_flag_zone_item_fini);
+
 /* flag.c */
+
+typedef struct r_flag_item_t RFlagItem;
+
+R_VEC_TYPE (RVecFlagItemPtr, RFlagItem *);
+
+#define r_flag_item_vec_foreach(list, it, pos) \
+	if (list) \
+		for (it = R_VEC_START_ITER (list); it != R_VEC_END_ITER (list) && (pos = *it, 1); it++)
+
+static inline RFlagItem *r_flag_item_vec_last(const RVecFlagItemPtr *list) {
+	RFlagItem **item = list? RVecFlagItemPtr_last (list): NULL;
+	return item? *item: NULL;
+}
+
+static inline size_t r_flag_item_vec_length(const RVecFlagItemPtr *list) {
+	return list? RVecFlagItemPtr_length (list): 0;
+}
 
 typedef struct r_flags_at_addr_t {
 	ut64 addr;
-	RList *flags;   /* list of RFlagItem at addr */
+	RVecFlagItemPtr flags; /* RFlagItem at addr */
 } RFlagsAtOffset;
 
 typedef struct r_flag_item_meta_t {
@@ -32,12 +58,9 @@ typedef struct r_flag_item_meta_t {
 	char *color;    /* item color */
 	char *comment;  /* item comment */
 	char *alias;    /* used to define a flag based on a math expression (e.g. foo + 3) */
-#if 0
-	bool demangled; /* real name from demangling? */
-#endif
 } RFlagItemMeta;
 
-typedef struct r_flag_item_t {
+struct r_flag_item_t {
 	ut32 id;        /* unique identifier, maybe use ut64? */
 	char *name;     /* unique name, escaped to avoid issues with r2 shell */
 	char *realname; /* real demangled, display name, without any escaping */
@@ -46,7 +69,7 @@ typedef struct r_flag_item_t {
 	ut64 addr;      /* address of the flag */
 	ut64 size;      /* size of the flag item */
 	RSpace *space;  /* flag space this item belongs to */
-} RFlagItem;
+};
 
 typedef struct r_flag_t {
 	RSpaces spaces;   /* handle flag spaces */
@@ -58,7 +81,7 @@ typedef struct r_flag_t {
 	RSkipList *by_addr; /* flags sorted by addr, value=RFlagsAtOffset */
 	HtPP *ht_name; /* hashmap key=item name, value=RFlagItem */
 	HtUP *ht_meta; // hashtable for the flags metadata
-	RList *zones;
+	RVecFlagZoneItem zones;
 	ut64 mask;
 	RThreadLock *lock;
 	ut32 lastid;
@@ -72,7 +95,7 @@ typedef bool (*RFlagExistAt)(RFlag *f, const char *flag_prefix, ut16 fp_size, ut
 typedef RFlagItem* (*RFlagGet)(RFlag *f, const char *name);
 typedef RFlagItem* (*RFlagGetAtAddr) (RFlag *f, bool prionospace, ut64);
 typedef RFlagItem* (*RFlagGetAt)(RFlag *f, ut64 addr, bool closest);
-typedef const RList* (*RFlagGetList)(RFlag *f, ut64 addr);
+typedef const RVecFlagItemPtr* (*RFlagGetList)(RFlag *f, ut64 addr);
 typedef RFlagItem* (*RFlagSet)(RFlag *f, const char *name, ut64 addr, ut32 size);
 typedef bool (*RFlagUnset)(RFlag *f, RFlagItem *item);
 typedef bool (*RFlagUnsetName)(RFlag *f, const char *name);
@@ -118,8 +141,8 @@ R_API RFlagItem *r_flag_get_by_spaces(RFlag *f, bool prionospace, ut64 addr, ...
 R_API RFlagItem *r_flag_get_at(RFlag *f, ut64 addr, bool closest);
 R_API RFlagItem *r_flag_closest_in_space(RFlag *f, const char *space, ut64 addr, ut64 radius);
 R_API RFlagItem *r_flag_closest_with_prefix(RFlag *f, const char *pfx, ut64 addr, ut64 radius);
-R_API RList *r_flag_all_list(RFlag *f, bool by_space);
-R_API const RList* /*<RFlagItem*>*/ r_flag_get_list(RFlag *f, ut64 addr);
+R_API RVecFlagItemPtr *r_flag_all_list(RFlag *f, bool by_space);
+R_API const RVecFlagItemPtr* /*<RFlagItem*>*/ r_flag_get_list(RFlag *f, ut64 addr);
 R_API char *r_flag_get_liststr(RFlag *f, ut64 addr);
 R_API bool r_flag_unset(RFlag *f, RFlagItem *item);
 R_API bool r_flag_unset_name(RFlag *f, const char *name);

radareorg/radare2

libr/core/cbin.c
@@ -2564,13 +2564,13 @@ static char *unique_symflag_for_addr(RCore *core, const char *pfx, const char *l
 	if (!base) {
 		return NULL;
 	}
-	const RList *lst = r_flag_get_list (core->flags, vaddr);
+	const RVecFlagItemPtr *lst = r_flag_get_vec (core->flags, vaddr);
 	if (lst) {
 		RFlagItem *match = NULL;
 		RFlagItem *firstsym = NULL;
-		RListIter *it;
+		RFlagItem **it;
 		RFlagItem *f_at;
-		r_list_foreach (lst, it, f_at) {
+		r_flag_item_vec_foreach (lst, it, f_at) {
 			if (!f_at || !f_at->name || !r_str_startswith (f_at->name, "sym.")) {
 				continue;
 			}
@@ -2658,11 +2658,11 @@ static void set_symbol_flag(RCore *core, RBinSymbol *symbol, const SymName *sn, 
 		return;
 	}
 	if (IS_MODE_RAD (mode) && r_str_startswith (fn, "sym.")) {
-		const RList *lst = r_flag_get_list (core->flags, addr);
+		const RVecFlagItemPtr *lst = r_flag_get_vec (core->flags, addr);
 		if (lst) {
 			RFlagItem *fi;
-			RListIter *it;
-			r_list_foreach (lst, it, fi) {
+			RFlagItem **it;
+			r_flag_item_vec_foreach (lst, it, fi) {
 				if (fi && fi->name && r_str_startswith (fi->name, "sym.")) {
 					return;
 				}
@@ -3028,11 +3028,11 @@ static bool bin_symbols(RCore *core, PJ *pj, int mode, ut64 laddr, int va, ut64 
 				/* Ignore duplicate sym.* names if the target address already holds one. */
 				if (IS_MODE_RAD (mode) && r_str_startswith (fn, "sym.")) {
 					RFlagItem *fi_at = NULL;
-					const RList *lst = r_flag_get_list (core->flags, addr);
+					const RVecFlagItemPtr *lst = r_flag_get_vec (core->flags, addr);
 					if (lst) {
-						RListIter *it;
+						RFlagItem **it;
 						RFlagItem *fi;
-						r_list_foreach (lst, it, fi) {
+						r_flag_item_vec_foreach (lst, it, fi) {
 							if (fi && fi->name && r_str_startswith (fi->name, "sym.")) {
 								fi_at = fi;
 								break;

libr/core/cmd_anal.inc.c
@@ -5820,8 +5820,7 @@ static void cmd_afsv(RCore *core, ut64 pcv, int mode) {
 				pj_o (pj);
 				ut64 v = arg->src;
 				pj_kn (pj, "num", v);
-				const RList *list = r_flag_get_list (core->flags, v);
-				RFlagItem *item = r_list_last (list);
+				RFlagItem *item = r_flag_item_vec_last (r_flag_get_vec (core->flags, v));
 				if (item) {
 					pj_ks (pj, "name", item->name);
 				}
@@ -5889,12 +5888,9 @@ static void cmd_afsv(RCore *core, ut64 pcv, int mode) {
 				// TODO: show value (string, flag if any in that address)
 				pj_o (pj);
 				pj_kn (pj, "num", v);
-				const RList *list = r_flag_get_list (core->flags, v);
-				if (list) {
-					RFlagItem *item = r_list_last (list);
-					if (item) {
-						pj_ks (pj, "name", item->name);
-					}
+				RFlagItem *item = r_flag_item_vec_last (r_flag_get_vec (core->flags, v));
+				if (item) {
+					pj_ks (pj, "name", item->name);
 				}
 				char *s = r_core_cmd_strf (core, "ps0 @ 0x%08"PFMT64x, v);
 				r_str_trim (s);
@@ -9034,12 +9030,12 @@ R_IPI void cmd_aei(RCore *core) {
 }
 
 R_IPI int core_type_by_addr(RCore *core, ut64 addr) {
-	RListIter *iter;
+	RFlagItem **iter;
 	RFlagItem *item;
 	bool has_flag = false;
 	int type = R_ANAL_REF_TYPE_DATA;
-	const RList *list = r_flag_get_list (core->flags, addr);
-	r_list_foreach (list, iter, item) {
+	const RVecFlagItemPtr *list = r_flag_get_vec (core->flags, addr);
+	r_flag_item_vec_foreach (list, iter, item) {
 		if (strchr (item->name, '.')) {
 			has_flag = true;
 			if (r_str_startswith (item->name, "str")) {

libr/core/cmd_debug.inc.c
@@ -1499,7 +1499,6 @@ end:
 
 static bool step_until_flag(RCore *core, const char *instr) {
 	R_RETURN_VAL_IF_FAIL (core && instr && core->dbg, false);
-	RListIter *iter;
 	RFlagItem *f;
 
 	bool honorbps = r_config_get_b (core->config, "dbg.bpforuntil");
@@ -1519,8 +1518,9 @@ static bool step_until_flag(RCore *core, const char *instr) {
 				break;
 			}
 		}
-		const RList *list = r_flag_get_list (core->flags, pc);
-		r_list_foreach (list, iter, f) {
+		const RVecFlagItemPtr *list = r_flag_get_vec (core->flags, pc);
+		RFlagItem **viter;
+		r_flag_item_vec_foreach (list, viter, f) {
 			if (R_STR_ISEMPTY (instr) || (f->realname && strstr (f->realname, instr))) {
 				r_cons_printf (core->cons, "[ 0x%08"PFMT64x" ] %s\n", f->addr, f->realname);
 				r_cons_break_pop (core->cons);

libr/core/cmd_print.inc.c
@@ -2921,15 +2921,15 @@ static void annotated_hexdump(RCore *core, const char *str, int len) {
 					r_cons_printf (core->cons, "/fcn.%s\n", fcn->name);
 				}
 			}
-			const RList *list = r_flag_get_list (core->flags, addr + j);
-			RListIter *iter;
+			const RVecFlagItemPtr *list = r_flag_get_vec (core->flags, addr + j);
+			RFlagItem **iter;
 			RFlagItem *fi;
 			ut64 flagsize = 0;
 			ut64 flagaddr = 0;
 			bool found = false;
 			char *flagname = NULL;
 			ut64 at = addr + j;
-			if (r_list_empty (list)) {
+			if (!list || RVecFlagItemPtr_empty (list)) {
 				// get flag fnear and check for size
 				RFlagItem *fnear = r_flag_get_at (core->flags, at, true);
 				if (fnear) {
@@ -2951,7 +2951,7 @@ static void annotated_hexdump(RCore *core, const char *str, int len) {
 					}
 				}
 			} else {
-				r_list_foreach (list, iter, fi) {
+				r_flag_item_vec_foreach (list, iter, fi) {
 					flagsize = R_MAX (flagsize, fi->size);
 					const char *fi_color = r_flag_item_set_color (core->flags, fi, NULL);
 					if (fi_color) {

libr/core/cmd_search.inc.c
@@ -905,20 +905,20 @@ R_API RList *r_core_get_boundaries_prot(RCore *core, R_UNUSED int perm, const ch
 		perm = R_PERM_RWX;
 	}
 	if (!strcmp (mode, "flag")) {
-		const RList *ls = r_flag_get_list (core->flags, core->addr);
+		const RVecFlagItemPtr *ls = r_flag_get_vec (core->flags, core->addr);
 		RFlagItem *fi;
-		RListIter *iter;
-		r_list_foreach (ls, iter, fi) {
+		RFlagItem **iter;
+		r_flag_item_vec_foreach (ls, iter, fi) {
 			if (fi->size > 1) {
 				append_bound (list, core->io, search_itv, fi->addr, fi->size, 7);
 			}
 		}
 	} else if (r_str_startswith (mode, "flag:")) {
 		const char *match = mode + 5;
-		const RList *ls = r_flag_get_list (core->flags, core->addr);
+		const RVecFlagItemPtr *ls = r_flag_get_vec (core->flags, core->addr);
 		RFlagItem *fi;
-		RListIter *iter;
-		r_list_foreach (ls, iter, fi) {
+		RFlagItem **iter;
+		r_flag_item_vec_foreach (ls, iter, fi) {
 			if (fi->size > 1 && r_str_glob (fi->name, match)) {
 				append_bound (list, core->io, search_itv, fi->addr, fi->size, 7);
 			}

libr/core/core.c
@@ -1853,12 +1853,12 @@ R_API char *r_core_anal_hasrefs_to_depth(RCore *core, ut64 value, PJ *pj, int de
 	}
 	if (value != 0 && value != UT64_MAX) {
 		if (pj) {
-			RListIter *iter;
+			RFlagItem **iter;
 			RFlagItem *f;
-			const RList *flags = r_flag_get_list (core->flags, value);
-			if (flags && !r_list_empty (flags)) {
+			const RVecFlagItemPtr *flags = r_flag_get_vec (core->flags, value);
+			if (flags && !RVecFlagItemPtr_empty (flags)) {
 				pj_ka (pj, "flags");
-				r_list_foreach (flags, iter, f) {
+				r_flag_item_vec_foreach (flags, iter, f) {
 					pj_s (pj, f->name);
 				}
 				pj_end (pj);

libr/include/r_flag.h
@@ -3,7 +3,7 @@
 
 #include <r_types.h>
 #include <r_util.h>
-#include <r_list.h>
+#include <r_vec.h>
 #include <r_skiplist.h>
 
 #ifdef __cplusplus
@@ -20,11 +20,36 @@ typedef struct r_flag_zone_item_t {
 	char *name;
 } RFlagZoneItem;
 
+static inline void r_flag_zone_item_fini(RFlagZoneItem *zi) {
+	if (zi) {
+		free (zi->name);
+	}
+}
+
+R_VEC_TYPE_WITH_FINI (RVecFlagZoneItem, RFlagZoneItem, r_flag_zone_item_fini);
+
 /* flag.c */
+
+typedef struct r_flag_item_t RFlagItem;
+
+R_VEC_TYPE (RVecFlagItemPtr, RFlagItem *);
+
+#define r_flag_item_vec_foreach(list, it, pos) \
+	if (list) \
+		for (it = R_VEC_START_ITER (list); it != R_VEC_END_ITER (list) && (pos = *it, 1); it++)
+
+static inline RFlagItem *r_flag_item_vec_last(const RVecFlagItemPtr *list) {
+	RFlagItem **item = list? RVecFlagItemPtr_last (list): NULL;
+	return item? *item: NULL;
+}
+
+static inline size_t r_flag_item_vec_length(const RVecFlagItemPtr *list) {
+	return list? RVecFlagItemPtr_length (list): 0;
+}
 
 typedef struct r_flags_at_addr_t {
 	ut64 addr;
-	RList *flags;   /* list of RFlagItem at addr */
+	RVecFlagItemPtr flags; /* RFlagItem at addr */
 } RFlagsAtOffset;
 
 typedef struct r_flag_item_meta_t {
@@ -32,12 +57,9 @@ typedef struct r_flag_item_meta_t {
 	char *color;    /* item color */
 	char *comment;  /* item comment */
 	char *alias;    /* used to define a flag based on a math expression (e.g. foo + 3) */
-#if 0
-	bool demangled; /* real name from demangling? */
-#endif
 } RFlagItemMeta;
 
-typedef struct r_flag_item_t {
+struct r_flag_item_t {
 	ut32 id;        /* unique identifier, maybe use ut64? */
 	char *name;     /* unique name, escaped to avoid issues with r2 shell */
 	char *realname; /* real demangled, display name, without any escaping */
@@ -49,7 +71,7 @@ typedef struct r_flag_item_t {
 	ut64 addr;      /* address of the flag */
 	ut64 size;      /* size of the flag item */
 	RSpace *space;  /* flag space this item belongs to */
-} RFlagItem;
+};
 
 typedef struct r_flag_t {
 	RSpaces spaces;   /* handle flag spaces */
@@ -61,7 +83,7 @@ typedef struct r_flag_t {
 	RSkipList *by_addr; /* flags sorted by addr, value=RFlagsAtOffset */
 	HtPP *ht_name; /* hashmap key=item name, value=RFlagItem */
 	HtUP *ht_meta; // hashtable for the flags metadata
-	RList *zones;
+	RVecFlagZoneItem zones;
 	ut64 mask;
 	RThreadLock *lock;
 	ut32 lastid;
@@ -75,7 +97,7 @@ typedef bool (*RFlagExistAt)(RFlag *f, const char *flag_prefix, ut16 fp_size, ut
 typedef RFlagItem* (*RFlagGet)(RFlag *f, const char *name);
 typedef RFlagItem* (*RFlagGetAtAddr) (RFlag *f, bool prionospace, ut64);
 typedef RFlagItem* (*RFlagGetAt)(RFlag *f, ut64 addr, bool closest);
-typedef const RList* (*RFlagGetList)(RFlag *f, ut64 addr);
+typedef const RVecFlagItemPtr* (*RFlagGetVec)(RFlag *f, ut64 addr);
 typedef RFlagItem* (*RFlagSet)(RFlag *f, const char *name, ut64 addr, ut32 size);
 typedef bool (*RFlagUnset)(RFlag *f, RFlagItem *item);
 typedef bool (*RFlagUnsetName)(RFlag *f, const char *name);
@@ -92,7 +114,7 @@ typedef struct r_flag_bind_t {
 	RFlagExistAt exist_at;
 	RFlagGet get;
 	RFlagGetAt get_at;
-	RFlagGetList get_list;
+	RFlagGetVec get_vec;
 	RFlagSet set;
 	RFlagUnset unset;
 	RFlagUnsetName unset_name;
@@ -122,8 +144,8 @@ R_API RFlagItem *r_flag_get_by_spaces(RFlag *f, bool prionospace, ut64 addr, ...
 R_API RFlagItem *r_flag_get_at(RFlag *f, ut64 addr, bool closest);
 R_API RFlagItem *r_flag_closest_in_space(RFlag *f, const char *space, ut64 addr, ut64 radius);
 R_API RFlagItem *r_flag_closest_with_prefix(RFlag *f, const char *pfx, ut64 addr, ut64 radius);
-R_API RList *r_flag_all_list(RFlag *f, bool by_space);
-R_API const RList* /*<RFlagItem*>*/ r_flag_get_list(RFlag *f, ut64 addr);
+R_API RVecFlagItemPtr *r_flag_all_list(RFlag *f, bool by_space);
+R_API const RVecFlagItemPtr* /*<RFlagItem*>*/ r_flag_get_vec(RFlag *f, ut64 addr);
 R_API char *r_flag_get_liststr(RFlag *f, ut64 addr);
 R_API bool r_flag_unset(RFlag *f, RFlagItem *item);
 R_API bool r_flag_unset_name(RFlag *f, const char *name);