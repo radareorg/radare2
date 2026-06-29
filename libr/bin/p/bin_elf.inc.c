ce76290545df3b2a090908381b2880ea06d98ec7
ada-l0velace <9060550+ada-l0velace@users.noreply.github.com>
2026-06-29 16:40:48 +0100

    Fix AArch64 ELF relocation patching ##bin

--- libr/bin/p/bin_elf.inc.c
+++ libr/bin/p/bin_elf.inc.c
@@ -1054,6 +1054,18 @@ static void _patch_reloc(ELFOBJ *bo, ut16 e_machine, RIOBind *iob, RBinElfReloc 
 			iob->overlay_write_at (iob->io, rel->rva, buf, 4);
 			break;
 		}
+		case R_AARCH64_RELATIVE:
+			V = B + A;
+			r_write_ble64 (buf, V, bo->endian);
+			iob->overlay_write_at (iob->io, rel->rva, buf, 8);
+			break;
+		case R_AARCH64_GLOB_DAT:
+		case R_AARCH64_JUMP_SLOT:
+		case R_AARCH64_ABS64:
+			V = S + A;
+			r_write_ble64 (buf, V, bo->endian);
+			iob->overlay_write_at (iob->io, rel->rva, buf, 8);
+			break;
 		default:
 			V = S + A;
 			iob->read_at (iob->io, rel->rva, buf, 8);
