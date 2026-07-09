78176aa461b689cac7ac453d8bf044acc98270ec
pancake <pancake@nopcode.org>
2026-07-09 18:25:32 +0200

    Add r_anal_cc_isclobber API ##analysis

--- libr/include/r_anal.h
+++ libr/include/r_anal.h
@@ -1454,6 +1454,7 @@ R_API void r_anal_cc_set_error(RAnal *anal, const char *convention, const char *
 R_API int r_anal_cc_max_arg(RAnal *anal, const char *cc);
 R_API const char *r_anal_cc_ret(RAnal *anal, const char *convention, int n);
 R_API bool r_anal_cc_argclob(RAnal *anal, const char *caller_cc, int n, const char *callee_cc);
+R_API bool r_anal_cc_isclobber(RAnal *anal, const char *cc, const char *reg);
 R_API const char *r_anal_cc_default(RAnal *anal);
 R_API const char *r_anal_function_cc(RAnalFunction *fcn);
 R_API const char *r_anal_call_convention(RAnal *anal, RAnalOp *op);
