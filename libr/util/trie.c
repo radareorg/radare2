ad1ca1a05c86d2234f6bb708dde8ba6760c4aba5
pancake <pancake@nopcode.org>
2026-07-20 18:34:31 +0200

    Initial import of the Trie data structure ##util

---
+++ libr/util/trie.c
@@ -0,0 +1,342 @@
+/* radare - LGPL - Copyright 2026 - pancake */
+
+#include <r_util/r_trie.h>
+#include <r_util.h>
+
+typedef struct r_trie_node_t {
+	ut8 *segment;
+	size_t segment_len;
+	void *value;
+	struct r_trie_node_t **children;
+	size_t children_count;
+	size_t children_capacity;
+} RTrieNode;
+
+struct r_trie_t {
+	RTrieNode *root;
+	RTrieFree free_value;
+	size_t size;
+};
+
+static RTrieNode *trie_node_new(RStrs segment, void *value) {
+	RTrieNode *node = R_NEW0 (RTrieNode);
+	node->segment_len = r_strs_len (segment);
+	if (node->segment_len) {
+		node->segment = malloc (node->segment_len);
+		if (!node->segment) {
+			free (node);
+			return NULL;
+		}
+		memcpy (node->segment, segment.a, node->segment_len);
+	}
+	node->value = value;
+	return node;
+}
+
+static void trie_node_free_shallow(RTrieNode *node) {
+	free (node->segment);
+	free (node->children);
+	free (node);
+}
+
+static void trie_node_free(RTrieNode *node, RTrieFree free_value) {
+	size_t i;
+	for (i = 0; i < node->children_count; i++) {
+		trie_node_free (node->children[i], free_value);
+	}
+	if (node->value && free_value) {
+		free_value (node->value);
+	}
+	trie_node_free_shallow (node);
+}
+
+static bool trie_node_find_child(const RTrieNode *node, ut8 first, size_t *index) {
+	size_t lo = 0;
+	size_t hi = node->children_count;
+	while (lo < hi) {
+		size_t mid = lo + ((hi - lo) / 2);
+		ut8 current = node->children[mid]->segment[0];
+		if (current < first) {
+			lo = mid + 1;
+		} else {
+			hi = mid;
+		}
+	}
+	*index = lo;
+	return lo < node->children_count && node->children[lo]->segment[0] == first;
+}
+
+static RTrieNode *trie_node_find_match(const RTrieNode *node, const char *key, size_t key_len, size_t *index) {
+	if (!trie_node_find_child (node, (ut8)key[0], index)) {
+		return NULL;
+	}
+	RTrieNode *child = node->children[*index];
+	if (child->segment_len > key_len || memcmp (child->segment, key, child->segment_len)) {
+		return NULL;
+	}
+	return child;
+}
+
+static bool trie_node_insert_child(RTrieNode *node, size_t index, RTrieNode *child) {
+	if (node->children_count == node->children_capacity) {
+		size_t capacity = 4;
+		size_t alloc_size;
+		if ((node->children_capacity && r_mul_overflow (node->children_capacity, (size_t)2, &capacity)) ||
+			r_mul_overflow (capacity, sizeof (RTrieNode *), &alloc_size)) {
+			return false;
+		}
+		RTrieNode **children = realloc (node->children, alloc_size);
+		if (!children) {
+			return false;
+		}
+		node->children = children;
+		node->children_capacity = capacity;
+	}
+	memmove (node->children + index + 1, node->children + index,
+		(node->children_count - index) * sizeof (RTrieNode *));
+	node->children[index] = child;
+	node->children_count++;
+	return true;
+}
+
+static size_t trie_common_prefix(const RTrieNode *node, RStrs key) {
+	size_t common = 0;
+	size_t key_len = r_strs_len (key);
+	size_t limit = R_MIN (node->segment_len, key_len);
+	while (common < limit && node->segment[common] == (ut8)key.a[common]) {
+		common++;
+	}
+	return common;
+}
+
+R_API RTrie *r_trie_new(RTrieFree free_value) {
+	RTrie *trie = R_NEW0 (RTrie);
+	trie->root = trie_node_new (r_strs_from_len (NULL, 0), NULL);
+	trie->free_value = free_value;
+	return trie;
+}
+
+R_API void r_trie_free(RTrie *trie) {
+	if (trie) {
+		trie_node_free (trie->root, trie->free_value);
+		free (trie);
+	}
+}
+
+R_API size_t r_trie_size(const RTrie *trie) {
+	R_RETURN_VAL_IF_FAIL (trie, 0);
+	return trie->size;
+}
+
+R_API bool r_trie_insert(RTrie *trie, RStrs key, void *value) {
+	R_RETURN_VAL_IF_FAIL (trie && key.a && key.b >= key.a && value, false);
+	RTrieNode *node = trie->root;
+	size_t key_len = r_strs_len (key);
+	size_t offset = 0;
+	while (offset < key_len) {
+		size_t index;
+		if (!trie_node_find_child (node, (ut8)key.a[offset], &index)) {
+			RTrieNode *leaf = trie_node_new (r_strs_sub (key, offset, key_len), value);
+			if (!leaf || !trie_node_insert_child (node, index, leaf)) {
+				if (leaf) {
+					trie_node_free_shallow (leaf);
+				}
+				return false;
+			}
+			trie->size++;
+			return true;
+		}
+		RTrieNode *child = node->children[index];
+		RStrs remaining = r_strs_sub (key, offset, key_len);
+		size_t common = trie_common_prefix (child, remaining);
+		if (common == child->segment_len) {
+			offset += common;
+			node = child;
+			continue;
+		}
+
+		RTrieNode *middle = trie_node_new (r_strs_from_len ((const char *)child->segment, common), NULL);
+		size_t child_suffix_len = child->segment_len - common;
+		ut8 *child_suffix = malloc (child_suffix_len);
+		if (!middle || !child_suffix) {
+			if (middle) {
+				trie_node_free_shallow (middle);
+			}
+			free (child_suffix);
+			return false;
+		}
+		memcpy (child_suffix, child->segment + common, child_suffix_len);
+		size_t key_suffix_offset = offset + common;
+		size_t key_suffix_len = key_len - key_suffix_offset;
+		RTrieNode *leaf = NULL;
+		if (key_suffix_len) {
+			leaf = trie_node_new (r_strs_sub (key, key_suffix_offset, key_len), value);
+			if (!leaf) {
+				free (child_suffix);
+				trie_node_free_shallow (middle);
+				return false;
+			}
+		} else {
+			middle->value = value;
+		}
+		middle->children_capacity = leaf? 2: 1;
+		middle->children = malloc (middle->children_capacity * sizeof (RTrieNode *));
+		if (!middle->children) {
+			if (leaf) {
+				trie_node_free_shallow (leaf);
+			}
+			free (child_suffix);
+			trie_node_free_shallow (middle);
+			return false;
+		}
+		if (leaf && leaf->segment[0] < child_suffix[0]) {
+			middle->children[0] = leaf;
+			middle->children[1] = child;
+		} else {
+			middle->children[0] = child;
+			if (leaf) {
+				middle->children[1] = leaf;
+			}
+		}
+		middle->children_count = middle->children_capacity;
+		free (child->segment);
+		child->segment = child_suffix;
+		child->segment_len = child_suffix_len;
+		node->children[index] = middle;
+		trie->size++;
+		return true;
+	}
+	if (node->value) {
+		if (node->value != value && trie->free_value) {
+			trie->free_value (node->value);
+		}
+	} else {
+		trie->size++;
+	}
+	node->value = value;
+	return true;
+}
+
+static RTrieNode *trie_find_node(const RTrie *trie, RStrs key) {
+	RTrieNode *node = trie->root;
+	size_t key_len = r_strs_len (key);
+	size_t offset = 0;
+	while (offset < key_len) {
+		size_t index;
+		RTrieNode *child = trie_node_find_match (node, key.a + offset, key_len - offset, &index);
+		if (!child) {
+			return NULL;
+		}
+		offset += child->segment_len;
+		node = child;
+	}
+	return node;
+}
+
+R_API void *r_trie_find(const RTrie *trie, RStrs key) {
+	R_RETURN_VAL_IF_FAIL (trie && key.a && key.b >= key.a, NULL);
+	RTrieNode *node = trie_find_node (trie, key);
+	return node? node->value: NULL;
+}
+
+R_API void *r_trie_find_longest_prefix(const RTrie *trie, RStrs input, R_OUT size_t *matched_len) {
+	R_RETURN_VAL_IF_FAIL (trie && input.a && input.b >= input.a && matched_len, NULL);
+	RTrieNode *node = trie->root;
+	void *best = node->value;
+	size_t input_len = r_strs_len (input);
+	size_t offset = 0;
+	*matched_len = 0;
+	while (offset < input_len) {
+		size_t index;
+		RTrieNode *child = trie_node_find_match (node, input.a + offset, input_len - offset, &index);
+		if (!child) {
+			break;
+		}
+		offset += child->segment_len;
+		node = child;
+		if (node->value) {
+			best = node->value;
+			*matched_len = offset;
+		}
+	}
+	return best;
+}
+
+static void trie_node_remove_child(RTrieNode *node, size_t index) {
+	memmove (node->children + index, node->children + index + 1,
+		(node->children_count - index - 1) * sizeof (RTrieNode *));
+	node->children_count--;
+}
+
+static void trie_node_compact_child(RTrieNode *node, size_t index) {
+	RTrieNode *child = node->children[index];
+	if (child->value) {
+		return;
+	}
+	if (child->children_count == 0) {
+		trie_node_remove_child (node, index);
+		trie_node_free_shallow (child);
+		return;
+	}
+	if (child->children_count > 1) {
+		return;
+	}
+	RTrieNode *grandchild = child->children[0];
+	size_t merged_len;
+	if (r_add_overflow (child->segment_len, grandchild->segment_len, &merged_len)) {
+		return;
+	}
+	ut8 *segment = realloc (child->segment, merged_len);
+	if (!segment) {
+		return;
+	}
+	memcpy (segment + child->segment_len, grandchild->segment, grandchild->segment_len);
+	child->segment = segment;
+	child->segment_len = merged_len;
+	child->value = grandchild->value;
+	free (child->children);
+	child->children = grandchild->children;
+	child->children_count = grandchild->children_count;
+	child->children_capacity = grandchild->children_capacity;
+	free (grandchild->segment);
+	free (grandchild);
+}
+
+static void *trie_node_take(RTrie *trie, RTrieNode *node, RStrs key, size_t offset) {
+	size_t key_len = r_strs_len (key);
+	if (offset == key_len) {
+		void *value = node->value;
+		if (value) {
+			node->value = NULL;
+			trie->size--;
+		}
+		return value;
+	}
+	size_t index;
+	RTrieNode *child = trie_node_find_match (node, key.a + offset, key_len - offset, &index);
+	if (!child) {
+		return NULL;
+	}
+	void *value = trie_node_take (trie, child, key, offset + child->segment_len);
+	if (value) {
+		trie_node_compact_child (node, index);
+	}
+	return value;
+}
+
+R_API void *r_trie_take(RTrie *trie, RStrs key) {
+	R_RETURN_VAL_IF_FAIL (trie && key.a && key.b >= key.a, NULL);
+	return trie_node_take (trie, trie->root, key, 0);
+}
+
+R_API bool r_trie_delete(RTrie *trie, RStrs key) {
+	R_RETURN_VAL_IF_FAIL (trie, false);
+	void *value = r_trie_take (trie, key);
+	if (!value) {
+		return false;
+	}
+	if (trie->free_value) {
+		trie->free_value (value);
+	}
+	return true;
+}
---
+++ test/unit/test_trie.c
@@ -0,0 +1,136 @@
+#include <r_util.h>
+#include "minunit.h"
+
+static int freed_values;
+
+static void count_free(void *value) {
+	freed_values++;
+	free (value);
+}
+
+static bool insert_string(RTrie *trie, const char *key, const char *value) {
+	char *copy = strdup (value);
+	if (!copy) {
+		return false;
+	}
+	if (!r_trie_insert (trie, r_strs_from (key), copy)) {
+		free (copy);
+		return false;
+	}
+	return true;
+}
+
+static bool test_r_trie_find(void) {
+	RTrie *trie = r_trie_new (free);
+	mu_assert_true (insert_string (trie, "a", "a"), "insert a");
+	mu_assert_true (insert_string (trie, "af", "af"), "insert af");
+	mu_assert_true (insert_string (trie, "afl", "afl"), "insert afl");
+	mu_assert_true (insert_string (trie, "agn", "agn"), "insert agn");
+	mu_assert_true (insert_string (trie, "pdj", "pdj"), "insert pdj");
+	mu_assert_eq (r_trie_size (trie), 5, "trie size");
+	mu_assert_streq (r_trie_find (trie, r_strs_from ("afl")), "afl", "find afl");
+	mu_assert_streq (r_trie_find (trie, r_strs_from ("agn")), "agn", "find agn");
+	mu_assert_null (r_trie_find (trie, r_strs_from ("afl?")), "exact lookup rejects suffix");
+	mu_assert_null (r_trie_find (trie, r_strs_from ("pd")), "exact lookup rejects prefix");
+	r_trie_free (trie);
+	mu_end;
+}
+
+static bool test_r_trie_longest_prefix(void) {
+	RTrie *trie = r_trie_new (free);
+	insert_string (trie, "a", "a");
+	insert_string (trie, "af", "af");
+	insert_string (trie, "afl", "afl");
+	insert_string (trie, "agn", "agn");
+	insert_string (trie, "pdj", "pdj");
+	size_t matched = UT64_MAX;
+	mu_assert_streq (r_trie_find_longest_prefix (trie, r_strs_from ("afl?"), &matched), "afl", "longest afl prefix");
+	mu_assert_eq (matched, 3, "afl prefix length");
+	mu_assert_streq (r_trie_find_longest_prefix (trie, r_strs_from ("afx"), &matched), "af", "longest af prefix");
+	mu_assert_eq (matched, 2, "af prefix length");
+	mu_assert_null (r_trie_find_longest_prefix (trie, r_strs_from ("px"), &matched), "missing prefix");
+	mu_assert_eq (matched, 0, "missing prefix length");
+	r_trie_free (trie);
+	mu_end;
+}
+
+static bool test_r_trie_split_order(void) {
+	RTrie *trie = r_trie_new (free);
+	insert_string (trie, "foobar", "foobar");
+	insert_string (trie, "foo", "foo");
+	insert_string (trie, "food", "food");
+	insert_string (trie, "f", "f");
+	insert_string (trie, "bar", "bar");
+	mu_assert_streq (r_trie_find (trie, r_strs_from ("foobar")), "foobar", "find split descendant");
+	mu_assert_streq (r_trie_find (trie, r_strs_from ("foo")), "foo", "find split parent");
+	mu_assert_streq (r_trie_find (trie, r_strs_from ("food")), "food", "find split sibling");
+	mu_assert_streq (r_trie_find (trie, r_strs_from ("f")), "f", "find split root");
+	mu_assert_streq (r_trie_find (trie, r_strs_from ("bar")), "bar", "find sorted root sibling");
+	r_trie_free (trie);
+	mu_end;
+}
+
+static bool test_r_trie_replace_take_delete(void) {
+	freed_values = 0;
+	RTrie *trie = r_trie_new (count_free);
+	insert_string (trie, "foo", "old");
+	insert_string (trie, "foobar", "foobar");
+	insert_string (trie, "foobaz", "foobaz");
+	insert_string (trie, "foo", "new");
+	mu_assert_eq (freed_values, 1, "replacement frees old value");
+	mu_assert_eq (r_trie_size (trie), 3, "replacement keeps size");
+	char *taken = r_trie_take (trie, r_strs_from ("foo"));
+	mu_assert_streq (taken, "new", "take returns value");
+	free (taken);
+	mu_assert_eq (r_trie_size (trie), 2, "take decrements size");
+	mu_assert_null (r_trie_find (trie, r_strs_from ("foo")), "taken key is absent");
+	mu_assert_true (r_trie_delete (trie, r_strs_from ("foobar")), "delete existing key");
+	mu_assert_false (r_trie_delete (trie, r_strs_from ("foobar")), "delete missing key");
+	mu_assert_streq (r_trie_find (trie, r_strs_from ("foobaz")), "foobaz", "sibling survives compaction");
+	r_trie_free (trie);
+	mu_assert_eq (freed_values, 3, "delete and trie free release remaining values");
+	mu_end;
+}
+
+static bool test_r_trie_empty_and_binary_keys(void) {
+	RTrie *trie = r_trie_new (free);
+	const char binary_key[] = { 'a', 0, 'b' };
+	mu_assert_true (insert_string (trie, "", "root"), "insert empty key");
+	char *binary_value = strdup ("binary");
+	mu_assert_true (r_trie_insert (trie, r_strs_from_len (binary_key, sizeof (binary_key)), binary_value), "insert binary key");
+	mu_assert_streq (r_trie_find (trie, r_strs_from_len (binary_key, sizeof (binary_key))), "binary", "find binary key");
+	size_t matched = UT64_MAX;
+	mu_assert_streq (r_trie_find_longest_prefix (trie, r_strs_from ("none"), &matched), "root", "empty key is fallback");
+	mu_assert_eq (matched, 0, "empty key prefix length");
+	r_trie_free (trie);
+	mu_end;
+}
+
+static bool test_r_trie_compact_chain(void) {
+	RTrie *trie = r_trie_new (free);
+	insert_string (trie, "a", "a");
+	insert_string (trie, "ab", "ab");
+	insert_string (trie, "abc", "abc");
+	mu_assert_true (r_trie_delete (trie, r_strs_from ("a")), "delete chain root");
+	mu_assert_streq (r_trie_find (trie, r_strs_from ("ab")), "ab", "find after first chain compaction");
+	mu_assert_true (r_trie_delete (trie, r_strs_from ("ab")), "delete compacted parent");
+	mu_assert_streq (r_trie_find (trie, r_strs_from ("abc")), "abc", "find after second chain compaction");
+	mu_assert_true (r_trie_delete (trie, r_strs_from ("abc")), "delete final chain value");
+	mu_assert_eq (r_trie_size (trie), 0, "empty after chain deletion");
+	r_trie_free (trie);
+	mu_end;
+}
+
+static int all_tests(void) {
+	mu_run_test (test_r_trie_find);
+	mu_run_test (test_r_trie_longest_prefix);
+	mu_run_test (test_r_trie_split_order);
+	mu_run_test (test_r_trie_replace_take_delete);
+	mu_run_test (test_r_trie_empty_and_binary_keys);
+	mu_run_test (test_r_trie_compact_chain);
+	return tests_passed != tests_run;
+}
+
+int main(int argc, char **argv) {
+	return all_tests ();
+}
