18731b888cd1fb3c6a8bf1370e20954cbf539ba4
phix33 <122955334+phix33@user.noreply.github.com>
2026-08-03 20:41:31 +1000

    Expose type signedness and variadic checks in the r_type api ##types

--- libr/include/r_util/r_type.h
+++ libr/include/r_util/r_type.h
@@ -39,6 +39,7 @@ R_API int r_type_set_link(Sdb *TDB, const char *val, ut64 addr);
 R_API int r_type_unlink(Sdb *TDB, ut64 addr);
 R_API int r_type_link_offset(Sdb *TDB, const char *val, ut64 addr);
 R_API char *r_type_format(Sdb *TDB, const char *t);
+R_API bool r_type_is_signed(Sdb * R_NONNULL TDB, const char * R_NONNULL type);
 
 // Function prototypes api
 R_API int r_type_func_exist(Sdb *TDB, const char *func_name);
@@ -49,6 +50,7 @@ R_API R_OWNED char *r_type_func_args_type(Sdb *TDB, const char * R_NONNULL func_
 R_API const char *r_type_func_args_name(Sdb *TDB, const char * R_NONNULL func_name, int i);
 R_API R_OWNED char *r_type_func_guess(Sdb *TDB, const char * R_NONNULL func_name);
 R_API R_OWNED char *r_type_func_name(Sdb *types, const char *fname);
+R_API bool r_type_func_is_variadic(Sdb *TDB, const char * R_NONNULL func_name);
 
 #ifdef __cplusplus
 }
