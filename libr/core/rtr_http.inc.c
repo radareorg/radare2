c4af7187f2b6391a70a5e43835492aad40ce070a
pancake <pancake@nopcode.org>
2026-08-06 13:45:34 +0200

    Prevent stale WebUI assets ##http

--- libr/core/rtr_http.inc.c
+++ libr/core/rtr_http.inc.c
@@ -530,7 +530,8 @@ static HttpRunResult r_core_rtr_http_run(RCore *core, int launch, int browse, co
 					char *f = r_file_slurp (path, &sz);
 					if (f) {
 						char *ct = guess_filetype (path);
-						char *hdr = r_str_newf ("%s%s", ct, headers);
+						// no-cache avoids serving stale webuis from the browser cache after an upgrade
+						char *hdr = r_str_newf ("%sCache-Control: no-cache\n%s", ct, headers);
 						r_socket_http_response (rs, 200, f, (int)sz, hdr);
 						free (hdr);
 						free (f);
