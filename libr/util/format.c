cc625a18482cd986643b663dee1793f8f42bc60f
pancake <pancake@nopcode.org>
2026-08-06 19:46:54 +0200

    Fix codeql scan bug 454. wrong parameter for format

--- libr/util/format.c
+++ libr/util/format.c
@@ -241,7 +241,8 @@ static void r_print_format_byte(RPrintFormat *pf, const char *setval, ut64 seeki
 	int elem;
 	PF_EXTRACT_ELEM (elem, size);
 	if (MUSTSET) {
-		r_print_printf (p, "'0x%08" PFMT64x "'w %s\n", setval, seeki + ((elem >= 0)? elem: 0));
+		ut64 addr = seeki + ((elem >= 0)? elem: 0);
+		r_print_printf (p, "'0x%08" PFMT64x "'w %s\n", addr, setval);
 	} else if (MUSTSEEJSON) {
 		if (size == -1) {
 			pj_kn (pf->pj, "value", buf[i]);
