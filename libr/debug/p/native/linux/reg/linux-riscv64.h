9ee5371bab284dc65a5878745ab6b1ed82744617
pancake <pancake@nopcode.org>
2026-08-08 19:08:08 +0200

    Fix Linux RV64 native debugger register profile ##debug

--- libr/debug/p/native/linux/reg/linux-riscv64.h
+++ libr/debug/p/native/linux/reg/linux-riscv64.h
@@ -1,7 +1,7 @@
 return strdup (
 	"=PC	pc\n"
 	"=SP	sp\n"
-	"=BP	bp\n"
+	"=BP	s0\n"
 	"=R0	a0\n"
 	"=R1	a1\n"
 	"=A0	a0\n"
@@ -14,7 +14,7 @@ return strdup (
 	"gpr	sp	.64	16	0\n" // SP stack pointer
 	"gpr	gp	.64	24	0\n" // GP global pointer
 	"gpr	tp	.64	32	0\n" // TP thread pointer
-	"gpr	t0	.64	40	0\n" // FP frame pointer -- BP
+	"gpr	t0	.64	40	0\n"
 	"gpr	t1	.64	48	0\n"
 	"gpr	t2	.64	56	0\n"
 	"gpr	s0	.64	64	0\n"
@@ -25,22 +25,22 @@ return strdup (
 	"gpr	a3	.64	104	0\n"
 	"gpr	a4	.64	112	0\n"
 	"gpr	a5	.64	120	0\n"
-	/* saved */
 	"gpr	a6	.64	128	0\n"
 	"gpr	a7	.64	136	0\n"
+	/* saved */
 	"gpr	s2	.64	144	0\n"
 	"gpr	s3	.64	152	0\n"
 	"gpr	s4	.64	160	0\n"
 	"gpr	s5	.64	168	0\n"
 	"gpr	s6	.64	176	0\n"
 	"gpr	s7	.64	184	0\n"
-	/* tmp */
 	"gpr	s8	.64	192	0\n"
-	"gpr	s10	.64	200	0\n"
-	/* special */
-	"gpr	s11	.64	208	0\n"
-	"gpr	t3	.64	216	0\n"
-	"gpr	t4	.64	224	0\n"
-	"gpr	t5	.64	232	0\n"
-	"gpr	t6	.64	240	0\n"
+	"gpr	s9	.64	200	0\n"
+	"gpr	s10	.64	208	0\n"
+	"gpr	s11	.64	216	0\n"
+	/* temporary */
+	"gpr	t3	.64	224	0\n"
+	"gpr	t4	.64	232	0\n"
+	"gpr	t5	.64	240	0\n"
+	"gpr	t6	.64	248	0\n"
 	);
