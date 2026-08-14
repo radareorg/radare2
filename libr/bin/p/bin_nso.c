dc58414dda8e66c4e226da0fae40a811b780c967
AGhebrea <alex.ghebrea4@gmail.com>
2026-08-14 15:35:23 +0300

    apply review

--- libr/bin/p/bin_nso.c
+++ libr/bin/p/bin_nso.c
@@ -35,12 +35,11 @@ static uint32_t decompress(const ut8 *cbuf, ut8 *obuf, int32_t csize, int32_t us
 	if (csize < 0 || usize < 0 || !cbuf || !obuf) {
 		return -1;
 	}
-	int consumed;
-	size_t osize;
-	ut8 *out = r_inflate_lz4 (cbuf, (int)csize, &consumed, (int *) (&osize));
+	int consumed, osize;
+	ut8 *out = r_inflate_lz4 (cbuf, (int)csize, &consumed, &osize);
 	if (out) {
 		// osize should be the same as usize
-		memcpy (obuf, out, R_MIN (usize, osize));
+		memcpy (obuf, out, R_MIN ((size_t)usize, osize));
 		free (out);
 		return usize;
 	}
