8c944540fae63c21313e6e45cb1b823c1eed2f22
Priyanshu Kumar <priyanshukumarpu@gmail.com>
2026-08-15 17:48:05 +0530

    Fix xref cleanup ordering and purge ##analysis
    Free the xref manager before destroying the analysis lock to avoid using a destroyed mutex during teardown. Reinitialize the manager during purge so references from the previous analysis cannot survive into the next one.

--- test/unit/test_anal_xrefs.c
+++ test/unit/test_anal_xrefs.c
@@ -18,8 +18,26 @@ bool test_r_anal_xrefs_count(void) {
 	mu_end;
 }
 
+bool test_r_anal_purge_clears_xrefs(void) {
+	RAnal *anal = r_anal_new ();
+
+	mu_assert_true (r_anal_xrefs_set (anal, 0x1000, 0x2000, R_ANAL_REF_TYPE_CODE),
+		"add xref before purge");
+	mu_assert_eq (r_anal_xrefs_count (anal), 1, "xref exists before purge");
+
+	r_anal_purge (anal);
+	mu_assert_eq (r_anal_xrefs_count (anal), 0, "purge clears xrefs");
+	mu_assert_true (r_anal_xrefs_set (anal, 0x3000, 0x4000, R_ANAL_REF_TYPE_CALL),
+		"xref manager remains usable after purge");
+	mu_assert_eq (r_anal_xrefs_count (anal), 1, "xref added after purge");
+
+	r_anal_free (anal);
+	mu_end;
+}
+
 int all_tests(void) {
 	mu_run_test (test_r_anal_xrefs_count);
+	mu_run_test (test_r_anal_purge_clears_xrefs);
 	return tests_passed != tests_run;
 }
 
