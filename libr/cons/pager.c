41f3b43bdb767817eae16f9cbfb227ab8cf07a32
pancake <pancake@nopcode.org>
2026-08-20 12:08:40 +0200

    Prevent internal pager from scrolling its first page ##cons

--- libr/cons/pager.c
+++ libr/cons/pager.c
@@ -70,8 +70,9 @@ R_IPI void pager_printpage(RCons *cons, const char *line, int *index, RList **ml
 		r_cons_reset_colors (cons);
 		// Calculate how many terminal lines this will take
 		int len = r_str_ansi_len (s);
-		actual_lines += (terminal_width > 0) ? (len / terminal_width) + 1 : 1;
-		if (i + 1 == to) {
+		int line_count = (terminal_width > 0) ? (len / terminal_width) + 1 : 1;
+		actual_lines += line_count;
+		if (i + 1 == to || actual_lines >= h) {
 			r_cons_print (cons, s);
 		} else {
 			r_cons_println (cons, s);
