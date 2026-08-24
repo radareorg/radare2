e95bb939144f9e46bfcfef38086cc3a95eed9005
ayush-codes6439 <ayyuussh08@gmail.com>
2026-08-24 22:08:32 +0530

    cons/stiv: refactor to return string directly

--- libr/cons/stiv.c
+++ libr/cons/stiv.c
@@ -2,10 +2,4 @@
 
 #include <r_cons.h>
 
-static int stiv_printf_cb(const char *fmt, ...) {
-	va_list ap;
-	va_start (ap, fmt);
-	r_cons_printf_list (NULL, fmt, ap);
-	va_end (ap);
-	return 0;
-}
+ 
