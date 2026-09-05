2551dc9794b4f391b718634436390a8f26093196
Mustafa Senoglu <mmustafasenoglu0@gmail.com>
2026-09-05 13:23:58 +0300

    Re-add ESIL ops used by arch plugins and tests
    The initial removal was too aggressive. These ops are actively used:
    
    - [3], =[3], =[*]: ARM poke tests and aeg graph analysis
    - F!=: float comparison tests (NaN/Inf handling)
    - +=[n], -=[n], etc. (all sizes): x86 plugin emits these via
      getarg() (e.g. add dword [addr], eax -> addr,+=[4])
    
    Also restore DFG counterparts for all re-added ops.
    Test update from 6860115 (expanded compound ops) is kept.

--- libr/esil/esil_ops.c
+++ libr/esil/esil_ops.c
@@ -1047,8 +1047,47 @@ static bool esil_poke8(REsil *esil) {
 	return esil_poke_n (esil, 64);
 }
 
+static bool esil_poke3(REsil *esil) {
+	return esil_poke_n (esil, 24);
+}
+
 static bool esil_poke16(REsil *esil) {
 	return esil_poke_n (esil, 128);
+}
+
+static bool esil_poke_some(REsil *esil) {
+	int i, regsize;
+	ut64 ptr, regs = 0, tmp;
+	const RStrs dst = r_esil_pop (esil);
+
+	if (!r_strs_empty (dst) && r_esil_get_parm_size (esil, dst, &tmp, &regsize)) {
+		isregornum (esil, dst, &ptr);
+		const RStrs count = r_esil_pop (esil);
+		if (!r_strs_empty (count)) {
+			isregornum (esil, count, &regs);
+			if (regs > 0) {
+				ut8 b[8] = {0};
+				ut64 num64;
+				for (i = 0; i < regs; i++) {
+					const RStrs foo = r_esil_pop (esil);
+					if (r_strs_empty (foo)) {
+						return true;
+					}
+					r_esil_get_parm_size (esil, foo, &tmp, &regsize);
+					isregornum (esil, foo, &num64);
+					r_write_ble (b, num64, esil_is_big_endian (esil), regsize);
+					const int size_bytes = regsize / 8;
+					const ut32 written = r_esil_mem_write (esil, ptr, b, size_bytes);
+					if (written != size_bytes) {
+						esil->trap = 1;
+					}
+					ptr += size_bytes;
+				}
+			}
+			return true;
+		}
+	}
+	return false;
 }
 
 /* PEEK */
@@ -1093,6 +1132,10 @@ static bool esil_peek1(REsil *esil) {
 
 static bool esil_peek2(REsil *esil) {
 	return esil_peek_n (esil, 16);
+}
+
+static bool esil_peek3(REsil *esil) {
+	return esil_peek_n (esil, 24);
 }
 
 static bool esil_peek4(REsil *esil) {
@@ -1475,6 +1518,21 @@ static bool esil_float_cmp(REsil *esil) {
 	return ret;
 }
 
+static bool esil_float_negcmp(REsil *esil) {
+	bool ret = false;
+	double s, d;
+	const RStrs dst = r_esil_pop (esil);
+	const RStrs src = r_esil_pop (esil);
+
+	if (esil_get_parm_float_strs (esil, src, &s) && esil_get_parm_float_strs (esil, dst, &d)) {
+		// NaN must compare unequal here, unlike F< / F<=
+		ret = r_esil_pushnum (esil, s != d);
+	} else {
+		R_LOG_DEBUG ("esil_float_negcmp: invalid parameters");
+	}
+	return ret;
+}
+
 static bool esil_float_less(REsil *esil) {
 	bool ret = false;
 	double s, d;
@@ -1796,6 +1854,7 @@ R_API bool r_esil_setup_ops(REsil *esil) {
 	ret &= OP ("=[4]", esil_poke4, 0, 2, OT_MEMW);
 	ret &= OP ("=[8]", esil_poke8, 0, 2, OT_MEMW);
 	ret &= OP ("=[16]", esil_poke16, 0, 2, OT_MEMW);
+	ret &= OP ("=[3]", esil_poke3, 0, 2, OT_MEMW);
 	ret &= OPD ("|=[1]", "DUP,ROT,SWAP,[1],|,SWAP,=[1]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
 	ret &= OPD ("|=[2]", "DUP,ROT,SWAP,[2],|,SWAP,=[2]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
 	ret &= OPD ("|=[4]", "DUP,ROT,SWAP,[4],|,SWAP,=[4]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
@@ -1809,14 +1868,46 @@ R_API bool r_esil_setup_ops(REsil *esil) {
 	ret &= OPD ("&=[4]", "DUP,ROT,SWAP,[4],&,SWAP,=[4]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
 	ret &= OPD ("&=[8]", "DUP,ROT,SWAP,[8],&,SWAP,=[8]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
 	ret &= OPD ("+=[1]", "DUP,ROT,SWAP,[1],+,SWAP,=[1]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("+=[2]", "DUP,ROT,SWAP,[2],+,SWAP,=[2]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("+=[4]", "DUP,ROT,SWAP,[4],+,SWAP,=[4]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("+=[8]", "DUP,ROT,SWAP,[8],+,SWAP,=[8]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
 	ret &= OPD ("-=[1]", "DUP,ROT,SWAP,[1],-,SWAP,=[1]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("-=[2]", "DUP,ROT,SWAP,[2],-,SWAP,=[2]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("-=[4]", "DUP,ROT,SWAP,[4],-,SWAP,=[4]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("-=[8]", "DUP,ROT,SWAP,[8],-,SWAP,=[8]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("%=[1]", "DUP,ROT,SWAP,[1],%,SWAP,=[1]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("%=[2]", "DUP,ROT,SWAP,[2],%,SWAP,=[2]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("%=[4]", "DUP,ROT,SWAP,[4],%,SWAP,=[4]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("%=[8]", "DUP,ROT,SWAP,[8],%,SWAP,=[8]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("/=[1]", "DUP,ROT,SWAP,[1],/,SWAP,=[1]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("/=[2]", "DUP,ROT,SWAP,[2],/,SWAP,=[2]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("/=[4]", "DUP,ROT,SWAP,[4],/,SWAP,=[4]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("/=[8]", "DUP,ROT,SWAP,[8],/,SWAP,=[8]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("*=[1]", "DUP,ROT,SWAP,[1],*,SWAP,=[1]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("*=[2]", "DUP,ROT,SWAP,[2],*,SWAP,=[2]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("*=[4]", "DUP,ROT,SWAP,[4],*,SWAP,=[4]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("*=[8]", "DUP,ROT,SWAP,[8],*,SWAP,=[8]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
 	ret &= OPD ("++=[1]", "DUP,[1],++,SWAP,=[1]", 0, 1, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("++=[2]", "DUP,[2],++,SWAP,=[2]", 0, 1, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("++=[4]", "DUP,[4],++,SWAP,=[4]", 0, 1, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("++=[8]", "DUP,[8],++,SWAP,=[8]", 0, 1, OT_MATH | OT_MEMR | OT_MEMW);
 	ret &= OPD ("--=[1]", "DUP,[1],--,SWAP,=[1]", 0, 1, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("--=[2]", "DUP,[2],--,SWAP,=[2]", 0, 1, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("--=[4]", "DUP,[4],--,SWAP,=[4]", 0, 1, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("--=[8]", "DUP,[8],--,SWAP,=[8]", 0, 1, OT_MATH | OT_MEMR | OT_MEMW);
 	ret &= OPD ("<<=[1]", "DUP,ROT,SWAP,[1],<<,SWAP,=[1]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("<<=[2]", "DUP,ROT,SWAP,[2],<<,SWAP,=[2]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("<<=[4]", "DUP,ROT,SWAP,[4],<<,SWAP,=[4]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD ("<<=[8]", "DUP,ROT,SWAP,[8],<<,SWAP,=[8]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
 	ret &= OPD (">>=[1]", "DUP,ROT,SWAP,[1],>>,SWAP,=[1]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD (">>=[2]", "DUP,ROT,SWAP,[2],>>,SWAP,=[2]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD (">>=[4]", "DUP,ROT,SWAP,[4],>>,SWAP,=[4]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
+	ret &= OPD (">>=[8]", "DUP,ROT,SWAP,[8],>>,SWAP,=[8]", 0, 2, OT_MATH | OT_MEMR | OT_MEMW);
 	ret &= OP ("[*]", esil_peek_some, 0, 0, OT_MEMR);
+	ret &= OP ("=[*]", esil_poke_some, 0, 0, OT_MEMW);
 	ret &= OP2 ("[1]", esil_peek1, 1, 1, OT_MEMR, "read 1 byte from address taken from stack push the byte value");
 	ret &= OP ("[2]", esil_peek2, 1, 1, OT_MEMR);
+	ret &= OP ("[3]", esil_peek3, 1, 1, OT_MEMR);
 	ret &= OP ("[4]", esil_peek4, 1, 1, OT_MEMR);
 	ret &= OP ("[8]", esil_peek8, 1, 1, OT_MEMR);
 	ret &= OP ("[16]", esil_peek16, 1, 1, OT_MEMR);
@@ -1846,6 +1937,7 @@ R_API bool r_esil_setup_ops(REsil *esil) {
 	ret &= OP ("D2F", esil_double_to_float, 1, 2, OT_MATH);
 	ret &= OP ("F2D", esil_float_to_double, 1, 2, OT_MATH);
 	ret &= OP ("F==", esil_float_cmp, 1, 2, OT_MATH);
+	ret &= OP ("F!=", esil_float_negcmp, 1, 2, OT_MATH);
 	ret &= OP ("F<", esil_float_less, 1, 2, OT_MATH);
 	ret &= OP ("F<=", esil_float_lesseq, 1, 2, OT_MATH);
 	ret &= OP ("F+", esil_float_add, 1, 2, OT_MATH);
