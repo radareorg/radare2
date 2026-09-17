c73eb312ceb9f133475cbd03ff5c00335f04e944
xXAbieGamingXx <65514695+xXAbieGamingXx@users.noreply.github.com>
2026-09-17 13:38:21 -0700

    S1C88 Review changes ##arch

--- libr/arch/p/s1c88/plugin.c
+++ libr/arch/p/s1c88/plugin.c
@@ -52,6 +52,22 @@ static char *s1c88_regs(RArchSession *as) {
 	return strdup (p);
 }
 
+/* start of variable bank area in logic space, 0x0-0x7fff is static */
+#define S1C88_BANK 0x8000
+
+static ut64 s1c88_phys(ut8 cb, ut16 pc) {
+	return (pc < S1C88_BANK)? pc: (((ut64)cb << 15) | (pc & (S1C88_BANK - 1)));
+}
+
+/* pc holds address in logic space */
+static ut16 s1c88_pc(ut64 addr) {
+	return (addr < S1C88_BANK)? (ut16)addr: (ut16) (S1C88_BANK | (addr & (S1C88_BANK - 1)));
+}
+
+static ut8 s1c88_cb(ut64 addr) {
+	return (addr < S1C88_BANK)? 0: (ut8) (addr >> 15);
+}
+
 static const s1c88_opcode *s1c88_lookup(const ut8 *b, int len, int *plen) {
 	if (len < 1) {
 		return NULL;
@@ -97,8 +113,13 @@ static bool s1c88_decode(RArchSession *as, RAnalOp *op, RArchDecodeMask mask) {
 	const ut8 *a = b + plen;
 	switch (arg) {
 	case S1C88_ARG_I8:
+		op->val = a[0];
+		break;
 	case S1C88_ARG_S8:
 		op->val = a[0];
+		if (! (e->type & S1C88_REL)) {
+			op->disp = (st8)a[0];
+		}
 		break;
 	case S1C88_ARG_I16:
 	case S1C88_ARG_S16:
@@ -111,14 +132,16 @@ static bool s1c88_decode(RArchSession *as, RAnalOp *op, RArchDecodeMask mask) {
 	}
 
 	if (e->type & S1C88_REL) {
-		const st64 disp = (arg == S1C88_ARG_S16) ? (st64)(st16)a[0] | (a[1] << 8) : (st64)(st8)a[0];
-		op->jump = (op->addr & 0xff0000ULL) | ((op->addr + size - 1 + disp) & 0xffff); /* calculated from the end of the opcode */
+		const st64 disp = (arg == S1C88_ARG_S16)? (st64) (st16) (a[0] | (a[1] << 8)): (st64) (st8)a[0];
+		const ut8 cb = s1c88_cb (op->addr);
+		const ut16 pc = s1c88_pc (op->addr);
+		op->jump = s1c88_phys (cb, (ut16) (pc + size - 1 + disp)); /* displacements count from the last byte of the opcode */
 		if (e->type & S1C88_COND) {
-			op->fail = op->addr + size;
+			op->fail = s1c88_phys (cb, (ut16) (pc + size));
 		}
 	}
 
-	op->eob = (e->type & S1C88_RET) || ((e->type & S1C88_JUMP) && !(e->type & (S1C88_COND | S1C88_CALL)));
+	op->eob = (e->type & S1C88_RET) || ((e->type & S1C88_JUMP) && ! (e->type & (S1C88_COND | S1C88_CALL)));
 
 	if (mask & R_ARCH_OP_MASK_DISASM) {
 		switch (arg) {
@@ -132,11 +155,12 @@ static bool s1c88_decode(RArchSession *as, RAnalOp *op, RArchDecodeMask mask) {
 			if (e->type & S1C88_REL) {
 				op->mnemonic = r_str_newf (e->name, (ut32)op->jump);
 			} else {
-				op->mnemonic = r_str_newf (e->name, a[0]);
+				const int d = (st8)a[0];
+				op->mnemonic = r_str_newf (e->name, (d < 0)? "-": "+", (ut32)R_ABS (d));
 			}
 			break;
 		case S1C88_ARG_I16:
-			op->mnemonic = r_str_newf (e->name, (ut32)(a[0] | (a[1] << 8)));
+			op->mnemonic = r_str_newf (e->name, (ut32) (a[0] | (a[1] << 8)));
 			break;
 		case S1C88_ARG_S16:
 			op->mnemonic = r_str_newf (e->name, (ut32)op->jump);
@@ -163,7 +187,7 @@ static char *s1c88_norm(const char *s, bool numbers) {
 				p++;
 			}
 
-			if (w == o || !*p || strchr (",][+", *p) || strchr (",[+", w[-1])) {
+			if (w == o || !*p || strchr (",][+-", *p) || strchr (",[+-", w[-1])) {
 				continue;
 			}
 			*w++ = ' ';
@@ -179,7 +203,7 @@ static char *s1c88_norm(const char *s, bool numbers) {
 				v = strtoull (p, &end, 10);
 			}
 			if (end && end != p) {
-				w += sprintf (w, "0x%"PFMT64x, v);
+				w += sprintf (w, "0x%" PFMT64x, v);
 				p = end;
 				continue;
 			}
@@ -192,7 +216,16 @@ static char *s1c88_norm(const char *s, bool numbers) {
 
 static int s1c88_match(const char *fmt, const char *in, ut64 *vals) {
 	int n = 0;
+	bool neg = false;
 	while (*fmt) {
+		if (fmt[0] == '%' && fmt[1] == 's') {
+			if (*in != '+' && *in != '-') {
+				return -1;
+			}
+			neg = (*in++ == '-');
+			fmt += 2;
+			continue;
+		}
 		if (fmt[0] == '0' && fmt[1] == 'x' && fmt[2] == '%') {
 			if (in[0] != '0' || in[1] != 'x' || n > 1) {
 				return -1;
@@ -204,9 +237,10 @@ static int s1c88_match(const char *fmt, const char *in, ut64 *vals) {
 			ut64 v = 0;
 			while (isxdigit ((unsigned char)*in)) {
 				const int c = (unsigned char)*in++;
-				v = (v << 4) | (ut64)(isdigit (c)? c - '0': (c | 32) - 'a' + 10);
+				v = (v << 4) | (ut64) (isdigit (c)? c - '0': (c | 32) - 'a' + 10);
 			}
-			vals[n++] = v;
+			vals[n++] = neg? 0 - v: v; /* two's complement, checked when emitted */
+			neg = false;
 			fmt += 3;
 			while (*fmt && *fmt != 'x') {
 				fmt++;
@@ -223,8 +257,7 @@ static int s1c88_match(const char *fmt, const char *in, ut64 *vals) {
 	return *in? -1: n;
 }
 
-static int s1c88_emit(ut8 *out, const s1c88_opcode *e, int prefix, ut8 opcode,
-		const ut64 *v, ut64 addr) {
+static int s1c88_emit(ut8 *out, const s1c88_opcode *e, int prefix, ut8 opcode, const ut64 *v, ut64 addr) {
 	const int arg = e->type & S1C88_ARG_MASK;
 	int plen = 0;
 	if (prefix >= 0) {
@@ -234,36 +267,43 @@ static int s1c88_emit(ut8 *out, const s1c88_opcode *e, int prefix, ut8 opcode,
 	const int size = plen + s1c88_arglen[arg];
 
 	if (e->type & S1C88_REL) {
-		if ((v[0] & ~0xffffULL) && (v[0] & 0xff0000ULL) != (addr & 0xff0000ULL)) {
-			return -1;
+		const ut8 cb = s1c88_cb (addr);
+		const ut16 target = s1c88_pc (v[0]);
+		if (s1c88_phys (cb, target) != v[0]) {
+			return -1; /* not reachable from this code bank */
 		}
-		const st64 disp = (st64)(st16)(ut16)(v[0] - (addr + size - 1));
+		const st64 disp = (st64) (st16) (ut16) (target - (s1c88_pc (addr) + size - 1));
 		if (arg == S1C88_ARG_S8) {
 			if (disp < -128 || disp > 127) {
 				return -1;
 			}
 			out[plen] = (ut8)disp;
 		} else {
-			out[plen] = (ut8)(disp & 0xff);
-			out[plen + 1] = (ut8)((disp >> 8) & 0xff);
+			out[plen] = (ut8) (disp & 0xff);
+			out[plen + 1] = (ut8) ((disp >> 8) & 0xff);
 		}
 		return size;
 	}
 	switch (arg) {
 	case S1C88_ARG_I8:
-	case S1C88_ARG_S8:
 		if (v[0] > 0xff) {
 			return -1;
 		}
 		out[plen] = (ut8)v[0];
+		break;
+	case S1C88_ARG_S8:
+		if ((st64)v[0] < -128 || (st64)v[0] > 127) {
+			return -1;
+		}
+		out[plen] = (ut8) (st8)v[0];
 		break;
 	case S1C88_ARG_I16:
 	case S1C88_ARG_S16:
 		if (v[0] > 0xffff) {
 			return -1;
 		}
-		out[plen] = (ut8)(v[0] & 0xff);
-		out[plen + 1] = (ut8)((v[0] >> 8) & 0xff);
+		out[plen] = (ut8) (v[0] & 0xff);
+		out[plen + 1] = (ut8) ((v[0] >> 8) & 0xff);
 		break;
 	case S1C88_ARG_I8I8:
 		if (v[0] > 0xff || v[1] > 0xff) {
@@ -277,7 +317,10 @@ static int s1c88_emit(ut8 *out, const s1c88_opcode *e, int prefix, ut8 opcode,
 }
 
 static int s1c88_asm_exact(ut8 *out, const char *in, ut64 addr) {
-	const struct { const s1c88_opcode *t; int prefix; } pages[] = {
+	const struct {
+		const s1c88_opcode *t;
+		int prefix;
+	} pages[] = {
 		{ s1c88_op, -1 }, { s1c88_op_ce, 0xce }, { s1c88_op_cf, 0xcf }
 	};
 	size_t pi;
