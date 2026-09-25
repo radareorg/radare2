0ef5ce12b252b36ca298d545e786e5817ff4b065
pancake <pancake@nowsecure.com>
2026-09-25 16:11:58 +0200

    Initialize empty cmdctx argument storage

--- libr/core/cmd_api.c
+++ libr/core/cmd_api.c
@@ -481,6 +481,7 @@ static bool cmd_context_parse_args(RCmdContext *context, RStrs rest, bool raw) {
 		return false;
 	}
 	context->args_storage = storage;
+	*storage = 0;
 	const char *src = rest.a;
 	char *dst = storage;
 	while (src < rest.b) {
