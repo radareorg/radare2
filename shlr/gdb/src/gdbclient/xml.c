ab19369517d503662661320c2f1ca642338434e2
Priyanshu Kumar <priyanshukumarpu@gmail.com>
2026-09-25 13:42:38 +0000

    Place target.xml registers at the offsets lldb stubs state ##debug

--- shlr/gdb/src/gdbclient/xml.c
+++ shlr/gdb/src/gdbclient/xml.c
@@ -178,6 +178,7 @@ typedef struct {
 	char type[8];
 	ut32 size;
 	ut32 flagnum;
+	ut64 offset; // in bits; UT64_MAX when the description does not state it
 } gdbr_xml_reg_t;
 
 static void _write_flag_bits(RStrBuf *buf, const gdbr_xml_flags_t *flags);
@@ -221,9 +222,12 @@ static int gdbr_parse_target_xml(libgdbr_t *g, char *xml_data, ut64 len) {
 		if (!tmpreg) {
 			continue;
 		}
+		// lldb's stubs state where a register sits in the 'g' block, which need not follow
+		// regnum order; gdb's convention is that the registers follow each other
+		const ut64 off = tmpreg->offset != UT64_MAX? tmpreg->offset: regoff;
 		r_str_ncpy (arch_regs[regnum].name, tmpreg->name, sizeof (arch_regs[regnum].name));
 		arch_regs[regnum].size = tmpreg->size;
-		arch_regs[regnum].offset = regoff;
+		arch_regs[regnum].offset = off;
 		r_strbuf_set (flag_bits, "");
 		tmpflag = NULL;
 		if (tmpreg->flagnum < r_list_length (flags)) {
@@ -235,7 +239,7 @@ static int gdbr_parse_target_xml(libgdbr_t *g, char *xml_data, ut64 len) {
 			packed_size = tmpreg->size / 8;
 		}
 		r_strbuf_appendf (profile_buf, "%s\t%s\t.%u\t.%" PFMT64d "\t%d\t%s\n", tmpreg->type,
-			tmpreg->name, tmpreg->size, regoff,
+			tmpreg->name, tmpreg->size, off,
 			packed_size,
 			r_strbuf_get (flag_bits));
 		// TODO write flag subregisters
@@ -244,11 +248,11 @@ static int gdbr_parse_target_xml(libgdbr_t *g, char *xml_data, ut64 len) {
 			for (i = 0; i < tmpflag->num_fields; i++) {
 				r_strbuf_appendf (profile_buf, "gpr\t%s\t"
 							".%u\t.%"PFMT64d"\t0\n", tmpflag->fields[i].name,
-							tmpflag->fields[i].sz, tmpflag->fields[i].bit_num + regoff);
+							tmpflag->fields[i].sz, tmpflag->fields[i].bit_num + off);
 			}
 		}
 		regnum++;
-		regoff += tmpreg->size;
+		regoff = R_MAX (regoff, off + tmpreg->size);
 	}
 	// Difficult to parse these out from xml. So manually added from gdb's xml files
 	switch (g->target.arch) {
@@ -328,6 +332,7 @@ static int gdbr_parse_target_xml(libgdbr_t *g, char *xml_data, ut64 len) {
 	r_strbuf_free (pc_alias);
 	r_strbuf_free (flag_bits);
 	g->target.valid = true;
+	free (g->registers);
 	g->registers = arch_regs;
 	return 0;
 
@@ -549,7 +554,11 @@ static int _resolve_arch(libgdbr_t *g, char *xml_data) {
 			if (r_str_startswith (arch, ":x86-64")) {
 				g->target.bits = 64;
 			}
-		} else if (r_str_startswith (arch, "aarch64")) {
+		} else if (r_str_startswith (arch, "x86_64")) {
+			// lldb-server
+			g->target.arch = R_SYS_ARCH_X86;
+			g->target.bits = 64;
+		} else if (r_str_startswith (arch, "aarch64") || r_str_startswith (arch, "arm64")) {
 			g->target.arch = R_SYS_ARCH_ARM;
 			g->target.bits = 64;
 		} else if (r_str_startswith (arch, "arm")) {
@@ -828,6 +837,20 @@ static RList *_extract_regs(char *regstr, RList *flags, RStrBuf *pc_alias) {
 			}
 			regnum = (ut32)parsed_regnum;
 		}
+		// offset, in bytes
+		ut64 regoffset = UT64_MAX;
+		if ((tmp1 = strstr (regstr, " offset="))) {
+			tmp1 += 9;
+			if (!isdigit ((ut8)*tmp1)) {
+				goto exit_err;
+			}
+			char *end;
+			unsigned long parsed_offset = strtoul (tmp1, &end, 10);
+			if (*end != '"' || parsed_offset > UT32_MAX / 8) {
+				goto exit_err;
+			}
+			regoffset = (ut64)parsed_offset * 8;
+		}
 		flagnum = r_list_length (flags);
 		if ((tmp1 = strstr (regstr, "group="))) {
 			tmp1 += 7;
@@ -891,6 +914,7 @@ static RList *_extract_regs(char *regstr, RList *flags, RStrBuf *pc_alias) {
 		r_str_ncpy (tmpreg->type, regtype, sizeof (tmpreg->type));
 		tmpreg->size = regsize;
 		tmpreg->flagnum = flagnum;
+		tmpreg->offset = regoffset;
 		if (code_ptr) {
 			r_strbuf_set (pc_alias, "=PC\t");
 			r_strbuf_append (pc_alias, tmpreg->name);
